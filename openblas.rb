42441dcb11f5cdbff2ce46a556dfd3c7a25d4cdd
samueljohn <github@samueljohn.de>
2012-08-24 16:46:46 +0200

    openblas: 0.2.4 and HEAD and with LAPACK.
    - Version bump! Huge one.
    - Enable built of (patched) LAPACK to get both: BLAS/LAPACK
    - LAPACK is downloaded by us, cached and checksum-ed.
    - Adding HEAD
    - Passing FC to make, so it is possible to use a
      custom gfortran (e.g. the gfortran-4.7 from
      the homebrew-dupes/gcc.rb formula).
    - Work around a (probably) superenv bug for brew's default
      gfortran. (switching to env :std)
    
    Signed-off-by: Samuel John <github@SamuelJohn.de>

--- openblas.rb
+++ openblas.rb
@@ -1,16 +1,43 @@
 require 'formula'
 
+# Avoid openblas downloading the LAPACK on demand.
+# We want openblas to build LAPACK because it knows how to patch LAPACK,
+# but we want to download, cache and checksum that!
+class OpenblasLapack < Formula
+  url 'http://www.netlib.org/lapack/lapack-3.4.2.tgz'
+  sha1 '93a6e4e6639aaf00571d53a580ddc415416e868b'
+end
+
 class Openblas < Formula
   homepage 'http://xianyi.github.com/OpenBLAS/'
-  url 'http://github.com/xianyi/OpenBLAS/zipball/v0.1.0'
-  sha1 '73b6df95993db106b922d8739e273699055c6e79'
+  # Maintainers, remember to update the LAPACK url in OpenblasLapack above.
+  # See the "LAPACK_URL" in the openblas Makefile for the right version.
+  url 'http://github.com/xianyi/OpenBLAS/zipball/v0.2.4'
+  sha1 'd3c17f7723a0a23022d8de1efcfcf4ace5a7d595'
+  head "https://github.com/xianyi/OpenBLAS.git"
 
+  # OS X provides the Accelerate.framework, which is a BLAS/LAPACK impl.
   keg_only :provided_by_osx
+
+  # Currently superenv works only with setting FC to gfortran-4.7 from
+  # homebrew/dupes/gcc (`brew install gcc --enable-fortran`).
+  # It's (probably) a superenv-bug that openblas does currently not work with
+  # gfortran from `brew install gfortran`, therefore:
+  env :std unless ENV['FC']
 
   def install
     ENV.fortran
+
+    lapack = OpenblasLapack.new
+    lapack.brew{}  # download and checksum
+    ohai "Using LAPACK: #{lapack.cached_download}"
+
+    inreplace 'Makefile',
+              'LAPACK_URL=http://www.netlib.org/lapack/lapack-3.4.2.tgz',
+              "LAPACK_URL=file://#{lapack.cached_download}"
+
     # Must call in two steps
-    system "make", "NOLAPACK=1"
-    system "make", "NOLAPACK=1", "PREFIX=#{prefix}", "install"
+    system "make", "FC=#{ENV['FC']}"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 end
