e6758ca05c8174d364aa97451b0e5aa84deeca89
Guilherme Brondani Torri <guitorri@gmail.com>
2012-09-26 18:48:54 +0200

    New Formula: rml-mmc 2.5.5
    Relational Meta-Language (RML) is used as a practical
    language for Natural Semantics in programming
    language implementations.
    
    RML is used to implement the MetaModelica compiler (MMC)
    on the OpenModelica project (https://www.openmodelica.org/).
    
    * This formula needs the StandardML of New Jersey (smlnj) to build RML.

---
+++ rml-mmc.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class RmlMmc < Formula
+  homepage 'http://www.ida.liu.se/labs/pelab/rml'
+  url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_255.orig.tar.gz'
+  version '2.5.5'
+  sha1 'ab173438a56f344904f6d02ccc8d6a360896219d'
+
+  # Attention, has a self-signed certificate and svn will prompt you, so
+  # do not use --HEAD as a dependency or automatic installation (built-bot).
+  head 'https://openmodelica.org/svn/MetaModelica/trunk', :using => :svn
+
+  depends_on 'smlnj'
+
+  def install
+    ENV.j1
+    # Change the next line to Formula.factory("smlnj").prefix/'SMLNJ_HOME'
+    # once https://github.com/mxcl/homebrew/pull/16355 is pulled!
+    ENV['SMLNJ_HOME'] = Formula.factory("smlnj").libexec
+    system "./configure --prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "#{bin}/rml", "-v"
+  end
+end
