c76193d1552e6afe83af5ed5c879c2fa742a55c8
Jonathan Goodson <jgoodson@umd.edu>
2012-11-20 15:30:43 -0500

    Added TransTermHP 2.09

---
+++ transtermhp.rb
@@ -0,0 +1,20 @@
+require 'formula'
+class Transtermhp < Formula
+  homepage 'http://transterm.cbcb.umd.edu/'
+  url 'http://transterm.cbcb.umd.edu/transterm_hp_v2.09.zip'
+  sha1 '492f4246f4c6629a315f921dae53526e0aaaa93a'
+  def install
+    system "make"
+    bin.install %W(transterm calibrate.sh)
+    (share/'transtermhp').install 'USAGE.txt'
+  end
+
+  def caveats
+    "Usage information: #{HOMEBREW_PREFIX}/share/transtermhp/USAGE.txt"
+  end
+
+  def test
+    # `transterm -h` sends to stderr, so we send it to stdin of grep
+    system "#{bin}/transterm -h 2>&1 | grep 'usage: transterm'"
+  end
+end
