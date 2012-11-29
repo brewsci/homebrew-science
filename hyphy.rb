5fcc55fbfb30002a607e15e2a90938c79a5c1a52
Jonathan Chang <jchang641@gmail.com>
2012-11-28 19:06:20 -0800

    HyPhy: New formula
    - Fixes #38

---
+++ hyphy.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Hyphy < Formula
+  homepage 'http://www.hyphy.org/'
+  url 'https://github.com/veg/hyphy/archive/2.1.2.tar.gz'
+  sha1 '3e8708256c92ee84c0ade5a69b20070fa20e103f'
+  head 'https://github.com/veg/hyphy.git'
+
+  depends_on 'cmake' => :build
+
+  def patches
+    # Single-threaded builds. This commit is from upstream and will
+    # be included in the next release.
+    'https://github.com/veg/hyphy/commit/c90bf9b93218a38e4c9f12150ffe36eb.diff'
+  end
+
+  def install
+    system "cmake", "-DINSTALL_PREFIX=#{prefix}", ".", *std_cmake_args
+    system "make SP"
+    system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    This formula builds a single-threaded version of HyPhy, as multithreaded
+    builds occasionally hang on certain analyses.
+    EOS
+  end
+end
