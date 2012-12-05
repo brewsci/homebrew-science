823933b02a0af133c53f74ab9c53509f64c83b99
Samuel John <github@SamuelJohn.de>
2012-12-05 22:13:54 +0100

    infernal: Added option for a make check

--- infernal.rb
+++ infernal.rb
@@ -5,9 +5,12 @@ class Infernal < Formula
   url 'ftp://selab.janelia.org/pub/software/infernal/infernal-1.1rc1.tar.gz'
   sha1 '89077437c53d36fac37ba1e677ce05c112cd7588'
 
+  option 'check', 'Run the test suite (`make check`). Takes a couple of minutes.'
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
+    system "make check" if build.include? 'check'
     system "make install"
   end
 
