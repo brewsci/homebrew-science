7611e9f25f1410202cb4ce20c9abcba54bf3413e
Samuel John <github@SamuelJohn.de>
2012-12-12 16:20:29 +0100

    mrbayes: Fix mpi compile issue, use beagle formula
    - Fails with llvm-gcc
    - Fix open-mpi to NOT use llvm when mpicc is called
    - Installed doc and examples unless --HEAD

--- mrbayes.rb
+++ mrbayes.rb
@@ -7,25 +7,54 @@ class Mrbayes < Formula
   head 'https://mrbayes.svn.sourceforge.net/svnroot/mrbayes/trunk/', :using => :svn
 
   option 'with-beagle', 'Build with BEAGLE library support'
+
   option 'with-mpi', 'Build with MPI parallel support'
-  option 'no-fast-math', 'Build with default Homebrew optimizations'
 
   depends_on :autoconf => :build
+  depends_on :automake => :build
   depends_on MPIDependency.new(:cc) if build.include? 'with-mpi'
+  depends_on 'beagle' => :optional if build.include? 'with-beagle'
+
+  fails_with :llvm do
+    build 2336
+    cause "build hangs at calling `as`: http://sourceforge.net/tracker/index.php?func=detail&aid=3426528&group_id=129302&atid=714418"
+  end
 
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}"]
-    args << "--with-beagle=no" unless build.include? 'with-beagle'
-    args << "--enable-mpi=yes" if build.include? 'with-mpi'
-    ENV.delete 'CFLAGS' unless build.include? 'no-fast-math'
+    args = ["--disable-debug", "--prefix=#{prefix}"]
+
+    if build.include? 'with-beagle'
+      args << "--with-beagle=#{Formula.factory('beagle').opt_prefix}"
+    else
+      args << "--with-beagle=no"
+    end
+
+    if build.include? 'with-mpi'
+      # Open-mpi builds only with llvm-gcc due to a bug (see open-mpi formula)
+      # therefore open-mpi attempts to run llvm-gcc instead of clang.
+      # But MrBayes hangs with llvm-gcc!
+      # https://sourceforge.net/tracker/index.php?func=detail&aid=3426528&group_id=129302&atid=714418
+      ENV['OMPI_CC'] = 'clang'
+      args << "--enable-mpi=yes"
+    end
 
     cd 'src' do
       system "autoconf"
       system "./configure", *args
       system "make"
       bin.install "mb"
+    end
+
+    # Doc and examples are not included in the svn
+    (share/'mrbayes').install ['documentation', 'examples'] unless build.head?
+  end
+
+  def caveats
+    unless build.head?
+      <<-EOS.undent
+        The documentation and examples are installed to
+            #{HOMEBREW_PREFIX}/share/mrbayes
+      EOS
     end
   end
 
