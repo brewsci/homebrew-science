c3420e298ab32bff17dc9cac3fa4f14578648886
Samuel John <github@SamuelJohn.de>
2012-12-13 20:16:44 +0100

    fast-statistical-alignment: Fails with clang 421
    Fixes #42

--- fast-statistical-alignment.rb
+++ fast-statistical-alignment.rb
@@ -6,7 +6,7 @@ class FastStatisticalAlignment < Formula
   sha1 '322c8270d027b573b0781d8729f0917206e9d775'
 
   fails_with :clang do
-    build 412
+    build 421
     cause <<-EOS.undent
       In file included from ../../src/annealing/dotplot.h:14:
       ../../src/util/array2d.h:258:82: error: member reference base type 'array2d_type *const' (aka 'array2d<value_type, xy_container_type> *const') is not a
