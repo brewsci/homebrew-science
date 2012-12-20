8a8359da29816d901ab11fc1b9fcceb0cd5bdcda
Samuel John <github@SamuelJohn.de>
2012-12-20 17:55:10 +0100

    paml: 4.7 as --devel
    - Install examples, too
    - Caveat about doc, examples, dat and ctl files.

--- paml.rb
+++ paml.rb
@@ -5,6 +5,11 @@ class Paml < Formula
   url 'http://abacus.gene.ucl.ac.uk/software/paml4.6.tgz'
   sha1 '91440572c6c42db05716358cb3977d23db930fc3'
 
+  devel do
+    url 'http://abacus.gene.ucl.ac.uk/software/paml4.7.tgz'
+    sha1 'a741db87aadcab7afa9be522f55f1875dcc660f0'
+  end
+
   def install
     cd 'src' do
       system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
@@ -14,5 +19,15 @@ class Paml < Formula
     (share+'paml').install 'dat'
     (share+'paml').install Dir['*.ctl']
     doc.install Dir['doc/*']
+    doc.install 'examples'
+  end
+
+  def caveats
+    <<-EOS.undent
+      Documentation and examples:
+        #{HOMEBREW_PREFIX}/share/doc/paml
+      Dat and ctl files:
+        #{HOMEBREW_PREFIX}/share/paml
+    EOS
   end
 end
