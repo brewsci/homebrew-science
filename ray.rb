ae2b83d8917345c330772afeeb939f4ddcbe7412
Shaun Jackman <sjackman@gmail.com>
2012-12-20 16:29:24 -0800

    Ray 2.1.0: New formula
    - Uses MPIDependency
    - Fails with llvm-gcc
    - has a test

---
+++ ray.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Ray < Formula
+  homepage 'http://denovoassembler.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/denovoassembler/Ray-v2.1.0.tar.bz2'
+  sha1 '4c09f2731445852857af53b65aa47e444792eeb0'
+  # The head does not build right now. Can you help us?
+  # head 'https://github.com/sebhtml/ray.git'
+
+  depends_on MPIDependency.new(:cxx)
+
+  fails_with :llvm do
+    build 2336
+    cause '"___gxx_personality_v0" ... ld: symbol(s) not found for architecture x86_64'
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make install"
+    bin.install 'Ray'
+  end
+
+  test do
+    system bin/'Ray', '--version'
+  end
+end
