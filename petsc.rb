decfb66a87f00c524740f2f7ef801f742b01f104
Kristofer Henriksson <kthenriksson@gmail.com>
2012-12-23 21:40:45 -0800

    petsc: Add formula for PETSc scientific library
    Fixes #49

---
+++ petsc.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Petsc < Formula
+  homepage 'http://www.mcs.anl.gov/petsc/index.html'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz '
+  sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
+
+  depends_on MPIDependency.new(:cc)
+
+  def install
+    ENV.deparallelize
+    ENV.fortran
+
+    system "./configure", "--with-debugging=0", "--prefix=#{prefix}"
+    system "make all"
+    system "make test"
+    system "make install"
+  end
+end
