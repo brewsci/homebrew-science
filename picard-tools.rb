4c926ae195cf7125437b097247d225f8302c16b6
roryk <rory.kirchner@gmail.com>
2012-12-30 13:53:31 -0500

    picard-tools: Added picard tools (JAR) files.
    Fixes #51

---
+++ picard-tools.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class PicardTools < Formula
+  homepage 'http://picard.sourceforge.net/'
+  url 'http://sourceforge.net/projects/picard/files/picard-tools/1.82/picard-tools-1.82.zip'
+  sha1 '89ab83988425560acbbd2294763d4f8429a7e70d'
+
+  def install
+    (share/'java').install Dir['*.jar']
+    (share/'java').install Dir['picard-tools-1.82/*.jar']
+  end
+
+  def caveats
+    <<-EOS.undent
+      The Java JAR files are installed to
+          #{HOMEBREW_PREFIX}/share/java
+    EOS
+  end
+end
