aacb459a24452074955e3e19db44b45b022203a1
Shaun Jackman <sjackman@gmail.com>
2013-01-07 15:11:36 -0800

    Bowtie2 2.0.5

--- bowtie2.rb
+++ bowtie2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bowtie2 < Formula
   homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
-  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.0.2/bowtie2-2.0.2-source.zip'
-  sha1 'b310061a3ff265c386cea6f838e0bafa0268f0b8'
+  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.0.5/bowtie2-2.0.5-source.zip'
+  sha1 '2f0cc813abec0e297e728352e350b561aff95347'
 
   def install
     system "make"
