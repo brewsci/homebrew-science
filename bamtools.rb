d8b24ba4ad4dc64e282b3fee8c36c632b83960ab
Shaun Jackman <sjackman@gmail.com>
2013-01-15 10:08:12 -0800

    bamtools: 2.2.3
    Update the patch
    
    Fixes #71 and Fixes #69

--- bamtools.rb
+++ bamtools.rb
@@ -2,12 +2,9 @@ require 'formula'
 
 class Bamtools < Formula
   homepage 'https://github.com/pezmaster31/bamtools'
-  url 'https://github.com/downloads/pezmaster31/bamtools/bamtools-1.0.2.tar.gz'
-  sha1 '70d4a1f8d7da73dd381b609b618ed19b6184366e'
-
-  # Unfortunately, our patch does not apply to head but the upstream issue #55
-  # is still not fixed.
-  # head 'https://github.com/pezmaster31/bamtools.git'
+  url 'https://github.com/pezmaster31/bamtools/archive/v2.2.3.tar.gz'
+  sha1 '130c20f13948e007516d2d0ec068bcb9685fc8d4'
+  head 'https://github.com/pezmaster31/bamtools.git'
 
   depends_on 'cmake' => :build
 
@@ -32,11 +29,12 @@ class Bamtools < Formula
 end
 
 __END__
-diff -ur bamtools-1.0.2.orig/src/api/CMakeLists.txt bamtools-1.0.2/src/api/CMakeLists.txt
---- bamtools-1.0.2.orig/src/api/CMakeLists.txt	2011-09-09 18:59:44.000000000 -0700
-+++ bamtools-1.0.2/src/api/CMakeLists.txt	2012-06-15 10:46:18.000000000 -0700
-@@ -54,8 +54,8 @@
- target_link_libraries( BamTools-static z )
+diff --git a/src/api/CMakeLists.txt b/src/api/CMakeLists.txt
+index 66eb35f..c4df349 100644
+--- a/src/api/CMakeLists.txt
++++ b/src/api/CMakeLists.txt
+@@ -54,8 +54,8 @@ target_link_libraries( BamTools ${APILibs} )
+ target_link_libraries( BamTools-static ${APILibs} )
  
  # set library install destinations
 -install( TARGETS BamTools LIBRARY DESTINATION "lib/bamtools" RUNTIME DESTINATION "bin")
@@ -46,27 +44,29 @@ diff -ur bamtools-1.0.2.orig/src/api/CMakeLists.txt bamtools-1.0.2/src/api/CMake
  
  # export API headers
  include(../ExportHeader.cmake)
-diff -ur bamtools-1.0.2.orig/src/third_party/jsoncpp/CMakeLists.txt bamtools-1.0.2/src/third_party/jsoncpp/CMakeLists.txt
---- bamtools-1.0.2.orig/src/third_party/jsoncpp/CMakeLists.txt	2012-06-15 10:39:40.000000000 -0700
-+++ bamtools-1.0.2/src/third_party/jsoncpp/CMakeLists.txt	2012-06-15 10:41:49.000000000 -0700
-@@ -10,7 +10,7 @@
+diff --git a/src/third_party/jsoncpp/CMakeLists.txt b/src/third_party/jsoncpp/CMakeLists.txt
+index 03c091b..cf95791 100644
+--- a/src/third_party/jsoncpp/CMakeLists.txt
++++ b/src/third_party/jsoncpp/CMakeLists.txt
+@@ -10,7 +10,7 @@ add_definitions( -DBAMTOOLS_JSONCPP_LIBRARY ) # (for proper exporting of library
  add_definitions( -fPIC ) # (attempt to force PIC compiling on CentOS, not being set on shared libs by CMake)
  
  # create jsoncpp library
--add_library ( jsoncpp SHARED 
-+add_library ( jsoncpp STATIC 
-               json_reader.cpp
-               json_value.cpp
-               json_writer.cpp
-diff -ur bamtools-1.0.2.orig/src/utils/CMakeLists.txt bamtools-1.0.2/src/utils/CMakeLists.txt
---- bamtools-1.0.2.orig/src/utils/CMakeLists.txt	2012-06-15 10:39:40.000000000 -0700
-+++ bamtools-1.0.2/src/utils/CMakeLists.txt	2012-06-15 10:41:34.000000000 -0700
-@@ -13,7 +13,7 @@
+-add_library( jsoncpp SHARED
++add_library( jsoncpp STATIC
+              json_reader.cpp
+              json_value.cpp
+              json_writer.cpp
+diff --git a/src/utils/CMakeLists.txt b/src/utils/CMakeLists.txt
+index 2d91ca3..b38859c 100644
+--- a/src/utils/CMakeLists.txt
++++ b/src/utils/CMakeLists.txt
+@@ -13,7 +13,7 @@ add_definitions( -DBAMTOOLS_UTILS_LIBRARY ) # (for proper exporting of library s
  add_definitions( -fPIC ) # (attempt to force PIC compiling on CentOS, not being set on shared libs by CMake)
  
  # create BamTools utils library
--add_library ( BamTools-utils SHARED 
-+add_library ( BamTools-utils STATIC 
-               bamtools_fasta.cpp
-               bamtools_options.cpp
-               bamtools_pileup_engine.cpp
+-add_library( BamTools-utils SHARED
++add_library( BamTools-utils STATIC
+              bamtools_fasta.cpp
+              bamtools_options.cpp
+              bamtools_pileup_engine.cpp
