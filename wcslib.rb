5cd714483e35fccf14b2b1b28fe09d1aa8b4845a
Samuel John <github@SamuelJohn.de>
2013-01-16 11:28:21 +0100

    wcslib: PGSBOX (plotting) & fortran & cfitsio
    - Fortran wrappers (--with-fortran)
    - PGSBOX plotting with pgplot (needs X11)
    - CFITSIO lib dependency build and linked
    - Option to do `make check`

--- wcslib.rb
+++ wcslib.rb
@@ -3,13 +3,38 @@ require 'formula'
 class Wcslib < Formula
   homepage 'http://www.atnf.csiro.au/people/mcalabre/WCS/'
   url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.16.tar.bz2'
-  sha1 '8c530c29866ca3414f62d27f439b3d5e6dcb39a1'
+  sha1 'd321ee3c82ecb14e447006ed475f3f0217aa589f'
+
+  option 'with-pgsbox', 'Build PGSBOX, a general curvilinear axis drawing routine for PGPLOT'
+  option 'with-fortran', "Build Fortran wrappers. Needed for --with-pgsbox."
+  option 'check', "Perform `make check`. Note, together --with-pgsbox it will display GUI"
+
+  depends_on 'cfitsio'
+  depends_on 'pgplot' if build.include? 'with-pgsbox'
+  depends_on :x11 if build.include? 'with-pgsbox'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    ENV.fortran if build.include? 'with-fortran' or build.include? 'with-pgsbox'
+
+    args = [ "--disable-debug", 
+             "--disable-dependency-tracking",
+             "--prefix=#{prefix}",
+             "--with-cfitsiolib=#{Formula.factory('cfitsio').opt_prefix}/lib",
+             "--with-cfitsioinc=#{Formula.factory('cfitsio').opt_prefix}/include" ]
+    
+    if build.include? 'with-pgsbox'
+      args << "--with-pgplotlib=#{Formula.factory('pgplot').opt_prefix}/lib"
+      args << "--with-pgplotinc=#{Formula.factory('pgplot').opt_prefix}/include"
+    else
+      args << "--without-pgplot"
+      args << "--disable-fortran" unless build.include? 'with-fortran'
+    end
+
+    system "./configure", *args
+                          
     ENV.deparallelize
     system "make"
+    system "make check" if build.include? 'check'
     system "make install"
   end
 end
