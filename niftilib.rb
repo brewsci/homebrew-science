a1ffd8a60252c963f7101ec78ae11464d2633007
Mark Howison <mhowison@brown.edu>
2013-01-17 09:22:16 -0500

    niftilib: 2.0.0
    I/O library for the NIFTI file format for MRI and fMRI brain images
    http://niftilib.sourceforge.net

---
+++ niftilib.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Niftilib < Formula
+  homepage 'http://niftilib.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/niftilib/nifticlib/nifticlib_2_0_0/nifticlib-2.0.0.tar.gz'
+  sha1 '3a6187cb09767f97cef997cf492d89ac3db211df'
+
+  def install
+    ENV.deparallelize
+    system "make"
+    bin.install Dir['bin/*']
+    lib.install Dir['lib/*']
+    include.install Dir['include/*']
+  end
+end
