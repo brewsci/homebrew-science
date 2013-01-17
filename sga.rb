f3af4117fd9cbd074ed7efe235f3ebcd06f6b698
Shaun Jackman <sjackman@gmail.com>
2013-01-17 10:51:04 -0800

    SGA 0.9.42
    HEAD once again builds with clang, since jts/sga#35 is fixed.
    
    Closes #74

--- sga.rb
+++ sga.rb
@@ -2,11 +2,9 @@ require 'formula'
 
 class Sga < Formula
   homepage 'https://github.com/jts/sga'
-  url 'https://github.com/jts/sga/tarball/v0.9.37'
-  sha1 '7bd04b2001e03dcd959215a4dd6a0fe2bbd3ff89'
-
-  # Currently deactivated due to "undeclared identifier omp_set_num_threads"
-  # head 'https://github.com/jts/sga.git'
+  url 'https://github.com/jts/sga/tarball/v0.9.42'
+  sha1 'dd2c778cbfc763f2877d07a6b67d35a59c71ce61'
+  head 'https://github.com/jts/sga.git'
 
   depends_on :autoconf => :build
   depends_on :automake => :build
