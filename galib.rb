806cd6d228d8fba34818bfe095898f6053c58cf9
Roberto Esposito <esposito@di.unito.it>
2013-01-21 11:20:46 +0100

    galib: 2.4.7
    galib is a c++ library for working with genetic algorithms. It is
    currently not included neither in homebrew/master nor in
    homebrew/homebrew-science.
    
    Added a 'make test' call just before the installation step. The test
    should end in about 15s on a fast machine.

---
+++ galib.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Galib < Formula
+  homepage 'http://lancet.mit.edu/ga/'
+  url 'http://lancet.mit.edu/ga/dist/galib247.tgz'
+  version '2.4.7'
+  sha1 '3411da19d6b5b67638eddc4ccfab37a287853541'
+
+  def install
+    mkdir "#{prefix}/lib"
+    system "make"
+    system "make test"
+    system "make DESTDIR=#{prefix} install"
+  end
+end
