90d6e50605755ee552c75182934c3cde6d316301
Dominique Orban <dominique.orban@gmail.com>
2013-01-23 13:25:49 -0500

    CoinUtils: 2.8.8
    CoinUtils installs basic tools used by other COIN-OR projects.
    See www.coin-or.org.

---
+++ coinutils.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Coinutils < Formula
+  homepage 'http://www.coin-or.org/projects/CoinUtils.xml'
+  url 'http://www.coin-or.org/download/source/CoinUtils/CoinUtils-2.8.8.tgz'
+  sha1 '64791e9c5f2dc50b891dd2b0d710ddf81d302864'
+
+  def install
+    ENV.fortran
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
+  end
+end
