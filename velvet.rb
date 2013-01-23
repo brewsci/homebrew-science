2bd8df58055114d21f977710ca44c51a9f4e1089
Mark Howison <mhowison@brown.edu>
2013-01-23 10:24:54 -0500

    velvet: sha1 update & option to support long kmers
    Velvet is usually built to support a maximum kmer size of either 31 or 63
    and this patch adds a '--with-63mer' option to build the latter.
    
    -  option to support longer kmers (alternate)
       Uses the values of the MAXKMERLENGTH env. var.
    
    Closes #76

--- velvet.rb
+++ velvet.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Velvet < Formula
   homepage 'http://www.ebi.ac.uk/~zerbino/velvet/'
   url 'http://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.08.tgz'
-  sha1 'cef486759fa577d86ff67d70898ff330f5d0403c'
+  sha1 '81432982c6a0a7fe8e5dd46fd5e88193dbd832aa'
 
   head 'https://github.com/dzerbino/velvet.git'
 
@@ -12,7 +12,25 @@ class Velvet < Formula
       # recommended in Makefile for compiling on Mac OS X
       s.change_make_var! "CFLAGS", "-Wall -m64"
     end
-    system "make velveth velvetg OPENMP=1 MAXKMERLENGTH=31 LONGSEQUENCES=1"
+
+    args = ["OPENMP=1", "LONGSEQUENCES=1"]
+    if ENV['MAXKMERLENGTH']
+      args << ("MAXKMERLENGTH=" + ENV['MAXKMERLENGTH'])
+    end
+
+    system "make", "velveth", "velvetg", *args
     bin.install 'velveth', 'velvetg'
+  end
+
+  def caveats
+    <<-EOS.undent
+      If you want to build with a different kmer length, you can set
+      MAXKMERLENGTH=X to a value (X) *before* you brew this formula.
+    EOS
+  end
+
+  def test
+    system "velveth --help"
+    system "velvetg --help"
   end
 end
