a36a0666a3b8e0276d1a2328179b5ac21e3f2241
Dominique Orban <dominique.orban@gmail.com>
2013-01-23 15:05:59 -0500

    coinmp: 1.6.0
    CoinMP is a standardized interface to several COIN-OR packages
    (CLP, CBC and CGL). See http://www.coin-or.org and
    http://www.coin-or.org/projects/CoinMP.xml.
    
    CoinMP conflicts with CoinUtils.

---
+++ coinmp.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Coinmp < Formula
+  homepage 'http://www.coin-or.org/projects/CoinMP.xml'
+  url 'http://www.coin-or.org/download/source/CoinMP/CoinMP-1.6.0.tgz'
+  sha1 '1f9dd57e4cdb4d0fa1594851ae411d366c3e8836'
+
+  conflicts_with 'coinutils', :because => 'CoinMP includes CoinUtils.'
+
+  def install
+    ENV.fortran
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    ENV.deparallelize  # make install fails in parallel.
+    system "make install"
+
+    # The following hack should be fixed upstream soon.
+    (include+'coin').install Dir['Clp/src/*.hpp']
+  end
+end
