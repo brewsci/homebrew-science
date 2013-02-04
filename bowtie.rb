ca149679a0df64daf51b9d5d882f63697ab3fee5
Mark Howison <mhowison@brown.edu>
2013-02-04 11:44:16 -0500

    bowtie: updated version to 0.12.9

--- bowtie.rb
+++ bowtie.rb
@@ -2,11 +2,15 @@ require 'formula'
 
 class Bowtie < Formula
   homepage 'http://bowtie-bio.sourceforge.net/index.shtml'
-  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie/0.12.8/bowtie-0.12.8-src.zip'
-  sha1 '56fd4dfd1d8ef995f041d11ce6078dd1e22a655f'
+  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie/0.12.9/bowtie-0.12.9-src.zip'
+  sha1 'ce261d82695e6e97ffc1bc9c3f6651a9fbb2feba'
 
   def install
     system "make"
     bin.install %W(bowtie bowtie-build bowtie-inspect)
+  end
+
+  def test
+    system "bowtie", "--version"
   end
 end
