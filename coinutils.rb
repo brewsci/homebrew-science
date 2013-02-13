7027f7dc6c87c8a55b65c8e644aca86fb4cffc3f
Samuel John <github@SamuelJohn.de>
2013-02-13 18:14:42 +0100

    coinutils: conflicts_with coinmp

--- coinutils.rb
+++ coinutils.rb
@@ -5,6 +5,8 @@ class Coinutils < Formula
   url 'http://www.coin-or.org/download/source/CoinUtils/CoinUtils-2.8.8.tgz'
   sha1 '64791e9c5f2dc50b891dd2b0d710ddf81d302864'
 
+  conflicts_with 'coinmp', :because => 'CoinMP already provides CoinUtils (and more).'
+
   def install
     ENV.fortran
 
