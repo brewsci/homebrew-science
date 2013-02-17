cffb1ffe77ba9bedbaee01c8e7e3b3faceb6889e
Kevin Emmett <kjemmett@gmail.com>
2013-02-16 21:23:44 -0500

    cddlib: 094g - New formula
    The C-library cddlib is a C implementation of the Double Description
    Method of Motzkin et al. for generating all vertices (i.e. extreme
    points) and extreme rays of a general convex polyhedron in R^d given
    by a system of linear inequalities:
    
       P = { x=(x1, ..., xd)^T :  b - A  x  >= 0 }
    
    where A is a given m x d real matrix, b is a given m-vector
    and 0 is the m-vector of all zeros.
    
    Closes #108

---
+++ cddlib.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Cddlib < Formula
+  homepage 'http://www.inf.ethz.ch/personal/fukudak/cdd_home/'
+  url 'ftp://ftp.ifor.math.ethz.ch/pub/fukuda/cdd/cddlib-094g.tar.gz'
+  sha1 '4ef38ca6efbf3d54a7a753c63ff25a8c95e5fc5f'
+
+  depends_on 'gmp'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    doc.install Dir['doc/*']
+    share.install Dir['examples*']
+  end
+
+end
