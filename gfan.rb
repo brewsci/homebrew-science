4ad8c5230ce75f5bca47880b130dc133a7c4484f
Kevin Emmett <kjemmett@gmail.com>
2013-02-16 21:26:00 -0500

    gfan: New Formula
    Gfan is a software package for computing Gröbner fans and tropical
    varieties.
    
    Closes #109

---
+++ gfan.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Gfan < Formula
+  homepage 'http://home.imf.au.dk/jensen/software/gfan/gfan.html'
+  url 'http://home.imf.au.dk/jensen/software/gfan/gfan0.5.tar.gz'
+  sha1 'ba4f3e4fac6bcafdfdd2244329d925e958d9ee63'
+
+  depends_on 'gmp'
+  depends_on 'cddlib'
+
+  def install
+    system "make"
+    system "make PREFIX=#{prefix} install"
+    doc.install Dir['doc/*']
+    share.install Dir['examples', 'homepage', 'testsuite']
+  end
+
+  def test
+    system "gfan --help"
+  end
+end
