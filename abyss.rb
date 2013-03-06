47ab9a77a5d23f148af9a70de44d946cb336944e
Shaun Jackman <sjackman@gmail.com>
2013-03-05 23:19:40 -0800

    abyss 1.3.5

--- abyss.rb
+++ abyss.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Abyss < Formula
   homepage 'http://www.bcgsc.ca/platform/bioinfo/software/abyss'
-  url 'http://www.bcgsc.ca/downloads/abyss/abyss-1.3.4.tar.gz'
-  sha1 '763dc423054421829011844ceaa5e18dc43f1ca9'
+  url 'http://www.bcgsc.ca/downloads/abyss/abyss-1.3.5.tar.gz'
+  sha1 '43c3caceb91f768290cb7a1b70f6b3113b8a36fc'
   head 'https://github.com/sjackman/abyss.git'
 
   option 'disable-popcnt', 'do not use the POPCNT instruction'
@@ -21,13 +21,6 @@ class Abyss < Formula
   # strip breaks the ability to read compressed files.
   skip_clean 'bin'
 
-  # Fix a compiler error on OS X 10.8 Mountain Lion.
-  # This issue is fixed upstream:
-  # https://github.com/sjackman/abyss/issues/13
-  def patches
-    DATA unless build.head?
-  end
-
   def install
     system "./autogen.sh" if build.head?
     args = [
@@ -42,18 +35,3 @@ class Abyss < Formula
     system "#{bin}/ABYSS", "--version"
   end
 end
-
-__END__
-diff --git a/Graph/ContigGraphAlgorithms.h b/Graph/ContigGraphAlgorithms.h
-index 023a898..0eac936 100644
---- a/Graph/ContigGraphAlgorithms.h
-+++ b/Graph/ContigGraphAlgorithms.h
-@@ -329,7 +329,7 @@ size_t addComplementaryEdges(ContigGraph<DG>& g)
-		if (!found) {
-			add_edge(vc, uc, g[e], static_cast<DG&>(g));
-			numAdded++;
--		} else if (g[e] != g[f]) {
-+		} else if (!(g[e] == g[f])) {
-			// The edge properties do not agree. Select the better.
-			g[e] = g[f] = BetterDistanceEst()(g[e], g[f]);
-		}
