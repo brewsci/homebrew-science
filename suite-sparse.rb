08b73091dc6bdce696bc11400946ac2354c7c05f
Andrew Janke <janke@pobox.com>
2013-04-15 15:20:52 -0400

    suite-sparse: update to 4.2.0
    Update suite-sparse to the new 4.2.0. This should fix the recent Octave
    build problems.
    
    Also change the metis dependency to use the metis4 formula instead of metis,
    since SuiteSparse is not compatible with Metix 5.x.
    
    Now uses built-in Mac make script.
    
    Use :recommend and :option for dependencies
    
    closes #128, closes #132, closes #139, closes #146, closes #153
    
    Signed-off-by: Derek Thomas <derekathomas@gmail.com>

--- suite-sparse.rb
+++ suite-sparse.rb
@@ -2,29 +2,26 @@ require 'formula'
 
 class SuiteSparse < Formula
   homepage 'http://www.cise.ufl.edu/research/sparse/SuiteSparse'
-  url 'http://www.cise.ufl.edu/research/sparse/SuiteSparse/SuiteSparse-4.1.0.tar.gz'
-  sha1 '93a0ae741b399d0dbecd43235d2f8977cdd9bc47'
+  url 'http://www.cise.ufl.edu/research/sparse/SuiteSparse/SuiteSparse-4.2.0.tar.gz'
+  sha1 'd99f88afc69ee4d073541ff7a68bc4f26bda5853'
 
-  option "without-tbb", "Do not link with tbb (Threading Building Block)"
-  option "with-metis", "Compile in metis libraries"
-  option "with-openblas", "Use openblas instead of Apple's Accelerate.framework"
-
-  depends_on "tbb" unless build.include? "without-tbb"
+  depends_on "tbb" => :recommended
+  depends_on "openblas" => :optional
   # Metis is optional for now because of
   # cholmod_metis.c:164:21: error: use of undeclared identifier 'idxtype'
-  depends_on "metis" if build.include? "with-metis"
-  depends_on "openblas" if build.include? "with-openblas"
+  depends_on "metis4" => :optional # metis 5.x is not yet supported by suite-sparse
 
   def install
     # SuiteSparse doesn't like to build in parallel
     ENV.j1
 
-    inreplace 'SuiteSparse_config/SuiteSparse_config.mk' do |s|
+    # Switch to the Mac base config, per SuiteSparse README.txt
+    system "mv SuiteSparse_config/SuiteSparse_config.mk SuiteSparse_config/SuiteSparse_config_orig.mk"
+    system "mv SuiteSparse_config/SuiteSparse_config_Mac.mk SuiteSparse_config/SuiteSparse_config.mk"
+    
+    inreplace "SuiteSparse_config/SuiteSparse_config.mk" do |s|
       if build.include? 'with-openblas'
         s.change_make_var! "BLAS", "-lopenblas"
-        s.change_make_var! "LAPACK", "$(BLAS)"
-      else
-        s.change_make_var! "BLAS", "-Wl,-framework -Wl,Accelerate"
         s.change_make_var! "LAPACK", "$(BLAS)"
       end
 
@@ -33,9 +30,9 @@ class SuiteSparse < Formula
         s.change_make_var! "TBB", "-ltbb"
       end
 
-      if build.include? 'with-metis'
+      if build.include? "with-metis4"
         s.remove_make_var! "METIS_PATH"
-        s.change_make_var! "METIS", Formula.factory("metis").lib + "libmetis.a"
+        s.change_make_var! "METIS", Formula.factory("metis4").lib + "libmetis.a"
       end
 
       s.change_make_var! "INSTALL_LIB", lib
