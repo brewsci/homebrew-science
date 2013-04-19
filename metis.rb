dcf9bd2e8ee731252c6a1ca4fc0716a6500a538a
Houtan Bastani <houtan@dynare.org>
2013-04-19 12:33:50 +0200

    metis: support universal builds
    Closes #149.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- metis.rb
+++ metis.rb
@@ -5,9 +5,12 @@ class Metis < Formula
   url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.0.2.tar.gz'
   sha1 'b5a278fa06c581e068a8296d158576a4b750f983'
 
+  option :universal
+
   depends_on 'cmake' => :build
 
   def install
+    ENV.universal_binary if build.universal?
     system "make", "config", "prefix=#{prefix}"
     system "make install"
   end
