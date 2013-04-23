47264891327621d13dc332a9f0fce7bd8515ae23
Kyungdahm Yun <tomyun@gmail.com>
2013-04-23 18:06:00 +0900

    radx 20130412
    Closes #155

--- radx.rb
+++ radx.rb
@@ -2,19 +2,14 @@ require 'formula'
 
 class Radx < Formula
   homepage 'http://www.ral.ucar.edu/projects/titan/docs/radial_formats/radx.html'
-  url 'ftp://ftp.rap.ucar.edu/pub/titan/radx/Radx-20121120.src.tgz'
-  version '20121120'
-  sha1 '2c0ffa04c0f6b942fb2de1ec35256f857b5d2d63'
+  url 'ftp://ftp.rap.ucar.edu/pub/titan/radx/Radx-20130412.src.tgz'
+  version '20130412'
+  sha1 'beb3ba8707bd9710e5c2154533b42562681171df'
 
   depends_on 'hdf5'
   depends_on 'udunits'
   depends_on 'netcdf' => 'enable-cxx-compat'
   depends_on 'fftw'
-
-  fails_with :clang do
-    build 421
-    cause "DsMdvx/msg_add.cc:516:11: error: '_printVsectWayPtsBuf' is a protected member of 'Mdvx'"
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
