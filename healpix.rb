3e05b59b6f17afde10de92ef4c5134f2c3a5b4e6
Will M. Farr <wmfarr@gmail.com>
2013-04-30 10:34:20 -0500

    Healpix version 3.11
    See http://healpix.jpl.nasa.gov/
    
    HEALPix is an acronym for Hierarchical Equal Area isoLatitude
    Pixelization of a sphere. As suggested in the name, this pixelization
    produces a subdivision of a spherical surface in which each pixel
    covers the same surface area as every other pixel.  Healpix is used
    extensively in astronomy to produce maps of data on the sky.
    
    Closes #169.

---
+++ healpix.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Healpix < Formula
+  homepage 'http://healpix.jpl.nasa.gov/'
+  url 'https://downloads.sourceforge.net/project/healpix/Healpix_3.11/Healpix_3.11_2013Apr24.tar.gz'
+  sha1 'f7d6a18ca6aad9fe85a66eca36d7a1f0ef783e95'
+  version '3.11'
+
+  depends_on 'cfitsio'
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+
+  def install
+    configure_args = %W[--disable-dependency-tracking --prefix=#{prefix}]
+    cd "src/C/autotools" do
+      system "autoreconf", "--install"
+      system "./configure", *configure_args
+      system "make", "install"
+    end
+
+    ENV.append_to_cflags "-DENABLE_FITSIO"
+    cd "src/cxx/autotools" do
+      system "echo \"AUTOMAKE_OPTIONS = subdir-objects\" >> Makefile.am"
+      system "autoreconf", "--install"
+      system "./configure", *configure_args
+      system "make", "install"
+    end
+  end
+end
