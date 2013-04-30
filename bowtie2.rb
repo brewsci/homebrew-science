1036d7c0b29e253e8f94785f42d61c4c4e101897
Ian Lancaster <git@iml.me>
2013-04-30 02:27:17 -0400

    bowtie2 2.1.0
    Closes #164

--- bowtie2.rb
+++ bowtie2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bowtie2 < Formula
   homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
-  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.0.6/bowtie2-2.0.6-source.zip'
-  sha1 '86068dd90312fca2f8068412cbf469c056366220'
+  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.1.0/bowtie2-2.1.0-source.zip'
+  sha256 '90a9d3a6bd19ddc3a8f90b935c6a2288478572de2ad4039b29f91016b95ef4b0'
 
   def install
     system "make"
