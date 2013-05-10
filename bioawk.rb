eeebd40d100077d1da070dc39d3bb676fd846335
Shaun Jackman <sjackman@gmail.com>
2013-05-10 11:11:06 -0700

    bioawk: New formula

---
+++ bioawk.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Bioawk < Formula
+  homepage 'https://github.com/lh3/bioawk'
+  head 'https://github.com/lh3/bioawk.git'
+
+  def install
+    ENV.j1
+    system 'make'
+    bin.install({'awk' => 'bioawk'})
+    man1.install({'awk.1' => 'bioawk.1'})
+  end
+
+  test do
+    system 'bioawk --version'
+  end
+end
