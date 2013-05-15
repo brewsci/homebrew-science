31faeff6ccc629da9db12a32972b7b8ca4ecc15d
Shaun Jackman <sjackman@gmail.com>
2013-05-15 10:06:52 -0700

    lastz 1.03.34
    Closes #115

--- lastz.rb
+++ lastz.rb
@@ -4,13 +4,17 @@ class Lastz < Formula
   homepage 'http://www.bx.psu.edu/~rsharris/lastz/'
   url 'http://www.bx.psu.edu/miller_lab/dist/lastz-1.02.00.tar.gz'
   sha1 '49680c6e18e2b1a417953107eedf41b2aece974d'
+  devel do
+    url 'http://www.bx.psu.edu/~rsharris/lastz/newer/lastz-1.03.34.tar.gz'
+    sha1 'febf450ff44c377ba104dd4e286a848d0b6b2e47'
+  end
 
   def install
-    system "make"
+    system 'make definedForAll=-Wall'
     bin.install %w(src/lastz src/lastz_D)
   end
 
   test do
-    system "lastz --help"
+    system 'lastz --help |grep -q lastz'
   end
 end
