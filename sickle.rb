12ca0348f9b29c4a05829c38510cbb08feb78d74
Shaun Jackman <sjackman@gmail.com>
2013-05-15 10:32:33 -0700

    sickle 1.2
    Closes #31

--- sickle.rb
+++ sickle.rb
@@ -1,11 +1,15 @@
 require 'formula'
 
 class Sickle < Formula
-  version '1.2'
   homepage 'https://github.com/najoshi/sickle'
-  url 'https://github.com/najoshi/sickle.git',
-      :revision => '032bf41f962ffeacca9a21ba222b6238760bc1cf'
+  url 'https://github.com/najoshi/sickle/archive/v1.2.tar.gz'
+  sha1 'ea69b11f45336a5232f940af94c7ad2d18e9aebc'
   head 'https://github.com/najoshi/sickle.git'
+
+  # Fix a linker error
+  def patches
+    'https://github.com/sjackman/sickle/commit/895207a5e9b97a64d579dc629b3641d0322828b3.diff'
+  end
 
   def install
     system 'make'
