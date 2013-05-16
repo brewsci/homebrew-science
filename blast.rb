d31d6192f167c84ea0b128b31a6de093e2a240e5
Shaun Jackman <sjackman@gmail.com>
2013-05-16 12:23:38 -0700

    blast: Support recent versions of gnutls
    Add an upstream patch to support recent versions of gnutls.
    Add an optional build dependency on gnutls.
    Add configure --without-debug --with-mt as recommended by upstream.
    Closes #161, closes #170.

--- blast.rb
+++ blast.rb
@@ -6,14 +6,21 @@ class Blast < Formula
   version '2.2.28'
   sha1 '6941d2b83c410b2e2424266d8ee29ee7581c23d6'
 
+  depends_on 'gnutls' => :optional
+
   option 'with-dll', "Create dynamic binaries instead of static"
+
+  def patches
+    # Support recent versions of gnutls
+    'http://www.ncbi.nlm.nih.gov/viewvc/v1/trunk/c%2B%2B/src/connect/ncbi_gnutls.c?view=patch&r1=57856&r2=57915'
+  end
 
   def install
     args = ["--prefix=#{prefix}"]
     args << "--with-dll" if build.include? 'with-dll'
 
     cd 'c++' do
-      system "./configure", *args
+      system './configure', '--without-debug', '--with-mt', *args
       system "make"
       system "make install"
     end
