15f240443b62cb67d00edd5ab132f645ca9a1bdd
Shaun Jackman <sjackman@gmail.com>
2013-05-18 15:58:20 -0700

    amos: Add dependencies
    Closes #39

--- amos.rb
+++ amos.rb
@@ -5,15 +5,31 @@ class Amos < Formula
   url 'http://sourceforge.net/projects/amos/files/amos/3.1.0/amos-3.1.0.tar.gz'
   sha1 '28e799e37713594ba7147d300ecae6574beb14a4'
 
+  depends_on 'blat' => :optional # for minimus2-blat
+  depends_on 'boost' => :recommended # for Bambus 2
+  depends_on 'mummer' => :recommended # for minimus2
+  depends_on 'qt' => [:optional, '--with-qt3support'] # for AMOS GUIs
+  depends_on 'Statistics::Descriptive' => [:perl, :recommended]
+
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+      "--with-Boost-dir=#{Formula.factory('boost').opt_prefix}",
+      "--with-qmake-qt4=#{Formula.factory('qt').opt_prefix}/bin/qmake",
+      "BLAT=#{Formula.factory('blat').opt_prefix}/bin/blat",
+      "DELTAFILTER=#{Formula.factory('mummer').opt_prefix}/libexec/delta-filter",
+      "SHOWCOORDS=#{Formula.factory('mummer').opt_prefix}/libexec/show-coords"
     system "make install"
   end
 
   def caveats; <<-EOS.undent
-    Perl modules have been placed in
-      #{lib}
+    The Perl modules have been installed in
+      #{lib}/AMOS
+      #{lib}/TIGR
     EOS
+  end
+
+  def test
+    system "#{bin}/bank-transact -h"
   end
 end
