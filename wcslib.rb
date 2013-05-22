fe1b6424f6d6e21cf0c925af229a5c006b44f13d
Derek Thomas <derekathomas@gmail.com>
2013-05-22 12:35:50 +0900

    wcslib: fix download
    fixes #138

--- wcslib.rb
+++ wcslib.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wcslib < Formula
   homepage 'http://www.atnf.csiro.au/people/mcalabre/WCS/'
-  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.16.tar.bz2'
-  sha1 'd321ee3c82ecb14e447006ed475f3f0217aa589f'
+  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.17.tar.bz2'
+  sha1 '74ec5fd6ec93e9a09798598c3738b3d4bc99b3e7'
 
   option 'with-pgsbox', 'Build PGSBOX, a general curvilinear axis drawing routine for PGPLOT'
   option 'with-fortran', "Build Fortran wrappers. Needed for --with-pgsbox."
