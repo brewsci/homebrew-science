30a95c5b6dd586ca7e771350009986460a19b1bd
Shaun Jackman <sjackman@gmail.com>
2013-05-31 12:01:47 -0700

    novoalign 3.00.05: New formula
    An aligner for single-ended and paired-end reads from the Illumina
    Genome Analyser. Novoalign finds global optimum alignments using full
    Needleman-Wunsch algorithm with affine gap penalties.

---
+++ novoalign.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Novoalign < Formula
+  homepage 'http://www.novocraft.com/'
+  version '3.00.05'
+  url 'http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz' %
+    [version, if MACOS then 'MacOSX' else 'gcc' end]
+  sha1(if MACOS then 'fde9f3e0d5aeb0fd6b7ac81e61c4a0163c19b2d7'
+    else 'e3856070e91a86e693f14d36c16b478f4cc4b80d' end)
+
+  def install
+    bin.install %w[isnovoindex novo2maq novo2paf novoalign novobarcode
+      novoindex novomethyl novope2bed.pl novorun.pl novosort novoutil]
+    # Conflicts with samtools
+    #bin.install 'novo2sam.pl'
+    doc.install %w[NovoBarcode.pdf NovoCraftV3.00.pdf
+      ReleaseNotesV3.00.pdf readme.txt readme_novomethyl.txt]
+  end
+
+  test do
+    system 'novoalign --version 2>&1 |grep -q Novoalign'
+  end
+end
