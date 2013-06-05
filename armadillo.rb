58e6cb671ec38db9c26b070d3ade29ccbeafe4b4
Trent Houliston <trent@houliston.me>
2013-06-05 17:20:05 +1000

    armadillo 3.900.1
    Released 2013-06-04
    Closes #202

--- armadillo.rb
+++ armadillo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Armadillo < Formula
   homepage 'http://arma.sourceforge.net/'
-  url 'http://sourceforge.net/projects/arma/files/armadillo-3.820.1.tar.gz'
-  sha1 '7a6797ff784e5e3ae501ba8581ce3b85cecb4660'
+  url 'http://sourceforge.net/projects/arma/files/armadillo-3.900.1.tar.gz'
+  sha1 '06863576978e2e341471cbf45fa24889c1e787a6'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
