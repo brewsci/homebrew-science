8ba09abbf18f71fad3b764c440b68a59087f3f0c
Samuel John <github@SamuelJohn.de>
2013-06-08 16:10:44 +0200

    galib: The lib dir has to be created at first

--- galib.rb
+++ galib.rb
@@ -6,6 +6,10 @@ class Galib < Formula
   sha1 '3411da19d6b5b67638eddc4ccfab37a287853541'
 
   def install
+    # To avoid that 'libga.a' will be install as 'lib' and not *into* lib:
+    mkdir "#{prefix}/lib"
+
+    # Sometime builds fail. It's fast anyway, so lets deparallelize
     ENV.deparallelize
     system "make"
     system "make test"
