0590b84daced24d7a9c692f2cb20e588b2e1076f
Samuel John <github@SamuelJohn.de>
2013-06-09 22:21:41 +0200

    openCV: Fix Python & patch for OpenCL headers

--- opencv.rb
+++ opencv.rb
@@ -5,8 +5,6 @@ class Opencv < Formula
   url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz'
   sha1 '9e25f821db9e25aa454a31976ba6b5a3a50b6fa4'
 
-  env :std # to find python
-
   option '32-bit'
   option 'with-qt',  'Build the Qt4 backend to HighGUI'
   option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
@@ -15,6 +13,7 @@ class Opencv < Formula
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
   depends_on 'numpy' => :python
+  depends_on :python
 
   depends_on 'eigen'   => :optional
   depends_on 'libtiff' => :optional
@@ -27,8 +26,13 @@ class Opencv < Formula
   # you don't need unless you're doing video analysis, and some of it isn't
   # in Homebrew anyway. Will depend on openexr if it's installed.
 
+  def patches
+    # Find openCL headers on case sensitive fs: https://github.com/Homebrew/homebrew-science/pull/200
+    'https://github.com/Itseez/opencv/commit/6e119049ce3228ca82acb7f4aaa2f4bceeddcbdf.patch'
+  end
+
   def install
-    args = std_cmake_args + %w[
+    args = std_cmake_args + %W[
       -DCMAKE_OSX_DEPLOYMENT_TARGET=
       -DWITH_CUDA=OFF
       -DBUILD_ZLIB=OFF
@@ -38,7 +42,10 @@ class Opencv < Formula
       -DBUILD_JASPER=OFF
       -DBUILD_TESTS=OFF
       -DBUILD_PERF_TESTS=OFF
+      -DPYTHON_INCLUDE_DIR='#{python.incdir}'
+      -DPYTHON_LIBRARY='#{python.libdir}/lib#{python.xy}.dylib'
     ]
+
     if build.build_32_bit?
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
       args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
@@ -49,36 +56,6 @@ class Opencv < Formula
     # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
     args << '-DWITH_OPENCL=OFF' if build.without? 'opencl' or MacOS.version < :lion
 
-    # The CMake `FindPythonLibs` Module is dumber than a bag of hammers when
-    # more than one python installation is available---for example, it clings
-    # to the Header folder of the system Python Framework like a drowning
-    # sailor.
-    #
-    # This code was cribbed from the VTK formula and uses the output to
-    # `python-config` to do the job FindPythonLibs should be doing in the first
-    # place.
-    python_prefix = `python-config --prefix`.strip
-    # Python is actually a library. The libpythonX.Y.dylib points to this lib, too.
-    if File.exist? "#{python_prefix}/Python"
-      # Python was compiled with --framework:
-      args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-      if !MacOS::CLT.installed? and python_prefix.start_with? '/System/Library'
-        # For Xcode-only systems, the headers of system's python are inside of Xcode
-        args << "-DPYTHON_INCLUDE_DIR='#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/2.7/Headers'"
-      else
-        args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/Headers'"
-      end
-    else
-      python_lib = "#{python_prefix}/lib/lib#{which_python}"
-      if File.exists? "#{python_lib}.a"
-        args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
-      else
-        args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
-      end
-      args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/include/#{which_python}'"
-    end
-    args << "-DPYTHON_PACKAGES_PATH='#{lib}/#{which_python}/site-packages'"
-
     args << '..'
     mkdir 'macbuild' do
       system 'cmake', *args
@@ -87,19 +64,8 @@ class Opencv < Formula
     end
   end
 
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
-  end
 
-  def site_package_dir
-    "lib/#{which_python}/site-packages"
-  end
-
-  def caveats; <<-EOS.undent
-    The OpenCV Python module will not work until you edit your PYTHONPATH like so:
-      export PYTHONPATH="#{HOMEBREW_PREFIX}/#{site_package_dir}:$PYTHONPATH"
-
-    To make this permanent, put it in your shell's profile (e.g. ~/.profile).
-    EOS
+  def caveats
+    python.standard_caveats if python
   end
 end
