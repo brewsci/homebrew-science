55cc3431425f01505a89caf64a3b8170fe967ff4
Simon Walker <s.r.walker101@googlemail.com>
2013-06-13 10:58:39 +0100

    cdsclient 3.71: New formula
    This package allows the querying of astronomical catalogues from the
    command line.
    
    The cdsclient package is a set of C and shell routines which can be
    built on Unix stations or PCs running Linux, which once compiled allow
    to query some databases located at CDS or on mirrors over the network.
    
    Closes #215

---
+++ cdsclient.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Cdsclient < Formula
+  homepage 'http://cdsarc.u-strasbg.fr/doc/cdsclient.html'
+  url 'http://cdsarc.u-strasbg.fr/ftp/pub/sw/cdsclient-3.71.tar.gz'
+  sha1 '67639a1bb14f4ad5e48444950f42bcfa123af0a0'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+      system 'findusnob1 -help 2>&1 | grep -q findusnob1'
+  end
+
+end
