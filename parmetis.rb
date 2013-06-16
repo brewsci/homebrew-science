3100dce530caca10380939cdb23e685fb09b1a7b
Michael Ford <fanquake@gmail.com>
2013-06-16 12:14:00 +0800

    parmetis 4.0.3
    Closes #218

--- parmetis.rb
+++ parmetis.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Parmetis < Formula
   homepage 'http://glaros.dtc.umn.edu/gkhome/metis/parmetis/overview'
-  url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.2.tar.gz'
-  sha1 '1699453f0bd2ffed34e5a755dec92048d797476d'
+  url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz'
+  sha1 'e0df69b037dd43569d4e40076401498ee5aba264'
 
   depends_on 'cmake' => :build
   depends_on :mpi => :cc
