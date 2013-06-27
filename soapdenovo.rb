5d78461efb67ba80b953f73dcf23de679a47f723
Shaun Jackman <sjackman@gmail.com>
2013-06-26 19:41:42 -0700

    soapdenovo: Specify the version explicitly
    The version number is incorrectly identified as `V1.05.src`.

--- soapdenovo.rb
+++ soapdenovo.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Soapdenovo < Formula
   homepage 'http://soap.genomics.org.cn/soapdenovo.html'
   url 'http://soap.genomics.org.cn/down/SOAPdenovo-V1.05.src.tgz'
+  version '1.05'
   sha1 'e7d8fc337de43cc6b96b2b24acbe5454cf0eadfe'
 
   def install
