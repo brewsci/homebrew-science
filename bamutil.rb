a2c7a7662fe9fbd651162cd5f8f5214ace9165d1
Shaun Jackman <sjackman@gmail.com>
2013-07-07 11:15:38 -0700

    bamutil 1.0.9
    Use the release tarball that includes libStatGen.

--- bamutil.rb
+++ bamutil.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Bamutil < Formula
   homepage 'http://genome.sph.umich.edu/wiki/BamUtil'
-  url 'https://github.com/statgen/bamUtil/archive/v1.0.7.tar.gz'
-  sha1 'a9d4790e0455aeab925896806871c707424b5c50'
+  url 'http://genome.sph.umich.edu/w/images/5/52/BamUtilLibStatGen.1.0.9.tgz'
+  sha1 '5997481f5668d6cc1c9570f3aae1383262af28c3'
   head 'https://github.com/statgen/bamUtil.git'
 
   def install
     ENV.j1
-    system 'make', 'cloneLib'
+    system 'make', 'cloneLib' if build.head?
     system 'make', 'install', "INSTALLDIR=#{bin}"
   end
 
