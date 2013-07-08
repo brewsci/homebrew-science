9ecfb4a0219cea90c15ae4dd88dcf774e64f142c
Shaun Jackman <sjackman@gmail.com>
2013-07-08 14:59:57 -0700

    seqtk 1.0
    Add the stable release.

--- seqtk.rb
+++ seqtk.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class Seqtk < Formula
   homepage 'https://github.com/lh3/seqtk'
+  url 'https://github.com/lh3/seqtk/archive/1.0.tar.gz'
+  sha1 '926ec33df2c4dace334d2a01db072b3d5411bcc9'
   head 'https://github.com/lh3/seqtk.git'
 
   def install
