7dcda5c8486e77026815de83bebeffbf87f46ff0
Nick Crawford <ngcrawford@gmail.com>
2013-07-08 15:09:00 -0700

    rainbow  2.0.2: RADseq assembler
    Efficient tool for clustering and assembling short reads, especially
    for RAD.
    Closes #255

---
+++ rainbow.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Rainbow < Formula
+  homepage 'http://sourceforge.net/projects/bio-rainbow/'
+  url 'http://downloads.sourceforge.net/project/bio-rainbow/rainbow_2.0.2.tar.gz'
+  sha1 'e3b91dd0900d9ec3e033a844eee1d156a4b33b71'
+
+  def install
+    system 'make'
+    bin.install %w{rainbow ezmsim rbasm rbmergetag}
+  end
+
+  test do
+    system 'rainbow 2>&1 |grep -q rainbow'
+  end
+end
