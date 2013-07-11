edf6a8728ad00f1d6fe96a5fa1e228b5f26fa5d0
Jonathan Chang <jchang641@gmail.com>
2013-07-10 18:28:51 -0700

    hyphy 2.2: new build options
    Closes #261

--- hyphy.rb
+++ hyphy.rb
@@ -2,30 +2,102 @@ require 'formula'
 
 class Hyphy < Formula
   homepage 'http://www.hyphy.org/'
-  url 'https://github.com/veg/hyphy/archive/2.1.2.tar.gz'
-  sha1 '3e8708256c92ee84c0ade5a69b20070fa20e103f'
+  url 'https://github.com/veg/hyphy/archive/2.2.tar.gz'
+  sha1 '9ff285a71a51f4699947a162fe46c3eb3458425e'
   head 'https://github.com/veg/hyphy.git'
 
+  option 'with-opencl', "Build a version with OpenCL GPU/CPU acceleration"
+  option 'without-multi-threaded', "Don't build a multi-threaded version"
+  option 'without-single-threaded', "Don't build a single-threaded version"
+
   depends_on 'cmake' => :build
+  depends_on :mpi => :optional
+
+  fails_with :clang do
+    build 77
+    cause "cmake gets passed the wrong flags"
+  end
 
   def patches
-    # Single-threaded builds. This commit is from upstream and will
-    # be included in the next release.
-    'https://github.com/veg/hyphy/commit/c90bf9b93218a38e4c9f12150ffe36eb.diff'
+    DATA # single-threaded builds
   end
 
   def install
     system "cmake", "-DINSTALL_PREFIX=#{prefix}", ".", *std_cmake_args
-    system "make SP"
+    system "make SP" unless build.without? "single-threaded"
+    system "make MP2" unless build.without? "multi-threaded"
+    system "make MPI" if build.with? :mpi
+    system "make OCL" if build.with? "opencl"
+
     system "make install"
     (share/'hyphy').install('help')
   end
 
   def caveats; <<-EOS.undent
-    This formula builds a single-threaded version of HyPhy, as multithreaded
-    builds occasionally hang on certain analyses.
-
     The help has been installed to #{HOMEBREW_PREFIX}/share/hyphy.
     EOS
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 76228a8..ee4bb80 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -299,6 +299,23 @@ add_custom_target(MP2 DEPENDS HYPHYMP)
+ 
+ 
+ #-------------------------------------------------------------------------------
++# hyphy sp target
++#-------------------------------------------------------------------------------
++add_executable(
++    HYPHYSP
++    EXCLUDE_FROM_ALL
++    ${SRC_COMMON} ${SRC_UNIXMAIN}
++)
++target_link_libraries(HYPHYSP ${DEFAULT_LIBRARIES})
++install(
++    TARGETS HYPHYSP
++    RUNTIME DESTINATION bin
++    OPTIONAL
++)
++add_custom_target(SP DEPENDS HYPHYSP)
++
++
++#-------------------------------------------------------------------------------
+ # hyphy OpenCL target
+ #-------------------------------------------------------------------------------
+ find_package(OpenCL)
+@@ -546,7 +563,7 @@ endif((${QT4_FOUND}))
+ #-------------------------------------------------------------------------------
+ if(UNIX)
+     set_property(
+-        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG
++        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG HYPHYSP
+         APPEND PROPERTY COMPILE_DEFINITIONS __UNIX__
+     )
+ endif(UNIX)
+@@ -557,7 +574,7 @@ set_property(
+ )
+ 
+ set_property(
+-    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG
++    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG HYPHYSP
+     APPEND PROPERTY COMPILE_DEFINITIONS _HYPHY_LIBDIRECTORY_="${CMAKE_INSTALL_PREFIX}/lib/hyphy"
+ )
+ 
+@@ -567,6 +584,13 @@ set_property(
+ )
+ 
+ set_target_properties(
++    HYPHYSP
++    PROPERTIES
++    COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS}"
++    LINK_FLAGS "${DEFAULT_LINK_FLAGS}"
++)
++
++set_target_properties(
+     hyphy_mp HYPHYMP
+     PROPERTIES
+     COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS} ${OpenMP_CXX_FLAGS}"
+
