55e9d401da87ed5c06d8919532d193fe68ed7b66
Ian Lancaster <git@iml.me>
2013-08-19 01:58:39 -0400

    tophat 2.0.9
    Closes #307

--- tophat.rb
+++ tophat.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Tophat < Formula
   homepage 'http://tophat.cbcb.umd.edu/'
-  url 'http://tophat.cbcb.umd.edu/downloads/tophat-2.0.8b.tar.gz'
-  sha1 '0744801fc5216104026259bcdf8b1846bf681d42'
+  url 'http://tophat.cbcb.umd.edu/downloads/tophat-2.0.9.tar.gz'
+  sha1 '6ca77ae70caf44cf78adca240987300baa72b3c5'
 
   depends_on 'samtools'
   depends_on 'boost'
 
-  # Variable length arrays using non-POD element types. Initialize with length=1
-  # Reported upstream via email to tophat-cufflinks@gmail.com on 28OCT2012
-  def patches; DATA; end
-
   def install
+    # Variable length arrays using non-POD element types. Initialize with length=1
+    # Reported upstream via email to tophat-cufflinks@gmail.com on 28OCT2012
+    inreplace 'src/tophat_reports.cpp', '[num_threads]', '[1]'
+
     # This can only build serially, otherwise it errors with no make target.
     ENV.deparallelize
 
@@ -28,22 +28,3 @@ class Tophat < Formula
     system "#{bin}/tophat", "--version"
   end
 end
-
-__END__
---- a/src/tophat_reports.cpp
-+++ b/src/tophat_reports.cpp
-@@ -2465,10 +2465,10 @@
-     fprintf(stderr, "Warning: %lu small overhang junctions!\n", (long unsigned int)small_overhangs);
-   */
- 
--  JunctionSet vfinal_junctions[num_threads];
--  InsertionSet vfinal_insertions[num_threads];
--  DeletionSet vfinal_deletions[num_threads];
--  FusionSet vfinal_fusions[num_threads];
-+  JunctionSet vfinal_junctions[1];
-+  InsertionSet vfinal_insertions[1];
-+  DeletionSet vfinal_deletions[1];
-+  FusionSet vfinal_fusions[1];
- 
-   for (int i = 0; i < num_threads; ++i)
-     {
