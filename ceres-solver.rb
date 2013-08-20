32820bb912670cec73c4cf6fabe3d2cc15481c41
Jack Morrison <jackmorrison1@gmail.com>
2013-08-20 11:14:08 -0400

    ceres-solver: Add head
    Closes #309

--- ceres-solver.rb
+++ ceres-solver.rb
@@ -4,6 +4,7 @@ class CeresSolver < Formula
   homepage 'http://code.google.com/p/ceres-solver/'
   url 'http://ceres-solver.googlecode.com/files/ceres-solver-1.6.0.tar.gz'
   sha1 '0fdd7a931498bd09dc167827f4412c368f89bd25'
+  head 'https://ceres-solver.googlesource.com/ceres-solver.git'
 
   depends_on 'cmake' => :build
   depends_on 'glog'
