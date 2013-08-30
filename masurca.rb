c077ddc029862624b0be3e5dc4e2acd2b532c1f6
Shaun Jackman <sjackman@gmail.com>
2013-08-30 10:33:18 -0700

    masurca 2.0.4

--- masurca.rb
+++ masurca.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Masurca < Formula
   homepage 'http://www.genome.umd.edu/masurca.html'
-  url 'ftp://ftp.genome.umd.edu/pub/MaSuRCA/MaSuRCA-2.0.0.tar.gz'
-  sha1 '91ef80b34206e8d3c3d93a1a88f058ded8c1dfb5'
+  url 'ftp://ftp.genome.umd.edu/pub/MaSuRCA/MaSuRCA-2.0.4.tar.gz'
+  sha1 '7e36afd82bd581566d46fd4dedd7ffc6cd751fb2'
 
   keg_only 'MaSuRCA conflicts with jellyfish.'
 
