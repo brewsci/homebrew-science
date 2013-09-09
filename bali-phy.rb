1ea55023b7b472b13f6cfcb54b21e34e23bb4816
Shaun Jackman <sjackman@debian.org>
2013-09-09 09:45:37 -0700

    bali-phy: Depends on pkg-config and cairo
    Fails with clang 425

--- bali-phy.rb
+++ bali-phy.rb
@@ -5,10 +5,12 @@ class BaliPhy < Formula
   url 'http://www.biomath.ucla.edu/msuchard/bali-phy/bali-phy-2.1.1.tar.gz'
   sha1 'e72073a1c5b05c797668e476bfd8517594f074e6'
 
+  depends_on 'pkg-config' => :build
+  depends_on 'cairo'
   depends_on 'gsl'
 
   fails_with :clang do
-    build 318
+    build 425
     cause <<-EOS.undent
       io.H:25:31: error: use of undeclared identifier 'push_back'
       void operator()(const T& t){push_back(t);}
