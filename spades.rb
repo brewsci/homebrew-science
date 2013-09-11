bb37eff1e9c5ff046bce8d421ea6d03f4379b7a1
Shaun Jackman <sjackman@debian.org>
2013-09-11 10:11:01 -0700

    spades: Depends on cmake

--- spades.rb
+++ spades.rb
@@ -5,6 +5,8 @@ class Spades < Formula
   url 'http://spades.bioinf.spbau.ru/release2.5.0/SPAdes-2.5.0.tar.gz'
   sha1 '35a40c617f0202ba3bfb5022a9f703fea0eccb4e'
 
+  depends_on 'cmake' => :build
+
   def install
     mkdir 'src/build' do
       system 'cmake', '..', *std_cmake_args
