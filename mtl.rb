d6c6340fa200b60e575f956fad4ca59cf010f4ff
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2013-09-17 16:41:44 -0400

    mtl 4.0.9482
    Closes #341

--- mtl.rb
+++ mtl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mtl < Formula
   homepage 'http://www.simunova.com'
-  url 'http://www.simunova.com/downloads/mtl4/MTL-4.0.9470-Linux.tar.bz2'
-  sha1 '33b27dd5d983f411f4020795ed906b4687ca0df9'
+  url 'http://www.simunova.com/downloads/mtl4/MTL-4.0.9482-Linux.tar.bz2'
+  sha1 'cff156ceb8f91a6dac4caba1b17ed6bfab322756'
   head 'https://simunova.zih.tu-dresden.de/svn/mtl4/trunk', :using => :svn
 
   depends_on 'cmake' => :build if build.head?
