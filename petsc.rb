e78fe22c610e6132d360ec333dc6f38af0bd5f33
Dominique Orban <dominique.orban@gmail.com>
2013-09-18 14:34:20 -0400

    Update to PETSc 3.4.
    - Download petsc-lite for smaller tarball
    - Build with shared libs
    - Compatible with petsc4py 3.4 and SLEPc 3.4.
    
    Closes #343.

--- petsc.rb
+++ petsc.rb
@@ -2,17 +2,39 @@ require 'formula'
 
 class Petsc < Formula
   homepage 'http://www.mcs.anl.gov/petsc/index.html'
-  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz'
-  sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.4.3.tar.gz'
+  sha1 '8c5d97ba4a28ea8fa830e513a9dcbfd61b51beaf'
+  head 'https://bitbucket.org/petsc/petsc', :using => :git
 
   depends_on :mpi => :cc
   depends_on :fortran
+  depends_on :x11 => MacOS::X11.installed? ? :recommended : :optional
 
   def install
     ENV.deparallelize
-    system "./configure", "--with-debugging=0", "--prefix=#{prefix}"
+
+    petsc_arch = 'arch-darwin-c-opt'
+    args = ["--with-debugging=0", "--with-shared-libraries=1", "--prefix=#{prefix}/#{petsc_arch}"]
+    args << "--with-x=0" if build.without? 'x11'
+    ENV['PETSC_DIR'] = Dir.getwd  # configure fails if those vars are set differently.
+    ENV['PETSC_ARCH'] = petsc_arch
+    system "./configure", *args
     system "make all"
     system "make test"
+    ohai 'Test results are in ~/Library/Logs/Homebrew/petsc. Please check.'
     system "make install"
+
+    # Link only what we want.
+    include.install_symlink Dir["#{prefix}/#{petsc_arch}/include/*h"], "#{prefix}/#{petsc_arch}/include/finclude", "#{prefix}/#{petsc_arch}/include/petsc-private"
+    prefix.install_symlink "#{prefix}/#{petsc_arch}/conf"
+    lib.install_symlink Dir["#{prefix}/#{petsc_arch}/lib/*.a"], Dir["#{prefix}/#{petsc_arch}/lib/*.dylib"]
+    share.install_symlink Dir["#{prefix}/#{petsc_arch}/share/*"]
+  end
+
+  def caveats; <<-EOS
+    Set PETSC_DIR to #{prefix}
+    and PETSC_ARCH to arch-darwin-c-opt.
+    Fortran module files are in #{prefix}/arch-darwin-c-opt/include.
+    EOS
   end
 end
