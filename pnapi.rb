30b6846da758065ec16ad6994e04ac55d37e1d56
Niels <niels.lohmann@gmail.com>
2013-09-25 00:54:27 +0200

    PNAPI: 4.03 - A C++ Petri Net API
    The Petri Net API is a C++ library of Petri net-related functions. It is
    currently used in more than a dozen Petri net tools, ranging from compilers
    to verification tools.
    
    Closes #353.

--- pnapi.rb
+++ pnapi.rb
@@ -2,14 +2,35 @@ require 'formula'
 
 class Pnapi < Formula
   homepage 'http://service-technology.org/pnapi/'
-  url 'http://download.gna.org/service-tech/pnapi/pnapi-4.02.tar.gz'
-  sha1 'caa61aaa5886381e594f7eb57a75c1feb6960c3a'
+  head 'http://svn.gna.org/svn/service-tech/trunk/pnapi'
+  url 'http://download.gna.org/service-tech/pnapi/pnapi-4.03.tar.gz'
+  sha1 'd054018c4a2e580add680ffac28731461cf9e308'
+
+  option "without-check", "skip build-time checks (not recommended)"
 
   depends_on "graphviz"
+  depends_on "lola"
+  depends_on 'pkg-config' => :build
+  depends_on "genet" => :optional
+  if build.head?
+    depends_on 'autoconf'
+    depends_on 'automake'
+    depends_on 'gengetopt'
+    depends_on 'help2man'
+    depends_on 'gnu-sed'
+    depends_on 'libtool'
+  end
 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    ENV.deparallelize if build.head?
+
+    system "autoreconf -i" if build.head?
+    system "./configure", "--disable-assert",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    system "make", "check" if build.with? "check"
     # for some reason config.h is not installed by the Makefile
     (include/'pnapi').install 'src/config.h'
   end
