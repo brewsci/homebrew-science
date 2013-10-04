4929f8cd617d3a8c151059c29e79a3aa2db746f3
Shaun Jackman <sjackman@gmail.com>
2013-10-03 21:07:04 -0700

    sratoolkit 2.3.3-4

--- sratoolkit.rb
+++ sratoolkit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sratoolkit < Formula
   homepage 'http://www.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=software'
-  url 'http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.3.2-4/sra_sdk-2.3.2-4.tar.gz'
-  sha1 'fb483457ae12c6f34ffecb70cf3515b5b91a9b2e'
+  url 'http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.3.3-4/sra_sdk-2.3.3-4.tar.gz'
+  sha1 '3461396b2298d845415abf064c22f4c8d8afb4dd'
   head 'https://github.com/NCBITools/sratoolkit.git'
 
   def install
