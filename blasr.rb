951eabde65d56968e832b04fe48b9b5c4b726aa9
Shaun Jackman <sjackman@gmail.com>
2013-10-18 15:25:23 -0700

    blasr 2.1

--- blasr.rb
+++ blasr.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class Blasr < Formula
   homepage 'https://github.com/PacificBiosciences/blasr'
+  url 'https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.1.tar.gz'
+  sha1 'ac6add1ba8a82cac2515da36c0ec53060c20ea0f'
   head 'https://github.com/PacificBiosciences/blasr.git'
 
   depends_on 'hdf5' => 'enable-cxx'
