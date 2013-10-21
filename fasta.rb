9525eb9fed67fd5511325d420df9e8cd1bb10e6d
Shaun Jackman <sjackman@gmail.com>
2013-10-20 22:34:31 -0700

    fasta 36.3.6d

--- fasta.rb
+++ fasta.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fasta < Formula
   homepage 'http://faculty.virginia.edu/wrpearson/fasta/'
-  url 'http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.6.tar.gz'
-  sha1 '7f5078135793d6d6cace0f7073368c2c5cab69b7'
+  url 'http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.6d.tar.gz'
+  sha1 'add828f56abf02e2d330a3b376b920e6ba95b06a'
 
   def install
     cd 'src' do
