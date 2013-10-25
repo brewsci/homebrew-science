f0882ba45f893d1cbda2916315a3ad7d2bfeb3b9
Dominique Orban <dominique.orban@gmail.com>
2013-10-25 16:13:29 -0400

    Updates to Scalapack formula
    - always run tests
    - make openblas optional instead of recommended.

--- scalapack.rb
+++ scalapack.rb
@@ -5,11 +5,9 @@ class Scalapack < Formula
   url 'http://www.netlib.org/scalapack/scalapack-2.0.2.tgz'
   sha1 'ff9532120c2cffa79aef5e4c2f38777c6a1f3e6a'
 
-  option 'test', 'Verify the build with make test'
-
   depends_on :mpi => [:cc, :f90]
   depends_on 'cmake' => :build
-  depends_on 'openblas' => :recommended
+  depends_on 'openblas' => :optional
   depends_on 'dotwrp' if build.without? 'openblas'
   depends_on :fortran
 
@@ -29,7 +27,7 @@ class Scalapack < Formula
     mkdir "build" do
       system 'cmake', '..', *args
       system 'make all'
-      system 'make test' if build.include? 'test'
+      system 'make test'
       system 'make install'
     end
   end
