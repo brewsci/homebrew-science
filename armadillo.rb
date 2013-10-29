abe335862a7e323b6cf909bd2fb6ebbfd990c555
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2013-10-28 23:27:10 -0400

    armadillo 3.920.2
    Closes #418

--- armadillo.rb
+++ armadillo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Armadillo < Formula
   homepage 'http://arma.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.900.1.tar.gz'
-  sha1 '06863576978e2e341471cbf45fa24889c1e787a6'
+  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.920.2.tar.gz'
+  sha1 'c03a99b203a7f12a95e893ae398fab96d22485aa'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
