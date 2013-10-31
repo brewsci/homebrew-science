b382b4a9dbad75fa4640f9aa8c5e37e731d68672
Gleb Gusev <gleb@artec-group.com>
2013-10-31 01:25:48 +0100

    ann: Fix compilation in mavericks
    Closes #425.

--- ann.rb
+++ ann.rb
@@ -6,6 +6,9 @@ class Ann < Formula
   sha1 '622be90314a603ef9b2abadcf62379f73f28f46c'
 
   def install
+    # Fix for Mavericks make error
+    inreplace 'ann2fig/ann2fig.cpp', 'main', 'int main' if MacOS.version >= :mavericks
+
     system "make", "macosx-g++"
     prefix.install "bin", "lib", "sample", "doc", "include"
   end
