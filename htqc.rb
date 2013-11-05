f2d4f07697bc53abd20d42103316babea4985b04
Shaun Jackman <sjackman@gmail.com>
2013-11-04 17:21:13 -0800

    htqc 0.90.2: New formula
    A toolkit including statistics tool for illumina high-throughput
    sequencing data, and filtration tools for sequence quality, length,
    tail quality, etc..

---
+++ htqc.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Htqc < Formula
+  homepage 'http://sourceforge.net/projects/htqc/'
+  url 'http://downloads.sourceforge.net/project/htqc/htqc-0.90.2-Source.tar.gz'
+  sha1 'c4204ed1b85d78daa3c90968b99eea113308ed72'
+
+  depends_on 'cmake' => :build
+  depends_on 'boost'
+
+  fails_with :clang do
+    build 500
+    cause "error: call to constructor of 'htio::FastaIO' is ambiguous"
+  end
+
+  def install
+    system 'cmake', '.', *std_cmake_args
+    system 'make', 'install'
+  end
+
+  test do
+    system 'ht-stat --version'
+  end
+end
