f386b0248f304ca608e646d0447413f9c25bcde3
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-11-06 17:55:25 +0100

    tophat: Fix build failure with clang
    segment_juncs.cpp:50:17: error: expected namespace name
    tophat_reports.cpp:2787:30: error: variable length array of non-POD
    element type 'JunctionSet' (aka 'map<Junction, JunctionStats>')
    
    Closes #450

--- tophat.rb
+++ tophat.rb
@@ -8,19 +8,16 @@ class Tophat < Formula
   depends_on 'samtools'
   depends_on 'boost'
 
-  fails_with :clang do
-    build 500
-    cause <<-EOS.undent
-      segment_juncs.cpp:50:17: error: expected namespace name
-      using namespace __gnu_cxx;
-    EOS
+  # 1. Fix namespace issue in segment_juncs.cpp
+  # 2. Fix "Variable length arrays using non-POD element types" error
+  # in tophat_reports.cpp
+  # Reported upstream:
+  # https://groups.google.com/forum/#!topic/tuxedo-tools-users/hOOVgXwB0NQ
+  def patches
+     DATA
   end
 
   def install
-    # Variable length arrays using non-POD element types. Initialize with length=1
-    # Reported upstream via email to tophat-cufflinks@gmail.com on 28OCT2012
-    inreplace 'src/tophat_reports.cpp', '[num_threads]', '[1]'
-
     # This can only build serially, otherwise it errors with no make target.
     ENV.deparallelize
 
@@ -36,3 +33,48 @@ class Tophat < Formula
     system "#{bin}/tophat", "--version"
   end
 end
+
+__END__
+diff --git a/src/segment_juncs.cpp b/src/segment_juncs.cpp
+index 80fb3b7..bc55db7 100644
+--- a/src/segment_juncs.cpp
++++ b/src/segment_juncs.cpp
+@@ -47,7 +47,6 @@
+
+ using namespace seqan;
+ using namespace std;
+-using namespace __gnu_cxx;
+
+ // daehwan //geo
+ //#define B_DEBUG 1
+diff --git a/src/tophat_reports.cpp b/src/tophat_reports.cpp
+index 9d0a927..a403dbe 100644
+--- a/src/tophat_reports.cpp
++++ b/src/tophat_reports.cpp
+@@ -2784,10 +2784,10 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
+ 			fprintf(stderr, "Warning: %lu small overhang junctions!\n", (long unsigned int)small_overhangs);
+ 	 */
+
+-	JunctionSet vfinal_junctions[num_threads];
+-	InsertionSet vfinal_insertions[num_threads];
+-	DeletionSet vfinal_deletions[num_threads];
+-	FusionSet vfinal_fusions[num_threads];
++	JunctionSet * vfinal_junctions = new JunctionSet[num_threads];
++	InsertionSet * vfinal_insertions = new InsertionSet[num_threads];
++	DeletionSet * vfinal_deletions = new DeletionSet[num_threads];
++	FusionSet * vfinal_fusions = new FusionSet[num_threads];
+
+ 	vector<SAlignStats> alnStats(num_threads);
+
+@@ -2931,6 +2931,11 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
+ 	}
+
+ 	fprintf(stderr, "Found %lu junctions from happy spliced reads\n", (long unsigned int)final_junctions.size());
++
++	delete[] vfinal_junctions;
++	delete[] vfinal_insertions;
++	delete[] vfinal_deletions;
++	delete[] vfinal_fusions;
+ }
+
+ void print_usage()
