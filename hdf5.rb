6e496decb771c63ad32b17fa5fce28a489ac5de1
bubble-07 <spagrab@gmail.com>
2013-11-13 14:02:06 -0500

    hdf5: Use a stable URL
    Only the current release is found in current/, but both the current
    and older releases are found in releases/. Use the latter.
    
    Closes #483

--- hdf5.rb
+++ hdf5.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Hdf5 < Formula
   homepage 'http://www.hdfgroup.org/HDF5'
-  url 'http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.11.tar.bz2'
+  url 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.11/src/hdf5-1.8.11.tar.bz2'
   sha1 '87ded0894b104cf23a4b965f4ac0a567f8612e5e'
 
   # TODO - warn that these options conflict
