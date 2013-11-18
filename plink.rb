eb1dfb4294f1dd645434a69813894917c98ff920
Shaun Jackman <sjackman@gmail.com>
2013-11-18 14:10:46 -0800

    plink: Fails with clang
    sets.cpp:771:37: error: redefinition of 'i' with a different type:
    'vector<Individual *>::iterator' (aka '__wrap_iter<pointer>') vs 'int'
          vector<Individual*>::iterator i = PP->sample.begin();
                                        ^
    sets.cpp:703:12: note: previous definition is here
      for (int i=0; i<snpset.size(); i++)
               ^
    
    Closes #492

--- plink.rb
+++ plink.rb
@@ -5,11 +5,20 @@ class Plink < Formula
   homepage 'http://pngu.mgh.harvard.edu/~purcell/plink/'
   sha1 'd41a2d014ebc02bf11e5235292b50fad6dedd407'
 
+  fails_with :clang do
+    build 500
+    cause %q[sets.cpp:771:37: error: redefinition of 'i' with a different type]
+  end
+
   def install
     ENV.deparallelize
-    inreplace "Makefile", "SYS = UNIX", "SYS = MAC"
-    system "make"
+    inreplace 'Makefile', 'SYS = UNIX', 'SYS = MAC' if OS.mac?
+    system 'make'
     (share+'plink').install %w{test.map test.ped}
-    bin.install "plink"
+    bin.install 'plink'
+  end
+
+  test do
+    system *%w[plink --help]
   end
 end
