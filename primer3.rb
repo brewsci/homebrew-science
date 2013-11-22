acbbc9c89f3918b4787b01cae29a7f5cd7b7fd4c
Ian Lancaster <git@iml.me>
2013-11-22 18:08:00 -0500

    primer3 2.3.6
    Closes #517

--- primer3.rb
+++ primer3.rb
@@ -2,13 +2,17 @@ require 'formula'
 
 class Primer3 < Formula
   homepage 'http://primer3.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/primer3/primer3/2.3.4/primer3-2.3.4.tar.gz'
-  sha1 '850d8e5cfbe84cdf3e4955a3974a6531a8ac6516'
+  url 'https://downloads.sourceforge.net/project/primer3/primer3/2.3.6/primer3-src-2.3.6.tar.gz'
+  sha1 '7dbd33a4e9c2a4fe06c74d6b83f8ff0f9ed1c49a'
 
   def install
     cd "src" do
       system "make all"
       bin.install %w(primer3_core ntdpal oligotm long_seq_tm_test)
     end
+  end
+
+  test do
+    system "#{bin}/long_seq_tm_test AAAAGGGCCCCCCCCTTTTTTTTTTT 3 20"
   end
 end
