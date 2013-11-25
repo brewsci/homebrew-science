82e5af86021e549018d2767f4109017b037a8ca5
Shaun Jackman <sjackman@gmail.com>
2013-11-25 10:42:02 -0800

    gmap-gsnap 2013-11-23

--- gmap-gsnap.rb
+++ gmap-gsnap.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GmapGsnap < Formula
   homepage 'http://research-pub.gene.com/gmap/'
-  version '2013-11-22'
+  version '2013-11-23'
   url "http://research-pub.gene.com/gmap/src/gmap-gsnap-#{version}.tar.gz"
-  sha1 'c671965e055088083c6798e25f61e8dd66ffb323'
+  sha1 'a15aa9041f485df9d1711c33a4079755d3497c20'
 
   depends_on "samtools"
 
