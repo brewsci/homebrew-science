9c6e12232c8c36113fde673030fda7016bbfc7e0
Shaun Jackman <sjackman@gmail.com>
2013-12-02 11:07:15 -0800

    libgtextutils: Now a subformula of fastx_toolkit
    Remove libgtextutils and make it a subformula of fastx_toolkit.
    fastx_toolkit is the only formula that requires libgtextutils.

--- fastx_toolkit.rb
+++ fastx_toolkit.rb
@@ -1,21 +1,34 @@
 require 'formula'
 
+class Libgtextutils < Formula
+  homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
+  url 'http://hannonlab.cshl.edu/fastx_toolkit/libgtextutils-0.6.1.tar.bz2'
+  sha1 'dbf1714be75511feae3313904a7449a1f680bc23'
+end
+
 class FastxToolkit < Formula
   homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
   url 'http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit-0.0.13.2.tar.bz2'
   sha1 '51fd9ddc1fc1ffea29d7cabc02e46dd8a1b860ec'
 
   depends_on 'pkg-config' => :build
-  depends_on 'libgtextutils'
 
   def install
+    Libgtextutils.new.brew do
+      system './configure', '--disable-debug',
+        '--disable-dependency-tracking',
+        "--prefix=#{prefix}"
+      system 'make'
+      system 'make', 'install'
+    end
+
     # --disable-debug causes fastx_trimmer to crash. See #216.
     system './configure', '--disable-dependency-tracking',
-      "--prefix=#{prefix}"
-    system "make install"
+      "--prefix=#{prefix}", "PKG_CONFIG_PATH=#{lib}/pkgconfig"
+    system 'make', 'install'
   end
 
-  def test
+  test do
     fixture = <<-EOS.undent
       >MY-ID
       AAAAAGGGGG
