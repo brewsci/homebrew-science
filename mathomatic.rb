a64db5a3b4041423f3cfc748b6be018f9687f981
Dominique Orban <dominique.orban@gmail.com>
2013-12-01 21:32:36 -0500

    Update Mathomatic URL and add devel link.
    Fixes #290.

--- mathomatic.rb
+++ mathomatic.rb
@@ -2,8 +2,13 @@ require 'formula'
 
 class Mathomatic < Formula
   homepage 'http://www.mathomatic.org/'
-  url 'http://mathomatic.org/mathomatic-16.0.5.tar.bz2'
+  url 'http://mathomatic.orgserve.de/mathomatic-16.0.5.tar.bz2'
   sha1 'aaaf4df4aa3dc9ea748211278e657c2195858c24'
+
+  devel do
+    url 'http://mathomatic.orgserve.de/am.tar.bz2'
+    sha1 '6fc3c6c265d1f0314fb622b57c8202d1ab4c46f9'
+  end
 
   def install
     ENV['prefix'] = prefix
