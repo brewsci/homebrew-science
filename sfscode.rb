93956c4f0d51bffbfb283f70adcb1d15033a1812
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2013-12-05 11:12:46 -0800

    sfscode 20130725: New formula
    Closes #543

---
+++ sfscode.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Sfscode < Formula
+  homepage 'http://sfscode.sourceforge.net/SFS_CODE/index/index.html'
+  url 'http://downloads.sourceforge.net/project/sfscode/sfscode/sfscode_20130725/sfscode_20130725.tgz'
+  sha1 '08480e69fb274fc06c929fb20b82ee1739526631'
+
+  def install
+    system "make sfs_code convertSFS_CODE"
+    bin.install "convertSFS_CODE", "sfs_code"
+  end
+
+  def test
+    system "#{bin}/sfs_code 2>&1 | grep -q sfs && #{bin}/convertSFS_CODE 2>&1 | grep -q SFS"
+  end
+end
