18ff7b45b3c7c135dbe38cf6094f5c7ae759bb22
Dominique Orban <dominique.orban@gmail.com>
2013-12-06 12:40:16 -0500

    Upate vigra to 10.0.
    - Add checks at build time
    - Optionally depend on hdf5 and Python.

--- vigra.rb
+++ vigra.rb
@@ -2,24 +2,33 @@ require 'formula'
 
 class Vigra < Formula
   homepage 'http://hci.iwr.uni-heidelberg.de/vigra/'
-  url 'http://hci.iwr.uni-heidelberg.de/vigra/vigra-1.9.0-src.tar.gz'
-  sha1 '6e4981f4ce75932ec62df6523f577c327f885ba0'
+  #url 'https://github.com/ukoethe/vigra/archive/Version-1-10-0.tar.gz'
+  #sha1 'fb0a2e5ba727e59c64a60dc5fab9c807927fc869'
+  url 'https://github.com/ukoethe/vigra/releases/download/Version-1-10-0/vigra-1.10.0-src-with-docu.tar.gz'
+  sha1 '0a882bc09f5a6ec1f8381ff571020259eb88ee67'
+  head 'https://github.com/ukoethe/vigra.git'
 
   depends_on :python => ['numpy', :optional]
   depends_on 'cmake' => :build
   depends_on 'jpeg'
   depends_on :libpng
   depends_on 'libtiff'
-  depends_on 'hdf5'
+  depends_on 'hdf5' => :recommended
   depends_on 'fftw' => :recommended
   depends_on 'openexr' => :optional
 
+  option 'without-check', 'skip tests'
+
   def install
     cmake_args = std_cmake_args
+    cmake_args << '-DWITH_VIGRANUMPY=0' if build.without? :python
+    cmake_args << '-DWITH_HDF5=0' if build.without? 'hdf5'
     cmake_args << '-DWITH_OPENEXR=1' if build.with? 'openexr'
     mkdir 'build' do
-      system "cmake", "..", *cmake_args
-      system "make", "install"
+      system 'cmake', '..', *cmake_args
+      system 'make'
+      system 'make', 'check' if build.with? 'check'
+      system 'make', 'install'
     end
   end
 end
