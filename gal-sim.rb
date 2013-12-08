81e494e136a3913f3ce4bbd5d78970f901687b9f
Dustin Lang <dstndstn@gmail.com>
2013-12-07 21:27:32 -0500

    add gal-sim: a galaxy simulation toolbox.
    GalSim is a package for simulating astronomical images of galaxies.
    It was developed to support the upcoming "Great3" challenge,
    http://great3challenge.info/, to test different methods for measuring
    weak gravitational lensing, a key way of measuring mass in the
    universe through its bending of light.
    
    Closes #545.

---
+++ gal-sim.rb
@@ -0,0 +1,45 @@
+require "formula"
+
+class GalSim < Formula
+  homepage "https://github.com/GalSim-developers/GalSim"
+  url "https://github.com/GalSim-developers/GalSim/archive/v1.0.1.tar.gz"
+  sha1 '519c9f83c0f9092507185ba46cb21231f60df9fe'
+  head "https://github.com/GalSim-developers/GalSim.git"
+
+  depends_on "scons" => :build
+  depends_on "fftw"
+  depends_on "boost"
+  depends_on "tmv-cpp"
+
+  # pyfits should come from pip
+  depends_on "pyfits" => :python
+  depends_on "numpy" => :python
+
+  option "with-openmp", "Enable openmp support (gcc only)"
+
+  def pyver
+    IO.popen("python -c 'import sys; print sys.version[:3]'").read.strip
+  end
+
+  def install
+    args = []
+    if build.with? "openmp"
+      if ENV.compiler == :clang
+        opoo "OpenMP support will not be enabled. Use --cc=gcc-x.y if you require OpenMP."
+      end
+      args << "WITH_OPENMP=true"
+    end
+    scons *args
+    scons "install", "PREFIX=#{prefix}", "PYPREFIX=#{lib}/python#{pyver}"
+  end
+
+  def caveats; <<-EOS.undent
+    The GalSim installer may warn you that #{lib}/python isn't in your python
+    search path. You may want to add all Homebrew python packages to the
+    default paths by running:
+       sudo bash -c 'echo \"/usr/local/lib/python\" >> \\\\
+         /Library/Python/#{pyver}/site-packages/homebrew.pth'
+    EOS
+  end
+
+end
