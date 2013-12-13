d161c73c1f69a0c2fff9ec77c88ffcc077b0171f
Shaun Jackman <sjackman@gmail.com>
2013-12-13 15:08:41 -0800

    bedtools 2.18.0

--- bedtools.rb
+++ bedtools.rb
@@ -1,14 +1,24 @@
 require 'formula'
 
 class Bedtools < Formula
-  homepage 'http://code.google.com/p/bedtools/'
-  url 'http://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz'
-  sha1 '1b1de3c35394a423f9ad98a9957a8853b426a578'
+  homepage 'https://github.com/arq5x/bedtools2'
+  url 'https://github.com/arq5x/bedtools2/archive/v2.18.0.tar.gz'
+  sha1 'd0cbe30d0d2be4dbda89d81f72189319f0e798e5'
+  head 'https://github.com/arq5x/bedtools2.git'
 
-  head 'https://github.com/arq5x/bedtools.git'
+  fails_with :clang do
+    # Reported upstream: https://github.com/arq5x/bedtools2/issues/2
+    build 500
+    cause "error: declaration of 'T' shadows template parameter"
+  end
 
   def install
-    system "make all"
+    system 'make'
     prefix.install 'bin'
+    doc.install %w[README.rst RELEASE_HISTORY]
+  end
+
+  test do
+    system 'bedtools --version'
   end
 end
