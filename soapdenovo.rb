59223a6b382aa7ee4413fbced7d19c1673f3a106
Shaun Jackman <sjackman@gmail.com>
2013-12-23 16:01:46 -0800

    soapdenovo 2.04-r240

--- soapdenovo.rb
+++ soapdenovo.rb
@@ -2,17 +2,23 @@ require 'formula'
 
 class Soapdenovo < Formula
   homepage 'http://soap.genomics.org.cn/soapdenovo.html'
-  url 'http://soap.genomics.org.cn/down/SOAPdenovo-V1.05.src.tgz'
-  version '1.05'
-  sha1 'e7d8fc337de43cc6b96b2b24acbe5454cf0eadfe'
+  if OS.mac?
+    url 'http://downloads.sourceforge.net/project/soapdenovo2/SOAPdenovo2/src/r240/SOAPdenovo2-src-r240-mac.tgz'
+    sha1 '2ddefdb9f19076d43a9badadb9240d2ed333518b'
+  else
+    url 'http://downloads.sourceforge.net/project/soapdenovo2/SOAPdenovo2/src/r240/SOAPdenovo2-src-r240.tgz'
+    sha1 'ca146c042b170b6a78f909dd9b3f1b1f051a08dc'
+  end
+  version '2.04-r240'
 
   def install
-    system 'make all install'
-    prefix.install 'LICENSE', 'MANUAL'
-    bin.install Dir['bin/*']
+    system 'make'
+    bin.install %w[SOAPdenovo-63mer SOAPdenovo-127mer]
+    doc.install %w[LICENSE MANUAL VERSION]
   end
 
   test do
-    system "#{bin}/SOAPdenovo-31mer"
+    system "#{bin}/SOAPdenovo-63mer"
+    system "#{bin}/SOAPdenovo-127mer"
   end
 end
