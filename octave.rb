2c223caf40cbf207fdde2b8f36062d18c0e03a46
ambs <ambs@cpan.org>
2013-12-28 20:14:26 +0000

    Octave: update to 3.8.
    New options:
    --without-docs
    --without-check
    --with-openblas
    --without-gnuplot
    --with-fltk
    
    New dependency on Arpack.
    
    Closes #428, closes #520, closes #574.
    Fixes #158, fixes #424, fixes #436.

--- octave.rb
+++ octave.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Octave < Formula
   homepage 'http://www.gnu.org/software/octave/index.html'
-  url 'http://ftpmirror.gnu.org/octave/octave-3.6.4.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/octave/octave-3.6.4.tar.bz2'
-  sha1 '3cc9366b6dbbd336eaf90fe70ad16e63705d82c4'
+  url 'http://ftpmirror.gnu.org/octave/octave-3.8.0.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/octave/octave-3.8.0.tar.bz2'
+  sha1 'ebb03485b72d97fa01f105460f81016f94680f77'
 
   skip_clean 'share/info' # Keep the docs
 
-  option 'without-fltk', 'Compile without fltk (disables native graphics)'
-  option 'test', 'Run tests before installing'
+  option 'without-check', 'Skip build-time tests (not recommended)'
+  option 'without-docs', 'Do not build documentation'
 
   depends_on :fortran
 
@@ -31,21 +31,18 @@ class Octave < Formula
   depends_on 'readline'
   depends_on 'curl' if MacOS.version == :leopard # Leopard's libcurl is too old
 
-  # additional features
+  depends_on 'arpack'
   depends_on 'suite-sparse'
-  depends_on 'glpk'
-  depends_on 'graphicsmagick' => :recommended
   depends_on 'hdf5'
   depends_on 'pcre'
   depends_on 'qhull'
   depends_on 'qrupdate'
 
-  if build.include? 'without-fltk'
-    # required for plotting if we don't have native graphics
-    depends_on 'gnuplot'
-  else
-    depends_on 'fltk'
-  end
+  depends_on 'openblas' => :optional
+  depends_on 'graphicsmagick' => :recommended
+  depends_on 'gnuplot' => :recommended
+  depends_on 'fltk' => :optional
+  depends_on 'qt' => :optional
 
   # The fix for std::unordered_map requires regenerating
   # configure. Remove once the fix is in next release.
@@ -53,18 +50,16 @@ class Octave < Formula
   depends_on :automake => :build
   depends_on :libtool => :build
 
-  def patches
-    # Pull upstream patch to fix configure script on std::unordered_map
-    # detection.
-    'http://hg.savannah.gnu.org/hgweb/octave/raw-rev/26b2983a8acd'
-  end
-
   def blas_flags
     flags = []
-    flags << "-ldotwrp" if MacOS.version == :snow_leopard and MacOS.prefer_64_bit?
-    # Cant use `-framework Accelerate` because `mkoctfile`, the tool used to
-    # compile extension packages, can't parse `-framework` flags.
-    flags << "-Wl,-framework,Accelerate"
+    if build.with? 'openblas'
+      flags << "-L#{Formula.factory('openblas').lib} -lopenblas"
+    else
+      flags << "-ldotwrp" if MacOS.version == :snow_leopard and MacOS.prefer_64_bit?
+      # Cant use `-framework Accelerate` because `mkoctfile`, the tool used to
+      # compile extension packages, can't parse `-framework` flags.
+      flags << "-Wl,-framework,Accelerate"
+    end
     flags.join(" ")
   end
 
@@ -76,12 +71,16 @@ class Octave < Formula
       "--disable-dependency-tracking",
       "--prefix=#{prefix}",
       "--with-blas=#{blas_flags}",
+      "--with-lapack=#{blas_flags}",
       # SuiteSparse-4.x.x fix, see http://savannah.gnu.org/bugs/?37031
       "--with-umfpack=-lumfpack -lsuitesparseconfig",
     ]
     args << "--without-framework-carbon" if MacOS.version >= :lion
     # avoid spurious 'invalid assignment to cs-list' erorrs on 32 bit installs:
     args << 'CXXFLAGS=-O0' unless MacOS.prefer_64_bit?
+    args << '--disable-docs' if build.without? 'docs'
+    args << '--without-opengl' if build.without? 'fltk'
+    args << '--disable-gui' if build.without? 'qt'
 
     # The fix for std::unordered_map requires regenerating
     # configure. Remove once the fix is in next release.
@@ -89,10 +88,10 @@ class Octave < Formula
 
     system "./configure", *args
     system "make all"
-    system "make check 2>&1 | tee make-check.log" if build.include? 'test'
+    system "make check 2>&1 | tee make-check.log" if build.with? 'check'
     system "make install"
 
-    prefix.install ["test/fntests.log", "make-check.log"] if build.include? 'test'
+    prefix.install ["test/fntests.log", "make-check.log"] if build.with? 'check'
   end
 
   def caveats
@@ -120,7 +119,7 @@ class Octave < Formula
     EOS
 
     s = glpk_caveats
-    s = gnuplot_caveats + s
-    s = native_caveats + s unless build.include? 'without-fltk'
+    s = gnuplot_caveats + s if build.with? 'gnuplot'
+    s = native_caveats + s if build.with? 'fltk'
   end
 end
