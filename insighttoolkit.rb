f9d83553a3c95f532f1e0cf5a8fd9358c486b6b0
Stefan <humdedum@icloud.com>
2013-12-28 13:59:47 +0100

    insighttoolkit 4.5.0
    Closes #572.

--- insighttoolkit.rb
+++ insighttoolkit.rb
@@ -2,25 +2,18 @@ require 'formula'
 
 class Insighttoolkit < Formula
   homepage 'http://www.itk.org'
-  url 'http://downloads.sourceforge.net/project/itk/itk/4.4/InsightToolkit-4.4.2.tar.gz'
-  sha1 '224b16472442e31512fd99070de6babd242f1d64'
+  url 'http://downloads.sourceforge.net/project/itk/itk/4.5/InsightToolkit-4.5.0.tar.gz'
+  sha1 '64a01e9464b6bd298ec218420967301590501dc2'
 
   head 'git://itk.org/ITK.git'
 
   depends_on 'cmake' => :build
   depends_on 'vtk' => :build
-  depends_on :python => :recommended
+  depends_on :python => :optional
 
   option 'examples', 'Compile and install various examples'
   option 'with-opencv-bridge', 'Include OpenCV bridge'
   option 'with-itkv3-compatibility', 'Include ITKv3 compatibility'
-
-  def patches
-    # Add a patch for ITK 4.4.2, so it can build with VTK 6.0.0 while ITKVtkGlue is set to ON
-    # Needs to be removed for ITK 4.4.3
-    # See : https://github.com/Kitware/ITK/commit/f715cc5c431186165480b8f5e3df9f78de9d141f
-    DATA unless build.head?
-  end
 
   def install
     args = std_cmake_args + %W[
@@ -53,21 +46,3 @@ class Insighttoolkit < Formula
     end
   end
 end
-
-__END__
-diff --git a/Modules/Bridge/VtkGlue/wrapping/CMakeLists.txt b/Modules/Bridge/VtkGlue/wrapping/CMakeLists.txt
-index 71df1b4..c84d1a3 100644
---- a/Modules/Bridge/VtkGlue/wrapping/CMakeLists.txt
-+++ b/Modules/Bridge/VtkGlue/wrapping/CMakeLists.txt
-@@ -8,5 +8,9 @@ itk_wrap_module(ITKVtkGlue)
- itk_end_wrap_module()
-
- if(ITK_WRAP_PYTHON)
--  target_link_libraries(ITKVtkGluePython vtkImagingPythonD vtkPythonCore)
-+  if(${VTK_VERSION} VERSION_LESS 6.0.0)
-+    target_link_libraries(ITKVtkGluePython vtkImagingPythonD vtkPythonCore)
-+  else()
-+    target_link_libraries(ITKVtkGluePython vtkImagingCorePythonD)
-+  endif()
- endif()
---
