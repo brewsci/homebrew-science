672aca838b678986fbfc87442ba233b08afc969f
Dominique Orban <dominique.orban@gmail.com>
2013-12-29 20:17:22 -0500

    Fix brew audit error.

--- picard-tools.rb
+++ picard-tools.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class PicardTools < Formula
   homepage 'http://picard.sourceforge.net/'
-  version '1.105'
-  url "http://downloads.sourceforge.net/project/picard/picard-tools/#{version}/picard-tools-#{version}.zip"
+  url "http://downloads.sourceforge.net/project/picard/picard-tools/1.105/picard-tools-1.105.zip"
   sha1 '34e9e6a959b8d5e2c4c2751516da596f9c4ac777'
 
   def install
