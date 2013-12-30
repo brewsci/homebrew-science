d4a6195bf89951cffaafcb6933b341f64402add7
Dominique Orban <dominique.orban@gmail.com>
2013-12-30 14:39:13 -0500

    Lammps: upgrade to 2013.12.17.
    Closes #455.

--- lammps.rb
+++ lammps.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Lammps < Formula
   homepage 'http://lammps.sandia.gov'
-  url 'http://lammps.sandia.gov/tars/lammps-14May13.tar.gz'
-  sha1 '5e522e9e3d4a4cbbe80c01c36002489777fb65d8'
+  url 'http://lammps.sandia.gov/tars/lammps-17Dec13.tar.gz'
+  sha1 '80dc6889516c9743c3103f3d5e2e7d2540bab198'
   # lammps releases are named after their release date. We transform it to
   # YYYY.MM.DD (year.month.day) so that we get a comparable version numbering (for brew outdated)
-  version '2013.05.14'
+  version '2013.12.17'
   head 'http://git.icms.temple.edu/lammps-ro.git'
 
   # user-submitted packages not considered "standard"
