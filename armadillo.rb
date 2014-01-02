f3c08b435abc29754149c8de9c91e28356e76dc9
Yan Zhou <zhouyan@me.com>
2014-01-02 02:29:59 +0000

    Update armadillo to latest stable version 3.930.2
    Closes #581.

--- armadillo.rb
+++ armadillo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Armadillo < Formula
   homepage 'http://arma.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.920.2.tar.gz'
-  sha1 'c03a99b203a7f12a95e893ae398fab96d22485aa'
+  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.930.2.tar.gz'
+  sha1 'cbd8b2b65d5af4af1e2ab8d93896cd641c27023c'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
