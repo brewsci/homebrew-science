0d075be88134d67931373a1f19f19987e621362f
A. Gordon <assafgordon@gmail.com>
2014-01-07 11:56:10 -0500

    fastx-toolkit 0.0.14
    No longer fails to build with clang.
    
    Closes #601

--- fastx_toolkit.rb
+++ fastx_toolkit.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Libgtextutils < Formula
   homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
-  url 'http://hannonlab.cshl.edu/fastx_toolkit/libgtextutils-0.6.1.tar.bz2'
-  sha1 'dbf1714be75511feae3313904a7449a1f680bc23'
+  url 'https://github.com/agordon/libgtextutils/releases/download/0.7/libgtextutils-0.7.tar.gz'
+  sha1 '92aef0099369d2e57ce47599a5339ca14abfd953'
   head 'https://github.com/agordon/libgtextutils.git'
 end
 
 class FastxToolkit < Formula
   homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
-  url 'http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit-0.0.13.2.tar.bz2'
-  sha1 '51fd9ddc1fc1ffea29d7cabc02e46dd8a1b860ec'
+  url 'https://github.com/agordon/fastx_toolkit/releases/download/0.0.14/fastx_toolkit-0.0.14.tar.bz2'
+  sha1 'd8434af2dd647e303506a54ab14fe667aabc1a86'
   head 'https://github.com/agordon/fastx_toolkit.git'
 
   if build.head?
@@ -19,11 +19,6 @@ class FastxToolkit < Formula
     depends_on :libtool => :build
   end
   depends_on 'pkg-config' => :build
-
-  fails_with :clang do
-    build 500
-    cause "error: Your version of gcc does not support the 'std::tr1' standard"
-  end unless build.head?
 
   def install
     Libgtextutils.new.brew do
