d95193f3377fc08bb88ddd3968b86296576d6730
A. Gordon <assafgordon@gmail.com>
2014-01-09 12:33:23 -0500

    idcoefs 2.1.1: New Formula
    Compute the condensed identity coefficients for pairs of individuals.
    
    Closes #610.

---
+++ idcoefs.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Idcoefs < Formula
+  homepage 'http://code.google.com/p/idcoefs/'
+  url 'http://idcoefs.googlecode.com/files/Idcoefs2_1_1.tar.gz'
+  sha1 'c8b4b8fcd7518628fce46333d30ab246e85a3ff3'
+
+  def install
+    system 'make'
+    bin.install('idcoefs')
+    doc.install('Example')
+  end
+
+  test do
+    system "#{bin}/idcoefs",
+             '-p', "#{doc}/Example/ex.pedigree",
+             '-s', "#{doc}/Example/ex.study",
+             '-o', 'foo.out'
+    system 'diff', 'foo.out', "#{doc}/Example/ex.output"
+  end
+end
