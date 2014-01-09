e804261f184fd258f0fcad245d1a10eea3aed8f6
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-01-09 08:04:22 -0800

    bali-phy 2.2.0
    No longer depends on gsl.
    Requires C++11.
    
    Closes #609

--- bali-phy.rb
+++ bali-phy.rb
@@ -2,26 +2,29 @@ require 'formula'
 
 class BaliPhy < Formula
   homepage 'http://www.bali-phy.org/'
-  url 'http://www.bali-phy.org/bali-phy-2.1.1.tar.gz'
-  sha1 'e72073a1c5b05c797668e476bfd8517594f074e6'
+  url 'http://www.bali-phy.org/bali-phy-2.2.0.tar.gz'
+  sha1 '595dfdd70a2478d2cad518703877d31d61d247eb'
 
   depends_on 'pkg-config' => :build
   depends_on 'cairo'
-  depends_on 'gsl'
 
+  # This package requires C++11, through either gcc-4.8 or through
+  # clang 3.3svn in Xcode 5 or newer.
+  # Clang 3.4.0 works with libstdc++ from gcc-4.8.
+  # On Mavericks, the Xcode defaults (libc++) work.
   fails_with :clang do
-    build 425
-    cause <<-EOS.undent
-      io.H:25:31: error: use of undeclared identifier 'push_back'
-      void operator()(const T& t){push_back(t);}
-      EOS
+     build 425
+     cause 'This formula requires C++11 support available in clang build 500 or newer.'
   end
 
   def install
     mkdir 'macbuild' do
       system "../configure", "--disable-debug", "--disable-dependency-tracking",
                              "--prefix=#{prefix}",
-                             "--enable-cairo"
+                             "--enable-cairo",
+                             # Necessary for clang 5.0, which has a
+                             # default depth of 128.
+                             "CXXFLAGS=-ftemplate-depth=256"
       system "make install"
     end
   end
