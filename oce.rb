81d15d21f3a357eef6c72707f6e62900690b7409
Johnathan Conley <johnathan.conley@gmail.com>
2014-01-21 11:23:51 -0600

    oce: New formula
    OpenCascade Community Edition
    
    Closes #629.

---
+++ oce.rb
@@ -0,0 +1,38 @@
+require "formula"
+
+class Oce < Formula
+  homepage "https://github.com/tpaviot/oce"
+  url "https://github.com/tpaviot/oce/archive/OCE-0.14.1.tar.gz"
+  sha1 "71b78c51b15e2b6902892fcec2d1f57886ba4b7e"
+
+  conflicts_with "opencascade", :because => "OCE is a fork for patches/improvements/experiments over OpenCascade"
+
+  depends_on "cmake" => :build
+  depends_on "freetype"
+  depends_on "ftgl"
+  depends_on "freeimage" => :recommended
+  depends_on "gl2ps" => :recommended
+  depends_on "tbb" => :recommended
+  depends_on :x11 => :recommended
+  depends_on :macos => :snow_leopard
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DOCE_INSTALL_PREFIX:STRING=#{prefix}"
+    cmake_args << "-DOCE_COPY_HEADERS_BUILD:BOOL=ON"
+    cmake_args << "-DOCE_DRAW:BOOL=ON"
+    cmake_args << "-DOCE_MULTITHREAD_LIBRARY:STRING=TBB" if build.with? "tbb"
+    cmake_args << "-DOCE_DISABLE_X11:BOOL=ON" if build.without? "x11"
+
+    %w{freeimage gl2ps}.each do |feature|
+      cmake_args << "-DOCE_WITH_#{feature.upcase}:BOOL=ON" if build.with? feature
+    end
+
+    system "cmake", ".", *cmake_args
+    system "make", "install/strip"
+  end
+
+  test do
+    "1\n"==`#{bin}/DRAWEXE -c \"pload ALL\"`
+  end
+end
