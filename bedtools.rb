a4b94013890ed54bad7ed901c1faa890fcf970fe
Shaun Jackman <sjackman@gmail.com>
2014-01-24 15:46:38 -0800

    bedtools 2.18.2
    Closes #611

--- bedtools.rb
+++ bedtools.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bedtools < Formula
   homepage 'https://github.com/arq5x/bedtools2'
-  url 'https://github.com/arq5x/bedtools2/archive/v2.18.1.tar.gz'
-  sha1 'ce7c1ca7ca3717f77e4260eee83e22dab9dfdd87'
+  url 'https://github.com/arq5x/bedtools2/archive/v2.18.2.tar.gz'
+  sha1 'eaecc3841cec23b9dbc06e205d8dd2838525f1b5'
   head 'https://github.com/arq5x/bedtools2.git'
 
   def install
