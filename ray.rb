278da244b51d6ff232d8c3055908e7d7aa694b2b
Shaun Jackman <sjackman@gmail.com>
2014-01-29 16:36:08 -0800

    ray 2.3.0
    Fails to build with clang, llvm and gcc-4.2. Builds with gcc-4.8.

--- ray.rb
+++ ray.rb
@@ -2,16 +2,30 @@ require 'formula'
 
 class Ray < Formula
   homepage 'http://denovoassembler.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/denovoassembler/Ray-v2.2.0.tar.bz2'
-  sha1 'c7e095bae64b0b14a35dbf05fe54e449a7117a3c'
+  url 'http://downloads.sourceforge.net/project/denovoassembler/Ray-2.3.0.tar.bz2'
+  sha1 '1b1c085d2d53431bcf671799f0ce351d835ba072'
   # The head does not build right now. Can you help us?
   # head 'https://github.com/sebhtml/ray.git'
 
   depends_on :mpi => :cxx
 
+  fails_with :clang do
+    build 500
+    cause "error: use of overloaded operator '>>' is ambiguous"
+  end
+
   fails_with :llvm do
     build 2336
     cause '"___gxx_personality_v0" ... ld: symbol(s) not found for architecture x86_64'
+  end
+
+  fails_with :gcc do
+    build 5666
+    cause "error: wrong number of arguments specified for '__deprecated__' attribute"
+  end
+
+  def patches
+    'https://github.com/sebhtml/ray/commit/e1ad06d8cb55427ff4bcca647b7bd4e4416619e3.patch'
   end
 
   def install
