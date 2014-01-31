19947830643bf85979efb74e175cb0ee2c975eb3
Shaun Jackman <sjackman@gmail.com>
2014-01-31 14:24:45 -0800

    genewise 2.4.1: New formula
    Wise2's key programs are genewise, a program for aligning proteins or
    protein HMMs to DNA, and dynamite a rather cranky "macro language"
    which automates the production of dynamic programming.
    
    http://www.ebi.ac.uk/~birney/wise2/

---
+++ genewise.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+class Genewise < Formula
+  homepage 'http://www.ebi.ac.uk/~birney/wise2/'
+  url 'http://www.ebi.ac.uk/~birney/wise2/wise2.4.1.tar.gz'
+  sha1 '5b1e5eb9ff9584dd04fd7db9327b79f719e723d1'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'glib'
+
+  def install
+    # Use pkg-config glib-2.0 rather than glib-config
+    inreplace %w[src/makefile src/corba/makefile
+        src/dnaindex/assembly/makefile src/dnaindex/makefile
+        src/dynlibsrc/makefile src/models/makefile
+        src/network/makefile src/other_programs/makefile
+        src/snp/makefile],
+      'glib-config', 'pkg-config glib-2.0'
+
+    # getline conflicts with stdio
+    inreplace 'src/HMMer2//sqio.c', 'getline', 'getline_ReadSeqVars'
+
+    system *%w[make -C src all]
+    bin.install Dir['src/bin/*']
+    doc.install %w[INSTALL LICENSE README], *Dir['docs/*']
+    (share/'genewise').install Dir['wisecfg/*']
+  end
+
+  def caveats; <<-EOS.undent
+    You must set WISECONFIGDIR:
+      export WISECONFIGDIR=#{HOMEBREW_PREFIX/'share/genewise'}
+    EOS
+  end
+
+  test do
+    system 'genewise -version 2>&1 |grep -q genewise'
+  end
+end
