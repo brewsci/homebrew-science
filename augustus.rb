b77b3b6aa859d5811e193bb05bdd44aec62584c8
Shaun Jackman <sjackman@gmail.com>
2014-02-04 15:53:40 -0800

    augustus 3.0.1: New formula
    AUGUSTUS is a program that predicts genes in eukaryotic genomic
    sequences.

---
+++ augustus.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Augustus < Formula
+  homepage 'http://bioinf.uni-greifswald.de/augustus/'
+  url 'http://bioinf.uni-greifswald.de/augustus/binaries/augustus.3.0.1.tar.gz'
+  sha1 '19f40b3b834aba0386646d9cb8120caf3f0eb64e'
+
+  depends_on 'boost' => :recommended # for gz support
+
+  fails_with :clang do
+    build 500
+    cause 'error: invalid operands to binary expression'
+  end
+
+  def install
+    system 'make'
+    rm_r %w[include mysql++ src]
+    libexec.install Dir['*']
+    bin.install_symlink '../libexec/bin/augustus'
+  end
+
+  def caveats; <<-EOS.undent
+    Set the environment variable AUGUSTUS_CONFIG_PATH:
+      export AUGUSTUS_CONFIG_PATH=#{opt_prefix}/libexec/config
+    EOS
+  end
+
+  test do
+    system 'augustus --version'
+  end
+end
