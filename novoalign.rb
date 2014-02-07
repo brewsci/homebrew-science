30fbf1103aa632e42dde3e1670d14b3551196247
Shaun Jackman <sjackman@gmail.com>
2014-02-06 21:48:08 -0800

    novoalign 3.02.02

--- novoalign.rb
+++ novoalign.rb
@@ -2,15 +2,16 @@ require 'formula'
 
 class Novoalign < Formula
   homepage 'http://www.novocraft.com/'
-  version '3.02.00'
+  version '3.02.02'
   url 'http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz' %
     [version, if OS.mac? then 'MacOSX' else 'Linux2.6' end]
-  sha1(if OS.mac? then '1a3f89f6dd9cd33d61ac143d622a8394dcd69b65'
-    else '4877eef6b236f2a00d47faf2adc74b2d313c5260' end)
+  sha1(if OS.mac? then '97984fbf7a7d5f73e47e5a44523812ea1c2f6ee6'
+    else 'f16ce82c9d6c4e7559101f819320e7fd39352a75' end)
 
   def install
-    bin.install %w[isnovoindex novo2maq novo2paf novoalign novobarcode
-      novoindex novomethyl novope2bed.pl novorun.pl novosort novoutil]
+    bin.install %w[isnovoindex novo2paf novoalign novoalignCS
+      novoalignCSMPI novoalignMPI novobarcode novoindex novomethyl
+      novope2bed.pl novorun.pl novosort novoutil]
     # Conflicts with samtools
     #bin.install 'novo2sam.pl'
     doc.install Dir['*.pdf', '*.txt']
