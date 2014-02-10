d7eeadf2a7fde44130d20744e6b87201ea5e36f1
Jonathan Chang <jchang641@gmail.com>
2014-02-10 13:08:35 -0800

    raxml 8.0.5
    Updates raxml to version 8.0.5. Also fixes old broken pthreads compile and automatically compiles vectorized versions based on detected CPU capabilities.
    
    Closes #667.

--- raxml.rb
+++ raxml.rb
@@ -2,19 +2,19 @@ require 'formula'
 
 class Raxml < Formula
   homepage 'http://sco.h-its.org/exelixis/web/software/raxml/index.html'
-  url 'https://github.com/stamatak/standard-RAxML/archive/v8.0.1.tar.gz'
-  sha1 'ddfeea52f0e7e64e7165e6c030b2a7cea4be6c7a'
+  url 'https://github.com/stamatak/standard-RAxML/archive/v8.0.5.tar.gz'
+  sha1 '15bc92181c64483f832f16c4a4340347b17c841d'
 
   head 'https://github.com/stamatak/standard-RAxML.git'
 
   def install
-    system "make", "-f", "Makefile.SSE3.gcc"
-    system "make", "-f", "Makefile.SSE3.PTHREADS.gcc"
-    bin.install("raxmlHPC-SSE3", "raxmlHPC-PTHREADS-SSE3")
+    system "make", "-f", "Makefile.PTHREADS.gcc"
+    (rm Dir["*.o"] and system "make", "-f", "Makefile.SSE3.PTHREADS.gcc") if Hardware::CPU.sse3?
+    (rm Dir["*.o"] and system "make", "-f", "Makefile.AVX.PTHREADS.gcc") if Hardware::CPU.avx?
+    bin.install Dir["raxmlHPC-*"]
   end
 
   def test
-    system "raxmlHPC-SSE3", "-v"
-    system "raxmlHPC-PTHREADS-SSE3", "-v"
+    system "raxmlHPC-PTHREADS", "-v"
   end
 end
