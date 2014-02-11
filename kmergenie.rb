ac744c871e5e84d1fba2ec2734d5a80303d553c4
Shaun Jackman <sjackman@gmail.com>
2014-02-11 10:24:19 -0800

    kmergenie 1.6213

--- kmergenie.rb
+++ kmergenie.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Kmergenie < Formula
   homepage 'http://kmergenie.bx.psu.edu/'
-  url 'http://kmergenie.bx.psu.edu/kmergenie-1.5351.tar.gz'
-  sha1 '08591e3a644b0da10e6104f36fda80a149980268'
+  url 'http://kmergenie.bx.psu.edu/kmergenie-1.6213.tar.gz'
+  sha1 'f93622c8f2ead32cb75ba0f36b91747e969d4b6e'
 
   depends_on 'r'
 
   def install
+    ENV.deparallelize
     system 'make'
     libexec.install 'kmergenie', 'specialk',
       'scripts', 'third_party'
