87f600150d0cbf7cbda03ad3a4a9fd9400ae0bc3
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2014-02-12 02:21:54 -0500

    nonpareil 2.303
    The sha1 was updated to v2.303 and the “version” test was replaced by a
    meaningful test with mock sequences.
    
    Closes #671.

--- nonpareil.rb
+++ nonpareil.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nonpareil < Formula
   homepage 'http://enve-omics.ce.gatech.edu/nonpareil'
-  url 'https://github.com/lmrodriguezr/nonpareil/archive/v2.302.tar.gz'
-  sha1 '14e3cd8bafcbf5bb91e6fb3cf5ef6ed7a550c0e0'
+  url 'https://github.com/lmrodriguezr/nonpareil/archive/v2.303.tar.gz'
+  sha1 '1d70d7a056f185669faf7a3ec5b59dd599d4d652'
 
   head 'https://github.com/lmrodriguezr/nonpareil.git'
 
@@ -14,10 +14,10 @@ class Nonpareil < Formula
     system "make", "nonpareil"
     system "make", "mpicpp=#{ENV['MPICXX']}", "nonpareil-mpi" if build.with? :mpi
     system "make", "prefix=#{prefix}", "mandir=#{man1}", "install"
+    libexec.install "test/test.fasta"
   end
 
   test do
-    system "nonpareil", "-V"
-    system "nonpareil-mpi", "-V" if build.with? 'open-mpi'
+    system "nonpareil", "-s", "#{libexec}/test.fasta", "-b", "#{libexec}/test"
   end
 end
