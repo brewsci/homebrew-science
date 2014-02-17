1958b0181210d147e5c0608eda6b3ff5f1a75589
Shaun Jackman <sjackman@gmail.com>
2014-02-17 11:37:18 -0800

    bowtie2: Add head

--- bowtie2.rb
+++ bowtie2.rb
@@ -4,12 +4,13 @@ class Bowtie2 < Formula
   homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
   url 'https://github.com/BenLangmead/bowtie2/archive/v2.2.0.tar.gz'
   sha256 '256ef9dcf13ed14e54c23a98e4019bb41399501c7b92dd4eb36bea8e5954fdc9'
+  head 'https://github.com/BenLangmead/bowtie2.git'
 
   def patches
     # Handle bowtie2 being a relative symlink. Reported upstream:
     # https://github.com/BenLangmead/bowtie2/pull/3
     'https://github.com/sjackman/bowtie2/commit/3c3ef74afd4984b5506a19445c6a8b1cdfbc1f42.patch'
-  end
+  end unless build.head?
 
   def install
     system "make"
