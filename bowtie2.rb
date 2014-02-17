76bfb7da3c5574e55baba758236b9bb41347deb4
Shaun Jackman <sjackman@gmail.com>
2014-02-16 21:24:42 -0800

    bowtie2 2.2.0
    Handle bowtie2 being a relative symlink.
    Install documents.

--- bowtie2.rb
+++ bowtie2.rb
@@ -2,12 +2,22 @@ require 'formula'
 
 class Bowtie2 < Formula
   homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
-  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.1.0/bowtie2-2.1.0-source.zip'
-  sha256 '90a9d3a6bd19ddc3a8f90b935c6a2288478572de2ad4039b29f91016b95ef4b0'
+  url 'https://github.com/BenLangmead/bowtie2/archive/v2.2.0.tar.gz'
+  sha256 '256ef9dcf13ed14e54c23a98e4019bb41399501c7b92dd4eb36bea8e5954fdc9'
+
+  def patches
+    # Handle bowtie2 being a relative symlink. Reported upstream:
+    # https://github.com/BenLangmead/bowtie2/pull/3
+    'https://github.com/sjackman/bowtie2/commit/3c3ef74afd4984b5506a19445c6a8b1cdfbc1f42.patch'
+  end
 
   def install
     system "make"
-    bin.install %W(bowtie2 bowtie2-align bowtie2-build bowtie2-inspect)
+    bin.install %w[bowtie2 bowtie2-align-l bowtie2-align-s
+      bowtie2-build bowtie2-build-l bowtie2-build-s
+      bowtie2-inspect bowtie2-inspect-l bowtie2-inspect-s]
+    doc.install %w[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS README
+      TUTORIAL VERSION]
   end
 
   def test
