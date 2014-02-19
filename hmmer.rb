5e0ddd107c5dca409fb3f7c127123f94486a8a75
Jack Nagel <jacknagel@gmail.com>
2014-02-18 20:22:11 -0500

    hmmer: use spec deps

--- hmmer.rb
+++ hmmer.rb
@@ -4,9 +4,11 @@ class Hmmer < Formula
   homepage 'http://hmmer.janelia.org/'
   url 'http://selab.janelia.org/software/hmmer3/3.1b1/hmmer-3.1b1.tar.gz'
   sha1 'e05907d28b7f03d4817bb714ff0a8b2ef0210220'
-  head 'https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk'
 
-  depends_on :autoconf if build.head?
+  head do
+    url 'https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk'
+    depends_on :autoconf
+  end
 
   def install
     system "autoconf" if build.head?
