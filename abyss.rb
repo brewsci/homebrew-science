6670e5da8896857ab87b684a64c487ed2c4d1ac5
Jack Nagel <jacknagel@gmail.com>
2014-02-18 20:22:10 -0500

    abyss: use spec deps

--- abyss.rb
+++ abyss.rb
@@ -4,7 +4,14 @@ class Abyss < Formula
   homepage 'http://www.bcgsc.ca/platform/bioinfo/software/abyss'
   url 'http://www.bcgsc.ca/platform/bioinfo/software/abyss/releases/1.3.7/abyss-1.3.7.tar.gz'
   sha1 '3d0831b1b7fd0601ded7c2d01938a24b3fc1931a'
-  head 'https://github.com/bcgsc/abyss.git'
+
+  head do
+    url 'https://github.com/bcgsc/abyss.git'
+
+    depends_on :autoconf => :build
+    depends_on :automake => :build
+    depends_on 'multimarkdown' => :build
+  end
 
   option 'disable-popcnt', 'do not use the POPCNT instruction'
   MAXK = [32, 64, 96, 128, 256, 512]
@@ -13,11 +20,6 @@ class Abyss < Formula
   end
 
   # Only header files are used from these packages, so :build is appropriate
-  if build.head?
-    depends_on :autoconf => :build
-    depends_on :automake => :build
-    depends_on 'multimarkdown' => :build
-  end
   depends_on 'boost' => :build
   depends_on 'google-sparsehash' => :build
   depends_on :mpi => [:cc, :recommended]
