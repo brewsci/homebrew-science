e2c2c06d6fc6baa80e008470cffd0a936567c737
Jack Nagel <jacknagel@gmail.com>
2014-02-18 20:22:11 -0500

    genet: use spec deps

--- genet.rb
+++ genet.rb
@@ -2,15 +2,18 @@ require 'formula'
 
 class Genet < Formula
   homepage 'http://www.lsi.upc.edu/~jcarmona/genet.html'
-  head 'http://svn.gna.org/svn/service-tech/trunk/genet'
   url 'http://download.gna.org/service-tech/genet/genet-1.2.tar.gz'
   sha1 '3daa288ee932c502b79bbe44d0ce55f12994b099'
 
-  depends_on 'autoconf' if build.head?
-  depends_on 'automake'if build.head?
-  depends_on 'libtool'if build.head?
-  depends_on 'gengetopt'if build.head?
-  depends_on 'help2man'if build.head?
+  head do
+    url 'http://svn.gna.org/svn/service-tech/trunk/genet'
+
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
+    depends_on 'gengetopt' => :build
+    depends_on 'help2man' => :build
+  end
 
   def install
     system "autoreconf -i" if build.head?
