366edae01aefb8956f591b66d2d29c373b74a4c6
Jack Nagel <jacknagel@gmail.com>
2014-02-18 20:22:11 -0500

    bpel2owfn: use spec deps

--- bpel2owfn.rb
+++ bpel2owfn.rb
@@ -2,18 +2,21 @@ require 'formula'
 
 class Bpel2owfn < Formula
   homepage 'http://www.gnu.org/software/bpel2owfn'
-  head 'http://svn.gna.org/svn/service-tech/trunk/bpel2owfn'
   url 'http://download.gna.org/service-tech/bpel2owfn/bpel2owfn-2.4.tar.gz'
   sha1 '7917a990d0df53f9edd3a775063036b7a60d6e71'
 
-  depends_on 'autoconf' if build.head?
-  depends_on 'automake' if build.head?
-  depends_on 'flex' if build.head?
-  depends_on 'bison' if build.head?
-  depends_on 'gengetopt' if build.head?
-  depends_on 'help2man' if build.head?
-  depends_on 'kimwitu++' if build.head?
-  depends_on 'gnu-sed' if build.head?
+  head do
+    url 'http://svn.gna.org/svn/service-tech/trunk/bpel2owfn'
+
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'flex' => :build
+    depends_on 'bison' => :build
+    depends_on 'gengetopt' => :build
+    depends_on 'help2man' => :build
+    depends_on 'kimwitu++' => :build
+    depends_on 'gnu-sed' => :build
+  end
 
   def install
     system "autoreconf -i" if build.head?
