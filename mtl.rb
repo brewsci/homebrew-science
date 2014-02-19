7beb939558fd2ceed798bbd6363556c9647e8817
Jack Nagel <jacknagel@gmail.com>
2014-02-18 20:22:11 -0500

    mtl: use spec deps

--- mtl.rb
+++ mtl.rb
@@ -4,9 +4,12 @@ class Mtl < Formula
   homepage 'http://www.simunova.com'
   url 'http://www.simunova.com/downloads/mtl4/MTL-4.0.9486-Linux.tar.bz2'
   sha1 'dea9d4eb4883588a24d0d6d73b061360904abf9f'
-  head 'https://simunova.zih.tu-dresden.de/svn/mtl4/trunk', :using => :svn
 
-  depends_on 'cmake' => :build if build.head?
+  head do
+    url 'https://simunova.zih.tu-dresden.de/svn/mtl4/trunk', :using => :svn
+    depends_on 'cmake' => :build
+  end
+
   depends_on 'boost' => :build
 
   def install
