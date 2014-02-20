b6b9eccb49f9af31e970560e52894a8e9b0f05dc
Shaun Jackman <sjackman@gmail.com>
2014-02-20 15:48:19 -0800

    ray 2.3.1
    No longer fails to build with clang.

--- ray.rb
+++ ray.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ray < Formula
   homepage 'http://denovoassembler.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/denovoassembler/Ray-2.3.0.tar.bz2'
-  sha1 '1b1c085d2d53431bcf671799f0ce351d835ba072'
+  url 'http://downloads.sourceforge.net/project/denovoassembler/Ray-2.3.1.tar.bz2'
+  sha1 'cf7de83f671b38b51177de21604944c49e161f89'
   head 'https://github.com/sebhtml/ray.git'
 
   resource 'RayPlatform' do
@@ -13,11 +13,6 @@ class Ray < Formula
 
   depends_on :mpi => :cxx
 
-  fails_with :clang do
-    build 500
-    cause "error: use of overloaded operator '>>' is ambiguous"
-  end
-
   fails_with :llvm do
     build 2336
     cause '"___gxx_personality_v0" ... ld: symbol(s) not found for architecture x86_64'
@@ -26,12 +21,6 @@ class Ray < Formula
   fails_with :gcc do
     build 5666
     cause "error: wrong number of arguments specified for '__deprecated__' attribute"
-  end
-
-  def patches
-    unless build.head?
-      'https://github.com/sebhtml/ray/commit/e1ad06d8cb55427ff4bcca647b7bd4e4416619e3.patch'
-    end
   end
 
   def install
