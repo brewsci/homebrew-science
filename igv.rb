709c76c8ad2d61adfab1eb4e82275ac7ccf0ada2
Ian Lancaster <ilancaster@gmail.com>
2014-02-20 01:21:38 -0500

    igv 2.3.8

--- igv.rb
+++ igv.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Igv < Formula
   homepage 'http://www.broadinstitute.org/software/igv'
-  url 'http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.25.zip'
-  sha1 '2427a501144ca74889ed35da21b2f40b7184736d'
+  url 'http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.8.zip'
+  sha1 '55d062c17bfe422095f0a5cd5cb7adac7533db9f'
 
   def install
-    inreplace 'igv.sh', /^prefix=.*/, 'prefix=' + libexec
+    inreplace "igv.sh", "`dirname $0`", "#{libexec}"
     libexec.install Dir['igv.sh', '*.jar']
     bin.install_symlink '../libexec/igv.sh' => 'igv'
     doc.install 'readme.txt'
