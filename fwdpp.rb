8d027add48cadaafb1bb3be3b140675657169111
Jeremy Van Cleve <jeremy.vancleve@gmail.com>
2014-02-21 05:22:07 -0500

    fwdpp 0.2.0 (new formula)
    The 'fwdpp' template library abstracts the basic operations
    required to implement forward-time simulations of population-
    and quantitative-genetic models.
    
    Closes #695.

---
+++ fwdpp.rb
@@ -0,0 +1,25 @@
+require "formula"
+
+class Fwdpp < Formula
+  homepage "http://www.molpopgen.org/software/fwdpp"
+  url "https://github.com/molpopgen/fwdpp/archive/0.2.0.tar.gz"
+  sha1 "e3371221c0bd352ccfb231d1b060ed7c26707bcc"
+  head "https://github.com/molpopgen/fwdpp.git"
+
+  depends_on 'gsl'
+  depends_on 'boost' => :build
+  depends_on 'libsequence'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+    system "make", "-C", "examples"
+    share.install Dir['examples']
+  end
+
+  def test
+    # run one of the example programs (https://github.com/molpopgen/fwdpp#diploid)
+    system "#{share}/examples/diploid 1000 10 10 1000 50 1 272"
+  end
+end
