616c5d049c8932f40eeb77bef85620aa8809e72f
Tamas Nepusz <ntamas@gmail.com>
2014-03-02 18:06:19 +0100

    igraph 0.7.0
    Update homepage to http://igraph.org.
    
    Closes #711.

--- igraph.rb
+++ igraph.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Igraph < Formula
-  homepage 'http://igraph.sourceforge.net'
-  url 'http://downloads.sourceforge.net/sourceforge/igraph/igraph-0.6.5.tar.gz'
-  sha1 'f1605c5592e8bf3c97473f7781e77b6608448f78'
+  homepage 'http://igraph.org'
+  url 'http://igraph.org/nightly/get/c/igraph-0.7.0.tar.gz'
+  sha1 '379d1cccec78435ff3fb7940d89ec9a2aa1de157'
 
   option :universal
 
