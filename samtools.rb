750f4719e9df01378ccd2b0f86df5c8f81feaa57
Jack Nagel <jacknagel@gmail.com>
2014-03-03 16:32:03 -0600

    samtools: use resource

--- samtools.rb
+++ samtools.rb
@@ -1,11 +1,5 @@
 require 'formula'
 
-class Dwgsim < Formula
-  homepage 'http://sourceforge.net/apps/mediawiki/dnaa/index.php?title=Whole_Genome_Simulation'
-  url 'https://downloads.sourceforge.net/project/dnaa/dwgsim/dwgsim-0.1.10.tar.gz'
-  sha1 'f3127e84d54cdc52c9b5c988585358f69b4bb675'
-end
-
 class Samtools < Formula
   homepage 'http://samtools.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/samtools/samtools/0.1.19/samtools-0.1.19.tar.bz2'
@@ -22,6 +16,12 @@ class Samtools < Formula
 
   option 'with-dwgsim', 'Build with "Whole Genome Simulation"'
 
+  resource 'dwgsim' do
+    # http://sourceforge.net/apps/mediawiki/dnaa/index.php?title=Whole_Genome_Simulation
+    url 'https://downloads.sourceforge.net/project/dnaa/dwgsim/dwgsim-0.1.10.tar.gz'
+    sha1 'f3127e84d54cdc52c9b5c988585358f69b4bb675'
+  end
+
   def install
     if build.devel?
       inreplace 'Makefile', 'include $(HTSDIR)/htslib.mk', ''
@@ -37,7 +37,7 @@ class Samtools < Formula
     if build.include? 'with-dwgsim'
       ohai "Building dwgsim"
       samtools = pwd
-      Dwgsim.new.brew do
+      resource('dwgsim').stage do
         system "ln -s #{samtools} samtools"
         system "make"
         bin.install %w{dwgsim dwgsim_eval}
