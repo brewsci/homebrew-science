5dbca2ad0a4592b5b8df7b369b6945a46a6798e2
Ian Lancaster <ilancaster@gmail.com>
2014-03-05 04:11:20 -0500

    allpaths-lg: fix url
    Add fails_with for GCC >=4.7 requirement.
    
    Closes #361.

--- allpaths-lg.rb
+++ allpaths-lg.rb
@@ -2,8 +2,18 @@ require 'formula'
 
 class AllpathsLg < Formula
   homepage 'http://www.broadinstitute.org/software/allpaths-lg/blog/'
-  url 'ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-47032.tar.gz'
+  url 'ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/2013-07/allpathslg-47032.tar.gz'
   sha1 'c53cfe3443d769ddd2a77b61e2c600b3cb49bb2a'
+
+  fails_with :clang do
+    build 500
+    cause "The only supported compiler is GCC(>=4.7)."
+  end
+
+  fails_with :gcc do
+    build 5666
+    cause "You must compile this with g++ 4.7 or higher."
+  end
 
   def install
     system './configure', '--disable-debug', '--disable-dependency-tracking',
