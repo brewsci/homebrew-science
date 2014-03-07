0af4f7d728bcdc89812f743f2e22c54c9776b401
Shaun Jackman <sjackman@gmail.com>
2014-03-06 23:39:36 -0800

    bcftools: Tidy up the formula

--- bcftools.rb
+++ bcftools.rb
@@ -1,29 +1,23 @@
-require 'formula'
+require "formula"
 
 class Bcftools < Formula
-  homepage 'https://github.com/samtools/bcftools'
-  version '0.2.0-rc6'
+  homepage "https://github.com/samtools/bcftools"
+  version "0.2.0-rc6"
   url "https://github.com/samtools/bcftools/archive/#{version}.tar.gz"
-  sha1 '544fb9f186960e55b492e5785023220fb3e004ea'
-  depends_on 'chapmanb/cbl/htslib'
+  sha1 "544fb9f186960e55b492e5785023220fb3e004ea"
+  head "https://github.com/samtools/bcftools.git"
+
+  depends_on "htslib"
 
   def install
-    inreplace 'Makefile', 'include $(HTSDIR)/htslib.mk', ''
-    htslib = Formula.factory('htslib').opt_prefix
-    inreplace 'Makefile' do |s|
-      s.change_make_var! 'HTSDIR', htslib
-      s.change_make_var! 'HTSLIB', "#{htslib}/lib/libhts.a"
-      s.change_make_var! 'INCLUDES', "-I. -I#{htslib}/include"
-      s.gsub! '$(HTSDIR)/htslib/', '$(HTSDIR)/include/htslib/'
-      s.gsub! 'all:$(PROG) plugins', 'all:$(PROG)'
-    end
-    # Write version since current Makefile logic fails on non-gmake systems
-    system "echo '#define BCFTOOLS_VERSION \"#{version}\"' > version.h"
-    system 'make'
-    system 'make', 'install', 'prefix=' + prefix
+    inreplace "Makefile", "include $(HTSDIR)/htslib.mk", ""
+    inreplace "Makefile", "$(HTSDIR)/version.h", "$(HTSDIR)/htslib/version.h"
+    htslib = Formula["htslib"].opt_prefix
+    system *%W[make bcftools HTSDIR=#{htslib}/include HTSLIB=#{htslib}/lib/libhts.a]
+    system *%W[make install prefix=#{prefix} HTSDIR=#{htslib}/include HTSLIB=#{htslib}/lib/libhts.a]
   end
 
   test do
-    system 'bcftools'
+    system "#{bin}/bcftools 2>&1 |grep -q bcftools"
   end
 end
