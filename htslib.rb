34264a7e893de26f7300c4a4c58dfc060f7ae6e6
Shaun Jackman <sjackman@gmail.com>
2014-03-07 21:03:58 -0800

    htslib HEAD: Use git rather than a tar.gz
    Use https://github.com/samtools/htslib.git
    rather than https://github.com/samtools/htslib/archive/develop.tar.gz

--- htslib.rb
+++ htslib.rb
@@ -5,7 +5,7 @@ class Htslib < Formula
   version '0.2.0-rc6'
   url "https://github.com/samtools/htslib/archive/#{version}.tar.gz"
   sha1 '08cd1cb79321b3928167c777aca8fa473493fca9'
-  head 'https://github.com/samtools/htslib/archive/develop.tar.gz'
+  head 'https://github.com/samtools/htslib.git'
 
   def patches
     # Makefile: Add razf.o to LIBHTS_OBJS
