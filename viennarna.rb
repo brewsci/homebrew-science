ba95e754ac5755dd1971ab01d5311a9ff9ccf6f2
Ian Lancaster <ilancaster@gmail.com>
2014-03-08 14:00:27 -0500

    viennarna: style cleanup

--- viennarna.rb
+++ viennarna.rb
@@ -1,18 +1,21 @@
-require 'formula'
+require "formula"
 
 class Viennarna < Formula
-  homepage 'http://www.tbi.univie.ac.at/~ronny/RNA/'
-  url 'http://www.tbi.univie.ac.at/~ronny/RNA/packages/source/ViennaRNA-2.1.6.tar.gz'
-  sha1 '04fd80cf659547959b9b90f59e04f9734e5e43ce'
+  homepage "http://www.tbi.univie.ac.at/~ronny/RNA/"
+  url "http://www.tbi.univie.ac.at/~ronny/RNA/packages/source/ViennaRNA-2.1.6.tar.gz"
+  sha1 "04fd80cf659547959b9b90f59e04f9734e5e43ce"
 
-  option 'with-perl', 'Build and install Perl interface'
+  option "with-perl", "Build and install Perl interface"
 
   def install
     ENV['ARCHFLAGS'] = "-arch x86_64"
-    args = ["./configure", "--disable-debug", "--disable-dependency-tracking",
-                      "--prefix=#{prefix}", "--disable-openmp"]
-    args << '--without-perl' unless build.include? 'with-perl'
-    system(*args)
+    args = ["--prefix=#{prefix}",
+            "--disable-debug",
+            "--disable-dependency-tracking",
+            "--disable-openmp"]
+    args << "--without-perl" unless build.include? "with-perl"
+    
+    system "./configure", *args
     system "make install"
   end
 
