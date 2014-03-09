9e649328f407133711efd338a603f7bdc906790d
Ian Lancaster <ilancaster@gmail.com>
2014-03-09 05:11:12 -0400

    nco 4.4.2
    Depends on newer texinfo.
    
    Closes #725.

--- nco.rb
+++ nco.rb
@@ -4,11 +4,12 @@ require 'formula'
 
 class Nco < Formula
   homepage 'http://nco.sourceforge.net'
-  url 'http://nco.sourceforge.net/src/nco-4.3.1.tar.gz'
-  sha1 '0f8cb342905bb310f54c45179ee2c0d0479f905a'
+  url 'https://downloads.sourceforge.net/project/nco/nco-4.4.2.tar.gz'
+  sha1 '6253e0d3b00359e1ef2c95f0c86e940697286a10'
 
-  depends_on 'netcdf'
   depends_on 'gsl'
+  depends_on 'netcdf'
+  depends_on 'texinfo'
   depends_on 'udunits'
 
   # NCO requires the C++ interface in Antlr2.
