b4400500574abcbcd052f612cd9b5b745935599e
Alex Reynolds <alexpreynolds@gmail.com>
2014-03-11 12:36:36 -0700

    hotspot 4.0.0 (new formula)
    Closes #739.

---
+++ hotspot.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Hotspot < Formula
+  homepage "https://github.com/rthurman/hotspot"
+  url "https://github.com/rthurman/hotspot/archive/4.0.0.tar.gz"
+  sha1 "5686637ac1653ba7d8a408f5f49b8d50cff61467"
+  head "https://github.com/rthurman/hotspot.git"
+
+  depends_on "gsl"
+
+  def install
+    ENV.deparallelize
+    system "make -C hotspot-distr/hotspot-deploy"
+    bin.install Dir["hotspot-distr/hotspot-deploy/bin/*"]
+    doc.install %w[LICENSE README.md]
+  end
+end
