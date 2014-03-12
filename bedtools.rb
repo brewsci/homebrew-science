49ee9cc9560ad07b16811a523cac8e80b7842d25
Lance Parsons <lparsons@princeton.edu>
2014-03-12 12:05:17 -0400

    bedtools 2.19.1
    Closes #742.

--- bedtools.rb
+++ bedtools.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bedtools < Formula
   homepage 'https://github.com/arq5x/bedtools2'
-  url 'https://github.com/arq5x/bedtools2/releases/download/v2.19.0/bedtools-2.19.0.tar.gz'
-  sha1 'f870c801ccc96a032f061db4d4a0c54f3ab46e64'
+  url 'https://github.com/arq5x/bedtools2/releases/download/v2.19.1/bedtools-2.19.1.tar.gz'
+  sha1 '7a9027d7992ae399cde8fb7dab520b332605740c'
   head 'https://github.com/arq5x/bedtools2.git'
 
   def install
