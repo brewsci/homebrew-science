9a24be0f2bb89a05e603250183ed1da07c17d16c
Dominique Orban <dominique.orban@gmail.com>
2014-03-13 02:02:37 -0700

    Trnascan: remove change_make_var

--- trnascan.rb
+++ trnascan.rb
@@ -7,30 +7,26 @@ class Trnascan < Formula
   version '1.23'
 
   def install
-    inreplace 'makefile' do |s|
-      s.change_make_var! 'CFLAGS', '-D_POSIX_C_SOURCE=1'
-      s.change_make_var! 'LIBDIR', libexec
-      s.change_make_var! 'BINDIR', bin
-    end
-    system 'make all'
+    make_args = ["CFLAGS=-D_POSIX_C_SOURCE=1", "LIBDIR=#{libexec}", "BINDIR=#{bin}"]
+    system "make", "all", *make_args
 
     bin.install %w[coves-SE covels-SE eufindtRNA trnascan-1.4]
-    bin.install 'tRNAscan-SE.src'.sub(/\.src/, '')
+    bin.install "tRNAscan-SE.src".sub(/\.src/, "")
 
-    (share/name).install Dir.glob('Demo/*.fa')
-    (share/name).install 'testrun.ref'
+    (share / name).install Dir.glob("Demo/*.fa")
+    (share / name).install "testrun.ref"
 
-    libexec.install Dir.glob('gcode.*')
-    libexec.install Dir.glob('*.cm')
-    libexec.install Dir.glob('*signal')
+    libexec.install Dir.glob("gcode.*")
+    libexec.install Dir.glob("*.cm")
+    libexec.install Dir.glob("*signal")
 
-    File.rename('tRNAscan-SE.man','tRNAscan-SE.1')
-    man1.install 'tRNAscan-SE.1'
+    File.rename("tRNAscan-SE.man", "tRNAscan-SE.1")
+    man1.install "tRNAscan-SE.1"
   end
 
   test do
     system "tRNAscan-SE -d -y -o test.out #{share}/#{name}/F22B7.fa"
-    if FileTest.exists? 'test.out'
+    if FileTest.exists? "test.out"
       %x(diff test.out #{share}/#{name}/testrun.ref).empty? ? true : false
     else
       false
