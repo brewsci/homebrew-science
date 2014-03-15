b51c92d7e192bfae55966199587823d313d4397b
Jonathan Chang <jchang641@gmail.com>
2014-03-14 23:35:30 -0700

    quicktree: new download url
    Closes #754.

--- quicktree.rb
+++ quicktree.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Quicktree < Formula
-  url 'ftp://ftp.sanger.ac.uk/pub4/resources/software/quicktree/quicktree.tar.gz'
+  url 'ftp://ftp.sanger.ac.uk/pub/resources/software/quicktree/quicktree.tar.gz'
   version '1.1'
   homepage 'http://www.sanger.ac.uk/resources/software/quicktree/'
   sha1 '9924d51801149d59fd90f704aa7e5802f7b1ef31'
