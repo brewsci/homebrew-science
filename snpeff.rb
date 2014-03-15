33ca528d97c35ddcd0980a9b1b036b6ea22f5a2e
Ian Lancaster <ilancaster@gmail.com>
2014-03-15 09:58:22 -0400

    snpeff: set share dir for downloads
    Move snpEff.config to #{share} and keeps jars in #{libexec}, downloads
    are extracted into #{share}/data.

--- snpeff.rb
+++ snpeff.rb
@@ -7,10 +7,13 @@ class Snpeff < Formula
   sha1 'edf0e0e0b50b9ec3821c445863dc2adae2486104'
 
   def install
-    libexec.install "snpEff.jar", "SnpSift.jar", "snpEff.config"
-    share.install "scripts", "galaxy"
+    inreplace "snpeff" do |s|
+      s.gsub! /^jardir=.*/, "jardir=#{libexec}"
+      s.gsub! "${jardir}/snpEff.config", "#{share}/snpEff.config"
+    end
 
-    inreplace "snpeff", /^jardir=.*/, "jardir=#{libexec}"
+    libexec.install "snpEff.jar", "SnpSift.jar"
+    share.install "snpEff.config", "scripts", "galaxy"
     bin.install "snpeff"
   end
 
@@ -18,7 +21,7 @@ class Snpeff < Formula
     puts <<-EOS.undent
       Download the human database using the command
           snpeff download -v GRCh37.74
-      The databases will be installed in #{share}/java/data
+      The databases will be installed in #{share}/data
     EOS
   end
 
