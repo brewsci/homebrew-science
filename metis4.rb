a13d827da0a6b0763bdd3f1c8e170bda62071ce7
Dominique Orban <dominique.orban@gmail.com>
2014-03-15 22:09:45 -0700

    Metis4: build shared lib and add tests.

--- metis4.rb
+++ metis4.rb
@@ -8,9 +8,27 @@ class Metis4 < Formula
   keg_only "Conflicts with metis (5.x)."
 
   def install
-    system "make"
-    bin.install %w(pmetis kmetis oemetis onmetis partnmesh partdmesh mesh2nodal mesh2dual graphchk Graphs/mtest)
-    lib.install 'libmetis.a'
-    include.install Dir['Lib/*.h']
+    inreplace "Lib/Makefile", "libmetis.a", "libmetis.dylib"
+    make_args = ["COPTIONS=-fPIC",
+                 "AR=libtool -dynamic -install_name #{lib}/$(notdir $@) -undefined dynamic_lookup -o",
+                 "RANLIB=echo",
+                 "METISLIB=../libmetis.dylib"]
+    system "make", *make_args
+    bin.install %w(pmetis kmetis oemetis onmetis partnmesh partdmesh mesh2nodal mesh2dual graphchk)
+    lib.install "libmetis.dylib"
+    include.install Dir["Lib/*.h"]
+    (share / "metis4").install %w(Graphs/mtest Graphs/4elt.graph Graphs/metis.mesh Graphs/test.mgraph)
+  end
+
+  def test
+    system "#{share}/metis4/mtest", "#{share}/metis4/4elt.graph"
+    system "#{bin}/kmetis", "#{share}/metis4/4elt.graph", "40"
+    system "#{bin}/onmetis", "#{share}/metis4/4elt.graph"
+    system "#{bin}/pmetis", "#{share}/metis4/test.mgraph", "2"
+    system "#{bin}/kmetis", "#{share}/metis4/test.mgraph", "2"
+    system "#{bin}/kmetis", "#{share}/metis4/test.mgraph", "5"
+    system "#{bin}/partnmesh", "#{share}/metis4/metis.mesh", "10"
+    system "#{bin}/partdmesh", "#{share}/metis4/metis.mesh", "10"
+    system "#{bin}/mesh2dual", "#{share}/metis4/metis.mesh"
   end
 end
