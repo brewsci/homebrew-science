c1bdcd06f2865af13bf07237d88270442426a164
Shaun Jackman <sjackman@gmail.com>
2014-03-19 15:36:12 -0700

    aragorn 1.2.36: New formula
    tRNA and tmRNA detection

---
+++ aragorn.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Aragorn < Formula
+  homepage "http://mbio-serv2.mbioekol.lu.se/ARAGORN/"
+  url "http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/aragorn1.2.36.tgz"
+  sha1 "3fd5dafa5fa0a65c303d866fc4506bfbc38efbcb"
+
+  def install
+    mv "aragorn#{version}.c", "aragorn.c"
+    system "make", "aragorn"
+    bin.install "aragorn"
+    man1.install "aragorn.1"
+  end
+
+  test do
+    system "#{bin}/aragorn -h"
+  end
+end
