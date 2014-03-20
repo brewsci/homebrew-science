4a852bc10d379dfd2d8c75378c474e9253889e16
Shaun Jackman <sjackman@gmail.com>
2014-03-20 14:50:39 -0700

    repeatmasker: Use RMBlast by default

--- repeatmasker.rb
+++ repeatmasker.rb
@@ -9,6 +9,7 @@ class Repeatmasker < Formula
   option 'without-configure', 'Do not run configure'
 
   depends_on 'hmmer' # at least version 3.1 for nhmmer
+  depends_on 'rmblast'
   depends_on 'trf'
 
   def install
@@ -20,29 +21,42 @@ class Repeatmasker < Formula
     # Enter path [ perl ]:
     # REPEATMASKER INSTALLATION DIRECTORY Enter path
     # TRF PROGRAM Enter path
+    # 2. RMBlast - NCBI Blast with RepeatMasker extensions: [ Un-configured ]
+    # RMBlast (rmblastn) INSTALLATION PATH
+    # Do you want RMBlast to be your default search engine for Repeatmasker?
     # 4. HMMER3.1 & DFAM
     # HMMER INSTALLATION PATH Enter path
     # Do you want HMMER to be your default search engine for Repeatmasker?
     # 5. Done
-    cd libexec do
-      open('config.txt', 'w') do |file|
-        file.write <<-EOS.undent
+    (libexec/"config.txt").write <<-EOS.undent
 
-          /usr/bin/perl
-          #{libexec}
-          #{HOMEBREW_PREFIX}/bin/trf
-          4
-          #{HOMEBREW_PREFIX}/bin
-          Y
-          5
-          EOS
-      end
-      system './configure <config.txt' if build.with? 'configure'
-    end
+      /usr/bin/perl
+      #{libexec}
+      #{HOMEBREW_PREFIX}/bin/trf
+      2
+      #{HOMEBREW_PREFIX}/bin
+      Y
+      4
+      #{HOMEBREW_PREFIX}/bin
+      N
+      5
+      EOS
+    system "cd #{libexec} && ./configure <config.txt" if build.with? "configure"
   end
 
   def caveats; <<-EOS.undent
-    You may reconfigure RepeatMasker by running
+    Congratulations!  RepeatMasker is now ready to use.
+    The program is installed with a minimal repeat library
+    by default.  This library only contains simple, low-complexity,
+    and common artefact ( contaminate ) sequences.  These are
+    adequate for use with your own custom repeat library.  If you
+    plan to search using common species specific repeats you will
+    need to obtain the complete RepeatMasker repeat library from
+    GIRI ( www.giriinst.org ) and install it in
+    #{libexec}
+
+    The default aligner is RMBlast. You may reconfigure RepeatMasker
+    by running
       cd #{libexec} && ./configure
     EOS
   end
