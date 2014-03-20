623b27db88f2a70a9e58a31bb32ce68287171cad
Ian Lancaster <ilancaster@gmail.com>
2014-03-20 02:50:06 -0400

    flann: fix `--with-examples` patch condition

--- flann.rb
+++ flann.rb
@@ -29,7 +29,7 @@ class Flann < Formula
       args << '-DBUILD_PYTHON_BINDINGS:BOOL=OFF'
     end
 
-    inreplace 'CMakeLists.txt', 'add_subdirectory( examples )', '' if build.with? 'examples'
+    inreplace 'CMakeLists.txt', 'add_subdirectory( examples )', '' if build.without? 'examples'
 
     mkdir 'build' do
       system 'cmake', '..', *args
