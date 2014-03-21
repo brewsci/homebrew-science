30032d232b2e81349ff0d20593168080bb4b951b
Ian Lancaster <ilancaster@gmail.com>
2014-03-21 11:38:08 -0400

    cddlib: add mirror

--- cddlib.rb
+++ cddlib.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Cddlib < Formula
   homepage 'http://www.inf.ethz.ch/personal/fukudak/cdd_home/'
   url 'ftp://ftp.ifor.math.ethz.ch/pub/fukuda/cdd/cddlib-094g.tar.gz'
+  mirror 'http://www.mirrorservice.org/sites/distfiles.finkmirrors.net/cddlib-094g.tar.gz'
   sha1 '4ef38ca6efbf3d54a7a753c63ff25a8c95e5fc5f'
 
   depends_on 'gmp'
