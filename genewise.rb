8197c62bb43d8e699344ae9f83c57c6c4e09b4de
Rayan Chikhi <chikhi@psu.edu>
2014-03-23 15:06:33 -0400

    genewise: Fix build error on Linux
    Replace isnumber with isdigit to fix the error:
    undefined reference to `isnumber'
    Tested with GCC 4.4.7
    
    Closes #775

--- genewise.rb
+++ genewise.rb
@@ -20,6 +20,10 @@ class Genewise < Formula
     # getline conflicts with stdio
     inreplace 'src/HMMer2//sqio.c', 'getline', 'getline_ReadSeqVars'
 
+    # undefined reference to `isnumber'
+    # patch suggested in http://korflab.ucdavis.edu/datasets/cegma/ubuntu_instructions_1.txt
+    inreplace 'src/models/phasemodel.c', 'isnumber', 'isdigit' if OS.linux?
+
     system *%w[make -C src all]
     bin.install Dir['src/bin/*']
     doc.install %w[INSTALL LICENSE README], *Dir['docs/*']
