bf6e467808a928049c07c8d1e4cc019a86eba6db
Ian Lancaster <ilancaster@gmail.com>
2014-03-30 20:41:20 -0400

    snpeff 3.5g
    Closes #792.

--- snpeff.rb
+++ snpeff.rb
@@ -3,29 +3,28 @@ require 'formula'
 class Snpeff < Formula
   homepage 'http://snpeff.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/snpeff/snpEff_v3_5_core.zip'
-  version '3.5e'
-  sha1 'edf0e0e0b50b9ec3821c445863dc2adae2486104'
+  version '3.5g'
+  sha1 'd1371a2173f6cdc2b79ad7b2cf8b5355bb80b83e'
 
   def install
-    inreplace "snpeff" do |s|
+    inreplace "scripts/snpEff" do |s|
       s.gsub! /^jardir=.*/, "jardir=#{libexec}"
       s.gsub! "${jardir}/snpEff.config", "#{share}/snpEff.config"
     end
 
+    bin.install "scripts/snpEff"
     libexec.install "snpEff.jar", "SnpSift.jar"
     share.install "snpEff.config", "scripts", "galaxy"
-    bin.install "snpeff"
   end
 
-  def caveats
-    puts <<-EOS.undent
+  def caveats; <<-EOS.undent
       Download the human database using the command
-          snpeff download -v GRCh37.74
+          snpEff download -v GRCh37.74
       The databases will be installed in #{share}/data
     EOS
   end
 
   test do
-    system "#{bin}/snpeff 2>&1 |grep -q snpEff"
+    system "#{bin}/snpEff 2>&1 |grep -q snpEff"
   end
 end
