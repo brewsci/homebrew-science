f3ae60d93ec36a9b3e5418eeee31126b5ac3a50c
Samuel John <github@SamuelJohn.de>
2014-04-04 13:25:43 +0200

    openblas: Remove outdated comment
    I made that comment ages ago when OpenBLAS did download an LAPACK
    on-demand during build. This is no longer the case as far as I can tell.

--- openblas.rb
+++ openblas.rb
@@ -2,8 +2,6 @@ require 'formula'
 
 class Openblas < Formula
   homepage 'http://xianyi.github.io/OpenBLAS/'
-  # Maintainers, remember to update the LAPACK url in OpenblasLapack above.
-  # See the "LAPACK_URL" in the openblas Makefile for the right version.
   url 'https://github.com/xianyi/OpenBLAS/archive/v0.2.8.tar.gz'
   sha1 'd012ebc2b8dcd3e95f667dff08318a81479a47c3'
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
@@ -14,7 +12,6 @@ class Openblas < Formula
   keg_only :provided_by_osx
 
   def install
-
     # Must call in two steps
     system "make", "FC=#{ENV['FC']}", "libs", "netlib", "shared"
     system "make", "FC=#{ENV['FC']}", "tests"
