51fa934f3d8cc70a0c711b9e1d0e58a01eae012e
Ian Lancaster <ilancaster@gmail.com>
2014-04-07 18:19:29 -0400

    prodigal: update url, add HEAD
    Development moved to GitHub.

--- prodigal.rb
+++ prodigal.rb
@@ -2,11 +2,14 @@ require 'formula'
 
 class Prodigal < Formula
   homepage 'http://prodigal.ornl.gov/'
-  url 'https://prodigal.googlecode.com/files/prodigal.v2_60.tar.gz'
-  sha1 '23a45dafedd98c04f9a4edbe82b037120644eaa2'
+  url 'https://github.com/hyattpd/Prodigal/archive/v2.60.tar.gz'
+  sha1 '3de4d1c64c6250cb38f37853c0edef885e03b5f5'
+
+  head 'https://github.com/hyattpd/Prodigal.git'
 
   def install
     system "make"
+    mv "prodigal2", "prodigal" if build.head?
     bin.install 'prodigal'
   end
 end
