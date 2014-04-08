ab67e0c7a03cea69c2bad7e590f603ab6e0ddcf1
Ian Lancaster <ilancaster@gmail.com>
2014-04-08 06:32:09 -0400

    snp_sites 1.5.0

--- snp_sites.rb
+++ snp_sites.rb
@@ -2,14 +2,27 @@ require 'formula'
 
 class SnpSites < Formula
   homepage 'https://github.com/sanger-pathogens/snp_sites'
-  url 'ftp://ftp.sanger.ac.uk/pub/pathogens/software/snp_sites/snp_sites-1.tar.gz'
-  sha1 'ee4ded1bcb9486b0ee454cf592b4286e891269ab'
+  url 'https://github.com/sanger-pathogens/snp_sites/archive/1.5.0.tar.gz'
+  sha1 '3b9e6dd25a6c4dd75631f95f593b18697115e110'
   head 'https://github.com/sanger-pathogens/snp_sites.git'
 
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    inreplace "src/Makefile.am", "-lrt", "" if OS.mac? # no librt for osx
+
+    system "autoreconf -i"
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--prefix=#{prefix}"
+
     system "make", "install"
+
+    ln_s "#{bin}/snp-sites", "#{bin}/snp_sites"
+    share.install "tests/data"
   end
 
   test do
