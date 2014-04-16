271ed3e8163d045768878f1a0517ff745607b906
Ian Lancaster <ilancaster@gmail.com>
2014-04-15 21:47:18 -0400

    bowtie2 2.2.2

--- bowtie2.rb
+++ bowtie2.rb
@@ -1,18 +1,20 @@
 require 'formula'
 
 class Bowtie2 < Formula
-  homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
-  url 'https://github.com/BenLangmead/bowtie2/archive/v2.2.1.tar.gz'
-  sha256 'b8561370bc472f14471fff8dd478f225c52f910c0836ad76bf917e2cd538320a'
-  head 'https://github.com/BenLangmead/bowtie2.git'
+  homepage "http://bowtie-bio.sourceforge.net/bowtie2/index.shtml"
+  url "https://github.com/BenLangmead/bowtie2/archive/v2.2.2.tar.gz"
+  sha1 "7cccb06ee9e57e9566b0bb0fce4b9a3d01d0ffd7"
+  head "https://github.com/BenLangmead/bowtie2.git"
 
   def install
     system "make"
-    bin.install %w[bowtie2 bowtie2-align-l bowtie2-align-s
-      bowtie2-build bowtie2-build-l bowtie2-build-s
-      bowtie2-inspect bowtie2-inspect-l bowtie2-inspect-s]
-    doc.install %w[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS README
-      TUTORIAL VERSION]
+    bin.install %W[bowtie2
+                   bowtie2-align-l bowtie2-align-s
+                   bowtie2-build   bowtie2-build-l   bowtie2-build-s
+                   bowtie2-inspect bowtie2-inspect-l bowtie2-inspect-s]
+
+    doc.install %W[AUTHORS LICENSE MANUAL
+                   NEWS README TUTORIAL VERSION]
   end
 
   def test
