743d4891c97ee37f84b46fece1bd27f7c773514c
Ian Lancaster <ilancaster@gmail.com>
2014-04-18 21:01:32 -0400

    snpeff 3.5h

--- snpeff.rb
+++ snpeff.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Snpeff < Formula
   homepage 'http://snpeff.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/snpeff/snpEff_v3_5_core.zip'
-  version '3.5g'
-  sha1 'd1371a2173f6cdc2b79ad7b2cf8b5355bb80b83e'
+  version '3.5h'
+  sha1 '77a45dd11b3ea8cddfbb0615d5015de5f92146fb'
 
   def install
     inreplace "scripts/snpEff" do |s|
