a88cb65b323043e45c18346032d4f8330e56cd3b
Ian Lancaster <ilancaster@gmail.com>
2014-04-18 21:12:26 -0400

    igvtools 2.3.32

--- igvtools.rb
+++ igvtools.rb
@@ -2,13 +2,14 @@ require "formula"
 
 class Igvtools < Formula
   homepage "http://www.broadinstitute.org/software/igv"
-  url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.31.zip"
-  sha1 "32af21b07a762d2c56bcb0af49c3ff90811d1e03"
+  url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.32.zip"
+  sha1 "7de35e1ac3908174f99a76e1edd2134b5f1f13cf"
 
   def install
-    libexec.install Dir["igvtools.jar"]
+    libexec.install "igvtools.jar"
     bin.write_jar_script libexec/"igvtools.jar", "igvtools"
     doc.install "igvtools_readme.txt"
+    share.install Dir["genomes"]
   end
 
   test do
