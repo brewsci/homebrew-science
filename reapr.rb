8c33d6d2b0360db0984466d01880f108cfd5034a
Ian Lancaster <ilancaster@gmail.com>
2014-04-29 16:13:47 -0400

    reapr 1.0.17
    Fixes #851.

--- reapr.rb
+++ reapr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Reapr < Formula
   homepage "https://www.sanger.ac.uk/resources/software/reapr/"
-  url "ftp://ftp.sanger.ac.uk/pub/resources/software/reapr/Reapr_1.0.16.tar.gz"
-  sha1 "8001ba02d5c0db9a895c48dd32dd7b77d95723bf"
+  url "ftp://ftp.sanger.ac.uk/pub/resources/software/reapr/Reapr_1.0.17.tar.gz"
+  sha1 "4d2a8856a6d19d259aeec8591a024ba598003acc"
 
   depends_on "bamtools"
   depends_on "samtools"
@@ -14,17 +14,17 @@ class Reapr < Formula
   def install
     system *%W[make -C src/tabix]
     system *%W[make -C src
-      CFLAGS=-I#{Formula["bamtools"].opt_prefix/"include/bamtools"}]
-    bin.install "src/reapr.pl" => "reapr"
-    doc.install %w[README changelog.txt licence.txt manual.pdf]
+      CFLAGS=-I#{Formula["bamtools"].opt_include}/bamtools]
+    doc.install %w[README changelog.txt licence.txt]
     cd "src" do
       libexec.install %w[bam2fcdEstimate bam2fragCov bam2insert
         bam2perfect fa2gaps fa2gc make_plots n50 scaff2contig
         task_break task_fcdrate task_gapresize task_score task_stats
         task_facheck.pl task_perfectfrombam.pl task_perfectmap.pl
         task_plots.pl task_preprocess.pl task_smaltmap.pl
-        task_summary.pl]
+        task_summary.pl reapr.pl]
     end
+    bin.install_symlink libexec+"reapr.pl" => "reapr"
   end
 
   test do
