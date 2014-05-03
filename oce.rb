5692247c4eea09c0c283ed76c44849cb4c481365
Peter Lama <peterldev94@gmail.com>
2014-05-03 12:02:02 -0700

    oce: remove X11 dependency
    OCE does not use X11 on Mac OS X.
    
    Closes #856.

--- oce.rb
+++ oce.rb
@@ -13,7 +13,6 @@ class Oce < Formula
   depends_on "freeimage" => :recommended
   depends_on "gl2ps" => :recommended
   depends_on "tbb" => :recommended
-  depends_on :x11 => :recommended
   depends_on :macos => :snow_leopard
 
   def install
@@ -22,7 +21,7 @@ class Oce < Formula
     cmake_args << "-DOCE_COPY_HEADERS_BUILD:BOOL=ON"
     cmake_args << "-DOCE_DRAW:BOOL=ON"
     cmake_args << "-DOCE_MULTITHREAD_LIBRARY:STRING=TBB" if build.with? "tbb"
-    cmake_args << "-DOCE_DISABLE_X11:BOOL=ON" if build.without? "x11"
+    cmake_args << "-DFREETYPE_INCLUDE_DIRS=#{Formula['freetype'].include}/freetype2"
 
     %w{freeimage gl2ps}.each do |feature|
       cmake_args << "-DOCE_WITH_#{feature.upcase}:BOOL=ON" if build.with? feature
