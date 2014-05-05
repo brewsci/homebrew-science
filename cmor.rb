4b83b770090bdb4d18d19813ec23b2177a057f70
Li Dong <dongli@lasg.iap.ac.cn>
2014-05-06 00:00:50 +0800

    cmor 2.9.1 (new formula)
    CMOR is Climate Model Output Rewriter, which is a standard tool for
    transforming climate model output data into standard form. For more
    information, see http://www2-pcmdi.llnl.gov/cmor.
    
    Closes #860.

---
+++ cmor.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class Cmor < Formula
+  homepage "http://www2-pcmdi.llnl.gov/cmor"
+  url "https://github.com/PCMDI/cmor/archive/CMOR-2.9.1.tar.gz"
+  sha1 "1e2d8e539ff4deb76b98886f28b1881464017a2a"
+
+  depends_on "ossp-uuid"
+  depends_on "udunits"
+  depends_on "netcdf" => "enable-fortran"
+  depends_on :fortran
+
+  def install
+    ENV.append "CFLAGS", "-Wno-error=unused-command-line-argument-hard-error-in-future"
+
+    args = %W[
+      --prefix=#{prefix}
+      --with-uuid=#{Formula["ossp-uuid"].opt_prefix}
+      --with-udunits2=#{Formula["udunits"].opt_prefix}
+      --with-netcdf=#{Formula["netcdf"].opt_prefix}
+    ]
+
+    system "./configure", *args
+    system "make install"
+  end
+end
