80dc7608e798ed93a16c5bf6eae0ef4ab7c18ca4
Dominique Orban <dominique.orban@gmail.com>
2014-05-04 20:03:27 -0700

    SuperLU: new formula.
    Thanks to @ysakamoto86
    Closes #721.

---
+++ superlu.rb
@@ -0,0 +1,40 @@
+require "formula"
+
+class Superlu < Formula
+  homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/"
+  url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz"
+  sha1 "d2863610d8c545d250ffd020b8e74dc667d7cbdd"
+
+  option "without-check", "skip build-time tests (not recommended)"
+
+  depends_on :fortran
+  depends_on 'openblas' => :optional
+
+  def install
+    ENV.deparallelize
+    cp "MAKE_INC/make.mac-x", "./make.inc"
+    make_args = ["RANLIB=true", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}",
+                 "FORTRAN=#{ENV.fc}", "FFLAGS=#{ENV.fcflags}"]
+
+    make_args << ((build.with? "openblas") ? "BLASLIB=-L#{Formula["openblas"].lib} -lopenblas" : "BLASLIB=-framework Accelerate")
+
+    system "make", "lib", *make_args
+    if build.with? "check"
+      system "make", "testing", "SuperLUroot=#{buildpath}", *make_args
+      cd "TESTING"
+      system "make", *make_args
+      %w[stest dtest ctest ztest].each do |tst|
+        ohai `tail -1 #{tst}.out`.chomp
+      end
+      cd ".."
+    end
+    prefix.install "make.inc"
+    File.open(prefix / "make_args.txt", "w") do |f|
+      f.puts(make_args.join(" "))  # Record options passed to make.
+    end
+    lib.install Dir["lib/*"]
+    (include / "superlu").install Dir["SRC/*.h"]
+    doc.install Dir["Doc/*"]
+    (share / "superlu").install "EXAMPLE"
+  end
+end
