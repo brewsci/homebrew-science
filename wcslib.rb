90ab52f1f11370de5893131c6cfc96ff64c89ef5
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-05-05 07:05:16 -0700

    wcslib 4.22
    Closes #859.

--- wcslib.rb
+++ wcslib.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Wcslib < Formula
   homepage "http://www.atnf.csiro.au/people/mcalabre/WCS/"
-  url "ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.21.tar.bz2"
-  sha1 "35267fb33dd4f4e2463e259bcf4b3822218feb5d"
+  url "ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.22.tar.bz2"
+  sha1 "f438bda7d02d7981b320a9659962608406b8e01d"
 
   option "with-pgsbox", "Build PGSBOX, a general curvilinear axis drawing routine for PGPLOT"
   option "with-fortran", "Build Fortran wrappers. Needed for --with-pgsbox."
