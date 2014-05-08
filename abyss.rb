af2ad3b5b2ddfec757428eecfb94682dccdec5c6
Shaun Jackman <sjackman@gmail.com>
2014-05-08 15:04:34 -0700

    abyss 1.5.1

--- abyss.rb
+++ abyss.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Abyss < Formula
   homepage 'http://www.bcgsc.ca/platform/bioinfo/software/abyss'
   #doi '10.1101/gr.089532.108'
-  url 'https://github.com/bcgsc/abyss/releases/download/1.5.0/abyss-1.5.0.tar.gz'
-  sha1 '3340a22889604b2ef0d12db962896afc3d72da1b'
+  url 'https://github.com/bcgsc/abyss/releases/download/1.5.1/abyss-1.5.1.tar.gz'
+  sha1 '80ebb01d7ae3960e5841c741d5409e0fc6fdf83f'
 
   head do
     url 'https://github.com/bcgsc/abyss.git'
