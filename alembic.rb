47a42abdffcc6582b3a08447360bd7a2fcfe1488
Ian Lancaster <ilancaster@gmail.com>
2014-05-09 23:03:16 -0400

    alembic 1.5.4
    Closes #403.

--- alembic.rb
+++ alembic.rb
@@ -1,20 +1,30 @@
-require 'formula'
+require "formula"
 
 class Alembic < Formula
-  homepage 'http://opensource.imageworks.com/?p=alembic'
-  url 'https://alembic.googlecode.com/files/Alembic_1.1.5_2013041100.tgz'
-  sha1 '539813017342d156ed5b0efafc983bda9b2cb001'
-  version '1.1.5'
+  homepage "http://alembic.io"
+  url "http://alembic.googlecode.com/archive/1_05_04.zip"
+  sha1 "6d3ebdee8c4891bb949203e97baae283148b43e6"
+  version "1.5.4"
 
-  depends_on 'cmake' => :build
-  depends_on 'boost'
-  depends_on 'hdf5'
-  depends_on 'ilmbase'
+  needs :cxx11
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "hdf5"
+  depends_on "ilmbase"
 
   def install
-    cmake_args = std_cmake_args + %w{. -DUSE_PYILMBASE=OFF -DUSE_PRMAN=OFF -DUSE_ARNOLD=OFF -DUSE_MAYA=OFF -DUSE_PYALEMBIC=OFF}
-    system "cmake", *cmake_args
+    cmake_args = std_cmake_args + %W[
+      -DUSE_PYILMBASE=OFF
+      -DUSE_PRMAN=OFF
+      -DUSE_ARNOLD=OFF
+      -DUSE_MAYA=OFF
+      -DUSE_PYALEMBIC=OFF
+      -DCMAKE_CXX_FLAGS='-std=c++11'
+    ]
+    system "cmake", ".", *cmake_args
     system "make", "install"
+
     #move everything upwards
     lib.install_symlink Dir[prefix/"alembic-#{version}/lib/static/*"]
     include.install_symlink Dir[prefix/"alembic-#{version}/include/*"]
