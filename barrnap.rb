61fc87530576f8ece76528832a0de0116c708b29
Shaun Jackman <sjackman@gmail.com>
2014-05-14 16:32:05 -0700

    barrnap 0.4.2

--- barrnap.rb
+++ barrnap.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Barrnap < Formula
   homepage "http://www.vicbioinformatics.com/software.barrnap.shtml"
-  url "http://www.vicbioinformatics.com/barrnap-0.4.tar.gz"
-  sha1 "28e8a039e88867eacaf3c349f79e72b9f3348ddf"
+  url "http://www.vicbioinformatics.com/barrnap-0.4.2.tar.gz"
+  sha1 "009d82d3f548c13675736b8a6a0aa0d3f5849c64"
   head "https://github.com/Victorian-Bioinformatics-Consortium/barrnap.git"
 
   depends_on "hmmer"
