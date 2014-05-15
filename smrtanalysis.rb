31298bd05c440af6fe8263b8ed52add6402ad8c3
Shaun Jackman <sjackman@gmail.com>
2014-05-15 16:51:33 -0700

    smrtanalysis 2.2.0.133377: New formula

---
+++ smrtanalysis.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Smrtanalysis < Formula
+  homepage "http://www.pacb.com/devnet/"
+  #doi "10.1038/nmeth.2474"
+  url "https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377.run"
+  sha1 "b64cdeb68f84f021ccaeffaa62ec5977029451af"
+
+  # Patch
+  # https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377-patch-1.run
+
+  def install
+    raise "SMRT Analysis cannot be installed on Mac OS" if OS.mac?
+    system "sh ./smrtanalysis-#{version}.run --extract-only --rootdir #{prefix}"
+  end
+
+  test do
+    system "#{prefix}/install/smrtanalysis-2.2.0.133377/analysis/bin/blasr"
+  end
+end
