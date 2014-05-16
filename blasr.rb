596c855209e0e48c5edab8d6dedb712575a45b9e
Shaun Jackman <sjackman@gmail.com>
2014-05-15 17:13:51 -0700

    blasr 2.1: Fails to build with clang and gcc-4.2

--- blasr.rb
+++ blasr.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Blasr < Formula
   homepage 'https://github.com/PacificBiosciences/blasr'
+  #doi '10.1186/1471-2105-13-238'
   url 'https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.1.tar.gz'
   sha1 'ac6add1ba8a82cac2515da36c0ec53060c20ea0f'
   head 'https://github.com/PacificBiosciences/blasr.git'
@@ -9,11 +10,20 @@ class Blasr < Formula
   depends_on 'hdf5' => 'enable-cxx'
 
   fails_with :clang do
-    build 500
+    build 503
     cause <<-EOS.undent
       error: destructor type 'HDFWriteBuffer<int>' in object
       destruction expression does not match the type
       'BufferedHDFArray<int>' of the object being destroyed
+    EOS
+  end
+
+  fails_with :gcc do
+    build 5666
+    cause <<-EOS.undent
+      error: invalid conversion
+      from ‘void (*)(H5::H5Object&, std::string, void*)’
+      to ‘void (*)(H5::H5Location&, std::string, void*)’
     EOS
   end
 
