1bdff52d1014f62eb146ab62a328264964e608b8
Shaun Jackman <sjackman@gmail.com>
2014-05-15 22:45:35 -0700

    exonerate 2.2.0: Add DOI and a test

--- exonerate.rb
+++ exonerate.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Exonerate < Formula
   homepage 'http://www.ebi.ac.uk/~guy/exonerate/'
+  #doi '10.1186/1471-2105-6-31'
   url 'http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.2.0.tar.gz'
   sha1 'ad4de207511e4d421e5cc28dda2261421c515bf0'
 
@@ -14,5 +15,9 @@ class Exonerate < Formula
     ENV.j1
     system "make"
     system "make install"
+  end
+
+  test do
+    system "#{bin}/exonerate --version |grep exonerate"
   end
 end
