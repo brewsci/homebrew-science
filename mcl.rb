b5c3ec4da29164e7f156eb50d857c0aa0fe2a918
Tamas Nepusz <ntamas@gmail.com>
2014-05-16 22:25:14 +0200

    mcl 14-136
    Closes #890.

--- mcl.rb
+++ mcl.rb
@@ -1,17 +1,17 @@
-require 'formula'
+require "formula"
 
 class Mcl < Formula
-  homepage 'http://micans.org/mcl'
-  url 'http://micans.org/mcl/src/mcl-12-135.tar.gz'
-  version '12-135'
-  sha1 '27e7bc08fe5f0d3361bbc98d343c9d045712e406'
+  homepage "http://micans.org/mcl"
+  url "http://micans.org/mcl/src/mcl-14-136.tar.gz"
+  version "14-136"
+  sha1 "7a0973cbd884b20f554bcb20215d7d280a0cb569"
+
+  # Fixes a problem in upstream where cpu_set_t is used without guarding it
+  # in an #ifdef _GNU_SOURCE block. Upstream has been notified; the patch can
+  # safely be removed when it is merged.
+  patch :DATA
 
   def install
-    # Force the compiler to run in C89 mode because one of the source
-    # files uses "restrict" as a variable name and this is a restricted
-    # keyword in C99
-    ENV.append_to_cflags '-std=c89'
-
     bin.mkpath
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -19,3 +19,20 @@ class Mcl < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/src/impala/matrix.c b/src/impala/matrix.c
+index 38f057c..087926f 100644
+--- a/src/impala/matrix.c
++++ b/src/impala/matrix.c
+@@ -2072,7 +2072,9 @@ mcxstatus mclxVectorDispatchGroup
+    ;  struct generic_arg* garg = mcxAlloc(n_thread * sizeof garg[0], EXIT_ON_FAIL)
+    ;  pthread_attr_t  t_attr
+    ;  dim thread_id = 0, t_spun = 0
++#ifdef _GNU_SOURCE
+    ;  cpu_set_t cpuset[512] = { 0 }
++#endif
+ 
+    ;  if (n_group == 0 || group_id >= n_group)
+       {  mcxErr("mclxVectorDispatchGroup PBD", "wrong parameters")
+
