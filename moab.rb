85c412adde1755ca74aaee394613cac3ff8af31c
Geoffrey Oxberry <oxberry1@llnl.gov>
2014-05-16 21:29:29 -0700

    moab 4.6.2
    Also use --libdir=libexec/lib
    
    Closes #891.

--- moab.rb
+++ moab.rb
@@ -2,19 +2,26 @@ require 'formula'
 
 class Moab < Formula
   homepage 'https://trac.mcs.anl.gov/projects/ITAPS/wiki/MOAB'
-  url 'https://bitbucket.org/fathomteam/moab/get/4.6.1.tar.gz'
-  sha1 '6fad0bc6ff6c8c067edbfb56b5e7068c92a8e7cd'
+  url 'https://bitbucket.org/fathomteam/moab/get/4.6.2.tar.gz'
+  sha1 'cecd30aaf9bdbb58078e06b0097a9144cac63d7f'
+
+  option 'without-check', "Skip build-time checks (not recommended)"
 
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
   depends_on 'netcdf'
   depends_on 'hdf5'
+  depends_on :fortran if build.with? 'check'
 
   def install
     system "autoreconf", "--force", "--install"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}", "--libdir=#{libexec}/lib"
+
     system "make install"
+    # Moab installs non-lib files in libdir. Link only the libraries.
+    lib.install_symlink Dir["#{libexec}/lib/*.a"]
+    system "make check" if build.with? 'check'
   end
 end
