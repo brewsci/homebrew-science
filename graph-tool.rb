a59c354a6081b9354483277d988b342f512d1d0b
Tzu-ping Chung <uranusjr@gmail.com>
2014-05-21 13:13:33 +0800

    graph-tool: fix dep; flag changes; add test
    Build failure fixed by requiring Python on Boost dependency.
    
    Sparsehash dependency is recommended offically. Since the sparsehash
    installation problem on 10.9.2 is now fixed (Homebrew/homebrew#27652),
    it should be enabled by default.
    
    Add option to drop Cairo dependency.
    
    Add test based on the official quick start example
    (http://graph-tool.skewed.de/static/doc/quickstart.html).
    
    Closes #917.

--- graph-tool.rb
+++ graph-tool.rb
@@ -6,12 +6,14 @@ class GraphTool < Formula
   sha1 '5e0b1c215ecd76191a82c745df0fac17e33bfb09'
   head 'https://github.com/count0/graph-tool.git'
 
+  option 'without-cairo', 'Build without cairo support'
+
   depends_on 'pkg-config' => :build
-  depends_on 'boost' => 'c++11'
+  depends_on 'boost' => ['c++11', 'with-python']
   depends_on 'cgal' => 'c++11'
-  depends_on 'google-sparsehash' => ['c++11', :optional]
-  depends_on 'cairomm' => 'c++11'
-  depends_on 'py2cairo'
+  depends_on 'google-sparsehash' => ['c++11', :recommended]
+  depends_on 'cairomm' => 'c++11' if build.with? 'cairo'
+  depends_on 'py2cairo' if build.with? 'cairo'
   depends_on 'matplotlib' => :python
   depends_on 'numpy' => :python
   depends_on 'scipy' => :python
@@ -25,9 +27,23 @@ class GraphTool < Formula
       --prefix=#{prefix}
       --with-python-module-path=#{lib}/python2.7/site-packages
     ]
+    config_args << "--disable-cairo" if build.without? 'cairo'
     config_args << "--disable-sparsehash" if build.without? 'google-sparsehash'
     system "./configure", "PYTHON_EXTRA_LDFLAGS= ", *config_args
     system "make", "install"
   end
 
+  test do
+    Pathname('test.py').write <<-EOS.undent
+      import graph_tool.all as gt
+      g = gt.Graph()
+      v1 = g.add_vertex()
+      v2 = g.add_vertex()
+      e = g.add_edge(v1, v2)
+    EOS
+
+    Language::Python.each_python(build) do |python, version|
+      system python, "test.py"
+    end
+  end
 end
