c755f00639c09b33e05494282b778a3e81a47cae
Shaun Jackman <sjackman@gmail.com>
2014-05-21 11:31:35 -0700

    bedtools: Add DOI

--- bedtools.rb
+++ bedtools.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Bedtools < Formula
   homepage 'https://github.com/arq5x/bedtools2'
+  #doi '10.1093/bioinformatics/btq033'
   url 'https://github.com/arq5x/bedtools2/releases/download/v2.19.1/bedtools-2.19.1.tar.gz'
   sha1 '7a9027d7992ae399cde8fb7dab520b332605740c'
   head 'https://github.com/arq5x/bedtools2.git'
