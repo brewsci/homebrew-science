8c3c2b30a9b224d97744d4bde207e5273bb35d3a
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-05-27 16:43:55 -0400

    bali-phy: switch to needs :cxx11
    Closes #925.

--- bali-phy.rb
+++ bali-phy.rb
@@ -8,14 +8,7 @@ class BaliPhy < Formula
   depends_on 'pkg-config' => :build
   depends_on 'cairo'
 
-  # This package requires C++11, through either gcc-4.8 or through
-  # clang 3.3svn in Xcode 5 or newer.
-  # Clang 3.4.0 works with libstdc++ from gcc-4.8.
-  # On Mavericks, the Xcode defaults (libc++) work.
-  fails_with :clang do
-     build 425
-     cause 'This formula requires C++11 support available in clang build 500 or newer.'
-  end
+  needs :cxx11
 
   def install
     mkdir 'macbuild' do
