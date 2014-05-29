5489e44bfa90e6fae28598a8d7467d1ac8c58ac1
Shaun Jackman <sjackman@gmail.com>
2014-05-29 11:00:15 -0700

    htslib: Conflicts with tabix
    Both htslib and tabix install bin/tabix

--- htslib.rb
+++ htslib.rb
@@ -7,6 +7,8 @@ class Htslib < Formula
   sha1 'c6db4573fba0cd189130add7d4d86c7d17295f34'
   head 'https://github.com/samtools/htslib.git'
 
+  conflicts_with "tabix", :because => "both htslib and tabix install bin/tabix"
+
   def install
     # Write version to avoid 0.0.1 version information output from Makefile
     system "echo '#define HTS_VERSION \"#{version}\"' > version.h"
