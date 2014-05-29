ae3120d7f91710f3d3898b50986cf1ce92639781
Ian Lancaster <ilancaster@gmail.com>
2014-05-29 15:54:41 -0400

    elph: fails with clang

--- elph.rb
+++ elph.rb
@@ -5,6 +5,11 @@ class Elph < Formula
   url 'ftp://ftp.cbcb.umd.edu/pub/software/elph/ELPH-1.0.1.tar.gz'
   sha1 '9c8bf6ac54ade29daa15dc07aeeb94747626298a'
 
+  fails_with :clang do
+    build 503
+    cause "error: '-I-' not supported, please use -iquote instead"
+  end
+
   def install
     cd 'sources' do
       system 'make'
