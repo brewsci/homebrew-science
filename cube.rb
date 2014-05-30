ede45f1b9fdec708c0f84869a0db60fecc1ef32c
Shaun Jackman <sjackman@gmail.com>
2014-05-29 22:48:27 -0700

    cube 4.2.2: New formula
    Cube, which is used as performance report explorer for Scalasca and
    Score-P, is a generic tool for displaying a multi-dimensional
    performance space consisting of the dimensions (i) performance metric,
    (ii) call path, and (iii) system resource.

---
+++ cube.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Cube < Formula
+  homepage "http://apps.fz-juelich.de/scalasca/"
+  url "http://apps.fz-juelich.de/scalasca/releases/cube/4.2/dist/cube-4.2.2.tar.gz"
+  sha1 "88597c2df082bdc4eeadd6a9dad791b5894c6daa"
+
+  def install
+    ENV.deparallelize
+
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/cube-config --version"
+  end
+end
