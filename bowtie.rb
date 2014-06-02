65cfe472e973fd71cc707ecedef5f30f7d3f1480
Ian Lancaster <ilancaster@gmail.com>
2014-06-02 18:13:15 -0400

    bowtie: add test suite, fails with gcc-4.8
    Refactor for tests, switch to more complete package archive.
    
    Closes #885.

--- bowtie.rb
+++ bowtie.rb
@@ -1,11 +1,15 @@
-require 'formula'
+require "formula"
 
 class Bowtie < Formula
-  homepage 'http://bowtie-bio.sourceforge.net/index.shtml'
-  url 'https://downloads.sourceforge.net/project/bowtie-bio/bowtie/1.0.1/bowtie-1.0.1-src.zip'
-  sha256 '41f6022652c65a7896b8daf0507fd7626241fd2a87894a02bc6fd428d4d0b447'
+  homepage "http://bowtie-bio.sourceforge.net/index.shtml"
+  #doi "10.1186/gb-2009-10-3-r25"
+  url "https://github.com/BenLangmead/bowtie/archive/v1.0.1.tar.gz"
+  sha256 "aedcada44803dbe6e3e2fa7bd13a040733fac95032b4102918612310f5bbaa54"
+  head "https://github.com/BenLangmead/bowtie.git"
 
-  head 'https://github.com/BenLangmead/bowtie.git'
+  fails_with :gcc => "4.8" do
+    cause "Build failures with gcc-4.8. Install with --cc=gcc-4.2"
+  end
 
   fails_with :clang do
     build 503
@@ -13,14 +17,33 @@ class Bowtie < Formula
   end
 
   def install
-    system 'make'
-    bin.install %w[bowtie bowtie-build bowtie-inspect]
-    doc.install %w[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS TUTORIAL
-      doc/manual.html]
-    libexec.install Dir['scripts/*']
+    system "make", "allall"
+
+    # preserve directory structure for tests/scripts
+    libexec.install %W[
+      bowtie bowtie-debug
+      bowtie-build bowtie-build-debug
+      bowtie-inspect bowtie-inspect-debug
+      scripts
+      genomes indexes reads
+    ]
+
+    bin.install_symlink %W[
+      #{libexec}/bowtie
+      #{libexec}/bowtie-build
+      #{libexec}/bowtie-inspect
+    ]
+
+    doc.install %W[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS TUTORIAL]
+
+    inreplace libexec/"scripts/test/simple_tests.pl" do |s|
+      s.gsub! "$bowtie = \"\"", "$bowtie = \"#{bin}/bowtie\""
+      s.gsub! "$bowtie_build = \"\"", "$bowtie_build = \"#{bin}/bowtie-build\""
+    end
   end
 
-  def test
-    system *%w[bowtie --version]
+  test do
+    cd libexec
+    system "sh", "./scripts/test/all.sh"
   end
 end
