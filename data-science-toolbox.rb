bab4c5f817f20fec5bfa8849f7ddf06bb9f29c63
Ian Lancaster <ilancaster@gmail.com>
2014-06-02 18:01:00 -0400

    data-science-toolbox 077c94f

--- data-science-toolbox.rb
+++ data-science-toolbox.rb
@@ -3,13 +3,13 @@ require 'formula'
 class DataScienceToolbox < Formula
   # See also http://jeroenjanssens.com/2013/09/19/seven-command-line-tools-for-data-science.html
   homepage 'https://github.com/jeroenjanssens/data-science-toolbox'
-  version '4bbe4cf'
+  version '077c94f'
   url "https://github.com/jeroenjanssens/data-science-toolbox/archive/#{version}.tar.gz"
-  sha1 '15710d8bc588d1a72d7ca6b10678b8fa57e7485c'
+  sha1 '35b4397f1d7356e833d923f446657219f58cb1c9'
   head 'https://github.com/jeroenjanssens/data-science-toolbox.git'
 
   def install
-    bin.install %w[Rio dumbplot explain pbc pca sample scrape]
+    bin.install Dir["tools/*"]
     doc.install 'README.md'
   end
 
