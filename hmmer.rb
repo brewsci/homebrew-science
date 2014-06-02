8cdd9eb621ab0ad24eaec778191f05fc92f95808
Ian Lancaster <ilancaster@gmail.com>
2014-06-02 00:46:32 -0400

    hmmer: add make check

--- hmmer.rb
+++ hmmer.rb
@@ -6,6 +6,8 @@ class Hmmer < Formula
   url 'http://selab.janelia.org/software/hmmer3/3.1b1/hmmer-3.1b1.tar.gz'
   sha1 'e05907d28b7f03d4817bb714ff0a8b2ef0210220'
 
+  option "without-check", "Skip build-time tests (not recommended)"
+
   head do
     url 'https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk'
     depends_on :autoconf
@@ -15,7 +17,10 @@ class Hmmer < Formula
     system "autoconf" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+
+    share.install "tutorial"
   end
 
   test do
