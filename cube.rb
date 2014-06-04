7578d815feda26d0a1aa1818f9d2d6ef80166eca
Shaun Jackman <sjackman@gmail.com>
2014-06-04 16:54:51 +0200

    cube: Add qt to recommended dependencies

--- cube.rb
+++ cube.rb
@@ -5,6 +5,8 @@ class Cube < Formula
   url "http://apps.fz-juelich.de/scalasca/releases/cube/4.2/dist/cube-4.2.2.tar.gz"
   sha1 "88597c2df082bdc4eeadd6a9dad791b5894c6daa"
 
+  depends_on "qt" => :recommended
+
   def install
     ENV.deparallelize
 
