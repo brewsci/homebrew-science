4f38512e21b1cf16c98e3814e628dc51509c68a3
Ian Lancaster <ilancaster@gmail.com>
2014-06-04 18:15:43 -0400

    vcftools: add option for brewed zlib
    Fixes #944.

--- vcftools.rb
+++ vcftools.rb
@@ -6,8 +6,17 @@ class Vcftools < Formula
   sha1 '66b982c67d2441f2b7fdbf2895f0749547852b13'
   version '0.1.12a-1' # detect new release, not an alpha version
 
+  depends_on "homebrew/dupes/zlib" => :optional
+
   def install
-    system "make", "install", "PREFIX=#{prefix}", "CPP=#{ENV.cxx}"
+    args = %W[PREFIX=#{prefix} CPP=#{ENV.cxx}]
+
+    if build.with? "zlib"
+      zlib = Formula["zlib"]
+      args << "LIB=-lz -L#{zlib.opt_lib} -I#{zlib.opt_include}"
+    end
+
+    system "make", "install", *args
   end
 
   def caveats; <<-EOS.undent
