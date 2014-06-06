43f00d8b3ede038d2aebe7859f68a0118fddfcd1
Tim D. Smith <git@tim-smith.us>
2014-06-06 15:15:15 -0700

    fcgene: new formula
    Closes #956.

---
+++ fcgene.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class Fcgene < Formula
+  homepage "http://sourceforge.net/projects/fcgene/"
+  url "https://downloads.sourceforge.net/project/fcgene/fcgene-1.0.7.tar.gz"
+  sha1 "cbc01d9dce8132972f159855d2982eb56af8921c"
+
+  def install
+    # upstream Makefile ignores $CXX
+    ENV['AC_CXX'] = ENV['CXX']
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.ped").write("1 1000000000 0 0 1 1 0 0 1 1\n1 1000000001 0 0 1 2 1 1 1 2\n")
+    (testpath/"test.map").write("1 rs0 0 1000\n1 rs10 0 1001\n")
+    system "#{bin}/fcgene", '--ped', 'test.ped', '--map', 'test.map'
+  end
+end
