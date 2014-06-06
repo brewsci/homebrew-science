ced68563e7107a2ec49f02e3c357b9af56551160
Ian Lancaster <ilancaster@gmail.com>
2014-06-05 23:24:46 -0400

    bioawk: tag stable release

--- bioawk.rb
+++ bioawk.rb
@@ -2,6 +2,9 @@ require 'formula'
 
 class Bioawk < Formula
   homepage 'https://github.com/lh3/bioawk'
+  version "5e8b41d"
+  url "https://github.com/lh3/bioawk/archive/#{version}.tar.gz"
+  sha1 "1042e98bfa6a8601488df1be29eb758b2359826d"
   head 'https://github.com/lh3/bioawk.git'
 
   def install
