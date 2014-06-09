9c3882ff99b1a9f632f1005390b7b99e4520a299
Dominique Orban <dominique.orban@gmail.com>
2014-06-09 23:25:54 +0200

    ASL: option to build Matlab interface.

--- asl.rb
+++ asl.rb
@@ -6,6 +6,14 @@ class Asl < Formula
   version '20140205'
   homepage 'http://www.ampl.com/hooking.html'
 
+  option 'with-matlab', 'Build MEX file for use with Matlab'
+  option 'with-mex-path=', 'Path to MEX executable, e.g., /Applications/Matlab/MATLAB_R2013b.app/bin/mex (default: mex)'
+
+  resource 'spamfunc' do
+    url 'http://netlib.org/ampl/solvers/examples/spamfunc.c'
+    sha1 '429a79fc54facc5ef99219fe460280a883c75dfa'
+  end
+
   def install
     ENV.universal_binary if OS.mac?
     cflags = %w[-I. -O -fPIC]
@@ -41,12 +49,24 @@ class Asl < Formula
     (include / 'asl').install Dir["*.h"]
     (include / 'asl').install Dir["*.hd"]
     doc.install 'README'
+
+    if build.with? "matlab"
+      mex = ARGV.value("with-mex-path") || "mex"
+      resource("spamfunc").stage do
+        system mex, "-f", File.join(File.dirname(mex), "mexopts.sh"),
+                    "-I#{include}/asl", "spamfunc.c", "-largeArrayDims",
+                    "-L#{lib}", "-lasl", "-lfuncadd0", "-outdir", bin
+      end
+    end
   end
 
-  def caveats; <<-EOS.undent
-    Include files are in #{include}/asl.
-    To link with the ASL, you may simply use
-    -L#{lib} -lasl -lfuncadd0
+  def caveats
+    s = <<-EOS.undent
+      Include files are in #{include}/asl.
+      To link with the ASL, you may simply use
+      -L#{lib} -lasl -lfuncadd0
     EOS
+    s += "\nAdd #{bin} to your MATLABPATH." if build.with? "matlab"
+    s
   end
 end
