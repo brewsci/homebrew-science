173d63e46be792d0852c67840995e9549e6ac414
Shaun Jackman <sjackman@gmail.com>
2014-06-12 09:57:10 -0700

    ropebwt2 r181
    Add a link to the PDF on arXiv.

--- ropebwt2.rb
+++ ropebwt2.rb
@@ -1,11 +1,13 @@
 require 'formula'
 
 class Ropebwt2 < Formula
-  homepage 'https://github.com/lh3/ropebwt2'
-  url 'https://github.com/lh3/ropebwt2/archive/d1c4de8.tar.gz'
-  version 'r132'
-  sha1 '753fde5c0025ea06c38c8b759158a6a86ea080f6'
-  head 'https://github.com/lh3/ropebwt2.git'
+  homepage "https://github.com/lh3/ropebwt2"
+  head "https://github.com/lh3/ropebwt2.git"
+  #pdf "http://arxiv.org/pdf/1406.0426v1.pdf"
+
+  url "https://github.com/lh3/ropebwt2/archive/49b280debe54db51d9ca81972a9db76c2f6290f7.tar.gz"
+  version "r181"
+  sha1 "0a0dba614a1e65a6296bbc23d1c72fb1718065f8"
 
   def install
     system 'make'
