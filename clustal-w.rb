368ba41d7af27e0d07b7bb8c6a593d943270bef8
Shaun Jackman <sjackman@gmail.com>
2014-06-20 16:11:16 -0700

    clustal-w: Fails to build with clang
    Closes #967

--- clustal-w.rb
+++ clustal-w.rb
@@ -5,6 +5,11 @@ class ClustalW < Formula
   url 'http://www.clustal.org/download/2.1/clustalw-2.1.tar.gz'
   sha1 'f29784f68585544baa77cbeca6392e533d4cf433'
 
+  fails_with :clang do
+    build 503
+    cause "error: implicit instantiation of undefined template"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
