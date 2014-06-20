361ff4022a2766e39904d41ea162b7dfb2ee333a
Niels <niels.lohmann@gmail.com>
2014-06-20 15:05:50 +0200

    LoLA: Updated to LoLA 2.0
    Closes #990.

--- lola.rb
+++ lola.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Lola < Formula
-  homepage 'http://www.informatik.uni-rostock.de/tpp/lola/'
-  url 'http://download.gna.org/service-tech/lola/lola-1.17.tar.gz'
-  sha1 '05e45e46f2fb2681ef369c2fd63f88115a912c55'
+  homepage 'http://service-technology.org/lola/'
+  url 'http://download.gna.org/service-tech/lola/lola-2.0.tar.gz'
+  sha1 '56d8ae73616e244b20dd8318eb027bb1da55c5bb'
+
+  option "with-check", "Run build-time tests"
 
   head do
-    url 'http://svn.gna.org/svn/service-tech/trunk/lola'
+    url 'http://svn.gna.org/svn/service-tech/trunk/lola2'
 
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
@@ -14,19 +16,18 @@ class Lola < Formula
     depends_on 'bison' => :build
     depends_on 'gengetopt' => :build
     depends_on 'help2man' => :build
+    depends_on 'kimwitu++' => :build
+    depends_on 'gnu-sed' => :build
   end
+
+  depends_on 'autoconf' if build.with? "check"
 
   def install
     ENV.deparallelize
 
     system "autoreconf -i" if build.head?
-    system "./configure", "--disable-assert",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make" if build.head?
-    system "make", "clean" if build.head?
-    system "make", "all-configs"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
+    system "make", "check" if build.with? "check"
   end
 end
