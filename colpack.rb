344cd0cedb831dcb465c31ed48259d579015e3ff
Dominique Orban <dominique.orban@gmail.com>
2014-06-19 20:16:28 -0400

    Colpack: option to build against libc++

--- colpack.rb
+++ colpack.rb
@@ -5,7 +5,10 @@ class Colpack < Formula
   url 'http://cscapes.cs.purdue.edu/download/ColPack/ColPack-1.0.9.tar.gz'
   sha1 'c963424c3e97a7bc3756d3feb742418e89721e48'
 
+  option 'with-libc++', 'build against libc++ instead of libstdc++' # for adol-c
+
   def install
+    ENV.libcxx if build.with? "libc++"
     system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
     system "make install"
   end
