e54675c5b3efd6b05a42869ccd50e04f67511d3e
Shaun Jackman <sjackman@gmail.com>
2014-06-20 16:13:37 -0700

    clustal-w: Add a test

--- clustal-w.rb
+++ clustal-w.rb
@@ -14,4 +14,8 @@ class ClustalW < Formula
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
+
+  test do
+    system "#{bin}/clustalw2 --version 2>&1 |grep CLUSTAL"
+  end
 end
