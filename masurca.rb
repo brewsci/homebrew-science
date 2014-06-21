d946a6ee6c44145d91c75cdb4c51fc0fc75b14a2
Shaun Jackman <sjackman@gmail.com>
2014-06-20 17:57:08 -0700

    masurca 2.2.1

--- masurca.rb
+++ masurca.rb
@@ -3,21 +3,25 @@ require 'formula'
 class Masurca < Formula
   homepage 'http://www.genome.umd.edu/masurca.html'
   #doi "10.1093/bioinformatics/btt476"
-  url 'ftp://ftp.genome.umd.edu/pub/MaSuRCA/MaSuRCA-2.0.4.tar.gz'
-  sha1 '7e36afd82bd581566d46fd4dedd7ffc6cd751fb2'
+  url "ftp://ftp.genome.umd.edu/pub/MaSuRCA/v2.2.1/MaSuRCA-2.2.1.tar.gz"
+  sha1 "a568d0afc9cf96e5351e8f4ef92c1b89a13011d6"
 
   keg_only 'MaSuRCA conflicts with jellyfish.'
 
-  depends_on "pkg-config"
+  depends_on "pkg-config" => :build
 
   def install
     raise "MaSuRCA fails to build on Mac OS. See https://github.com/Homebrew/homebrew-science/issues/344" if OS.mac?
-    ENV.j1
-    ENV['DEST'] = prefix
-    system './install.sh'
+
+    # Fix the error ./install.sh: line 46: masurca: No such file or directory
+    bin.install "SuperReads/src/masurca"
+    chmod 0755, bin/"masurca"
+
+    ENV.deparallelize
+    system "DEST=#{prefix} ./install.sh"
   end
 
   test do
-    system "#{bin}/runSRCA.pl"
+    system "#{bin}/masurca -h"
   end
 end
