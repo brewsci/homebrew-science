8db44f48e4d7dca62acf87ea59a9cf5a7cb65217
David Holm <dholmster@gmail.com>
2014-06-29 11:27:28 +0200

    hwloc 1.9
    Closes #1018.

--- hwloc.rb
+++ hwloc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hwloc < Formula
   homepage 'http://www.open-mpi.org/projects/hwloc/'
-  url 'http://www.open-mpi.org/software/hwloc/v1.8/downloads/hwloc-1.8.1.tar.gz'
-  sha1 '4ed7c5d873fd16974b756efa5727c5b2caaa0118'
+  url "http://www.open-mpi.org/software/hwloc/v1.9/downloads/hwloc-1.9.tar.bz2"
+  sha1 "99646446502e0f9952170bf1082be63361d99b6d"
 
   depends_on 'pkg-config' => :build
   depends_on 'cairo' => :optional
