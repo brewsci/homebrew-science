36c73b3ccfd453528738aa4d93850997a57390d4
Ian Lancaster <ilancaster@gmail.com>
2014-07-04 21:35:58 -0400

    bedtools 2.20.1

--- bedtools.rb
+++ bedtools.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bedtools < Formula
   homepage 'https://github.com/arq5x/bedtools2'
   #doi '10.1093/bioinformatics/btq033'
-  url 'https://github.com/arq5x/bedtools2/releases/download/v2.19.1/bedtools-2.19.1.tar.gz'
-  sha1 '7a9027d7992ae399cde8fb7dab520b332605740c'
+  url 'https://github.com/arq5x/bedtools2/releases/download/v2.20.1/bedtools-2.20.1.tar.gz'
+  sha1 '4b7b5866199b0eefd093f39fef260dcd369ae13a'
   head 'https://github.com/arq5x/bedtools2.git'
 
   def install
