0ca70a19edffe4f84960e19bff08e9ef091aef9b
John Cupitt <jcupitt@gmail.com>
2014-07-05 14:42:51 +0100

    Vips: update to 7.40.3
    update vips.rb to latest stable, list of changes here:
    
    http://libvips.blogspot.co.uk/2014/06/new-stable-version-740.html
    
    add the new libgsf-1 optional dep
    
    reorder the formula to get all recommended and all optional deps listed
    together
    
    Closes #1035.

--- vips.rb
+++ vips.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vips < Formula
   homepage 'http://www.vips.ecs.soton.ac.uk/'
-  url 'http://www.vips.ecs.soton.ac.uk/supported/7.38/vips-7.38.5.tar.gz'
-  sha1 'c55ab4c4a42d3bcd40c6c03e38ead13f71c97fc0'
+  url 'http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.3.tar.gz'
+  sha1 '45cc4ad9e26a844359d77520ba964e6bde35c802'
 
   option 'without-check', 'Disable build time checks (not recommended)'
 
@@ -11,17 +11,20 @@ class Vips < Formula
   depends_on :fontconfig
   depends_on 'gettext'
   depends_on 'glib'
+
   depends_on :libpng => :recommended
   depends_on 'jpeg' => :recommended
   depends_on 'orc' => :recommended
-  depends_on 'openslide' => :optional
+  depends_on 'libgsf' => :recommended
   depends_on 'libtiff' => :recommended
-  depends_on 'imagemagick' => :optional
-  depends_on 'graphicsmagick' => :optional
   depends_on 'fftw' => :recommended
   depends_on 'little-cms' => :recommended
   depends_on 'pango' => :recommended
   depends_on 'libexif' => :recommended
+
+  depends_on 'openslide' => :optional
+  depends_on 'imagemagick' => :optional
+  depends_on 'graphicsmagick' => :optional
   depends_on 'openexr' => :optional
   depends_on 'cfitsio' => :optional
   depends_on 'webp' => :optional
