8fa0f4497cb19c847479cd9d82ca02746ae88ce4
Ian Lancaster <ilancaster@gmail.com>
2014-07-04 21:24:52 -0400

    openblas 0.2.9
    Add --devel 0.2.10-rc2.
    
    Closes #1038.

--- openblas.rb
+++ openblas.rb
@@ -1,11 +1,16 @@
 require 'formula'
 
 class Openblas < Formula
-  homepage 'http://xianyi.github.io/OpenBLAS/'
-  url 'https://github.com/xianyi/OpenBLAS/archive/v0.2.9.rc2.tar.gz'
-  sha1 '60fbd30e94bb37e510fddd46bcfadfdb44cfff8c'
-  version '0.2.9-rc2'
+  homepage "http://www.openblas.net/"
+  url "https://github.com/xianyi/OpenBLAS/archive/v0.2.9.tar.gz"
+  sha1 "e8197711c4fe43ac286366693bd7c1683003c894"
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
+
+  devel do
+    url "https://github.com/xianyi/OpenBLAS/archive/v0.2.10.rc2.tar.gz"
+    sha1 "be0610c508e4105fa9ba31d8b0d421df9d634b41"
+    version "0.2.10-rc2"
+  end
 
   depends_on :fortran
 
