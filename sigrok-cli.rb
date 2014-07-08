8deea8a88bb0e55e1d081ca028798c1ae06e05c3
tduehr <tduehr@gmail.com>
2014-07-08 08:43:14 -0500

    new formula: sigrok-cli 0.5.0
    Closes #1045.

---
+++ sigrok-cli.rb
@@ -0,0 +1,37 @@
+require "formula"
+
+class SigrokDownloadStrategy < GitDownloadStrategy
+  def support_depth?
+    false
+  end
+end
+
+class SigrokCli < Formula
+  url "http://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.5.0.tar.gz"
+  homepage "http://sigrok.org/"
+  head "git://sigrok.org/sigrok-cli", :using => SigrokDownloadStrategy
+  sha1 "6fb5d6ff75f6492bca8d3da66ba446a6438438de"
+
+  if build.head?
+    depends_on "glib"
+    depends_on "libtool" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+
+  depends_on "libsigrokdecode"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    ENV.delete "PYTHONPATH"
+    system "#{bin}/sigrok-cli", "--version"
+  end
+end
