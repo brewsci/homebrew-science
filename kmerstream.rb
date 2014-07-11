b6280e32ff373c45036b01df38b2670450c674df
Shaun Jackman <sjackman@gmail.com>
2014-07-11 11:03:05 -0400

    kmerstream 1.0: New formula
    KmerStream: Streaming algorithms for k-mer abundance estimation

---
+++ kmerstream.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class Kmerstream < Formula
+  homepage "https://github.com/pmelsted/KmerStream"
+  #doi "10.1101/003962"
+  head "https://github.com/pmelsted/KmerStream.git"
+
+  url "https://github.com/pmelsted/KmerStream/archive/v1.0.tar.gz"
+  sha1 "97688502c15c2b7457054aaf8e8a8f07f4fd159b"
+
+  fails_with :clang do
+    build 503
+    cause "Requires OpenMP"
+  end
+
+  def install
+    system "make"
+    bin.install "KmerStream"
+  end
+
+  test do
+    system "#{bin}/KmerStream 2>&1 |grep KmerStream"
+  end
+end
