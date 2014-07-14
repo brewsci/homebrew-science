f44081a7f6c3c365ce8bba00a1797c33326d7a58
Ian Lancaster <ilancaster@gmail.com>
2014-07-13 20:26:58 -0400

    tbl2asn 23.6

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class Tbl2asn < Formula
   homepage "https://www.ncbi.nlm.nih.gov/genbank/tbl2asn2/"
-  version "23.0"
+  version "23.6"
   if OS.mac?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/mac.tbl2asn.gz"
-    sha1 "2a38b87caf876d5d1a28382078ccb80cece861e3"
+    sha1 "80b9bb33ffb9b070ef40107b399d380485044366"
   elsif OS.linux?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
-    sha1 "7508df69771376e0f4e1309848fb883fa9e33045"
+    sha1 "436e5143547b918514cc90c77b9b7b483eadf035"
   else
     raise "Unknown operating system"
   end
