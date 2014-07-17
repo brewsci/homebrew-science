daaa23ae12af4838968c727e75917b44e0e8f351
Ian Lancaster <ilancaster@gmail.com>
2014-07-16 20:14:59 -0400

    bwa 0.7.10

--- bwa.rb
+++ bwa.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Bwa < Formula
   homepage 'http://bio-bwa.sourceforge.net/'
   #doi '10.1093/bioinformatics/btp324'
-  url 'https://downloads.sf.net/project/bio-bwa/bwa-0.7.9a.tar.bz2'
-  sha1 'a77ce327e8acba554b1d27bfdd1c148f79726484'
+  url 'https://downloads.sf.net/project/bio-bwa/bwa-0.7.10.tar.bz2'
+  sha1 '4a8b692d5835993fdb8dce350570951076daac4f'
 
   head 'https://github.com/lh3/bwa.git'
 
