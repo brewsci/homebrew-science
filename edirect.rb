c8a82ed16c79083f8ad60da4bd9886ab94c3e944
Ian Lancaster <ilancaster@gmail.com>
2014-07-17 00:45:51 -0400

    edirect 1.80

--- edirect.rb
+++ edirect.rb
@@ -3,8 +3,8 @@ require "formula"
 class Edirect < Formula
   homepage "http://www.ncbi.nlm.nih.gov/books/NBK179288/"
   url "ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.tar.gz"
-  sha1 "8b9ae110c93f4a2eddc84bd510c1dcbf371388f3"
-  version "1.70"
+  sha1 "e79de85075be9e9eab11dca49001863ccdec2cd3"
+  version "1.80"
 
   def install
     doc.install "README"
