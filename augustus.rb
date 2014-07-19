c99df29c3c680c53450815cd7a76474af6d119b9
Ian Lancaster <ilancaster@gmail.com>
2014-07-19 00:35:24 -0400

    augustus: add mirror

--- augustus.rb
+++ augustus.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Augustus < Formula
   homepage 'http://bioinf.uni-greifswald.de/augustus/'
   url 'http://bioinf.uni-greifswald.de/augustus/binaries/augustus.3.0.1.tar.gz'
+  mirror 'https://science-annex.org/pub/augustus/augustus.3.0.1.tar.gz'
   sha1 '19f40b3b834aba0386646d9cb8120caf3f0eb64e'
 
   depends_on 'boost' => :recommended # for gz support
