f4691f71f4e71ec32689b61cde7dc6455d85bdb3
Ian Lancaster <ilancaster@gmail.com>
2014-07-19 00:35:42 -0400

    rml-mmc 274

--- rml-mmc.rb
+++ rml-mmc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RmlMmc < Formula
   homepage 'http://www.ida.liu.se/labs/pelab/rml'
-  url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_255.orig.tar.gz'
-  version '2.5.5'
-  sha1 'ab173438a56f344904f6d02ccc8d6a360896219d'
+  url "http://build.openmodelica.org/apt/pool/contrib/rml-mmc_274.orig.tar.gz"
+  version "274"
+  sha1 "d6fa3ad52dbf0f4606acd727bf0ed58d6c3cc88f"
 
   # Attention, has a self-signed certificate and svn will prompt you, so
   # do not use --HEAD as a dependency or automatic installation (built-bot).
