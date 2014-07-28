bb9071a8bc3a4e09dafd6af358855daf1349d543
chapmanb <chapmanb@50mail.com>
2014-07-28 11:26:29 -0400

    lumpy-sv: v0.2.4 and de-parallelize
    Update to latest lumpy version (with Perl scripts for speedseq
    compatibility) and de-parallelize make file to try and avoid issues
    building on CentOS5 with gcc-4.1
    
    Closes #1096.

--- lumpy-sv.rb
+++ lumpy-sv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class LumpySv < Formula
   homepage 'https://github.com/arq5x/lumpy-sv'
-  url 'https://github.com/arq5x/lumpy-sv/releases/download/0.2.2.2/lumpy-sv-0.2.2.2.tar.gz'
-  sha1 '87c8c686a0f554c3b34cf69597a05b1d31f9c6ee'
+  url 'https://github.com/arq5x/lumpy-sv/releases/download/0.2.4/lumpy-sv-0.2.4.tar.gz'
+  sha1 '5c951aac124b5dafa27de2de9ef1f3a64bef95cc'
 
   depends_on 'bamtools' => :recommended
   depends_on 'samtools' => :recommended
@@ -12,8 +12,10 @@ class LumpySv < Formula
   depends_on 'novoalign' => :optional
   depends_on 'yaha' => :optional
   depends_on 'Statistics::Descriptive' => [:perl, :optional]
+  skip_clean :all
 
   def install
+    ENV.deparallelize
     system 'make'
     bin.install 'bin/lumpy'
     (share/'lumpy-sv').install Dir['scripts/*']
