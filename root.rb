537ff6cade6c180ba1ea6fcb808115cfe3d23a06
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2014-07-30 15:13:30 -0400

    root: upgrade to 5.34.19, workaround ROOT-5773
    https://sft.its.cern.ch/jira/browse/ROOT-5773
    
    Fixes #1094.
    Closes #1098.

--- root.rb
+++ root.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Root < Formula
   homepage 'http://root.cern.ch'
-  url 'ftp://root.cern.ch/root/root_v5.34.18.source.tar.gz'
-  mirror 'https://fossies.org/linux/misc/root_v5.34.18.source.tar.gz'
-  version '5.34.18'
-  sha1 'e24e9bf8b142f2780f6cec9503409d87e4b9f8da'
+  url 'ftp://root.cern.ch/root/root_v5.34.19.source.tar.gz'
+  mirror 'http://ftp.riken.jp/pub/ROOT/root_v5.34.19.source.tar.gz'
+  version '5.34.19'
+  sha1 '77d3c7c6e2a7fd797382f5992d0181eb55566cb4'
   head 'https://github.com/root-mirror/root.git', :branch => 'v5-34-00-patches'
 
   option 'with-qt', "Build with Qt graphics backend and GSI's Qt integration"
@@ -14,18 +14,6 @@ class Root < Formula
   depends_on 'qt' => [:optional, 'with-qt3support']
   depends_on :x11 => :optional
   depends_on :python
-
-  if build.with? "x11"
-    patch :p1, :DATA
-  else
-    unless build.head?
-      patch :p1 do
-        # https://sft.its.cern.ch/jira/browse/ROOT-6297
-        url "https://gist.githubusercontent.com/veprbl/9ab33daa07b68c28671c/raw/31317dfa11eba19595207dc32851a1bb2d836b0a/gistfile1.txt"
-        sha1 "6d8625fd63fce92976e27248e4ad3698741e7eba"
-      end
-    end
-  end
 
   def install
     # brew audit doesn't like non-executables in bin
@@ -49,6 +37,7 @@ class Root < Formula
       #{arch}
       --all
       --enable-builtin-glew
+      --enable-builtin-freetype
     ]
 
     if build.with? 'x11'
@@ -106,19 +95,3 @@ class Root < Formula
     EOS
   end
 end
-
-__END__
-# Consider removing this once
-# /opt/X11/bin/freetype-config --ftversion
-# is reporting version >=2.5.1
---- a/graf2d/freetype/Module.mk
-+++ b/graf2d/freetype/Module.mk
-@@ -8,7 +8,7 @@
- ifneq ($(BUILTINFREETYPE),yes)
- 
- FREETYPELIBF    := $(shell freetype-config --libs)
--FREETYPEINC     := $(shell freetype-config --cflags)
-+FREETYPEINC     := $(subst -I,-isystem,$(shell freetype-config --cflags)) -Wp,-v
- FREETYPELIB     := $(filter -l%,$(FREETYPELIBF))
- FREETYPELDFLAGS := $(filter-out -l%,$(FREETYPELIBF))
- FREETYPEDEP     :=
