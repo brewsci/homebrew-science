7cbdd7ee62017317d35c48c595e9c91169a0deca
Shaun Jackman <sjackman@gmail.com>
2014-07-30 18:10:28 -0700

    uniqtag 1.0: New formula
    Abbreviate strings to short unique identifiers.
    UniqTag: Assign unique, reasonably stable, content-derived identifiers to genes
    Shaun D. Jackman, Joerg Bohlmann, Inanc Birol

---
+++ uniqtag.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Uniqtag < Formula
+  homepage "https://github.com/sjackman/uniqtag"
+  head "https://github.com/sjackman/uniqtag.git"
+  url "https://github.com/sjackman/uniqtag/archive/1.0.tar.gz"
+  sha1 "f507a3f61bc6abb6c992bae55b827b42d45be3bd"
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+    doc.install "README.md"
+  end
+
+  test do
+    system "#{bin}/uniqtag --version"
+  end
+end
