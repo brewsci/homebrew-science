99979693c04720533931f76ea19ec7c6d706e265
Ian Lancaster <ilancaster@gmail.com>
2014-08-01 03:27:28 -0400

    blat: style cleanup

--- blat.rb
+++ blat.rb
@@ -1,29 +1,27 @@
-require 'formula'
+require "formula"
 
 class Blat < Formula
-  homepage 'http://genome.ucsc.edu/FAQ/FAQblat.html'
-  url 'http://users.soe.ucsc.edu/~kent/src/blatSrc35.zip'
-  sha1 'a2cae7407e512166bf7b1ed300db9be6649693bd'
+  homepage "http://genome.ucsc.edu/FAQ/FAQblat.html"
+  url "http://users.soe.ucsc.edu/~kent/src/blatSrc35.zip"
+  sha1 "a2cae7407e512166bf7b1ed300db9be6649693bd"
 
-  depends_on 'libpng' => :build
+  depends_on "libpng" => :build
 
   def install
     bin.mkpath
     system "make", "MACHTYPE=darwin", "BINDIR=#{bin}"
   end
 
-  def test
-    mktemp do
-      Pathname.new('db.fa').write <<-EOF.undent
-        >gi|5524211|gb|AAD44166.1| cytochrome b [Elephas maximus maximus]
-        LCLYTHIGRNIYYGSYLYSETWNTGIMLLLITMATAFMGYVLPWGQMSFWGATVITNLFSAIPYIGTNLV
-      EOF
-      Pathname.new('query.fa').write <<-EOF.undent
-        >spam
-        CLYTHIGRNIYYGSY
-      EOF
-      system "#{bin}/blat", "-prot", "db.fa", "query.fa", "out.fa"
-      system "cat out.fa | grep spam"
-    end
+  test do
+    (testpath/"db.fa").write <<-EOF.undent
+      >gi|5524211|gb|AAD44166.1| cytochrome b [Elephas maximus maximus]
+      LCLYTHIGRNIYYGSYLYSETWNTGIMLLLITMATAFMGYVLPWGQMSFWGATVITNLFSAIPYIGTNLV
+    EOF
+    (testpath/"query.fa").write <<-EOF.undent
+      >spam
+      CLYTHIGRNIYYGSY
+    EOF
+    system "#{bin}/blat", "-prot", "db.fa", "query.fa", "out.fa"
+    system "cat out.fa | grep spam"
   end
 end
