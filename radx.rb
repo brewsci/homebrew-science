ce44a5489d640a245dd46274b36f0614aadb79ff
Ian Lancaster <ilancaster@gmail.com>
2014-08-01 03:22:21 -0400

    radx: fix url, audit warnings

--- radx.rb
+++ radx.rb
@@ -2,22 +2,22 @@ require "formula"
 
 class Radx < Formula
   homepage "http://www.ral.ucar.edu/projects/titan/docs/radial_formats/radx.html"
-  url "ftp://ftp.rap.ucar.edu/pub/titan/radx/radx-20140417.src.tgz"
+  url "ftp://ftp.rap.ucar.edu/pub/titan/radx/previous_releases/radx-20140417.src.tgz"
   mirror "http://science-annex.org/pub/radx/radx-20140417.src.tgz"
   version "20140417"
   sha1 "2959154e6c8aea4502dbb9fe98723c54fcd1bf39"
 
-  depends_on 'hdf5' => 'enable-cxx'
-  depends_on 'udunits'
-  depends_on 'netcdf' => 'enable-cxx-compat'
-  depends_on 'fftw'
+  depends_on "hdf5" => "enable-cxx"
+  depends_on "udunits"
+  depends_on "netcdf" => "enable-cxx-compat"
+  depends_on "fftw"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
-  def test
+  test do
     system "#{bin}/RadxPrint", "-h"
   end
 end
