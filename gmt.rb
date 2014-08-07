46f2fd53b4680d65d1795e771a3cd0532aa5ea2f
Ian Lancaster <ilancaster@gmail.com>
2014-08-06 21:27:23 -0700

    gmt: cleanup, use opt_prefix

--- gmt.rb
+++ gmt.rb
@@ -1,52 +1,53 @@
-require 'formula'
+require "formula"
 
 class Gmt < Formula
-  homepage 'http://gmt.soest.hawaii.edu/'
-  url 'ftp://ftp.soest.hawaii.edu/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://ftp.iris.washington.edu/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://ftp.iag.usp.br/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://gmt.mirror.ac.za/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://ftp.geologi.uio.no/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  mirror 'ftp://gd.tuwien.ac.at/pub/gmt/gmt-5.1.1-src.tar.bz2'
-  sha1 'ff64936dfdec8a57a89d29f505f27e435169d33f'
+  homepage "http://gmt.soest.hawaii.edu/"
+  url "ftp://ftp.soest.hawaii.edu/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://ftp.iris.washington.edu/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://ftp.iag.usp.br/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://gmt.mirror.ac.za/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://ftp.geologi.uio.no/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  mirror "ftp://gd.tuwien.ac.at/pub/gmt/gmt-5.1.1-src.tar.bz2"
+  sha1 "ff64936dfdec8a57a89d29f505f27e435169d33f"
 
-  depends_on 'cmake' => :build
-  depends_on 'gdal'
-  depends_on 'netcdf'
-  depends_on 'fftw'
-  depends_on 'pcre'
+  depends_on "cmake" => :build
+  depends_on "gdal"
+  depends_on "netcdf"
+  depends_on "fftw"
+  depends_on "pcre"
 
-  conflicts_with 'gmt4', :because => 'Both versions install the same binaries.'
+  conflicts_with "gmt4", :because => "both versions install the same binaries"
 
-  resource 'gshhg' do
-    url 'ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://ftp.iris.washington.edu/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://ftp.iag.usp.br/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://gmt.mirror.ac.za/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://ftp.geologi.uio.no/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    mirror 'ftp://gd.tuwien.ac.at/pub/gmt/gshhg-gmt-2.3.1.tar.gz'
-    sha1 'fe9e4e1c415faf09d51666e65c5b9d4b492c8a15'
+  resource "gshhg" do
+    url "ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.iris.washington.edu/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.iag.usp.br/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://gmt.mirror.ac.za/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.geologi.uio.no/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://gd.tuwien.ac.at/pub/gmt/gshhg-gmt-2.3.1.tar.gz"
+    sha1 "fe9e4e1c415faf09d51666e65c5b9d4b492c8a15"
   end
 
-  resource 'dcw' do
-    url 'ftp://ftp.soest.hawaii.edu/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://ftp.iris.washington.edu/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://ftp.iag.usp.br/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://gmt.mirror.ac.za/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://ftp.geologi.uio.no/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    mirror 'ftp://gd.tuwien.ac.at/pub/gmt/dcw-gmt-1.1.1.tar.gz'
-    sha1 'deca85f21426604c8574a18d16c931a1fd9ae27b'
+  resource "dcw" do
+    url "ftp://ftp.soest.hawaii.edu/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://ftp.iris.washington.edu/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://ftp.iag.usp.br/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://ftp.scc.u-tokai.ac.jp/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://gmt.mirror.ac.za/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://ftp.geologi.uio.no/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    mirror "ftp://gd.tuwien.ac.at/pub/gmt/dcw-gmt-1.1.1.tar.gz"
+    sha1 "deca85f21426604c8574a18d16c931a1fd9ae27b"
   end
 
   def install
-    gshhgdir = buildpath/'gshhg'
-    dcwdir = buildpath/'dcw'
+    gshhgdir = buildpath/"gshhg"
+    dcwdir = buildpath/"dcw"
+
     args = std_cmake_args.concat %W[
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DGMT_INSTALL_TRADITIONAL_FOLDERNAMES:BOOL=FALSE
@@ -57,19 +58,19 @@ class Gmt < Formula
       -DCOPY_GSHHG:BOOL=TRUE
       -DDCW_ROOT=#{dcwdir}
       -DCOPY_DCW:BOOL=TRUE
-      -DNETCDF_ROOT=#{HOMEBREW_PREFIX}
-      -DGDAL_ROOT=#{HOMEBREW_PREFIX}
-      -DPCRE_ROOT=#{HOMEBREW_PREFIX}
-      -DFFTW3_ROOT=#{HOMEBREW_PREFIX}
+      -DNETCDF_ROOT=#{Formula["netcdf"].opt_prefix}
+      -DGDAL_ROOT=#{Formula["gdal"].opt_prefix}
+      -DPCRE_ROOT=#{Formula["pcre"].opt_prefix}
+      -DFFTW3_ROOT=#{Formula["fftw"].opt_prefix}
       -DLICENSE_RESTRICTED:BOOL=FALSE
       -DFLOCK:BOOL=TRUE
     ]
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      gshhgdir.install resource('gshhg')
-      dcwdir.install resource('dcw')
-      system 'cmake', '..', *args
-      system 'make install'
+
+    mkdir "build" do
+      gshhgdir.install resource("gshhg")
+      dcwdir.install resource("dcw")
+      system "cmake", "..", *args
+      system "make", "install"
     end
   end
 
