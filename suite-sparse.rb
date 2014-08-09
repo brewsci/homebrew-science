9dbf8ebe42e10e0807e560038a285994a33fef2b
Dominique Orban <dominique.orban@gmail.com>
2014-08-09 13:38:03 -0400

    SuiteSparse: install Matlab interfaces.

--- suite-sparse.rb
+++ suite-sparse.rb
@@ -6,9 +6,15 @@ class SuiteSparse < Formula
   mirror 'http://pkgs.fedoraproject.org/repo/pkgs/suitesparse/SuiteSparse-4.2.1.tar.gz/4628df9eeae10ae5f0c486f1ac982fce/SuiteSparse-4.2.1.tar.gz'
   sha1 '2fec3bf93314bd14cbb7470c0a2c294988096ed6'
 
+  option "with-matlab", "Install Matlab interfaces and tools"
+  option "with-matlab-path=", "Path to Matlab executable (default: matlab)"
+
   depends_on "tbb" => :recommended
   depends_on "openblas" => :optional
   depends_on "metis4" => :optional # metis 5.x is not yet supported by suite-sparse
+
+  # Mathworks only support gcc/gfortran 4.3 on OSX.
+  depends_on "homebrew/versions/gcc43" => [:build, "enable-fortran"] if build.with? "matlab"
 
   def install
     # SuiteSparse doesn't like to build in parallel
@@ -28,5 +34,31 @@ class SuiteSparse < Formula
     lib.mkpath
     include.mkpath
     system "make", "install", *make_args
+
+    matlab = ARGV.value("with-matlab-path") || "matlab"
+    if build.with? "matlab"
+      system matlab, "-nodesktop", "-nosplash", "-r", "run('SuiteSparse_install(false)'); exit;"
+
+      # Install Matlab scripts and Mex files.
+      %w[AMD BTF CAMD CCOLAMD CHOLMOD COLAMD CSparse CXSparse KLU LDL SPQR UMFPACK].each do |m|
+        (share / "suite-sparse/matlab/#{m}").install Dir["#{m}/MATLAB/*"]
+      end
+
+      mdest = share / "suite-sparse/matlab"
+      mdest.install "MATLAB_Tools"
+      mdest.install "RBio/RBio"
+    end
+  end
+
+  def caveats
+    s = ""
+    if build.with? "matlab"
+      s += <<-EOS.undent
+        Matlab interfaces and tools have been installed to
+
+          #{share}/suite-sparse/matlab
+      EOS
+    end
+    return s
   end
 end
