198723c5c1fa4cf79bf2e343be93aef6fdeb30d6
Ian Lancaster <ilancaster@gmail.com>
2014-08-09 20:57:59 -0400

    gmt4: add mirror, opt_prefix

--- gmt4.rb
+++ gmt4.rb
@@ -1,18 +1,20 @@
-require 'formula'
+require "formula"
 
 class Gmt4 < Formula
-  homepage 'http://gmt.soest.hawaii.edu/'
-  url 'ftp://ftp.soest.hawaii.edu/gmt/gmt-4.5.12-src.tar.bz2'
-  sha1 'a5ce7419a1aeb523540d2f93ca62153e47ce72fc'
+  homepage "http://gmt.soest.hawaii.edu/"
+  url "ftp://ftp.soest.hawaii.edu/gmt/gmt-4.5.12-src.tar.bz2"
+  mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gmt-4.5.12-src.tar.bz2"
+  sha1 "a5ce7419a1aeb523540d2f93ca62153e47ce72fc"
 
-  depends_on 'gdal'
-  depends_on 'netcdf'
+  depends_on "gdal"
+  depends_on "netcdf"
 
-  conflicts_with 'gmt', :because => 'Both versions install the same binaries.'
+  conflicts_with "gmt", :because => "both versions install the same binaries."
 
-  resource 'gshhg' do
-    url 'ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.1.tar.gz'
-    sha1 'fe9e4e1c415faf09d51666e65c5b9d4b492c8a15'
+  resource "gshhg" do
+    url "ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-2.3.1.tar.gz"
+    mirror "ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gshhg-gmt-2.3.1.tar.gz"
+    sha1 "fe9e4e1c415faf09d51666e65c5b9d4b492c8a15"
   end
 
   def install
@@ -20,8 +22,8 @@ class Gmt4 < Formula
     datadir = share/name
     system "./configure", "--prefix=#{prefix}",
                           "--datadir=#{datadir}",
-                          "--enable-gdal=#{HOMEBREW_PREFIX}",
-                          "--enable-netcdf=#{HOMEBREW_PREFIX}",
+                          "--enable-gdal=#{Formula["gdal"].opt_prefix}",
+                          "--enable-netcdf=#{Formula["netcdf"].opt_prefix}",
                           "--enable-shared",
                           "--enable-triangle",
                           "--disable-xgrid",
@@ -31,6 +33,6 @@ class Gmt4 < Formula
     system "make install-data"
     system "make install-suppl"
     system "make install-man"
-    datadir.install resource('gshhg')
+    datadir.install resource("gshhg")
   end
 end
