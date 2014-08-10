307037f58a945437822da3f4e8f84826ffbdf599
Ian Lancaster <ilancaster@gmail.com>
2014-08-09 22:27:06 -0400

    emboss: add mirror, option to run embossupdate

--- emboss.rb
+++ emboss.rb
@@ -1,19 +1,22 @@
-require 'formula'
+require "formula"
 
 class Emboss < Formula
-  homepage 'http://emboss.sourceforge.net/'
-  url 'ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz'
-  mirror 'https://science-annex.org/pub/emboss/EMBOSS-6.6.0.tar.gz'
-  sha1 '93749ebd0a777efd3749974d2401c3a2a013a3fe'
+  homepage "http://emboss.sourceforge.net/"
+  url "ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz"
+  mirror "ftp://ftp.ebi.ac.uk/pub/software/unix/EMBOSS/EMBOSS-6.6.0.tar.gz"
+  mirror "https://science-annex.org/pub/emboss/EMBOSS-6.6.0.tar.gz"
+  sha1 "93749ebd0a777efd3749974d2401c3a2a013a3fe"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libharu'    => :optional
-  depends_on 'gd'         => :optional
+  option "with-embossupdate", "Run embossupdate after `make install`"
+
+  depends_on "pkg-config" => :build
+  depends_on "libharu"    => :optional
+  depends_on "gd"         => :optional
   depends_on :libpng      => :recommended
   depends_on :x11         => :recommended
 
   def install
-    inreplace "Makefile.in", "$(bindir)/embossupdate", ""
+    inreplace "Makefile.in", "$(bindir)/embossupdate", "" if build.without? "embossupdate"
 
     args = %W[
       --disable-debug
@@ -23,6 +26,7 @@ class Emboss < Formula
       --with-thread
     ]
     args << "--without-x" if build.without? "x11"
+
     system "./configure", *args
     system "make", "install"
   end
