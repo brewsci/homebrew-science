e3e9313e15d5fd77ebe4991aeb103abc3e205d04
Pepijn Kokke <pepijn.kokke@gmail.com>
2014-08-11 14:43:34 +0200

    vislcg3: 0.9.8.10063 (new formula)
    Closes #1120.

---
+++ vislcg3.rb
@@ -0,0 +1,36 @@
+require "formula"
+require "open3"
+
+class Vislcg3 < Formula
+  homepage "http://beta.visl.sdu.dk/cg3.html"
+  url "http://beta.visl.sdu.dk/download/vislcg3/vislcg3-0.9.8.10063.tar.bz2"
+  sha1 "98943be3d85824be9c256b501f8c58cd937c51ee"
+  head "http://beta.visl.sdu.dk/svn/visl/tools/vislcg3/trunk", :using => :svn
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "icu4c"
+
+  option "without-check", "Disable build-time checking (not recommended)"
+
+  def install
+    ENV["LDFLAGS"]  = "-L/usr/local/opt/icu4c/lib"
+    ENV["CPPFLAGS"] = "-I/usr/local/opt/icu4c/include"
+    system "cmake", ".", *std_cmake_args
+    system "make"
+
+    if build.with? "check"
+      Open3.popen3('./test/runall.pl', {:err => [:child, :out]}) do |input,output|
+        output.read.each_line do |line|
+          fail line if line.start_with?("T_") and not line.end_with?("Success Success.\n")
+        end
+      end
+    end
+
+    system "make", "install"
+  end
+
+  test do
+    system "vislcg3", "--help"
+  end
+end
