b9b4760187120c7af974582ea3479225526a624f
Shaun Jackman <sjackman@gmail.com>
2014-08-12 15:24:12 -0700

    musket 1.1: New formula
    Musket is an efficient multistage k-mer based corrector for Illumina short read
    data.

---
+++ musket.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Musket < Formula
+  homepage "http://musket.sourceforge.net/"
+  #doi "10.1093/bioinformatics/bts690"
+
+  url "https://downloads.sourceforge.net/project/musket/musket-1.1.tar.bz"
+  sha1 "48b0c6eba3c00be0dd4eab9aef7ed199e3e97857"
+
+  def install
+    system "make"
+    bin.install "musket"
+    doc.install "LICENSE", "README"
+  end
+
+  test do
+    system "#{bin}/musket 2>&1 |grep musket"
+  end
+end
