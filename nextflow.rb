32a408d783aa4a06a6e6c7aad09a6b7434019842
Shaun Jackman <sjackman@gmail.com>
2014-08-12 11:56:22 -0700

    nextflow 0.9.0

--- nextflow.rb
+++ nextflow.rb
@@ -3,12 +3,12 @@ require "formula"
 class Nextflow < Formula
   homepage "http://www.nextflow.io/"
   head "https://github.com/nextflow-io/nextflow.git"
-  
+
   depends_on :java => "1.7"
-  
-  version "0.8.5"
-  url "http://www.nextflow.io/releases/v0.8.5/nextflow"
-  sha1 "e7d1746041782af402623a1ed0f20a747f524da7"
+
+  version "0.9.0"
+  url "http://www.nextflow.io/releases/v0.9.0/nextflow"
+  sha1 "b4fde5c4fa81a080f9302c90924b8a0292252873"
 
   def install
     chmod 0755, "nextflow"
@@ -17,6 +17,6 @@ class Nextflow < Formula
   end
 
   test do
-    system "echo \"println 'hello'\" | #{bin}/nextflow -q | grep -q hello"
+    system "echo \"println 'hello'\" |#{bin}/nextflow -q run - |grep hello"
   end
 end
