bddee2895fb42c83838bab37ffb7bae0442d07cb
roryk <rory.kirchner@gmail.com>
2014-08-12 18:04:53 -0400

    sickle 1.33
    Closes #1125

--- sickle.rb
+++ sickle.rb
@@ -2,17 +2,9 @@ require 'formula'
 
 class Sickle < Formula
   homepage 'https://github.com/najoshi/sickle'
-  url 'https://github.com/najoshi/sickle/archive/v1.2.tar.gz'
-  sha1 'ea69b11f45336a5232f940af94c7ad2d18e9aebc'
+  url 'https://github.com/najoshi/sickle/archive/v1.33.tar.gz'
+  sha1 '593274fb7e12a52c9086dff69623aedca1799a5c'
   head 'https://github.com/najoshi/sickle.git'
-
-  # Fix a linker error
-  unless build.head?
-    patch do
-      url "https://github.com/sjackman/sickle/commit/895207a.diff"
-      sha1 "dba25e09e700770767629d659e4f12f743bb5ca1"
-    end
-  end
 
   def install
     system 'make'
