0ea842a640aa2f06ed1cf25a5091ef5aec15344c
Shaun Jackman <sjackman@gmail.com>
2014-08-14 09:04:02 -0700

    aragorn: Add DOI

--- aragorn.rb
+++ aragorn.rb
@@ -2,6 +2,8 @@ require "formula"
 
 class Aragorn < Formula
   homepage "http://mbio-serv2.mbioekol.lu.se/ARAGORN/"
+  #doi "10.1093/nar/gkh152"
+
   url "http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/aragorn1.2.36.tgz"
   sha1 "3fd5dafa5fa0a65c303d866fc4506bfbc38efbcb"
 
