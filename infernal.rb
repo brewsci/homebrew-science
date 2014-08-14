7eba9a83dc6e3cfd4fc8c81522e6bc0d2f6a8fee
Shaun Jackman <sjackman@gmail.com>
2014-08-14 09:04:11 -0700

    infernal: Add DOI

--- infernal.rb
+++ infernal.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class Infernal < Formula
   homepage 'http://infernal.janelia.org/'
+  #doi "10.1093/bioinformatics/btp157"
+
   url 'http://selab.janelia.org/software/infernal/infernal-1.1.tar.gz'
   sha1 'b3bd2659fdda1c1af35c3b3e40f61fcdac23c3a3'
 
