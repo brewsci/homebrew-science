8d2e7572b94dfa72247b252e05e1dfd866d91987
Ian Lancaster <ilancaster@gmail.com>
2014-08-21 18:58:48 -0400

    abyss-explorer: add doi, tag

--- abyss-explorer.rb
+++ abyss-explorer.rb
@@ -1,13 +1,16 @@
-require 'formula'
+require "formula"
 
 class AbyssExplorer < Formula
-  homepage 'http://www.bcgsc.ca/platform/bioinfo/software/abyss-explorer'
-  url 'http://www.bcgsc.ca/downloads/abyss-explorer/abyss-explorer-1.3.4/abyss-explorer-1.3.4.zip'
-  sha1 'f9b41c5c9d458cf46399571de9c6070b04a2e8b5'
+  homepage "http://www.bcgsc.ca/platform/bioinfo/software/abyss-explorer"
+  #doi "10.1109/TVCG.2009.116"
+  #tag "bioinformatics"
+
+  url "http://www.bcgsc.ca/downloads/abyss-explorer/abyss-explorer-1.3.4/abyss-explorer-1.3.4.zip"
+  sha1 "f9b41c5c9d458cf46399571de9c6070b04a2e8b5"
 
   def install
-    libexec.install 'AbyssExplorer.jar', 'lib'
-    (bin / 'abyss-explorer').write <<-EOS.undent
+    libexec.install "AbyssExplorer.jar", "lib"
+    (bin / "abyss-explorer").write <<-EOS.undent
       #!/bin/sh
       set -eu
       exec java -jar #{libexec}/AbyssExplorer.jar "$@"
