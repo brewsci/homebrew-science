8dc4ccd8716a7f184971e894050ab5efc48a296f
Ian Lancaster <ilancaster@gmail.com>
2014-08-21 18:58:30 -0400

    vips: style cleanup

--- vips.rb
+++ vips.rb
@@ -1,41 +1,43 @@
-require 'formula'
+require "formula"
 
 class Vips < Formula
-  homepage 'http://www.vips.ecs.soton.ac.uk/'
-  url 'http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.6.tar.gz'
-  sha1 '7a04f32ab4c3e69477044f6cf7a1d7a47b92dc56'
+  homepage "http://www.vips.ecs.soton.ac.uk/"
+  url "http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.6.tar.gz"
+  sha1 "7a04f32ab4c3e69477044f6cf7a1d7a47b92dc56"
 
-  option 'without-check', 'Disable build time checks (not recommended)'
+  option "without-check", "Disable build time checks (not recommended)"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :fontconfig
-  depends_on 'gettext'
-  depends_on 'glib'
+  depends_on "gettext"
+  depends_on "glib"
 
   depends_on :libpng => :recommended
-  depends_on 'jpeg' => :recommended
-  depends_on 'orc' => :recommended
-  depends_on 'libgsf' => :recommended
-  depends_on 'libtiff' => :recommended
-  depends_on 'fftw' => :recommended
-  depends_on 'little-cms' => :recommended
-  depends_on 'pango' => :recommended
-  depends_on 'libexif' => :recommended
+  depends_on "jpeg" => :recommended
+  depends_on "orc" => :recommended
+  depends_on "libgsf" => :recommended
+  depends_on "libtiff" => :recommended
+  depends_on "fftw" => :recommended
+  depends_on "little-cms" => :recommended
+  depends_on "pango" => :recommended
+  depends_on "libexif" => :recommended
 
-  depends_on 'openslide' => :optional
-  depends_on 'imagemagick' => :optional
-  depends_on 'graphicsmagick' => :optional
-  depends_on 'openexr' => :optional
-  depends_on 'cfitsio' => :optional
-  depends_on 'webp' => :optional
+  depends_on "openslide" => :optional
+  depends_on "imagemagick" => :optional
+  depends_on "graphicsmagick" => :optional
+  depends_on "openexr" => :optional
+  depends_on "cfitsio" => :optional
+  depends_on "webp" => :optional
 
   def install
-    args = [ "--disable-dependency-tracking",
-             "--prefix=#{prefix}"]
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+    args.concat %w[--with-magick --with-magickpackage=GraphicsMagick] if build.with? "graphicsmagick"
 
-    args.concat ['--with-magick', '--with-magickpackage=GraphicsMagick'] if build.with? 'graphicsmagick'
     system "./configure", *args
-    system 'make', 'check' if build.with? 'check'
-    system 'make', 'install'
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 end
