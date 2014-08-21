57a4cd5d0bdbbad7e8799d5c2edaa288ad262bd5
Shaun Jackman <sjackman@gmail.com>
2014-08-21 11:25:51 -0700

    cerulean 0.1: New formula
    Cerulean extends contigs assembled using short read datasets like Illumina
    paired-end reads using long reads like PacBio RS long reads.

---
+++ cerulean.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Cerulean < Formula
+  homepage "https://sourceforge.net/projects/ceruleanassembler/"
+  #doi "arXiv:1307.7933"
+  #tag "bioinformatics"
+
+  url "https://downloads.sourceforge.net/project/ceruleanassembler/Cerulean_v_0_1.tar.gz"
+  sha1 "535e3486da3cf1de5bbaee794c3c14c53b1ee4e4"
+
+  depends_on "abyss" => :recommended
+  depends_on "python"
+
+  def install
+    doc.install "README"
+    libexec.install Dir["src/*"]
+    prefix.install "data"
+  end
+
+  test do
+    system "python #{libexec}/Cerulean.py -h"
+  end
+end
