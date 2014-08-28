e14d125cd15e40b1945f4965d220889bb111c2ff
Ian Lancaster <ilancaster@gmail.com>
2014-08-28 15:29:11 -0400

    primer3: add primer3_config
    Closes #1143.

--- primer3.rb
+++ primer3.rb
@@ -1,14 +1,18 @@
-require 'formula'
+require "formula"
 
 class Primer3 < Formula
-  homepage 'http://primer3.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/primer3/primer3/2.3.6/primer3-src-2.3.6.tar.gz'
-  sha1 '7dbd33a4e9c2a4fe06c74d6b83f8ff0f9ed1c49a'
+  homepage "http://primer3.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/primer3/primer3/2.3.6/primer3-src-2.3.6.tar.gz"
+  sha1 "7dbd33a4e9c2a4fe06c74d6b83f8ff0f9ed1c49a"
+
+  option "with-check", "Run build-time tests"
 
   def install
     cd "src" do
-      system "make all"
-      bin.install %w(primer3_core ntdpal oligotm long_seq_tm_test)
+      system "make", "test" if build.with? "check"
+      system "make", "all"
+      bin.install %w[primer3_core ntdpal ntthal oligotm long_seq_tm_test]
+      share.install "primer3_config"
     end
   end
 
