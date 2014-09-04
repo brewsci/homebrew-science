22d67c455de4714f23bc7871fa5536a171bd0db4
Ian Lancaster <ilancaster@gmail.com>
2014-09-04 00:24:13 -0400

    lp_solve: fix audit warning
    Use NumpyHasHeaders over LanguageModuleDependency.

--- lp_solve.rb
+++ lp_solve.rb
@@ -1,29 +1,20 @@
-require 'formula'
-
-def numpy_include_dir
-  "#{`python -c "import numpy.distutils.misc_util as u; print(u.get_numpy_include_dirs())[0]"`.strip}"
-end
+require "formula"
 
 class NumpyHasHeaders < Requirement
-  def satisfied?
-    not Dir["#{numpy_include_dir}/numpy/*.h"].empty?
+  def numpy_include_dir
+    "#{`python -c "import numpy.distutils.misc_util as u; print(u.get_numpy_include_dirs())[0]"`.strip}"
   end
-
+  def satisfied?
+    Dir["#{numpy_include_dir}/numpy/*.h"].any?
+  end
   def fatal?
     true
   end
-
-  def message
-    s = <<-EOS.undent
-      Apple ships NumPy without the header files :-/
-      To fix this, do either of the following options:
+  def message; <<-EOS.undent
+    lp_solve requires NumPy headers not provided by Apple.
+    Install numpy via the Homebrew/python formula:
+      `brew tap homebrew/python && brew install numpy`
     EOS
-    unless MacOS::CLT.installed?
-      s += "  * Install the 'Command Line Tools for Xcode' via the Preferences in Xcode.\n"
-    end
-    s += "  * `brew tap homebrew/python` and `brew install numpy`\n"
-    s += "  * `pip install numpy`\n"
-    s
   end
 end
 
@@ -33,9 +24,8 @@ class LpSolve < Formula
   version '5.5.2.0'  # automatic version parser spits out "solve" as version
   sha1 'e2830053cf079839b9ce21662cbc886ac6d31c80'
 
-  option 'python', 'Python extension to interface with lp_solve'
-  depends_on 'numpy' => :python if build.include? 'python'
-  depends_on NumpyHasHeaders.new if build.include? 'python'
+  depends_on :python => :optional
+  depends_on NumpyHasHeaders.new if build.with? "python"
 
   resource 'lp_solve_python' do
     # 'http://lpsolve.sourceforge.net/5.5/Python.htm'
@@ -52,10 +42,11 @@ class LpSolve < Formula
     # tells the compiler stupid things. And Xcode-only works like charm.
 
     # Clang on :snow_leopard does not ignore `-Wno-long-double` and errors out.
-    ["configure", "configure.ac", "demo/ccc.osx", "lp_solve/ccc.osx",
-     "lpsolve55/ccc.osx", "lpsolve55/cccLUSOL.osx"].each do |f|
-      inreplace f, '-Wno-long-double', ''
-    end if MacOS.version <= :snow_leopard
+    if MacOS.version <= :snow_leopard
+      files = %w[configure configure.ac demo/ccc.osxbblp_solve/ccc.osx
+                 lpsolve55/ccc.osx lpsolve55/cccLUSOL.osx]
+      files.each { |f| inreplace f, "-Wno-long-double", ""}
+    end
 
     cd 'lpsolve55' do
       system "sh ccc.osx # lpsolve55 library"
@@ -74,7 +65,7 @@ class LpSolve < Formula
     include.install 'shared/commonlib.h', 'shared/myblas.h'
     include.install Dir['bfp/bfp_LUSOL/LUSOL/lusol*.h']
 
-    if build.include? 'python' then
+    if build.with? "python"
       # In order to install into the Cellar, the dir must exist and be in the
       # PYTHONPATH.
       temp_site_packages = lib/which_python/'site-packages'
@@ -82,7 +73,6 @@ class LpSolve < Formula
       ENV['PYTHONPATH'] = temp_site_packages
       args = [
         "--no-user-cfg",
-        "install",
         "--force",
         "--verbose",
         "--install-scripts=#{share}/python",
@@ -104,7 +94,7 @@ class LpSolve < Formula
           # Even their version number is broken ...
           inreplace "setup.py", 'version = "5.5.0.9",', "version = '#{version}',"
 
-          system "python", "setup.py", *args
+          system "python", "setup.py", "install", *args
 
           # Save the examples
           (share/'lp_solve').install Dir['ex*.py'], 'lpdemo.py', 'Python.htm'
@@ -113,17 +103,22 @@ class LpSolve < Formula
     end
   end
 
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+  if build.with? "python"
+    def numpy_include_dir
+      "#{`python -c "import numpy.distutils.misc_util as u; print(u.get_numpy_include_dirs())[0]"`.strip}"
+    end
+    def which_python
+      "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+    end
+
+    def caveats; <<-EOS.undent
+      For non-homebrew Python, you need to amend your PYTHONPATH like so:
+        export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
+
+      Python examples and doc are installed to #{HOMEBREW_PREFIX}/share/lp_solve
+      EOS
+    end
   end
-
-  def caveats; <<-EOS.undent
-    For non-homebrew Python, you need to amend your PYTHONPATH like so:
-      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
-
-    Python examples and doc are installed to #{HOMEBREW_PREFIX}/share/lp_solve.
-    EOS
-  end if build.include? 'python'
 
   test do
     input = <<-EOS.undent
@@ -135,12 +130,9 @@ class LpSolve < Formula
     EOS
     (testpath/'input.lp').write(input)
     output = `#{bin}/lp_solve -S3 input.lp`
-    #puts output
+    puts output
     match = output =~ /Value of objective function: 6315\.6250/
     raise if match.nil?
-    # if build.include? 'python'
-    #   system 'python', "#{HOMEBREW_PREFIX}/share/lp_solve/lpdemo.py"
-    # end
   end
 end
 
