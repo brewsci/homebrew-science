daba6f50712e47b754e1a877ee553c37605ee32f
Ian Lancaster <ilancaster@gmail.com>
2014-09-04 01:54:23 -0400

    openblas 0.2.11

--- openblas.rb
+++ openblas.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Openblas < Formula
   homepage "http://www.openblas.net/"
-  url "https://github.com/xianyi/OpenBLAS/archive/v0.2.10.tar.gz"
-  sha1 "c4a5ca4cb9876a90193f81a0c38f4abccdf2944d"
+  url "https://github.com/xianyi/OpenBLAS/archive/v0.2.11.tar.gz"
+  sha1 "ee504e5115a93cb62042eec01f2f929ca3fa255e"
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
 
   depends_on :fortran
