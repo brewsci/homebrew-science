e4f7e2c2406bef708b3676aa79f603a8a4b0fd8f
Ian Lancaster <ilancaster@gmail.com>
2014-09-15 03:21:43 -0400

    edirect 2.00

--- edirect.rb
+++ edirect.rb
@@ -3,11 +3,12 @@ require "formula"
 class Edirect < Formula
   homepage "http://www.ncbi.nlm.nih.gov/books/NBK179288/"
   url "ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.tar.gz"
-  sha1 "e82cfc00c8e20208b2918dff090f6b4ce218491d"
-  version "1.90"
+  sha1 "f5ffa5a466ad2c971a1a3fbc3c13348ab7770317"
+  version "2.00"
 
   def install
     doc.install "README"
+    libexec.install "setup.sh", "setup-deps.pl"
     bin.install Dir["*"]
   end
 
