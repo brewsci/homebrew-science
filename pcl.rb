b81548ae4d7827417703e662a46a912c20102d5e
Francisco Facioni <fran6co@gmail.com>
2014-09-15 12:54:24 -0300

    pcl 1.7.2 (new formula)
    Closes #1181.

---
+++ pcl.rb
@@ -0,0 +1,87 @@
+require "formula"
+
+class Pcl < Formula
+  homepage "http://www.pointclouds.org/"
+  url "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.2.tar.gz"
+  sha1 "7a59e9348a81f42725db1f8b1194c9c3313372ae"
+
+  option "with-examples", "Build pcl examples."
+  option "without-tools", "Build without tools."
+  option "without-apps", "Build without apps."
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "flann"
+  depends_on "cminpack"
+
+  depends_on "qhull"
+  depends_on "libusb"
+  depends_on "qt" => :recommended
+  if build.with? "qt"
+    depends_on "vtk" => [:recommended,"with-qt"]
+  else
+    depends_on "vtk" => :recommended
+  end
+  depends_on "openni" => :optional
+  depends_on "openni2" => :optional
+
+  def install
+    args = std_cmake_args + %W[
+      -DBUILD_SHARED_LIBS:BOOL=ON
+      -DBUILD_simulation:BOOL=AUTO_OFF
+      -DBUILD_outofcore:BOOL=AUTO_OFF
+      -DBUILD_people:BOOL=AUTO_OFF
+      -DWITH_CUDA:BOOL=OFF
+      -DBUILD_global_tests:BOOL=OFF
+      -DWITH_TUTORIALS:BOOL=OFF
+      -DWITH_DOCS:BOOL=OFF
+    ]
+
+    if build.with? "openni2"
+      ENV.append "OPENNI2_INCLUDE", "#{Formula["openni2"].opt_include}/ni2"
+      ENV.append "OPENNI2_LIB", "#{Formula["openni2"].opt_lib}/ni2"
+      args << "-DBUILD_OPENNI2:BOOL=ON"
+    end
+
+    if build.with? "apps"
+      args = args + %W[
+        -DBUILD_apps=AUTO_OFF
+        -DBUILD_apps_3d_rec_framework=AUTO_OFF
+        -DBUILD_apps_cloud_composer=AUTO_OFF
+        -DBUILD_apps_in_hand_scanner=AUTO_OFF
+        -DBUILD_apps_modeler=AUTO_OFF
+        -DBUILD_apps_optronic_viewer=AUTO_OFF
+        -DBUILD_apps_point_cloud_editor=AUTO_OFF
+      ]
+    else
+      args << "-DBUILD_apps:BOOL=OFF"
+    end
+
+    args << "-DBUILD_tools:BOOL=OFF" if build.without? "tools"
+
+    if build.with? "examples"
+      args << "-DBUILD_examples:BOOL=ON"
+    else
+      args << "-DBUILD_examples:BOOL=OFF"
+    end
+
+    if build.with? "openni"
+      args << "-DOPENNI_INCLUDE_DIR=#{Formula["openni"].opt_include}/ni"
+    else
+      args << "-DCMAKE_DISABLE_FIND_PACKAGE_OpenNI:BOOL=TRUE"
+    end
+
+    args << "-DCMAKE_DISABLE_FIND_PACKAGE_Qt4:BOOL=TRUE" if build.without? "qt"
+    args << "-DCMAKE_DISABLE_FIND_PACKAGE_VTK:BOOL=TRUE" if build.without? "vtk"
+
+    args << ".."
+    mkdir "macbuild" do
+      system "cmake", *args
+      system "make"
+      system "make install"
+    end
+  end
+end
