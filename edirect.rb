287e10d6a96420a0e01657e9fa1cb6031bba454c
Shaun Jackman <sjackman@gmail.com>
2014-09-16 10:17:01 -0700

    edirect: Add tag

--- edirect.rb
+++ edirect.rb
@@ -2,6 +2,7 @@ require "formula"
 
 class Edirect < Formula
   homepage "http://www.ncbi.nlm.nih.gov/books/NBK179288/"
+  #tag "bioinformatics"
   url "ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.tar.gz"
   sha1 "8bd970c630fe77b7ceff8f7a8c18bf0d5225253d"
   version "2.00"
