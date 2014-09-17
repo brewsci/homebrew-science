c9982b8be624569c860d4a1b544be66b01c236bf
Shaun Jackman <sjackman@gmail.com>
2014-09-17 11:24:35 -0700

    abyss: Run make check and add --without-check

--- abyss.rb
+++ abyss.rb
@@ -15,6 +15,14 @@ class Abyss < Formula
     depends_on 'multimarkdown' => :build
   end
 
+  resource "gtest" do
+    #homepage "https://code.google.com/p/googletest/"
+    url "https://googletest.googlecode.com/files/gtest-1.7.0.zip"
+    sha1 "f85f6d2481e2c6c4a18539e391aa4ea8ab0394af"
+  end
+
+  option "without-check", "Skip build-time tests (not recommended)"
+
   MAXK = [32, 64, 96, 128, 256, 512]
   MAXK.each do |k|
     option "enable-maxk=#{k}", "set the maximum k-mer length to #{k}"
@@ -29,15 +37,26 @@ class Abyss < Formula
   skip_clean 'bin'
 
   def install
+    resource("gtest").stage do
+      system "./configure"
+      system "make"
+      (buildpath/"gtest").install "include", "lib"
+    end if build.with? "check"
+
     system "./autogen.sh" if build.head?
+
     args = [
       '--disable-dependency-tracking',
       "--prefix=#{prefix}"]
+    args << "--with-gtest=#{buildpath}/gtest" if build.with? "check"
     MAXK.each do |k|
       args << "--enable-maxk=#{k}" if build.include? "enable-maxk=#{k}"
     end
+
     system "./configure", *args
-    system "make install"
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 
   test do
