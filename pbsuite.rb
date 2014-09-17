6a2abf93f3f5a5668cbbbe0401acf0b950718f39
Shaun Jackman <sjackman@gmail.com>
2014-09-17 10:47:02 -0700

    pbsuite: Add dependency on blasr and python
    Add bioinformatics tag and the DOI for PBHoney.

--- pbsuite.rb
+++ pbsuite.rb
@@ -2,11 +2,16 @@ require "formula"
 
 class Pbsuite < Formula
   homepage "http://sourceforge.net/projects/pb-jelly/"
-  #doi "10.1371/journal.pone.0047768"
+  #doi "10.1371/journal.pone.0047768" => "PBJelly", "10.1186/1471-2105-15-180" => "PBHoney"
+  #tag "bioinformatics"
+
   url "https://downloads.sourceforge.net/project/pb-jelly/PBSuite_14.7.14.tgz"
   sha1 "1a6530f24af6a54af26285bae5d7a8e58e94a2a2"
 
   conflicts_with "bedtools", :because => "Both install bin/bamToFastq"
+
+  depends_on "blasr" => :recommended
+  depends_on "python"
 
   def caveats; <<-EOS.undent
     Set the PYTHONPATH environment variable:
