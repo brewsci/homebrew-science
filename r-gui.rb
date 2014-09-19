b8fb26844939cc4e5dc20b5e82cd8d1276c67780
Josh Bode <joshbode@gmail.com>
2014-09-19 23:26:30 +1000

    r-gui 1.65
    - Updated to new version (1.65).
    - Removed unnecessary patch.
    
    Closes #1188.

--- r-gui.rb
+++ r-gui.rb
@@ -3,8 +3,8 @@ require "formula"
 class RGui < Formula
 
   homepage "http://cran.r-project.org/bin/macosx/"
-  url "http://cran.r-project.org/bin/macosx/Mac-GUI-1.64.tar.gz"
-  sha1 "8c31fb05ad990f0c83e94e1921738e0951624bd9"
+  url "http://cran.r-project.org/bin/macosx/Mac-GUI-1.65.tar.gz"
+  sha1 "9c6aaabbfe2307f4cedd6a4b49ffda88952a8a1e"
 
   head "https://svn.r-project.org/R-packages/trunk/Mac-GUI"
 
@@ -13,11 +13,6 @@ class RGui < Formula
   depends_on :arch => :x86_64
 
   depends_on "r"
-
-  stable do
-    # fix for 1.64 in GUI-tools.R
-    patch :DATA
-  end
 
   def install
     # ugly hack to get updateSVN script in build to not fail
@@ -32,18 +27,3 @@ class RGui < Formula
     prefix.install "build/Release/R.app"
   end
 end
-
-__END__
-diff --git a/GUI-tools.R b/GUI-tools.R
-index a02b0b3..30383f4 100644
---- a/GUI-tools.R
-+++ b/GUI-tools.R
-@@ -99,7 +99,7 @@ add.fn("package.manager", function ()
-     pkgs.status <- character(length(is.loaded))
-     pkgs.status[which(is.loaded)] <- "loaded"
-     pkgs.status[which(!is.loaded)] <- " "
--    pkgs.url <- file.path(.find.package(pkgs), "html", "00Index.html")
-+    pkgs.url <- file.path(find.package(pkgs, quiet=TRUE), "html", "00Index.html")
-     load.idx <-
-         .Call("pkgmanager", is.loaded, pkgs, pkgs.desc, pkgs.url)
-     toload <- which(load.idx & !is.loaded)
