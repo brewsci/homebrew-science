104fd8edf93f5bbf809e0d085e4cd6b96791ad55
Tomoya Tanjo <ttanjo@gmail.com>
2014-09-22 17:12:13 +0900

    Zoltan 3.8: graph partitioning and load balancing
    Closes #1192.

---
+++ zoltan.rb
@@ -0,0 +1,35 @@
+require "formula"
+
+class Zoltan < Formula
+  url "http://www.cs.sandia.gov/~kddevin/Zoltan_Distributions/zoltan_distrib_v3.8.tar.gz"
+  homepage "http://www.cs.sandia.gov/Zoltan"
+  sha1 "632b851485ac37cf10ad4d0ee8586c2fed79dfbf"
+
+  option "without-check", "Skip build-time tests (not recommended)"
+
+  depends_on "scotch"   => :optional
+  depends_on "parmetis" => :optional
+  depends_on :fortran   => :optional
+
+  mpilang = [:cc, :cxx]
+  mpilang << :f90 if build.with? :fortran
+  depends_on :mpi => mpilang
+
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "CC=#{ENV["MPICC"]}",
+      "CXX=#{`which mpic++`.chomp}",
+    ]
+    args << "--with-scotch"         if build.with? "scotch"
+    args << "--with-parmetis"       if build.with? "parmetis"
+    args << "--enable-f90interface" if build.with? "fortran"
+
+    mkdir "zoltan-build" do
+      system "../configure", *args
+      system "make everything"
+      system "make check" if build.with? "check"
+      system "make install"
+    end
+  end
+end
