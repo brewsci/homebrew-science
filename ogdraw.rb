5f1f102733404a4a00b887ff3c518081f80277d8
Shaun Jackman <sjackman@gmail.com>
2014-09-22 15:56:31 -0700

    ogdraw: Install irscan
    drawgenemap: Add --irscan and --ircoord options

--- ogdraw.rb
+++ ogdraw.rb
@@ -19,9 +19,16 @@ class Ogdraw < Formula
   depends_on 'PostScript::Simple' => :perl
   depends_on 'XML::Generator' => :perl
 
+  # drawgenemap: Add --irscan and --ircoord options
+  patch do
+    url "https://github.com/sjackman/OGDraw/commit/ac56bd6c871635588db1911b1f962929e2f13b88.diff"
+    sha1 "4d5d3287cbe35f1a30ef7ba966ab393a3ec0e2cc"
+  end
+
   def install
     system *%W[perl Makefile.PL PREFIX=#{prefix}]
     system "make", "pure_install"
+    bin.install "irscan/bin/irscan_linux_x86" => "irscan" if OS.linux?
   end
 
   def caveats; <<-EOS.undent
