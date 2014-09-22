12988a5186df8f4934cbb921fa5c49785f565586
warrenlr <rwarren@bcgsc.ca>
2014-09-22 08:30:44 -0700

    hlaminer 1.1: New formula
    HLAminer is a software for HLA class I and II predictions from next-generation
    shotgun (NGS) sequence read data that supports direct read alignment and
    targeted assembly of sequence reads.
    
    Closes #1196

---
+++ hlaminer.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Hlaminer < Formula
+  homepage "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer"
+  #doi "10.1186/gm396"
+  #tag "bioinformatics"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.1/HLAminer.tar.gz"
+  version "1.1.0"
+  sha1 "5fd145df85a1257147310a0bf5fee3e2f9ff6ec0"
+
+  depends_on "tasr"
+
+  def install
+    # Conflicts with tasr
+    rm "bin/TASR"
+
+    prefix.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/HLAminer.pl |grep hlaminer"
+  end
+end
