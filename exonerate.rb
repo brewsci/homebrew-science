560ca01ed6d67981dfc9332e874de1fb36aa324a
Shaun Jackman <sjackman@gmail.com>
2014-09-24 16:08:21 -0700

    exonerate 2.4.0 devel

--- exonerate.rb
+++ exonerate.rb
@@ -3,8 +3,14 @@ require 'formula'
 class Exonerate < Formula
   homepage 'http://www.ebi.ac.uk/~guy/exonerate/'
   #doi '10.1186/1471-2105-6-31'
+  #tag "bioinformatics"
   url 'http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.2.0.tar.gz'
   sha1 'ad4de207511e4d421e5cc28dda2261421c515bf0'
+
+  devel do
+    url "http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.4.0.tar.gz"
+    sha1 "5b119c0aef0fa08c3f4a11014544f2ac5ca8afde"
+  end
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
