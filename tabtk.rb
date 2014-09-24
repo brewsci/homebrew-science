227c418609683fbcde792cfedefcb61bd63abc2c
Ian Lancaster <ilancaster@gmail.com>
2014-09-23 23:51:48 -0400

    tabtk 2014-09-22-r9

--- tabtk.rb
+++ tabtk.rb
@@ -2,8 +2,9 @@ require "formula"
 
 class Tabtk < Formula
   homepage "https://github.com/lh3/tabtk"
-  url "https://github.com/lh3/tabtk.git", :revision => "3ac8bb90"
-  version "2014-09-13-r7"
+  url "https://github.com/lh3/tabtk/archive/3ac8bb90.tar.gz"
+  sha1 "cd75b63a91a1d3d7e1f2c7929fb6e58d8bddfa91"
+  version "2014-09-22-r9"
 
   def install
     system "make"
