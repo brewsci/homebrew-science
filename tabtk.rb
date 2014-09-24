080cb77b918b63b8aabfb94c45eedabe40bdec81
Ian Lancaster <ilancaster@gmail.com>
2014-09-23 23:56:46 -0400

    tabtk: fix r9 url

--- tabtk.rb
+++ tabtk.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Tabtk < Formula
   homepage "https://github.com/lh3/tabtk"
-  url "https://github.com/lh3/tabtk/archive/3ac8bb90.tar.gz"
+  url "https://github.com/lh3/tabtk/archive/7109d8b.tar.gz"
   sha1 "cd75b63a91a1d3d7e1f2c7929fb6e58d8bddfa91"
   version "2014-09-22-r9"
 
