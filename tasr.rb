5b66e293f2e3d540dcb483b35d9110e40ced71d9
Shaun Jackman <sjackman@gmail.com>
2014-09-26 13:20:21 -0700

    tasr 1.5.1: Update the URL
    Closes #1204

--- tasr.rb
+++ tasr.rb
@@ -4,20 +4,17 @@ class Tasr < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/tasr"
   #doi "10.1371/journal.pone.0019816"
   #tag "bioinformatics"
-  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1.5.1-tar.gz"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1-5-1.tar.gz"
   version "1.5.1"
-  sha1 "d12cdd3e1812f1ca0ade30d242bcb877eb763196"
+  sha1 "095f3fa5dd92252331abae7cd04848ef0dbab9cb"
 
   def install
-    system "tar xf tasr_v#{version}-tar"
-    cd "tasr_v#{version}" do
-      bin.install "TASR"
-      doc.install "TASR.readme"
-      prefix.install "test", "tools"
-    end
+    bin.install "TASR"
+    doc.install "TASR.readme"
+    prefix.install "test", "tools"
   end
 
   test do
-    system "tasr |grep tasr"
+    system "#{bin}/tasr |grep tasr"
   end
 end
