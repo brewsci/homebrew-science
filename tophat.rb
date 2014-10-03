5578de354182f716cf71e306bfc82d3423aa12de
Saket Choudhary <saketkc@gmail.com>
2014-10-02 22:18:55 -0700

    tophat 2.0.13
    Closes #1223.

--- tophat.rb
+++ tophat.rb
@@ -2,20 +2,14 @@ require 'formula'
 
 class Tophat < Formula
   homepage 'http://ccb.jhu.edu/software/tophat'
-  url 'http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.12.tar.gz'
-  sha1 '966f8748a55820dfb64d3a5618f38105858dff3e'
+  url 'http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.13.tar.gz'
+  sha1 '71e8b63e42008ddd10e6324712c67e0e3428c378'
 
-  depends_on 'samtools'
   depends_on 'boost'
+  # Patch for OS X
+  patch :p0, :DATA
   # bowtie or bowtie2 is required to execute tophat
   depends_on 'bowtie2' => :recommended
-
-  # 1. Fix namespace issue in segment_juncs.cpp
-  # 2. Fix "Variable length arrays using non-POD element types" error
-  # in tophat_reports.cpp
-  # Reported upstream:
-  # https://groups.google.com/forum/#!topic/tuxedo-tools-users/hOOVgXwB0NQ
-  patch :DATA
 
   def install
     # This can only build serially, otherwise it errors with no make target.
@@ -41,48 +35,16 @@ class Tophat < Formula
     end
   end
 end
-
 __END__
-diff --git a/src/segment_juncs.cpp b/src/segment_juncs.cpp
-index 80fb3b7..bc55db7 100644
---- a/src/segment_juncs.cpp
-+++ b/src/segment_juncs.cpp
-@@ -47,7 +47,6 @@
+--- src/samtools-0.1.18/ksort.h.orig	2014-10-02 22:42:08.000000000 -0700
++++ src/samtools-0.1.18/ksort.h	2014-10-02 22:43:14.000000000 -0700
+@@ -141,7 +141,7 @@
+ 			tmp = *l; *l = l[i]; l[i] = tmp; ks_heapadjust_##name(0, i, l); \
+ 		}																\
+ 	}																	\
+-	inline void __ks_insertsort_##name(type_t *s, type_t *t)			\
++	static inline void __ks_insertsort_##name(type_t *s, type_t *t)			\
+ 	{																	\
+ 		type_t *i, *j, swap_tmp;										\
+ 		for (i = s + 1; i < t; ++i)										\
 
- using namespace seqan;
- using namespace std;
--using namespace __gnu_cxx;
-
- // daehwan //geo
- //#define B_DEBUG 1
-diff --git a/src/tophat_reports.cpp b/src/tophat_reports.cpp
-index 9d0a927..a403dbe 100644
---- a/src/tophat_reports.cpp
-+++ b/src/tophat_reports.cpp
-@@ -2784,10 +2784,10 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
- 			fprintf(stderr, "Warning: %lu small overhang junctions!\n", (long unsigned int)small_overhangs);
- 	 */
-
--	JunctionSet vfinal_junctions[num_threads];
--	InsertionSet vfinal_insertions[num_threads];
--	DeletionSet vfinal_deletions[num_threads];
--	FusionSet vfinal_fusions[num_threads];
-+	JunctionSet * vfinal_junctions = new JunctionSet[num_threads];
-+	InsertionSet * vfinal_insertions = new InsertionSet[num_threads];
-+	DeletionSet * vfinal_deletions = new DeletionSet[num_threads];
-+	FusionSet * vfinal_fusions = new FusionSet[num_threads];
-
- 	vector<SAlignStats> alnStats(num_threads);
-
-@@ -2931,6 +2931,11 @@ for (; deletion_iter != deletions.end(); ++deletion_iter)
- 	}
-
- 	fprintf(stderr, "Found %lu junctions from happy spliced reads\n", (long unsigned int)final_junctions.size());
-+
-+	delete[] vfinal_junctions;
-+	delete[] vfinal_insertions;
-+	delete[] vfinal_deletions;
-+	delete[] vfinal_fusions;
- }
-
- void print_usage()
