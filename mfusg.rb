25afdbf9bd31b4aab39124e31344138313248991
Justin R. Jent <jentjr@gmail.com>
2014-10-05 13:52:06 -0400

    mfusg (new formula)
    Closes #1230.

---
+++ mfusg.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Mfusg < Formula
+  homepage "http://water.usgs.gov/ogw/mfusg"
+  url "http://water.usgs.gov/ogw/mfusg/mfusg.1_2_00.zip"
+  sha1 "ab25e4e816ad3c9b83720b7b367ddd4e287caf0f"
+
+  depends_on :python
+  depends_on :fortran
+
+  def install
+    prefix.install "mfusg.txt", "readme.txt", "release.txt", "problems.txt"
+    share.install "test"
+    doc.install Dir["doc/*"]
+    Dir.chdir "pymake"
+    system "python", "makebin.py"
+    bin.install "mfusg"
+  end
+
+  test do
+    system "#{bin}/mfusg", "#{share}/test/03_conduit_confined/ex3.nam"
+  end
+end
