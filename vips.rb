07fd4ea98da452950207f247d0f63c61c5c88d3e
James Tyrrell <james@tyrrells.com>
2014-10-09 12:56:53 +1100

    vips 7.40.11
    Closes #1239.

--- vips.rb
+++ vips.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Vips < Formula
   homepage "http://www.vips.ecs.soton.ac.uk/"
-  url "http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.10.tar.gz"
-  sha1 "9fb29cd0b0140af4a7c011e79788588fe954ca83"
+  url "http://www.vips.ecs.soton.ac.uk/supported/7.40/vips-7.40.11.tar.gz"
+  sha1 "2acecf7fc13d8f1c0ee4736f33c86205e67988e6"
 
   option "without-check", "Disable build time checks (not recommended)"
 
