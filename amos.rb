5bfc3f1f1d3d7317d090c14da8e2aeea0f907f6d
Ian Lancaster <ilancaster@gmail.com>
2014-10-21 23:55:37 -0400

    amos: fix Perl module dependency, cleanup
    Bump clang version
    Check configure args against build.with?
    
    Fixes #1254.

--- amos.rb
+++ amos.rb
@@ -1,30 +1,43 @@
-require 'formula'
+require "formula"
 
 class Amos < Formula
-  homepage 'http://sourceforge.net/apps/mediawiki/amos/index.php?title=AMOS'
-  url 'https://downloads.sourceforge.net/project/amos/amos/3.1.0/amos-3.1.0.tar.gz'
-  sha1 '28e799e37713594ba7147d300ecae6574beb14a4'
+  homepage "http://sourceforge.net/apps/mediawiki/amos/index.php?title=AMOS"
+  url "https://downloads.sourceforge.net/project/amos/amos/3.1.0/amos-3.1.0.tar.gz"
+  sha1 "28e799e37713594ba7147d300ecae6574beb14a4"
 
-  depends_on 'blat' => :optional # for minimus2-blat
-  depends_on 'boost' => :recommended # for Bambus 2
-  depends_on 'mummer' => :recommended # for minimus2
-  depends_on 'qt' => [:optional, 'with-qt3support'] # for AMOS GUIs
-  depends_on 'Statistics::Descriptive' => [:perl, :recommended]
+  depends_on "blat" => :optional # for minimus2-blat
+  depends_on "boost" => :recommended # for Bambus 2
+  depends_on "mummer" => :recommended # for minimus2
+  depends_on "qt" => [:optional, "with-qt3support"] # for AMOS GUIs
+  depends_on "Statistics::Descriptive" => :perl
 
   fails_with :clang do
-    build 500
+    build 600
     cause "error: reference to 'hash' is ambiguous"
   end
 
   def install
+    # http://seqanswers.com/forums/showthread.php?t=17802
+    inreplace "src/Align/find-tandem.cc", "#include <sys/time.h>", "#include <sys/time.h>\n#include <getopt.h>"
+
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}",
-      "--with-Boost-dir=#{Formula["boost"].opt_prefix}",
-      "--with-qmake-qt4=#{Formula["qt"].opt_prefix}/bin/qmake",
-      "BLAT=#{Formula["blat"].opt_prefix}/bin/blat",
-      "DELTAFILTER=#{Formula["mummer"].opt_prefix}/libexec/delta-filter",
-      "SHOWCOORDS=#{Formula["mummer"].opt_prefix}/libexec/show-coords"
-    system "make install"
+    args = %W[--prefix=#{prefix} --disable-dependency-tracking]
+    args << "--with-Boost-dir=#{Formula["boost"].opt_prefix}" if build.with? "boost"
+    args << "BLAT=#{Formula["blat"].opt_bin}/blat" if build.with? "blat"
+
+    if build.with? "qt"
+      args << "--with-qmake-qt4=#{Formula["qt"].opt_bin}/qmake"
+    else
+      args << "BUILD_QT4=no"
+    end
+
+    if build.with? "mummer"
+      args << "DELTAFILTER=#{Formula["mummer"].opt_libexec}/delta-filter"
+      args << "SHOWCOORDS=#{Formula["mummer"].opt_libexec}/show-coords"
+    end
+
+    system "./configure", *args
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
