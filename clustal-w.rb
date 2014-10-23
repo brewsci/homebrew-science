a8003267b5f75a5322d7629aa09e99e25a2fb8ad
Ian Lancaster <ilancaster@gmail.com>
2014-10-23 17:07:58 -0400

    clustal-w: bump clang, use double quotes
    Closes #1281.

--- clustal-w.rb
+++ clustal-w.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class ClustalW < Formula
-  homepage 'http://www.clustal.org/clustal2/'
+  homepage "http://www.clustal.org/clustal2/"
   #doi "10.1093/nar/22.22.4673"
-  url 'http://www.clustal.org/download/2.1/clustalw-2.1.tar.gz'
-  sha1 'f29784f68585544baa77cbeca6392e533d4cf433'
+  url "http://www.clustal.org/download/2.1/clustalw-2.1.tar.gz"
+  sha1 "f29784f68585544baa77cbeca6392e533d4cf433"
 
   fails_with :clang do
-    build 503
+    build 600
     cause "error: implicit instantiation of undefined template"
   end
 
