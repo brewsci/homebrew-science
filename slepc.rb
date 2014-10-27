4c2cec109f3ec7f705e823f630eb430319517f2e
Denis Davydov <davydden@gmail.com>
2014-10-27 11:00:34 +0100

    Slepc 3.5.2 (update)
    Update SLEPc to the newest version.
    Add optional dependency on Arpack.
    
    Closes #1298.

--- slepc.rb
+++ slepc.rb
@@ -2,31 +2,47 @@ require 'formula'
 
 class Slepc < Formula
   homepage 'http://www.grycap.upv.es/slepc'
-  url 'http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.4.4.tar.gz'
-  sha1 'd7c09f3e2bb8910758e488e84c16a6eb266cf379'
+  url 'http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.5.2.tar.gz'
+  sha1 '23675bee5c010d20f4a08f80f22120119ddb940a'
+
+  option "complex", "Use complex version by default. Otherwise, real-valued version will be symlinked"
 
   depends_on 'petsc' => :build
   depends_on :mpi => [:cc, :f90]
   depends_on :fortran
-  depends_on :x11  => MacOS::X11.installed? ? :recommended : :optional
-
-  # Trick SLEPc into thinking we don't have a prefix install of PETSc.
-  patch :DATA
+  depends_on :x11  => :optional
+  depends_on 'arpack' => [:recommended, "with-mpi"]
 
   def install
     ENV.deparallelize
-    petsc_arch = 'arch-darwin-c-opt'
+
+    # these must be consistent with petsc.rb
+    petsc_arch_real="real"
+    petsc_arch_complex="complex"
+
     ENV['SLEPC_DIR'] = Dir.getwd
-    ENV['PETSC_DIR'] = Formula["petsc"].opt_prefix
-    ENV['PETSC_ARCH'] = petsc_arch
-    system "./configure", "--prefix=#{prefix}/#{petsc_arch}"
-    system "make PETSC_ARCH=#{petsc_arch}"
-    system "make PETSC_ARCH=#{petsc_arch} install"
-    ENV['PETSC_ARCH'] = ''
-    system "make SLEPC_DIR=#{prefix}/#{petsc_arch} test"
+    args = %W[--with-clean=true]
+    args << "--with-arpack-dir=#{Formula["arpack"].lib}" << "--with-arpack-flags=-lparpack,-larpack" if build.with? "arpack"
+
+    # real
+    ENV['PETSC_DIR'] = ("#{Formula["petsc"].opt_prefix}" +"/"+petsc_arch_real)
+    system "./configure", *args, "--prefix=#{prefix}/#{petsc_arch_real}"
+    system "make"
+    system "make test"
+    system "make install"
+
+    # complex
+    ENV['PETSC_DIR'] = ("#{Formula["petsc"].opt_prefix}" +"/"+petsc_arch_complex)
+    system "./configure", *args, "--prefix=#{prefix}/#{petsc_arch_complex}"
+    system "make"
+    system "make test"
+    system "make install"
+
     ohai 'Test results are in ~/Library/Logs/Homebrew/slepc. Please check.'
 
     # Link what we need.
+    petsc_arch = ((build.include? "complex") ? petsc_arch_complex : petsc_arch_real)
+
     include.install_symlink Dir["#{prefix}/#{petsc_arch}/include/*.h"], "#{prefix}/#{petsc_arch}/finclude", "#{prefix}/#{petsc_arch}/slepc-private"
     lib.install_symlink Dir["#{prefix}/#{petsc_arch}/lib/*.a"], Dir["#{prefix}/#{petsc_arch}/lib/*.dylib"]
     prefix.install_symlink "#{prefix}/#{petsc_arch}/conf"
@@ -35,24 +51,8 @@ class Slepc < Formula
   end
 
   def caveats; <<-EOS.undent
-    Set your SLEPC_DIR to #{prefix}/arch-darwin-c-opt.
-    Fortran modules are in #{prefix}/arch-darwin-c-opt/include.
+    Set your SLEPC_DIR to #{prefix}/real or #{prefix}/complex.
+    Fortran modules are in #{prefix}/real/include and #{prefix}/complex/include.
     EOS
   end
 end
-
-__END__
-diff --git a/config/configure.py b/config/configure.py
-index 7d2fd64..22351c3 100755
---- a/config/configure.py
-+++ b/config/configure.py
-@@ -208,8 +208,6 @@ if petscversion.VERSION < slepcversion.VERSION:
- petscconf.Load(petscdir)
- if not petscconf.PRECISION in ['double','single','__float128']:
-   sys.exit('ERROR: This SLEPc version does not work with '+petscconf.PRECISION+' precision')
--if prefixinstall and not petscconf.ISINSTALL:
--  sys.exit('ERROR: SLEPc cannot be configured for non-source installation if PETSc is not configured in the same way.')
-
- # Check whether this is a working copy of the repository
- isrepo = 0
-
