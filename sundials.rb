d199f851de2c54c9dd1beae172942b7f03addfea
Dominique Orban <dominique.orban@gmail.com>
2014-10-28 12:05:12 -0400

    Sundials: update url.
    The official Sundials site requires registration and doesn't provide
    a direct download link any more.

--- sundials.rb
+++ sundials.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class Sundials < Formula
   homepage 'https://computation.llnl.gov/casc/sundials/main.html'
-  url 'https://computation.llnl.gov/casc/sundials/download/code/sundials-2.5.0.tar.gz'
-  mirror 'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sundials-2.5.0.tar.gz'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sundials-2.5.0.tar.gz'
   sha1 '9affcc525269e80c8ec6ae1db1e0a0ff201d07e0'
 
   depends_on "openblas" => :optional
