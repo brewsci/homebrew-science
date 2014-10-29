1c270948162726b661b381a6a34e580f6bcac9c5
David Hall <dhcrawley@gmail.com>
2014-10-29 17:12:35 +0000

    cuba 4.0
    Closes #1305.

--- cuba.rb
+++ cuba.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Cuba < Formula
   homepage "http://www.feynarts.de/cuba"
-  url "http://www.feynarts.de/cuba/Cuba-3.3.tar.gz"
-  sha1 "7cfab584f2d3617c66b9913b0f0a464e0769d68c"
+  url "http://www.feynarts.de/cuba/Cuba-4.0.tar.gz"
+  sha1 "002e6c5decf5bc95e2a75265938f28bc8939c56b"
 
   option "without-check", "Skip build-time tests (not recommended)"
 
