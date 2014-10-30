ac67ab2d9fba8ed8ab61c45025385b825765ab43
Shaun Jackman <sjackman@gmail.com>
2014-10-30 11:40:56 -0700

    sga 0.10.13: Do not install Makefile Makefile.in

--- sga.rb
+++ sga.rb
@@ -21,7 +21,7 @@ class Sga < Formula
                             "--with-bamtools=#{Formula["bamtools"].opt_prefix}",
                             "--with-sparsehash=#{Formula["google-sparsehash"].opt_prefix}"
       system "make install"
-      bin.install Dir["bin/*"] - ["bin/Makefile.am"]
+      bin.install Dir["bin/*"] - Dir["bin/Makefile*"]
     end
   end
 
