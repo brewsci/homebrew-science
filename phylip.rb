4505802746e8582da47c1541252db155ee1ee46e
Ian Lancaster <ilancaster@gmail.com>
2014-11-03 14:27:24 -0500

    phylip 3.696

--- phylip.rb
+++ phylip.rb
@@ -1,51 +1,41 @@
-require 'formula'
+require "formula"
 
 class Phylip < Formula
-  homepage 'http://evolution.genetics.washington.edu/phylip.html'
-  url 'http://evolution.gs.washington.edu/phylip/download/phylip-3.695.tar.gz'
-  sha1 '1505d1b3bb1378244e1733a6ecb173b3259c3d0f'
-
-  depends_on :x11
+  homepage "http://evolution.genetics.washington.edu/phylip.html"
+  url "http://evolution.gs.washington.edu/phylip/download/phylip-3.696.tar.gz"
+  sha1 "e3ac52ca37c3397f81bb9325ee21ca8e5a8a2fa4"
 
   def install
-    cd 'src' do
+    cd "src" do
       system "make -f Makefile.unx all"
-      system "make -f Makefile.unx put EXEDIR=#{bin}"
-      # Perhaps one day the Mac apps will work (with cocoa) "make -f Makefile.osx apps"
+      system "make -f Makefile.unx put EXEDIR=#{libexec}"
     end
 
-    # Remove installed fonts
-    bin.cd do
-      rm Dir['font*']
-    end
-
-    (share/'phylip').install ['phylip.html', 'doc']
+    rm Dir["#{libexec}/font*"]
+    bin.install_symlink Dir["#{libexec}/*"] - Dir["#{libexec}/*.{so,jar,unx}"]
+    (share/"phylip").install ["phylip.html", "doc"]
   end
 
   def caveats
     <<-EOS.undent
-      The documentation has been installed to #{HOMEBREW_PREFIX}/share/phylip/phylip.html.
+      The documentation has been installed to #{HOMEBREW_PREFIX}/share/phylip/
     EOS
   end
 
   test do
     # From http://evolution.genetics.washington.edu/phylip/doc/pars.html
-    mktemp do
-      Pathname.new('infile').write <<-EOF.undent
-        7         6
-        Alpha1    110110
-        Alpha2    110110
-        Beta1     110000
-        Beta2     110000
-        Gamma1    100110
-        Delta     001001
-        Epsilon   001110
-      EOF
-      Pathname.new('expected').write <<-EOF.undent
-        (((Epsilon:0.00,Delta:3.00):2.00,Gamma1:0.00):1.00,(Beta2:0.00,Beta1:0.00):2.00,Alpha2:0.00,Alpha1:0.00);
-      EOF
-      system "echo 'Y' | #{bin}/pars"
-      compare_file 'outtree', 'expected'
-    end
+    (testpath/"infile").write <<-EOF.undent
+      7         6
+      Alpha1    110110
+      Alpha2    110110
+      Beta1     110000
+      Beta2     110000
+      Gamma1    100110
+      Delta     001001
+      Epsilon   001110
+    EOF
+    expected = "(((Epsilon:0.00,Delta:3.00):2.00,Gamma1:0.00):1.00,(Beta2:0.00,Beta1:0.00):2.00,Alpha2:0.00,Alpha1:0.00);"
+    system "echo 'Y' | #{bin}/pars"
+    assert File.read("outtree").include?(expected)
   end
 end
