ae060a038d7ff284b9a1d1fed165e76ab4e243c8
Shaun Jackman <sjackman@gmail.com>
2014-11-10 09:44:49 -0800

    stacks 1.21
    Depends on htslib rather than samtools for BAM support.

--- stacks.rb
+++ stacks.rb
@@ -1,32 +1,22 @@
-require 'formula'
+require "formula"
 
 class Stacks < Formula
-  homepage 'http://creskolab.uoregon.edu/stacks/'
-  url 'http://creskolab.uoregon.edu/stacks/source/stacks-1.20.tar.gz'
-  sha1 'fad38ceb3eb8ab4d240385c769b45be52fb6ee2b'
-  #doi '10.1111/mec.12354'
+  homepage "http://creskolab.uoregon.edu/stacks/"
+  #doi "10.1111/mec.12354"
+  #tag "bioinformatics
+
+  url "http://creskolab.uoregon.edu/stacks/source/stacks-1.21.tar.gz"
+  sha1 "5510eeba37198a4c59f1eda6034e7340dbc233f2"
 
   depends_on "google-sparsehash" => :recommended
-  depends_on "samtools"          => :recommended
+  depends_on "htslib"
 
   needs :cxx11
-
-  fails_with :clang do
-    build 503
-    cause "error: comparison between pointer and integer ('const char *' and 'int')"
-  end
 
   def install
     # OpenMP doesn't yet work on OS X with Apple-provided compilers.
     args = ["--disable-dependency-tracking", "--disable-openmp", "--prefix=#{prefix}"]
     args << "--enable-sparsehash" if build.with? "google-sparsehash"
-
-    if build.with? "samtools"
-      samtools = Formula["samtools"].opt_prefix
-      args += ["--enable-bam",
-        "--with-bam-include-path=#{samtools}/include/bam",
-        "--with-bam-lib-path=#{samtools}/lib"]
-    end
 
     system "./configure", *args
     system "make", "install"
