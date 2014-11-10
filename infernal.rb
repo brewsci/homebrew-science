c9d0c2fdd39ad91ee2388bfe397f29bfd36bdd08
Shaun Jackman <sjackman@gmail.com>
2014-11-10 09:48:38 -0800

    infernal 1.1.1: Add tag bioinformatics

--- infernal.rb
+++ infernal.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Infernal < Formula
   homepage 'http://infernal.janelia.org/'
   #doi "10.1093/bioinformatics/btp157"
+  #tag "bioinformatics"
 
   url 'http://selab.janelia.org/software/infernal/infernal-1.1.1.tar.gz'
   sha1 'b83fbc3a50c49ad51e40b37195aeacec94897d30'
