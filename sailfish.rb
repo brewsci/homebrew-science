e9d98b06b5d0866cadbe1e2dd077974f92eb4ba5
Shaun Jackman <sjackman@gmail.com>
2014-11-10 16:21:34 -0800

    sailfish 0.6.2: Fails to build with clang 600
    Add bioinformatics tag.

--- sailfish.rb
+++ sailfish.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Sailfish < Formula
   homepage 'http://www.cs.cmu.edu/~ckingsf/software/sailfish'
   #doi '10.1038/nbt.2862'
+  #tag "bioinformatics"
+
   url 'https://github.com/kingsfordgroup/sailfish/releases/download/v0.6.2/Sailfish-0.6.2-Source.tar.gz'
   sha1 'b8bf01cab7b685fd9b9608bf3e97e25df28e0639'
 
@@ -13,7 +15,7 @@ class Sailfish < Formula
   keg_only 'sailfish conflicts with jellyfish.'
 
   fails_with :clang do
-    build 500
+    build 600
     cause 'Currently, the only supported compiler is GCC(>=4.7). We hope to support Clang soon.'
   end
 
