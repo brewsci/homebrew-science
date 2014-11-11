b042d2bdca1385ed3762506e3cc3b2a15df2d3c9
Saket Choudhary <saketkc@gmail.com>
2014-11-11 11:44:54 -0800

    bedtools 2.22.0
    Closes #1351.

--- bedtools.rb
+++ bedtools.rb
@@ -4,8 +4,8 @@ class Bedtools < Formula
   homepage 'https://github.com/arq5x/bedtools2'
   #doi '10.1093/bioinformatics/btq033'
   #tag "bioinformatics"
-  url "https://github.com/arq5x/bedtools2/releases/download/v2.21.0/bedtools-2.21.0.tar.gz"
-  sha1 "ceeb1871bec66e90f68a1bef24d82c06669c47d8"
+  url "https://github.com/arq5x/bedtools2/releases/download/v2.22.0/bedtools-2.22.0.tar.gz"
+  sha1 "96f9f7031806de5d4ec694aeb9db31c3f8188f9b"
   head 'https://github.com/arq5x/bedtools2.git'
 
   def install
