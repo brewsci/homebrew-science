ff3110e865524e4c8034331c79cf389facd22443
Ian Lancaster <ilancaster@gmail.com>
2014-11-14 18:59:09 -0500

    infernal: update check option

--- infernal.rb
+++ infernal.rb
@@ -1,20 +1,22 @@
-require 'formula'
+require "formula"
 
 class Infernal < Formula
-  homepage 'http://infernal.janelia.org/'
+  homepage "http://infernal.janelia.org/"
   #doi "10.1093/bioinformatics/btp157"
   #tag "bioinformatics"
 
-  url 'http://selab.janelia.org/software/infernal/infernal-1.1.1.tar.gz'
-  sha1 'b83fbc3a50c49ad51e40b37195aeacec94897d30'
+  url "http://selab.janelia.org/software/infernal/infernal-1.1.1.tar.gz"
+  sha1 "b83fbc3a50c49ad51e40b37195aeacec94897d30"
 
-  option 'check', 'Run the test suite (`make check`). Takes a couple of minutes.'
+  deprecated_option "check" => "with-check"
+
+  option "with-check", "Run the test suite (`make check`). Takes a couple of minutes."
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make check" if build.include? 'check'
-    system "make install"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 
   test do
