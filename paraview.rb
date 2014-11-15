5628a00ea0f328267c366abe260021a6ffa45e07
Ian Lancaster <ilancaster@gmail.com>
2014-11-14 19:23:41 -0500

    paraview: general cleanup

--- paraview.rb
+++ paraview.rb
@@ -1,26 +1,24 @@
-require 'formula'
+require "formula"
 
 class Paraview < Formula
-  homepage 'http://paraview.org'
-  url 'http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.2&type=source&os=all&downloadFile=ParaView-v4.2.0-source.tar.gz'
-  sha1 'a440ba9912549bdd23a949e22add41696715dd32'
-  head 'git://paraview.org/ParaView.git'
+  homepage "http://paraview.org"
+  url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.2&type=source&os=all&downloadFile=ParaView-v4.2.0-source.tar.gz"
+  sha1 "a440ba9912549bdd23a949e22add41696715dd32"
+  head "git://paraview.org/ParaView.git"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
-  depends_on 'boost' => :recommended
-  depends_on 'cgns' => :recommended
-  depends_on 'ffmpeg' => :recommended
-  depends_on 'qt' => :recommended
+  depends_on "boost" => :recommended
+  depends_on "cgns" => :recommended
+  depends_on "ffmpeg" => :recommended
+  depends_on "qt" => :recommended
   depends_on :mpi => [:cc, :cxx, :optional]
   depends_on :python => :recommended
 
-  depends_on 'matplotlib' => [:python, :optional]
-
-  depends_on 'freetype'
-  depends_on 'hdf5'
-  depends_on 'jpeg'
-  depends_on 'libtiff'
+  depends_on "freetype"
+  depends_on "hdf5"
+  depends_on "jpeg"
+  depends_on "libtiff"
   depends_on :fontconfig
   depends_on :libpng
 
@@ -40,28 +38,29 @@ class Paraview < Formula
       -DVTK_USE_SYSTEM_ZLIB:BOOL=ON
     ]
 
-    args << "-DPARAVIEW_BUILD_QT_GUI:BOOL=OFF" if build.without? 'qt'
-    args << '-DPARAVIEW_USE_MPI:BOOL=ON' if build.with? :mpi
-    args << '-DPARAVIEW_ENABLE_FFMPEG:BOOL=ON' if build.with? 'ffmpeg'
-    args << '-DPARAVIEW_USE_VISITBRIDGE:BOOL=ON' if build.with? 'boost'
-    args << '-DVISIT_BUILD_READER_CGNS:BOOL=ON' if build.with? 'cgns'
-    mkdir 'build' do
-      if build.with? 'python'
-        args << '-DPARAVIEW_ENABLE_PYTHON:BOOL=ON'
-        # CMake picks up the system's python dylib, even if we have a brewed one.
+    args << "-DPARAVIEW_BUILD_QT_GUI:BOOL=OFF" if build.without? "qt"
+    args << "-DPARAVIEW_USE_MPI:BOOL=ON" if build.with? "mpi"
+    args << "-DPARAVIEW_ENABLE_FFMPEG:BOOL=ON" if build.with? "ffmpeg"
+    args << "-DPARAVIEW_USE_VISITBRIDGE:BOOL=ON" if build.with? "boost"
+    args << "-DVISIT_BUILD_READER_CGNS:BOOL=ON" if build.with? "cgns"
+
+    mkdir "build" do
+      if build.with? "python"
+        args << "-DPARAVIEW_ENABLE_PYTHON:BOOL=ON"
+        # CMake picks up the system"s python dylib, even if we have a brewed one.
         args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
       else
-        args << '-DPARAVIEW_ENABLE_PYTHON:BOOL=OFF'
+        args << "-DPARAVIEW_ENABLE_PYTHON:BOOL=OFF"
       end
-      args << '..'
+      args << ".."
 
-      system 'cmake', *args
-      system 'make'
-      system 'make', 'install'
+      system "cmake", *args
+      system "make"
+      system "make", "install"
     end
   end
 
   test do
-    system '#{prefix}/paraview.app/Contents/MacOS/paraview --version'
+    system "#{prefix}/paraview.app/Contents/MacOS/paraview", "--version"
   end
 end
