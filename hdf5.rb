d646803b66dadf4d86db1df67523c3776e8e5d51
Ian Lancaster <ilancaster@gmail.com>
2014-11-14 23:35:58 -0500

    hdf5: update options
    Closes #1403.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- hdf5.rb
+++ hdf5.rb
@@ -3,21 +3,24 @@ require "formula"
 class Hdf5 < Formula
   homepage "http://www.hdfgroup.org/HDF5"
   url "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.13/src/hdf5-1.8.13.tar.bz2"
-  mirror "https://distfiles.macports.org/hdf5-18/hdf5-1.8.13.tar.bz2"
+  mirror "http://www.mirrorservice.org/sites/distfiles.macports.org/hdf5/hdf5-1.8.13.tar.bz2"
   sha1 "712955025f03db808f000d8f4976b8df0c0d37b5"
 
-  # TODO - warn that these options conflict
+  deprecated_option "enable-fortran" => "with-fortran"
+  deprecated_option "enable-threadsafe" => "with-threadsafe"
+  deprecated_option "enable-parallel" => "with-mpi"
+  deprecated_option "enable-fortran2003" => "with-fortran2003"
+  deprecated_option "enable-cxx" => "with-cxx"
+
   option :universal
-  option "enable-fortran", "Compile Fortran bindings"
-  option "enable-threadsafe", "Trade performance and C++ or Fortran support for thread safety"
-  option "enable-parallel", "Compile parallel bindings"
-  option "enable-fortran2003", "Compile Fortran 2003 bindings. Requires enable-fortran"
-  option "enable-cxx", "Compile C++ bindings"
+  option "with-threadsafe", "Trade performance and C++ or Fortran support for thread safety"
+  option "with-fortran2003", "Compile Fortran 2003 bindings. Requires with-fortran"
+  option "with-cxx", "Compile C++ bindings"
   option :cxx11
 
-  depends_on :fortran if build.include? "enable-fortran" or build.include? "enable-fortran2003"
+  depends_on :fortran => :optional
   depends_on "szip"
-  depends_on :mpi => [:cc, :cxx, :f90] if build.include? "enable-parallel"
+  depends_on :mpi => [:optional, :cc, :cxx, :f90]
 
   def install
     ENV.universal_binary if build.universal?
@@ -34,23 +37,23 @@ class Hdf5 < Formula
       --enable-shared=yes
     ]
 
-    args << "--enable-parallel" if build.include? "enable-parallel"
+    args << "--enable-parallel" if build.with? "mpi"
 
-    if build.include? "enable-threadsafe"
-      raise "--enable-threadsafe conflicts with Fortran bindings" if build.include? "enable-fortran"
-      raise "--enable-threadsafe conflicts with C++ support" if build.cxx11? or build.include? "enable-cxx"
+    if build.with? "threadsafe"
+      raise "--enable-threadsafe conflicts with Fortran bindings" if build.with? "fortran"
+      raise "--enable-threadsafe conflicts with C++ support" if build.cxx11? or build.with? "cxx"
       args.concat %w[--with-pthread=/usr --enable-threadsafe]
     else
       ENV.cxx11 if build.cxx11?
-      args << "--enable-cxx" if build.cxx11? or build.include? "enable-cxx"
+      args << "--enable-cxx" if build.cxx11? or build.with? "cxx"
 
-      if build.include? "enable-fortran" or build.include? "enable-fortran2003"
+      if build.with? "fortran"
         args << "--enable-fortran"
-        args << "--enable-fortran2003" if build.include? "enable-fortran2003"
+        args << "--enable-fortran2003" if build.with? "fortran2003"
       end
     end
 
-    if build.include? "enable-parallel"
+    if build.with? "mpi"
       ENV["CC"] = "mpicc"
       ENV["FC"] = "mpif90"
     end
