31751bcf8a3c206a3e35794fa0c86498126eddc6
Konrad Rieck <konrad@mlsec.org>
2014-11-18 13:59:34 +0100

    Sally: Add support for head
    Closes #1368.

--- sally.rb
+++ sally.rb
@@ -5,11 +5,20 @@ class Sally < Formula
   url "http://www.mlsec.org/sally/files/sally-0.9.1.tar.gz"
   sha1 "8fb44d56e5c1112512c2d467b5fa76a2951af31a"
 
+  head do
+    url "https://github.com/rieck/sally.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libconfig"
   depends_on "libarchive" => :recommended
 
   def install
+    system "./bootstrap" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
