80d8ae8ca6823509fd5d88d55271eb6a7d6e0d6b
Ian Lancaster <ilancaster@gmail.com>
2014-11-20 20:47:56 -0500

    nexusformat: add make check
    Closes #1379.

--- nexusformat.rb
+++ nexusformat.rb
@@ -5,15 +5,16 @@ class Nexusformat < Formula
   url "https://github.com/nexusformat/code/archive/4.3.3.tar.gz"
   sha1 "89f1e7ec5706868f9a488a44aeb2305587ab5180"
 
-  option:cxx11
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'libmxml'
-  depends_on 'readline' => :recommended
-  depends_on 'hdf5' => (build.cxx11?) ? ["c++11"] : :build
-  depends_on 'homebrew/versions/hdf4' => :recommended
-  depends_on 'doxygen' => :optional
+  option :cxx11
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "libmxml"
+  depends_on "readline" => :recommended
+  depends_on "hdf5" => (build.cxx11? ? "c++11" : :build)
+  depends_on "homebrew/versions/hdf4" => :recommended
+  depends_on "doxygen" => :optional
 
   def install
     args = %W[
@@ -21,11 +22,13 @@ class Nexusformat < Formula
       --disable-silent-rules
       --disable-dependency-tracking
       --disable-debug
-      --with-hdf4=#{Formula['homebrew/versions/hdf4'].opt_prefix}
+      --with-hdf4=#{Formula["homebrew/versions/hdf4"].opt_prefix}
     ]
     system "/bin/sh", "autogen.sh"
     ENV.cxx11 if build.cxx11?
     system "./configure", *args
+    system "make"
+    system "make", "check"
     system "make", "install"
   end
 
