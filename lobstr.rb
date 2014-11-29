b82a46c40ed607f8ea915fd7860d2f0d33fce8c0
A. Gordon <assafgordon@gmail.com>
2014-11-28 22:41:12 -0500

    lobstr 3.0.3 (new formula)
    lobSTR: A short tandem repeat profiler for personal genomes
    http://genome.cshlp.org/content/early/2012/04/19/gr.135780.111.abstract
    
    Closes #1424.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

---
+++ lobstr.rb
@@ -0,0 +1,43 @@
+require "formula"
+
+class Lobstr < Formula
+  homepage "http://lobstr.teamerlich.org"
+  url "http://erlichlab.wi.mit.edu/lobSTR/lobSTR-3.0.3.tar.gz"
+  sha1 "1f1578e9d1c36eb7268037c977204152f5022361"
+
+  option "without-check", "Disable build-time checking (not recommended)"
+
+  head do
+    url "https://github.com/mgymrek/lobstr-code.git"
+    depends_on :autoconf => :build
+    depends_on :automake => :build
+  end
+
+  depends_on :libtool => :build
+  depends_on "pkg-config" => :build
+  depends_on "cppunit" => :build
+  depends_on "gsl"
+  depends_on "boost"
+
+  def install
+    system "sh", "./reconf" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+
+  test do
+    mktemp do
+      system "#{bin}/lobSTR",
+               "--verbose",
+               "--index-prefix", "#{share}/lobSTR/test-ref/lobSTR_",
+               "--fastq", "-f", "#{share}/lobSTR/sample/tiny.fq",
+               "--rg-sample", "test",
+               "--rg-lib", "test",
+               "--out", "./test"
+      assert $?.success?
+    end
+  end
+end
