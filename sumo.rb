b024e6d8d028531a78ccc7454ba6e5dc9ee46cee
alexvoronov <alexey.voronov@gmail.com>
2014-12-01 22:48:15 +0100

    sumo 0.22.0 (new formula)
    A formula for Simulation of Urban MObility (SUMO).
    
    The formula has a hand-made test, gtest tests, and TextTest tests. About
    1% of TextTest tests were failing on my machine, but as far as I can
    tell, SUMO works fine anyway. TextTest tests are disabled by default.
    
    (This commit is a squash of six previous commits).
    
    Closes #1412.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

---
+++ sumo.rb
@@ -0,0 +1,108 @@
+class Sumo < Formula
+  homepage "https://sourceforge.net/projects/sumo/"
+  url "https://downloads.sourceforge.net/project/sumo/sumo/version%200.22.0/sumo-all-0.22.0.tar.gz"
+  sha1 "32206adaccb35e41afb697bb3c841a5578e8f167"
+
+  option "with-check", "Enable additional build-time checking"
+
+  depends_on :x11
+  depends_on "xerces-c"
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "libtiff"
+  depends_on "proj"
+  depends_on "gdal"
+  depends_on "fox"
+  depends_on :python
+
+  resource "gtest" do
+    url "http://googletest.googlecode.com/files/gtest-1.7.0.zip"
+    sha1 "f85f6d2481e2c6c4a18539e391aa4ea8ab0394af"
+  end
+
+  resource "TextTest" do
+    url "https://pypi.python.org/packages/source/T/TextTest/TextTest-3.27.tar.gz"
+    sha1 "065495abe3d0b19161ef81f110c82ad4d09efe39"
+  end
+
+  def install
+    resource("gtest").stage do
+      system "./configure"
+      system "make"
+      buildpath.install "../gtest-1.7.0"
+    end
+
+    ENV["LDFLAGS"] = "-lpython"  # My compilation fails without this flag, despite :python dependency.
+    ENV.append_to_cflags "-I#{buildpath}/gtest-1.7.0/include"
+
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-python",
+                          "--with-gtest-config=gtest-1.7.0/scripts/gtest-config"
+
+    system "make", "install"
+
+    # Basic tests, they are fast, so execute them always.
+    system "unittest/src/sumo-unittest"
+
+    # Additional tests. These take more time, and some fail on my machine...
+    if build.with? "check"
+      ENV.prepend_create_path "PYTHONPATH", buildpath/"vendor/lib/python2.7/site-packages"
+      resource("TextTest").stage { Language::Python.setup_install "python", buildpath/"vendor" }
+      ENV.prepend_create_path "PATH", buildpath/"vendor/bin"
+      system "tests/runTests.sh", "-l", "-zen", "-b", "homebrew-compilation-check"
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    Some SUMO scripts require SUMO_HOME environmental variable:
+      export SUMO_HOME=#{prefix}
+    EOS
+  end
+
+  test do
+    # A simple hand-made test to see if sumo compiled and linked well.
+
+    (testpath/"hello.nod.xml").write <<-EOS.undent
+      <nodes>
+          <node id="1" x="-250.0" y="0.0" />
+          <node id="2" x="+250.0" y="0.0" />
+          <node id="3" x="+251.0" y="0.0" />
+      </nodes>
+    EOS
+
+    (testpath/"hello.edg.xml").write <<-EOS.undent
+      <edges>
+          <edge from="1" id="1to2" to="2" />
+          <edge from="2" id="out" to="3" />
+      </edges>
+    EOS
+
+    system "#{bin}/netconvert", "--node-files=#{testpath}/hello.nod.xml", "--edge-files=#{testpath}/hello.edg.xml", "--output-file=#{testpath}/hello.net.xml"
+
+    (testpath/"hello.rou.xml").write <<-EOS.undent
+      <routes>
+          <vType accel="1.0" decel="5.0" id="Car" length="2.0" maxSpeed="100.0" sigma="0.0" />
+          <route id="route0" edges="1to2 out"/>
+          <vehicle depart="1" id="veh0" route="route0" type="Car" />
+      </routes>
+    EOS
+
+    (testpath/"hello.sumocfg").write <<-EOS.undent
+      <configuration>
+          <input>
+              <net-file value="hello.net.xml"/>
+              <route-files value="hello.rou.xml"/>
+          </input>
+          <time>
+              <begin value="0"/>
+              <end value="10000"/>
+          </time>
+      </configuration>
+    EOS
+
+    system "#{bin}/sumo", "-c", "hello.sumocfg"
+  end
+end
