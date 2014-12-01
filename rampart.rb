f2274f2a3cc828ccb9788b78841fd341846d2ccb
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2014-12-01 11:51:47 +0000

    rampart 0.10.3
    - added bioinformatics tag under head
    - added some new dependencies
    - made most non-essential dependencies optional
    - Making quast optional on mountain lion in order to avoid lack of matplotlib
      on this platform.
    - Moutain lion issue for jellyfish 2 (via quake).  Making quake optional for
      mountain lion.
    
    Closes #1376.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- rampart.rb
+++ rampart.rb
@@ -2,38 +2,58 @@ require "formula"
 
 class Rampart < Formula
   homepage "https://github.com/TGAC/RAMPART"
+  #tag "bioinformatics"
 
   head do
     url "https://github.com/TGAC/RAMPART.git", :branch => "develop"
     depends_on "maven" => :build
   end
 
-  url "https://github.com/TGAC/RAMPART/releases/download/Release-0.9.0/rampart-0.9.0.tar.gz"
-  sha1 "b9a4e4ff59f92087f826230750ab94e3f7ffbbc9"
+  url "https://github.com/TGAC/RAMPART/releases/download/Release-0.10.3/rampart-0.10.3.tar.gz"
+  sha1 "8f8c5db27e8088fbf03fc08222bfead6cf96fb11"
 
   depends_on :java => "1.7"
 
   # Dataset improvement
-  depends_on "musket" => :recommended
-  depends_on "quake" => :recommended
-  depends_on "sickle" => :recommended
+  depends_on "musket" => :optional
+  depends_on "sickle" => :optional
+  # quake (see below)
+
+  # Kmer optimisation
+  depends_on "kmergenie" => :recommended
 
   # Assemblers
   depends_on "abyss" => :recommended
-  depends_on "allpaths-lg" => :recommended
-  depends_on "platanus" => :recommended
-  depends_on "soapdenovo" => :recommended
+  depends_on "allpaths-lg" => :optional
+  depends_on "platanus" => :optional
+  depends_on "soapdenovo" => :optional
   depends_on "velvet" => :recommended
+  depends_on "spades" => :optional
 
   # Assembly improvement
   # No formula: depends_on "sspace" => :recommended
   # SOAP scaffolder and gap closer
   # Platanus scaffolder and gap closer
+  depends_on "reapr" => :optional
 
   # Assembly analysis
-  depends_on "cegma" => :recommended
+  depends_on "cegma" => :optional
   depends_on "kat" => :recommended
-  depends_on "quast" => :recommended
+  # quast (see below)
+
+  # Quast and quake are now optional on mountain lion but recommended on
+  # all other platforms.  For Quast, this is because mountian lion does not
+  # seem to have matplotlib installed by default, which is a key dependency.
+  # For Quake, it has a dependency on jellyfish2, which doesn't seem to be
+  # configured correctly to use the right compiler (C++) at this point in
+  # time (4th Dec 2014).  We may remove this check in the future.
+  if OS.mac? && MacOS.version <= :mountain_lion
+    depends_on "quake" => :optional
+    depends_on "quast" => :optional
+  else
+    depends_on "quake" => :recommended
+    depends_on "quast" => :recommended
+  end
 
   def install
     if build.head?
