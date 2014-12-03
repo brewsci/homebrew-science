655446fd691d2e7e55d3f10db47c19bf7272aa33
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-03 08:46:17 +0000

    paml: add 4.8a bottle.

--- paml.rb
+++ paml.rb
@@ -6,6 +6,14 @@ class Paml < Formula
   sha1 "6160f441577a2bb82afd793e7691c42bd5491376"
   version "4.8a"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "ce419b8e5908fde0054ab5869534fde7a4e346c1" => :yosemite
+    sha1 "219f8dfb87f93093fad1ab8f28a04d6938f182cd" => :mavericks
+    sha1 "37142a7141b15cf2af6871525cc5491ab42100f6" => :mountain_lion
+  end
+
   def install
     cd "src" do
       system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
