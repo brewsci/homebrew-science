b1dd0b299cb5a7ac91f19489e822b0cd24d5ba6c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-03 23:20:52 +0000

    fasta: add 36.3.7 bottle.

--- fasta.rb
+++ fasta.rb
@@ -8,6 +8,14 @@ class Fasta < Formula
   url "http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.7.tar.gz"
   sha1 "184acbac526ebdb3cad0009d87bd2a68b4756a03"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "752acd5b851cb56d4d42468a3e0257aa8771a5a3" => :yosemite
+    sha1 "ef93a8f05f798a36f04cdd5813668afab612bbff" => :mavericks
+    sha1 "00b921910231d3112263e2fb9749d7e8b4fee7fa" => :mountain_lion
+  end
+
   def install
     mkdir "bin"
     cd "src" do
