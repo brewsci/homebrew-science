41c7789e0c05bf0614713296b3aba6a8537ea438
Ian Lancaster <ilancaster@gmail.com>
2014-12-06 00:19:43 -0500

    kraken: simplify fails_with clang

--- kraken.rb
+++ kraken.rb
@@ -5,25 +5,16 @@ class Kraken < Formula
   url "http://ccb.jhu.edu/software/kraken/dl/kraken-0.10.3-beta.tgz"
   sha1 "64f88004c341871d883235f4ae7c876c0136b885"
 
-  fails_with :clang do
-    build 600
-    cause "error: 'omp.h' file not found"
-  end
-
-  fails_with :llvm do
-    build 500
-    cause "error: 'omp.h' file not found"
-  end
+  needs :openmp
 
   def install
     libexec.mkdir
-    system './install_kraken.sh', libexec
-    bin.install_symlink(['kraken', 'kraken-build', 'kraken-report'].map do
-      |x| '../libexec/' + x
-    end)
+    system "./install_kraken.sh", libexec
+    libexec_bins = ["kraken", "kraken-build", "kraken-report"].map { |x| libexec + x }
+    bin.install_symlink(libexec_bins)
   end
 
   test do
-    system 'kraken --version'
+    system "kraken", "--version"
   end
 end
