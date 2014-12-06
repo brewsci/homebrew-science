56b249db41c707287a78baf58ca4c6146a2e66b1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-06 00:06:43 +0000

    emboss: add 6.6.0 bottle.

--- emboss.rb
+++ emboss.rb
@@ -7,6 +7,13 @@ class Emboss < Formula
   mirror "https://science-annex.org/pub/emboss/EMBOSS-6.6.0.tar.gz"
   sha1 "93749ebd0a777efd3749974d2401c3a2a013a3fe"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "cd15996f4eee38f64226a8293c55bce46f02774b" => :yosemite
+    sha1 "7e2fb841a987b596f436ae3dd1354b48c7c0e425" => :mavericks
+    sha1 "286d090099f6bed5e63f29a5b69e6f3340adaeff" => :mountain_lion
+  end
+
   option "with-embossupdate", "Run embossupdate after `make install`"
 
   depends_on "pkg-config" => :build
