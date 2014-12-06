f59d1057410c899d53637e5bcb6d74f94acb4e7e
Ian Lancaster <ilancaster@gmail.com>
2014-12-05 19:17:28 -0500

    armadillo 4.550.2
    Closes #1431.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- armadillo.rb
+++ armadillo.rb
@@ -1,21 +1,37 @@
-require 'formula'
+require "formula"
 
 class Armadillo < Formula
-  homepage 'http://arma.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/arma/armadillo-4.500.0.tar.gz'
-  sha1 '4747a6fb4350f5adabfb71f3c0031d5ae44b38d3'
+  homepage "http://arma.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/arma/armadillo-4.550.2.tar.gz"
+  sha1 "61b786f982caa4bbccc717e288ca0fea4e8d35d2"
 
-  depends_on 'cmake' => :build
-  depends_on 'arpack'
+  depends_on "cmake" => :build
+  depends_on "arpack"
 
   option :cxx11
 
   def install
     ENV.cxx11 if build.cxx11?
     system "cmake", ".", *std_cmake_args
-    system "make install"
+    system "make", "install"
 
     # Copy examples/ directory to prefix
     prefix.install "examples"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS
+      #include <iostream>
+      #include <armadillo>
+      using namespace std;
+      using namespace arma;
+
+      int main(int argc, char** argv)
+        {
+        cout << arma_version::as_string() << endl;
+        }
+    EOS
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-larmadillo", "-o", "test"
+    assert `./test`.include?(version)
   end
 end
