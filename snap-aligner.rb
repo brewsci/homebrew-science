701b063e41f9955006de85a876a8052853697c14
Brian Osborne <briano@bioteam.net>
2014-12-07 15:35:18 -0500

    snap-aligner 0.15: New formula
    SNAP is a new sequence aligner that is 3-20x faster and just as
    accurate as existing tools like BWA-mem, Bowtie2 and Novoalign.
    
    Closes #1567.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ snap-aligner.rb
@@ -0,0 +1,19 @@
+class SnapAligner < Formula
+  homepage "http://snap.cs.berkeley.edu"
+  #doi "arXiv:1111.5572v1"
+  #tag "bioinformatics"
+
+  url "https://github.com/amplab/snap/archive/v0.15.tar.gz"
+  sha1 "f104f35f4209554a43ccfc8c353bd71213791a64"
+  head "https://github.com/amplab/snap.git"
+
+  def install
+    system "make"
+    bin.install "snap"
+    doc.install "LICENSE", "README.md"
+  end
+
+  test do
+    system "#{bin}/snap |grep SNAP"
+  end
+end
