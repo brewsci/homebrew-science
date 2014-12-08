4354c264b19030d4601faa076873c7d88c3c1159
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-08 03:14:01 +0000

    field3d: add 1.4.3_1 bottle.

--- field3d.rb
+++ field3d.rb
@@ -6,6 +6,14 @@ class Field3d < Formula
   sha1 '2a6d2bc535cd6c89cbb9dd2fe24734bd83dd9426'
   revision 1
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "a8b12023df85cce80486922f6f1dc799193a73c0" => :yosemite
+    sha1 "cda2b036a3a36d3000406943f1f708d0cf71fd0a" => :mavericks
+    sha1 "9e7854c38d8af4a2f07f21b931d2751a533813e9" => :mountain_lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'boost'
   depends_on 'ilmbase'
