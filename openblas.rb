3273ee1ff81ddba175c6c1109dd2f7f48e3ac388
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 03:50:19 +0000

    openblas: add 0.2.12_1 bottle.

--- openblas.rb
+++ openblas.rb
@@ -7,6 +7,13 @@ class Openblas < Formula
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
   revision 1
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "76c513443a817460021884a75eb53cacb9c75c53" => :yosemite
+    sha1 "301cfa90190768a5ff93bae40bf9e3704da6f460" => :mavericks
+    sha1 "c1a879f01180bda6727b7817e3123b1849acc327" => :mountain_lion
+  end
+
   depends_on :fortran
 
   # OS X provides the Accelerate.framework, which is a BLAS/LAPACK impl.
