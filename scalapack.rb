ea9f8f04f060dc2bf2e618c68fccdefc1e6a05f2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 03:50:19 +0000

    scalapack: add 2.0.2_1 bottle.

--- scalapack.rb
+++ scalapack.rb
@@ -7,6 +7,13 @@ class Scalapack < Formula
   head 'https://icl.cs.utk.edu/svn/scalapack-dev/scalapack/trunk', :using => :svn
   revision 1
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "f93023fc4796abf28a9f4a9c79551569dbdf31e2" => :yosemite
+    sha1 "94fdfa3445c1fc301f8649478aea02576d194865" => :mavericks
+    sha1 "82ab5e50ba36891716c8c9fe2605082fcdc3dc78" => :mountain_lion
+  end
+
   option 'with-shared-libs', 'Build shared libs (some tests may fail)'
   option 'without-check', 'Skip build-time tests (not recommended)'
 
