d5e1948f2db3cffa15106bf9e7c9e1af89979868
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 03:50:19 +0000

    tisean: add 3.0.1_1 bottle.

--- tisean.rb
+++ tisean.rb
@@ -6,6 +6,14 @@ class Tisean < Formula
   sha1 "7fe71899b063abe1b3d9aae88f153988495d623b"
   revision 1
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "79737ddd009613dd7cef302fd521b8a21ea47e59" => :yosemite
+    sha1 "f84e675c6e18d978d7c4aee8f3f1b9f06b9406ed" => :mavericks
+    sha1 "287c055e7d1a40b1db6af19f18e047e8db35a82c" => :mountain_lion
+  end
+
   option "without-prefixed-binaries", "Do not prefix binaries with `tisean-`"
 
   depends_on :fortran
