7c2eb4929a3ab61f35b8b6d3e1d21e7bc30405bc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 03:50:19 +0000

    qrupdate: add 1.1.2_1 bottle.

--- qrupdate.rb
+++ qrupdate.rb
@@ -6,6 +6,14 @@ class Qrupdate < Formula
   sha1 'f7403b646ace20f4a2b080b4933a1e9152fac526'
   revision 1
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "e8752934d8eac5396f0e763434eb6318f2e93265" => :yosemite
+    sha1 "d597026b125fb2d72148f230dbf969c1d5d0ae74" => :mavericks
+    sha1 "fa3ca20a2f04998118df68c9e02d52e2e07742b9" => :mountain_lion
+  end
+
   option "without-check", "Skip build-time tests (not recommended)"
 
   depends_on :fortran
