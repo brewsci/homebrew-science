5f65b30b836610847ecb87bb2311185d7f5a03fa
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-16 08:15:45 +0000

    mhap: add 0.1 bottle.

--- mhap.rb
+++ mhap.rb
@@ -3,6 +3,14 @@ require "formula"
 class Mhap < Formula
   homepage "https://github.com/marbl/MHAP"
   head "https://github.com/marbl/MHAP.git"
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "effd7e5aa3d11957c679f2049f93849d5b6d9d8f" => :yosemite
+    sha1 "fb2198b8e91a6d7ba99ec84ecda4e4cf7329a91c" => :mavericks
+    sha1 "af6b3d2dc212fc2e4a3ba0c4a81d1a0f7903593b" => :mountain_lion
+  end
+
   #doi "10.1101/008003" => "bioRxiv"
   #tag "bioinformatics"
 
