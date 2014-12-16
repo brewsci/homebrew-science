7b693c6d9704d6bf2f2e8eb68c564b1246161e6a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-16 19:51:04 +0000

    cuba: add 4.1 bottle.

--- cuba.rb
+++ cuba.rb
@@ -5,6 +5,14 @@ class Cuba < Formula
   url "http://www.feynarts.de/cuba/Cuba-4.1.tar.gz"
   sha1 "2415fb0d29e06a9b86eadb7750fe79bc77dd44c0"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "ab45cb4ad7dee2a0ef6e6fb95b515dbee2ea8f6e" => :yosemite
+    sha1 "0f2d8087ebe59c0d345b4aafa457023419e1656a" => :mavericks
+    sha1 "3ff524e9b690162decc9dd89e163a42ca035f684" => :mountain_lion
+  end
+
   option "without-check", "Skip build-time tests (not recommended)"
 
   def install
