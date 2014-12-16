c0034c59224bc739aa8d63328e9b0ba1c8f30ef2
Steven Hahn <hahnse@ornl.gov>
2014-12-16 14:40:35 -0500

    cuba 4.1
    Closes #1478.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- cuba.rb
+++ cuba.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Cuba < Formula
   homepage "http://www.feynarts.de/cuba"
-  url "http://www.feynarts.de/cuba/Cuba-4.0.tar.gz"
-  sha1 "002e6c5decf5bc95e2a75265938f28bc8939c56b"
+  url "http://www.feynarts.de/cuba/Cuba-4.1.tar.gz"
+  sha1 "2415fb0d29e06a9b86eadb7750fe79bc77dd44c0"
 
   option "without-check", "Skip build-time tests (not recommended)"
 
