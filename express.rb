0ce4b7d35ace184184effb65a4b0b9f41ab768a9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-17 23:06:05 +0000

    express: add 1.5.1 bottle.

--- express.rb
+++ express.rb
@@ -1,6 +1,14 @@
 class Express < Formula
   homepage "http://bio.math.berkeley.edu/eXpress/"
   head "https://github.com/adarob/eXpress.git"
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "1ab122db4fa3af3e8e9d93e49702c1a1aa75a249" => :yosemite
+    sha1 "3e17818affce1700e88a1d0934be13d76e4241ae" => :mavericks
+    sha1 "6d648767ea1dd5fc3eb45ded0389b8c9eb4ad8b3" => :mountain_lion
+  end
+
   #doi "10.1038/nmeth.2251"
   #tag "bioinformatics"
 
