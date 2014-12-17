3b023681f15caa4ebfee46c7c7467015c20f0216
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-17 14:18:24 +0000

    geant: add 4.10.01 bottle.

--- geant.rb
+++ geant.rb
@@ -5,6 +5,13 @@ class Geant < Formula
   url 'http://geant4.cern.ch/support/source/geant4.10.01.tar.gz'
   sha1 'd888b992b789a0d0e0a1a13debc9a51dae5e3743'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "4cbef3a10978ba3cbbd869415359291369cb4219" => :yosemite
+    sha1 "0024b1c66a2a725f051d040212549ab040535fae" => :mavericks
+    sha1 "b3f7a2bff728e0f54b6186ef92ce6837253c3d45" => :mountain_lion
+  end
+
   option 'with-g3tog4', 'Use G3toG4 Library'
   option 'with-gdml', 'Use GDML'
   option 'with-notimeout', 'Set notimeout in installing data'
