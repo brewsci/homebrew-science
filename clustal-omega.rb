f36fcab88929c835aacc8979a38969d3eccc46c1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-20 06:51:26 +0000

    clustal-omega: add 1.2.0 bottle.

--- clustal-omega.rb
+++ clustal-omega.rb
@@ -5,6 +5,14 @@ class ClustalOmega < Formula
   url "http://www.clustal.org/omega/clustal-omega-1.2.0.tar.gz"
   sha1 "3416bdc81328fa955a500aaf6c3a77414c8e0c2b"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "542ab8b7114dcc9ecbe18ff30241636f3ce6119f" => :yosemite
+    sha1 "ef3fb88b983cf5513f78db6973bbb9d47f2b4d61" => :mavericks
+    sha1 "3f39c602cfa65c5e9c886417eca892af52fc4246" => :mountain_lion
+  end
+
   depends_on "argtable"
 
   def install
