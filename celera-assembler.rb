97cf5908ca4f4a87bb7bcda8071f215aa5a0e5e7
Ian Lancaster <ilancaster@gmail.com>
2014-12-20 11:34:17 -0500

    celera-assembler 8.2
    Closes #1353.
    
    Closes #1502.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- celera-assembler.rb
+++ celera-assembler.rb
@@ -1,25 +1,20 @@
-require 'formula'
-
 class CeleraAssembler < Formula
-  homepage 'http://wgs-assembler.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/wgs-assembler/wgs-assembler/wgs-8.1/wgs-8.1.tar.bz2'
-  sha1 '76f38c869b4876b414794b59e90d4f36c3e13488'
+  homepage "http://wgs-assembler.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/wgs-assembler/wgs-assembler/wgs-8.2/wgs-8.2.tar.bz2"
+  sha1 "a3c299e145bcdd1492bab4a677a445a308509e57"
 
-  # Doesn't build, because the 'kmer' directory is missing.
-  #head 'https://svn.code.sf.net/p/wgs-assembler/svn/trunk'
+  depends_on "samtools-0.1"
 
-  fails_with :clang do
-    build 503
-    cause "error: use of undeclared identifier 'use_safe_malloc_instead'"
-  end
+  # Fails with clang: https://sourceforge.net/p/wgs-assembler/bugs/262/
+  needs :openmp
 
   def install
     ENV.j1
-    system *%w[make -C kmer install]
-    system *%w[make -C src]
-    arch = Pathname.new(Dir['*/bin'][0]).dirname
+    system "make", "-C", "kmer", "install", "CC=#{ENV.cc}"
+    system "make", "-C", "src", "CC=#{ENV.cc}", "LDLIBS=-rdynamic"
+    arch = Pathname.new(Dir["*/bin"][0]).dirname
     libexec.install arch
-    bin.install_symlink libexec / arch / 'bin/runCA'
+    bin.install_symlink libexec/arch/"bin/runCA"
   end
 
   test do
