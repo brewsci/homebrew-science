94a5da3c58f85dc1c89897d2bc88db73344480b5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-20 07:02:04 +0000

    clustal-w: add 2.1 bottle.

--- clustal-w.rb
+++ clustal-w.rb
@@ -5,6 +5,14 @@ class ClustalW < Formula
   url "http://www.clustal.org/download/2.1/clustalw-2.1.tar.gz"
   sha1 "f29784f68585544baa77cbeca6392e533d4cf433"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "60bf60642a075b54773032eca33594f01cd92a4c" => :yosemite
+    sha1 "38c7402db59638e80c44384c94106266bdb427e5" => :mavericks
+    sha1 "42f9faaf5f18d12dc7ac0aa269c36b580dbdfd24" => :mountain_lion
+  end
+
   fails_with :clang do
     build 600
     cause "error: implicit instantiation of undefined template"
