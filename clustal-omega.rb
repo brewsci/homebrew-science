d834cd8d1cd207bcf3be263d2e09eaa7fb918389
Ian Lancaster <ilancaster@gmail.com>
2014-12-20 01:46:11 -0500

    clustal-omega: style cleanup
    Closes #1495.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- clustal-omega.rb
+++ clustal-omega.rb
@@ -1,21 +1,19 @@
-require 'formula'
-
 class ClustalOmega < Formula
-  homepage 'http://www.clustal.org/omega/'
+  homepage "http://www.clustal.org/omega/"
   #tag "bioinformatics"
   #doi "10.1038/msb.2011.75"
-  url 'http://www.clustal.org/omega/clustal-omega-1.2.0.tar.gz'
-  sha1 '3416bdc81328fa955a500aaf6c3a77414c8e0c2b'
+  url "http://www.clustal.org/omega/clustal-omega-1.2.0.tar.gz"
+  sha1 "3416bdc81328fa955a500aaf6c3a77414c8e0c2b"
 
-  depends_on 'argtable'
+  depends_on "argtable"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system bin/'clustalo', '--version'
+    system "#{bin}/clustalo", "--version"
   end
 end
