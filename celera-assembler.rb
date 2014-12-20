a32eed54f0b782cfcd8ce6a3c77bd4a8ff9e40a9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-20 17:58:44 +0000

    celera-assembler: add 8.2 bottle.

--- celera-assembler.rb
+++ celera-assembler.rb
@@ -3,6 +3,13 @@ class CeleraAssembler < Formula
   url "https://downloads.sourceforge.net/project/wgs-assembler/wgs-assembler/wgs-8.2/wgs-8.2.tar.bz2"
   sha1 "a3c299e145bcdd1492bab4a677a445a308509e57"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "3a85819d72beed36990f358167bcbd0f28faea6b" => :yosemite
+    sha1 "dc1c3f744d0ce05e79fcadb6561f5b91d22bf56b" => :mavericks
+    sha1 "97a5c61310b924a03bb1d587f256f5ac35cb425c" => :mountain_lion
+  end
+
   depends_on "samtools-0.1"
 
   # Fails with clang: https://sourceforge.net/p/wgs-assembler/bugs/262/
