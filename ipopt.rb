422a98e938358f2182b13120d973c25eddd12be1
Dominique Orban <dominique.orban@gmail.com>
2014-12-20 21:50:57 -0500

    IPOPT: use sequential MUMPS.
    Closes #1505.

--- ipopt.rb
+++ ipopt.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Ipopt < Formula
-  homepage 'https://projects.coin-or.org/Ipopt'
-  url 'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.9.tgz'
-  sha1 '1bc6db565e5fb1ecbc40ff5179eab8409ba92b07'
-  head 'https://projects.coin-or.org/svn/Ipopt/trunk', :using => :svn
+  homepage "https://projects.coin-or.org/Ipopt"
+  url "http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.9.tgz"
+  sha1 "1bc6db565e5fb1ecbc40ff5179eab8409ba92b07"
+  head "https://projects.coin-or.org/svn/Ipopt/trunk", :using => :svn
   revision 1
 
   bottle do
@@ -14,36 +14,24 @@ class Ipopt < Formula
     sha1 "ee24ecfa4d2f2318a2975ecd5f596a7bb539c26b" => :mountain_lion
   end
 
-  option 'without-check', 'Skip build-time tests (not recommended)'
+  option "without-check", "Skip build-time tests (not recommended)"
 
-  depends_on 'asl' => :recommended
-  depends_on 'openblas' => :optional
-  depends_on 'pkg-config' => :build
-  depends_on 'mumps' => (build.with? 'openblas') ? ['with-openblas'] : :build
+  depends_on "asl" => :recommended
+  depends_on "openblas" => :optional
+  depends_on "pkg-config" => :build
+
+  # IPOPT is not able to use parallel MUMPS.
+  depends_on "mumps" => ["without-mpi"] + ((build.with? "openblas") ? ["with-openblas"] : [])
 
   depends_on :fortran
 
-  def mumps_options
-    Tab.for_formula(Formula["mumps"])
-  end
-
   def install
-    ENV.delete('MPICC')  # configure will pick these up and use them to link
-    ENV.delete('MPIFC')  # which leads to the linker crashing.
-    ENV.delete('MPICXX')
-    mumps_libs = %w[-ldmumps -lmumps_common -lpord]
-
-    # See whether the parallel or sequential MUMPS library was built.
-    if mumps_options.without? "mpi"
-      mumps_libs << '-lmpiseq'
-      mumps_incdir = Formula["mumps"].libexec / 'include'
-    else
-      # The MPI libs were installed as a MUMPS dependency.
-      mumps_libs += %w[-lmpi_cxx -lmpi_mpifh]
-      mumps_incdir = Formula["mumps"].opt_include
-      ENV.append_to_cflags "-DHAVE_MPI_INITIALIZED"  # Mumps initializes MPI.
-    end
-    mumps_libcmd = "-L#{Formula["mumps"].opt_lib} " + mumps_libs.join(' ')
+    ENV.delete("MPICC")  # configure will pick these up and use them to link
+    ENV.delete("MPIFC")  # which leads to the linker crashing.
+    ENV.delete("MPICXX")
+    mumps_libs = %w(-ldmumps -lmumps_common -lpord -lmpiseq)
+    mumps_incdir = Formula["mumps"].libexec / "include"
+    mumps_libcmd = "-L#{Formula['mumps'].opt_lib} " + mumps_libs.join(" ")
 
     args = ["--disable-debug",
             "--disable-dependency-tracking",
@@ -53,16 +41,16 @@ class Ipopt < Formula
             "--enable-shared",
             "--enable-static"]
 
-    if build.with? 'openblas'
-      args << "--with-blas-incdir=#{Formula["openblas"].opt_include}"
-      args << "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
-      args << "--with-lapack-incdir=#{Formula["openblas"].opt_include}"
-      args << "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas"
+    if build.with? "openblas"
+      args << "--with-blas-incdir=#{Formula['openblas'].opt_include}"
+      args << "--with-blas-lib=-L#{Formula['openblas'].opt_lib} -lopenblas"
+      args << "--with-lapack-incdir=#{Formula['openblas'].opt_include}"
+      args << "--with-lapack-lib=-L#{Formula['openblas'].opt_lib} -lopenblas"
     end
 
-    if build.with? 'asl'
-      args << "--with-asl-incdir=#{Formula["asl"].opt_include}/asl"
-      args << "--with-asl-lib=-L#{Formula["asl"].opt_lib} -lasl -lfuncadd0"
+    if build.with? "asl"
+      args << "--with-asl-incdir=#{Formula['asl'].opt_include}/asl"
+      args << "--with-asl-lib=-L#{Formula['asl'].opt_lib} -lasl -lfuncadd0"
     end
 
     system "./configure", *args
