c7554f57b4b17c1d6749bd4be974f246eddb7752
Shaun Jackman <sjackman@gmail.com>
2014-12-22 12:20:32 -0800

    novoalign 3.02.10
    Closes #1446

--- novoalign.rb
+++ novoalign.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class Novoalign < Formula
   homepage 'http://www.novocraft.com/'
-  version '3.02.02'
+  version "3.02.10"
   url 'http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz' %
     [version, if OS.mac? then 'MacOSX' else 'Linux2.6' end]
-  sha1(if OS.mac? then '97984fbf7a7d5f73e47e5a44523812ea1c2f6ee6'
-    else 'f16ce82c9d6c4e7559101f819320e7fd39352a75' end)
+  sha1(if OS.mac? then "13984b67b9cc2b00ee8ae2bbabdd57fa9e0f7d6d"
+    elsif OS.linux? then "01b238a622393f3b9162a40412a4236bbd0cd088"
+    else raise "Unknown operating system"
+    end)
 
   def install
     bin.install %w[isnovoindex novo2paf novoalign novoalignCS
