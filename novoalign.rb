ab8500e4c9d5dfe3af2464a4a35b369cba1e354d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-22 20:30:56 +0000

    novoalign: add 3.02.10 bottle.

--- novoalign.rb
+++ novoalign.rb
@@ -11,6 +11,13 @@ class Novoalign < Formula
     elsif OS.linux? then "01b238a622393f3b9162a40412a4236bbd0cd088"
     else raise "Unknown operating system"
     end)
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "def48a21b0cf4f896a35f22813096f348b0d2d1f" => :yosemite
+    sha1 "503ecf71abb71665fafcbab59a605cc564d64703" => :mavericks
+    sha1 "449919f5a2514b4936b867d701ef487bf73a5d4d" => :mountain_lion
+  end
 
   def install
     bin.install %w[isnovoindex novo2paf novoalign novoalignCS
