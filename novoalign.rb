f7a482406799aa6edf4187625693d7c4d4aca9a1
Shaun Jackman <sjackman@gmail.com>
2014-12-22 12:26:48 -0800

    novoalign 3.02.10: Change single quotes to double
    Closes #1514.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- novoalign.rb
+++ novoalign.rb
@@ -1,10 +1,12 @@
-require 'formula'
+require "formula"
 
 class Novoalign < Formula
-  homepage 'http://www.novocraft.com/'
+  homepage "http://www.novocraft.com/"
+  #tag "bioinformatics"
+
   version "3.02.10"
-  url 'http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz' %
-    [version, if OS.mac? then 'MacOSX' else 'Linux2.6' end]
+  url "http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz" %
+    [version, if OS.mac? then "MacOSX" else "Linux2.6" end]
   sha1(if OS.mac? then "13984b67b9cc2b00ee8ae2bbabdd57fa9e0f7d6d"
     elsif OS.linux? then "01b238a622393f3b9162a40412a4236bbd0cd088"
     else raise "Unknown operating system"
@@ -15,11 +17,11 @@ class Novoalign < Formula
       novoalignCSMPI novoalignMPI novobarcode novoindex novomethyl
       novope2bed.pl novorun.pl novosort novoutil]
     # Conflicts with samtools
-    #bin.install 'novo2sam.pl'
-    doc.install Dir['*.pdf', '*.txt']
+    #bin.install "novo2sam.pl"
+    doc.install Dir["*.pdf", "*.txt"]
   end
 
   test do
-    system 'novoalign --version 2>&1 |grep -q Novoalign'
+    system "#{bin}/novoalign --version 2>&1 |grep -q Novoalign"
   end
 end
