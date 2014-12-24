7a2d43809b5b46bdc14f3b895dcfab19a252d792
Shaun Jackman <sjackman@gmail.com>
2014-12-23 16:39:07 -0800

    consed 28.0
    Closes #1522.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- consed.rb
+++ consed.rb
@@ -4,13 +4,13 @@ class Consed < Formula
   homepage "http://bozeman.mbt.washington.edu/consed/consed.html"
   #doi "10.1101/gr.8.3.195" => "1998", "10.1093/bioinformatics/btt515" => "2013"
 
-  version "27.0"
+  version "28.0"
   if OS.mac?
     url "http://bozeman.mbt.washington.edu/consed/distributions/#{version}/consed_mac.tar.gz"
-    sha1 "3fe5b303573c645bb072b570176de1e13eb1bae3"
+    sha1 "58594afada7666b807f28217d7cf3ca76e53dcec"
   elsif OS.linux?
     url "http://bozeman.mbt.washington.edu/consed/distributions/#{version}/consed_linux.tar.gz"
-    sha1 "64a5a5e28b1fb5653b89b9b09b9da2749dc42d4f"
+    sha1 "2df1ee47fd7e7d7e370346fb04596cfdaa63082d"
   else
     raise "Unknown operating system"
   end
