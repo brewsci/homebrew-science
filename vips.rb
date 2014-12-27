37ece8a5c542ae85a55a100e34a9e586f5882b7c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-27 22:26:01 +0000

    vips: add 7.42.1 bottle.

--- vips.rb
+++ vips.rb
@@ -5,6 +5,13 @@ class Vips < Formula
   url "http://www.vips.ecs.soton.ac.uk/supported/7.42/vips-7.42.1.tar.gz"
   sha1 "b31576c776b9590e3e9ae06c0d1849ebd7775997"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "dd53d46bc27968f97c3c4e818f6f0151ac980312" => :yosemite
+    sha1 "e0ef1938bee08bb7d929d1036e15442c183f644e" => :mavericks
+    sha1 "35717ed244e370645f7cdaa3112a2a15cc6afbd8" => :mountain_lion
+  end
+
   option "without-check", "Disable build time checks (not recommended)"
 
   depends_on "pkg-config" => :build
