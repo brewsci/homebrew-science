062f2dc301a88778db1e0092321c7abe5edc704a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-28 17:56:03 +0000

    fasttree: add 2.1.7 bottle.

--- fasttree.rb
+++ fasttree.rb
@@ -5,6 +5,14 @@ class Fasttree < Formula
   url 'http://www.microbesonline.org/fasttree/FastTree-2.1.7.c'
   sha1 'd9381924829e7d19d56154ebbde0e44044b4b7ab'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "369bb09b371ed05d55a403750738c5749fb95c07" => :yosemite
+    sha1 "829bfc5f6fc144a6642c29b8a2e6a4e078998e8c" => :mavericks
+    sha1 "acad8b55ad7eb9f6a63cf6b96ef3961e1d01ed79" => :mountain_lion
+  end
+
   needs :openmp # => :recommended
 
   fails_with :clang do
