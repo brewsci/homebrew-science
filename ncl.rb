edd615e6addf1f5d330c5aea672cd23214cd22b5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-28 18:14:49 +0000

    ncl: add 2.1.18 bottle.

--- ncl.rb
+++ ncl.rb
@@ -6,6 +6,13 @@ class Ncl < Formula
   url "https://downloads.sourceforge.net/project/ncl/NCL/ncl-2.1.18/ncl-2.1.18.tar.gz"
   sha1 "54e4f1ff4fef52cfd633b467a839e57a2670a397"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "b947e17defc2915c7b8eb80727c5105e33a97a4c" => :yosemite
+    sha1 "78b8bced22543506a48bba1ab2d86b72263c2d06" => :mavericks
+    sha1 "f20283932a9b76677fe96f97e8d34b2238da8c8b" => :mountain_lion
+  end
+
   def install
     # The option --with-constfuncs is required by garli.
     # --with-constfuncs=yes Defines the NCL_CONST_FUNCS macro so
