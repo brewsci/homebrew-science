28001b9772f0ffb4a7a3901e647e4776c17d6abc
Sebastian Schöps <sebastian@schoeps.org>
2014-12-29 22:16:43 +0100

    GMSH: support for petsc and opencascade.
    Closes #1464.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- gmsh.rb
+++ gmsh.rb
@@ -13,10 +13,20 @@ class Gmsh < Formula
 
   head 'https://geuz.org/svn/gmsh/trunk', :using => GmshSvnStrategy
 
+  depends_on :fortran
+  depends_on :mpi => [:cc, :cxx, :f90, :recommended]
   depends_on 'cmake' => :build
+  depends_on "petsc" => :optional
+  depends_on "slepc" => :optional
+  depends_on "opencascade" => :recommended
   depends_on 'fltk' => :optional
 
   def install
+    if not build.head? and (build.with? "petsc" or build.with? "slepc")
+      onoe "stable is incompatible with PETSc/SLEPc 3.5.2. Build with --HEAD."
+      exit 1
+    end
+
     # In OS X, gmsh sets default directory locations as if building a
     # binary. These locations must be reset so that they make sense
     # for a Homebrew-based build.
@@ -26,6 +36,30 @@ class Gmsh < Formula
                              "-DGMSH_DOC=#{share}/gmsh",
                              "-DGMSH_MAN=#{man}"]
 
+    if build.with? "opencascade"
+      ENV["CASROOT"] = Formula["opencascade"].opt_prefix
+      args << "-DENABLE_OCC=ON"
+    else
+      args << "-DENABLE_OCC=OFF"
+    end
+
+    if build.with? "petsc"
+      ENV["PETSC_DIR"] = Formula["petsc"].opt_prefix
+      ENV["PETSC_ARCH"] = "real"
+    else
+      args << "-DENABLE_PETSC=OFF"
+    end
+
+    if build.with? "slepc"
+      ENV["SLEPC_DIR"] = "#{Formula['slepc'].opt_prefix}/real"
+    else
+      args << "-DENABLE_SLEPC=OFF"
+    end
+
+    if (build.with? "petsc") or (build.with? "slepc")
+      args << "-DENABLE_MPI=ON" if build.with? :mpi
+    end
+
     # Make sure native file dialogs are used
     args << "-DENABLE_NATIVE_FILE_CHOOSER=ON"
 
@@ -33,15 +67,22 @@ class Gmsh < Formula
     args << "-DENABLE_BUILD_LIB=ON"
     args << "-DENABLE_BUILD_SHARED=ON"
 
-    # Todo: enable PETSc and SLEPc.
-    args << "-DENABLE_PETSC=OFF"
-    args << "-DENABLE_SLEPC=OFF"
+    # Build with or without GUI
     args << "-DENABLE_FLTK=OFF" if build.without? "fltk"
+
     mkdir "build" do
       system "cmake", "..", *args
       system "make"
       system "make", "install"
+
+      # move onelab.py into libexec instead of bin
+      rm "#{bin}/onelab.py"
+      libexec.install "onelab.py"
     end
+  end
+
+  def caveats
+     "To use onelab.py set your PYTHONDIR to #{libexec}"
   end
 
   test do
