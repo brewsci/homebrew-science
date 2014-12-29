9acbdf78f0f4b2c33d9f12d4fa3a1a7b6167d833
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:56:07 +0000

    jellyfish-1.1: add 1.1.11 bottle.

--- jellyfish-1.1.rb
+++ jellyfish-1.1.rb
@@ -8,6 +8,14 @@ class Jellyfish11 < Formula
   url "http://www.cbcb.umd.edu/software/jellyfish/jellyfish-1.1.11.tar.gz"
   sha1 "8bd6a7b382e94d37adfddd916dec2b0e36f840fd"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "b4b2ff85afb9eb9a01195cc18f57f9eeb95588f3" => :yosemite
+    sha1 "3c50af45fa15a69b72ec34f012f406b7b866382f" => :mavericks
+    sha1 "64ca4a63f5b5bd5d4d4f48705ea694b5925a3de7" => :mountain_lion
+  end
+
   keg_only "It conflicts with jellyfish."
 
   fails_with :clang do
