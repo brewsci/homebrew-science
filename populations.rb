99398856297700e7d50d726b5cb24db70e33a0dc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 08:45:14 +0000

    populations: add 1.2.33-2 bottle.

--- populations.rb
+++ populations.rb
@@ -8,6 +8,14 @@ class Populations < Formula
   sha1 "ff190be19352e2bb66f7decb87aed311b924529c"
   version "1.2.33-2"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "d5c96260ab82fe602b674f7d25f6427d53efea82" => :yosemite
+    sha1 "b9ff1df51ef395aeacbabc8e7ba1426655125616" => :mavericks
+    sha1 "9d71c75bdc9d35619adb006ac05737ce3ddb7136" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "gettext"
   depends_on "qt"
