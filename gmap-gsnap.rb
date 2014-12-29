4b688e8aaa2e6427432462e23d249e6a35ca66a1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:42:06 +0000

    gmap-gsnap: add 2014-05-15 bottle.

--- gmap-gsnap.rb
+++ gmap-gsnap.rb
@@ -9,6 +9,13 @@ class GmapGsnap < Formula
   url "http://research-pub.gene.com/gmap/src/gmap-gsnap-#{version}.tar.gz"
   sha1 "775e427e8747a3348adfc7caecf720bcd6c26f58"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "50861e3482efc2e6dbd301bc006db9ab75656e08" => :yosemite
+    sha1 "37c897f1c50bc3186d468b2e4780b93490c8c1a5" => :mavericks
+    sha1 "4e2c7e4d63d1873714c1d89a0c20ca2c82501e40" => :mountain_lion
+  end
+
   depends_on "samtools"
 
   def install
