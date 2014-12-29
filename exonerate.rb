08dbdf4deb66c699f2d09dd77e7820ac3e382505
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:30:13 +0000

    exonerate: add 2.2.0 bottle.

--- exonerate.rb
+++ exonerate.rb
@@ -7,6 +7,13 @@ class Exonerate < Formula
   url "http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.2.0.tar.gz"
   sha1 "ad4de207511e4d421e5cc28dda2261421c515bf0"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "dea2a01264de5d3b918afacdd01946960f12f75a" => :yosemite
+    sha1 "c2a808a60fd9f77b7d09133b2a136a3d3af05b8d" => :mavericks
+    sha1 "d337e9190f4cade5fabd3cc6ba4ce33ee1615e4b" => :mountain_lion
+  end
+
   devel do
     url "http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.4.0.tar.gz"
     sha1 "5b119c0aef0fa08c3f4a11014544f2ac5ca8afde"
