57b82687fbb6e2d1dfcd7ddbb3d56d018801f12d
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:35:44 -0800

    masurca 2.2.1: Add bioinformatics tag
    Closes #1552.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- masurca.rb
+++ masurca.rb
@@ -1,8 +1,10 @@
-require 'formula'
+require "formula"
 
 class Masurca < Formula
-  homepage 'http://www.genome.umd.edu/masurca.html'
+  homepage "http://www.genome.umd.edu/masurca.html"
   #doi "10.1093/bioinformatics/btt476"
+  #tag "bioinformatics"
+
   url "ftp://ftp.genome.umd.edu/pub/MaSuRCA/v2.2.1/MaSuRCA-2.2.1.tar.gz"
   sha1 "a568d0afc9cf96e5351e8f4ef92c1b89a13011d6"
 
@@ -25,6 +27,6 @@ class Masurca < Formula
   end
 
   test do
-    system "#{bin}/masurca -h"
+    system "#{bin}/masurca", "-h"
   end
 end
