ddb3275fa70e88dbaf2d0d0d498fbdc3a2466b4e
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:33:25 -0800

    kmergenie 1.6476: Change single quotes to double
    Closes #1551.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- kmergenie.rb
+++ kmergenie.rb
@@ -1,20 +1,22 @@
-require 'formula'
+require "formula"
 
 class Kmergenie < Formula
-  homepage 'http://kmergenie.bx.psu.edu/'
-  #doi '10.1093/bioinformatics/btt310'
-  url 'http://kmergenie.bx.psu.edu/kmergenie-1.6476.tar.gz'
-  sha1 '744ccd0f033af6be15fc9f6edfa31af08b316e40'
+  homepage "http://kmergenie.bx.psu.edu/"
+  #doi "10.1093/bioinformatics/btt310"
+  #tag "bioinformatics"
 
-  depends_on 'r'
+  url "http://kmergenie.bx.psu.edu/kmergenie-1.6476.tar.gz"
+  sha1 "744ccd0f033af6be15fc9f6edfa31af08b316e40"
+
+  depends_on "r"
 
   def install
     ENV.deparallelize
-    system 'make'
-    libexec.install 'kmergenie', 'specialk',
-      'scripts', 'third_party'
-    bin.install_symlink '../libexec/kmergenie'
-    doc.install 'CHANGELOG', 'LICENSE', 'README'
+    system "make"
+    libexec.install "kmergenie", "specialk",
+      "scripts", "third_party"
+    bin.install_symlink "../libexec/kmergenie"
+    doc.install "CHANGELOG", "LICENSE", "README"
   end
 
   test do
