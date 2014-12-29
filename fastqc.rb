4b6beae94f6dde38c17d665880eb79544be5f0ef
Shaun Jackman <sjackman@gmail.com>
2014-12-28 22:56:47 -0800

    fastqc 0.11.2: Change single quotes to double
    Closes #1542.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fastqc.rb
+++ fastqc.rb
@@ -1,15 +1,17 @@
-require 'formula'
+require "formula"
 
 class Fastqc < Formula
-  homepage 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/'
-  url 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.2.zip'
-  sha1 'd4dc1b903de35aa4de8e995c4974e0869db99dda'
+  homepage "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/"
+  #tag "bioinformatics"
+
+  url "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.2.zip"
+  sha1 "d4dc1b903de35aa4de8e995c4974e0869db99dda"
 
   def install
-    chmod 0755, 'fastqc'
-    prefix.install Dir['*']
+    chmod 0755, "fastqc"
+    prefix.install Dir["*"]
     mkdir_p bin
-    ln_s prefix/'fastqc', bin/'fastqc'
+    ln_s prefix/"fastqc", bin/"fastqc"
   end
 
   test do
