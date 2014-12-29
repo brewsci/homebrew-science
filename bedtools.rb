a4b18faa0af0cff07243868ce27ec2eb1f2f67ad
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:02:59 -0800

    bedtools 2.22.0: Change single quotes to double
    Closes #1544.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bedtools.rb
+++ bedtools.rb
@@ -1,20 +1,20 @@
-require 'formula'
+require "formula"
 
 class Bedtools < Formula
-  homepage 'https://github.com/arq5x/bedtools2'
-  #doi '10.1093/bioinformatics/btq033'
+  homepage "https://github.com/arq5x/bedtools2"
+  #doi "10.1093/bioinformatics/btq033"
   #tag "bioinformatics"
   url "https://github.com/arq5x/bedtools2/releases/download/v2.22.0/bedtools-2.22.0.tar.gz"
   sha1 "96f9f7031806de5d4ec694aeb9db31c3f8188f9b"
-  head 'https://github.com/arq5x/bedtools2.git'
+  head "https://github.com/arq5x/bedtools2.git"
 
   def install
-    system 'make'
-    prefix.install 'bin'
+    system "make"
+    prefix.install "bin"
     doc.install %w[README.md RELEASE_HISTORY]
   end
 
   test do
-    system "#{bin}/bedtools --version"
+    system "#{bin}/bedtools", "--version"
   end
 end
