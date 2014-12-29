0901dd6b9d134f521e970dfb10c38c7fdd654c79
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:15:28 +0000

    cegma: add 2.4.010312 bottle.

--- cegma.rb
+++ cegma.rb
@@ -8,6 +8,14 @@ class Cegma < Formula
   url "http://korflab.ucdavis.edu/datasets/cegma/cegma_v2.4.010312.tar.gz"
   sha1 "4c046fe0376d69f6969a32af3481c60088306b9b"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "05109e3eb93ebe55011ffa94350082a4b7894e8c" => :yosemite
+    sha1 "eee84e57db04c733be24fe0e910828fc861b60c4" => :mavericks
+    sha1 "0db56c758491ba137490d0d45afcd82a3e17602e" => :mountain_lion
+  end
+
   depends_on "blast"
   depends_on "geneid"
   depends_on "genewise"
