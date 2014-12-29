8b9c4f343739f23605cede7564761f01b3110fc5
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:41:38 -0800

    prodigal 2.6.1: Change single quotes to double
    Closes #1554.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- prodigal.rb
+++ prodigal.rb
@@ -1,21 +1,21 @@
-require 'formula'
+require "formula"
 
 class Prodigal < Formula
-  homepage 'http://prodigal.ornl.gov/'
-  #doi '10.1186/1471-2105-11-119'
+  homepage "http://prodigal.ornl.gov/"
+  #doi "10.1186/1471-2105-11-119"
   #tag "bioinformatics"
   url "https://github.com/hyattpd/Prodigal/archive/v2.6.1.tar.gz"
   sha1 "aebcfbfb33010cbbba480c1db8b2ba5ebf5c7bd7"
 
-  head 'https://github.com/hyattpd/Prodigal.git'
+  head "https://github.com/hyattpd/Prodigal.git"
 
   def install
     system "make"
     mv "prodigal2", "prodigal" if build.head?
-    bin.install 'prodigal'
+    bin.install "prodigal"
   end
 
   test do
-    system "#{bin}/prodigal -v"
+    system "#{bin}/prodigal", "-v"
   end
 end
