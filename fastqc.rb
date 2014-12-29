cca4b3eb85da06db79c79500775dbd33a0801595
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:00:47 +0000

    fastqc: add 0.11.2 bottle.

--- fastqc.rb
+++ fastqc.rb
@@ -7,6 +7,13 @@ class Fastqc < Formula
   url "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.2.zip"
   sha1 "d4dc1b903de35aa4de8e995c4974e0869db99dda"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "fdd145443d6034ea7771f0029ac303e6525ce68b" => :yosemite
+    sha1 "705413545eeab36baf99ad66863882c91a2bccb9" => :mavericks
+    sha1 "0c78a464a90f35496d758023665d173f00f5b857" => :mountain_lion
+  end
+
   def install
     chmod 0755, "fastqc"
     prefix.install Dir["*"]
