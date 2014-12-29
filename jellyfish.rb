44bdbcee012393e2e4750b012ddb477e73f90a32
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:49:59 +0000

    jellyfish: add 2.1.3 bottle.

--- jellyfish.rb
+++ jellyfish.rb
@@ -8,6 +8,13 @@ class Jellyfish < Formula
   url "ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz"
   sha1 "b115dc7066cf24ab4026d8d8d429f094f3917cf2"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "ebda78603c58f742b2c158e0f77a55a73fc1fcb9" => :yosemite
+    sha1 "eaab1b21b747f6f252f9d5ec4c9a5fd6ed3e9d27" => :mavericks
+  end
+
   #head "https://github.com/gmarcais/Jellyfish.git"
 
   def install
