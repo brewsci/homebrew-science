99b3af4c0e4d5bce51abbb429ce07e9acd525fbb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 08:34:14 +0000

    prodigal: add 2.6.1 bottle.

--- prodigal.rb
+++ prodigal.rb
@@ -9,6 +9,14 @@ class Prodigal < Formula
 
   head "https://github.com/hyattpd/Prodigal.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "58b6b6ea89bc542585104fba63ffd5b9a1a5a52b" => :yosemite
+    sha1 "cad820e17038130c8e245df756a3c3b6bf75ab72" => :mavericks
+    sha1 "00c541a276de2938ffff968174c4b3076943a136" => :mountain_lion
+  end
+
   def install
     system "make"
     mv "prodigal2", "prodigal" if build.head?
