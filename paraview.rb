46282450e58c8c2a340a541e9034587630b16334
Dominique Orban <dominique.orban@gmail.com>
2014-12-28 23:02:11 -0500

    Paraview: Fix cast issue related to FreeType.
    Fixes #1486.
    
    Closes #1541.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- paraview.rb
+++ paraview.rb
@@ -22,6 +22,10 @@ class Paraview < Formula
   depends_on "fontconfig"
   depends_on "libpng"
 
+  # Temporary fix for a cast issue related to FreeType.
+  # See https://bugs.gentoo.org/show_bug.cgi?id=533444
+  patch :DATA
+
   def install
     args = std_cmake_args + %W[
       -DBUILD_SHARED_LIBS=ON
@@ -64,3 +68,29 @@ class Paraview < Formula
     system "#{prefix}/paraview.app/Contents/MacOS/paraview", "--version"
   end
 end
+
+__END__
+diff --git a/VTK/Rendering/FreeType/vtkFreeTypeTools.cxx b/VTK/Rendering/FreeType/vtkFreeTypeTools.cxx
+index fcbb323..7a48f62 100644
+--- a/VTK/Rendering/FreeType/vtkFreeTypeTools.cxx
++++ b/VTK/Rendering/FreeType/vtkFreeTypeTools.cxx
+@@ -1183,7 +1183,7 @@ bool vtkFreeTypeTools::CalculateBoundingBox(const T& str,
+     if (bitmap)
+       {
+       metaData.ascent = std::max(bitmapGlyph->top - 1, metaData.ascent);
+-      metaData.descent = std::min(-(bitmap->rows - (bitmapGlyph->top - 1)),
++      metaData.descent = std::min(-(static_cast<int>(bitmap->rows) - (bitmapGlyph->top - 1)),
+                                   metaData.descent);
+       }
+     ++heightString;
+@@ -1950,8 +1950,8 @@ void vtkFreeTypeTools::GetLineMetrics(T begin, T end, MetaData &metaData,
+     if (bitmap)
+       {
+       bbox[0] = std::min(bbox[0], pen[0] + bitmapGlyph->left);
+-      bbox[1] = std::max(bbox[1], pen[0] + bitmapGlyph->left + bitmap->width);
+-      bbox[2] = std::min(bbox[2], pen[1] + bitmapGlyph->top - 1 - bitmap->rows);
++      bbox[1] = std::max(bbox[1], pen[0] + bitmapGlyph->left + static_cast<int>(bitmap->width));
++      bbox[2] = std::min(bbox[2], pen[1] + bitmapGlyph->top - 1 - static_cast<int>(bitmap->rows));
+       bbox[3] = std::max(bbox[3], pen[1] + bitmapGlyph->top - 1);
+       }
+     else
