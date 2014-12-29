bf1e8952dc037cdbd552dbb6defc422284c33a14
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:00:30 -0800

    augustus 3.0.1: Change single quotes to double
    Closes #1543.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- augustus.rb
+++ augustus.rb
@@ -1,26 +1,26 @@
-require 'formula'
+require "formula"
 
 class Augustus < Formula
-  homepage 'http://bioinf.uni-greifswald.de/augustus/'
+  homepage "http://bioinf.uni-greifswald.de/augustus/"
   #doi "10.1093/nar/gkh379"
   #tag "bioinformatics"
 
-  url 'http://bioinf.uni-greifswald.de/augustus/binaries/augustus.3.0.1.tar.gz'
-  mirror 'https://science-annex.org/pub/augustus/augustus.3.0.1.tar.gz'
-  sha1 '19f40b3b834aba0386646d9cb8120caf3f0eb64e'
+  url "http://bioinf.uni-greifswald.de/augustus/binaries/augustus.3.0.1.tar.gz"
+  mirror "https://science-annex.org/pub/augustus/augustus.3.0.1.tar.gz"
+  sha1 "19f40b3b834aba0386646d9cb8120caf3f0eb64e"
 
-  depends_on 'boost' => :recommended # for gz support
+  depends_on "boost" => :recommended # for gz support
 
   fails_with :clang do
     build 600
-    cause 'error: invalid operands to binary expression'
+    cause "error: invalid operands to binary expression"
   end
 
   def install
-    system 'make'
+    system "make"
     rm_r %w[include mysql++ src]
-    libexec.install Dir['*']
-    bin.install_symlink '../libexec/bin/augustus'
+    libexec.install Dir["*"]
+    bin.install_symlink "../libexec/bin/augustus"
   end
 
   def caveats; <<-EOS.undent
@@ -30,6 +30,6 @@ class Augustus < Formula
   end
 
   test do
-    system 'augustus --version'
+    system "#{bin}/augustus", "--version"
   end
 end
