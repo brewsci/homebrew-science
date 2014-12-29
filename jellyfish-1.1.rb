512552323591fd660017d030ba6980b55fe4de57
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:28:47 -0800

    jellyfish-1.1 1.1.11: Changes single quotes
    Closes #1549.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- jellyfish-1.1.rb
+++ jellyfish-1.1.rb
@@ -1,10 +1,12 @@
-require 'formula'
+require "formula"
 
 class Jellyfish11 < Formula
-  homepage 'http://www.cbcb.umd.edu/software/jellyfish/'
+  homepage "http://www.cbcb.umd.edu/software/jellyfish/"
   #doi "10.1093/bioinformatics/btr011"
-  url 'http://www.cbcb.umd.edu/software/jellyfish/jellyfish-1.1.11.tar.gz'
-  sha1 '8bd6a7b382e94d37adfddd916dec2b0e36f840fd'
+  #tag "bioinformatics"
+
+  url "http://www.cbcb.umd.edu/software/jellyfish/jellyfish-1.1.11.tar.gz"
+  sha1 "8bd6a7b382e94d37adfddd916dec2b0e36f840fd"
 
   keg_only "It conflicts with jellyfish."
 
@@ -19,6 +21,6 @@ class Jellyfish11 < Formula
   end
 
   test do
-    system "#{bin}/jellyfish --version"
+    system "#{bin}/jellyfish", "--version"
   end
 end
