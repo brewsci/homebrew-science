b3ae1307c3c71d73ad3452e0c911c4f7cf246d65
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:21:39 -0800

    gmap-gsnap 2014-05-15: Change single quotes
    Closes #1547.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- gmap-gsnap.rb
+++ gmap-gsnap.rb
@@ -1,19 +1,21 @@
-require 'formula'
+require "formula"
 
 class GmapGsnap < Formula
-  homepage 'http://research-pub.gene.com/gmap/'
-  #doi '10.1093/bioinformatics/btq057'
-  version '2014-05-15'
+  homepage "http://research-pub.gene.com/gmap/"
+  #doi "10.1093/bioinformatics/btq057"
+  #tag "bioinformatics"
+
+  version "2014-05-15"
   url "http://research-pub.gene.com/gmap/src/gmap-gsnap-#{version}.tar.gz"
-  sha1 '775e427e8747a3348adfc7caecf720bcd6c26f58'
+  sha1 "775e427e8747a3348adfc7caecf720bcd6c26f58"
 
   depends_on "samtools"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
   end
 
   def caveats; <<-EOF.undent
