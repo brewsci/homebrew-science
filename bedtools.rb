efa5450b192c13d2f6b4aa62233222bd9a48ef69
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-29 07:17:44 +0000

    bedtools: add 2.22.0 bottle.

--- bedtools.rb
+++ bedtools.rb
@@ -8,6 +8,14 @@ class Bedtools < Formula
   sha1 "96f9f7031806de5d4ec694aeb9db31c3f8188f9b"
   head "https://github.com/arq5x/bedtools2.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "8c42e2006d59cbe8510acb245f2045602c7145b7" => :yosemite
+    sha1 "552196fd1fe129e24814ae0c6ddaa58baf6f7535" => :mavericks
+    sha1 "e94f17b8d473ebb26fb0acd7936bf268327f78b5" => :mountain_lion
+  end
+
   def install
     system "make"
     prefix.install "bin"
