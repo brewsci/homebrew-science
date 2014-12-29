fc46e9d66c739a44b23eedc858be6c7a1c95b016
Shaun Jackman <sjackman@gmail.com>
2014-12-28 23:31:32 -0800

    jellyfish 2.1.3: Add bioinformatics tag
    Closes #1550.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- jellyfish.rb
+++ jellyfish.rb
@@ -2,15 +2,17 @@ require "formula"
 
 class Jellyfish < Formula
   homepage "http://www.genome.umd.edu/jellyfish.html"
+  #doi "10.1093/bioinformatics/btr011"
+  #tag "bioinformatics"
+
   url "ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz"
   sha1 "b115dc7066cf24ab4026d8d8d429f094f3917cf2"
 
   #head "https://github.com/gmarcais/Jellyfish.git"
-  #doi "10.1093/bioinformatics/btr011"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
