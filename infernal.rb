1ea8efa1fe523fda7e1e9dd1ba04af83b30b2ff5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 21:51:46 +0000

    infernal: add 1.1.1 bottle.

--- infernal.rb
+++ infernal.rb
@@ -6,6 +6,14 @@ class Infernal < Formula
   url "http://selab.janelia.org/software/infernal/infernal-1.1.1.tar.gz"
   sha1 "b83fbc3a50c49ad51e40b37195aeacec94897d30"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "1babacebaf18df5698ca52eb54313a5b03aa2475" => :yosemite
+    sha1 "b10867d6dcf06bf4bc86edd42220652ca5d64272" => :mavericks
+    sha1 "74847dd59196b2b68a6afb79f956675a5e29562c" => :mountain_lion
+  end
+
   deprecated_option "check" => "with-check"
 
   option "with-check", "Run the test suite (`make check`). Takes a couple of minutes."
