4d03882adbcaa055e6288b93c04115bd737d9a83
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 23:31:41 +0000

    tasr: add 1.5.1 bottle.

--- tasr.rb
+++ tasr.rb
@@ -7,6 +7,14 @@ class Tasr < Formula
   version "1.5.1"
   sha1 "98f3b2207a5e7a1904cc858dec6614677c6b38df"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "3a0971056c247c128cdf5d9e358041ea52320f51" => :yosemite
+    sha1 "cc104749c8987de39ad6dd8f20c03f450f16f1b8" => :mavericks
+    sha1 "e5b7c109d35f4d204377f0950c5827ddf622bc2a" => :mountain_lion
+  end
+
   def install
     bin.install "TASR"
     doc.install "TASR.readme"
