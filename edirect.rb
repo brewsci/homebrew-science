2e37806cb574a55f3e54a2d0349e66053af31351
Shaun Jackman <sjackman@gmail.com>
2014-12-30 08:49:08 -0800

    edirect 2.00: Remove require "formula"
    Closes #1572.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- edirect.rb
+++ edirect.rb
@@ -1,8 +1,7 @@
-require "formula"
-
 class Edirect < Formula
   homepage "http://www.ncbi.nlm.nih.gov/books/NBK179288/"
   #tag "bioinformatics"
+
   url "ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.tar.gz"
   sha1 "45578de2ae034960dd65a9fc0fbcb81ffdb0898c"
   version "2.00"
@@ -14,6 +13,6 @@ class Edirect < Formula
   end
 
   test do
-    system "#{bin}/esearch -version"
+    system "#{bin}/esearch", "-version"
   end
 end
