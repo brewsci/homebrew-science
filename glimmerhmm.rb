f2e668f3a6480191fa47c02851944143db89c7c2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 21:08:34 +0000

    glimmerhmm: add 3.0.2 bottle.

--- glimmerhmm.rb
+++ glimmerhmm.rb
@@ -6,6 +6,14 @@ class Glimmerhmm < Formula
   url "ftp://ccb.jhu.edu/pub/software/glimmerhmm/GlimmerHMM-3.0.2.tar.gz"
   sha1 "e85b28db2cc2906d7ecc8b93963e6e9163ad4950"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "3a0c44274577c5c287c86ee14da88947efdf9950" => :yosemite
+    sha1 "dae57d85c6a4b06f603d711ce2bc403191d7a542" => :mavericks
+    sha1 "4aa039c9b8fdd654dcfad45f2db9a3e96a5c33b1" => :mountain_lion
+  end
+
   def install
     # fatal error: 'malloc.h' file not found
     inreplace %w[sources/oc1.h sources/util.c train/utils.c], "malloc.h", "stdlib.h"
