5d72d7624a6995b6a263f6160e81d3decb494853
Shaun Jackman <sjackman@gmail.com>
2014-12-29 22:57:59 -0800

    muscle 3.8.31: Add doi and tag
    Closes #1563.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- muscle.rb
+++ muscle.rb
@@ -1,10 +1,11 @@
-require 'formula'
-
 class Muscle < Formula
-  homepage 'http://www.drive5.com/muscle/'
-  url 'http://www.drive5.com/muscle/downloads3.8.31/muscle3.8.31_src.tar.gz'
-  version '3.8.31'
-  sha1 '2fe55db73ff4e7ac6d4ca692f8f213d1c5071dac'
+  homepage "http://www.drive5.com/muscle/"
+  #doi "10.1093/nar/gkh340", "10.1186/1471-2105-5-113"
+  #tag "bioinformatics"
+
+  url "http://www.drive5.com/muscle/downloads3.8.31/muscle3.8.31_src.tar.gz"
+  version "3.8.31"
+  sha1 "2fe55db73ff4e7ac6d4ca692f8f213d1c5071dac"
 
   def install
     # This patch makes 3.8.31 build on OSX >= Lion.
