55042d92995b653b11e3be808d8db3f595381a3f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 22:30:39 +0000

    repeatscout: add 1.0.5 bottle.

--- repeatscout.rb
+++ repeatscout.rb
@@ -6,6 +6,14 @@ class Repeatscout < Formula
   url "http://repeatscout.bioprojects.org/RepeatScout-1.0.5.tar.gz"
   sha1 "507fe8813de341244c5380836ddcf4257bb46c81"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "99cab9bddcdc39c7e0e7aca2cdb4c850bf64c460" => :yosemite
+    sha1 "cef581a1b57c329eb1730dc1b669ae86005fcc7f" => :mavericks
+    sha1 "51ba7be2837631c8748bfab9d06e63f1cca3756f" => :mountain_lion
+  end
+
   depends_on "trf" => :optional
 
   def install
