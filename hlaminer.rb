90340effa2b61732f622be7087908d2509fe4458
Shaun Jackman <sjackman@gmail.com>
2014-12-30 08:58:22 -0800

    hlaminer 1.1: Remove require "formula"
    Closes #1576.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- hlaminer.rb
+++ hlaminer.rb
@@ -1,15 +1,14 @@
-require "formula"
-
 class Hlaminer < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer"
   #doi "10.1186/gm396"
   #tag "bioinformatics"
+
   url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.1/HLAminer.tar.gz"
   version "1.1.0"
   sha1 "d1c97731885bc7a12f67027e0e0b01c8bfb03bd2"
 
-  depends_on "tasr"
   depends_on "blast"
+  depends_on "tasr"
 
   def install
     # Conflicts with tasr
