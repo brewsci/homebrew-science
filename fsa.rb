ddc0ad93ab55845ff530a0db351adcf4f86b244f
Shaun Jackman <sjackman@gmail.com>
2014-12-30 08:50:40 -0800

    fsa 1.15.9: Remove require "formula"
    Closes #1573.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fsa.rb
+++ fsa.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Fsa < Formula
   homepage "http://fsa.sourceforge.net/"
   #doi "10.1371/journal.pcbi.1000392"
   #tag "bioinformatics"
+
   url "https://downloads.sourceforge.net/project/fsa/fsa-1.15.9.tar.gz"
   sha1 "457aee5baca17357e52041eac7e3ecbc226bea75"
 
@@ -19,6 +18,6 @@ class Fsa < Formula
   end
 
   test do
-    system "#{bin}/fsa --version"
+    system "#{bin}/fsa", "--version"
   end
 end
