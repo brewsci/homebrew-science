ee0d5cb7f67ca0f44fa11329ea4ba657467b47d2
Shaun Jackman <sjackman@gmail.com>
2014-12-30 09:29:19 -0800

    tasr 1.5.1: Remove require "formula"
    Closes #1590.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- tasr.rb
+++ tasr.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Tasr < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/tasr"
   #doi "10.1371/journal.pone.0019816"
   #tag "bioinformatics"
+
   url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1-5-1.tar.gz"
   version "1.5.1"
   sha1 "98f3b2207a5e7a1904cc858dec6614677c6b38df"
