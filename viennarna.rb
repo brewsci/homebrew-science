8e7c8b7be9861c491462af0893eb07009fb5bdaf
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 02:08:29 +0000

    viennarna: add 2.1.8 bottle.

--- viennarna.rb
+++ viennarna.rb
@@ -8,6 +8,14 @@ class Viennarna < Formula
   url "http://www.tbi.univie.ac.at/~ronny/RNA/packages/source/ViennaRNA-2.1.8.tar.gz"
   sha1 "9f26c89043a00fddbac94751cd4450d0a7235fe2"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "0b8cc67011e8070f27d6e4bede85c259ac9f2add" => :yosemite
+    sha1 "e3f1c752a9dd85be264944c9e3a048d3e87a519b" => :mavericks
+    sha1 "b449ec9a67b872f89b868f1b51ba817dad0c3c6d" => :mountain_lion
+  end
+
   option "with-perl", "Build and install Perl interface"
 
   depends_on :python => :optional
