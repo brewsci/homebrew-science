56e063f80e9d95de8e571dbda313396bd120a129
Shaun Jackman <sjackman@gmail.com>
2014-12-30 10:06:11 -0800

    allpaths-lg 50378: Change single quotes to double

--- allpaths-lg.rb
+++ allpaths-lg.rb
@@ -1,11 +1,10 @@
-require 'formula'
-
 class AllpathsLg < Formula
-  homepage 'http://www.broadinstitute.org/software/allpaths-lg/blog/'
-  #doi '10.1073/pnas.1017351108'
+  homepage "http://www.broadinstitute.org/software/allpaths-lg/blog/"
+  #doi "10.1073/pnas.1017351108"
+  #tag "bioinformatics"
 
-  url 'ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-50378.tar.gz'
-  sha1 '06cf7ed3ca8ed55847e895a93cf41b760a2b7021'
+  url "ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-50378.tar.gz"
+  sha1 "06cf7ed3ca8ed55847e895a93cf41b760a2b7021"
 
   fails_with :clang do
     cause "The only supported compiler is GCC(>=4.7)."
@@ -17,12 +16,13 @@ class AllpathsLg < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    system "#{bin}/RunAllPathsLG", '--version'
+    system "#{bin}/RunAllPathsLG", "--version"
   end
 end
