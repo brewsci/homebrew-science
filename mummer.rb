0037aa6b48b93214a9460de3e2beb70be308310c
Shaun Jackman <sjackman@gmail.com>
2014-12-29 23:56:18 -0800

    mummer 3.23: Change single quotes to double quotes
    Closes #1565.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- mummer.rb
+++ mummer.rb
@@ -1,53 +1,53 @@
-require 'formula'
-
 class Mummer < Formula
-  homepage 'http://mummer.sourceforge.net/'
-  #doi '10.1186/gb-2004-5-2-r12'
-  url 'https://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz'
-  sha1 '69261ed80bb77e7595100f0560973fe1f810c5fa'
+  homepage "http://mummer.sourceforge.net/"
+  #doi "10.1186/gb-2004-5-2-r12"
+  #tag "bioinformatics"
+
+  url "https://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz"
+  sha1 "69261ed80bb77e7595100f0560973fe1f810c5fa"
 
   def install
 
-    inreplace 'scripts/Makefile', '$(CURDIR)', prefix
+    inreplace "scripts/Makefile", "$(CURDIR)", prefix
 
-    system 'make'
+    system "make"
 
     %w[dnadiff mummerplot nucmer promer mapview].each do |script|
       inreplace script, "use lib \"#{prefix}", "use lib \"#{share}/mummer"
     end
 
-    inreplace 'promer' do |s|
+    inreplace "promer" do |s|
       s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
       s.gsub! /AUX_BIN_DIR = "[^"]+/ , "AUX_BIN_DIR = \"#{libexec}"
       s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
     end
 
-    inreplace 'nucmer' do |s|
+    inreplace "nucmer" do |s|
       s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
       s.gsub! /AUX_BIN_DIR = "[^"]+/ , "AUX_BIN_DIR = \"#{libexec}"
       s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
     end
 
-    inreplace 'dnadiff' do |s|
+    inreplace "dnadiff" do |s|
       s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
       s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
     end
 
-    inreplace 'mummerplot' do |s|
+    inreplace "mummerplot" do |s|
       s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
       s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
     end
 
-    inreplace 'exact-tandems' do |s|
+    inreplace "exact-tandems" do |s|
       s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
       s.gsub! /scriptdir = \S+/ , "scriptdir = #{libexec}"
     end
 
-    inreplace 'run-mummer1' do |s|
+    inreplace "run-mummer1" do |s|
       s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
     end
 
-    inreplace 'run-mummer3' do |s|
+    inreplace "run-mummer3" do |s|
       s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
     end
 
@@ -59,14 +59,14 @@ class Mummer < Formula
        mapview nucmer2xfig annotate gaps].each do |app|
       libexec.install app
     end
-    libexec.install 'scripts/tandem-repeat.awk'
+    libexec.install "scripts/tandem-repeat.awk"
 
     # postnuc, postpro, prenuc, prepro
-    Dir.glob('aux_bin/*').each do |script|
+    Dir.glob("aux_bin/*").each do |script|
       libexec.install script
     end
 
-    (share/'mummer').install 'scripts/Foundation.pm'
+    (share/"mummer").install "scripts/Foundation.pm"
 
   end
 
