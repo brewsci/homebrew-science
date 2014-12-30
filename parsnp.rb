3786b95cb790c3470da74a3284f8526f3720ce33
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 09:16:48 +0000

    parsnp: add 1.0 bottle.

--- parsnp.rb
+++ parsnp.rb
@@ -5,6 +5,14 @@ class Parsnp < Formula
 
   head "https://github.com/marbl/parsnp.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "eb6802a03f95fcd3f9d918ce03aea2f2da3cafc8" => :yosemite
+    sha1 "63d40206dcf353a9624e3f10913a17b2f1a6915d" => :mavericks
+    sha1 "8090d479662d6aae0d5e8f395f7e1aaba4fc976d" => :mountain_lion
+  end
+
   if OS.mac?
     url "https://github.com/marbl/parsnp/releases/download/v1.0/parsnp-OSX64-v1.0.tar.gz"
     sha1 "721c34434857c876fbf3f0b3aabda8145d6ee7eb"
