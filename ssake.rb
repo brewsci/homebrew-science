b611f071dd5a727291b47f94d1f5f40a7620f15a
Shaun Jackman <sjackman@gmail.com>
2014-12-30 09:22:07 -0800

    ssake 3.8.2: Remove require "formula"
    Closes #1588.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- ssake.rb
+++ ssake.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Ssake < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/ssake"
   #doi "10.1093/bioinformatics/btl629"
   #tag "bioinformatics"
+
   url "http://www.bcgsc.ca/platform/bioinfo/software/ssake/releases/3.8.2/ssake_v3-8-2.tar.gz"
   version "3.8.2"
   sha1 "a06300a7715578774914c14e7e025c2d9174c39d"
