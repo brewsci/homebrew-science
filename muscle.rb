646ca9a674e776874f6d6202b3e6e3dbf10cc179
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 07:42:24 +0000

    muscle: add 3.8.31 bottle.

--- muscle.rb
+++ muscle.rb
@@ -7,6 +7,14 @@ class Muscle < Formula
   version "3.8.31"
   sha1 "2fe55db73ff4e7ac6d4ca692f8f213d1c5071dac"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "b29b1f47580ef76f031bc19d0edded75b17dc5ff" => :yosemite
+    sha1 "08147f3f1f174c2d4e9c631dfbc2647d67a3cd6c" => :mavericks
+    sha1 "8398248dcadcae5a753eae15630cb815d9b19cdc" => :mountain_lion
+  end
+
   def install
     # This patch makes 3.8.31 build on OSX >= Lion.
     # It has been reported upstream but not fixed yet.
