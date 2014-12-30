3d01134002ed4127c4730053524c7f983ef836bd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 23:03:16 +0000

    ssake: add 3.8.2 bottle.

--- ssake.rb
+++ ssake.rb
@@ -7,6 +7,14 @@ class Ssake < Formula
   version "3.8.2"
   sha1 "a06300a7715578774914c14e7e025c2d9174c39d"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "5cb173578454d2734be7728649731052736f5314" => :yosemite
+    sha1 "8f120502cadff08b74dff6efe540c3a6f455d26b" => :mavericks
+    sha1 "d8ea940b5e2aacb30509e698f3530688923678fe" => :mountain_lion
+  end
+
   def install
     bin.install "SSAKE"
     doc.install "SSAKE.pdf", "SSAKE.readme"
