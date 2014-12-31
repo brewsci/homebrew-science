ff70fed888a919b9a2212734f9b83751b9677bbb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 18:42:17 +0000

    stacks: add 1.23 bottle.

--- stacks.rb
+++ stacks.rb
@@ -6,6 +6,12 @@ class Stacks < Formula
   url "http://creskolab.uoregon.edu/stacks/source/stacks-1.23.tar.gz"
   sha1 "392607a195191d3f11f1db70a665ba33e9df0551"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "62926c850a22ff8c4da00a320d05de253c02ccab" => :yosemite
+    sha1 "9c64ae474370d2bf60394edd6f110a8048cda863" => :mavericks
+  end
+
   depends_on "google-sparsehash" => :recommended
   depends_on "htslib"
 
