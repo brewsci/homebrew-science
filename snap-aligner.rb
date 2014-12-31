c48389956c184f4f24240442bb0c0362e336d97f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 18:56:01 +0000

    snap-aligner: add 0.15 bottle.

--- snap-aligner.rb
+++ snap-aligner.rb
@@ -7,6 +7,14 @@ class SnapAligner < Formula
   sha1 "f104f35f4209554a43ccfc8c353bd71213791a64"
   head "https://github.com/amplab/snap.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "1156de4f5ae32546089cf7d7959ced5dd84d766f" => :yosemite
+    sha1 "1222129ca847bce9e868afc049a6133d099233ab" => :mavericks
+    sha1 "50834c97faf37fd27d1f3361567b86c215f0388a" => :mountain_lion
+  end
+
   conflicts_with "snap", :because => "both install bin/snap"
 
   def install
