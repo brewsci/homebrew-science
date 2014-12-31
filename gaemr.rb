682c77a72d97d4418b7fad793dc0f64e3412ae2d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 03:08:48 +0000

    gaemr: add 1.0.1 bottle.

--- gaemr.rb
+++ gaemr.rb
@@ -5,6 +5,14 @@ class Gaemr < Formula
   url "http://www.broadinstitute.org/software/gaemr/wp-content/uploads/2012/12/GAEMR-1.0.1.tar.gz"
   sha1 "3747204b177eb32bfc2a0adc239d920320a2ff09"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "f00e97db2e69268acafc58e44b8840e011ef788b" => :yosemite
+    sha1 "8a6d07f2d64bf496a851b03663f238d0ccdce379" => :mavericks
+    sha1 "3212881a78c9a4ac17c7f5d462c5c44e60323f13" => :mountain_lion
+  end
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink "../libexec/bin/GAEMR.py"
