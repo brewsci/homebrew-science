26a85f4a8da55c228442b9d062b1c261a24e2a38
Shaun Jackman <sjackman@gmail.com>
2014-12-30 18:47:44 -0800

    stacks 1.22

--- stacks.rb
+++ stacks.rb
@@ -3,8 +3,8 @@ class Stacks < Formula
   #doi "10.1111/mec.12354"
   #tag "bioinformatics
 
-  url "http://creskolab.uoregon.edu/stacks/source/stacks-1.21.tar.gz"
-  sha1 "5510eeba37198a4c59f1eda6034e7340dbc233f2"
+  url "http://creskolab.uoregon.edu/stacks/source/stacks-1.22.tar.gz"
+  sha1 "4c22bfc1ad313875050cd76666d1c500b351ecc5"
 
   depends_on "google-sparsehash" => :recommended
   depends_on "htslib"
