bd498b7ea0fb3aa9654e3d91f4816961a75ad264
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 18:11:42 +0000

    seqtk: add 75 bottle.

--- seqtk.rb
+++ seqtk.rb
@@ -8,6 +8,14 @@ class Seqtk < Formula
 
   head "https://github.com/lh3/seqtk.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "0f57d5741e5cbc781b23d18dc3d5c5611e3e4b2e" => :yosemite
+    sha1 "ae24c7991ab1f41674df5d8e5628b47548b776d5" => :mavericks
+    sha1 "248bd05dbad49ff4ca9421f5df0089f8134b7984" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "seqtk"
