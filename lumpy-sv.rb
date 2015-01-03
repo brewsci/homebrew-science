e6e5a99feeac6f5a324b28b403f55182a2fbe79e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-03 01:05:49 +0000

    lumpy-sv: add 0.2.8 bottle.

--- lumpy-sv.rb
+++ lumpy-sv.rb
@@ -5,6 +5,14 @@ class LumpySv < Formula
   url "https://github.com/arq5x/lumpy-sv/releases/download/0.2.8/lumpy-sv-0.2.8.tar.gz"
   sha1 "d337c6871c1ecc374ff274af04978cadbf364b54"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "a5a489043b490388f615f62ab0b1c10edeaf60f5" => :yosemite
+    sha1 "6d8b7dc39b39e0cb351fb67254562f25b8e4deca" => :mavericks
+    sha1 "b7d514ef6a9f96feaeab07bda98a17d1c53cc608" => :mountain_lion
+  end
+
   depends_on "bamtools" => :recommended
   depends_on "samtools" => :recommended
   depends_on "bedtools" => :recommended
