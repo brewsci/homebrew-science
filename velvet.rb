8ac1d1f917ae03a1522b4c533cdc3b67a028e15a
Ian Lancaster <ilancaster@gmail.com>
2015-01-02 21:53:14 -0500

    velvet: add doi & tag
    Closes #1614.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- velvet.rb
+++ velvet.rb
@@ -1,28 +1,27 @@
-require 'formula'
-
 class Velvet < Formula
-  homepage 'http://www.ebi.ac.uk/~zerbino/velvet/'
-  url 'http://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz'
-  sha1 '216f0941609abf3a73adbba19ef1f364df489d18'
+  homepage "http://www.ebi.ac.uk/~zerbino/velvet/"
+  url "http://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz"
+  sha1 "216f0941609abf3a73adbba19ef1f364df489d18"
+  #doi "10.1101/gr.074492.107"
+  #tag "bioinformatics"
+  head "https://github.com/dzerbino/velvet.git"
 
-  head 'https://github.com/dzerbino/velvet.git'
-
-  option 'with-maxkmerlength=', 'Specify maximum k-mer length, any positive odd integer (default: 31)'
-  option 'with-categories=', 'Specify number of categories, any positive integer (default: 2)'
+  option "with-maxkmerlength=", "Specify maximum k-mer length, any positive odd integer (default: 31)"
+  option "with-categories=", "Specify number of categories, any positive integer (default: 2)"
 
   def install
     args = ["CFLAGS=-Wall -m64", "LONGSEQUENCES=1"]
     args << "OPENMP=1" unless ENV.compiler == :clang
-    maxkmerlength = ARGV.value('with-maxkmerlength') || '-1'
-    categories = ARGV.value('with-categories') || '-1'
+    maxkmerlength = ARGV.value("with-maxkmerlength") || "-1"
+    categories = ARGV.value("with-categories") || "-1"
     args << "MAXKMERLENGTH=#{maxkmerlength}" if maxkmerlength.to_i > 0
     args << "CATEGORIES=#{categories}" if categories.to_i > 0
 
     system "make", "velveth", "velvetg", *args
-    bin.install 'velveth', 'velvetg'
+    bin.install "velveth", "velvetg"
 
     # install additional contributed scripts
-    (share / 'velvet/contrib').install Dir['contrib/shuffleSequences_fasta/shuffleSequences_*']
+    (share / "velvet/contrib").install Dir["contrib/shuffleSequences_fasta/shuffleSequences_*"]
   end
 
   def caveats
@@ -33,7 +32,7 @@ class Velvet < Formula
   end
 
   test do
-    system "velveth --help"
-    system "velvetg --help"
+    system "velveth", "--help"
+    system "velvetg", "--help"
   end
 end
