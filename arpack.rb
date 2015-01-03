35da551404bf0317b03ea5ab251a740bd209d8e4
Dominique Orban <dominique.orban@gmail.com>
2015-01-03 11:48:30 -0500

    Arpack: Omit vecLibFort on Linux.
    Closes #1617.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- arpack.rb
+++ arpack.rb
@@ -17,7 +17,7 @@ class Arpack < Formula
   depends_on :fortran
   depends_on :mpi => [:optional, :f77]
   depends_on "openblas" => :optional
-  depends_on "veclibfort" if build.without? "openblas"
+  depends_on "veclibfort" if build.without? "openblas" and OS.mac?
 
   def install
     ENV.m64 if MacOS.prefer_64_bit?
@@ -26,8 +26,10 @@ class Arpack < Formula
     args << "--enable-mpi" if build.with? :mpi
     if build.with? "openblas"
       args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas"
-    else
+    elsif OS.mac?
       args << "--with-blas=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort"
+    else
+      args << "--with-blas=-lblas -llapack"
     end
 
     if build.with? :mpi
