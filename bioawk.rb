cd4c9d7c26bb416089404299aff98ddc9a4f8cb6
Saket Choudhary <saketkc@gmail.com>
2015-01-05 11:49:26 -0800

    bioawk 5e8b41d: Change single quotes to double

--- bioawk.rb
+++ bioawk.rb
@@ -1,21 +1,21 @@
-require 'formula'
+require "formula"
 
 class Bioawk < Formula
-  homepage 'https://github.com/lh3/bioawk'
+  homepage "https://github.com/lh3/bioawk"
   version "5e8b41d"
   url "https://github.com/lh3/bioawk/archive/#{version}.tar.gz"
   sha1 "1042e98bfa6a8601488df1be29eb758b2359826d"
-  head 'https://github.com/lh3/bioawk.git'
+  head "https://github.com/lh3/bioawk.git"
 
   def install
     ENV.j1
-    system 'make'
-    bin.install 'bioawk'
-    doc.install 'README.md'
-    man1.install({'awk.1' => 'bioawk.1'})
+    system "make"
+    bin.install "bioawk"
+    doc.install "README.md"
+    man1.install({"awk.1" => "bioawk.1"})
   end
 
   test do
-    system 'bioawk --version'
+    system "bioawk --version"
   end
 end
