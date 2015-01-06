46339b9e3821d59ec95ec70f8763ca07ef8689a6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-06 01:32:44 +0000

    hmmer: add 3.1b1 bottle.

--- hmmer.rb
+++ hmmer.rb
@@ -5,6 +5,14 @@ class Hmmer < Formula
   url "http://selab.janelia.org/software/hmmer3/3.1b1/hmmer-3.1b1.tar.gz"
   sha1 "e05907d28b7f03d4817bb714ff0a8b2ef0210220"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "7bbcead1760d3dbab4125bdead5cf82130a986f6" => :yosemite
+    sha1 "d7110164cd86dcfb8c500366fc669354a4ce6b6d" => :mavericks
+    sha1 "33da00cd8b9075f4a5437e8ed3f4b47019256623" => :mountain_lion
+  end
+
   option "without-check", "Skip build-time tests (not recommended)"
 
   head do
