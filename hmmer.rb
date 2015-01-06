8ac32ff1a0c4dd73c77c7c00e09b48739fad1d25
Ian Lancaster <ilancaster@gmail.com>
2015-01-05 20:24:42 -0500

    hmmer: style cleanup
    Closes #1635.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- hmmer.rb
+++ hmmer.rb
@@ -1,15 +1,14 @@
-require 'formula'
-
 class Hmmer < Formula
-  homepage 'http://hmmer.janelia.org/'
-  #doi '10.1371/journal.pcbi.1002195'
-  url 'http://selab.janelia.org/software/hmmer3/3.1b1/hmmer-3.1b1.tar.gz'
-  sha1 'e05907d28b7f03d4817bb714ff0a8b2ef0210220'
+  homepage "http://hmmer.janelia.org"
+  # doi "10.1371/journal.pcbi.1002195"
+  # tag "bioinformatics"
+  url "http://selab.janelia.org/software/hmmer3/3.1b1/hmmer-3.1b1.tar.gz"
+  sha1 "e05907d28b7f03d4817bb714ff0a8b2ef0210220"
 
   option "without-check", "Skip build-time tests (not recommended)"
 
   head do
-    url 'https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk'
+    url "https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk"
     depends_on "autoconf" => :build
   end
 
