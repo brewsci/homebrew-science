4b1b596d057f81c0e3bbdea3617b3d55010641a6
Ian Lancaster <ilancaster@gmail.com>
2015-01-06 22:19:38 -0500

    bowtie: fix build with yosemite
    Closes #1640.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- bowtie.rb
+++ bowtie.rb
@@ -1,7 +1,7 @@
 class Bowtie < Formula
   homepage "http://bowtie-bio.sourceforge.net/index.shtml"
-  #doi "10.1186/gb-2009-10-3-r25"
-  #tag "bioinformatics"
+  # doi "10.1186/gb-2009-10-3-r25"
+  # tag "bioinformatics"
 
   url "https://github.com/BenLangmead/bowtie/archive/v1.1.1.tar.gz"
   sha1 "297b0c56d3847a8cc11a4c03917c03bd6080d365"
@@ -16,15 +16,18 @@ class Bowtie < Formula
   end
 
   def install
+    ENV.libstdcxx
     system "make", "allall"
 
     # preserve directory structure for tests/scripts
-    libexec.install Dir["bowtie-*"]
-    libexec.install %w[bowtie scripts genomes indexes reads]
-
-    bin.install_symlink Dir["#{libexec}/bowtie*"]
-
-    doc.install %W[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS TUTORIAL]
+    libexec.install Dir["bowtie*"]
+    libexec.install %w[scripts genomes indexes reads]
+    bin.install_symlink %W[
+      #{libexec}/bowtie
+      #{libexec}/bowtie-build
+      #{libexec}/bowtie-inspect
+    ]
+    doc.install %w[AUTHORS LICENSE MANUAL MANUAL.markdown NEWS TUTORIAL]
 
     inreplace libexec/"scripts/test/simple_tests.pl" do |s|
       s.gsub! "$bowtie = \"\"", "$bowtie = \"#{bin}/bowtie\""
