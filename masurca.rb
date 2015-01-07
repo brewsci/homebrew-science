cb04196957c5185c6bc4b16f1078d24d4473f0ef
Shaun Jackman <sjackman@gmail.com>
2015-01-07 15:37:09 -0800

    masurca 2.2.1: Fix the build for Mac OS
    Closes #1650.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- masurca.rb
+++ masurca.rb
@@ -6,10 +6,26 @@ class Masurca < Formula
   url "ftp://ftp.genome.umd.edu/pub/MaSuRCA/v2.2.1/MaSuRCA-2.2.1.tar.gz"
   sha1 "a568d0afc9cf96e5351e8f4ef92c1b89a13011d6"
 
+  fails_with :clang do
+    build 600
+    cause "error: use of undeclared identifier 'use_safe_malloc_instead'"
+  end
+
   depends_on "parallel"
 
   def install
-    raise "MaSuRCA fails to build on Mac OS. See https://github.com/Homebrew/homebrew-science/issues/344" if OS.mac?
+    if OS.mac?
+      # Fix error: 'operator()' is not a member of 'reallocator<basic_charb<reallocator<char> > >'
+      inreplace "SuperReads/include/reallocators.hpp",
+        "reallocator<T>::operator()",
+        "reallocator<T>::realloc"
+
+      # Fix ld: library not found for -lrt
+      inreplace "SuperReads//Makefile.in", "-lrt", ""
+
+      # Fix cp: CA/Linux-amd64/bin/*: No such file or directory
+      inreplace "install.sh", "Linux-amd64", "Darwin-amd64"
+    end
 
     # Fix brew audit: Non-executables were installed to bin
     inreplace "SuperReads/src/fix_unitigs.sh", /^/, "#!/bin/sh\n"
