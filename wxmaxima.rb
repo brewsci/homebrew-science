91b297b1c6a3092a7d65fd3da8c4a86cf200c158
Ian Lancaster <ilancaster@gmail.com>
2015-01-08 00:42:48 -0500

    wxmaxima: add gnuplot caveat
    Remove redundant linkapp instructions.
    
    Closes #1654.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- wxmaxima.rb
+++ wxmaxima.rb
@@ -1,43 +1,38 @@
-require 'formula'
-
 class Wxmaxima < Formula
-  homepage 'http://andrejv.github.io/wxmaxima'
-  url 'https://downloads.sourceforge.net/project/wxmaxima/wxMaxima/13.04.2/wxMaxima-13.04.2.tar.gz'
-  sha1 '9508d3badb6c339f34e73e01c5065f679329a17c'
+  homepage "http://andrejv.github.io/wxmaxima"
+  url "https://downloads.sourceforge.net/project/wxmaxima/wxMaxima/13.04.2/wxMaxima-13.04.2.tar.gz"
+  sha1 "9508d3badb6c339f34e73e01c5065f679329a17c"
 
-  depends_on 'wxmac'
+  depends_on "wxmac"
 
   head do
-    url 'https://github.com/andrejv/wxmaxima.git'
-    depends_on 'autoconf' => :build
-    depends_on 'automake' => :build
-    depends_on 'gettext' => :build
+    url "https://github.com/andrejv/wxmaxima.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
   end
 
   def install
     system "./bootstrap" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system 'make'
-    cd 'locales' do
-      system 'make', 'allmo'
+    system "make"
+    cd "locales" do
+      system "make", "allmo"
     end
-    system 'make', 'wxMaxima.app'
-    prefix.install 'wxMaxima.app'
-    system "make install"
+    system "make", "wxMaxima.app"
+    prefix.install "wxMaxima.app"
+    system "make", "install"
   end
 
-  def caveats
-    <<-EOS.undent
-      The program you want to run is wxmaxima.app, and it gets installed into:
-        #{prefix}
+  def caveats; <<-EOS.undent
+    When you start wxMaxima the first time, set the path to Maxima
+    (e.g. #{HOMEBREW_PREFIX}/bin/maxima) in the Preferences.
 
-       To symlink it into Applications, you can type:
-         ln -s #{prefix}/wxmaxima.app /Applications
-
-       When you start wxmaxima the first time, you have to open Preferences,
-       and tell it where maxima is located.
-
+    Enable gnuplot functionality by setting the following variables
+    in ~/.maxima/maxima-init.mac:
+      gnuplot_command:"#{HOMEBREW_PREFIX}/bin/gnuplot"$
+      draw_command:"#{HOMEBREW_PREFIX}/bin/gnuplot"$
     EOS
   end
 end
