b0e3f001cd86eb4467aa725a51db0d264e3ea2fe
Ian Lancaster <ilancaster@gmail.com>
2015-01-08 18:39:41 -0500

    reapr: vend perl module dep
    Resolve dependency conflict with samtools.
    
    Closes #1670.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- reapr.rb
+++ reapr.rb
@@ -1,30 +1,43 @@
-require "formula"
-
 class Reapr < Formula
   homepage "https://www.sanger.ac.uk/resources/software/reapr/"
   url "ftp://ftp.sanger.ac.uk/pub/resources/software/reapr/Reapr_1.0.17.tar.gz"
   sha1 "4d2a8856a6d19d259aeec8591a024ba598003acc"
 
   depends_on "bamtools"
-  depends_on "samtools"
+  depends_on "samtools-0.1"
   depends_on "tabix"
 
-  depends_on "File::Spec::Link" => :perl
+  resource "File::Spec::Link" do
+    url "http://search.cpan.org/CPAN/authors/id/R/RM/RMBARKER/File-Copy-Link-0.140.tar.gz"
+    sha1 "515178bc42ab25eb2248bd5fb854e1c805fdf12d"
+  end
 
   def install
-    system *%W[make -C src/tabix]
-    system *%W[make -C src
-      CFLAGS=-I#{Formula["bamtools"].opt_include}/bamtools]
+    ENV.prepend_create_path "PERL5LIB", libexec+"lib/perl5"
+
+    resource("File::Spec::Link").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    system "make", "-C", "src/tabix"
+    system "make", "-C", "src",
+           "CFLAGS=-I#{Formula["bamtools"].opt_include}/bamtools"
     doc.install %w[README changelog.txt licence.txt]
+
     cd "src" do
-      libexec.install %w[bam2fcdEstimate bam2fragCov bam2insert
+      libexec.install %w[
+        bam2fcdEstimate bam2fragCov bam2insert
         bam2perfect fa2gaps fa2gc make_plots n50 scaff2contig
         task_break task_fcdrate task_gapresize task_score task_stats
         task_facheck.pl task_perfectfrombam.pl task_perfectmap.pl
         task_plots.pl task_preprocess.pl task_smaltmap.pl
-        task_summary.pl reapr.pl]
+        task_summary.pl reapr.pl
+      ]
     end
     bin.install_symlink libexec+"reapr.pl" => "reapr"
+    bin.env_script_all_files(libexec, :PERL5LIB => ENV["PERL5LIB"])
   end
 
   test do
