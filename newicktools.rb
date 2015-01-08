de05f4a33e322e54a36154322c247ce05cbae79e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-08 20:34:02 +0000

    newicktools: add 0.1 bottle.

--- newicktools.rb
+++ newicktools.rb
@@ -6,6 +6,14 @@ class Newicktools < Formula
   sha1 "debdb6a8182ff3716fd991e8cf3a5d7dfbee5262"
   head "https://github.com/lindenb/newicktools.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "4793277d4f2db8807184a1574f7dc908319cb758" => :yosemite
+    sha1 "909db193225821b2294e6650961e73b78fb496ac" => :mavericks
+    sha1 "b5b26082e106eee6ff7ef43adef5b0a36e5e10d4" => :mountain_lion
+  end
+
   depends_on "graphviz" => :optional
 
   def install
