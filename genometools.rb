32f1fc8146c3c4729cfad55ede76d389a55fd330
Ian Lancaster <ilancaster@gmail.com>
2015-01-07 23:30:32 -0500

    genometools: style cleanup
    Closes #1653.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- genometools.rb
+++ genometools.rb
@@ -1,16 +1,15 @@
-require "formula"
-
 class Genometools < Formula
   homepage "http://genometools.org/"
-  #doi "10.1109/TCBB.2013.68"
+  # doi "10.1109/TCBB.2013.68"
+  # tag "bioinformatics"
   url "http://genometools.org/pub/genometools-1.5.3.tar.gz"
   sha1 "aa2bd8e2fe7ca274d7c9aecda6f23ab6547d935a"
   head "https://github.com/genometools/genometools.git"
 
   option :universal
-  option 'with-check', "Run tests which require approximately one hour to run"
-  option 'without-pangocairo', 'Build without Pango/Cairo (disables AnnotationSketch tool)'
-  option 'with-hmmer', 'Build with HMMER (to enable protein domain search functionality in the ltrdigest tool)'
+  option "with-check", "Run tests which require approximately one hour to run"
+  option "without-pangocairo", "Build without Pango/Cairo (disables AnnotationSketch tool)"
+  option "with-hmmer", "Build with HMMER (to enable protein domain search functionality in the ltrdigest tool)"
 
   depends_on "pkg-config" => :build
 
@@ -22,18 +21,18 @@ class Genometools < Formula
   def install
     args = ["prefix=#{prefix}"]
     args << "cairo=no" if build.without? "pangocairo"
-    args << 'with-hmmer=yes' if build.with? 'hmmer'
-    args << 'universal=yes' if build.universal?
-    args << '64bit=yes' if MacOS.prefer_64_bit?
+    args << "with-hmmer=yes" if build.with? "hmmer"
+    args << "universal=yes" if build.universal?
+    args << "64bit=yes" if MacOS.prefer_64_bit?
 
-    system 'make', *args
-    system 'make', 'test', *args if build.with? 'check'
-    system 'make', 'install', *args
+    system "make", *args
+    system "make", "test", *args if build.with? "check"
+    system "make", "install", *args
 
-    (share/'genometools').install bin/'gtdata'
+    (share/"genometools").install bin/"gtdata"
   end
 
   test do
-    system "#{bin}/gt -test"
+    system "#{bin}/gt", "-test"
   end
 end
