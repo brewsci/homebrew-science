5aff5786ace712af57a925f9a0991c77bf8f30b6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-08 00:43:39 +0000

    lumpy-sv: update 0.2.9 bottle.

--- lumpy-sv.rb
+++ lumpy-sv.rb
@@ -8,9 +8,9 @@ class LumpySv < Formula
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
     cellar :any
-    sha1 "a5a489043b490388f615f62ab0b1c10edeaf60f5" => :yosemite
-    sha1 "6d8b7dc39b39e0cb351fb67254562f25b8e4deca" => :mavericks
-    sha1 "b7d514ef6a9f96feaeab07bda98a17d1c53cc608" => :mountain_lion
+    sha1 "76b8040a6bb5586829cee50723ada9b0a08c95db" => :yosemite
+    sha1 "347569ef2f558c13cc860857113e287ed046fb31" => :mavericks
+    sha1 "9a951d727f67fc9a315d26a260d0ad4f1b69cd6f" => :mountain_lion
   end
 
   depends_on "bamtools" => :recommended
