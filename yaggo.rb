0171ab5dcf9ce9cfea0dd039b1ca7bbbca15334f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-08 18:36:15 +0000

    yaggo: add 1.5.4 bottle.

--- yaggo.rb
+++ yaggo.rb
@@ -4,6 +4,14 @@ class Yaggo < Formula
   sha1 "fc31de9ef3d0d0bdc2f01ebeea05b612bf1f7847"
   head "https://github.com/gmarcais/yaggo.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "ede41d5f56f1891e7f8cded47408a336cda5a06a" => :yosemite
+    sha1 "1f29452de12580785532298d047cdce559369784" => :mavericks
+    sha1 "a8d79edd5fecd2fb69c6b79c8fd3725c23bc3202" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "yaggo"
