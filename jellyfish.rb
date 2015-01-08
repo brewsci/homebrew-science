d1e59e5b96542c231d8b7b34b0a79b0a0e6975f8
Shaun Jackman <sjackman@gmail.com>
2015-01-08 10:17:34 -0800

    jellyfish 2.1.4: Add head and test
    Closes #1660.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- jellyfish.rb
+++ jellyfish.rb
@@ -1,12 +1,10 @@
-require "formula"
-
 class Jellyfish < Formula
   homepage "http://www.genome.umd.edu/jellyfish.html"
   #doi "10.1093/bioinformatics/btr011"
   #tag "bioinformatics"
 
-  url "ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.3.tar.gz"
-  sha1 "b115dc7066cf24ab4026d8d8d429f094f3917cf2"
+  url "ftp://ftp.genome.umd.edu/pub/jellyfish/jellyfish-2.1.4.tar.gz"
+  sha1 "37ddd37c1eb16031716f40c732516c30fbf37544"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -15,11 +13,22 @@ class Jellyfish < Formula
     sha1 "eaab1b21b747f6f252f9d5ec4c9a5fd6ed3e9d27" => :mavericks
   end
 
-  #head "https://github.com/gmarcais/Jellyfish.git"
+  head do
+    url "https://github.com/gmarcais/Jellyfish.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "yaggo" => :build
+  end
 
   def install
+    system "autoreconf", "-i" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jellyfish", "--version"
   end
 end
