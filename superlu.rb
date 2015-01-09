c8c835be092d25185e1e59c64b48d798a375e0b8
Dominique Orban <dominique.orban@gmail.com>
2015-01-09 15:34:56 -0500

    SuperLU: Fix audit warnings, add `test do` block.
    Closes #1677.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- superlu.rb
+++ superlu.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Superlu < Formula
   homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/"
   url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz"
@@ -18,7 +16,7 @@ class Superlu < Formula
   option "without-check", "skip build-time tests (not recommended)"
 
   depends_on :fortran
-  depends_on 'openblas' => :optional
+  depends_on "openblas" => :optional
 
   def install
     ENV.deparallelize
@@ -33,13 +31,18 @@ class Superlu < Formula
     system "make", "lib", *make_args
     if build.with? "check"
       system "make", "testing", *make_args
-      cd "TESTING"
-      system "make", *make_args
-      %w[stest dtest ctest ztest].each do |tst|
-        ohai `tail -1 #{tst}.out`.chomp
+      cd "TESTING" do
+        system "make", *make_args
+        %w[stest dtest ctest ztest].each do |tst|
+          ohai `tail -1 #{tst}.out`.chomp
+        end
       end
-      cd ".."
     end
+
+    cd "EXAMPLE" do
+      system "make", *make_args
+    end
+
     prefix.install "make.inc"
     File.open(prefix / "make_args.txt", "w") do |f|
       f.puts(make_args.join(" "))  # Record options passed to make.
@@ -47,6 +50,40 @@ class Superlu < Formula
     lib.install Dir["lib/*"]
     (include / "superlu").install Dir["SRC/*.h"]
     doc.install Dir["Doc/*"]
-    (share / "superlu").install "EXAMPLE"
+    (share / "superlu").install Dir["EXAMPLE/*[^.o]"]
+  end
+
+  test do
+    cd share / "superlu" do
+      system "./superlu"
+      system "./slinsol < g20.rua"
+      system "./slinsolx  < g20.rua"
+      system "./slinsolx1 < g20.rua"
+      system "./slinsolx2 < g20.rua"
+
+      system "./dlinsol < g20.rua"
+      system "./dlinsolx  < g20.rua"
+      system "./dlinsolx1 < g20.rua"
+      system "./dlinsolx2 < g20.rua"
+
+      system "./clinsol < cg20.cua"
+      system "./clinsolx < cg20.cua"
+      system "./clinsolx1 < cg20.cua"
+      system "./clinsolx2 < cg20.cua"
+
+      system "./zlinsol < cg20.cua"
+      system "./zlinsolx < cg20.cua"
+      system "./zlinsolx1 < cg20.cua"
+      system "./zlinsolx2 < cg20.cua"
+
+      system "./sitersol -h < g20.rua"
+      system "./sitersol1 -h < g20.rua"
+      system "./ditersol -h < g20.rua"
+      system "./ditersol1 -h < g20.rua"
+      system "./citersol -h < g20.rua"
+      system "./citersol1 -h < g20.rua"
+      system "./zitersol -h < cg20.cua"
+      system "./zitersol1 -h < cg20.cua"
+    end
   end
 end
