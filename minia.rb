288e8eaa94f73e6612e352ecaa2ad07e4c5b5184
Shaun Jackman <sjackman@gmail.com>
2015-01-09 14:47:11 -0800

    minia 1.6906
    Closes #1678.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- minia.rb
+++ minia.rb
@@ -1,17 +1,18 @@
-require 'formula'
-
 class Minia < Formula
-  homepage 'http://minia.genouest.org/'
-  url 'http://minia.genouest.org/files/minia-1.6088.tar.gz'
-  sha1 'ce3252e4fce9c86d3bba9cac9a3a90f50cfb3284'
+  homepage "http://minia.genouest.org/"
+  #doi "10.1186/1748-7188-8-22"
+  #tag "bioinformatics"
+
+  url "http://minia.genouest.org/files/minia-1.6906.tar.gz"
+  sha1 "f54003afbd4e2f3e8b52db08e1d7fca644e751fa"
 
   def install
-    system 'make'
-    bin.install 'minia'
-    doc.install 'README', 'manual/manual.pdf'
+    system "make"
+    bin.install "minia"
+    doc.install "README", "manual/manual.pdf"
   end
 
   test do
-    system 'minia 2>&1 |grep -q minia'
+    system "#{bin}/minia 2>&1 |grep -q minia"
   end
 end
