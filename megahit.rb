31fab41457c08266bec2bc62b6d196c8e9b7af08
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-12 06:57:45 +0000

    megahit: add 0.1.3 bottle.

--- megahit.rb
+++ megahit.rb
@@ -8,6 +8,14 @@ class Megahit < Formula
 
   head "https://github.com/voutcn/megahit.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "98659f38c059b1196c92263440b25ee3e2b7f301" => :yosemite
+    sha1 "045e00905aff83cabb3794e2f96940439a7c5caf" => :mavericks
+    sha1 "d728fe0a0f9a1041822befe6875b7e3e4591bb7b" => :mountain_lion
+  end
+
   # Fix error: 'omp.h' file not found
   needs :openmp
 
