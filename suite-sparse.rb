9ed3f8ebb5d8b6f4702c9c895c774a6551b0ab84
Dominique Orban <dominique.orban@gmail.com>
2015-01-11 21:56:20 -0500

    SuiteSparse: Update home and url. Fix style issues.
    Closes #1684.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- suite-sparse.rb
+++ suite-sparse.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class SuiteSparse < Formula
-  homepage 'http://www.cise.ufl.edu/research/sparse/SuiteSparse'
-  url 'http://www.cise.ufl.edu/research/sparse/SuiteSparse/SuiteSparse-4.2.1.tar.gz'
-  mirror 'http://pkgs.fedoraproject.org/repo/pkgs/suitesparse/SuiteSparse-4.2.1.tar.gz/4628df9eeae10ae5f0c486f1ac982fce/SuiteSparse-4.2.1.tar.gz'
-  sha1 '2fec3bf93314bd14cbb7470c0a2c294988096ed6'
+  homepage "http://faculty.cse.tamu.edu/davis/suitesparse.html"
+  url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.2.1.tar.gz"
+  mirror "http://pkgs.fedoraproject.org/repo/pkgs/suitesparse/SuiteSparse-4.2.1.tar.gz/4628df9eeae10ae5f0c486f1ac982fce/SuiteSparse-4.2.1.tar.gz"
+  sha1 "2fec3bf93314bd14cbb7470c0a2c294988096ed6"
 
   option "with-matlab", "Install Matlab interfaces and tools"
   option "with-matlab-path=", "Path to Matlab executable (default: matlab)"
@@ -21,23 +19,29 @@ class SuiteSparse < Formula
     ENV.deparallelize
 
     # Switch to the Mac base config, per SuiteSparse README.txt
-    system "mv SuiteSparse_config/SuiteSparse_config.mk SuiteSparse_config/SuiteSparse_config_orig.mk"
-    system "mv SuiteSparse_config/SuiteSparse_config_Mac.mk SuiteSparse_config/SuiteSparse_config.mk"
+    mv "SuiteSparse_config/SuiteSparse_config.mk",
+       "SuiteSparse_config/SuiteSparse_config_orig.mk"
+    mv "SuiteSparse_config/SuiteSparse_config_Mac.mk",
+       "SuiteSparse_config/SuiteSparse_config.mk"
 
     make_args = ["INSTALL_LIB=#{lib}", "INSTALL_INCLUDE=#{include}"]
-    make_args << "BLAS=" + ((build.with? 'openblas') ? "-L#{Formula['openblas'].opt_lib} -lopenblas" : "-framework Accelerate")
+    make_args << "BLAS=" + ((build.with? "openblas") ? "-L#{Formula["openblas"].opt_lib} -lopenblas" : "-framework Accelerate")
     make_args << "LAPACK=$(BLAS)"
-    make_args += ["SPQR_CONFIG=-DHAVE_TBB", "TBB=-L#{Formula['tbb'].opt_lib} -ltbb"] if build.with? "tbb"
-    make_args += ["METIS_PATH=", "METIS=-L#{Formula['metis4'].opt_lib} -lmetis"] if build.with? "metis4"
+    make_args += ["SPQR_CONFIG=-DHAVE_TBB",
+                  "TBB=-L#{Formula["tbb"].opt_lib} -ltbb"] if build.with? "tbb"
+    make_args += ["METIS_PATH=",
+                  "METIS=-L#{Formula["metis4"].opt_lib} -lmetis"] if build.with? "metis4"
 
-    system "make", "library", *make_args
+    system "make", "default", *make_args  # Also build demos.
     lib.mkpath
     include.mkpath
     system "make", "install", *make_args
 
     matlab = ARGV.value("with-matlab-path") || "matlab"
     if build.with? "matlab"
-      system matlab, "-nodesktop", "-nosplash", "-r", "run('SuiteSparse_install(false)'); exit;"
+      system matlab,
+             "-nodesktop", "-nosplash",
+             "-r", "run('SuiteSparse_install(false)'); exit;"
 
       # Install Matlab scripts and Mex files.
       %w[AMD BTF CAMD CCOLAMD CHOLMOD COLAMD CSparse CXSparse KLU LDL SPQR UMFPACK].each do |m|
@@ -59,6 +63,6 @@ class SuiteSparse < Formula
           #{share}/suite-sparse/matlab
       EOS
     end
-    return s
+    s
   end
 end
