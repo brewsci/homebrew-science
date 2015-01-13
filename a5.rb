b37b306b60ea8b7c868b0d02bf5ab2184f93f300
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-13 02:15:03 +0000

    a5: add 20141120 bottle.

--- a5.rb
+++ a5.rb
@@ -5,6 +5,12 @@ class A5 < Formula
 
   if OS.mac?
     url "https://downloads.sourceforge.net/project/ngopt/a5_miseq_macOS_20141120.tar.gz"
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "3c6267d80a3b0100394b900793ea23c109d2593c" => :mountain_lion
+  end
+
     sha1 "720728be842202345892dd76a74784c4f2eddbaa"
   elsif OS.linux?
     url "https://downloads.sourceforge.net/project/ngopt/a5_miseq_linux_20141120.tar.gz"
