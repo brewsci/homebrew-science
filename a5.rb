a56052c2e18807276f87664a55441825243d5f4e
Shaun Jackman <sjackman@gmail.com>
2015-01-12 17:50:37 -0800

    a5 20141120: New formula
    Andrew And Aaron’s Awesome Assembly pipeline
    
    Closes #1690.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ a5.rb
@@ -0,0 +1,23 @@
+class A5 < Formula
+  homepage "https://sourceforge.net/projects/ngopt/"
+  #doi "10.1371/journal.pone.0042304", "arXiv:1401.5130"
+  #tag "bioinformatics"
+
+  if OS.mac?
+    url "https://downloads.sourceforge.net/project/ngopt/a5_miseq_macOS_20141120.tar.gz"
+    sha1 "720728be842202345892dd76a74784c4f2eddbaa"
+  elsif OS.linux?
+    url "https://downloads.sourceforge.net/project/ngopt/a5_miseq_linux_20141120.tar.gz"
+    sha1 "3a9bfdecb519064db98065cb3cb525705c641207"
+  end
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "../libexec/bin/a5_pipeline.pl"
+  end
+
+  test do
+    system "#{bin}/a5_pipeline.pl --version 2>&1 |grep A5"
+    system "#{libexec}/test.a5.sh"
+  end
+end
