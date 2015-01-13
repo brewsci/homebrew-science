5d56d8036127b1b2e38a369e6d2aaeb7cc162c36
Ian Lancaster <ilancaster@gmail.com>
2015-01-12 20:54:29 -0500

    nauty: style nits
    Reword -checks option.

--- nauty.rb
+++ nauty.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Nauty < Formula
   homepage "http://cs.anu.edu.au/~bdm/nauty/"
   url "http://cs.anu.edu.au/~bdm/nauty/nauty25r9.tar.gz"
@@ -15,7 +13,8 @@ class Nauty < Formula
   end
 
   deprecated_option "run-tests" => "with-checks"
-  option "without-checks", "Runs the included test programs"
+
+  option "without-checks", "Skip building the included test programs"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -23,17 +22,19 @@ class Nauty < Formula
     system "make", "all"
     system "make", "checks" if build.with? "checks"
 
-    bin.install %w{ NRswitchg addedgeg amtog biplabg catg complg copyg countg
+    bin.install %w[
+      NRswitchg addedgeg amtog biplabg catg complg copyg countg
       deledgeg directg dreadnaut dretog genbg geng genrang gentourng labelg
       linegraphg listg multig newedgeg pickg planarg ranlabg shortg showg subdivideg
-      watercluster2 }
+      watercluster2
+    ]
 
     prefix.install "nug25.pdf"
   end
 
   def caveats; <<-EOS.undent
     User guide was saved locally to:
-      #{prefix}/nug25.pdf
+      #{opt_prefix}/nug25.pdf
     EOS
   end
 end
