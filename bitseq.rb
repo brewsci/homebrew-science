6766fb4c8acb3ff21aa5be85ff21ccaa4ea0d764
Ian Lancaster <ilancaster@gmail.com>
2015-01-15 00:29:13 -0500

    bitseq: style cleanup

--- bitseq.rb
+++ bitseq.rb
@@ -1,13 +1,9 @@
-require 'formula'
-
 class Bitseq < Formula
-  homepage 'http://code.google.com/p/bitseq/'
-  url 'https://bitseq.googlecode.com/files/BitSeq-0.4.3.tar.gz'
-  sha1 '603feb5fccdd95d496c27fe78e7f1f81e46bc1ed'
+  homepage "https://code.google.com/p/bitseq/"
+  url "https://bitseq.googlecode.com/files/BitSeq-0.4.3.tar.gz"
+  sha1 "603feb5fccdd95d496c27fe78e7f1f81e46bc1ed"
 
-  fails_with :clang do
-    cause "bitseq requires OpenMP support"
-  end
+  needs :openmp
 
   def install
     system "make"
@@ -26,6 +22,6 @@ class Bitseq < Formula
   end
 
   test do
-    system "#{bin}/parseAlignment --help"
+    system "#{bin}/parseAlignment", "--help"
   end
 end
