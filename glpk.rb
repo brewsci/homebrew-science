f64d1bfe5bb51d83a6a65ced7e605c52e34bd46a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-15 06:11:36 +0000

    glpk: add 4.52 bottle.

--- glpk.rb
+++ glpk.rb
@@ -4,6 +4,14 @@ class Glpk < Formula
   mirror "https://ftp.gnu.org/gnu/glpk/glpk-4.52.tar.gz"
   sha1 "44b30b0de777a0a07e00615ac6791af180ff4d2c"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "0ccfb05a60ae7ddbc9c53d5333b0a51b3e86694e" => :yosemite
+    sha1 "da31c189499c716772e13ec8037ae896d01866ab" => :mavericks
+    sha1 "0d02d5430580f440c1477996dffa596871931796" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
