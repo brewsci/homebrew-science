ea7d5c4dd4b15c682d61edade036ccebe05e1efa
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-15 18:01:52 +0000

    nextflow: add 0.12.0 bottle.

--- nextflow.rb
+++ nextflow.rb
@@ -4,6 +4,14 @@ class Nextflow < Formula
   homepage "http://www.nextflow.io/"
   head "https://github.com/nextflow-io/nextflow.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "d1a995f74179f52db106a2fd7f6ad6fd9518706b" => :yosemite
+    sha1 "1209d43c4b6f965f9ea90890aee641412c3b5240" => :mavericks
+    sha1 "287cf8196abbd6c11b457793403914333f78bb81" => :mountain_lion
+  end
+
   depends_on :java => "1.7"
 
   version "0.12.0"
