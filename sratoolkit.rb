20ead55e03ce3a3b7a09ff108c8e65d5ced874ca
Shaun Jackman <sjackman@gmail.com>
2015-01-16 15:07:26 -0800

    sratoolkit 2.4.3
    Closes #1714.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sratoolkit.rb
+++ sratoolkit.rb
@@ -3,13 +3,13 @@ class Sratoolkit < Formula
   # doi "10.1093/nar/gkq1019"
   # tag "bioinformatics"
 
-  version "2.3.5-2"
+  version "2.4.3"
   if OS.mac?
     url "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/#{version}/sratoolkit.#{version}-mac64.tar.gz"
-    sha1 "7238ec47089710c626a6fe0610382653c0767cec"
+    sha1 "baf9312d7185593e514806744cab7077b808a598"
   elsif OS.linux?
     url "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/#{version}/sratoolkit.#{version}-ubuntu64.tar.gz"
-    sha1 "9e05eb581cdc0a9cde794042800cfea5532c3b94"
+    sha1 "da07935448a2a1567c591e514c667aa9e3846ec3"
   end
 
   head "https://github.com/NCBITools/sratoolkit.git"
