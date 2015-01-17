e0f8b7c69c01ef1664337d90140b4d554bfa7377
Ian Lancaster <ilancaster@gmail.com>
2015-01-17 18:25:48 -0500

    orocos-kdl 1.3.0
    Fixes #1712.
    Closes #1718.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- orocos-kdl.rb
+++ orocos-kdl.rb
@@ -1,47 +1,30 @@
-require "formula"
-
 class OrocosKdl < Formula
   homepage "http://www.orocos.org/kdl"
+  url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.3.0.tar.gz"
+  sha1 "9f59b44d683fcdf290affd71f8a0179bded0bf1d"
+  head "https://github.com/orocos/orocos_kinematics_dynamics.git"
 
-  stable do
-    url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.2.2.tar.gz"
-    sha1 "cfd18664a615c2babde96fb839b9bbc5dcd7a3ff"
-    depends_on "eigen2"
-  end
+  option "without-check", "Disable build-time checking"
 
-  head do
-    url "https://github.com/orocos/orocos_kinematics_dynamics.git"
-    depends_on "eigen"
-  end
-
-  option "with-check", "Enable build-time checking (requires that cppunit was built with gcc)"
-
+  depends_on "eigen"
   depends_on "cmake"   => :build
-  depends_on "cppunit" => :build if build.with? "check"
-
-  fails_with :clang do
-    build 503
-    cause <<-EOS.undent
-      More information at this webpage:
-      http://answers.ros.org/question/94771/building-ros-on-osx-109-solution/
-      which says that a patch has been submitted upstream.
-    EOS
-  end
+  depends_on "cppunit" => :build
+  depends_on "boost"   => :build
 
   def install
-    cd "orocos_kdl"
+    cd "orocos_kdl" do
+      # Removes solvertest from orocos-kdl, as per
+      # http://www.orocos.org/forum/rtt/rtt-dev/bug-1043-new-errors-underlying-ik-solver-are-not-correctly-processed
+      inreplace "tests/CMakeLists.txt", "ADD_TEST(solvertest solvertest)", "" if build.head?
 
-    # Removes solvertest from orocos-kdl, as per
-    # http://www.orocos.org/forum/rtt/rtt-dev/bug-1043-new-errors-underlying-ik-solver-are-not-correctly-processed
-    inreplace "tests/CMakeLists.txt", "ADD_TEST(solvertest solvertest)", "" if build.head?
-
-    mkdir "build" do
-      args = std_cmake_args
-      args << "-DENABLE_TESTS=ON" if build.with? "check"
-      system "cmake", "..", *args
-      system "make"
-      system "make check" if build.with? "check"
-      system "make install"
+      mkdir "build" do
+        args = std_cmake_args
+        args << "-DENABLE_TESTS=ON" if build.with? "check"
+        system "cmake", "..", *args
+        system "make"
+        system "make", "check" if build.with? "check"
+        system "make", "install"
+      end
     end
   end
 end
