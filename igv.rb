32d632a00a3ffcae1006c70428e08ab4bf250abb
Ian Lancaster <ilancaster@gmail.com>
2015-01-17 21:58:48 -0500

    igv 2.3.40

--- igv.rb
+++ igv.rb
@@ -1,21 +1,20 @@
-require 'formula'
-
 class Igv < Formula
-  homepage 'http://www.broadinstitute.org/software/igv'
-  #doi '10.1093/bib/bbs017'
-  head 'https://github.com/broadinstitute/IGV.git'
-  url 'http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.34.zip'
-  sha1 '1aeadbf2222f0adddd6b68b3171eb02184b7aa41'
+  homepage "http://www.broadinstitute.org/software/igv"
+  # tag "bioinformatics"
+  # doi "10.1093/bib/bbs017"
+  head "https://github.com/broadinstitute/IGV.git"
+  url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.40.zip"
+  sha1 "5060e7d9de990b83bbf7ceb8103baffb9409c010"
 
   def install
-    inreplace 'igv.sh', /^prefix=.*/, 'prefix=' + libexec
-    libexec.install Dir['igv.sh', '*.jar']
-    bin.install_symlink '../libexec/igv.sh' => 'igv'
-    doc.install 'readme.txt'
+    inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
+    libexec.install Dir["igv.sh", "*.jar"]
+    bin.install_symlink libexec/"igv.sh" => "igv"
+    doc.install "readme.txt"
   end
 
   test do
-    (testpath / 'script').write 'exit'
-    system 'igv -b script |grep -q IGV'
+    (testpath/"script").write "exit"
+    system "igv", "-b", "script", "|grep", "-q", "IGV"
   end
 end
