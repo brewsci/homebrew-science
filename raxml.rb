12cc53accc548667ef6965b674f65ee86383818b
Ian Lancaster <ilancaster@gmail.com>
2015-01-17 22:15:58 -0500

    raxml 8.1.15

--- raxml.rb
+++ raxml.rb
@@ -1,16 +1,21 @@
-require 'formula'
-
 class Raxml < Formula
-  homepage 'http://sco.h-its.org/exelixis/web/software/raxml/index.html'
-  url 'https://github.com/stamatak/standard-RAxML/archive/v8.0.20.tar.gz'
-  sha1 '91eaa19974eefc869ad60b5224a0c8c31c88795b'
-
-  head 'https://github.com/stamatak/standard-RAxML.git'
+  homepage "http://sco.h-its.org/exelixis/web/software/raxml/index.html"
+  url "https://github.com/stamatak/standard-RAxML/archive/v8.1.15.tar.gz"
+  sha1 "6bc3ba99788d59be197ffb949daf85ba0f1b2860"
+  head "https://github.com/stamatak/standard-RAxML.git"
 
   def install
     system "make", "-f", "Makefile.PTHREADS.gcc"
-    (rm Dir["*.o"] and system "make", "-f", "Makefile.SSE3.PTHREADS.gcc") if Hardware::CPU.sse3?
-    (rm Dir["*.o"] and system "make", "-f", "Makefile.AVX.PTHREADS.gcc") if Hardware::CPU.avx?
+
+    if Hardware::CPU.sse3?
+      rm Dir["*.o"]
+      system "make", "-f", "Makefile.SSE3.PTHREADS.gcc"
+    end
+    if Hardware::CPU.avx?
+      rm Dir["*.o"]
+      system "make", "-f", "Makefile.AVX.PTHREADS.gcc"
+    end
+
     bin.install Dir["raxmlHPC-*"]
   end
 
