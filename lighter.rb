a37808c09628d8101393950e41dbac0faacc6d4e
Ian Lancaster <ilancaster@gmail.com>
2015-01-18 03:22:19 -0500

    lighter 1.0.4
    Closes #1723.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- lighter.rb
+++ lighter.rb
@@ -1,10 +1,11 @@
-require "formula"
-
 class Lighter < Formula
   homepage "https://github.com/mourisl/Lighter"
-  url "https://github.com/mourisl/Lighter/archive/v1.0.3.tar.gz"
+  # tag 'bioinformatics'
+  # doi '10.1186/s13059-014-0509-9'
+  url "https://github.com/mourisl/Lighter/archive/v1.0.4.tar.gz"
   head "https://github.com/mourisl/Lighter.git"
-  sha1 "60451effb3b4185f5d2f0a2862dfb9b56e8ffe2c"
+  sha1 "32d42434999071dcca3b234d199b948f231d2932"
+
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
     cellar :any
@@ -13,9 +14,6 @@ class Lighter < Formula
     sha1 "a2514b84f19e238e77905f574cbbcadc5c6c7e99" => :mountain_lion
   end
 
-  #tag 'bioinformatics'
-  #doi '10.1186/s13059-014-0509-9'
-
   def install
     system "make"
     bin.install "lighter"
@@ -23,6 +21,6 @@ class Lighter < Formula
   end
 
   test do
-    system "#{bin}/lighter -h"
+    system "#{bin}/lighter", "-h"
   end
 end
