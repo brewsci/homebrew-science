f989b8f641740cee4af169020578da67816d45f0
Dominique Orban <dominique.orban@gmail.com>
2015-01-19 12:01:38 -0500

    Arpack: clean up formula.
    Closes #1728.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- arpack.rb
+++ arpack.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Arpack < Formula
-  homepage 'http://forge.scilab.org/index.php/p/arpack-ng'
-  url 'http://forge.scilab.org/index.php/p/arpack-ng/downloads/get/arpack-ng_3.1.4.tar.gz'
-  sha1 '1fb817346619b04d8fcdc958060cc0eab2c73c6f'
-  head 'git://git.forge.scilab.org/arpack-ng.git'
+  homepage "http://forge.scilab.org/index.php/p/arpack-ng"
+  url "http://forge.scilab.org/index.php/p/arpack-ng/downloads/get/arpack-ng_3.1.4.tar.gz"
+  sha1 "1fb817346619b04d8fcdc958060cc0eab2c73c6f"
+  head "git://git.forge.scilab.org/arpack-ng.git"
   revision 1
 
   bottle do
@@ -18,12 +16,13 @@ class Arpack < Formula
   depends_on :fortran
   depends_on :mpi => [:optional, :f77]
   depends_on "openblas" => :optional
-  depends_on "veclibfort" if build.without? "openblas" and OS.mac?
+  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
 
   def install
     ENV.m64 if MacOS.prefer_64_bit?
 
-    args = ["--disable-dependency-tracking", "--prefix=#{libexec}"]
+    cc_args = (build.with? :mpi) ? ["F77=#{ENV["MPIF77"]}"] : []
+    args = cc_args + ["--disable-dependency-tracking", "--prefix=#{libexec}"]
     args << "--enable-mpi" if build.with? :mpi
     if build.with? "openblas"
       args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas"
@@ -33,24 +32,18 @@ class Arpack < Formula
       args << "--with-blas=-lblas -llapack"
     end
 
-    if build.with? :mpi
-      ENV['CC']  = "mpicc"
-      ENV['CXX'] = "mpic++"
-      ENV['F77'] = "mpif77"
-    end
-
     system "./configure", *args
     system "make"
     system "make", "install"
     lib.install_symlink Dir["#{libexec}/lib/*"].select { |f| File.file?(f) }
-    (lib/'pkgconfig').install_symlink Dir["#{libexec}/lib/pkgconfig/*"]
-    (libexec/"share").install "TESTS/testA.mtx"
+    (lib / "pkgconfig").install_symlink Dir["#{libexec}/lib/pkgconfig/*"]
+    (libexec / "share").install "TESTS/testA.mtx"
   end
 
   test do
     cd libexec/"share" do
       ["dnsimp", "bug_1323"].each do |slv|
-        system "#{libexec}/bin/#{slv}"              # Reads testA.mtx
+        system "#{libexec}/bin/#{slv}"  # Reads testA.mtx
       end
     end
   end
