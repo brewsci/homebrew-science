d708cd4b8c18b650c70f0f19638ff7ffe8402c1c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-20 03:27:25 +0000

    bamutil: add 1.0.12 bottle.

--- bamutil.rb
+++ bamutil.rb
@@ -5,6 +5,14 @@ class Bamutil < Formula
   sha1 "55fb13337c71e1bcd3301bac5636d62b13cd2388"
   head "https://github.com/statgen/bamUtil.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "3e59cc21fe42f8399875c07f4797f6eab76fc5c5" => :yosemite
+    sha1 "18fd138775828456a819567b8b57e97999d8cfb9" => :mavericks
+    sha1 "ea3f2e30e44016b1adeff118bb6a627df2a4b905" => :mountain_lion
+  end
+
   def install
     ENV.j1
     system "make", "cloneLib" if build.head?
