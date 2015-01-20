a49b8877aba4d89daabf2288609830ec1fe578a5
Shaun Jackman <sjackman@gmail.com>
2015-01-19 16:12:17 -0800

    tbl2asn 23.9: Add tag bioinformatics
    Closes #1729.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -1,7 +1,7 @@
-require "formula"
-
 class Tbl2asn < Formula
   homepage "https://www.ncbi.nlm.nih.gov/genbank/tbl2asn2/"
+  #tag "bioinformatics"
+
   version "23.9"
   if OS.mac?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/mac.tbl2asn.gz"
@@ -9,13 +9,11 @@ class Tbl2asn < Formula
   elsif OS.linux?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
     sha1 "581a07dae7a3080d2f07f33552204ed796b572c1"
-  else
-    raise "Unknown operating system"
   end
 
-  resource 'doc' do
-    url 'ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/tbl2asn.txt'
-    sha1 '7b42a95b10aa6493e24074372b493dfa6fd9c9ce'
+  resource "doc" do
+    url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/tbl2asn.txt"
+    sha1 "7b42a95b10aa6493e24074372b493dfa6fd9c9ce"
   end
 
   def install
@@ -24,10 +22,10 @@ class Tbl2asn < Formula
     elsif OS.linux?
       bin.install "linux64.tbl2asn" => "tbl2asn"
     end
-    doc.install resource('doc')
+    doc.install resource("doc")
   end
 
   test do
-    system "#{bin}/tbl2asn --help"
+    system "#{bin}/tbl2asn", "--help"
   end
 end
