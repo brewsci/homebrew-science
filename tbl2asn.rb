d6087348a170cc61a1d28cef24922214490e6a08
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-20 00:21:16 +0000

    tbl2asn: add 23.9 bottle.

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -11,6 +11,14 @@ class Tbl2asn < Formula
     sha1 "581a07dae7a3080d2f07f33552204ed796b572c1"
   end
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "fc5c56b3791194cfdf272b90b0735180e9d33b88" => :yosemite
+    sha1 "39d37f0daead4cf2bf6e3184987f3072e569af0a" => :mavericks
+    sha1 "143d56cb341b6db292d156f2f85c819e3ac240d3" => :mountain_lion
+  end
+
   resource "doc" do
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/tbl2asn.txt"
     sha1 "7b42a95b10aa6493e24074372b493dfa6fd9c9ce"
