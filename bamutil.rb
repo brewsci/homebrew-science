913c44aaf1290d177872da43957bd54ecfddcfe6
Ian Lancaster <ilancaster@gmail.com>
2015-01-19 21:56:48 -0500

    bamutil 1.0.12
    Closes #1730.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- bamutil.rb
+++ bamutil.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class Bamutil < Formula
-  homepage 'http://genome.sph.umich.edu/wiki/BamUtil'
-  url 'http://genome.sph.umich.edu/w/images/7/77/BamUtilLibStatGen.1.0.11.tar.gz'
-  sha1 'aada189439704d3881378566e16cb2cc789f43a1'
-
-  head 'https://github.com/statgen/bamUtil.git'
+  homepage "http://genome.sph.umich.edu/wiki/BamUtil"
+  # tag "bioinformatics"
+  url "http://genome.sph.umich.edu/w/images/5/5e/BamUtilLibStatGen.1.0.12.tar.gz"
+  sha1 "55fb13337c71e1bcd3301bac5636d62b13cd2388"
+  head "https://github.com/statgen/bamUtil.git"
 
   def install
     ENV.j1
-    system 'make', 'cloneLib' if build.head?
-    system 'make', 'install', "INSTALLDIR=#{bin}"
+    system "make", "cloneLib" if build.head?
+    system "make", "install", "INSTALLDIR=#{bin}"
   end
 
   test do
-    system 'bam 2>&1 |grep -q BAM'
+    system "bam 2>&1 |grep -q BAM"
   end
 end
