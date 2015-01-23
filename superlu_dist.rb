6cf1516294b3fc0ef5f32c231acc09cb1143d52f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-23 20:31:51 +0000

    superlu_dist: update 3.3_1 bottle.

--- superlu_dist.rb
+++ superlu_dist.rb
@@ -6,9 +6,10 @@ class SuperluDist < Formula
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "5edab91513d1ef5efff95e93b30a037027338fb2" => :yosemite
-    sha1 "31dcdd6c88ca5d6bc468001c9a3170679a9f4f75" => :mavericks
-    sha1 "0b33b46ef6a277b6e8a6ff63393983e95dc79a89" => :mountain_lion
+    revision 1
+    sha1 "98b312752cc9088b8e18a25bfb941761fd280bcf" => :yosemite
+    sha1 "db0a8bc76629cf3407d3e59209c7dad7344f42b8" => :mavericks
+    sha1 "2294ef2401f6d1a6c2c2dd608ae8aa82ea1efe67" => :mountain_lion
   end
 
   depends_on :fortran
