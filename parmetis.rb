200db79c3c3d826f892a8ab6f1a0b9982d93aa30
Dominique Orban <dominique.orban@gmail.com>
2015-01-24 14:18:19 -0500

    ParMETIS: Build against brewed METIS lib.
    - Remove unnecessary require formula
    - Build shared library.
    
    Closes #1759.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- parmetis.rb
+++ parmetis.rb
@@ -1,19 +1,21 @@
-require "formula"
-
 class Parmetis < Formula
   homepage "http://glaros.dtc.umn.edu/gkhome/metis/parmetis/overview"
   url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz"
   sha1 "e0df69b037dd43569d4e40076401498ee5aba264"
 
-  # METIS 5.* is required. It comes bundled with ParMETIS but is not
-  # installed by default. We prefer to brew it ourselves.
+  # METIS 5.* is required. It comes bundled with ParMETIS.
+  # We prefer to brew it ourselves.
   depends_on "metis"
 
   depends_on "cmake" => :build
   depends_on :mpi => :cc
 
+  # Do not build the METIS 5.* that ships with ParMETIS.
+  patch :DATA
+
   def install
-    system "make", "config", "prefix=#{prefix}"
+    ENV["LDFLAGS"] = "-L#{Formula["metis"].lib} -lmetis"
+    system "make", "config", "prefix=#{prefix}", "shared=1"
     system "make", "install"
     share.install "Graphs" # Sample data for test
   end
@@ -23,3 +25,18 @@ class Parmetis < Formula
     ohai "Test results are in ~/Library/Logs/Homebrew/parmetis."
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index ca945dd..1bf94e9 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -33,7 +33,7 @@ include_directories(${GKLIB_PATH})
+ include_directories(${METIS_PATH}/include)
+
+ # List of directories that cmake will look for CMakeLists.txt
+-add_subdirectory(${METIS_PATH}/libmetis ${CMAKE_BINARY_DIR}/libmetis)
++#add_subdirectory(${METIS_PATH}/libmetis ${CMAKE_BINARY_DIR}/libmetis)
+ add_subdirectory(include)
+ add_subdirectory(libparmetis)
+ add_subdirectory(programs)
