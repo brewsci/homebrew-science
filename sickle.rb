11fc8ab95ab9fa82dc0c578654c5b56ef7c907a8
Shaun Jackman <sjackman@gmail.com>
2015-01-26 11:49:34 -0800

    sickle 1.33: Add bioinformatics tag
    Closes #1764.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sickle.rb
+++ sickle.rb
@@ -1,17 +1,18 @@
-require 'formula'
-
 class Sickle < Formula
-  homepage 'https://github.com/najoshi/sickle'
-  url 'https://github.com/najoshi/sickle/archive/v1.33.tar.gz'
-  sha1 '593274fb7e12a52c9086dff69623aedca1799a5c'
-  head 'https://github.com/najoshi/sickle.git'
+  homepage "https://github.com/najoshi/sickle"
+  # tag "bioinformatics
+
+  url "https://github.com/najoshi/sickle/archive/v1.33.tar.gz"
+  sha1 "593274fb7e12a52c9086dff69623aedca1799a5c"
+  head "https://github.com/najoshi/sickle.git"
 
   def install
-    system 'make'
-    bin.install 'sickle'
+    system "make"
+    bin.install "sickle"
+    doc.install "README.md"
   end
 
   test do
-    system "#{bin}/sickle", '--version'
+    system "#{bin}/sickle", "--version"
   end
 end
