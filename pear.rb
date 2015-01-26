60ffce30bab11e30666a798ca05e92c9b717d0b3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-26 20:16:35 +0000

    pear: add 0.9.6 bottle.

--- pear.rb
+++ pear.rb
@@ -6,6 +6,14 @@ class Pear < Formula
   url "http://sco.h-its.org/exelixis/web/software/pear/files/pear-0.9.6-src.tar.gz"
   sha1 "351bde183653facb9c6a4e0b7a4328a130c3b903"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "03273d7ee8328f621c153db9694cfc777d259023" => :yosemite
+    sha1 "216efd5def8ba3ad843bb0ccc290e7120a4a0605" => :mavericks
+    sha1 "27393ec465f041f174b433fcfe60d41949b0eb2c" => :mountain_lion
+  end
+
   def install
     system "./configure",
       "--disable-debug",
