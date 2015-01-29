d92e961933115ba7f3847ea70e36ce25095fd89c
Shaun Jackman <sjackman@gmail.com>
2015-01-29 10:25:18 -0800

    igvtools 2.3.40
    Closes #1781.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- igvtools.rb
+++ igvtools.rb
@@ -1,18 +1,19 @@
-require "formula"
-
 class Igvtools < Formula
-  homepage "http://www.broadinstitute.org/software/igv"
-  url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.32.zip"
-  sha1 "7de35e1ac3908174f99a76e1edd2134b5f1f13cf"
+  homepage "http://www.broadinstitute.org/software/igv/download"
+  # tag "bioinformatics"
+
+  url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.40.zip"
+  sha1 "0e9eeba7de3fa400d69bf224be84a4a257395aaf"
 
   def install
-    libexec.install "igvtools.jar"
-    bin.write_jar_script libexec/"igvtools.jar", "igvtools"
+    java = share/"java"
+    java.install "igvtools.jar"
+    bin.write_jar_script java/"igvtools.jar", "igvtools"
     doc.install "igvtools_readme.txt"
     share.install "genomes"
   end
 
   test do
-    system "igvtools |grep igvtools"
+    system "#{bin}/igvtools |grep igvtools"
   end
 end
