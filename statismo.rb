5df2a36f64af3895e4dd11852f6e65d766ea7605
Arnaud Gelas <arnaudgelas@gmail.com>
2015-01-29 08:56:50 +0100

    Statismo: v0.10.2
    Closes #1778.

--- statismo.rb
+++ statismo.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Statismo < Formula
   homepage "https://github.com/statismo/statismo"
-  url "https://github.com/statismo/statismo/archive/v0.10.1.tar.gz"
-  sha1 "b42f67ea25ffacfe6a769aefb0c71400512a1923"
-  head 'https://github.com/statismo/statismo.git'
+  url "https://github.com/statismo/statismo/archive/v0.10.2.tar.gz"
+  sha256 "e0f66dea821e53d6327c8a624d41ac407ba7066849c33b6d9e58253b355955e8"
+  head "https://github.com/statismo/statismo.git"
 
   depends_on "cmake" => :build
   depends_on "eigen" => :build
@@ -12,17 +10,20 @@ class Statismo < Formula
   depends_on "hdf5" => :build
   depends_on "vtk" => :build
   depends_on "insighttoolkit" => :build
+  depends_on "python" => :build
 
   def install
     args = std_cmake_args + %W[
       -DBUILD_SHARED_LIBS=ON
-      -DBUILD_TESTING=ON
+      -DBUILD_TESTING=OFF
+      -DBUILD_EXAMPLES=ON
+      -DBUILD_DOCUMENTATION=OFF
       -DVTK_SUPPORT=ON
       -DITK_SUPPORT=ON
     ]
     args << ".."
 
-    mkdir 'statismo-build' do
+    mkdir "statismo-build" do
       system "cmake", *args
       system "make", "install"
     end
