4ad64ab28e2a59f8cbe5a90889848dfa69e7cb9e
Shaun Jackman <sjackman@gmail.com>
2015-02-03 11:44:44 -0800

    salmon 0.2.7: New formula
    Salmon is a tool for transcript quantification from RNA-seq data.

---
+++ salmon.rb
@@ -0,0 +1,24 @@
+class Salmon < Formula
+  homepage "http://www.cs.cmu.edu/~ckingsf/software/sailfish/downloads.html"
+  # tag "bioinformatics"
+
+  url "https://github.com/kingsfordgroup/sailfish/archive/v0.2.7.tar.gz"
+  sha1 "442226fb38fffd51db2a274f7b5597611b578324"
+  head "https://github.com/kingsfordgroup/sailfish.git", :branch => "develop"
+
+  depends_on "cmake" => :build
+  depends_on "tbb"
+
+  def install
+    # Fix error: Unable to find the requested Boost libraries.
+    ENV.deparallelize
+
+    # Salmon uses Shark which requires Boost 1.55.0
+    system "cmake", "-DFETCH_BOOST=TRUE", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/salmon", "--version"
+  end
+end
