d436c3aec2522eda97cb4d2e46c0a31fbcb60b91
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-04 16:23:33 +0000

    pathvisio: add 3.1.3 bottle.

--- pathvisio.rb
+++ pathvisio.rb
@@ -6,6 +6,14 @@ class Pathvisio < Formula
   sha1 "54b14c34464c2714fc4455dd933038f03985ddcd"
   version "3.1.3"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "f31c60620d127906391421d52beb77bb425f7d0c" => :yosemite
+    sha1 "c2677391d334b0360c48c39da9dfdf879891f245" => :mavericks
+    sha1 "b50a242187b691fc91028ecc15dfb80afcfd34ff" => :mountain_lion
+  end
+
   depends_on :java => "1.7"
 
   def install
