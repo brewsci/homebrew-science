2f34aa3aa6c275d81ad458a807b89dc193ab95d0
Ian Lancaster <ilancaster@gmail.com>
2015-02-05 19:43:03 -0500

    libsequence 1.8.4
    Closes #1800.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- libsequence.rb
+++ libsequence.rb
@@ -2,9 +2,8 @@ class Libsequence < Formula
   homepage "https://molpopgen.github.io/libsequence/"
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btg316"
-  url "https://github.com/molpopgen/libsequence/archive/1.8.3-1.tar.gz"
-  version "1.8.3-1"
-  sha1 "e31e5ba18ea27a4ec363a64fd5cf38cf5c69aa21"
+  url "https://github.com/molpopgen/libsequence/archive/1.8.4.tar.gz"
+  sha1 "1cac19fffad293309c834f3f356023c990422988"
   head "https://github.com/molpopgen/libsequence.git"
 
   bottle do
