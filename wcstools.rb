7eaede20cf0b7a68e92c4d355e25e9d52986eb28
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-10 12:55:16 +0000

    wcstools: add 3.9.1 bottle.

--- wcstools.rb
+++ wcstools.rb
@@ -5,6 +5,14 @@ class Wcstools < Formula
   url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.1.tar.gz"
   sha1 "05d97277fdfcabd40f926b0b7ac66b0f434497f0"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "e33240bd4f007854cc524e63bb16632788e78d6e" => :yosemite
+    sha1 "388f01432720ad17b31e6ce4d1c726346f785d22" => :mavericks
+    sha1 "a77678d6d1af694167dad79bce2562985cf612e3" => :mountain_lion
+  end
+
   def install
     system "make", "-f", "Makefile.osx", "all"
 
