974e770c95c9b265371429cad619bd6b56154517
Saket Choudhary <saketkc@gmail.com>
2015-02-10 17:51:20 -0800

    sratoolkit 2.4.5: Build from source
    Closes #1811.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sratoolkit.rb
+++ sratoolkit.rb
@@ -1,18 +1,11 @@
 class Sratoolkit < Formula
-  homepage "http://www.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=software"
+  homepage "https://github.com/ncbi/sra-tools"
   # doi "10.1093/nar/gkq1019"
   # tag "bioinformatics"
 
-  version "2.4.3"
-  if OS.mac?
-    url "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/#{version}/sratoolkit.#{version}-mac64.tar.gz"
-    sha1 "baf9312d7185593e514806744cab7077b808a598"
-  elsif OS.linux?
-    url "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/#{version}/sratoolkit.#{version}-ubuntu64.tar.gz"
-    sha1 "da07935448a2a1567c591e514c667aa9e3846ec3"
-  end
-
-  head "https://github.com/NCBITools/sratoolkit.git"
+  url "https://github.com/ncbi/sra-tools/archive/2.4.5.tar.gz"
+  sha1 "d2acda13662feecff653d2ba1d7f8c7f321e3c15"
+  head "https://github.com/ncbi/sra-tools.git"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -22,10 +15,48 @@ class Sratoolkit < Formula
     sha1 "ff305cfd27ed3b1147384a32a921cf06661e3b67" => :mountain_lion
   end
 
+  resource "ngs-sdk" do
+    url "https://github.com/ncbi/ngs/archive/1.1.0.tar.gz"
+    sha1 "0386bd85f4843df0933a18b0950b97665b716ecf"
+  end
+
+  resource "ncbi-vdb" do
+    url "https://github.com/ncbi/ncbi-vdb/archive/3973c21.tar.gz"
+    sha1 "5fa5ba9bd18c8d9114af9d710006ad2b28fc472c"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "libxml2"
+  depends_on "libmagic" => :recommended
+  depends_on "hdf5" => :recommended
+
   def install
-    bin.mkdir
-    cp Dir["bin/*[a-z]"].select {|x| File.symlink? x}, bin
-    share.install "schema"
+    ENV.deparallelize
+    resource("ngs-sdk").stage do
+      cd "ngs-sdk" do
+        system "./configure", "--prefix=#{prefix}", "--build=#{prefix}"
+        system "make"
+        system "make", "install"
+      end
+    end
+
+    resource("ncbi-vdb").stage do
+      system "./configure", "--with-ngs-sdk-prefix=#{prefix}", "--prefix=#{prefix}", "--build=#{prefix}"
+      system "make"
+      system "make", "install"
+      (include/"ncbi-vdb").install Dir["*"]
+    end
+
+    inreplace "tools/copycat/Makefile", "-smagic-static", "-smagic"
+    system "./configure", "--prefix=#{prefix}", "--with-ngs-sdk-prefix=#{prefix}", "--with-ncbi-vdb-sources=#{include}/ncbi-vdb", "--with-ncbi-vdb-build=#{prefix}", "--prefix=#{prefix}", "--build=#{prefix}"
+    system "make"
+    system "make", "install"
+    rm_rf "#{bin}/ncbi"
+    rm_rf "#{prefix}/sra-tools"
+    rm_rf "#{prefix}/ngs-sdk"
+    rm_rf "#{prefix}/ncbi-vdb"
+    rm_rf "#{lib}64"
+    rm_rf "#{include}"
   end
 
   test do
