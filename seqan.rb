42c6ebd09af79b916c5fa091b342c0d2239f8646
Shaun Jackman <sjackman@gmail.com>
2015-02-11 09:26:20 -0800

    seqan 1.4.2
    Add devel seqan 2.0.0
    
    Closes #1816.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- seqan.rb
+++ seqan.rb
@@ -1,13 +1,20 @@
-require 'formula'
-
 class Seqan < Formula
-  homepage 'http://www.seqan.de/'
-  url 'http://packages.seqan.de/seqan-library/seqan-library-1.4.1.tar.bz2'
-  sha1 'a7d33813d34f999015e4f27bb398255436b202b2'
-  head 'http://svn.seqan.de/seqan/trunk/core'
+  homepage "http://www.seqan.de/"
+  # doi "10.1186/1471-2105-9-11"
+  # tag "bioinformatics"
+
+  url "http://packages.seqan.de/seqan-library/seqan-library-1.4.2.tar.bz2"
+  sha1 "2413d558c2665ae813bb0a2ab3aa57e07cc00675"
+
+  devel do
+    url "http://packages.seqan.de/seqan-library/seqan-library-2.0.0.tar.bz2"
+    sha1 "872335665632ca63d115ac3042b4f1d28ab0d872"
+  end
+
+  head "http://svn.seqan.de/seqan/trunk/core"
 
   def install
-    include.install 'include/seqan'
-    doc.install Dir['share/doc/seqan/*'] unless build.head?
+    include.install "include/seqan"
+    doc.install Dir["share/doc/seqan/*"] unless build.head?
   end
 end
