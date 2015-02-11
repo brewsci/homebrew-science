6fdd8d0217d3ec5c1f11c1051c902bf4df2658bc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-11 17:38:22 +0000

    seqan: add 1.4.2 bottle.

--- seqan.rb
+++ seqan.rb
@@ -6,6 +6,14 @@ class Seqan < Formula
   url "http://packages.seqan.de/seqan-library/seqan-library-1.4.2.tar.bz2"
   sha1 "2413d558c2665ae813bb0a2ab3aa57e07cc00675"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "26f7482ddf437875f0254d75887e0a088c545c4f" => :yosemite
+    sha1 "bfcd857c0b28f56770ea87b9cbada300c944787c" => :mavericks
+    sha1 "d2df35b6b75cac64ab4b9fcb772a477d16c52c75" => :mountain_lion
+  end
+
   devel do
     url "http://packages.seqan.de/seqan-library/seqan-library-2.0.0.tar.bz2"
     sha1 "872335665632ca63d115ac3042b4f1d28ab0d872"
