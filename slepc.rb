47d8929b88f726d51e74a334a7b210d06ef2a156
Dominique Orban <dominique.orban@gmail.com>
2015-02-11 23:57:32 -0500

    SLEPc: Fix audit warnings.
    Closes #1820.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- slepc.rb
+++ slepc.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Slepc < Formula
-  homepage 'http://www.grycap.upv.es/slepc'
-  url 'http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.5.3.tar.gz'
-  sha1 '5e886c5018dc0d227ae815feb80d4cdd8779c23c'
+  homepage "http://www.grycap.upv.es/slepc"
+  url "http://www.grycap.upv.es/slepc/download/download.php?filename=slepc-3.5.3.tar.gz"
+  sha1 "5e886c5018dc0d227ae815feb80d4cdd8779c23c"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -12,49 +10,52 @@ class Slepc < Formula
     sha1 "f1af93dee89952b511eb2b765a157c1c4b5870ed" => :mountain_lion
   end
 
-  option "complex", "Use complex version by default. Otherwise, real-valued version will be symlinked"
+  deprecated_option "complex" => "with-complex"
 
-  depends_on 'petsc' => :build
+  option "with-complex", "Use complex version by default. Otherwise, real-valued version will be symlinked"
+
+  depends_on "petsc" => :build
   depends_on :mpi => [:cc, :f90]
   depends_on :fortran
   depends_on :x11  => :optional
-  depends_on 'arpack' => [:recommended, "with-mpi"]
+  depends_on "arpack" => [:recommended, "with-mpi"]
 
   def install
     ENV.deparallelize
 
     # these must be consistent with petsc.rb
-    petsc_arch_real="real"
-    petsc_arch_complex="complex"
+    petsc_arch_real = "real"
+    petsc_arch_complex = "complex"
 
-    ENV['SLEPC_DIR'] = Dir.getwd
-    args = %W[--with-clean=true]
+    ENV["SLEPC_DIR"] = Dir.getwd
+    args = ["--with-clean=true"]
     args << "--with-arpack-dir=#{Formula["arpack"].lib}" << "--with-arpack-flags=-lparpack,-larpack" if build.with? "arpack"
 
     # real
-    ENV['PETSC_DIR'] = ("#{Formula["petsc"].opt_prefix}" +"/"+petsc_arch_real)
+    ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/#{petsc_arch_real}"
     system "./configure", "--prefix=#{prefix}/#{petsc_arch_real}", *args
     system "make"
-    system "make test"
-    system "make install"
+    system "make", "test"
+    system "make", "install"
 
     # complex
-    ENV['PETSC_DIR'] = ("#{Formula["petsc"].opt_prefix}" +"/"+petsc_arch_complex)
+    ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/#{petsc_arch_complex}"
     system "./configure", "--prefix=#{prefix}/#{petsc_arch_complex}", *args
     system "make"
-    system "make test"
-    system "make install"
+    system "make", "test"
+    system "make", "install"
 
-    ohai 'Test results are in ~/Library/Logs/Homebrew/slepc. Please check.'
+    ohai "Test results are in ~/Library/Logs/Homebrew/slepc. Please check."
 
     # Link what we need.
     petsc_arch = ((build.include? "complex") ? petsc_arch_complex : petsc_arch_real)
 
-    include.install_symlink Dir["#{prefix}/#{petsc_arch}/include/*.h"], "#{prefix}/#{petsc_arch}/finclude", "#{prefix}/#{petsc_arch}/slepc-private"
+    include.install_symlink Dir["#{prefix}/#{petsc_arch}/include/*.h"],
+                            "#{prefix}/#{petsc_arch}/finclude", "#{prefix}/#{petsc_arch}/slepc-private"
     lib.install_symlink Dir["#{prefix}/#{petsc_arch}/lib/*.a"], Dir["#{prefix}/#{petsc_arch}/lib/*.dylib"]
     prefix.install_symlink "#{prefix}/#{petsc_arch}/conf"
-    doc.install 'docs/slepc.pdf', Dir["docs/*.htm"], 'docs/manualpages'  # They're not really man pages.
-    share.install 'share/slepc/datafiles'
+    doc.install "docs/slepc.pdf", Dir["docs/*.htm"], "docs/manualpages"  # They're not really man pages.
+    share.install "share/slepc/datafiles"
   end
 
   def caveats; <<-EOS.undent
