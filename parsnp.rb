d6ba4758ecc3cd973b0aaadb51b8b1207c230618
Shaun Jackman <sjackman@gmail.com>
2015-02-12 11:01:42 -0800

    parsnp 1.1
    Closes #1823.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- parsnp.rb
+++ parsnp.rb
@@ -1,7 +1,7 @@
 class Parsnp < Formula
   homepage "https://github.com/marbl/parsnp"
-  #tag "bioinformatics"
-  #doi "10.1186/s13059-014-0524-x"
+  # tag "bioinformatics"
+  # doi "10.1186/s13059-014-0524-x"
 
   head "https://github.com/marbl/parsnp.git"
 
@@ -14,13 +14,11 @@ class Parsnp < Formula
   end
 
   if OS.mac?
-    url "https://github.com/marbl/parsnp/releases/download/v1.0/parsnp-OSX64-v1.0.tar.gz"
-    sha1 "721c34434857c876fbf3f0b3aabda8145d6ee7eb"
+    url "https://github.com/marbl/parsnp/releases/download/v1.1/parsnp-OSX64-v1.1.tar.gz"
+    sha1 "230387082702a57ad41989507a8c1f7c63d4dfd0"
   elsif OS.linux?
-    url "https://github.com/marbl/parsnp/releases/download/v1.0/parsnp-Linux64-v1.0.tar.gz"
-    sha1 "a00173f05aa30b38e5013b44297d194b2695de89"
-  else
-    raise "Unsupported operating system"
+    url "https://github.com/marbl/parsnp/releases/download/v1.1/parsnp-Linux64-v1.1.tar.gz"
+    sha1 "443d2db8b498081b1c608ce8641d25dfcc39ad48"
   end
 
   def install
@@ -28,6 +26,6 @@ class Parsnp < Formula
   end
 
   test do
-    system "#{bin}/parsnp -h |grep parsnp"
+    system "#{bin}/parsnp", "-h"
   end
 end
