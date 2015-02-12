826b3397756b4369daae8787c58bea8fec1d8af8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-12 21:43:29 +0000

    stiff: add 2.4.0 bottle.

--- stiff.rb
+++ stiff.rb
@@ -3,6 +3,13 @@ class Stiff < Formula
   url "https://www.astromatic.net/download/stiff/stiff-2.4.0.tar.gz"
   sha256 "f4e85146c17fe8dcf160d12dc6df08bbd9212bb8444162b2e6ebf03f7513a992"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "ca69fddd5decd31af464a0a625c1c893ccb34530" => :yosemite
+    sha1 "b98b28d0614841fccd631d5c9062277577e720f0" => :mavericks
+    sha1 "09cc0873ce0f60c5127129f1cb779bbd6bb4c90e" => :mountain_lion
+  end
+
   option "without-check", "Disable build-time checking (not recommended)"
 
   depends_on "libtiff"
