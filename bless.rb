a4baa800c5a2eb7e382345af3b34795f47c715ab
Shaun Jackman <sjackman@gmail.com>
2015-02-16 13:21:34 -0800

    bless 0.24: New formula
    BLESS: Bloom filter-based error correction solution for
    high-throughput sequencing reads
    
    Closes #1839.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ bless.rb
@@ -0,0 +1,29 @@
+class Bless < Formula
+  homepage "https://sourceforge.net/projects/bless-ec/"
+  # doi "10.1093/bioinformatics/btu030"
+  # tag "bioinformatics"
+
+  url "https://downloads.sourceforge.net/project/bless-ec/bless.v0p24.tgz"
+  version "0.24"
+  sha1 "95082ceda46ea01ee6b8caa10718de00901a04de"
+
+  needs :openmp
+
+  depends_on "boost"
+  depends_on "google-sparsehash" => :build
+  depends_on "kmc" => :recommended
+  depends_on :mpi
+
+  def install
+    # Do not build vendored dependency, kmc.
+    inreplace "Makefile", "cd kmc; make CC=$(CC)", ""
+
+    system "make"
+    bin.install "bless"
+    doc.install "README", "LICENSE"
+  end
+
+  test do
+    system "#{bin}/bless"
+  end
+end
