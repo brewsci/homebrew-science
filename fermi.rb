5b86d1c614fff44916620bc717ce54b83fc7bcd6
Shaun Jackman <sjackman@gmail.com>
2015-02-16 12:20:41 -0800

    fermi 1.1: Add doi and tag
    Closes #1838.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fermi.rb
+++ fermi.rb
@@ -1,18 +1,19 @@
-require 'formula'
-
 class Fermi < Formula
-  homepage 'https://github.com/lh3/fermi'
-  url 'https://github.com/lh3/fermi/archive/1.1.tar.gz'
-  sha1 '95d9a78df345def9ac781be0485b5c7680e0ad04'
-  head 'https://github.com/lh3/fermi.git'
+  homepage "https://github.com/lh3/fermi"
+  # doi "10.1093/bioinformatics/bts280"
+  # tag "bioinformatics"
+
+  url "https://github.com/lh3/fermi/archive/1.1.tar.gz"
+  sha1 "95d9a78df345def9ac781be0485b5c7680e0ad04"
+  head "https://github.com/lh3/fermi.git"
 
   def install
-    system 'make'
-    prefix.install 'README.md'
-    bin.install 'fermi', 'run-fermi.pl'
+    system "make"
+    prefix.install "README.md"
+    bin.install "fermi", "run-fermi.pl"
   end
 
   test do
-    system 'fermi 2>&1 |grep -q fermi'
+    system "#{bin}/fermi 2>&1 |grep -q fermi"
   end
 end
