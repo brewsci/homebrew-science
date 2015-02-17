3388121cccea89bec121a194bbe37bbeb700f3a4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-17 07:28:08 +0000

    grabix: add 1.4 bottle.

--- grabix.rb
+++ grabix.rb
@@ -5,6 +5,14 @@ class Grabix < Formula
   sha1 "687c6874741127c012e84cad4d85a5116afc1034"
   head "https://github.com/arq5x/grabix.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    cellar :any
+    sha1 "b10d761ad686e2ea8e86e5fac59cb49f1abc85ff" => :yosemite
+    sha1 "627dd3da48e1ba0888a0527e597d271e740e40f8" => :mavericks
+    sha1 "ed81ea6340dfefdb5862aac293e7e8a17204ddc1" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "grabix"
