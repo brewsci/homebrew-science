ba8a9d70440905ec654e4cdf70643cafb4d74150
Dominique Orban <dominique.orban@gmail.com>
2015-02-18 00:11:46 -0500

    METIS4: Audit fixes and Linux build.
    Closes #1852.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- metis4.rb
+++ metis4.rb
@@ -1,23 +1,25 @@
-require 'formula'
-
 class Metis4 < Formula
-  url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz'
-  homepage 'http://glaros.dtc.umn.edu/gkhome/views/metis'
-  sha1 '63303786414a857eaeea2b2a006521401bccda5e'
+  url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz"
+  homepage "http://glaros.dtc.umn.edu/gkhome/views/metis"
+  sha1 "63303786414a857eaeea2b2a006521401bccda5e"
 
   keg_only "Conflicts with metis (5.x)."
 
   def install
-    inreplace "Lib/Makefile", "libmetis.a", "libmetis.dylib"
-    make_args = ["COPTIONS=-fPIC",
-                 "AR=libtool -dynamic -install_name #{lib}/$(notdir $@) -undefined dynamic_lookup -o",
-                 "RANLIB=echo",
-                 "METISLIB=../libmetis.dylib"]
+    if OS.mac?
+      so = "dylib"
+      ar = "libtool -dynamic -install_name #{lib}/$(notdir $@) -undefined dynamic_lookup -o"
+    else
+      so = "so"
+      ar = "$(CC) -shared -Wl,-soname -Wl,#{lib}/$(notdir $@) -o"
+    end
+    inreplace "Lib/Makefile", "libmetis.a", "libmetis.#{so}"
+    make_args = ["COPTIONS=-fPIC", "AR=#{ar}", "RANLIB=echo", "METISLIB=../libmetis.#{so}"]
     system "make", *make_args
-    bin.install %w(pmetis kmetis oemetis onmetis partnmesh partdmesh mesh2nodal mesh2dual graphchk)
-    lib.install "libmetis.dylib"
+    bin.install %w[pmetis kmetis oemetis onmetis partnmesh partdmesh mesh2nodal mesh2dual graphchk]
+    lib.install "libmetis.#{so}"
     include.install Dir["Lib/*.h"]
-    (share / "metis4").install %w(Graphs/mtest Graphs/4elt.graph Graphs/metis.mesh Graphs/test.mgraph)
+    (share / "metis4").install %w[Graphs/mtest Graphs/4elt.graph Graphs/metis.mesh Graphs/test.mgraph]
   end
 
   test do
