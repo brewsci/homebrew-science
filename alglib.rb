19ea5c82c4073655188fcf827ca5f8b20fd4bda2
Ian Lancaster <ilancaster@gmail.com>
2015-02-18 03:16:10 -0500

    alglib: write with heredocs

--- alglib.rb
+++ alglib.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Alglib < Formula
   homepage "http://www.alglib.net"
   url "http://alglib.net/translator/re/alglib-2.6.0.cpp.zip"
@@ -9,39 +7,39 @@ class Alglib < Formula
   depends_on "pkg-config" => :build
 
   def install
-    File.open("#{buildpath}/alglib.pro", "w") do |f|
-      f.puts("TEMPLATE = subdirs")
-      f.puts("SUBDIRS = \\")
-      f.puts("    src")
-    end
+    (buildpath/"alglib.pro").write <<-EOS.undent
+      TEMPLATE = subdirs
+      SUBDIRS = \\
+          src
+    EOS
 
-    File.open("#{buildpath}/src/src.pro", "w") do |f|
-      f.puts("isEmpty(PREFIX) {")
-      f.puts("  PREFIX = /usr/local")
-      f.puts("}")
-      f.puts("CONFIG      += warn_on release static_and_shared")
-      f.puts("QT      -= gui core")
-      f.puts("LIBS    -= -lQtGui -lQtCore")
-      f.puts("TARGET       = alglib")
-      f.puts("VERSION      = 2.6.0")
-      f.puts("TEMPLATE     = lib")
-      f.puts("target.path = $$PREFIX/lib")
-      f.puts("DEPENDPATH += .")
-      f.puts("INCLUDEPATH += .")
-      f.puts("OBJECTS_DIR  = ../_tmp")
-      f.puts("DESTDIR = ../")
-      f.puts("HEADERS += *.h")
-      f.puts("SOURCES += *.cpp")
-      f.puts("header_files.files = $$HEADERS")
-      f.puts("header_files.path = $$PREFIX/include/alglib")
-      f.puts("INSTALLS += target")
-      f.puts("INSTALLS += header_files")
-      f.puts("CONFIG += create_pc create_prl no_install_prl")
-      f.puts("QMAKE_PKGCONFIG_LIBDIR = $$PREFIX/lib/")
-      f.puts("QMAKE_PKGCONFIG_INCDIR = $$PREFIX/include/alglib")
-      f.puts("QMAKE_PKGCONFIG_CFLAGS = -I$$PREFIX/include/")
-      f.puts("QMAKE_PKGCONFIG_DESTDIR = pkgconfig")
-    end
+    (buildpath/"src/src.pro").write <<-EOS.undent
+      isEmpty(PREFIX) {
+        PREFIX = /usr/local
+      }
+      CONFIG      += warn_on release static_and_shared
+      QT      -= gui core
+      LIBS    -= -lQtGui -lQtCore
+      TARGET       = alglib
+      VERSION      = 2.6.0
+      TEMPLATE     = lib
+      target.path = $$PREFIX/lib
+      DEPENDPATH += .
+      INCLUDEPATH += .
+      OBJECTS_DIR  = ../_tmp
+      DESTDIR = ../
+      HEADERS += *.h
+      SOURCES += *.cpp
+      header_files.files = $$HEADERS
+      header_files.path = $$PREFIX/include/alglib
+      INSTALLS += target
+      INSTALLS += header_files
+      CONFIG += create_pc create_prl no_install_prl
+      QMAKE_PKGCONFIG_LIBDIR = $$PREFIX/lib/
+      QMAKE_PKGCONFIG_INCDIR = $$PREFIX/include/alglib
+      QMAKE_PKGCONFIG_CFLAGS = -I$$PREFIX/include/
+      QMAKE_PKGCONFIG_DESTDIR = pkgconfig
+    EOS
 
     system "qmake", "alglib.pro", "PREFIX=#{prefix}"
     system "make"
