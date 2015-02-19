6ee64e22abb8504c16d9adfd7bf631212262d410
Dominique Orban <dominique.orban@gmail.com>
2015-02-18 23:22:03 -0500

    Scalapack: address audit issues.

--- scalapack.rb
+++ scalapack.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Scalapack < Formula
-  homepage 'http://www.netlib.org/scalapack/'
-  url 'http://www.netlib.org/scalapack/scalapack-2.0.2.tgz'
-  sha1 'ff9532120c2cffa79aef5e4c2f38777c6a1f3e6a'
-  head 'https://icl.cs.utk.edu/svn/scalapack-dev/scalapack/trunk', :using => :svn
+  homepage "http://www.netlib.org/scalapack/"
+  url "http://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
+  sha1 "ff9532120c2cffa79aef5e4c2f38777c6a1f3e6a"
+  head "https://icl.cs.utk.edu/svn/scalapack-dev/scalapack/trunk", :using => :svn
   revision 1
 
   bottle do
@@ -16,12 +14,12 @@ class Scalapack < Formula
   end
 
   option 'with-shared-libs', 'Build shared libs (some tests may fail)'
-  option 'without-check', 'Skip build-time tests (not recommended)'
+  option "without-check", "Skip build-time tests (not recommended)"
 
   depends_on :mpi => [:cc, :f90]
-  depends_on 'cmake' => :build
-  depends_on 'openblas' => :optional
-  depends_on 'veclibfort' if build.without? 'openblas' and OS.mac?
+  depends_on "cmake" => :build
+  depends_on "openblas" => :optional
+  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
   depends_on :fortran
 
   def install
@@ -29,19 +27,19 @@ class Scalapack < Formula
     args << "-DBUILD_SHARED_LIBS=ON" if build.with? "shared-libs"
 
     if build.with? "openblas"
-      blas = "-L#{Formula['openblas'].opt_lib} -lopenblas"
+      blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
       lapack = blas
     else
-      blas = (OS.mac?) ? "-L#{Formula['veclibfort'].opt_lib} -lveclibfort" : "-lblas"
+      blas = (OS.mac?) ? "-L#{Formula["veclibfort"].opt_lib} -lveclibfort" : "-lblas"
       lapack = (OS.mac?) ? blas : "-llapack"
     end
     args += ["-DBLAS_LIBRARIES=#{blas}", "-DLAPACK_LIBRARIES=#{lapack}"]
 
     mkdir "build" do
-      system 'cmake', '..', *args
-      system 'make all'
-      system 'make test' if build.with? 'check'
-      system 'make install'
+      system "cmake", "..", *args
+      system "make", "all"
+      system "make", "test" if build.with? "check"
+      system "make", "install"
     end
   end
 end
