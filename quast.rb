5af2a2602a252e01725ffe4f4c3d14293a993101
Shaun Jackman <sjackman@gmail.com>
2015-02-19 11:23:49 -0800

    quast 2.3: Move from libexec to prefix
    Add tag "bioinformatics"
    Depend on "homebrew/python/matplotlib" for Mountain Lion and older
    Install symlink from quast.py to quast
    Run quast --test
    
    Closes #1857.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- quast.rb
+++ quast.rb
@@ -1,21 +1,29 @@
-require 'formula'
-
 class Quast < Formula
-  homepage 'http://bioinf.spbau.ru/en/quast'
-  #doi '10.1093/bioinformatics/btt086'
+  homepage "http://bioinf.spbau.ru/en/quast"
+  # doi "10.1093/bioinformatics/btt086"
+  # tag "bioinformatics"
 
-  url 'https://downloads.sourceforge.net/project/quast/quast-2.3.tar.gz'
-  sha1 '9bf176f852cf1b77f201b15e7d9262ae29cff727'
+  url "https://downloads.sourceforge.net/project/quast/quast-2.3.tar.gz"
+  sha1 "9bf176f852cf1b77f201b15e7d9262ae29cff727"
 
-  depends_on 'matplotlib' => :python
+  if OS.mac? && MacOS.version <= :mountain_lion
+    depends_on "homebrew/python/matplotlib"
+  else
+    # Mavericks and newer include matplotlib
+    depends_on "matplotlib" => :python
+  end
 
   def install
-    libexec.install 'quast.py', 'metaquast.py', 'libs'
-    bin.install_symlink '../libexec/quast.py', '../libexec/metaquast.py'
-    doc.install 'manual.html'
+    prefix.install Dir["*"]
+    bin.install_symlink "../quast.py", "../metaquast.py"
+    bin.install_symlink "quast.py" => "quast", "metaquast.py" => "metaquast"
   end
 
   test do
-    system 'quast.py 2>&1 |grep -q quast'
+    system "#{bin}/quast"
+    cd prefix do
+      system "#{bin}/quast", "--test"
+      rm_rf "quast_test_output"
+    end
   end
 end
