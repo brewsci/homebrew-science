0254d4b02160e068392f6f248c5c73a8a061e3fa
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-23 19:52:14 +0000

    quast: add 2.3 bottle.

--- quast.rb
+++ quast.rb
@@ -6,6 +6,14 @@ class Quast < Formula
   url "https://downloads.sourceforge.net/project/quast/quast-2.3.tar.gz"
   sha1 "9bf176f852cf1b77f201b15e7d9262ae29cff727"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha1 "e47b24a193b840caaacb7d51b79bd82ee8da4fbd" => :yosemite
+    sha1 "2bd608e09c1acff89b02b0ab8e283b9b7cdeb177" => :mavericks
+    sha1 "6c9d011cd0449cce589b2046b95dd8b11385f2ab" => :mountain_lion
+  end
+
   if OS.mac? && MacOS.version <= :mountain_lion
     depends_on "homebrew/python/matplotlib"
   else
