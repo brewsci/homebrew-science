2f361a209925660e240121a01c260480ca2b2d02
Ian Lancaster <ilancaster@gmail.com>
2015-02-23 22:52:56 -0500

    adam: use chmod method

--- adam.rb
+++ adam.rb
@@ -1,12 +1,10 @@
-require "formula"
-
 class Adam < Formula
   homepage "https://github.com/bigdatagenomics/adam"
   url "https://github.com/bigdatagenomics/adam/releases/download/adam-parent-0.13.0/adam-0.13.0-bin.tar.gz"
   sha1 "5cfe704c768b7be0d493e3052257578f971fc3ec"
 
   head do
-    url "https://github.com/bigdatagenomics/adam.git", :branch => "master"
+    url "https://github.com/bigdatagenomics/adam.git"
     depends_on "maven" => :build
   end
 
@@ -17,7 +15,7 @@ class Adam < Formula
       system "mvn", "clean", "install",
                     "-DskipAssembly=True",
                     "-DskipTests=" + (build.with?("check") ? "False" : "True")
-      system "chmod", "+x", "adam-cli/target/appassembler/bin/adam"
+      chmod 0755, "adam-cli/target/appassembler/bin/adam"
       prefix.install "adam-cli/target/appassembler/repo"
       bin.install "adam-cli/target/appassembler/bin/adam"
     else
@@ -27,6 +25,6 @@ class Adam < Formula
   end
 
   test do
-    system "#{bin}/adam buildinfo"
+    system "#{bin}/adam", "buildinfo"
   end
 end
