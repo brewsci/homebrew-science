081600d556844182de532b3982150d2e36abf8e1
Saket Choudhary <saketkc@gmail.com>
2015-02-23 17:18:22 -0800

    bedtools 2.23.0
    Closes #1873.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bedtools.rb
+++ bedtools.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Bedtools < Formula
   homepage "https://github.com/arq5x/bedtools2"
   #doi "10.1093/bioinformatics/btq033"
   #tag "bioinformatics"
-  url "https://github.com/arq5x/bedtools2/releases/download/v2.22.0/bedtools-2.22.0.tar.gz"
-  sha1 "96f9f7031806de5d4ec694aeb9db31c3f8188f9b"
+  url "https://github.com/arq5x/bedtools2/archive/v2.23.0.tar.gz"
+  sha1 "7618000d534816cf54983eac39e0eb8f93f9e204"
   head "https://github.com/arq5x/bedtools2.git"
 
   bottle do
