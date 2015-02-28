5cef8404cf15b47fcd25bebb0d64858959064bfb
Shaun Jackman <sjackman@gmail.com>
2015-02-28 08:34:27 -0800

    snap 2013-11-29: Add bioinformatics tag
    Closes #1893.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- snap.rb
+++ snap.rb
@@ -1,26 +1,26 @@
-require 'formula'
-
 class Snap < Formula
-  homepage 'http://korflab.ucdavis.edu/software.html'
-  #doi "10.1186/1471-2105-5-59"
-  version '2013-11-29'
+  homepage "http://korflab.ucdavis.edu/software.html"
+  # doi "10.1186/1471-2105-5-59"
+  # tag "bioinformatics"
+
+  version "2013-11-29"
   url "http://korflab.ucdavis.edu/Software/snap-#{version}.tar.gz"
-  sha1 '0ff0612ecb7040dfaa58b4330396d025abc0b758'
+  sha1 "0ff0612ecb7040dfaa58b4330396d025abc0b758"
 
   def install
-    system 'make'
-    bin.install *(%w[exonpairs fathom forge hmm-info snap] + Dir['*.pl'])
+    system "make"
+    bin.install *(%w[exonpairs fathom forge hmm-info snap] + Dir["*.pl"])
     doc.install *%w[00README LICENSE example.zff ]
-    libexec.install *%w[DNA HMM Zoe]
+    prefix.install *%w[DNA HMM Zoe]
   end
 
   def caveats; <<-EOS.undent
     Set the ZOE environment variable:
-      export ZOE=#{opt_libexec}
+      export ZOE=#{opt_prefix}
     EOS
   end
 
   test do
-    system 'snap -help'
+    system "#{bin}/snap", "-help"
   end
 end
