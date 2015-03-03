ad40d2d996f28e296673c425224b8f0fcaa05516
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-03 17:45:07 +0000

    bamhash: add 1.0 bottle.

--- bamhash.rb
+++ bamhash.rb
@@ -7,6 +7,14 @@ class Bamhash < Formula
   sha1 "1e7f6d31bee0ea66b60150c8ef587a10cb90af26"
   head "https://github.com/DecodeGenetics/BamHash.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha1 "7b0a924e7d19c6f24c0a3ad3a13cb1623ef2f4c8" => :yosemite
+    sha1 "e87aab324de4290409cc764eb0ca4cf35b57a7b6" => :mavericks
+    sha1 "64688eea9d72ea394f705a27aa107ef142236fef" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
