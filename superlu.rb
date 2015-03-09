c4239cc4d6ba25b977a48e0a5e73d72f57151949
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-09 20:42:36 +0000

    superlu: update 4.3_1 bottle.

--- superlu.rb
+++ superlu.rb
@@ -5,12 +5,12 @@ class Superlu < Formula
   revision 1
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    revision 2
-    sha1 "e6af03f74a1401aa7fe90810741498e00f9e50ff" => :yosemite
-    sha1 "864caefe41169919a3041ec89b1252bcc34e09b7" => :mavericks
-    sha1 "b6086ee4133eb84c335bb1ab5424e35113447811" => :mountain_lion
+    revision 3
+    sha256 "4547fbc3f0aad075102b8274c99fbd116c0bd26b1dc74eac6b25cfe4ab048cd6" => :yosemite
+    sha256 "017eba66d0898865031be538e38bd860d114bd2a8dc8b9a70be569947ed7d259" => :mavericks
+    sha256 "98a0638d2fd4b1e819180cddc06e8f9ce5d46747920c191fe3111b3e10cb135e" => :mountain_lion
   end
 
   option "without-check", "skip build-time tests (not recommended)"
