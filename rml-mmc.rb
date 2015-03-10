1688bbea1a57be08709aa708f15f8a4558bf8c91
Ian Lancaster <ilancaster@gmail.com>
2015-03-09 23:02:45 -0400

    rml-mmc: reformat version

--- rml-mmc.rb
+++ rml-mmc.rb
@@ -1,7 +1,7 @@
 class RmlMmc < Formula
   homepage "http://www.ida.liu.se/labs/pelab/rml"
   url "http://build.openmodelica.org/apt/pool/contrib/rml-mmc_280.orig.tar.gz"
-  version "280"
+  version "2.8.0"
   sha256 "7b184af0a802847fa4bfeb610d0f0ef12352debe68fe500fcf73c931b5cf3ffb"
 
   head "https://openmodelica.org/svn/MetaModelica/trunk", :using => :svn
