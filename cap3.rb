aab8e75ddbd1cbfe02457bad1c58ca60523c02d5
Saket Choudhary <saketkc@gmail.com>
2015-03-12 16:16:21 -0700

    cap3 2015-02-11: Modernize, fix sha256
    Fixes #1943.
    Closes #1944.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- cap3.rb
+++ cap3.rb
@@ -1,22 +1,20 @@
-require 'formula'
-
 class Cap3 < Formula
-  homepage 'http://seq.cs.iastate.edu/cap3.html'
+  homepage "http://seq.cs.iastate.edu/cap3.html"
   if OS.mac? then
-    url 'http://seq.cs.iastate.edu/CAP3/cap3.macosx.intel64.tar'
-    sha1 '42606cea94a70bcbcb128fe7d427c35a7ebc5f19'
+    url "http://seq.cs.iastate.edu/CAP3/cap3.macosx.intel64.tar"
+    sha256 "4b6e8fa6b39147b23ada6add080854ea9fadace9a9c8870a97ac79ff1c75338e"
   elsif OS.linux? then
-    url 'http://seq.cs.iastate.edu/CAP3/cap3.linux.x86_64.tar'
-    sha1 'c062ff742abb422f25f6e7db76482694b34dab8f'
+    url "http://seq.cs.iastate.edu/CAP3/cap3.linux.x86_64.tar"
+    sha256 "3aff30423e052887925b32f31bdd76764406661f2be3750afbf46341c3d38a06"
   end
-  version '2007-12-21'
+  version "2015-02-11"
 
   def install
-    bin.install 'cap3', 'formcon'
+    bin.install "cap3", "formcon"
     doc.install %w[README aceform doc example]
   end
 
   test do
-    system 'cap3 2>&1 |grep -q cap3'
+    system "cap3 2>&1 |grep -q cap3"
   end
 end
