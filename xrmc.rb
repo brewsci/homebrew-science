cbbb71459a5cc3e55c313795e3e0dcbf9e24d3ab
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-12 14:42:51 +0000

    xrmc: add 6.5.0 bottle.

--- xrmc.rb
+++ xrmc.rb
@@ -4,6 +4,13 @@ class Xrmc < Formula
   mirror "https://xrmc.s3.amazonaws.com/xrmc-6.5.0.tar.gz"
   sha256 "4995eaaf3b4583d443d0cf2003d73d1855b443938e431a4f758a607f540e026a"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "1cf2a27ffe0c5d1a71476a86da7d3ce4352004af5d4d66a30b0bb118780fbb16" => :yosemite
+    sha256 "00da13bea2ed0330a6610fdd0d38289e95dbf3a977da3ccfd4299920c3c37f4f" => :mavericks
+    sha256 "80093f4721f71d0acb92c175aa2724c4b832f8630ef64219aaf39d72489bd4b7" => :mountain_lion
+  end
+
   depends_on "xraylib"
   depends_on "pkg-config" => :build
   needs :openmp
