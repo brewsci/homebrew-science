868fd82a894a5c4dc4a0419e29eba95e4115cfa4
Jonathan Chang <jchang641@gmail.com>
2015-03-11 21:54:31 -0700

    kent-tools 313 (new formula)
    Closes #1937.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ kent-tools.rb
@@ -0,0 +1,62 @@
+class KentTools < Formula
+  homepage "http://genome.ucsc.edu/"
+  url "http://hgdownload.cse.ucsc.edu/admin/exe/userApps.v313.src.tgz"
+  head "git://genome-source.cse.ucsc.edu/kent.git"
+  sha256 "2d374e9ad20c5c092feb081a85c7a8d04fee8324c41d33751c931c747ca36a0f"
+
+  depends_on :mysql
+  depends_on "libpng"
+  depends_on "openssl"
+
+  def install
+    libpng = Formula["libpng"]
+    mysql = Formula["mysql"]
+
+    args = ["userApps", "BINDIR=#{bin}", "SCRIPTS=#{bin}"]
+    args << "MACHTYPE=#{`uname -m`.chomp}"
+    args << "PNGLIB=-L#{libpng.opt_lib} -lpng"
+    args << "PNGINCL=-I#{libpng.opt_include}"
+
+    # On Linux, depends_on :mysql looks at system MySQL so check if Homebrew
+    # MySQL already exists. If it does, then link against that. Otherwise, let
+    # kent-tools link against system MySQL (see kent/src/inc/common.mk)
+    if mysql.installed?
+      args << "MYSQLINC=#{mysql.opt_include}/mysql"
+      args << "MYSQLLIBS=-lmysqlclient -lz"
+    end
+
+    cd build.head? ? "src" : "kent/src" do
+      system "make", *args
+    end
+
+    cd bin do
+      blat_bin = %w[blat faToTwoBit gfClient gfServer nibFrag pslPretty
+                    pslReps pslSort twoBitInfo twoBitToFa]
+      rm blat_bin
+      mv "calc", "kent-tools-calc"
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    The `calc` tool has been renamed to `kent-tools-calc`.
+
+    This only installs the standalone tools located at
+      http://hgdownload.cse.ucsc.edu/admin/exe/
+
+    If you need the full UCSC Genome Browser, run:
+      brew install ucsc-genome-browser
+
+    This does not install the BLAT tools. To install BLAT:
+      brew install blat
+    EOS
+  end
+
+  test do
+    (testpath/"test.fa").write <<-EOF.undent
+      >test
+      ACTG
+    EOF
+    system "#{bin}/faOneRecord test.fa test > out.fa"
+    compare_file "test.fa", "out.fa"
+  end
+end
