0c77f8182d1cd28dd996c0fb5ed85099bb08d829
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-13 22:27:38 +0000

    plink2: add 1.90b3 bottle.

--- plink2.rb
+++ plink2.rb
@@ -1,6 +1,14 @@
 class Plink2 < Formula
   url "https://github.com/chrchang/plink-ng/archive/v1.90b3.tar.gz"
   version "1.90b3"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "26d74e4add75728f538e98911b19a30a45936592b4447bce4ee3b71504a4bcfc" => :yosemite
+    sha256 "e30eccab2712c8479c62e5d89ac510e74192ceecaf8f18b40cca279db1737e99" => :mavericks
+    sha256 "fbdfe8f84c6385fc052080646ade2c6bb6f0f3081afc6c242e6c84bd9f99a9c1" => :mountain_lion
+  end
+
   # doi "10.1186/s13742-015-0047-8"
   # tag "bioinformatics"
   homepage "https://www.cog-genomics.org/plink2"
