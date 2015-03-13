d1ed6ce5f4197c550576b71f98a38f8b4866860a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-13 05:56:59 +0000

    readsim: add 1.6 bottle.

--- readsim.rb
+++ readsim.rb
@@ -5,6 +5,14 @@ class Readsim < Formula
   url "https://downloads.sourceforge.net/project/readsim/readsim-1.6.tar.gz"
   sha256 "44ff24eb2e3127bfa717ba65857ecd5380303b6c2f4067d3b4b30a41eb790bb3"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "0b3103ce65332c1fe781d6a52a3ac46d89c99d0d87a8e12f872c6d50ee597c9e" => :yosemite
+    sha256 "c663b3d74d092eb246a1e6d23bc1b466fc10b73bd89c621510c5155ff2095516" => :mavericks
+    sha256 "3a15b549621a44b9cfbad453757686e1251f0092a5e15cd3cfa934cb59b555be" => :mountain_lion
+  end
+
   def install
     prefix.install Dir["*"]
     bin.install_symlink "../src/readsim.py", "../src/dnasim.py"
