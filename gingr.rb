09df565729be85315254a1447aa140859e2f01fc
Torsten Seemann <torsten.seemann@gmail.com>
2015-03-15 10:46:50 +1100

    Gingr 1.2 (formula version update)
    Closes #1968.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- gingr.rb
+++ gingr.rb
@@ -1,28 +1,28 @@
 class Gingr < Formula
-  homepage "https://github.com/marbl/gingr"
+  homepage "https://github.com/marbl/harvest/blob/master/docs/content/gingr.rst"
   head "https://github.com/marbl/gingr.git"
-  #tag "bioinformatics"
-  #doi "10.1186/s13059-014-0524-x"
+  # tag "bioinformatics"
+  # doi "10.1186/s13059-014-0524-x"
 
   if OS.mac?
-    url "https://github.com/marbl/gingr/releases/download/v1.0.1/gingr-OSX64-v1.0.1.zip"
-    sha1 "30d6549f08a099b7a0fe278b947a3b7ce56e4c4d"
+    url "https://github.com/marbl/gingr/releases/download/v1.2/gingr-OSX64-v1.2.zip"
+    sha1 "c848640f5987eacf1fd3bf7ec4a59e72fc2d59e9"
   elsif OS.linux?
-    url "https://github.com/marbl/gingr/releases/download/v1.0.1/gingr-Linux64-v1.0.1.tar.gz"
-    sha1 "8b43bc52340542cac1ec43aa808fa4b151187bd9"
+    url "https://github.com/marbl/gingr/releases/download/v1.2/gingr-Linux64-v1.2.tar.gz"
+    sha1 "b1f3e9f31bbc3510aa2d9b286643dd465ca61e13"
   else
     raise "Unsupported operating system"
   end
 
   def install
     if OS.mac?
-      prefix.install "../gingr.app"
+      prefix.install "../Gingr.app"
     else
       bin.install "gingr"
     end
   end
 
   test do
-    system "#{bin}/gingr", "-h" unless OS.mac?
+    # gingr no longer supports command line options, only GUI
   end
 end
