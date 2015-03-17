6acdf179be8fe25c2833093e70f4025af5f24a73
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-17 03:00:19 +0000

    muscle: update 3.8.31 bottle.

--- muscle.rb
+++ muscle.rb
@@ -8,11 +8,12 @@ class Muscle < Formula
   sha1 "2fe55db73ff4e7ac6d4ca692f8f213d1c5071dac"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    sha1 "b29b1f47580ef76f031bc19d0edded75b17dc5ff" => :yosemite
-    sha1 "08147f3f1f174c2d4e9c631dfbc2647d67a3cd6c" => :mavericks
-    sha1 "8398248dcadcae5a753eae15630cb815d9b19cdc" => :mountain_lion
+    revision 1
+    sha256 "0a990ee3f64ded25220d31a433c042bcdba901a73dbece2ec9ada87922f63223" => :yosemite
+    sha256 "8d13788647466efb841666a9c063cc464abf41fdb430b4b2cb20936bec74b4f3" => :mavericks
+    sha256 "00f1fff48e1003d2e31a3868bd7360a4c2d2736bce3e398e6149dbce87bcf1de" => :mountain_lion
   end
 
   def install
