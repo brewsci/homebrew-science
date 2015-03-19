33d0d6fd016b4b881fafb88aa76b9ff308edcf88
Saket Choudhary <saketkc@gmail.com>
2015-03-19 15:55:10 -0700

    circos: 0.67-7

--- circos.rb
+++ circos.rb
@@ -1,24 +1,22 @@
-require 'formula'
-
 class Circos < Formula
-  homepage 'http://circos.ca'
-  url 'http://circos.ca/distribution/circos-0.64.tgz'
-  sha1 'f24a5806a533ae451d89bc441c03f77c87be0394'
+  homepage "http://circos.ca"
+  url "http://circos.ca/distribution/circos-0.67-7.tgz"
+  sha256 "6e1fc82e1f2f74056fa3229eb43f069e5813e911e9d75e6267f5e1e569a87b49"
 
-  depends_on 'gd'
-  depends_on 'Config::General' => :perl
-  depends_on 'Font::TTF::Font' => :perl
-  depends_on 'GD::Polyline' => :perl
-  depends_on 'Math::Bezier' => :perl
-  depends_on 'Math::VecStat' => :perl
-  depends_on 'Readonly' => :perl
-  depends_on 'Set::IntSpan' => :perl
-  depends_on 'Text::Format' => :perl
+  depends_on "gd"
+  depends_on "Config::General" => :perl
+  depends_on "Font::TTF::Font" => :perl
+  depends_on "GD::Polyline" => :perl
+  depends_on "Math::Bezier" => :perl
+  depends_on "Math::VecStat" => :perl
+  depends_on "Readonly" => :perl
+  depends_on "Set::IntSpan" => :perl
+  depends_on "Text::Format" => :perl
 
   def install
-    inreplace 'bin/circos', '#!/bin/env perl', '#!/usr/bin/env perl'
-    libexec.install Dir['*']
-    bin.install_symlink '../libexec/bin/circos' => 'circos'
+    inreplace "bin/circos", "#!/bin/env perl", "#!/usr/bin/env perl"
+    libexec.install Dir["*"]
+    bin.install_symlink "../libexec/bin/circos" => "circos"
   end
 
   def caveats; <<-EOS.undent
