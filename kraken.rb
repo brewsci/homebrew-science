af4cea762f4c555bfe7a04bac7225eac54ef713a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-20 21:40:44 +0000

    kraken: add 0.10.5-beta bottle.

--- kraken.rb
+++ kraken.rb
@@ -3,6 +3,14 @@ class Kraken < Formula
   url "https://ccb.jhu.edu/software/kraken/dl/kraken-0.10.5-beta.tgz"
   sha256 "7c0ac64ee0acdcce18e16b51b636b7cdc6d07ea7ab465bb64df078c5a710346b"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "8aa0b49d43549c08ab6dbad12cf0a5e2762b5c8f020a9e57019880a5ab0aeda9" => :yosemite
+    sha256 "2f2bf03509e930ee0dd6080ec81fe73dc2bc271d0adf40e198edb898e6f77860" => :mavericks
+    sha256 "7dd3ad72403d173bb5cab35a51099e20bc7408c2148376dbc08edadc4ed1fa5e" => :mountain_lion
+  end
+
   needs :openmp
 
   def install
