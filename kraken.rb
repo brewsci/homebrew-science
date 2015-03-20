cf53f61d6afc9d09f7104f81d71dae930fcf6a7e
Saket Choudhary <saketkc@gmail.com>
2015-03-20 12:52:05 -0700

    kraken 0.10-5-beta
    Closes #1984.
    Closes #1990.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- kraken.rb
+++ kraken.rb
@@ -1,20 +1,19 @@
-require "formula"
-
 class Kraken < Formula
   homepage "http://ccb.jhu.edu/software/kraken/"
-  url "http://ccb.jhu.edu/software/kraken/dl/kraken-0.10.3-beta.tgz"
-  sha1 "64f88004c341871d883235f4ae7c876c0136b885"
+  url "https://ccb.jhu.edu/software/kraken/dl/kraken-0.10.5-beta.tgz"
+  sha256 "7c0ac64ee0acdcce18e16b51b636b7cdc6d07ea7ab465bb64df078c5a710346b"
 
   needs :openmp
 
   def install
     libexec.mkdir
+    inreplace "install_kraken.sh", "[ -x \"$file\" ] && echo \"  $file\"", "if [ -x \"$file\" ]; then echo \"  $file\"; fi"
     system "./install_kraken.sh", libexec
-    libexec_bins = ["kraken", "kraken-build", "kraken-report"].map { |x| libexec + x }
+    libexec_bins = ["kraken", "kraken-build", "kraken-filter", "kraken-mpa-report", "kraken-report", "kraken-translate"].map { |x| libexec + x }
     bin.install_symlink(libexec_bins)
   end
 
   test do
-    system "kraken", "--version"
+    system "#{bin}/kraken", "--version"
   end
 end
