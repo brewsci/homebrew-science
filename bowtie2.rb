22d2f04d68bd0e2ba4fb57396a6e9eda92556459
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-23 08:01:11 +0000

    bowtie2: add 2.2.5 bottle.

--- bowtie2.rb
+++ bowtie2.rb
@@ -6,6 +6,14 @@ class Bowtie2 < Formula
   url "https://github.com/BenLangmead/bowtie2/archive/v2.2.5.tar.gz"
   sha256 "fbbd630596d066f84c925d08db854d448b9780d672702e13bd76648133ac92e2"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "67239a1075d5116d7833b0a04b18c9efed3f4fb27510a9dfc0546d52afce0a63" => :yosemite
+    sha256 "0c84c5e5505dc7ef83c7da6ba691948cebf2c9cd68a0b051dd67e347908f9eea" => :mavericks
+    sha256 "fd0bda8aec4d0d9a1f9b433d465fed2c026d1b88ea44688315fbde892d9d64a9" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install %W[bowtie2
