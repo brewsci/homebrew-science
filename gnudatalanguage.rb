9b49c33e74d7014db8717820860f638f48844249
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-23 23:07:30 +0000

    gnudatalanguage: add 0.9.5 bottle.

--- gnudatalanguage.rb
+++ gnudatalanguage.rb
@@ -3,6 +3,13 @@ class Gnudatalanguage < Formula
   url "https://downloads.sourceforge.net/project/gnudatalanguage/gdl/0.9.5/gdl-0.9.5.tar.gz"
   sha256 "cc9635e836b5ea456cad93f8a07d589aed8649668fbd14c4aad22091991137e2"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "ee611942e88f7876834e74498d2780905bb80657d447ebde5b5046c06bace626" => :yosemite
+    sha256 "06676b26c8a04b2986f53762793c5c7b8db0f3b8bf9236758ca7ba993dad73c6" => :mavericks
+    sha256 "ffa274854445489a7f04a6cb49d669e8660e4c8beb7bae8984018d0ae575305c" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "plplot"
