c49651e4be7e083ca98e06d39473804fe34d47ae
Ian Lancaster <ilancaster@gmail.com>
2015-03-24 07:38:04 -0400

    colpack: always build with libcxx

--- colpack.rb
+++ colpack.rb
@@ -1,15 +1,11 @@
-require 'formula'
-
 class Colpack < Formula
-  homepage 'http://www.cscapes.org/coloringpage/software.htm'
-  url 'http://cscapes.cs.purdue.edu/download/ColPack/ColPack-1.0.9.tar.gz'
-  sha1 'c963424c3e97a7bc3756d3feb742418e89721e48'
-
-  option 'with-libc++', 'build against libc++ instead of libstdc++' # for adol-c
+  homepage "http://www.cscapes.org/coloringpage/software.htm"
+  url "http://cscapes.cs.purdue.edu/download/ColPack/ColPack-1.0.9.tar.gz"
+  sha1 "c963424c3e97a7bc3756d3feb742418e89721e48"
 
   def install
-    ENV.libcxx if build.with? "libc++"
-    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
-    system "make install"
+    ENV.libcxx
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make", "install"
   end
 end
