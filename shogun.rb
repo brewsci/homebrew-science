89d7fdcdcc5bd8b04fa8d82f6d739363e8912e4c
Ian Lancaster <ilancaster@gmail.com>
2015-03-24 07:34:39 -0400

    shogun 4.0.0
    Closes #2010. Closes #1818.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- shogun.rb
+++ shogun.rb
@@ -1,83 +1,80 @@
-require 'formula'
-
 class Shogun < Formula
-  homepage 'http://www.shogun-toolbox.org'
-  url 'http://shogun-toolbox.org/archives/shogun/releases/2.1/sources/shogun-2.1.0.tar.bz2'
-  sha1 '706401267bfceedff981c7709be1098b18ffb031'
-  revision 1
+  homepage "http://www.shogun-toolbox.org"
+  url "http://shogun-toolbox.org/archives/shogun/releases/4.0/sources/shogun-4.0.0.tar.bz2"
+  sha256 "ab39d3cc27fc1ddf6128f6e3fc60217e73a843b51c20f5d3ceb69e6565a43695"
 
-  bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "a4c6fcf6f847778a25483ef5e85a1ac71c510d09" => :yosemite
-    sha1 "7ed6fd9c35fd9a42b4a7738a54d1adb6000f7812" => :mavericks
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "xz"
+  depends_on "lzo"
+  depends_on "snappy"
+  depends_on "hdf5"
+  depends_on "protobuf" => (MacOS.version < :mavericks) ? ["c++11"] : []
+  depends_on "json-c"
+  depends_on "readline"
+  depends_on "nlopt"
+  depends_on "eigen"
+  depends_on "arpack"
+  depends_on "colpack"
+  depends_on "glpk"
+  depends_on "r" => :optional
+  depends_on "lua" => :optional
+  depends_on "octave" => :optional
+  depends_on "opencv" => :optional
+  depends_on :java => :optional
+  depends_on :python => :optional
+
+  if build.with? "python"
+    depends_on "swig" => :build
+    depends_on "numpy" => :python
+    depends_on "matplotlib" => :python
   end
 
-  option 'disable-svm-light', 'Disable SVM-light module, which is GPLv3 and makes all of shogun GPLv3'
+  needs :cxx11
 
-  depends_on 'pkg-config' => :build
-  depends_on 'hdf5' => :recommended
-  depends_on 'json-c' => :recommended
-  depends_on 'readline' => :recommended
-  depends_on 'nlopt' => :recommended
-  depends_on 'eigen' => :recommended
-  depends_on 'glpk' => :recommended
-  depends_on 'lzo' => :recommended
-  depends_on 'snappy' => :recommended
-  depends_on 'xz' => :recommended  # provides lzma
-  depends_on 'swig' => [:recommended, :build] # needef for dynamic python bindings
-  depends_on 'numpy' => :python # You may want `brew tap homebrew/python && brew install numpy`
-  depends_on 'matplotlib' => :python # You may want `brew tap homebrew/python && brew install matplotlib`
-  depends_on 'r' => :optional
-  depends_on 'lua' => :optional
-  depends_on 'octave' => :optional
+  def cmake_use(opt, arg)
+    "-D#{arg}=#{build.with?(opt) ? "ON" : "OFF"}"
+  end
 
-  # Todo: support additional deps: arpack, mosek, superlu, cplex, lpsolve
-  #       Help us by hacking on this and open a pull request! Thanks.
   def install
-    pydir = "#{which_python}/site-packages"
+    # fix build of modular interfaces with SWIG 3.0.5 on MacOSX
+    # https://github.com/shogun-toolbox/shogun/pull/2694
+    # https://github.com/shogun-toolbox/shogun/commit/fef8937d215db7
+    ENV.append_to_cflags "-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0"
 
-    args = [ "--prefix=#{prefix}",
-             "--pydir=#{pydir}",
-             "--enable-hmm-parallel" ]
-    args << "--disable-svm-light" if build.include? 'disable-svm-light'
+    args = std_cmake_args + [
+      "-DBUILD_EXAMPLES=OFF",
+      "-DBUNDLE_ARPREC=OFF",
+      "-DBUNDLE_COLPACK=OFF",
+      "-DBUNDLE_EIGEN=OFF",
+      "-DBUNDLE_JSON=OFF",
+      "-DBUNDLE_NLOPT=OFF",
+      "-DENABLE_TESTING=ON",
+      "-DENABLE_COVERAGE=OFF",
+      "-DLIB_INSTALL_DIR=#{lib}",
+      "-DCmdLineStatic=ON",
+      cmake_use("opencv", "OpenCV"),
+      cmake_use("r", "RModular"),
+      cmake_use("octave", "OctaveModular"),
+      cmake_use("lua", "LuaModular"),
+      cmake_use("python", "PythonModular"),
+      cmake_use("java", "JavaModular"),
+    ]
 
-    # Todo: if we have depends_on :python => :recommended, we can disable
-    #       swig, numpy and matplotlib deps if `--without-python`.
-
-    unless MacOS::CLT.installed?
-      # fix: "Checking for Mac OS vector library ... no"
-      ["src/configure", "src/shogun/mathematics/lapack.h"].each do |f|
-        inreplace f, "#include </System/Library/Frameworks/vecLib.framework",
-                     "#include <#{MacOS.sdk_path}/System/Library/Frameworks/vecLib.framework"
-      end
-    end
-
-    # Todo: fix the following configure msgs:
-    #       "Checking for Readline support ... no"
-
-    cd 'src' do
-      system "./configure", *args
+    mkdir "build" do
+      system "cmake", "..", *args
       system "make"
-      system "make install"
+      system "make", "install"
     end
-    share.install Dir['examples']
   end
 
   test do
-    (testpath/'test.sg').write <<-EOF.undent
+    (testpath/"test.sg").write <<-EOF.undent
       new_classifier LIBSVM
       save_classifier test.model
       exit
     EOF
     system bin/"shogun", "test.sg"
-    raise "failed" unless File.exist?("test.model")
-
-    # if build.with? 'python'
-    #   system "python", share/"examples/documented/python_modular/graphical/svm.py"
-    # end
-  end
-
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+    assert File.exist?("test.model")
   end
 end
