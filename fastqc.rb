9862050666f41dc5ab8a235714a2e22ecc0e2a56
Shaun Jackman <sjackman@gmail.com>
2015-03-25 11:00:04 -0700

    fastqc 0.11.3
    Depends on Java
    
    Closes #2013.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fastqc.rb
+++ fastqc.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Fastqc < Formula
   homepage "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/"
-  #tag "bioinformatics"
+  # tag "bioinformatics"
 
-  url "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.2.zip"
-  sha1 "d4dc1b903de35aa4de8e995c4974e0869db99dda"
+  url "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.3.zip"
+  sha256 "ed2e8e6793bcedbd749d65a38dd8419be841e4653319e5fdd1e0e2b063b60ffb"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -13,6 +11,8 @@ class Fastqc < Formula
     sha1 "705413545eeab36baf99ad66863882c91a2bccb9" => :mavericks
     sha1 "0c78a464a90f35496d758023665d173f00f5b857" => :mountain_lion
   end
+
+  depends_on :java
 
   def install
     chmod 0755, "fastqc"
