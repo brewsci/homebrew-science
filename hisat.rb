451d46ec614289f5c63cddfff11388f9b68e5be6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-27 02:49:03 +0000

    hisat: add 0.1.5b bottle.

--- hisat.rb
+++ hisat.rb
@@ -6,6 +6,14 @@ class Hisat < Formula
   sha256 "0a58d820297fae2f90a783bdb714621a6051fe2bde0a60d518cb3672eeda2210"
   version "0.1.5b"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "d9540bb34062a9037d402352f99a61f6cda54732460d3eab39f439a4ea449fdf" => :yosemite
+    sha256 "30639176fdd893df3b6d64761a09de3a44ada9cb038771296651e2277dabf6f8" => :mavericks
+    sha256 "12d5c4dc85b63920c6e0e6b7808336fd7818111e88e940552ec663557e0d4b60" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "hisat", Dir["hisat-*"]
