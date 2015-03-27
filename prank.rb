d9a4391d3c11960a32722c67096c0070dcaa7846
Ian Lancaster <ilancaster@gmail.com>
2015-03-27 10:33:21 -0400

    prank: update homepage

--- prank.rb
+++ prank.rb
@@ -1,14 +1,12 @@
-require 'formula'
-
 class Prank < Formula
-  homepage 'http://code.google.com/p/prank-msa/'
-  url 'https://prank-msa.googlecode.com/files/prank.src.130410.tgz'
-  sha1 '8bfaec6e9bed1b0276188752609e753088e28acf'
+  homepage "http://wasabiapp.org/software/prank/"
+  url "https://prank-msa.googlecode.com/files/prank.src.130410.tgz"
+  sha1 "8bfaec6e9bed1b0276188752609e753088e28acf"
 
-  head 'https://code.google.com/p/prank-msa/', :using => :git
+  head "https://code.google.com/p/prank-msa/", :using => :git
 
-  depends_on 'mafft'
-  depends_on 'exonerate'
+  depends_on "mafft"
+  depends_on "exonerate"
 
   def install
     cd "src" do
