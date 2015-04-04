f4c53fbd7033c44b19fddeafcec2f75a12708384
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-04 20:17:00 +0100

    beast: add 1.8.2 bottle.

--- beast.rb
+++ beast.rb
@@ -7,6 +7,14 @@ class Beast < Formula
   version "1.8.2"
   sha1 "47a5aca20fecf6cb61a301f8b03d1e750858721a"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "e1357fad70b3a51ce734a705667f2e9d16bdddf480bf340559cdad0bbcaacb65" => :yosemite
+    sha256 "c411831dc26441e4b5bd92dc1926fbd8171d5c8d26d17239f2ce1e9604f67f8b" => :mavericks
+    sha256 "c3974c08c01dfa26db9407b070b4302a109043725fef586b4d82290603f2dfee" => :mountain_lion
+  end
+
   head do
     url "https://beast-mcmc.googlecode.com/svn/trunk/"
     depends_on :ant
