83b0d1685d562268f4190776505be152e31ac838
Ian Lancaster <ilancaster@gmail.com>
2015-04-05 13:04:52 -0400

    parsnp: remove bottle block

--- parsnp.rb
+++ parsnp.rb
@@ -2,17 +2,7 @@ class Parsnp < Formula
   homepage "https://github.com/marbl/parsnp"
   # tag "bioinformatics"
   # doi "10.1186/s13059-014-0524-x"
-
   head "https://github.com/marbl/parsnp.git"
-
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-science"
-    cellar :any
-    revision 1
-    sha256 "17db88e2d32cf5305289c6f01efc7c5aa84322b320c88a5fb3b1edb08af38d66" => :yosemite
-    sha256 "2af68fb53847adc2dfaccae9de21298f158e818afb2672c64d0b092298440030" => :mavericks
-    sha256 "a29141c0299d5fc72be88c9b29b1501ecba66b29d79e3c756d70a13c1bd1bac2" => :mountain_lion
-  end
 
   if OS.mac?
     url "https://github.com/marbl/parsnp/releases/download/v1.2/parsnp-OSX64-v1.2.tar.gz"
