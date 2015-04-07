7e135e6f5938696310c569b159dbfe746be9a214
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-07 09:55:05 +0100

    flash: add 1.2.11 bottle.

--- flash.rb
+++ flash.rb
@@ -6,6 +6,14 @@ class Flash < Formula
   url "https://downloads.sourceforge.net/project/flashpage/FLASH-1.2.11.tar.gz"
   sha256 "685ca6f7fedda07434d8ee03c536f4763385671c4509c5bb48beb3055fd236ac"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "206839e1465012091e27ec4c3c35f8872f35602b24e28d5883d59f56ab9640f8" => :yosemite
+    sha256 "888410d1c714b4cd7d26231774b0cfc767bf2c89e1ecd719f5252de1e1a90c34" => :mavericks
+    sha256 "21ea6e6967301f942336c7c9f45502786bbf7ee02ac5338fe6c02e4f65b1e429" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "flash"
