ee55af13bf35966a16a5ce36640f53208dbf2ec7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-07 09:19:51 +0100

    stringtie: add 1.0.3 bottle.

--- stringtie.rb
+++ stringtie.rb
@@ -1,6 +1,14 @@
 class Stringtie < Formula
   homepage "http://ccb.jhu.edu/software/stringtie"
   head "https://github.com/gpertea/stringtie"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "629fc4718be56af23c58ad49450445443280cf5f4abe7e0d9fdad62a01ea7d39" => :yosemite
+    sha256 "3f615ab88a8d943cdb1555b1176506aa547be2b37ac213c9d1332463329f6051" => :mavericks
+    sha256 "299ffb79d445e9984f5fe8050f34e74d67a3ba93c363d574105edef47f1cbdc5" => :mountain_lion
+  end
+
   # doi "10.1038/nbt.3122"
   # tag "bioinformatics"
 
