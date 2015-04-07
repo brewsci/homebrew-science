ef223db9b3fca77b31e5a5c919d90c90bc296228
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-07 11:10:09 +0100

    weblogo: add 2.8.2 bottle.

--- weblogo.rb
+++ weblogo.rb
@@ -6,6 +6,14 @@ class Weblogo < Formula
   url "http://weblogo.berkeley.edu/release/weblogo.2.8.2.tar.gz"
   sha256 "2d3e0040c0c1e363c1dfd57f8b585387eb682ed08b2cc2fe2e4cc2a33ac52266"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "1ee5b0c428f165ae1c41e08e05ccd2cc3190c60c069b36e798225cf0c1aef19e" => :yosemite
+    sha256 "7c4085e2c4f372e6b2b32bad9ac64936d25c5c0daa040b35541a4531f1dad5f4" => :mavericks
+    sha256 "be96774db77a74553b96553d17920a2f3ac61519d846fbcfd0c5348038a2e589" => :mountain_lion
+  end
+
   depends_on "imagemagick"
   depends_on "ghostscript"
   depends_on "Getopt::Std" => :perl
