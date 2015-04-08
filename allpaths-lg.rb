5ddbad2f85738f5f88498a804891fccfa4f73bb4
Tim D. Smith <git@tim-smith.us>
2015-04-07 17:51:48 -0700

    allpaths-lg: add LinuxRequirement
    allpaths-lg does not build on OS X.
    https://github.com/Homebrew/homebrew-science/issues/1329#issuecomment-68387020
    
    Closes #1975. Closes #1889. Closes #1329.
    
    Closes #2062.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- allpaths-lg.rb
+++ allpaths-lg.rb
@@ -1,3 +1,11 @@
+class LinuxRequirement < Requirement
+  fatal true
+  satisfy OS.linux?
+  def message
+    "This software only builds on Linux."
+  end
+end
+
 class AllpathsLg < Formula
   homepage "http://www.broadinstitute.org/software/allpaths-lg/blog/"
   #doi "10.1073/pnas.1017351108"
@@ -5,6 +13,10 @@ class AllpathsLg < Formula
 
   url "ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-50378.tar.gz"
   sha1 "06cf7ed3ca8ed55847e895a93cf41b760a2b7021"
+
+  # Prove us wrong!
+  # https://github.com/Homebrew/homebrew-science/issues/1329#issuecomment-68387020
+  depends_on LinuxRequirement
 
   fails_with :clang do
     cause "The only supported compiler is GCC(>=4.7)."
