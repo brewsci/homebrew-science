ae1ab576456e0ee9fb64369847910f6091132193
Alex Dunn <dunn.alex@gmail.com>
2015-04-07 23:07:25 -0700

    nco 4.4.8 + devel
    Closes #2070.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- nco.rb
+++ nco.rb
@@ -1,28 +1,43 @@
-require 'formula'
-
-# NCO: Tools for slicing and dicing NetCDF files.
-
 class Nco < Formula
-  homepage 'http://nco.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/nco/nco-4.4.2.tar.gz'
-  sha1 '6253e0d3b00359e1ef2c95f0c86e940697286a10'
+  homepage "http://nco.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/nco/nco-4.4.8.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/n/nco/nco_4.4.8.orig.tar.gz"
+  sha256 "974dea6ac11d8b265e8dd5b29376fede5ea76a50dc06afb4d5de49d7e8d48774"
 
-  depends_on 'gsl'
-  depends_on 'netcdf'
-  depends_on 'texinfo'
-  depends_on 'udunits'
+  devel do
+    url "https://github.com/czender/nco/archive/nco-4.4.9-alpha.tar.gz"
+    sha256 "bebd22d810b9a58e3a148fe316929c238403c6352b77f3ea9757662222540204"
+    version "4.4.9-alpha"
+  end
+
+  head do
+    url "https://github.com/czender/nco.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
+  depends_on "gsl"
+  depends_on "netcdf"
+  depends_on "texinfo"
+  depends_on "udunits"
 
   # NCO requires the C++ interface in Antlr2.
-  depends_on 'homebrew/versions/antlr2'
+  depends_on "homebrew/versions/antlr2"
 
   def install
-    inreplace 'configure' do |s|
+    system "./autogen.sh" if build.head?
+    inreplace "configure" do |s|
       # The Antlr 2.x program installed by Homebrew is called antlr2
-      s.gsub! 'for ac_prog in runantlr antlr', 'for ac_prog in runantlr antlr2'
+      s.gsub! "for ac_prog in runantlr antlr", "for ac_prog in runantlr antlr2"
     end
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--enable-netcdf4"
-    system "make install"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-netcdf4"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"ncap", "--help"
   end
 end
