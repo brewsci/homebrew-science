e666c2fed2d546ce16f3d3730cc2155ae2854310
Alex Dunn <dunn.alex@gmail.com>
2015-04-07 21:15:53 -0700

    gtsam 3.5.1
    Closes #2067.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- gtsam.rb
+++ gtsam.rb
@@ -1,17 +1,22 @@
-require 'formula'
-
 class Gtsam < Formula
-  homepage 'https://collab.cc.gatech.edu/borg/gtsam/'
-  url 'https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-2.3.0.tgz'
-  sha1 'ec1a3e8a91365a9680ba92349705b37c98555e7c'
+  homepage "https://collab.cc.gatech.edu/borg/gtsam/"
+  url "https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-3.2.1.tgz"
+  sha256 "1e9217c11d92e6838e2d0bec3a7dd0d36d2131acdf2e50264f6dc225d8ce1a97"
 
-  depends_on 'cmake' => :build
-  depends_on 'boost'
+  depends_on "cmake" => :build
+  depends_on "boost"
+
+  option "with-tests", "Run unit tests at build-time"
 
   def install
-    mkdir 'build' do
+    mkdir "build" do
       system "cmake", "..", *std_cmake_args
+      system "make", "check" if build.with? "tests"
       system "make", "install"
     end
+  end
+
+  test do
+    system bin/"wrap"
   end
 end
