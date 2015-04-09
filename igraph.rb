3b1babd72805b5342fccb784c7155e7931cd67f1
Dominique Orban <dominique.orban@gmail.com>
2015-04-09 11:38:39 -0400

    iGraph: disable GMP if elected + new options.
    Add options for BLAS/LAPACK, ARPACK and GLPK.
    Fix audit warnings.
    
    Fixes #1969.
    Closes #2078.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- igraph.rb
+++ igraph.rb
@@ -1,27 +1,39 @@
-require 'formula'
-
 class Igraph < Formula
-  homepage 'http://igraph.org'
-  url 'http://igraph.org/nightly/get/c/igraph-0.7.1.tar.gz'
-  sha1 '2cf3528a60c52810a3d5ed9f117692f8f639aac1'
+  homepage "http://igraph.org"
+  url "http://igraph.org/nightly/get/c/igraph-0.7.1.tar.gz"
+  sha256 "d978030e27369bf698f3816ab70aa9141e9baf81c56cc4f55efbe5489b46b0df"
 
   option :universal
 
+  # Use Homebrew Arpack, but disable thread-local storage.
+  # If not selected, iGraph uses a built-in Arpack.
+  depends_on "arpack" => :optional
+
   # GMP is optional, and doesn't have a universal build
-  depends_on 'gmp' => :optional unless build.universal?
+  depends_on "gmp" => :optional unless build.universal?
+
+  depends_on "glpk" => :recommended
 
   def install
     ENV.universal_binary if build.universal?
 
+    # There doesn't seem to be a way to specify which BLAS/LAPACK, ARPACK or GPLK.
+    # iGraph just looks for -lblas, -llapack, -larpack and -lglpk on the path.
+    extra_opts = ["--with-external-blas", "--with-external-lapack"]
+    extra_opts << ((build.with? "glpk") ? "--with-external-glpk" : "--disable-glpk")
+    extra_opts << "--disable-gmp" if build.without? "gmp"
+    extra_opts += ["--with-external-arpack", "--disable-tls"] if build.with? "arpack"
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+                          "--prefix=#{prefix}",
+                          *extra_opts
+    system "make", "install"
   end
 
   test do
     # Adapted from http://igraph.org/c/doc/igraph-tutorial.html
-    (testpath/"igraph_test.c").write <<-EOS.undent
+    (testpath / "igraph_test.c").write <<-EOS.undent
       #include <igraph.h>
 
       int main(void)
