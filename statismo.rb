c729be58336bfec3e5742c785f7b4a970ad64e70
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-09 21:33:01 +0100

    statismo: add 0.10.2 bottle.

--- statismo.rb
+++ statismo.rb
@@ -4,6 +4,13 @@ class Statismo < Formula
   sha256 "e0f66dea821e53d6327c8a624d41ac407ba7066849c33b6d9e58253b355955e8"
   head "https://github.com/statismo/statismo.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "916a4135113d9842866fbbca9c26ba2a229f25dd9914e101524406dac51b718f" => :yosemite
+    sha256 "0fb090a76886999f7c6d85b1227ca140e7d4f4827b411ecee7b1610c1128f5f1" => :mavericks
+    sha256 "f23c9e5f546976c09788339255e3b37544f1f6eddae5367dd56c74d920b58956" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "eigen" => :build
   depends_on "boost" => :build
