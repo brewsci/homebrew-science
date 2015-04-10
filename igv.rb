75a8609e5859bca2cd3fff64b1318936457a23c6
なつき <i@ntk.me>
2015-04-09 17:17:48 -0700

    igv 2.3.48

--- igv.rb
+++ igv.rb
@@ -3,8 +3,8 @@ class Igv < Formula
   # tag "bioinformatics"
   # doi "10.1093/bib/bbs017"
   head "https://github.com/broadinstitute/IGV.git"
-  url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.43.zip"
-  sha256 "4420140f5301413fba73b00e954f81a2733f2d42e12b99aa9a3f73c3b01e49e1"
+  url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.48.zip"
+  sha256 "cdfc7082c2c831b5809a19d7e86dc09d35eff7e725d70a96837bc28b2f8c534a"
 
   def install
     inreplace "igv.sh", /^prefix=.*/, "prefix=#{libexec}"
