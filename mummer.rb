7f7d3c3d52db227a7282b19030d164c08beb903d
Shaun Jackman <sjackman@gmail.com>
2015-04-10 12:58:19 +0200

    mummer 3.23: Tidy up
    Closes #2088.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- mummer.rb
+++ mummer.rb
@@ -1,10 +1,10 @@
 class Mummer < Formula
   homepage "http://mummer.sourceforge.net/"
-  #doi "10.1186/gb-2004-5-2-r12"
-  #tag "bioinformatics"
+  # doi "10.1186/gb-2004-5-2-r12"
+  # tag "bioinformatics"
 
   url "https://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz"
-  sha1 "69261ed80bb77e7595100f0560973fe1f810c5fa"
+  sha256 "1efad4f7d8cee0d8eaebb320a2d63745bb3a160bb513a15ef7af46f330af662f"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -14,86 +14,23 @@ class Mummer < Formula
     sha1 "78a0688d0c6d181c0dc7cfdf7ca1bf1f0eaf87fd" => :mountain_lion
   end
 
+  TOOLS = %w[
+    annotate combineMUMs delta-filter dnadiff exact-tandems mapview mgaps
+    mummer mummerplot nucmer promer repeat-match
+    run-mummer1 run-mummer3 show-coords show-diff show-snps show-tiling]
+
   def install
-
-    inreplace "scripts/Makefile", "$(CURDIR)", prefix
-
-    system "make"
-
-    %w[dnadiff mummerplot nucmer promer mapview].each do |script|
-      inreplace script, "use lib \"#{prefix}", "use lib \"#{share}/mummer"
+    prefix.install Dir["*"]
+    cd prefix do
+      system "make"
+      rm_r "src"
     end
-
-    inreplace "promer" do |s|
-      s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
-      s.gsub! /AUX_BIN_DIR = "[^"]+/ , "AUX_BIN_DIR = \"#{libexec}"
-      s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
-    end
-
-    inreplace "nucmer" do |s|
-      s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
-      s.gsub! /AUX_BIN_DIR = "[^"]+/ , "AUX_BIN_DIR = \"#{libexec}"
-      s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
-    end
-
-    inreplace "dnadiff" do |s|
-      s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
-      s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
-    end
-
-    inreplace "mummerplot" do |s|
-      s.gsub! /BIN_DIR = "[^"]+/ , "BIN_DIR = \"#{libexec}"
-      s.gsub! /SCRIPT_DIR = "[^"]+/ , "SCRIPT_DIR = \"#{share}/mummer"
-    end
-
-    inreplace "exact-tandems" do |s|
-      s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
-      s.gsub! /scriptdir = \S+/ , "scriptdir = #{libexec}"
-    end
-
-    inreplace "run-mummer1" do |s|
-      s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
-    end
-
-    inreplace "run-mummer3" do |s|
-      s.gsub! /bindir = \S+/ , "bindir = #{libexec}"
-    end
-
-    %w[run-mummer1 run-mummer3 nucmer promer].each do |app|
-      bin.install app
-    end
-    %w[show-diff repeat-match show-snps combineMUMs mgaps dnadiff
-      show-tiling delta-filter exact-tandems show-coords mummer mummerplot
-       mapview nucmer2xfig annotate gaps].each do |app|
-      libexec.install app
-    end
-    libexec.install "scripts/tandem-repeat.awk"
-
-    # postnuc, postpro, prenuc, prepro
-    Dir.glob("aux_bin/*").each do |script|
-      libexec.install script
-    end
-
-    (share/"mummer").install "scripts/Foundation.pm"
-
-  end
-
-  def caveats
-    <<-EOS.undent
-      show-diff, repeat-match, show-snps, combineMUMs, mgaps, dnadiff, show-tiling,
-      delta-filter, exact-tandems, show-coords, mummer, mummerplot, mapview, nucmer2xfig,
-      annotate, and gaps have been installed in #{libexec}
-    EOS
+    TOOLS.each { |tool| bin.install_symlink "../#{tool}" }
   end
 
   test do
-    %w[ nucmer promer run-mummer1 run-mummer3 ].each do |script|
-      system "#{bin}/#{script} -h 2>&1 | grep -i USAGE | grep #{script}"
-    end
-    %w[ dnadiff mapview mummerplot show-diff repeat-match show-snps combineMUMs
-        mgaps show-tiling delta-filter show-coords mummer mummerplot mapview].each do |script|
-      system "#{libexec}/#{script} -h 2>&1 | grep -i USAGE | grep #{script}"
+    TOOLS.each do |tool|
+      system "#{bin}/#{tool} -h 2>&1 |grep #{tool}"
     end
   end
-
 end
