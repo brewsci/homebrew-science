0dc0e425fb2e48c34c7c3eee6971cf4286192336
Ian Lancaster <ilancaster@gmail.com>
2015-04-10 00:26:05 -0400

    igvtools: remove bottle block

--- igvtools.rb
+++ igvtools.rb
@@ -5,14 +5,6 @@ class Igvtools < Formula
   url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.48.zip"
   sha256 "0ae8fc2f6500495ae84aa0232887d9466c73c188f5af91d64a089e5808d3a84d"
 
-  bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    cellar :any
-    sha1 "8f569e86b130e851e9f6389af18cdf6597fd1486" => :yosemite
-    sha1 "6304d3bc4d32528f2ddb1e9c92adb6001f139559" => :mavericks
-    sha1 "b976141a7fd94e25f97c463f3d53885c7bd2ee05" => :mountain_lion
-  end
-
   def install
     java = share/"java"
     java.install "igvtools.jar"
