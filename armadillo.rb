71a1119e590bb789d5c1271b4855a0b36a23b314
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-11 23:43:28 +0100

    armadillo: update 4.650.4 bottle.

--- armadillo.rb
+++ armadillo.rb
@@ -4,10 +4,10 @@ class Armadillo < Formula
   sha256 "ccc56580ec932ec9beade5679b07ecbed889e9b602866f673d1a05a7692b4bf8"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "de8127e4a288f77632b80c17373044e94a0656d2" => :yosemite
-    sha1 "ba1520f259937d34c5c1bd878fccc7e9518bc3ef" => :mavericks
-    sha1 "64ed43d130bfab2333c39ceb64eb2adea0506e2a" => :mountain_lion
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "132ea8fc2806cc92e974d5047aea984c867916a7e301ae845cd4db87aab07ad2" => :yosemite
+    sha256 "2e733759cf914864b181f6b43dd24278ba12626aa0965837637ba2d3c3094106" => :mavericks
+    sha256 "013164528de9b672dab1679ac10e10dd3075d6eb29d28740091dd44f63f6a23d" => :mountain_lion
   end
 
   option "with-hdf5", "Enable the ability to save and load matrices stored in the HDF5 format"
