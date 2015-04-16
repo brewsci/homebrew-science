00556937d1d5dc79d7787f2bdb2fe54eeec857dd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-16 21:01:43 +0100

    metis: add 5.1.0 bottle.

--- metis.rb
+++ metis.rb
@@ -3,6 +3,14 @@ class Metis < Formula
   url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz"
   sha256 "76faebe03f6c963127dbb73c13eab58c9a3faeae48779f049066a21c087c5db2"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "1e127767a51ae71e36fee29e3f646dc038898c1cca7da5b707266456ff50f5ac" => :yosemite
+    sha256 "da4e731c9c8c1b295e300eafe1851171cb08793484b0583ca6606a8344dae9ee" => :mavericks
+    sha256 "5ab878427e696a9893aeb9c69b18938430adfd8d1cdfd37b0c7149aa5aa68e19" => :mountain_lion
+  end
+
   option :universal
 
   depends_on "cmake" => :build
