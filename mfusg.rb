b4a295c18dd015ee8d48eb5a6d9e54428415a4f2
Ian Lancaster <ilancaster@gmail.com>
2015-04-17 04:19:28 -0400

    mfusg: remove :python

--- mfusg.rb
+++ mfusg.rb
@@ -1,21 +1,20 @@
-require "formula"
-
 class Mfusg < Formula
   homepage "http://water.usgs.gov/ogw/mfusg"
   url "http://water.usgs.gov/ogw/mfusg/mfusg.1_2_00.zip"
-  sha1 "ab25e4e816ad3c9b83720b7b367ddd4e287caf0f"
+  sha256 "41feb2d595a9b87feca301cfcf00dd142cd67fc466c101548f35bc09c57fadbc"
   revision 1
 
-  depends_on :python
   depends_on :fortran
 
   def install
     prefix.install "mfusg.txt", "readme.txt", "release.txt", "problems.txt"
     share.install "test"
     doc.install Dir["doc/*"]
-    Dir.chdir "pymake"
-    system "python", "makebin.py"
-    bin.install "mfusg"
+
+    cd "pymake" do
+      system "python", "makebin.py"
+      bin.install "mfusg"
+    end
   end
 
   test do
