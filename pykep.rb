426dda45eebd3743d510d2f83af20ee34941014c
Ian Lancaster <ilancaster@gmail.com>
2015-04-17 05:09:49 -0400

    pykep 1.2.1

--- pykep.rb
+++ pykep.rb
@@ -1,26 +1,31 @@
-require "formula"
-
 class Pykep < Formula
   homepage "http://esa.github.io/pykep/"
-  url "https://github.com/esa/pykep/archive/1.1.3.tar.gz"
-  sha1 "15603482ff38221e874a59c11d4d1a575ee3801f"
+  url "https://github.com/esa/pykep/archive/1.2.1.tar.gz"
+  sha256 "e297ea58079fe9d766215f20749d32ec57b4edc91ec1e83cd19ca5c212301608"
   head "https://github.com/esa/pykep.git"
-  revision 1
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "boost-python"
-  depends_on :python
+  depends_on :python => :recommended
 
   def install
+    args = std_cmake_args + [
+      "-DBUILD_TESTS=ON",
+      "-DBUILD_PYKEP=ON",
+      "-DPYTHON_MODULES_DIR=#{lib}/python2.7/site-packages",
+    ]
+
     mkdir "build" do
-      system "cmake", "../", "-DBUILD_TESTS=ON", "-DBUILD_PYKEP=ON", *std_cmake_args
+      system "cmake", "..", *args
       system "make"
       system "make", "install"
     end
   end
 
   test do
-    system "python -c 'import PyKEP'"
+    Language::Python.each_python(build) do |python, _version|
+      system python, "-c", "import PyKEP"
+    end
   end
 end
