a6c7b686abe7876dc176c7281341a4a0be0517c7
Ian Lancaster <ilancaster@gmail.com>
2015-04-17 18:17:25 -0400

    moab 4.8.0
    Closes #1235. Closes #2118.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- moab.rb
+++ moab.rb
@@ -1,27 +1,36 @@
-require 'formula'
-
 class Moab < Formula
-  homepage 'https://trac.mcs.anl.gov/projects/ITAPS/wiki/MOAB'
-  url 'https://bitbucket.org/fathomteam/moab/get/4.6.2.tar.gz'
-  sha1 '20494f8f13ea621a7b3fa30d98e0535957b237aa'
-
-  option 'without-check', "Skip build-time checks (not recommended)"
+  homepage "http://press3.mcs.anl.gov/sigma/moab-library/"
+  url "http://ftp.mcs.anl.gov/pub/fathom/moab-4.8.0.tar.gz"
+  sha256 "349e66e06cac38325926eafb01807b9d520bfce73016088d5dd7b973e687467a"
+  head "https://bitbucket.org/fathomteam/moab.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on 'netcdf'
-  depends_on 'hdf5'
-  depends_on :fortran if build.with? 'check'
+  depends_on "netcdf"
+  depends_on "hdf5"
+  depends_on :fortran
 
   def install
-    system "autoreconf", "--force", "--install"
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--libdir=#{libexec}/lib"
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--enable-shared",
+      "--enable-static",
+      "--prefix=#{prefix}",
+      "--with-netcdf=#{Formula["netcdf"].opt_prefix}",
+      "--with-hdf5=#{Formula["hdf5"].opt_prefix}",
+      "--without-cgns",
+    ]
 
-    system "make install"
-    # Moab installs non-lib files in libdir. Link only the libraries.
-    lib.install_symlink Dir["#{libexec}/lib/*.a"]
-    system "make check" if build.with? 'check'
+    system "autoreconf", "-fi"
+    system "./configure", *args
+    system "make", "install"
+    system "make", "check"
+
+    cd lib do
+      # Move non-libraries out of lib
+      prefix.install %w[iMesh-Defs.inc moab.config moab.make MOABConfig.cmake]
+    end
   end
 end
