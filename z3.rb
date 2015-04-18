05d85347c4423bc8516d3651c32fe641ce09205a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-18 21:26:19 +0100

    z3: add 4.3.2 bottle.

--- z3.rb
+++ z3.rb
@@ -4,6 +4,14 @@ class Z3 < Formula
   sha256 "c9ccc7d1e9b9dcc7129eaf1160c1a577726f4e870ba4681ea9c8a4521bfaa0f3"
   head "https://github.com/Z3Prover/z3.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "2ce2e28b1ed47fd2df764fdba8ed61fa6896f6cb4f443afed7dcee6fae69b080" => :yosemite
+    sha256 "cb5f83f5a2b2d263d51ae8df5c0cbf7fdee4d72d58c26a17ea57f19bf2a96ccb" => :mavericks
+    sha256 "9e58713737a98619cc05c0f89fb63ba61ed66c4a98beeea650d6a83c4bdb8beb" => :mountain_lion
+  end
+
   depends_on :python
 
   def install
