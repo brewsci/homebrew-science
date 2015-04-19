f941c38800cb1dec5b05aaef1013a6e7f5c656a2
Alex Dunn <dunn.alex@gmail.com>
2015-04-19 15:36:46 -0700

    dssp 2.2.1
    Closes #2125.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- dssp.rb
+++ dssp.rb
@@ -1,33 +1,40 @@
-require 'formula'
-
 class Dssp < Formula
-  homepage 'http://swift.cmbi.ru.nl/gv/dssp/'
-  url 'ftp://ftp.cmbi.ru.nl/pub/software/dssp/dssp-2.1.0.tgz'
-  sha1 'ac943f49e2bdce73b3523434ec811857e50d82a2'
+  homepage "http://swift.cmbi.ru.nl/gv/dssp/"
+  url "https://mirrors.kernel.org/debian/pool/main/d/dssp/dssp_2.2.1.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dssp/dssp_2.2.1.orig.tar.gz"
+  sha256 "5fb5e7c085de16c05981e3a72869c8b082911a0b46e6dcc6dbd669c9f267e8e1"
 
-  depends_on 'boost'
+  depends_on "boost"
+
+  resource "pdb" do
+    url "ftp://ftp.cmbi.ru.nl/pub/molbio/data/pdb_redo/zz/3zzz/3zzz_0cyc.pdb.gz"
+    sha256 "6ee5ab16972d8f3ae6c2f92fce789a40fecb1a6a8c0de42257b35fc7e9d82149"
+  end
 
   def install
     # Create a make.config file that contains the configuration for boost
     boost = Formula["boost"].opt_prefix
-    File.open('make.config', 'w') do |makeconf|
+    File.open("make.config", "w") do |makeconf|
       makeconf.puts "BOOST_LIB_SUFFIX = -mt"
-      makeconf.puts "BOOST_LIB_DIR = #{boost / 'lib'}"
-      makeconf.puts "BOOST_INC_DIR = #{boost / 'include'}"
+      makeconf.puts "BOOST_LIB_DIR = #{boost}/lib"
+      makeconf.puts "BOOST_INC_DIR = #{boost}/include"
     end
 
     # There is no need for the build to be static and static build causes
     # an error: ld: library not found for -lcrt0.o
-    inreplace 'makefile' do |s|
-      s.gsub! /-static/, ''
+    inreplace "makefile" do |s|
+      s.gsub!(/-static/, "")
     end
 
-    # The makefile ask for g++ as a compiler but that causes a error at link
-    # time: ld: library not found for -lgcc_ext.10.5
-    system "make", "install", "DEST_DIR=#{prefix}", "MAN_DIR=#{man1}", "CXX=c++"
+    system "make", "install", "DEST_DIR=#{prefix}", "MAN_DIR=#{man1}"
   end
 
   test do
-    system 'mkdssp --version'
+    resource("pdb").stage do
+      system bin/"mkdssp", "-i", "3zzz_0cyc.pdb",
+             "-o", testpath/"test.dssp"
+    end
+    assert_match "POLYPYRIMIDINE TRACT BINDING PROTEIN RRM2",
+                 (testpath/"test.dssp").read
   end
 end
