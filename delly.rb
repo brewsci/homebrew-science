d327fd76cbff0b6f1cd7e3895e487764a1812659
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 21:20:36 -0700

    delly 0.6.5
    Closes #2135.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- delly.rb
+++ delly.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Delly < Formula
   homepage "https://github.com/tobiasrausch/delly"
-  url "https://github.com/tobiasrausch/delly/archive/v0.6.1.tar.gz"
-  sha1 "7f01feb98ca8f7f68422762336ef36b4802c5a71"
+  url "https://github.com/tobiasrausch/delly/archive/v0.6.5.tar.gz"
+  sha256 "6977001ef3a3eb5049515a4586640f77d60c4f784f7636c7c5cc456912081283"
+
+  head "https://github.com/tobiasrausch/delly.git"
 
   option "with-binary", "Install a statically linked binary for 64-bit Linux" if OS.linux?
 
@@ -13,33 +13,33 @@ class Delly < Formula
     depends_on "htslib"
   end
 
-  resource "linux-binary-0.6.1" do
-    url "https://github.com/tobiasrausch/delly/releases/download/v0.6.1/delly_v0.6.1_CentOS5.4_x86_64bit"
-    sha1 "5941f6eaa528f5ab7d83e4ffa8c0c90ff01cb674"
+  resource "linux-binary-0.6.5" do
+    url "https://github.com/tobiasrausch/delly/releases/download/v0.6.5/delly_v0.6.5_CentOS5.4_x86_64bit"
+    sha256 "56ccdbd5e21d3570c7e34c82593e9ef5d86509f23f57e3e364bcd3f5a7e6899c"
   end
 
   def install
-    inreplace "Makefile" do |s|
-      s.gsub! "${BAMTOOLS_ROOT}/include", "#{Formula["bamtools"].opt_include}/bamtools"
-      s.gsub! "${BAMTOOLS_ROOT}/lib",     "#{Formula["bamtools"].opt_lib}"
-      s.gsub! "${BOOST_ROOT}",            "#{Formula["boost"].opt_prefix}"
-      s.gsub! "${SEQTK_ROOT}",            "#{Formula["htslib"].opt_include}/htslib"
-      s.gsub! ".htslib .bamtools",        ""
-    end
-
     if build.with? "binary"
-      resource("linux-binary-0.6.1").stage { bin.install "delly_v0.6.1_CentOS5.4_x86_64bit" => "delly" }
-
+      resource("linux-binary-0.6.5").stage do
+        bin.install "delly_v0.6.6_CentOS5.4_x86_64bit" => "delly"
+      end
     else
-      system "make", "CXX=#{ENV.cxx}"
+      inreplace "Makefile", ".htslib .bamtools .boost", ""
+      ENV.append_to_cflags "-I#{Formula["bamtools"].opt_include}/bamtools"
+      ENV.append_to_cflags "-I#{Formula["htslib"].opt_include}/htslib"
+
+      system "make", "BAMTOOLS_ROOT=#{Formula["bamtools"].opt_prefix}",
+                     "SEQTK_ROOT=#{Formula["htslib"].opt_prefix}",
+                     "BOOST_ROOT=#{Formula["boost"].opt_prefix}",
+                     "src/delly"
       bin.install "src/delly"
     end
-
-    share.install %W[python/somaticFilter.py python/populationFilter.py human.hg19.excl.tsv]
+    share.install "test", "variantFiltering"
     doc.install "README.md"
   end
 
   test do
-    system 'delly 2>&1 |grep -q delly'
+    system "delly", "--outfile=#{testpath}/test.vcf", share/"test/DEL.bam"
+    File.exist? testpath/"test.vcf"
   end
 end
