f19a129fff172e8bf8f1d4e16166028745828078
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-24 22:55:28 +0100

    paml: update 4.8a bottle.

--- paml.rb
+++ paml.rb
@@ -7,10 +7,10 @@ class Paml < Formula
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    revision 1
-    sha256 "578a692dadffed72b1845b1e64ba9e1561ff7fcc79c986c4c2e9e9731ad2e69e" => :yosemite
-    sha256 "a56081655e0a7e59e6266c385d1a7ad7183b0223da698c660a64be66627a71f9" => :mavericks
-    sha256 "b50727426a1324fb3e945e19e1453a9587355e4961d74c2ef04f9f18c3470233" => :mountain_lion
+    revision 2
+    sha256 "37becefadba2dcd90bf6f9bdc7c100d5558e795cd3eb8d038c33bdf6bce366f9" => :yosemite
+    sha256 "040cf5f030bf1b27b3ee20d0f2f1118e8bbfb54bdeb005296a0754b08f622ba8" => :mavericks
+    sha256 "6ff2fb523b30c68704442cad420827dcb2035402cfcc36fae80f33347e605946" => :mountain_lion
   end
 
   def install
