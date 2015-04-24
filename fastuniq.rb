0e196445f86c308cb27886f6ac27d3ba7767136b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-24 08:38:05 +0100

    fastuniq: add 1.1 bottle.

--- fastuniq.rb
+++ fastuniq.rb
@@ -6,6 +6,14 @@ class Fastuniq < Formula
   url "https://downloads.sourceforge.net/project/fastuniq/FastUniq-1.1.tar.gz"
   sha256 "9ebf251566d097226393fb5aa9db30a827e60c7a4bd9f6e06022b4af4cee0eae"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "19465fcbe38c09187aed0df6a5ff2aeb640cabee976220b9a6eefb51d632de5e" => :yosemite
+    sha256 "07890a374729fe11b214426e5ab17f5b57b80b735bd5f84611dd37f71de2259f" => :mavericks
+    sha256 "f662ad9cce7dc9251ec813b170c3f3178e7f7998bcecd8399900742b1604d807" => :mountain_lion
+  end
+
   def install
     inreplace "source/fastq_uniq.c", "FSATQ", "FASTQ"  # fix typo
     system "make", "-C", "source"
