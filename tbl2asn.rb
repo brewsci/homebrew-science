052ae105d338343cd1c16a5688fa27b28dbfcd83
なつき <i@ntk.me>
2015-04-24 13:49:13 -0700

    tbl2asn 24.3
    Closes #2153.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -2,13 +2,13 @@ class Tbl2asn < Formula
   homepage "https://www.ncbi.nlm.nih.gov/genbank/tbl2asn2/"
   #tag "bioinformatics"
 
-  version "24.2"
+  version "24.3"
   if OS.mac?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/mac.tbl2asn.gz"
-    sha256 "c8c307b11d4089ad2a368d1ff1a1ce94dc0c40993478a233bc3331ef3f37ff00"
+    sha256 "0fc76b2d35974c9d20a444272ed556a0f094659a3bc4162b16ffbddaceff4ce9"
   elsif OS.linux?
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
-    sha256 "a5fe31cfa37e5de1f7e4d971f294ad2ca2be9868f5591477fcfb2862668d04c7"
+    sha256 "d1ccef96cec34fcfc74f4c70a1d0582f66508a754fa3ae137bb33e1ce310fe37"
   end
 
   bottle do
@@ -21,7 +21,7 @@ class Tbl2asn < Formula
 
   resource "doc" do
     url "ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/tbl2asn.txt"
-    sha256 "f2da249ee8d60b385a8e71b67b480bff9c6d905a9d674e035c1186ce40a4f5c7"
+    sha256 "b5e139c2a22cea4e1b5c7a063e3fb1f311d6b8802f2a8cca1433d7f16f816300"
   end
 
   def install
