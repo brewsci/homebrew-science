52c446b68e3854859dc4f8bcef5aaf7cfb44f266
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-29 05:02:21 +0100

    fastml: add 3.1 bottle.

--- fastml.rb
+++ fastml.rb
@@ -6,6 +6,13 @@ class Fastml < Formula
   url "http://fastml.tau.ac.il/source/FastML.v3.1.tgz"
   sha256 "16c8631a4186f434f81f5b7e8c1147660e79b025b3c93a6db9889c30477bd5f4"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "522293e9f76d618eef20ed52e8b161fe00c1c59d7152bd643ad07811e0e99750" => :yosemite
+    sha256 "9b6fef363a5cf10cea598b57a9d77ae80c773c5b0f6e39d4187895616121557b" => :mavericks
+  end
+
   depends_on :macos => :mavericks   # won't build on [Mountain] Lion
 
   def install
