057d9850d5ffa5c2148cd48fc92106897801a0fb
Tim D. Smith <git@tim-smith.us>
2015-04-28 17:21:49 -0700

    flux-simulator: don't interpolate version

--- flux-simulator.rb
+++ flux-simulator.rb
@@ -1,7 +1,6 @@
 class FluxSimulator < Formula
   homepage "http://sammeth.net/confluence/display/SIM/Home"
-  version "1.2.1"
-  url "http://artifactory.sammeth.net/artifactory/barna/barna/barna.simulator/#{version}/flux-simulator-#{version}.tgz"
+  url "http://artifactory.sammeth.net/artifactory/barna/barna/barna.simulator/1.2.1/flux-simulator-1.2.1.tgz"
   sha256 "9e4da0b4e1c21bddd9f8d0cca67bd6a55de43c08db40833ac2fbb531f3a8cff1"
 
   bottle do
