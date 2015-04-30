e9a7ca3b5afb4aee7f9dcb2aedc7ad52c92c10f2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-30 01:02:54 +0100

    trimadap: add 0.1 bottle.

--- trimadap.rb
+++ trimadap.rb
@@ -5,6 +5,14 @@ class Trimadap < Formula
   url "https://github.com/lh3/trimadap/archive/0.1.tar.gz"
   sha256 "553069d81004b120d9df7d6161edce9317c0f95e5eefe2ec3325dd4081a90acd"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "8d249f2eab9c8ab60062c86863e6d50a200544f5ea04ba680c68d2587812284d" => :yosemite
+    sha256 "f2feda18417156d7ebc03750267caa85a8375605eaa7b0941e7c7b3771379cbb" => :mavericks
+    sha256 "92e6dbda5b7992577f565da669cfc5beb5a0fb14b37fc9b22a8c17279a189bd6" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "trimadap-mt"
