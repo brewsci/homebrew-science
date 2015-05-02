fb46ddf1f3c91d4d33b85df53c103f4f30961abb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-02 12:39:13 +0100

    bact: add 0.13 bottle.

--- bact.rb
+++ bact.rb
@@ -3,6 +3,14 @@ class Bact < Formula
   url "http://chasen.org/~taku/software/bact/bact-0.13.tar.gz"
   sha256 "4aa55c30621afd3793bd15471b3c0d77d3b489051cbfb30a9f0640de8928ab40"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "8de8fd7cda5405a06d39eacd2615ee3629aeb87952f54539d795e273b1acdfe8" => :yosemite
+    sha256 "c778234069cb91598fcf7589cdfd8349bfbc09006087d4400491d1261a1c89b8" => :mavericks
+    sha256 "70944f64bdf141a1f084e612e0e816785f89e07d8de5b1a1bc2d3d1c1b0b6708" => :mountain_lion
+  end
+
   def install
     system "make"
     system "make", "test"
