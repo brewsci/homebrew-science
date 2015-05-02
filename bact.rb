35a6a207adb66fed98ab625e4d2c24aa217a711b
Torsten Seemann <torsten.seemann@gmail.com>
2015-05-02 21:02:32 +1000

    Bact 0.13 (sha256 + missing docs + missing share)
    Closes #2190.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- bact.rb
+++ bact.rb
@@ -1,13 +1,13 @@
-require 'formula'
-
 class Bact < Formula
-  homepage 'http://chasen.org/~taku/software/bact/'
-  url 'http://chasen.org/~taku/software/bact/bact-0.13.tar.gz'
-  sha1 'cdc8815e9258868699d98449598058011e993218'
+  homepage "http://chasen.org/~taku/software/bact/"
+  url "http://chasen.org/~taku/software/bact/bact-0.13.tar.gz"
+  sha256 "4aa55c30621afd3793bd15471b3c0d77d3b489051cbfb30a9f0640de8928ab40"
 
   def install
     system "make"
-    system "make test"
-    bin.install "bact_learn", "bact_classify","bact_mkmodel"
+    system "make", "test"
+    bin.install "bact_learn", "bact_classify", "bact_mkmodel"
+    doc.install "README", "AUTHORS", "COPYING", "index.html", "bact.css"
+    (share/"bact").install Dir["jp*"], Dir["med.*"]
   end
 end
