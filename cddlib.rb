ac542fc17bab0b8e7ebd95ad72ae1135c06b3b2e
Torsten Seemann <torsten.seemann@gmail.com>
2015-05-02 21:08:26 +1000

    cddlib (modernization, sha256, etc)
    Closes #2191.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- cddlib.rb
+++ cddlib.rb
@@ -1,19 +1,16 @@
-require 'formula'
-
 class Cddlib < Formula
-  homepage 'http://www.inf.ethz.ch/personal/fukudak/cdd_home/'
-  url 'ftp://ftp.ifor.math.ethz.ch/pub/fukuda/cdd/cddlib-094g.tar.gz'
-  mirror 'http://www.mirrorservice.org/sites/distfiles.finkmirrors.net/cddlib-094g.tar.gz'
-  sha1 '4ef38ca6efbf3d54a7a753c63ff25a8c95e5fc5f'
+  homepage "http://www.inf.ethz.ch/personal/fukudak/cdd_home/"
+  url "ftp://ftp.ifor.math.ethz.ch/pub/fukuda/cdd/cddlib-094g.tar.gz"
+  mirror "http://www.mirrorservice.org/sites/distfiles.finkmirrors.net/cddlib-094g.tar.gz"
+  sha256 "af1b81226514abf731800e2e104def8a7274817d7e645693f8b99fc2b1432155"
 
-  depends_on 'gmp'
+  depends_on "gmp"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
-    doc.install Dir['doc/*']
-    share.install Dir['examples*']
+    doc.install Dir["doc/*"]
+    (share/"cddlib").install Dir["examples*"]
   end
-
 end
