6a6225643426a97362d6e4ecc26cfc8dd26a1cbd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-05 05:15:05 +0100

    blat: update 35 bottle.

--- blat.rb
+++ blat.rb
@@ -7,11 +7,12 @@ class Blat < Formula
   sha1 "a2cae7407e512166bf7b1ed300db9be6649693bd"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    sha1 "13852547459831466ac80ce6a39416c9381f32f7" => :yosemite
-    sha1 "eb935f7f25e3bc7db280fa509e7fe0e1ced5b8dd" => :mavericks
-    sha1 "910c75d17c0b5af2adca972825433f8ed0397ce9" => :mountain_lion
+    revision 1
+    sha256 "7e1ae73487db28bca75585482ccf50f206468f714365730af8dae7cc1c892ef2" => :yosemite
+    sha256 "fc541ee6ebb3f895b2e9700431325687c9a95bf2ab55a0d6fcafa95110d30c29" => :mavericks
+    sha256 "c21f79a061ee6d0594f1f35b743e99e84c6d1a604f34c738ea32370a1234864b" => :mountain_lion
   end
 
   depends_on "libpng" => :build
