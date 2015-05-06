b146acbd2d9da029f62783d11240b0161850b50e
Ian Lancaster <ilancaster@gmail.com>
2015-05-06 01:59:44 -0400

    nextflow: remove bottle block
    Take two.

--- nextflow.rb
+++ nextflow.rb
@@ -6,14 +6,6 @@ class Nextflow < Formula
   sha256 "64eae8d4095dd004840afd94a4b30012b26630808adbc78aebf7445061c5271e"
   head "https://github.com/nextflow-io/nextflow.git"
 
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-science"
-    cellar :any
-    sha256 "4dd7fdfdd9b55482243382a6305a262d71b25a94a5f3546af1cf056c5d63db26" => :yosemite
-    sha256 "ee764c45097a25b8d1897f9b3f9c2a23b7f96ce0ff4698607f9847fe4735eaf0" => :mavericks
-    sha256 "68af288c973dd2578de1cded63dd616ec329e442ab85abc5826306fca90f499b" => :mountain_lion
-  end
-
   depends_on :java => "1.7+"
 
   def install
