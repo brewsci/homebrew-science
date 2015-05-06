ea554e2a6e93bd9b3fd4e78bf7f67f9dfba33d17
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-06 19:35:21 +0100

    fermikit: add 0.12 bottle.

--- fermikit.rb
+++ fermikit.rb
@@ -6,6 +6,14 @@ class Fermikit < Formula
   url "https://github.com/lh3/fermikit/releases/download/v0.12/fermikit-0.12.tar.bz2"
   sha256 "7d0f3d5ff6e790defc32b1f529dcd33b0c6f6a6d7223b8866542e7cebecfc125"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "7cfc4ed60eec5fa75cd2af4d09c1a31080949ba8dda2fd85750109a82fcac60a" => :yosemite
+    sha256 "a61fc63afe190b41ea0f5ac38705c44b9b6f33f990177bceb638e1eb7ea5b911" => :mavericks
+    sha256 "b0f62232855027e6adccf5fafd87855616784d564ebe100560292ff40a15377a" => :mountain_lion
+  end
+
   depends_on "bfc"
   depends_on "bwa"
   depends_on "fermi2"
