b49dcfbce86142c251c2500520e39bf8f9bfdc71
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-06 19:30:45 +0100

    fermi2: add 0.1 bottle.

--- fermi2.rb
+++ fermi2.rb
@@ -4,6 +4,14 @@ class Fermi2 < Formula
   url "https://github.com/lh3/fermi2/archive/0.1.tar.gz"
   sha256 "1308ce95505270b1aac8ab4d115d19809eebd3ba0e2ee18a66c90c4c78dd2366"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "ca79efffdf75b8943314d713357fccef22c2d66da853cf91457fff66430e78ac" => :yosemite
+    sha256 "e3c3944c79d7f857ac5a8a67ec778d4e199ef85b4d18b56314d84eecb0404645" => :mavericks
+    sha256 "5037d2613a91c6662e9b5d405a012fb898da42edc857c38f2661d4b3b9c334de" => :mountain_lion
+  end
+
   depends_on "ropebwt2"
 
   def install
