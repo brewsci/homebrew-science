7c8c16a08ac36970af7c29911b8305261f234e92
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-06 06:34:09 +0100

    proverif: add 1.89 bottle.

--- proverif.rb
+++ proverif.rb
@@ -3,6 +3,14 @@ class Proverif < Formula
   url "http://prosecco.gforge.inria.fr/personal/bblanche/proverif/proverif1.89.tar.gz"
   sha256 "985a2b1a82e358c32b71b707ef915ca8204c3e3584504fb2188025410ce30660"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "90d02aabbc2df37bf61f6486c030e2d91968eccd6c76e7db55e58ac4c0eeffcc" => :yosemite
+    sha256 "4bcf183ee2bd750cd732d54c250cf4e978707aba28b01bb05d5194999eee9f62" => :mavericks
+    sha256 "3765f3715e03aee2ffe1eb8c7f17c4efc865af62455271762c6c337a5deba134" => :mountain_lion
+  end
+
   depends_on "objective-caml"
 
   def install
