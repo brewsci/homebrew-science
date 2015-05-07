93aee46eb5cd54917998b484e7a1fbff7cf874db
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-07 20:13:54 +0100

    igv: add 2.3.52 bottle.

--- igv.rb
+++ igv.rb
@@ -6,6 +6,14 @@ class Igv < Formula
   url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.52.zip"
   sha256 "b0bef5e92372c80e2d42f58e2e75d5fd736cc4fcb27caf13a85b8f09180bbb41"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "858a5ea4634a473f0ce4c786eab14762403a7aa08208d098397f2603e7d59b12" => :yosemite
+    sha256 "3c8aff4b35fd9b2b5fa748ca0588d4c7237cd632706edbc69648c8e1c2966a60" => :mavericks
+    sha256 "90d71592d24bb5893d374ddd018474e93b55c6c691bbab2bf91659723de8ca41" => :mountain_lion
+  end
+
   depends_on :java
 
   def install
