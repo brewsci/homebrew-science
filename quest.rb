7718233be8d26ee3e877394d8bef671a3cde517a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-08 06:51:32 +0100

    quest: add 2.4 bottle.

--- quest.rb
+++ quest.rb
@@ -5,6 +5,14 @@ class Quest < Formula
   url "http://www-hsc.usc.edu/~valouev/QuEST/QuEST_2.4.tar.gz"
   sha256 "4a1e75798192938b2bc709140181b16aa83e7add60471735b4d84e2c23679ac7"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "d6a7bdafb4229d4bca2837884c9ee50a073f7bfce181b0a34db94cb3c3455885" => :yosemite
+    sha256 "68dca3f3bcb940b146768e18579dfbd7969ed3a6626f0e19bdf3fb1257620b53" => :mavericks
+    sha256 "533ff62d0823dfcb74bbc3aaa6de47c61f65401585a36c3660a86d91f4eabfe4" => :mountain_lion
+  end
+
   def install
     ENV.deparallelize
     system "./configure.pl"
