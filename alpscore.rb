fbd19326e7cc20ad6ea6aaf03556ea2d1193d0ec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-09 22:57:09 +0100

    alpscore: add 0.4.5 bottle.

--- alpscore.rb
+++ alpscore.rb
@@ -1,29 +1,33 @@
 class Alpscore < Formula
   homepage "http://alpscore.org"
-  sha256 "b043f5043f6fdca5efd8e1fc2ba0d893da0fd04bff8adaa213c797b44d68e72e"
-
   url "https://github.com/ALPSCore/ALPSCore/archive/v0.4.5.tar.gz"
-
+  sha256 "b043f5043f6fdca5efd8e1fc2ba0d893da0fd04bff8adaa213c797b44d68e72e"
   head "https://github.com/ALPSCore/ALPSCore.git"
 
-  option "with-static", "Build static instead of shared libraries"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "dff1ed0ffee80e639a15257abb4b6618a33f219d4420c356d2422908730b5133" => :yosemite
+    sha256 "7868af8f9808fefb7d3a2a4b04aafe59dd1550a0dcd71879443607bc0284e660" => :mavericks
+    sha256 "cdb246670465d0780a554d8e0bf51b70ddccea35387cc8d5bb1efc90cf5a09d5" => :mountain_lion
+  end
+
   option :cxx11
-  option "with-doc",    "Build documentation"
   option "with-check",  "Build and run shipped tests"
+  option "with-doc",    "Build documentation"
+  option "with-static", "Build static instead of shared libraries"
 
   depends_on "cmake"   => :build
   depends_on :mpi => [:cc, :cxx, :recommended]
-  # boost - check mpi and c++11
+
   boost_options = []
   boost_options += ["with-mpi", "without-single"] if build.with? "mpi"
   boost_options << "c++11" if build.cxx11?
   depends_on "boost" => boost_options
-  # hdf5 - check c++11
+
   depends_on "hdf5" => ((build.cxx11?) ? ["c++11"] : [])
 
   def install
     args = std_cmake_args
-    # force release mode (taken from eigen formula)
     args.delete "-DCMAKE_BUILD_TYPE=None"
     args << "-DCMAKE_BUILD_TYPE=Release"
 
@@ -66,17 +70,13 @@ class Alpscore < Formula
         p["myparam"] = 1.0;
       }
     EOS
-    args_compile = ["test.cpp", "-lalps-accumulators", "-lalps-hdf5", "-lalps-utilities", "-lalps-params"]
-    args_compile << "-lboost_filesystem-mt" << "-lboost_system-mt" << "-lboost_program_options-mt"
-    if build.with? "mpi"
-      args_compile << "-lboost_mpi-mt"
-    end
+    args_compile = ["test.cpp",
+                    "-lalps-accumulators", "-lalps-hdf5", "-lalps-utilities", "-lalps-params",
+                    "-lboost_filesystem-mt", "-lboost_system-mt", "-lboost_program_options-mt"
+                   ]
+    args_compile << "-lboost_mpi-mt" if build.with? "mpi"
     args_compile << "-o" << "test"
-    if build.with? "mpi"
-      system "mpicxx", *args_compile
-    else
-      system ENV.cxx, *args_compile
-    end
+    system ((build.with? "mpi") ? "mpicxx" : ENV.cxx), *args_compile
     system "./test"
   end
 end
