66768a53e33c43ae8d4c2dfba6f7030fd6fc6366
Dominique Orban <dominique.orban@gmail.com>
2015-05-10 12:22:12 -0400

    ASL: version 2.0.1.

--- asl.rb
+++ asl.rb
@@ -1,7 +1,7 @@
 class Asl < Formula
   homepage "http://www.ampl.com"
-  url "https://github.com/ampl/mp/archive/1.3.0.tar.gz"
-  sha1 "0581cdbfbeeb54a0f2ebac65ba39aa3ccb36a926"
+  url "https://github.com/ampl/mp/archive/2.0.1.tar.gz"
+  sha256 "628b5ef035b58184113e721b05b9340c8bce77d22f9261eaa2448ec96d97dd8a"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -16,13 +16,15 @@ class Asl < Formula
   depends_on "cmake" => :build
   depends_on "doxygen" => :optional
 
-  # https://github.com/ampl/mp/issues/24
-  # https://github.com/ampl/mp/issues/27
-  patch :DATA
+  # https://github.com/ampl/mp/issues/55
+  patch do
+    url "https://github.com/ampl/mp/commit/8a777497b9ccac035a5d59cb12e3d9a3ba815256.diff"
+    sha256 "5da9fa46e1509bce744933166891da12c895b8ffd2e6705008377bdb81259b22"
+  end
 
   resource "miniampl" do
     url "https://github.com/dpo/miniampl/archive/v1.0.tar.gz"
-    sha1 "4518ee9a9895b0782169085126ee149d05ba66a7"
+    sha256 "b836dbf1208426f4bd93d6d79d632c6f5619054279ac33453825e036a915c675"
   end
 
   def install
@@ -71,53 +73,3 @@ class Asl < Formula
     system "#{bin}/miniampl", "#{share}/asl/example/examples/wb", "showname=1", "showgrad=1"
   end
 end
-
-__END__
-diff --git a/test/os-test.cc b/test/os-test.cc
-index 52c4f5c..d6bb6c4 100644
---- a/test/os-test.cc
-+++ b/test/os-test.cc
-@@ -296,9 +296,9 @@ TEST(MemoryMappedFileTest, CloseFile) {
-     // Running lsof with filename failed, so check the full lsof output instead.
-     ExecuteShellCommand("lsof > out", false);
-     std::string line;
--    std::ifstream out("out");
-+    std::ifstream ifs("out");
-     bool found = false;
--    while (std::getline(out, line)) {
-+    while (std::getline(ifs, line)) {
-       if (line.find(path) != std::string::npos) {
-         if (line.find(MEM) != string::npos)
-           found = true;
-
-diff --git a/src/asl/CMakeLists.txt b/src/asl/CMakeLists.txt
-index f8cc9c6..c4dd762 100644
---- a/src/asl/CMakeLists.txt
-+++ b/src/asl/CMakeLists.txt
-@@ -281,6 +281,7 @@ if (MATLAB_FOUND)
-       COMPILE_FLAGS -I${CMAKE_CURRENT_BINARY_DIR}
-                     -I${solvers_dir} ${MP_MEX_OPTIONS}
-       LIBRARIES ${matlab_asl})
-+    install(FILES $<TARGET_PROPERTY:${name},FILENAME> DESTINATION bin)
-   endforeach ()
- endif ()
-
-diff --git a/support/cmake/FindMATLAB.cmake b/support/cmake/FindMATLAB.cmake
-index 7c6bb76..cf27f5c 100644
---- a/support/cmake/FindMATLAB.cmake
-+++ b/support/cmake/FindMATLAB.cmake
-@@ -7,7 +7,7 @@
-
- if (APPLE)
-   set(MATLAB_GLOB "/Applications/MATLAB*")
--  set(MATLAB_MEX_SUFFIX mac)
-+  set(MATLAB_MEX_SUFFIX maci64)
- elseif (UNIX)
-   set(MATLAB_GLOB "/opt/MATLAB/*")
-   set(MATLAB_MEX_SUFFIX a64)
-@@ -51,4 +51,5 @@ function (add_mex name)
-       ${sources} ${libs} -output ${filename}
-     DEPENDS ${sources} ${add_mex_LIBRARIES})
-   add_custom_target(${name} ALL SOURCES ${filename})
-+  set_target_properties(${name} PROPERTIES FILENAME ${filename})
- endfunction ()
