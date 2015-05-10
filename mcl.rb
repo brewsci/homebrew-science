5c03bd1f413053f8bc85e78426627fc1b820f73d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-10 12:01:40 +0100

    mcl: add 14-137 bottle.

--- mcl.rb
+++ mcl.rb
@@ -4,6 +4,14 @@ class Mcl < Formula
   version "14-137"
   sha256 "b5786897a8a8ca119eb355a5630806a4da72ea84243dba85b19a86f14757b497"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "ee751daa79605617936d8ec30f456d46d13729dbf3a43238b746683f721145c2" => :yosemite
+    sha256 "e8e0fcb0da6f1177a00d4c11674824df03944c0896d1e752f513fdd94f74fa17" => :mavericks
+    sha256 "0eb411a4a383fdfee14fefa90588a5d3cad38930a94866a4683bb22e219d6db6" => :mountain_lion
+  end
+
   def install
     bin.mkpath
     system "./configure", "--disable-dependency-tracking",
