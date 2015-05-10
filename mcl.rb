e5cf191bf3eea9ca42a95e82a79d87bd76c4090e
Torsten Seemann <torsten.seemann@gmail.com>
2015-05-10 20:51:14 +1000

    MCL: fix hard-coded /usr/local/bin/perl, add SHA256
    Closes #2233.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- mcl.rb
+++ mcl.rb
@@ -1,16 +1,16 @@
-require "formula"
-
 class Mcl < Formula
   homepage "http://micans.org/mcl"
   url "http://micans.org/mcl/src/mcl-14-137.tar.gz"
   version "14-137"
-  sha1 "9016240ecdc39d26e493c1e18967e46ff0742bdd"
+  sha256 "b5786897a8a8ca119eb355a5630806a4da72ea84243dba85b19a86f14757b497"
 
   def install
     bin.mkpath
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-blast"
-    system "make install"
+    system "make", "install"
+    inreplace bin/"mcxdeblast", "/usr/local/bin/perl -w", "/usr/bin/env perl\nuse warnings;"
+    inreplace bin/"clxdo", "/usr/local/bin/perl", "perl"
   end
 end
