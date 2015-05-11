dae3b4d2716407885fa09dde0e20aa629224dae3
Rene Warren <rwarren@bcgsc.ca>
2015-05-11 15:17:22 -0700

    links-scaffolder 1.5
    Closes #2239.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -3,18 +3,21 @@ class LinksScaffolder < Formula
   # doi "10.1101/016519"
   # tag "bioinformatics"
 
-  url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.1/links_v1-1.tar.gz"
-  version "1.1"
-  sha256 "c27f2e0a2ae21f51dd0a0a141bb65a8a200d75482fa94724310a764b202b7227"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.5/links_v1-5.tar.gz"
+  version "1.5"
+  sha256 "20b42024fa9da153512167c1ec4ebe75bfbf1118642e1ac964f40cabc400df04"
+
+  depends_on "Bloom::Faster" => :perl
 
   def install
     bin.install "LINKS"
     chmod 0644, "LINKS-readme.txt"
     doc.install "LINKS-readme.txt"
     prefix.install "test"
+    prefix.install "tools"
   end
 
   test do
-    assert_match "LINKS", shell_output("#{bin}/LINKS", 255)
+    assert_match "Usage", shell_output("#{bin}/LINKS", 255)
   end
 end
