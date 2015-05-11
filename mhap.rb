2ba55849609b07a447a81308195c8eabc94b56cf
Shaun Jackman <sjackman@gmail.com>
2015-05-11 15:15:37 -0700

    mhap 0.1: Update formula

--- mhap.rb
+++ mhap.rb
@@ -1,21 +1,20 @@
-require "formula"
-
 class Mhap < Formula
   homepage "https://github.com/marbl/MHAP"
+  # doi "10.1101/008003"
+  # tag "bioinformatics"
+
+  url "https://github.com/marbl/MHAP/releases/download/v0.1/mhap-0.1.tar.gz"
+  sha1 "c98096803c12d9d5e5281f0bf7809d909bccf185"
+
   head "https://github.com/marbl/MHAP.git"
+
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
     sha1 "effd7e5aa3d11957c679f2049f93849d5b6d9d8f" => :yosemite
     sha1 "fb2198b8e91a6d7ba99ec84ecda4e4cf7329a91c" => :mavericks
     sha1 "af6b3d2dc212fc2e4a3ba0c4a81d1a0f7903593b" => :mountain_lion
   end
-
-  #doi "10.1101/008003" => "bioRxiv"
-  #tag "bioinformatics"
-
-  url "https://github.com/marbl/MHAP/releases/download/v0.1/mhap-0.1.tar.gz"
-  sha1 "c98096803c12d9d5e5281f0bf7809d909bccf185"
 
   def install
     prefix.install "mhap-0.1.jar", Dir["lib/*"]
@@ -23,6 +22,6 @@ class Mhap < Formula
   end
 
   test do
-    system "#{bin}/mhap 2>&1 |grep MHAP"
+    assert_match "Usage", shell_output("#{bin}/mhap 2>&1", 1)
   end
 end
