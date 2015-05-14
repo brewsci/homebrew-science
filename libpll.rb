0a39a6a12f6d94c8d2fa934b0032a5b04042083a
Kevin Gori <kgori@ebi.ac.uk>
2015-05-14 16:05:24 +0100

    libpll v1.0.2
    Closes #2259.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- libpll.rb
+++ libpll.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Libpll < Formula
   homepage "http://www.libpll.org/"
-  url "http://www.libpll.org/Downloads/libpll-1.0.0.tar.gz"
-  sha1 "023e0e704c57c463c34f85b31d597391972af810"
+  url "http://www.libpll.org/Downloads/libpll-1.0.2.tar.gz"
+  sha256 "c6112e95b2b34af0cc72208840281860b048588794c31f1f46c15ba5a0655e3d"
   head "https://git.assembla.com/phylogenetic-likelihood-library.git"
 
   depends_on "libtool" => :build
@@ -12,46 +10,12 @@ class Libpll < Formula
 
   option "without-check", "Disable build-time checking (not recommended)"
 
-  # Don't include malloc.h (linux requirement)
-  stable do
-    patch do
-      url "https://gist.githubusercontent.com/kgori/2e0d4075207fdbeebe7b/raw/217db2debf9e8eccca55e363f243b8f710bee6b6/mem_alloc_patch"
-      sha1 "8885119361e6b7bcccefb0840cd2a6c3263419ba"
-    end
-  end
-
-  # Get automake to work properly over subdirectories
-  patch :DATA
-
   def install
-    system "glibtoolize"
-    system "autoheader"
-    system "aclocal", "-I", "m4"
-    system "automake", "--add-missing"
-    system "autoreconf"
+    system "autoreconf", "-fvi"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    cd "src" do
-      system "make"
-      system "make", "check" if build.with? "check"
-      system "make", "install"
-    end
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 end
-
-__END__
-
-diff --git a/configure.ac b/configure.ac
-index bb9f535..fba1f9c 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -4,7 +4,7 @@
- AC_PREREQ([2.68])
- AC_INIT([libpll], [1.0.0], [Tomas.Flouri@h-its.org])
- AC_CONFIG_MACRO_DIR([m4])
--AM_INIT_AUTOMAKE
-+AM_INIT_AUTOMAKE([subdir-objects])
-
-
- # AM_MAINTAINER_MODE
-
