168def6f13e9a435f508d3266933cdee01f0405b
Alex Dunn <dunn.alex@gmail.com>
2015-05-19 12:28:34 -0700

    sfscode 20140719
    Closes #2281.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- sfscode.rb
+++ sfscode.rb
@@ -1,16 +1,16 @@
-require 'formula'
-
 class Sfscode < Formula
-  homepage 'http://sfscode.sourceforge.net/SFS_CODE/index/index.html'
-  url 'https://downloads.sourceforge.net/project/sfscode/sfscode/sfscode_20130725/sfscode_20130725.tgz'
-  sha1 '08480e69fb274fc06c929fb20b82ee1739526631'
+  desc "Population genetics simulator"
+  homepage "http://sfscode.sourceforge.net/SFS_CODE/index/index.html"
+  url "https://downloads.sourceforge.net/project/sfscode/sfscode/sfscode_20140719/sfscode_20140719.tgz"
+  sha256 "1f69ed96e20f1453df2882ce78171b49cb3378b25538a3cacd18db00e07c979a"
 
   def install
-    system "make sfs_code convertSFS_CODE"
-    bin.install "convertSFS_CODE", "sfs_code"
+    ENV.j1
+    system "make", "CC=#{ENV.cc}", "EXEDIR=#{bin}"
+    doc.install "doc/SFS_CODE_doc.pdf"
   end
 
   test do
-    system "#{bin}/sfs_code 2>&1 | grep -q sfs && #{bin}/convertSFS_CODE 2>&1 | grep -q SFS"
+    system bin/"sfs_code", "1", "1"
   end
 end
