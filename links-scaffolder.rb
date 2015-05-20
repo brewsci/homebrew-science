2abfa402791a81017efe1dfacb1c002499c5802b
Rene Warren <rwarren@bcgsc.ca>
2015-05-20 14:02:20 -0700

    links-scaffolder 1.5: Add PDF documentation
    Update SHA256.
    
    Closes #2289.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -5,7 +5,7 @@ class LinksScaffolder < Formula
 
   url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.5/links_v1-5.tar.gz"
   version "1.5"
-  sha256 "20b42024fa9da153512167c1ec4ebe75bfbf1118642e1ac964f40cabc400df04"
+  sha256 "b06f7d4c4c9de4e6c723d54fd2a2786f236a295166d3d36b9a166ecab36c5161"
 
   depends_on "Bloom::Faster" => :perl
 
@@ -13,6 +13,8 @@ class LinksScaffolder < Formula
     bin.install "LINKS"
     chmod 0644, "LINKS-readme.txt"
     doc.install "LINKS-readme.txt"
+    chmod 0644, "LINKS-readme.pdf"
+    doc.install "LINKS-readme.pdf"
     prefix.install "test"
     prefix.install "tools"
   end
