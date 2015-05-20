f63f140131967942457dea618feb5b002663dd6d
Denis Davydov <davydden@gmail.com>
2015-05-20 11:04:49 +0200

    dealii 8.3 (update and fix)
    Update to 8.3.
    Added missing dependencies.
    
    Closes #2285.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- dealii.rb
+++ dealii.rb
@@ -1,7 +1,8 @@
 class Dealii < Formula
+  desc "open source finite element library"
   homepage "http://www.dealii.org"
-  url "https://github.com/dealii/dealii/releases/download/v8.2.1/dealii-8.2.1.tar.gz"
-  sha256 "d75674e45fe63cd9fa294460fe45228904d51a68f744dbb99cd7b60720f3b2a0"
+  url "https://github.com/dealii/dealii/releases/download/v8.3.0/dealii-8.3.0.tar.gz"
+  sha256 "4ddf72632eb501e1c814e299f32fc04fd680d6fda9daff58be4209e400e41779"
 
   bottle do
     sha256 "ca9f1bbe60c229cfcbd34beda08ff279802939d9fbaedf7cb33eead2987c77a1" => :yosemite
@@ -24,28 +25,36 @@ class Dealii < Formula
 
   depends_on "arpack"       => [:recommended] + mpidep + openblasdep
   depends_on "boost"        => :recommended
+  #-depends_on "doxygen"      => :optional # installation error: CMake Error at doc/doxygen/cmake_install.cmake:31 (file)
   depends_on "hdf5"         => [:recommended] + mpidep
   depends_on "metis"        => :recommended
-  depends_on "mumps"        => [:recommended] + openblasdep
-  depends_on "muparser"     => :recommended
+  depends_on "muparser"     => :recommended if MacOS.version != :mountain_lion # Undefined symbols for architecture x86_64
   depends_on "netcdf"       => [:recommended, "with-fortran", "with-cxx-compat"]
   depends_on "opencascade"  => :recommended
-  depends_on "petsc"        => [:recommended] + openblasdep
   depends_on "p4est"        => [:recommended] + openblasdep if build.with? "mpi"
+  depends_on "parmetis"     => :recommended if build.with? "mpi"
+  depends_on "petsc"        => [:recommended] + openblasdep
   depends_on "slepc"        => :recommended
-  # TODO: We won't have bottles if we make Trilinos :recommended because the bot times out.
-  depends_on "trilinos"     => [:optional] + openblasdep
+  depends_on "suite-sparse" => [:recommended] + openblasdep
+  depends_on "tbb"          => :recommended
+  depends_on "trilinos"     => [:recommended] + openblasdep
 
+  needs :cxx11
   def install
+    ENV.cxx11
     args = %W[
       -DCMAKE_BUILD_TYPE=DebugRelease
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DCMAKE_FIND_FRAMEWORK=LAST
       -Wno-dev
       -DDEAL_II_COMPONENT_COMPAT_FILES=OFF
+      -DDEAL_II_COMPONENT_EXAMPLES=ON
+      -DDEAL_II_COMPONENT_MESH_CONVERTER=ON
     ]
     # constrain Cmake to look for libraries in homebrew's prefix
     args << "-DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}"
+
+    args << "-DDEAL_II_COMPONENT_DOCUMENTATION=ON" if build.with? "doxygen"
 
     if build.with? "openblas"
       ext = OS.mac? ? "dyld" : "so"
@@ -61,15 +70,18 @@ class Dealii < Formula
       args << "-DCMAKE_Fortran_COMPILER=mpif90"
     end
 
-    args << "-DHDF5_DIR=#{Formula["hdf5"].opt_prefix}" if build.with? "hdf5"
-    args << "-DMUMPS_DIR=#{Formula["mumps"].opt_prefix}" if build.with? "mumps"
-    args << "-DMETIS_DIR=#{Formula["metis"].opt_prefix}" if build.with? "metis"
     args << "-DARPACK_DIR=#{Formula["arpack"].opt_prefix}" if build.with? "arpack"
-    args << "-DP4EST_DIR=#{Formula["p4est"].opt_prefix}" if build.with? "p4est"
-    args << "-DOPENCASCADE_DIR=#{Formula["opencascade"].opt_prefix}" if build.with? "opencascade"
+    args << "-DBOOST_DIR=#{Formula["boost"].opt_prefix}" if build.with? "boost"
+    args << "-DHDF5_DIR=#{Formula["hdf5"].opt_prefix}" if build.with? "hdf5"
+    args << "-DMETIS_DIR=#{Formula["metis"].opt_prefix}" if build.with? "metis"
+    args << "-DMUPARSER_DIR=#{Formula["muparser"].opt_prefix}" if build.with? "muparser"
     args << "-DNETCDF_DIR=#{Formula["netcdf"].opt_prefix}" if build.with? "netcdf"
+    args << "-DOPENCASCADE_DIR=#{Formula["opencascade"].opt_prefix}" if build.with? "opencascade"
+    args << "-DP4EST_DIR=#{Formula["p4est"].opt_prefix}" if build.with? "p4est"
     args << "-DPETSC_DIR=#{Formula["petsc"].opt_prefix}" if build.with? "petsc"
     args << "-DSLEPC_DIR=#{Formula["slepc"].opt_prefix}" if build.with? "slepc"
+    args << "-DUMFPACK_DIR=#{Formula["suite-sparse"].opt_prefix}" if build.with? "suite-sparse"
+    args << "-DTBB_DIR=#{Formula["tbb"].opt_prefix}" if build.with? "tbb"
     args << "-DTRILINOS_DIR=#{Formula["trilinos"].opt_prefix}" if build.with? "trilinos"
 
     mkdir "build" do
