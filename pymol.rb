16f75e703a2c332ec300aa7d14dadca9c6534586
Ian Lancaster <ilancaster@gmail.com>
2015-05-20 00:09:50 -0400

    pymol 1.7.6.0
    Fixes #2273.
    Closes #2283.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- pymol.rb
+++ pymol.rb
@@ -1,9 +1,8 @@
 class Pymol < Formula
   homepage "http://pymol.org"
-  url "https://downloads.sourceforge.net/project/pymol/pymol/1.7/pymol-v1.7.4.0.tar.bz2"
-  sha1 "8e091a0ed6a3d7389f10021a77b0d217b6c36331"
+  url "https://downloads.sourceforge.net/project/pymol/pymol/1.7/pymol-v1.7.6.0.tar.bz2"
+  sha256 "cf142732b5206ea65ebcc0cc162f9d3c88ecacc701ac41d1a5e9b38972700395"
   head "https://svn.code.sf.net/p/pymol/code/trunk/pymol"
-  revision 1
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
@@ -13,41 +12,23 @@ class Pymol < Formula
     sha256 "d94bb41550f2b57786cc4b563b13ec48073f98c925c3baf38f2b9b02fa4267e4" => :mountain_lion
   end
 
-  depends_on "homebrew/dupes/tcl-tk" => ["with-threads", "with-x11"]
   depends_on "glew"
   depends_on "python" => "with-tcl-tk"
-  depends_on "freetype"
-  depends_on "libpng"
+  depends_on "homebrew/dupes/tcl-tk" => ["with-threads", "with-x11"]
   depends_on :x11
 
-  resource "pmw" do
-    url "https://downloads.sourceforge.net/project/pmw/Pmw/Pmw.1.3.3/Pmw.1.3.3.tar.gz"
-    sha1 "0ff7f03245640da4f37a97167967de8d09e4c6a6"
-  end
-
-  # This patch disables the vmd plugin. VMD is not something we can depend on for now.
-  # The plugin is set to always install as of revision 4019.
+  # Fix outdated X11 path: https://sourceforge.net/p/pymol/patches/10/
   patch :DATA
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    resource("pmw").stage do
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
-    end
+    ENV.append_to_cflags "-Qunused-arguments" if MacOS.version < :mavericks
 
-    # PyMol uses ./ext as a backup to look for ./ext/include and ./ext/lib
-    ln_s HOMEBREW_PREFIX, "./ext"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", "-s", "setup.py", "install",
+                     "--bundled-pmw",
+                     "--install-scripts=#{libexec}/bin",
+                     "--install-lib=#{libexec}/lib/python2.7/site-packages"
 
-    args = [
-      "--verbose",
-      "--install-scripts=#{libexec}/bin",
-      "--install-lib=#{libexec}/lib/python2.7/site-packages",
-    ]
-
-    system "python", "-s", "setup.py", "install", *args
     bin.install libexec/"bin/pymol"
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
@@ -55,34 +36,25 @@ class Pymol < Formula
   end
 
   def caveats; <<-EOS.undent
-    In order to get the most out of PyMOL, you will want the external
-    GUI. This requires a thread enabled Tk installation and Python
-    linked to it. Install these with the following commands:
-      brew tap homebrew/dupes
-      brew install homebrew/dupes/tcl-tk --enable-threads --with-x11
-      brew install python --with-brewed-tk
-
     On some Macs, the graphics drivers do not properly support stereo
     graphics. This will cause visual glitches and shaking that stay
     visible until X11 is completely closed. This may even require
     restarting your computer. Launch explicitly in Mono mode using:
       pymol -M
-
     EOS
   end
 end
-
 __END__
-diff --git a/setup.py b/setup.py
-index 8fd9bc1..5684ae3 100644
---- a/setup.py
-+++ b/setup.py
-@@ -197,7 +197,7 @@ ext_link_args = []
- data_files = []
- ext_modules = []
+diff --git a/modules/pymol/__init__.py b/modules/pymol/__init__.py
+index 792d273..3d335fd 100644
+--- a/modules/pymol/__init__.py
++++ b/modules/pymol/__init__.py
+@@ -391,7 +391,7 @@ def prime_pymol():
 
--if True:
-+if False:
-     # VMD plugin support
-     pymol_src_dirs += [
-         'contrib/uiuc/plugins/include',
+     # legacy X11 launching on OSX
+     if sys.platform == 'darwin' and invocation.options.external_gui == 1:
+-        xdpyinfo = "/usr/X11R6/bin/xdpyinfo"
++        xdpyinfo = "/opt/X11/bin/xdpyinfo"
+         if not os.path.exists(xdpyinfo) or \
+                 os.system(xdpyinfo + " >/dev/null 2>&1"):
+             # launch X11 (if needed)
