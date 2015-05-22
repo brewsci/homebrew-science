cd65bcea9a9de61a59c87c04b4276c8d653b45b0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-22 01:40:51 +0100

    fgsl: update 1.0.0_1 bottle.

--- fgsl.rb
+++ fgsl.rb
@@ -5,10 +5,10 @@ class Fgsl < Formula
   revision 1
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "be4fa1734a0f0dc960f5856767c3d4cd1ee4df45" => :yosemite
-    sha1 "e2923d74d33b2dac361d861411882c80700625b1" => :mavericks
-    sha1 "820bb9eeb5a3c783dedf2522cdcba89b31ec2c9d" => :mountain_lion
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "5056b6160e68259055c51680934e4f0039728300f4dcd6224d8f09a0f73c520d" => :yosemite
+    sha256 "57a6c6e3a4337d520e1ff588a0e2c2c8b5df80c32260bd443019b709a660bfa7" => :mavericks
+    sha256 "c17e3285cf8099aec32622baa70dcbb66e27c840641738cd8b0d0f7a134c1603" => :mountain_lion
   end
 
   depends_on :fortran
