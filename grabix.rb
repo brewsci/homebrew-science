1d57217cdd8356dc8151cd5a0ccd2e2d294c5b99
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-22 06:14:05 +0100

    grabix: update 0.1.6 bottle.

--- grabix.rb
+++ grabix.rb
@@ -8,9 +8,9 @@ class Grabix < Formula
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    sha256 "47903048212bd2cbe4ef80745860bee4aa719d249626251e5f41ab943c5ff6d0" => :yosemite
-    sha256 "298640d3efa6b8cc3f24f9bdd1025330b4f397d48d6694b37829acef381f841b" => :mavericks
-    sha256 "b5f9657e9017f8bb29988184da0fbdd0bdb8a7a9462722ff12e2b80ca9ae936c" => :mountain_lion
+    sha256 "136a6e742f0b3fbfff7577c90747556cd5848ecbefb2665f883e87be1e757bb6" => :yosemite
+    sha256 "fc98a1091a2983421a8dcd76db93197c7f8abeb14c766af0c1a8097537a5ac2f" => :mavericks
+    sha256 "3e53adfbd78ca26832fc597f910a1be9590176f6a67228592064495bde14205c" => :mountain_lion
   end
 
   def install
