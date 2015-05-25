b93062224b96bed3e5502eea975ca2038b4d61dd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-25 20:03:51 +0100

    mitofy: add 20120322 bottle.

--- mitofy.rb
+++ mitofy.rb
@@ -7,6 +7,14 @@ class Mitofy < Formula
   sha256 "29e73b0f0a09e698209809081cc0de1ef0ee7e3cf9ae873b01504911025bb244"
   version "20120322"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "3194d6f95b55e17e7e2099583d2590df400531c82d17333ca94b1f892ea0e74b" => :yosemite
+    sha256 "5d8abbd913f8881787cba13c0b22f5fe27c6e3104a1432896edfe075f866fa37" => :mavericks
+    sha256 "a8641e8824c35651d7062aa3e403aafeaf038ca62011f8fa93d9849446178942" => :mountain_lion
+  end
+
   # Includes vendored dependencies of BLAST 2.2.25 and tRNAscan-SE 1.4 compiled for Mac OS.
 
   def install
