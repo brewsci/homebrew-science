e8d43be6c5a5bafbcb02eff17a6ab9d8cde6e89a
Shaun Jackman <sjackman@gmail.com>
2015-05-26 13:23:05 -0700

    cap3 2015-02-11: Add DOI and tag bioinformatics
    Closes #2316.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- cap3.rb
+++ cap3.rb
@@ -1,9 +1,13 @@
 class Cap3 < Formula
   homepage "http://seq.cs.iastate.edu/cap3.html"
-  if OS.mac? then
+  desc "CAP3: A DNA Sequence Assembly Program"
+  # doi "10.1101/gr.9.9.868"
+  # tag "bioinformatics"
+
+  if OS.mac?
     url "http://seq.cs.iastate.edu/CAP3/cap3.macosx.intel64.tar"
     sha256 "4b6e8fa6b39147b23ada6add080854ea9fadace9a9c8870a97ac79ff1c75338e"
-  elsif OS.linux? then
+  elsif OS.linux?
     url "http://seq.cs.iastate.edu/CAP3/cap3.linux.x86_64.tar"
     sha256 "3aff30423e052887925b32f31bdd76764406661f2be3750afbf46341c3d38a06"
   end
@@ -15,6 +19,6 @@ class Cap3 < Formula
   end
 
   test do
-    system "cap3 2>&1 |grep -q cap3"
+    assert_match "Usage", shell_output("#{bin}/cap3 2>&1", 1)
   end
 end
