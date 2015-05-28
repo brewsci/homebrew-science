da61f63cdf8302438e5a6f86019ddb4ae3501626
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-28 07:18:24 +0100

    bedtools: update 2.24.0 bottle.

--- bedtools.rb
+++ bedtools.rb
@@ -12,9 +12,9 @@ class Bedtools < Formula
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    sha1 "9d63ea92161b5144b234e9247f28574e2c24797d" => :yosemite
-    sha1 "b7caff266b94c391b8fba0c8aeee7486a101d148" => :mavericks
-    sha1 "d003c70d8175fd80de939cc65e87b8c37456bc23" => :mountain_lion
+    sha256 "5f1fcc24f01e23dafb14a492b276f749f54be2c64bf1570eb4410d170043f649" => :yosemite
+    sha256 "ecae0fef48e1a28abb1acec96408cd4c3236e52da188daaa9f2eaf8368526b44" => :mavericks
+    sha256 "6a9e646386fc5eef3f3d5fff8cb5ad13f6c4bf3d377e9fa393f088b024bc0c1f" => :mountain_lion
   end
 
   def install
