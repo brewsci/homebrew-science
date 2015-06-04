c7a4bb9c94d7158f89a703106bd31c12065241ff
Shaun Jackman <sjackman@gmail.com>
2015-06-04 13:08:56 -0700

    bamtools 2.3.0: Add tag bioinformatics
    Closes #2355.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bamtools.rb
+++ bamtools.rb
@@ -1,24 +1,26 @@
-require 'formula'
-
 class Bamtools < Formula
-  homepage 'https://github.com/pezmaster31/bamtools'
-  url 'https://github.com/pezmaster31/bamtools/archive/v2.3.0.tar.gz'
-  sha1 '397d595b373ccc11503856d7e2804833aa8ea811'
-  head 'https://github.com/pezmaster31/bamtools.git'
+  desc "BamTools: API and command-line toolkit for BAM data"
+  homepage "https://github.com/pezmaster31/bamtools"
+  # tag "bioinformatics"
 
-  depends_on 'cmake' => :build
+  url "https://github.com/pezmaster31/bamtools/archive/v2.3.0.tar.gz"
+  sha256 "288046e6d5d41afdc5fce8608c5641cf2b8e670644587c1315b90bbe92f039af"
+
+  head "https://github.com/pezmaster31/bamtools.git"
+
+  depends_on "cmake" => :build
 
   patch do
     # Install libbamtools in /usr/local/lib.
     # https://github.com/pezmaster31/bamtools/pull/82
     url "https://github.com/sjackman/bamtools/commit/3b6b89d.diff"
-    sha1 "89f659243dac265684705b485b5580e7cac559f7"
+    sha256 "de9438e932884a3f941a683984bed32b2eaca5c5483642c5a2253074f4a559ff"
   end
 
   def install
-    mkdir 'default' do
+    mkdir "build" do
       system "cmake", "..", *std_cmake_args
-      system "make install"
+      system "make", "install"
     end
   end
 
