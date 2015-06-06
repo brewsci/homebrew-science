633676a893731261015c62ee1c341e6cd635c9fa
Tim D. Smith <git@tim-smith.us>
2015-06-06 00:19:04 -0700

    cantera: allow bottling
    * bundle cython
    * remove unit tests that require data from source tree from test
    * remove caveats since they aren't Homebrew-specific
    
    Closes #2367.

--- cantera.rb
+++ cantera.rb
@@ -1,28 +1,29 @@
-require 'formula'
-
 class Cantera < Formula
-  homepage 'http://github.com/Cantera/cantera'
-  head 'https://github.com/cantera/cantera.git', :branch => 'master'
-
-  stable do
-    url "https://github.com/Cantera/cantera/releases/download/v2.2.0/cantera-2.2.0.tar.gz"
-    sha1 "b6e4226f27075ffb8686a24661be79d6d3ff9888"
-  end
+  homepage "https://github.com/Cantera/cantera"
+  url "https://github.com/Cantera/cantera/releases/download/v2.2.0/cantera-2.2.0.tar.gz"
+  sha256 "306c218500eaabdf1e920601348d2b3acc1fb66b02eea842d98b3fbb41ebbc78"
+  head "https://github.com/cantera/cantera.git"
 
   option "with-matlab=", "Path to Matlab root directory"
   option "without-check", "Disable build-time checking (not recommended)"
 
-  depends_on :python if MacOS.version <= :snow_leopard
   depends_on "scons" => :build
+  depends_on :python if OS.mac? && MacOS.version <= :snow_leopard
   depends_on "numpy" => :python
-  depends_on "cython" => :python
   depends_on "sundials" => :recommended
-  depends_on :python3 => :optional
   depends_on "graphviz" => :optional
+  depends_on :python3 => :optional
+
+  resource "Cython" do
+    url "https://pypi.python.org/packages/source/C/Cython/cython-0.22.tar.gz"
+    sha256 "14307e7a69af9a0d0e0024d446af7e51cc0e3e4d0dfb10d36ba837e5e5844015"
+  end
 
   def install
-    # Make sure to use Homebrew Python to do CTI to CTML conversions
-    inreplace "src/base/ct2ctml.cpp", 's = "python";', 's = "/usr/local/bin/python";'
+    ENV.prepend_create_path "PYTHONPATH", buildpath/"cython/lib/python2.7/site-packages"
+    resource("Cython").stage do
+      system "python", *Language::Python.setup_install_args(buildpath/"cython")
+    end
 
     build_args = ["prefix=#{prefix}",
                   "python_package=full",
@@ -32,36 +33,23 @@ class Cantera < Formula
 
     matlab_path = ARGV.value("with-matlab")
     build_args << "matlab_path=" + matlab_path if matlab_path
-    build_args << "python3_package=y" if build.with? :python3
+    build_args << "python3_package=" + (build.with?("python3") ? "y" : "n")
 
     scons "build", *build_args
     scons "test" if build.with? "check"
     scons "install"
-    prefix.install Dir["License.*"]
   end
 
   test do
-    # Run those portions of the test suite that do not depend of data
-    # that's only available in the source tree.
-    system("python", "-m", "unittest", "-v",
-           "cantera.test.test_thermo",
-           "cantera.test.test_kinetics",
-           "cantera.test.test_transport",
-           "cantera.test.test_purefluid",
-           "cantera.test.test_mixture")
-  end
-
-  def caveats; <<-EOS.undent
-    The license, demos, tutorials, data, etc. can be found in:
-      #{opt_prefix}
-
-    Try the following in python to find the equilibrium composition of a
-    stoichiometric methane/air mixture at 1000 K and 1 atm:
-    >>> import cantera as ct
-    >>> g = ct.Solution('gri30.cti')
-    >>> g.TPX = 1000, ct.one_atm, 'CH4:1, O2:2, N2:8'
-    >>> g.equilibrate('TP')
-    >>> g()
-    EOS
+    pythons = ["python"]
+    pythons << "python3" if build.with? "python3"
+    pythons.each do |python|
+      # Run those portions of the test suite that do not depend of data
+      # that's only available in the source tree.
+      system(python, "-m", "unittest", "-v",
+             "cantera.test.test_transport",
+             "cantera.test.test_purefluid",
+             "cantera.test.test_mixture")
+    end
   end
 end
