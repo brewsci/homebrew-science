fb89f9f81d47f58118780614840cac01c7ce0d17
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-06-08 17:01:47 +0200

    nextflow 0.14.1
    Closes #2383.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- nextflow.rb
+++ nextflow.rb
@@ -1,9 +1,9 @@
 class Nextflow < Formula
   homepage "http://www.nextflow.io/"
   # tag "bioinformatics"
-  version "0.14.0"
-  url "http://www.nextflow.io/releases/v0.14.0/nextflow"
-  sha256 "26f50c8f459068f91a625ea71c64d5168e798279b4a56060734422e04102e1f0"
+  version "0.14.1"
+  url "http://www.nextflow.io/releases/v0.14.1/nextflow"
+  sha256 "08d1166ab989f7933375063389b1620e9916d6f5bc0d4a3354529236da4032c0"
   head "https://github.com/nextflow-io/nextflow.git"
 
   depends_on :java => "1.7+"
