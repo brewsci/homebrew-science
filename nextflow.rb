81cf11de0ba07c6f56e56875d54bfc296c0a823e
Shaun Jackman <sjackman@gmail.com>
2015-06-12 11:22:10 -0700

    nextflow: Add desc and doi

--- nextflow.rb
+++ nextflow.rb
@@ -1,9 +1,13 @@
 class Nextflow < Formula
+  desc "Data-driven computational pipelines"
   homepage "http://www.nextflow.io/"
+  # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
+
   version "0.14.1"
   url "http://www.nextflow.io/releases/v0.14.1/nextflow"
   sha256 "08d1166ab989f7933375063389b1620e9916d6f5bc0d4a3354529236da4032c0"
+
   head "https://github.com/nextflow-io/nextflow.git"
 
   depends_on :java => "1.7+"
