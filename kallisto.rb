eaabced8565b508a673e04a410d160e5471e6dc0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-15 04:06:15 +0100

    kallisto: update 0.42.2 bottle.

--- kallisto.rb
+++ kallisto.rb
@@ -9,9 +9,10 @@ class Kallisto < Formula
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
-    sha256 "4dfccb2a52d19960f8dbc9f804ac3b822dc5b8b18d7cb07793320115738f4b2f" => :yosemite
-    sha256 "b5bc2849f6c9ec3fc1f44b3360db671cd7ba883a5ceae71c31c6c7bc9df15375" => :mavericks
-    sha256 "7bfb1b855d4c1ddd5e4aa8e0538766a7c77ba2052c74ef2dcd7af87706a78763" => :mountain_lion
+    revision 1
+    sha256 "c8b7c6a9684f79cff90ab95d0ab4a1f11517f983e08b2ff8eb01a8c446dce6df" => :yosemite
+    sha256 "df50619cee612d023a1ec8c37b6083d4c20b03a21be3c4257610bf15a4a7f3b0" => :mavericks
+    sha256 "2ddbf8882262dae72a63b99f9b21826cc1b4d21b0473ca70f885f5c505e69e28" => :mountain_lion
   end
 
   needs :cxx11
