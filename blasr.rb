5ed6104ff30c14c44e500fba8532bada8f8c70d4
Shaun Jackman <sjackman@gmail.com>
2015-06-17 23:31:45 -0700

    blasr 2.1: Add desc and tag "bioinformatics"
    Closes #2415.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- blasr.rb
+++ blasr.rb
@@ -1,16 +1,18 @@
-require 'formula'
-
 class Blasr < Formula
-  homepage 'https://github.com/PacificBiosciences/blasr'
-  #doi '10.1186/1471-2105-13-238'
-  url 'https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.1.tar.gz'
-  sha1 'ac6add1ba8a82cac2515da36c0ec53060c20ea0f'
-  head 'https://github.com/PacificBiosciences/blasr.git'
+  desc "PacBio long read aligner"
+  homepage "https://github.com/PacificBiosciences/blasr"
+  # doi "10.1186/1471-2105-13-238"
+  # tag "bioinformatics"
+
+  url "https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.1.tar.gz"
+  sha256 "12b20c05fcfdd166d654971402ba872dc23cb948e46e5cd40a432fb6c1b814f1"
+
+  head "https://github.com/PacificBiosciences/blasr.git"
 
   depends_on "hdf5"
 
   fails_with :clang do
-    build 503
+    build 602
     cause <<-EOS.undent
       error: destructor type 'HDFWriteBuffer<int>' in object
       destruction expression does not match the type
@@ -32,10 +34,10 @@ class Blasr < Formula
     system "make", "STATIC=",
       "HDF5INCLUDEDIR=#{hdf5.opt_include}",
       "HDF5LIBDIR=#{hdf5.opt_lib}"
-    system 'make', 'install', 'PREFIX=' + prefix
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system 'blasr'
+    system "#{bin}/blasr"
   end
 end
