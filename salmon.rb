dcdc4de39571ef61960b534dcab14015f7448bf6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-19 00:58:51 +0100

    salmon: update 0.4.2 bottle.

--- salmon.rb
+++ salmon.rb
@@ -9,9 +9,10 @@ class Salmon < Formula
   head "https://github.com/COMBINE-lab/salmon.git"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
-    sha1 "52e52d191915334cc33f7613fdcce3e0b4d71d1f" => :yosemite
-    sha1 "a04b4b5eadde9c0e93c1fc6253ea8af6f3d1cb6a" => :mavericks
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "b71a13cc8ef5a0be45c94645279b95703156cfd571485501c807aa994c9410e1" => :yosemite
+    sha256 "02bf82b66b2cf967aa014203606e2292abd77e41ceaadeaff48273572152643d" => :mavericks
+    sha256 "e94f189aaf588c89f9d371323f9b92b47cf54e3cccbdb8d8afabd81bdeca022b" => :mountain_lion
   end
 
   # See https://github.com/kingsfordgroup/sailfish/issues/74
