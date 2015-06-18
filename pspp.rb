8dc8cfdad66513c53bc88bdf0f80fbb91f45760c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 15:19:54 +0100

    pspp: add 0.8.4_1 bottle.

--- pspp.rb
+++ pspp.rb
@@ -5,6 +5,13 @@ class Pspp < Formula
   sha256 "bfcc3b0e98a5e2d44b4f3383e52c1a26b7eacf5bf419786f49fa16d00bc1c52c"
   revision 1
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "de81367499dae0031cbab3a3421dc2817f884a1e7ba92a69aa6707e00ef9d63e" => :yosemite
+    sha256 "78e70c08f4f537fafd7c0f297926bb17776c89acc3b1318125b94e9398090db4" => :mavericks
+    sha256 "a66ac66e4dbf4426ef2ecb849b9f02462db0f0715a5726dae0111adb01303c31" => :mountain_lion
+  end
+
   option "without-check", "Skip running the PSPP test suite"
   option "without-gui", "Build without gui support"
 
