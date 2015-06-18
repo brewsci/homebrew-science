ba6c6027a9ab8da4bea2e9ec41192199dde126c1
Shaun Jackman <sjackman@gmail.com>
2015-06-18 10:51:50 -0700

    blasr 2.2
    Closes #2421.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- blasr.rb
+++ blasr.rb
@@ -4,8 +4,8 @@ class Blasr < Formula
   # doi "10.1186/1471-2105-13-238"
   # tag "bioinformatics"
 
-  url "https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.1.tar.gz"
-  sha256 "12b20c05fcfdd166d654971402ba872dc23cb948e46e5cd40a432fb6c1b814f1"
+  url "https://github.com/PacificBiosciences/blasr/archive/smrtanalysis-2.2.tar.gz"
+  sha256 "bef789accf2662aed409b31227e029c61582d5dfe6a289043db4c1b27fd7ab12"
 
   head "https://github.com/PacificBiosciences/blasr.git"
 
@@ -34,6 +34,10 @@ class Blasr < Formula
     system "make", "STATIC=",
       "HDF5INCLUDEDIR=#{hdf5.opt_include}",
       "HDF5LIBDIR=#{hdf5.opt_lib}"
+
+    # Fix the error: install: mkdir /usr/local/Cellar/blasr/2.2/bin: exists
+    ENV.deparallelize
+
     system "make", "install", "PREFIX=#{prefix}"
   end
 
