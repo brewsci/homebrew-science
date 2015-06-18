17ecb5fde25f0330e384f487803234fb3f5c0ed8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 23:42:48 +0100

    wfdb: add 10.5.24 bottle.

--- wfdb.rb
+++ wfdb.rb
@@ -5,6 +5,13 @@ class Wfdb < Formula
   sha256 "be3be34cd1c2c0eaaae56a9987de13f49a3c53bf1539ce7db58f885dc6e34b7b"
   head "https://github.com/bemoody/wfdb.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "90a6785716be809f80a3d269b0d849b7b047a5b6979b15c82a843f09e980bbf8" => :yosemite
+    sha256 "1bb573712810085ef699519126707d1236b92cdada363a4966f63bbf318d94cb" => :mavericks
+    sha256 "0b8a7209b37e2c64f265cdc8bd7708d70997a3304b0098eadb479063ff719324" => :mountain_lion
+  end
+
   def install
     ENV.deparallelize
 
