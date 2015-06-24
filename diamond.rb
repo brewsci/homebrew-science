7038db14cb8b082cceb26aa15a237b3fe0cc94a5
Simon Ye <sye737@gmail.com>
2015-06-24 16:46:07 -0400

    diamond: No longer needs openmp.
    Closes #2443.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- diamond.rb
+++ diamond.rb
@@ -1,13 +1,11 @@
 class Diamond < Formula
+  desc "Accelerated BLAST compatible local sequence aligner"
   homepage "http://ab.inf.uni-tuebingen.de/software/diamond/"
   # doi "10.1038/nmeth.3176"
   # tag "bioinformatics"
 
   url "https://github.com/bbuchfink/diamond/archive/v0.7.9.tar.gz"
   sha256 "25dc43e41768f7a41c98b8b1dcf5aa2c51c0eaf62e71bff22ad01c97b663d341"
-
-  # Fix fatal error: 'omp.h' file not found
-  needs :openmp
 
   depends_on "boost"
 
