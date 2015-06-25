dbd6b3f6b5c45cc742fea19156a9cc23436b5f47
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 11:32:13 +0100

    ace-corrector: add 20150501 bottle.

--- ace-corrector.rb
+++ ace-corrector.rb
@@ -1,6 +1,14 @@
 class AceCorrector < Formula
   desc "correct substitution errors in Illumina reads"
   homepage "https://github.com/Sheikhizadeh/ACE"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "b2c892395c18bcc13554e31a6ed081a452ca200248969624e88e6dcd81c3fc18" => :yosemite
+    sha256 "fc42d8edbc95c265d8a07d66768a769877fb1f14ebafd2e9217cbcb1b4230ee7" => :mavericks
+    sha256 "4d3ecdea8c3226b4c9bb2e438fa3020f0e5e3836d47b17d6111fa8096a894681" => :mountain_lion
+  end
+
   # doi "10.1093/bioinformatics/btv332"
   # tag "bioinformatics"
 
