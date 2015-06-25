1d7e603dc432c2cb8d90c3406c41b4c96db316ef
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 11:27:16 +0100

    andi: add 0.9.2 bottle.

--- andi.rb
+++ andi.rb
@@ -1,6 +1,14 @@
 class Andi < Formula
   desc "Estimate evolutionary distance between similar genomes"
   homepage "https://github.com/EvolBioInf/andi"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "caa3c95977c845f8e3635d342a6c07b43e1e26505b68ab1d817aed7b7f664f31" => :yosemite
+    sha256 "b1cedba1d1807db3811f25e06f6942e51aa86a1d11c8645892a9a43bd86d4203" => :mavericks
+    sha256 "75953fc9b966ef6fc4efab3c2c47fa30d096a91617788f9431a58d6d97b35f13" => :mountain_lion
+  end
+
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btu815"
 
