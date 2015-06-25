a0158237fdf46d0f6eb8cb4f50d6ba297bea3a20
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 08:46:44 +0100

    bcalm: add 1 bottle.

--- bcalm.rb
+++ bcalm.rb
@@ -1,6 +1,14 @@
 class Bcalm < Formula
   desc "de Bruijn CompAction in Low Memory"
   homepage "https://github.com/Malfoy/bcalm"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "9643c4c2b921dd21cfbedd2225ae4f106e637578299fcfeb0bf98156228f4a46" => :yosemite
+    sha256 "28f50d8f30f129a665e4f5064cc09b191a9d32b3f4373b07def55d859c922861" => :mavericks
+    sha256 "bec10ae52ca743085f8129bf5524c204ee1014062bfa126b0dac108760d69cba" => :mountain_lion
+  end
+
   # tag "bioinformatics"
 
   url "https://github.com/Malfoy/bcalm/archive/1.tar.gz"
