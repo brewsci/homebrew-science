cb38f6d49058bcb619c7aed42f9fd00b19b4e6b0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 06:15:07 +0100

    diamond: add 0.7.9 bottle.

--- diamond.rb
+++ diamond.rb
@@ -1,6 +1,13 @@
 class Diamond < Formula
   desc "Accelerated BLAST compatible local sequence aligner"
   homepage "http://ab.inf.uni-tuebingen.de/software/diamond/"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    sha256 "e0eb3edc6f875a6b8e37b2d369b27510ec714faf2e94ff414edb94fbd34a1141" => :yosemite
+    sha256 "da0750e96465902fbd7827dc2220c2cb298f71ae488d6175885eb2044e2066ad" => :mavericks
+    sha256 "459c5a98274de600b7a270e51a589c21bae1add6384fe76d4a4b5c3d988778cb" => :mountain_lion
+  end
+
   # doi "10.1038/nmeth.3176"
   # tag "bioinformatics"
 
