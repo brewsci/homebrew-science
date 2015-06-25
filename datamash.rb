9d2574f4c48d4eef3a5caacda8ae8a6ddc397608
vim <saketkc@gmail.com>
2015-06-24 22:26:33 -0700

    datamash 1.0.7
    Closes #2447.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- datamash.rb
+++ datamash.rb
@@ -1,14 +1,8 @@
 class Datamash < Formula
+  desc "Tool to perform numerical, textual & statistical operations"
   homepage "https://www.gnu.org/software/datamash"
-  url "http://ftpmirror.gnu.org/datamash/datamash-1.0.6.tar.gz"
-  sha256 "0154c25c45b5506b6d618ca8e18d0ef093dac47946ac0df464fb21e77b504118"
-
-  head do
-    url "git://git.savannah.gnu.org/datamash.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gettext" => :build
-  end
+  url "http://ftpmirror.gnu.org/datamash/datamash-1.0.7.tar.gz"
+  sha256 "1a0b300611a5dff89e08e20773252b00f5e2c2d65b2ad789872fc7df94fa8978"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
@@ -16,6 +10,13 @@ class Datamash < Formula
     sha256 "507abcb8fc1ddd8f3d15347355e9ca0069c34e4eedc40dcafda5f10c930c3278" => :yosemite
     sha256 "f6beb68ca96513fb83bbce4d20f8303ee9cccde90b83f2ede375d7aee240940d" => :mavericks
     sha256 "ddc8d16975d24b36a6853c011436feafb99822254829cad79cc5ca00a17771cd" => :mountain_lion
+  end
+
+  head do
+    url "git://git.savannah.gnu.org/datamash.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
   end
 
   def install
