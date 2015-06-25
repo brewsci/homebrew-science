1eeef4f4ceb7e69e4b795e95b61f4fe9b9521dfe
Torsten Seemann <torsten@seemann.id.au>
2015-06-25 15:41:15 +1000

    Fix audit failure

--- diamond.rb
+++ diamond.rb
@@ -1,18 +1,18 @@
 class Diamond < Formula
   desc "Accelerated BLAST compatible local sequence aligner"
   homepage "http://ab.inf.uni-tuebingen.de/software/diamond/"
+  # doi "10.1038/nmeth.3176"
+  # tag "bioinformatics"
+
+  url "https://github.com/bbuchfink/diamond/archive/v0.7.9.tar.gz"
+  sha256 "25dc43e41768f7a41c98b8b1dcf5aa2c51c0eaf62e71bff22ad01c97b663d341"
+
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     sha256 "e0eb3edc6f875a6b8e37b2d369b27510ec714faf2e94ff414edb94fbd34a1141" => :yosemite
     sha256 "da0750e96465902fbd7827dc2220c2cb298f71ae488d6175885eb2044e2066ad" => :mavericks
     sha256 "459c5a98274de600b7a270e51a589c21bae1add6384fe76d4a4b5c3d988778cb" => :mountain_lion
   end
-
-  # doi "10.1038/nmeth.3176"
-  # tag "bioinformatics"
-
-  url "https://github.com/bbuchfink/diamond/archive/v0.7.9.tar.gz"
-  sha256 "25dc43e41768f7a41c98b8b1dcf5aa2c51c0eaf62e71bff22ad01c97b663d341"
 
   depends_on "boost"
 
@@ -26,7 +26,6 @@ class Diamond < Formula
     bin.install "bin/diamond"
     doc.install "README.rst"
   end
-
 
   test do
     assert_match "gapextend", shell_output("diamond -h 2>&1", 0)
