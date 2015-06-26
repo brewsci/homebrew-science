88c4d2712f7d428ef5780565e6281c88a18c9ed3
Torsten Seemann <torsten@seemann.id.au>
2015-06-26 20:13:25 +1000

    andi: fix bottle block placement

--- andi.rb
+++ andi.rb
@@ -1,6 +1,12 @@
 class Andi < Formula
   desc "Estimate evolutionary distance between similar genomes"
   homepage "https://github.com/EvolBioInf/andi"
+  # tag "bioinformatics"
+  # doi "10.1093/bioinformatics/btu815"
+
+  url "https://github.com/EvolBioInf/andi/releases/download/v0.9.2/andi-0.9.2.tar.gz"
+  sha256 "3bb5a114995c50d800d9d7c4cd984f259b18f785a6627f81eceee47481a4f1d3"
+
   bottle do
     root_url "https://homebrew.bintray.com/bottles-science"
     cellar :any
@@ -8,12 +14,6 @@ class Andi < Formula
     sha256 "b1cedba1d1807db3811f25e06f6942e51aa86a1d11c8645892a9a43bd86d4203" => :mavericks
     sha256 "75953fc9b966ef6fc4efab3c2c47fa30d096a91617788f9431a58d6d97b35f13" => :mountain_lion
   end
-
-  # tag "bioinformatics"
-  # doi "10.1093/bioinformatics/btu815"
-
-  url "https://github.com/EvolBioInf/andi/releases/download/v0.9.2/andi-0.9.2.tar.gz"
-  sha256 "3bb5a114995c50d800d9d7c4cd984f259b18f785a6627f81eceee47481a4f1d3"
 
   depends_on "libdivsufsort"
 
