4675ce8a80070b093f56e071ab9f47360c0463c8
Torsten Seemann <torsten@seemann.id.au>
2015-06-26 14:33:51 +1000

    Fix formula order

--- nextflow.rb
+++ nextflow.rb
@@ -1,14 +1,6 @@
 class Nextflow < Formula
   desc "Data-driven computational pipelines"
   homepage "http://www.nextflow.io/"
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-science"
-    cellar :any
-    sha256 "a63c9eeb236de4d38bf14b3c5ae84d054c16335c9afeee85274eee914c1c7956" => :yosemite
-    sha256 "0233a03b52b2e2cef07922ad4a3166b0895ac193d48904e9f95662d5ffd1185d" => :mavericks
-    sha256 "f6a838b641fac81bb55df883d01f41985af753ac1a6ba1ddb916732a1c72b2c9" => :mountain_lion
-  end
-
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
@@ -17,6 +9,14 @@ class Nextflow < Formula
   sha256 "26b7f29c3682173900290b664a8e3e9e0cdb98553317345842e524bd033c2a3d"
 
   head "https://github.com/nextflow-io/nextflow.git"
+
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "a63c9eeb236de4d38bf14b3c5ae84d054c16335c9afeee85274eee914c1c7956" => :yosemite
+    sha256 "0233a03b52b2e2cef07922ad4a3166b0895ac193d48904e9f95662d5ffd1185d" => :mavericks
+    sha256 "f6a838b641fac81bb55df883d01f41985af753ac1a6ba1ddb916732a1c72b2c9" => :mountain_lion
+  end
 
   depends_on :java => "1.7+"
 
