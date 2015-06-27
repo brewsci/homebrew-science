bb360bf8b50db9ac20faaefe3e9536a875061479
Dominique Orban <dominique.orban@gmail.com>
2015-06-27 12:51:55 +0200

    parmetis: add desc and install test graphs in share/parmetis.
    See https://github.com/Homebrew/homebrew-science/issues/2450
    
    Closes #2465.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- parmetis.rb
+++ parmetis.rb
@@ -1,5 +1,6 @@
 class Parmetis < Formula
   homepage "http://glaros.dtc.umn.edu/gkhome/metis/parmetis/overview"
+  desc "MPI-based library for graph/mesh partitioning and computing fill-reducing orderings"
   url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz"
   sha1 "e0df69b037dd43569d4e40076401498ee5aba264"
 
@@ -27,11 +28,11 @@ class Parmetis < Formula
 
     system "make", "config", "prefix=#{prefix}", "shared=1"
     system "make", "install"
-    share.install "Graphs" # Sample data for test
+    (share/"parmetis").install "Graphs" # Sample data for test
   end
 
   test do
-    system "mpirun", "-np", "4", "#{bin}/ptest", "#{share}/Graphs/rotor.graph"
+    system "mpirun", "-np", "4", "#{bin}/ptest", "#{share}/parmetis/Graphs/rotor.graph"
     ohai "Test results are in ~/Library/Logs/Homebrew/parmetis."
   end
 end
