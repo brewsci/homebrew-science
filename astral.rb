bccd01619a16b9c58f0c80c15dd27e32671892d5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-29 01:46:56 +0100

    astral: add 4.7.8 bottle.

--- astral.rb
+++ astral.rb
@@ -1,6 +1,14 @@
 class Astral < Formula
   desc "Estimate species tree given set of unrooted gene trees"
   homepage "https://github.com/smirarab/ASTRAL"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "fd942a7f95f5af65cf37b664711b0c163709b344c600e9b54549fe3ed34aab8d" => :yosemite
+    sha256 "fd942a7f95f5af65cf37b664711b0c163709b344c600e9b54549fe3ed34aab8d" => :mavericks
+    sha256 "9abe785b08c07bfbb746cd5f3a78c2f3f4b6071fb0698f4e80cc4ae30e94a879" => :mountain_lion
+  end
+
   # doi "10.1093/bioinformatics/btu462"
   # tag "bioinformatics"
 
