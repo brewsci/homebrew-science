3891193c0748e554e3b02628bd80a3585fc7a289
kylebgorman <kylebgorman@gmail.com>
2015-07-02 18:29:50 -0400

    Updates openfst formula to v 1.5.0
    Closes #2479.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- openfst.rb
+++ openfst.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Openfst < Formula
   homepage "http://www.openfst.org/"
-  url "http://openfst.cs.nyu.edu/twiki/pub/FST/FstDownload/openfst-1.4.1.tar.gz"
-  sha1 "2e5ff58c7c70e681bced49206bd81748eeb7106d"
+  url "http://openfst.org/twiki/pub/FST/FstDownload/openfst-1.5.0.tar.gz"
+  sha256 "01c2b810295a942fede5b711bd04bdc9677855c846fedcc999c792604e02177b"
 
   needs :cxx11
 
@@ -11,8 +11,37 @@ class Openfst < Formula
     ENV.cxx11
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
+                          "--enable-compact-fsts",
+                          "--enable-compress",
+                          "--enable-const-fsts",
                           "--enable-far",
+                          "--enable-linear-fsts",
+                          "--enable-lookahead-fsts",
+                          "--enable-mpdt",
+                          "--enable-ngram-fsts",
                           "--enable-pdt"
     system "make install"
+  end
+
+  test do
+    # Based on http://openfst.org/twiki/bin/view/FST/FstExamples.
+    # Makes symbol table.
+    File.open("ascii.syms", "w") do |sink|
+      sink.puts "<epsilon>\t0"
+      sink.puts "<space>\t32"
+      33.upto(126) { |i| sink.puts "#{i.chr}\t#{i}" }
+    end
+    # Makes arclist for downcasing FST.
+    File.open("downcase.arc", "w") do |sink|
+      sink.puts "0\t0\t<epsilon>\t<epsilon>"
+      sink.puts "0\t0\t<space>\t<space>"
+      33.upto(126) { |i| sink.puts "0\t0\t#{i.chr}\t#{i.chr.downcase}" }
+      sink.puts "0"
+    end
+    ## Compile the machine
+    system bin/"fstcompile", "--isymbols=ascii.syms",
+                             "--osymbols=ascii.syms",
+                             "downcase.arc",
+                             "downcase.fst"
   end
 end
