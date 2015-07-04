79787e8fccc6c51e8f492bb5b0be63b400a8d9ff
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-04 08:39:29 +0100

    phipack: add 2013-03-05 bottle.

--- phipack.rb
+++ phipack.rb
@@ -1,6 +1,14 @@
 class Phipack < Formula
   desc "A quick and robust genomic recombination test"
   homepage "http://www.maths.otago.ac.nz/~dbryant/software.html"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-science"
+    cellar :any
+    sha256 "1fabe869b7a87d8fec85ca7cf7ee1899d12a38994a6046f203f83026733c91a3" => :yosemite
+    sha256 "c0af865b47d6146ffa65ae63049933c0cebe523e8ea51edd3b43e85af63c43c8" => :mavericks
+    sha256 "8c0f3cd292ba7964acbcd388ad04dccee785c69288c9d78169e9bf7fba9e5ef6" => :mountain_lion
+  end
+
   # doi "10.1534/genetics.105.048975"
   # tag "bioinformatics"
 
