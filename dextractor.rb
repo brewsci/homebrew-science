9bb7541910c82cd26f44b2a20231f2fb57ee0613
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-07 21:21:54 +0100

    dextractor: add 1.0 bottle.

--- dextractor.rb
+++ dextractor.rb
@@ -1,6 +1,13 @@
 class Dextractor < Formula
   desc "DEXTRACTOR: Bax File Decoder and Data Compressor"
   homepage "https://github.com/thegenemyers/DEXTRACTOR"
+  bottle do
+    cellar :any
+    sha256 "63a69bf5d6de189043f484b1e91e62fa10359ac09f0a3fb3418cc565c41df585" => :yosemite
+    sha256 "bae5327c44d5eb973a19c0212bbdf5ff381e3b00be8b138402fbe7184d7d178f" => :mavericks
+    sha256 "f1c0d3ca01ad5b7901d9b387b6fdf992ae2d33c7ac31a60dacd867fb1ddb135e" => :mountain_lion
+  end
+
   # doi "10.1007/978-3-662-44753-6_5"
   # tag "bioinformatics"
 
