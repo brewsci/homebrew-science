da15a1f1ca5dfdbd9b41e1b0e99c8ddeab69f330
Denis Davydov <davydden@gmail.com>
2015-07-11 06:50:33 +0200

    Trilinos 12.0.1
    Closes #2507

--- trilinos.rb
+++ trilinos.rb
@@ -1,7 +1,8 @@
 class Trilinos < Formula
+  desc "Algorithms for the solution of large-scale, complex multi-physics engineering and scientific problems"
   homepage "http://trilinos.sandia.gov"
-  url "http://trilinos.org/oldsite/download/files/trilinos-11.12.1-Source.tar.bz2"
-  sha256 "a41539414529c65905260b3befe3aee4f1dd1015ff719f0f6b5a10902d576fda"
+  url "https://trilinos.org/oldsite/download/files/trilinos-12.0.1-Source.tar.bz2"
+  sha256 "cab674e88c8ca2d2c54176af60030ed28203c0793f3c64c240363dbe7fa46b99"
   head "https://software.sandia.gov/trilinos/repositories/publicTrilinos", :using => :git
 
   option "with-teko",  "Enable the Teko secondary-stable package"
@@ -33,6 +34,7 @@ class Trilinos < Formula
   depends_on "cppunit"      => :recommended
   depends_on "doxygen"      => ["with-graphviz", :recommended]
   depends_on "hwloc"        => :recommended
+  depends_on "libmatio"     => [:recommended] + ((build.with? "hdf5") ? ["with-hdf5"] : [])
   depends_on "metis"        => :recommended
   depends_on "mumps"        => :recommended
   depends_on "netcdf"       => ["with-fortran", :recommended]
@@ -48,20 +50,21 @@ class Trilinos < Formula
   #-depends_on "binutils"     => :optional # libiberty is deliberately omitted in Homebrew (see PR #35881)
 
   # Experimental TPLs:
-  #-depends_on "eigen"        => :optional # Intrepid_test_Discretization_Basis_HGRAD_TET_Cn_FEM_ORTH_Test_02 fails to build
+  depends_on "eigen"        => :recommended
   depends_on "hypre"        => [:recommended] + ((build.with? "mpi") ? ["with-mpi"] : []) # EpetraExt tests fail to compile
   depends_on "glpk"         => :recommended
   depends_on "hdf5"         => [:recommended] + ((build.with? "mpi") ? ["with-mpi"] : [])
   depends_on "tbb"          => :recommended
   depends_on "glm"          => :recommended
+  depends_on "yaml-cpp"     => :recommended
 
   #-depends_on "lemon"        => :optional # lemon is currently built as executable only, no libraries
   #-depends_on "cask"         => :optional # cask  is currently built as executable only, no libraries
 
   # Missing TPLS:
-  # YAML, BLACS, Y12M, XDMF, tvmet, thrust, taucs, SPARSEKIT, qpOASES, Portals,
+  # BLACS, Y12M, XDMF, tvmet, thrust, taucs, SPARSEKIT, qpOASES, Portals,
   # Pnetcdf, Peano, PaToH, PAPI, Pablo, Oski, OVIS, OpenNURBS, Nemesis, MF,
-  # Matio, MA28, LibTopoMap, InfiniBand, HPCToolkit, HIPS, gtest, gpcd, Gemini,
+  # MA28, LibTopoMap, InfiniBand, HPCToolkit, HIPS, gtest, gpcd, Gemini,
   # ForUQTK, ExodusII, CUSPARSE, Cusp, CrayPortals, Coupler, Clp, CCOLAMD,
   # BGQPAMI, BGPDCMF, ARPREC, ADIC
 
@@ -86,9 +89,7 @@ class Trilinos < Formula
                -DTrilinos_ENABLE_EXAMPLES:BOOL=ON
                -DTrilinos_VERBOSE_CONFIGURE:BOOL=OFF
                -DTrilinos_WARNINGS_AS_ERRORS_FLAGS=""
-               -DTrilinos_ENABLE_OpenMP:BOOL=OFF
-               -DTPL_ENABLE_Matio=OFF
-               -DSacado_ENABLE_TESTS=OFF]
+               -DTrilinos_ENABLE_OpenMP:BOOL=OFF]
 
     # constrain Cmake to look for libraries in homebrew's prefix
     args << "-DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}"
@@ -106,8 +107,6 @@ class Trilinos < Formula
       args << "-DLAPACK_LIBRARY_DIRS=#{Formula["openblas"].opt_lib}"
     end
 
-    args << "-DEpetraExt_ENABLE_TESTS=OFF" if build.with? "hypre"
-
     args << "-DTrilinos_ASSERT_MISSING_PACKAGES=OFF" if build.head?
 
     args << onoff("-DTPL_ENABLE_MPI:BOOL=",         (build.with? "mpi"))
@@ -118,12 +117,23 @@ class Trilinos < Formula
     args << onoff("-DTrilinos_ENABLE_ShyLU:BOOL=",  (build.with? "shylu"))
     args << onoff("-DTrilinos_ENABLE_Teko:BOOL=",   (build.with? "teko"))
 
+    # Temporary disable due to compiler errors:
+    args << "-DTrilinos_ENABLE_STK=OFF"
+    args << "-DTrilinos_ENABLE_SEACAS=OFF"
+    args << "-DIntrepid_ENABLE_TESTS=OFF"
+    args << "-DSacado_ENABLE_TESTS=OFF"
+    args << "-DEpetraExt_ENABLE_TESTS=OFF" if build.with? "hypre"
+    args << "-DTrilinos_ENABLE_FEI=OFF" if not OS.mac?
+    args << "-DTrilinos_ENABLE_Sundance=OFF" if not OS.mac?
+
     # Third-party libraries
     args << onoff("-DTPL_ENABLE_Boost:BOOL=",       (build.with? "boost"))
     args << onoff("-DTPL_ENABLE_Scotch:BOOL=",      (build.with? "scotch"))
     args << onoff("-DTPL_ENABLE_Netcdf:BOOL=",      (build.with? "netcdf"))
     args << onoff("-DTPL_ENABLE_ADOLC:BOOL=",       (build.with? "adol-c"))
     args << onoff("-DTPL_ENABLE_AMD:BOOL=",         (build.with? "suite-sparse"))
+    args << onoff("-DTPL_ENABLE_Matio:BOOL=",       (build.with? "libmatio"))
+    args << onoff("-DTPL_ENABLE_yaml-cpp:BOOL=",    (build.with? "yaml-cpp"))
 
     if (build.with? "suite-sparse") && (build.with? "csparse")
       args << "-DTPL_ENABLE_CSparse:BOOL=ON"
@@ -149,8 +159,9 @@ class Trilinos < Formula
     # Even though METIS seems to conflicts with ParMETIS in Trilinos config (see TPLsList.cmake in the source folder),
     # we still need to provide METIS_INCLUDE_DIRS so that metis.h is picked up on Linuxbrew.
     if build.with? "metis"
+      ext = OS.mac? ? "dylib" : "so"
       args << "-DTPL_ENABLE_METIS:BOOL=ON"
-      args << "-DMETIS_LIBRARIES=#{Formula["metis"].opt_lib}/libmetis.a"
+      args << "-DTPL_METIS_LIBRARIES=#{Formula["metis"].opt_lib}/libmetis.#{ext}"
       args << "-DMETIS_INCLUDE_DIRS=#{Formula["metis"].opt_include}"
     else
       args << "-DTPL_ENABLE_METIS:BOOL=OFF"
@@ -169,8 +180,10 @@ class Trilinos < Formula
 
     if build.with? "parmetis"
       # Ensure CMake picks up METIS 5 and not METIS 4.
+      ext = OS.mac? ? "dylib" : "so"
+      ext_parmetis = OS.mac? ? "a" : "so"
       args << "-DTPL_ENABLE_ParMETIS:BOOL=ON"
-      args << "-DParMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.a;#{Formula["metis"].opt_lib}/libmetis.a"
+      args << "-DTPL_ParMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.#{ext_parmetis};#{Formula["metis"].opt_lib}/libmetis.#{ext}"
       args << "-DParMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include}"
     else
       args << "-DTPL_ENABLE_ParMETIS:BOOL=OFF"
@@ -181,6 +194,8 @@ class Trilinos < Formula
     args << onoff("-DTPL_ENABLE_SuperLU:BOOL=",     (build.with? "superlu"))
     args << "-DSuperLU_INCLUDE_DIRS=#{Formula["superlu"].opt_include}/superlu" if build.with? "superlu"
 
+    # fix for 4.0:
+    args << "-DHAVE_SUPERLUDIST_LUSTRUCTINIT_2ARG:BOOL=ON" if build.with? "superlu_dist"
     args << onoff("-DTPL_ENABLE_SuperLUDist:BOOL=", (build.with? "superlu_dist"))
     args << "-DSuperLUDist_INCLUDE_DIRS=#{Formula["superlu_dist"].opt_include}/superlu_dist" if build.with? "superlu_dist"
 
@@ -210,46 +225,138 @@ class Trilinos < Formula
     end
   end
 
+  def caveats; <<-EOS
+    On Linuxbrew install with:
+      --with-openblas --without-scotch
+    EOS
+  end
+
+
   test do
     system "#{bin}/Epetra_BasicPerfTest_test.exe", "16", "12", "1", "1", "25", "-v"
     system "mpirun", "-np", "2", "#{bin}/Epetra_BasicPerfTest_test.exe", "10", "12", "1", "2", "9", "-v" if build.with? "mpi"
     system "#{bin}/Epetra_BasicPerfTest_test_LL.exe", "16", "12", "1", "1", "25", "-v"
     system "mpirun", "-np", "2", "#{bin}/Epetra_BasicPerfTest_test_LL.exe", "10", "12", "1", "2", "9", "-v" if build.with? "mpi"
     # system "#{bin}/Ifpack2_BelosTpetraHybridPlatformExample.exe"                    # this file is not there
-    system "#{bin}/KokkosClassic_SerialNodeTestAndTiming.exe"
-    #-system "#{bin}/KokkosClassic_TPINodeTestAndTiming.exe"                          # this file is not there
-    system "#{bin}/KokkosClassic_TBBNodeTestAndTiming.exe" if build.with? "tbb"
-    system "#{bin}/Tpetra_GEMMTiming_TBB.exe" if build.with? "tbb"
-    # system "#{bin}/Tpetra_GEMMTiming_TPI.exe"                                       # Fails!!
+    # system "#{bin}/KokkosClassic_SerialNodeTestAndTiming.exe"                       # this file is not there
+    # system "#{bin}/KokkosClassic_TPINodeTestAndTiming.exe"                          # this file is not there
+    # system "#{bin}/KokkosClassic_TBBNodeTestAndTiming.exe" if build.with? "tbb"     # this file is not there
+    # system "#{bin}/Tpetra_GEMMTiming_TBB.exe" if build.with? "tbb"                  # this file is not there
+    # system "#{bin}/Tpetra_GEMMTiming_TPI.exe"                                       # this file is not there
   end
 end
 
 __END__
 diff --git a/cmake/TPLs/FindTPLUMFPACK.cmake b/cmake/TPLs/FindTPLUMFPACK.cmake
-index e26fc7e..f0b7ab6 100644
+index 963eb71..998cd02 100644
 --- a/cmake/TPLs/FindTPLUMFPACK.cmake
 +++ b/cmake/TPLs/FindTPLUMFPACK.cmake
 @@ -55,6 +55,6 @@
  
  
- TRIBITS_TPL_DECLARE_LIBRARIES( UMFPACK
+ TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( UMFPACK
 -  REQUIRED_HEADERS umfpack.h amd.h UFconfig.h
 +  REQUIRED_HEADERS umfpack.h amd.h SuiteSparse_config.h
    REQUIRED_LIBS_NAMES umfpack amd
    )
-diff --git a/packages/amesos/src/Amesos_Superludist.cpp b/packages/amesos/src/Amesos_Superludist.cpp
-index 1fcac5d..12036fa 100644
---- a/packages/amesos/src/Amesos_Superludist.cpp
-+++ b/packages/amesos/src/Amesos_Superludist.cpp
-@@ -473,8 +473,8 @@ int Amesos_Superludist::Factor()
-     else                    PrivateSuperluData_->options_.ReplaceTinyPivot = (yes_no_t)NO;
+diff --git a/packages/rol/test/vector/test_03.cpp b/packages/rol/test/vector/test_03.cpp
+index 5722915..fa53818 100644
+--- a/packages/rol/test/vector/test_03.cpp
++++ b/packages/rol/test/vector/test_03.cpp
+@@ -75,8 +75,8 @@ int main(int argc, char *argv[]) {
+ 	RCP<Vector<RealT> > y = rcp(new StdVector<RealT>(y_rcp)); 
+ 	RCP<Vector<RealT> > z = rcp(new StdVector<RealT>(z_rcp)); 
  
-     if( IterRefine_ == "NO" ) PrivateSuperluData_->options_.IterRefine = (IterRefine_t)NO;
--    else if( IterRefine_ == "DOUBLE" ) PrivateSuperluData_->options_.IterRefine = DOUBLE;
--    else if( IterRefine_ == "EXTRA" ) PrivateSuperluData_->options_.IterRefine = EXTRA;
-+    else if( IterRefine_ == "DOUBLE" ) PrivateSuperluData_->options_.IterRefine = SLU_DOUBLE;
-+    else if( IterRefine_ == "EXTRA" ) PrivateSuperluData_->options_.IterRefine = SLU_EXTRA;
+-	ArrayRCP<RCP<Vector<RealT>>> A_rcp(2);
+-	ArrayRCP<RCP<Vector<RealT>>> B_rcp(2);
++	ArrayRCP<RCP<Vector<RealT> > > A_rcp(2);
++	ArrayRCP<RCP<Vector<RealT> > > B_rcp(2);
  
-     //  Without the following two lines, SuperLU_DIST cannot be made
-     //  quiet.
+ 	A_rcp[0] = x;     
+ 	A_rcp[1] = y;     
+@@ -84,8 +84,8 @@ int main(int argc, char *argv[]) {
+ 	B_rcp[0] = w;     
+ 	B_rcp[1] = z;     
+ 
+-	RCP<MultiVector<RealT>> A = rcp(new MultiVectorDefault<RealT>(A_rcp));
+-	RCP<MultiVector<RealT>> B = rcp(new MultiVectorDefault<RealT>(B_rcp));
++	RCP<MultiVector<RealT> > A = rcp(new MultiVectorDefault<RealT>(A_rcp));
++	RCP<MultiVector<RealT> > B = rcp(new MultiVectorDefault<RealT>(B_rcp));
+        
+ 	// Test norm
+ 	if(static_cast<int>(norm_sum(*A)) != 6) {
+@@ -93,13 +93,13 @@ int main(int argc, char *argv[]) {
+ 	}
+ 
+ 	// Test clone
+-	RCP<MultiVector<RealT>> C = A->clone();    
++	RCP<MultiVector<RealT> > C = A->clone();    
+ 	if(norm_sum(*C) != 0) {
+ 	    ++errorFlag;
+ 	}
+ 
+ 	// Test deep copy
+-        RCP<MultiVector<RealT>> D = A->deepCopy();
++        RCP<MultiVector<RealT> > D = A->deepCopy();
+ 	if(static_cast<int>(norm_sum(*D)) != 6) {
+ 	    ++errorFlag;
+ 	}
+@@ -108,7 +108,7 @@ int main(int argc, char *argv[]) {
+ 	std::vector<int> index(1);
+ 	index[0] = 0;
+ 
+-        RCP<MultiVector<RealT>> S = A->shallowCopy(index);
++        RCP<MultiVector<RealT> > S = A->shallowCopy(index);
+ 	if(static_cast<int>(norm_sum(*S)) != 1) {
+ 	    ++errorFlag;
+ 	}
+diff --git a/packages/didasko/examples/hypre/hypre_Helpers.hpp b/packages/didasko/examples/hypre/hypre_Helpers.hpp
+index 930719e..70ac59f 100644
+--- a/packages/didasko/examples/hypre/hypre_Helpers.hpp
++++ b/packages/didasko/examples/hypre/hypre_Helpers.hpp
+@@ -51,11 +51,11 @@
+ 
+ #include <string>
+ 
+-EpetraExt_HypreIJMatrix::EpetraExt_HypreIJMatrix* newHypreMatrix(int N);
++EpetraExt_HypreIJMatrix* newHypreMatrix(int N);
+ 
+-Epetra_CrsMatrix::Epetra_CrsMatrix* newCrsMatrix(int N);
++Epetra_CrsMatrix* newCrsMatrix(int N);
+ 
+-Epetra_CrsMatrix::Epetra_CrsMatrix* GetCrsMatrix(EpetraExt_HypreIJMatrix &Matrix);
++Epetra_CrsMatrix* GetCrsMatrix(EpetraExt_HypreIJMatrix &Matrix);
+ 
+ bool EquivalentVectors(Epetra_MultiVector &X, Epetra_MultiVector &Y, double tol);
 
+diff --git a/packages/didasko/examples/hypre/hypre_Helpers.cpp b/packages/didasko/examples/hypre/hypre_Helpers.cpp
+index 1bf1b2c..793e218 100644
+--- a/packages/didasko/examples/hypre/hypre_Helpers.cpp
++++ b/packages/didasko/examples/hypre/hypre_Helpers.cpp
+@@ -60,7 +60,7 @@
+ 
+ using Teuchos::RCP;
+ using Teuchos::rcp;
+-EpetraExt_HypreIJMatrix::EpetraExt_HypreIJMatrix* newHypreMatrix(const int N)
++EpetraExt_HypreIJMatrix* newHypreMatrix(const int N)
+ {
+   HYPRE_IJMatrix Matrix;
+   int ierr = 0;
+@@ -117,7 +117,7 @@ EpetraExt_HypreIJMatrix::EpetraExt_HypreIJMatrix* newHypreMatrix(const int N)
+   return RetMat;
+ }
+ 
+-Epetra_CrsMatrix::Epetra_CrsMatrix* newCrsMatrix(int N){
++Epetra_CrsMatrix* newCrsMatrix(int N){
+ 
+   Epetra_MpiComm Comm(MPI_COMM_WORLD);
+ 
+@@ -138,7 +138,7 @@ Epetra_CrsMatrix::Epetra_CrsMatrix* newCrsMatrix(int N){
+   return Matrix;
+ }
+ 
+-Epetra_CrsMatrix::Epetra_CrsMatrix* GetCrsMatrix(EpetraExt_HypreIJMatrix *Matrix)
++Epetra_CrsMatrix* GetCrsMatrix(EpetraExt_HypreIJMatrix *Matrix)
+ {
+   int N = Matrix->NumGlobalRows();
+   Epetra_CrsMatrix* TestMat = new Epetra_CrsMatrix(Copy, Matrix->RowMatrixRowMap(), Matrix->RowMatrixColMap(), N, false);
