edd6d139767b49ca43d57b96fa9a14a1f340b14d
chapmanb <chapmanb@50mail.com>
2015-07-13 07:06:55 -0400

    lumpy-sv 0.2.11
    Updates to the latest lumpy release:
    - Includes support for new lumpyexpress script.
    - Update dependencies based on current suggestions.
    
    Closes #2515.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- lumpy-sv.rb
+++ lumpy-sv.rb
@@ -1,9 +1,9 @@
 class LumpySv < Formula
   homepage "https://github.com/arq5x/lumpy-sv"
+  url "https://github.com/arq5x/lumpy-sv/releases/download/0.2.11/lumpy-sv-0.2.11.tar.gz"
+  sha256 "ce2d8d6353cbce1c83a0aaa25a5bd9238f0ebaa8253b873b01bd446290e54379"
   # doi "10.1186/gb-2014-15-6-r84"
   # tag "bioinformatics"
-  url "https://github.com/arq5x/lumpy-sv/releases/download/0.2.9/lumpy-sv-0.2.9.tar.gz"
-  sha1 "3ca24aed2b5a57a5e8a55f2f65057520474fb77c"
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
@@ -13,21 +13,30 @@ class LumpySv < Formula
     sha1 "9a951d727f67fc9a315d26a260d0ad4f1b69cd6f" => :mountain_lion
   end
 
-  depends_on "bamtools" => :recommended
-  depends_on "samtools" => :recommended
-  depends_on "bedtools" => :recommended
+  depends_on "samblaster" => :optional
+  depends_on "samtools" => :optional
+  depends_on "sambamba" => :optional
   depends_on "bwa" => :optional
-  depends_on "novoalign" => :optional
-  depends_on "yaha" => :optional
+
+  resource "bamkit" do
+    url "https://github.com/cc2qe/bamkit/archive/6b8c20.tar.gz"
+    sha256 "98d8180f4b7f5e32deb531cc3562cd3eeed0d2377ca539230f43806281393513"
+  end
 
   def install
     ENV.deparallelize
+    inreplace "scripts/lumpyexpress.config" do |s|
+      s.change_make_var! "LUMPY_HOME", "#{HOMEBREW_PREFIX}/share/lumpy-sv"
+    end
     system "make"
     bin.install "bin/lumpy"
-    (share/"lumpy-sv").install Dir["scripts/*"]
+    bin.install Dir["scripts/lumpyexpress*"]
+    (pkgshare/"scripts").install Dir["scripts/*"]
+    resource("bamkit").stage pkgshare/"scripts/bamkit"
   end
 
   test do
     system "#{bin}/lumpy 2>&1 |grep -q structural"
+    system "#{bin}/lumpyexpress 2>&1 |grep -q lumpyexpress"
   end
 end
