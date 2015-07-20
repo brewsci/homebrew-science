0c4afe8c2a2f7904a25bd1d82dbdc74304e5ad36
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-21 00:42:51 +0100

    boost-compute: add 0.4 bottle.

--- boost-compute.rb
+++ boost-compute.rb
@@ -4,6 +4,13 @@ class BoostCompute < Formula
   url "https://github.com/kylelutz/compute/archive/v0.4.tar.gz"
   sha256 "d6986155d01a02b12351d6c9cc6c85498292209c3949b3a4628c0712ec7cf01b"
 
+  bottle do
+    cellar :any
+    sha256 "ec92f5a55c38b17c928d407bc63322602613789ac4ae821fde03917c18eccf50" => :yosemite
+    sha256 "711ad539e5a83f864691145e407456ba9eb9d4170a06c56ccb724655501d4b9f" => :mavericks
+    sha256 "1acd47a79eb8d1e3349812e307c85b6c21437022faf307876d2fe930cef88c69" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
 
