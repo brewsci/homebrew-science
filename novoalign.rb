da256332b1c1e58c0ed3370f9c2dd6011fa582fb
Shaun Jackman <sjackman@gmail.com>
2015-07-21 11:30:03 -0700

    novoalign 3.02.13: Update style

--- novoalign.rb
+++ novoalign.rb
@@ -1,16 +1,16 @@
-require "formula"
-
 class Novoalign < Formula
+  desc "Map short reads to a reference genome"
   homepage "http://www.novocraft.com/"
-  #tag "bioinformatics"
+  # tag "bioinformatics"
 
   version "3.02.13"
-  url "http://www.novocraft.com/homebrew/novocraftV%s.%s.tar.gz" %
-    [version, if OS.mac? then "MacOSX" else "Linux2.6" end]
-  sha256(if OS.mac? then "cb51ac1f7ffa96b528fea18c4b9f9b16a7ebd195efddd20caa54a251e2c19e76"
-      elsif OS.linux? then "e2cce841c44638ac93704c6f51c5390cf116d7aca7a77cc4551cb2aeceebf47d"
+  url "http://www.novocraft.com/homebrew/novocraftV#{version}.#{OS.mac? ? "MacOSX" : "Linux2.6"}.tar.gz"
+  sha256(
+    if OS.mac? then "cb51ac1f7ffa96b528fea18c4b9f9b16a7ebd195efddd20caa54a251e2c19e76"
+    elsif OS.linux? then "e2cce841c44638ac93704c6f51c5390cf116d7aca7a77cc4551cb2aeceebf47d"
     else raise "Unknown operating system"
     end)
+
   bottle do
     cellar :any
     sha256 "e900cd36677ec495d0c6fa3a84e0700bd35680c509b28e2528b18fe40a0aa00b" => :yosemite
@@ -19,11 +19,12 @@ class Novoalign < Formula
   end
 
   def install
-    bin.install %w[isnovoindex novo2paf novoalign novoalignCS
+    bin.install %w[
+      isnovoindex novo2paf novoalign novoalignCS
       novoalignCSMPI novoalignMPI novobarcode novoindex novomethyl
       novope2bed.pl novorun.pl novosort novoutil]
     # Conflicts with samtools
-    #bin.install "novo2sam.pl"
+    # bin.install "novo2sam.pl"
     doc.install Dir["*.pdf", "*.txt"]
   end
 
