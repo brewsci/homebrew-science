d28710f938210c9ce2f28d65a5f6b0bf3ca2c6dc
Sebastian Schöps <sebastian@schoeps.org>
2015-07-24 23:29:42 +0200

    getdp 2.6
    Closes #2292.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- getdp.rb
+++ getdp.rb
@@ -1,17 +1,16 @@
-require "formula"
-
-class GmshSvnStrategy < SubversionDownloadStrategy
-  def quiet_safe_system *args
+class GetdpSvnStrategy < SubversionDownloadStrategy
+  def quiet_safe_system(*args)
     super *args + ["--username", "getdp", "--password", "getdp"]
   end
 end
 
 class Getdp < Formula
+  desc "GetDP is an open source finite element solver using mixed elements."
   homepage "http://www.geuz.org/getdp/"
-  url "http://www.geuz.org/getdp/src/getdp-2.4.4-source.tgz"
-  sha1 "e4c3a2a0d9281acdab2c6ba2c3c6ecd53fea4d0c"
+  url "http://www.geuz.org/getdp/src/getdp-2.6.0-source.tgz"
+  sha256 "ebbf6791e815dda7a306efbfe3cc0acd30cc2ad9ecf6ac0f2fb9fc75a9aae051"
 
-  head 'https://geuz.org/svn/getdp/trunk', :using => GmshSvnStrategy
+  head "https://geuz.org/svn/getdp/trunk", :using => GetdpSvnStrategy
 
   option "without-check", "skip build-time tests (not recommended)"
 
@@ -25,10 +24,6 @@ class Getdp < Formula
   depends_on "cmake" => :build
 
   def install
-    if not build.head? and (build.with? "petsc" or build.with? "slepc")
-      onoe "stable is incompatible with PETSc/SLEPc 3.5.2. Build with --HEAD."
-      exit 1
-    end
     args = std_cmake_args
     args << "-DENABLE_BUILD_SHARED=ON"
     args << "-DENABLE_ARPACK=OFF" if build.without? "arpack"
@@ -43,12 +38,12 @@ class Getdp < Formula
     end
 
     if build.with? "slepc"
-      ENV["SLEPC_DIR"] = "#{Formula['slepc'].opt_prefix}/real"
+      ENV["SLEPC_DIR"] = "#{Formula["slepc"].opt_prefix}/real"
     else
       args << "-DENABLE_SLEPC=OFF"
     end
 
-    if (build.with? "petsc") or (build.with? "slepc")
+    if (build.with? "petsc") || (build.with? "slepc")
       args << "-DENABLE_MPI=ON" if build.with? :mpi
     end
 
@@ -63,4 +58,7 @@ class Getdp < Formula
     end
   end
 
+  test do
+    system "#{bin}/getdp", "#{share}/doc/getdp/demos/magnet.pro"
+  end
 end
