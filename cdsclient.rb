9735db5bfde0ed4f74d6f2261f22c175870306c3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-25 01:08:17 +0100

    cdsclient: add 3.80 bottle.

--- cdsclient.rb
+++ cdsclient.rb
@@ -4,6 +4,13 @@ class Cdsclient < Formula
   url "http://cdsarc.u-strasbg.fr/ftp/pub/sw/cdsclient-3.80.tar.gz"
   sha256 "ed399b80e158c75730b61862c3ec467f061598f28e9649cb2c109562df489360"
 
+  bottle do
+    cellar :any
+    sha256 "f721d5d4ff9c37c90db6e70fcb7c6d4fd69d526162d7a9dbaf9f0dff05bb3476" => :yosemite
+    sha256 "3cf5e5544824dddfa2e5aa2eb25ab71ba7762c1f5f905411bbfadbe680fc0d3e" => :mavericks
+    sha256 "d7f9c281d6053801733d7e127cfefc2c65c2885590a50049a341fa66574dbe39" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
