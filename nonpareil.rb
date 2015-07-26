31b0b5e571ff8edb905d06e0e7687ad18d5af95a
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-07-26 16:57:26 -0400

    Update nonpareil.rb
    style compliant

--- nonpareil.rb
+++ nonpareil.rb
@@ -1,11 +1,10 @@
 class Nonpareil < Formula
-  homepage 'http://enve-omics.ce.gatech.edu/nonpareil'
+  desc "Estimates coverage in metagenomic datasets."
+  homepage "http://enve-omics.ce.gatech.edu/nonpareil"
   # doi "10.1093/bioinformatics/btt584"
   # tag "bioinformatics"
-
   url "https://github.com/lmrodriguezr/nonpareil/archive/v2.4.01.tar.gz"
   sha256 "ca5955e877098ed4a679404ac87635e28a855d15d6970ca51a6be422266c0999"
-
   head "https://github.com/lmrodriguezr/nonpareil.git"
 
   depends_on "r"
