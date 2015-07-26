41992b5dc996f2fa0e1890c577f2ac09998c2af3
Noah Spies <nspies@stanford.edu>
2015-07-26 14:30:00 -0700

    yass 1.14 (new formula)
    Closes #2568.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- yass.rb
+++ yass.rb
@@ -1,5 +1,5 @@
 class Yass < Formula
-  desc "Yet Another Similarity Searcher genomic similarity search tool"
+  desc "YASS genomic similarity search tool"
   homepage "http://bioinfo.lifl.fr/yass/"
   url "http://bioinfo.lifl.fr/yass/files/yass-current/yass-1.14.tar.gz"
   sha256 "2ea4d2a32bb17fb6de590b0e8bce5231e2506b490b3456700b4bc029544a1982"
