ae83aff0d2ff23b862e326ab6a8e4dc3d17039f2
Tim D. Smith <git@tim-smith.us>
2015-07-27 13:12:11 -0700

    nonpareil: unbottle
    Bottles don't include the R package because R CMD INSTALL installs
    outside of HOMEBREW_PREFIX.
    
    Workaround for #2559 pending real support for distributing R packages
    with Homebrew.

--- nonpareil.rb
+++ nonpareil.rb
@@ -7,12 +7,6 @@ class Nonpareil < Formula
   sha256 "ca5955e877098ed4a679404ac87635e28a855d15d6970ca51a6be422266c0999"
   head "https://github.com/lmrodriguezr/nonpareil.git"
 
-  bottle do
-    cellar :any
-    sha256 "654afb8d18101c7a82c96b5c3254e6fec0d6816df24cdc90d4b92af7fc06a419" => :yosemite
-    sha256 "a130e6c42e95307364af276989286ffbdef539281f46e5246426ed6864adcd40" => :mavericks
-    sha256 "85fecd99f93abe1e30fdc17ca997bb94bcccb97a0d84e8e5fc1d23bff98e38cf" => :mountain_lion
-  end
   depends_on "r"
   depends_on :mpi => [:cxx, :optional]
 
