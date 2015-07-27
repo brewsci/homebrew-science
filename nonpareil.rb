2448600a6eb20eb69c314e9c053cc2c816419f13
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-07-27 01:32:02 -0400

    nonpareil bottle restored
    Closes #2567.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- nonpareil.rb
+++ nonpareil.rb
@@ -7,6 +7,12 @@ class Nonpareil < Formula
   sha256 "ca5955e877098ed4a679404ac87635e28a855d15d6970ca51a6be422266c0999"
   head "https://github.com/lmrodriguezr/nonpareil.git"
 
+  bottle do
+    cellar :any
+    sha1 "9fa1b02ccc8d561c9586b07d854c71227a77739e" => :yosemite
+    sha1 "cd8a9999c84c41fc39aaa7f62cc1ab44a3a26300" => :mavericks
+    sha1 "11adac7a92b44e5fd171bc1684a8b76fdceb848f" => :mountain_lion
+  end
   depends_on "r"
   depends_on :mpi => [:cxx, :optional]
 
