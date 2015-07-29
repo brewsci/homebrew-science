e5cde3c983b93545434eec08158f19c4520a9c38
Ian Lancaster <ilancaster@gmail.com>
2015-07-29 01:43:50 -0400

    igvtools: use pkgshare

--- igvtools.rb
+++ igvtools.rb
@@ -1,7 +1,6 @@
 class Igvtools < Formula
   homepage "http://www.broadinstitute.org/software/igv/download"
   # tag "bioinformatics"
-
   url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.48.zip"
   sha256 "0ae8fc2f6500495ae84aa0232887d9466c73c188f5af91d64a089e5808d3a84d"
 
@@ -10,7 +9,7 @@ class Igvtools < Formula
     java.install "igvtools.jar"
     bin.write_jar_script java/"igvtools.jar", "igvtools"
     doc.install "igvtools_readme.txt"
-    share.install "genomes"
+    pkgshare.install "genomes"
   end
 
   test do
