b7d90c0590029c802d7b95ff6b53eb7329f65601
Ian Lancaster <ilancaster@gmail.com>
2015-08-02 19:08:08 -0400

    hotspot: use pkgshare, add desc
    Closes #2604.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- hotspot.rb
+++ hotspot.rb
@@ -1,21 +1,20 @@
-require "formula"
-
 class Hotspot < Formula
+  desc "Identify regions of local enrichment of short-read sequence tags"
   homepage "https://github.com/rthurman/hotspot"
   url "https://github.com/rthurman/hotspot/archive/v4.1.0.tar.gz"
-  sha1 "d1b40651eabadd7de6110ce49ae2580cc1c1021b"
+  sha256 "9ecdbba612b80f137b78314c23ac94aa6840d217bf4256faf8adee88a73fdd0c"
   head "https://github.com/rthurman/hotspot.git"
 
   depends_on "gsl"
 
   def install
     ENV.deparallelize
-    system "make -C hotspot-distr/hotspot-deploy"
+    system "make", "-C", "hotspot-distr/hotspot-deploy"
 
     inreplace "hotspot-distr/pipeline-scripts/test/runall.tokens.txt",
               "/full/path/to/hotspot-distr",
-              "#{share}"
-    share.install "hotspot-distr/pipeline-scripts", "hotspot-distr/data"
+              "#{pkgshare}"
+    pkgshare.install "hotspot-distr/pipeline-scripts", "hotspot-distr/data"
 
     bin.install Dir["hotspot-distr/hotspot-deploy/bin/*"]
     doc.install %w[LICENSE README.md]
@@ -23,7 +22,7 @@ class Hotspot < Formula
 
   def caveats; <<-EOS.undent
     Run the test suite (~1 hr):
-      #{share}/pipeline-scripts/test/runhotspot
+      #{opt_pkgshare}/pipeline-scripts/test/runhotspot
     EOS
   end
 end
