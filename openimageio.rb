6a3d8d2d697128392a1387921f054e58f222d709
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-02 16:45:42 +0100

    openimageio: update 1.5.14_1 bottle.

--- openimageio.rb
+++ openimageio.rb
@@ -8,9 +8,9 @@ class Openimageio < Formula
 
   bottle do
     cellar :any
-    sha256 "434018fb5d454145ca6bcebb756e2feb3a7e1b20990e706dd906fa6c35f40364" => :yosemite
-    sha256 "0065c7db083fc0a6b67dc229473a27437efda04067fbce0be3224153303c80ee" => :mavericks
-    sha256 "4b5a7ac6395cf8c913d44d00f00a4957f452a20c4c95ee4b3975fdf56bb45f85" => :mountain_lion
+    sha256 "e7185968cd34d2f26b0310a2a2dddb3ec5399e22ee5fcbda6602e650125484aa" => :yosemite
+    sha256 "a53476e313fdafa04333b8d0ff080e4082c17198bc521610c65f6ff633fb08ff" => :mavericks
+    sha256 "edfb576f58c6938d25be5d2509419c52dd6ab66942b33123b2026438540057ed" => :mountain_lion
   end
 
   option "with-tests",  "Dowload 95MB of test images and verify Oiio (~2 min)"
