8db0146f2874f50766748099ee4c91633ad6df2e
Ian Lancaster <ilancaster@gmail.com>
2015-08-02 18:50:19 -0400

    phyml: use pkgshare
    Closes #2602.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- phyml.rb
+++ phyml.rb
@@ -1,9 +1,10 @@
-require 'formula'
-
 class Phyml < Formula
-  homepage 'http://www.atgc-montpellier.fr/phyml/'
-  url 'https://phyml.googlecode.com/files/phyml-20120412.tar.gz'
-  sha1 '8a4699ad2bba7e2f76c8eefa277d29290f92071c'
+  desc "Fast maximum likelihood-based phylogenetic inference"
+  # tag "bioinformatics"
+  # doi "10.1093/sysbio/syq010"
+  homepage "http://www.atgc-montpellier.fr/phyml/"
+  url "https://phyml.googlecode.com/files/phyml-20120412.tar.gz"
+  sha256 "a43e51534a1ae2d1ee4c94ced3a1855813ff4872a6c9c2b83b369ddb71198417"
 
   def install
     # separate steps required
@@ -13,23 +14,21 @@ class Phyml < Formula
     system "./configure", "--prefix=#{prefix}", "--enable-phytime"
     system "make"
 
-    bin.install %w(src/phyml src/phytime)
-    doc.install Dir['doc/phyml-manual.pdf']
-    rm_rf Dir['examples/.svn']
-    chmod 0644, Dir['examples/*']
-    share.install Dir['examples/*']
+    bin.install "src/phyml", "src/phytime"
+    doc.install "doc/phyml-manual.pdf"
+    pkgshare.install Dir["examples/*"]
   end
 
   def caveats; <<-EOS.undent
     Examples have been installed here:
-      #{share}
+      #{opt_pkgshare}
 
     See options for phyml by running:
       phmyl --help
 
     PhyML must be run with the "-i" option to specify an input or it will
     segfault. Example:
-      phyml -i #{share}/nucleic
+      phyml -i #{opt_pkgshare}/nucleic
     EOS
   end
 end
