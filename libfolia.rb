76f4d5b3d9d1e8a176008696df6a8c7f0e194269
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-03 04:08:37 +0100

    libfolia: add 0.13 bottle.

--- libfolia.rb
+++ libfolia.rb
@@ -4,6 +4,13 @@ class Libfolia < Formula
   url "http://software.ticc.uvt.nl/libfolia-0.13.tar.gz"
   sha256 "a9fc9e475bb79629dc014cf7a78af64486c0f1bb902821ba2bb36c38c77a5d24"
 
+  bottle do
+    cellar :any
+    sha256 "5259f25027ac5f434b58d26b40ab0a1d79c1c623238ab5390435ae0b86813263" => :yosemite
+    sha256 "a5e58e875d353398800fd2c3f6efa824a1af4b8aea735225ca1f981f9bc93327" => :mavericks
+    sha256 "245d72db8e3c2141ebc37d68112b0d24d85b071d46ba1df2a20dff5c48392bb2" => :mountain_lion
+  end
+
   option "without-check", "skip build-time checks (not recommended)"
 
   depends_on "pkg-config" => :build
