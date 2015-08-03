18b8b8ceebefd7d251281f7a5053379e6f731626
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-03 01:00:04 +0100

    primer3: add 2.3.6 bottle.

--- primer3.rb
+++ primer3.rb
@@ -4,6 +4,13 @@ class Primer3 < Formula
   url "https://downloads.sourceforge.net/project/primer3/primer3/2.3.6/primer3-src-2.3.6.tar.gz"
   sha256 "2ff54faf957f0d7e4c79d9536fa1027b028bc5e4c6005f142df42ef85562ecd4"
 
+  bottle do
+    cellar :any
+    sha256 "a2063477d744b6f8a941b12e0858db9068aeae9d5b3b80c358037a54f6b0502f" => :yosemite
+    sha256 "68df30a6984ad73ec68e2200180f8c8f21e906a2b4c4af8f62d2909a9cdbd7d3" => :mavericks
+    sha256 "0eb3406fb87e960f7c46656f746db8849f2daf3ab9aee31154107312ebab0bf2" => :mountain_lion
+  end
+
   option "without-check", "Skip build-time tests"
 
   def install
