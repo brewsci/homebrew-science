0adbb7e9a2cf8bc57232daad45b5413f6bc38d5a
Ian Lancaster <ilancaster@gmail.com>
2015-08-02 21:32:43 -0400

    trimal: use pkgshare, add desc
    Closes #2609.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- trimal.rb
+++ trimal.rb
@@ -1,11 +1,12 @@
 class Trimal < Formula
+  desc "Automated alignment trimming in phylogenetic analyses"
   homepage "http://trimal.cgenomics.org/"
   # doi "10.1093/bioinformatics/btp348"
   # tag "bioinformatics"
-  head "https://github.com/scapella/trimal"
-
   url "https://github.com/scapella/trimal/archive/v1.4.1.tar.gz"
   sha256 "cb8110ca24433f85c33797b930fa10fe833fa677825103d6e7f81dd7551b9b4e"
+
+  head "https://github.com/scapella/trimal"
 
   bottle do
     cellar :any
@@ -17,13 +18,13 @@ class Trimal < Formula
   def install
     system "make", "-C", "source", "CC=c++"
     bin.install "source/readal", "source/trimal", "source/statal"
-    share.install "dataset"
+    pkgshare.install "dataset"
     doc.install %w[AUTHORS CHANGELOG LICENSE README]
   end
 
   test do
-    assert_match "Salvador", shell_output("trimal 2>&1", 0)
-    assert_match "Salvador", shell_output("readal 2>&1", 0)
-    assert_match "Salvador", shell_output("statal 2>&1", 0)
+    assert_match "Salvador", shell_output("#{bin}/trimal 2>&1", 0)
+    assert_match "Salvador", shell_output("#{bin}/readal 2>&1", 0)
+    assert_match "Salvador", shell_output("#{bin}/statal 2>&1", 0)
   end
 end
