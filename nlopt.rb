b18c447fd76d82089dc48a7d0b26d951e0ca6da0
Ge Tan <gtan@me.com>
2015-08-04 19:49:03 +0100

    nlopt: add lib name links for C programs
    This homebrew nlopt library is compiled with C++ with lib names which
    are not recognised by some C programs. Add links like libnlopt_cxx.dylib
    to libnlopt.dylib
    
    Add the "desc" tag.
    
    Closes #2618.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- nlopt.rb
+++ nlopt.rb
@@ -1,4 +1,5 @@
 class Nlopt < Formula
+  desc "A free/open-source library for nonlinear optimization"
   homepage "http://ab-initio.mit.edu/nlopt"
   url "http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz"
   sha256 "8099633de9d71cbc06cd435da993eb424bbcdbded8f803cdaa9fb8c6e09c8e89"
@@ -21,7 +22,7 @@ class Nlopt < Formula
     args = [
       "--prefix=#{prefix}",
       "--with-cxx",
-      "--enable-shared",
+      "--enable-shared"
     ]
     args << "--without-octave" if build.without? "octave"
     args << "--without-python" if build.without? "python"
@@ -35,6 +36,11 @@ class Nlopt < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
+
+    # Create lib links for C programs
+    lib.install_symlink lib/"libnlopt_cxx.0.dylib" => lib/"libnlopt.0.dylib"
+    lib.install_symlink lib/"libnlopt_cxx.dylib" => lib/"libnlopt.dylib"
+    lib.install_symlink lib/"libnlopt_cxx.a" => lib/"libnlopt.a"
   end
 
   def caveats
