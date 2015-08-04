7a1356af5ddfc2cdc51353648043e91b392088f2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-04 04:35:51 +0100

    ucto: add 0.8.0 bottle.

--- ucto.rb
+++ ucto.rb
@@ -4,6 +4,12 @@ class Ucto < Formula
   url "http://software.ticc.uvt.nl/ucto-0.8.0.tar.gz"
   sha256 "d3de9886aac95b5eaa48c45a6f8e69e3bb0c350f1d827987e802f4a2ccbcd0bc"
 
+  bottle do
+    sha256 "b6ef7f2f3927c57be81fa609a655a66dbe4d5846655b82b219aa3597429ed85b" => :yosemite
+    sha256 "cb0d3f6beb8b20ea718ef9d6727f07dce46584fab879c5df4619909246c354b5" => :mavericks
+    sha256 "54d366bf282a357f72b2bdc106bd353dc6a071d7697183905b8e803f8feb2c54" => :mountain_lion
+  end
+
   option "without-check", "skip build-time checks (not recommended)"
 
   depends_on "pkg-config" => :build
