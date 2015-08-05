08892ec654e73401739382ea944af1a1eb8ef984
Shaun Jackman <sjackman@gmail.com>
2015-08-05 10:40:27 -0700

    htsbox r311
    Closes #2623.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- htsbox.rb
+++ htsbox.rb
@@ -1,6 +1,14 @@
 class Htsbox < Formula
+  desc "Experimental tools on top of htslib"
   homepage "https://github.com/lh3/htsbox"
+  # tag "bioinformatics"
+
+  url "https://github.com/lh3/htsbox/archive/1b2dee28b52f28afdd14510f551e6aaf658efa8d.tar.gz"
+  version "r311"
+  sha256 "9a1e8bee4527973d1c4f27203a959380ea266cc23ad7d3a51d530815d0ec6ec4"
+
   head "https://github.com/lh3/htsbox.git"
+
   bottle do
     cellar :any
     sha1 "4a0b008515d88edea4307d336da15b9950a6e170" => :yosemite
@@ -8,21 +16,15 @@ class Htsbox < Formula
     sha1 "74cf4e471b03bfb74e52ed92b6a01a159bfc7d56" => :mountain_lion
   end
 
-  #tag "bioinformatics"
-
-  version "r266"
-  url "https://github.com/lh3/htsbox/archive/1559856bb799c88a4c9cf5b01a4cfd8846fb05d7.tar.gz"
-  sha1 "d7c4d1608eced7823fcab8b6e65bc87f2deab4f2"
-
   depends_on "htslib"
 
   def install
-    system "make"
+    system "make", "CC=#{ENV.cc}"
     bin.install "htsbox"
     doc.install "README.md"
   end
 
   test do
-    system "#{bin}/htsbox 2>&1 |grep htsbox"
+    assert_match "Usage", shell_output("#{bin}/htsbox 2>&1", 1)
   end
 end
