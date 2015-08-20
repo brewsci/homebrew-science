f351ed338cb426e69704b6334bd16829dbb910b5
Bede Constantinides <bedeabc@gmail.com>
2015-08-20 13:57:20 +0100

    SPAdes 3.6.0
    Closes #2673.
    Closes #2675.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- spades.rb
+++ spades.rb
@@ -1,26 +1,18 @@
-require 'formula'
-
 class Spades < Formula
+  desc "SPAdes: de novo genome assembly"
   homepage "http://bioinf.spbau.ru/spades/"
-  #tag "bioinformatics"
-  #doi "10.1089/cmb.2012.0021"
+  # tag "bioinformatics"
+  # doi "10.1089/cmb.2012.0021"
 
-  url "http://spades.bioinf.spbau.ru/release3.5.0/SPAdes-3.5.0.tar.gz"
-  sha1 "cca0dde2acb21854e9a87b0ace9ac3e08da55202"
+  url "http://spades.bioinf.spbau.ru/release3.6.0/SPAdes-3.6.0.tar.gz"
+  sha256 "6c04e55d068ad3e5b44c862cb031d41fcd433ab97e5dedadebcc5f785cde13a9"
 
-  bottle do
-    cellar :any
-    sha1 "9a1c20f1088872cc813d160f6bdf9e62b2670818" => :yosemite
-    sha1 "c379f3bfdce510a95fb5fd4b6a5678988c48b77a" => :mavericks
-    sha1 "92f6aba80bc8b969ffb30a5360db77da5be8e860" => :mountain_lion
-  end
-
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
-    mkdir 'src/build' do
-      system 'cmake', '..', *std_cmake_args
-      system 'make', 'install'
+    mkdir "src/build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
     end
 
     # Fix the audit error "Non-executables were installed to bin"
@@ -31,6 +23,5 @@ class Spades < Formula
 
   test do
     system "spades.py", "--test"
-    rm bin/"spades_init.pyc"
   end
 end
