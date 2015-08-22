8d4d6c7287d331ab68cf201d8ba1b0b6062324bc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-22 20:03:41 +0100

    hwloc: update 1.11.0 bottle.

--- hwloc.rb
+++ hwloc.rb
@@ -5,10 +5,10 @@ class Hwloc < Formula
   sha256 "9740f401b564b608c26b21cd06cf30157f0fdd731b0b264db6e8e2d47c4b3721"
 
   bottle do
-    revision 1
-    sha256 "2314fd97617643387585b8ee4bdf85a3320466ab3037f6c550b04dbe07640e8f" => :yosemite
-    sha256 "76e34bac6990a4caaf56c8eddf595573c9128ec6071d5c4fdabb74a8bf5d35ba" => :mavericks
-    sha256 "d940c936bf0da987756ff3b01f425f5bce06718e2ea8aba28d865e44ebad7617" => :mountain_lion
+    cellar :any
+    sha256 "5b11efcb125cf9cceb3a0d2cd5eadd5ce53007f2d86c60c2915830c408a6ec5c" => :yosemite
+    sha256 "8903789215d14430c0ae7e1cad0815491f027767bd6a5cac0ba635105a1b1bc4" => :mavericks
+    sha256 "c3b52dd0b2cca4e88f03fd9e33d09500e969baa6f32e9fe98ea045c939ebe8a5" => :mountain_lion
   end
 
   head do
