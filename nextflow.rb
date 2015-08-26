d83cda9b125632f48cd15b81d0553ba8fc89ccb7
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-08-26 14:33:42 +0200

    nextflow 0.15.3
    Closes #2688.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- nextflow.rb
+++ nextflow.rb
@@ -4,9 +4,9 @@ class Nextflow < Formula
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
-  version "0.15.1"
-  url "http://www.nextflow.io/releases/v0.15.1/nextflow"
-  sha256 "c3671e83d68269724d817aa5b6dede8331587f4c87ec23d8d09cb08fc174c5ca"
+  version "0.15.3"
+  url "http://www.nextflow.io/releases/v0.15.3/nextflow"
+  sha256 "d75ee8f2d17afe76486aac63e8c30905bda5228405a0507aab90724572b489b3"
 
   head "https://github.com/nextflow-io/nextflow.git"
 
@@ -23,11 +23,8 @@ class Nextflow < Formula
     bin.install "nextflow"
   end
 
-  def post_install
-    system "#{bin}/nextflow", "-download"
-  end
-
   test do
-    system "echo \"println 'hello'\" |#{bin}/nextflow -q run - |grep hello"
+    system "#{bin}/nextflow", "-download"
+    system "echo \"println 'hello'\" | #{bin}/nextflow -q run - |grep hello"
   end
 end
