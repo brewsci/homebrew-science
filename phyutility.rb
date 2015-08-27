fce461877d4bb601a1ffa687e9aae3d7c3379713
Jonathan Chang <jchang641@gmail.com>
2015-08-26 18:49:23 -0700

    phyutility 2.7.1 (new formula)
    Phyutility is a command line program that performs simple analyses or modifications on both phylogenetic trees and data matrices.
    
    Closes #2689.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

---
+++ phyutility.rb
@@ -0,0 +1,26 @@
+class Phyutility < Formula
+  desc "analyze and modify trees and data matrices"
+  homepage "http://blackrim.org/programs/phyutility/"
+  url "https://github.com/blackrim/phyutility/releases/download/v2.7.1/phyutility_2.7.1.tar.gz"
+  sha256 "3438336abda593cf3043d49910815dc8b8e506e9e44831726407f37a1a7506bc"
+  # doi "10.1093/bioinformatics/btm619"
+  # tag "bioinformatics'
+
+  def install
+    libexec.install "phyutility.jar"
+    bin.write_jar_script libexec/"phyutility.jar", "phyutility"
+    pkgshare.install "examples", "manual.pdf"
+  end
+
+  def caveats; <<-EOS.undent
+    The manual and examples are in:
+      #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    cp Dir[pkgshare/"examples/*"], testpath
+    system *%W[#{bin}/phyutility -concat -in test.aln test2.aln -out test_new.aln]
+    compare_file "test_new.aln", "test_all.aln"
+  end
+end
