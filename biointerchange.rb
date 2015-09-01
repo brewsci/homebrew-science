42cb189f3a726e91e0a9fd738ae0d2a91ebb5b8a
Tim D. Smith <git@tim-smith.us>
2015-08-31 21:40:19 -0700

    biointerchange 2.0.3+100
    Closes #2685.

--- biointerchange.rb
+++ biointerchange.rb
@@ -1,24 +1,16 @@
 class Biointerchange < Formula
   desc "Scaling and integrating genomics data."
   homepage "https://www.codamono.com"
-
   # tag "bioinformatics"
 
   if OS.mac?
-    version "2.0.2+83"
-    url "https://www.codamono.com/brew/osx-10-10/biointerchange-2.0.2+83.tar.gz"
-    sha256 "5559366bda2141fef951b770ec975d493871ab120173861f80a24734caaee617"
+    version "2.0.3+100"
+    url "https://www.codamono.com/brew/osx-10-10/biointerchange-2.0.3+100.tar.gz"
+    sha256 "0bc61490b086b40c56132c4e405c7bf5e9fca28782bb0ee73cd88f4563896b32"
   elsif OS.linux?
-    version "2.0.2+84"
-    url "https://www.codamono.com/brew/debian-8-amd64/biointerchange-2.0.2+84.tar.gz"
-    sha256 "05a317da8c11817b9a4ad6288daabb43d7ca8403f6636dc3037bfc70b83482f9"
-  end
-
-  bottle do
-    cellar :any
-    sha256 "f99e547e7b61db910a21ba5a7d7b0cb4c9691cddda7107cf413b63afa2190488" => :yosemite
-    sha256 "0a2d558c1b3c3be8b823d988c14ab74125f13cb61fd13ca1f96d0aa118ed3ff4" => :mavericks
-    sha256 "3153e9ad2002e07f47749f238eaa43a928104bc832afe9e27a4e7ca2e2440f5d" => :mountain_lion
+    version "2.0.3+100"
+    url "https://www.codamono.com/brew/debian-8-amd64/biointerchange-2.0.3+100.tar.gz"
+    sha256 "4f1687adfb1f44dd094edd6a1f451cd6c25fd9f82f84b5457b4c0a3be43b5e5e"
   end
 
   def install
