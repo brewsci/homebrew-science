f2de8d1a7038b8052e0b932695b7e89288273873
roryk <rory.kirchner@gmail.com>
2015-09-02 09:27:58 -0400

    sailfish 0.7.5
    Build on this version is cleaner as it will use pre-installed
    dependencies for more libraries rather than trying to build everything
    itself.
    
    Closes #2706.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sailfish.rb
+++ sailfish.rb
@@ -1,24 +1,17 @@
 class Sailfish < Formula
+  desc "Rapid Mapping-based Isoform Quantification from RNA-Seq Reads"
   homepage "http://www.cs.cmu.edu/~ckingsf/software/sailfish"
   # doi "10.1038/nbt.2862"
   # tag "bioinformatics"
-  url "https://github.com/kingsfordgroup/sailfish/archive/v0.6.3.tar.gz"
-  sha256 "5fded7dc88e525d17eb7abe102ac8ce8d8c3c8e9997d8aa1c512dddbef813632"
+  url "https://github.com/kingsfordgroup/sailfish/archive/v0.7.5.tar.gz"
+  sha256 "74f589f6dfa8d9316a9a77bb8ad9c2962f08e3a285deedc134893f6f7b1d1b18"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "boost"
   depends_on "cmake" => :build
-  depends_on "boost" => :recommended
-  depends_on "tbb" => :recommended
-
-  keg_only "sailfish conflicts with jellyfish."
-
-  fails_with :clang do
-    cause "Currently, the only supported compiler is GCC(>=4.7). We hope to support Clang soon."
-  end
-
-  fails_with :gcc do
-    build 5666
-    cause "Sailfish requires g++ 4.7 or greater."
-  end
+  depends_on "tbb"
+  needs :cxx11
 
   def install
     ENV.deparallelize
