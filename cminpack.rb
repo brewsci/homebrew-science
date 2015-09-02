fe0acfc918e26ac21376cf45666e41cb62642a48
Tim D. Smith <git@tim-smith.us>
2015-09-01 20:55:34 -0700

    cminpack: fix out-of-prefix install
    Applies devernay/cminpack#4.
    
    Fixes #2701.

--- cminpack.rb
+++ cminpack.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Cminpack < Formula
-  homepage 'http://devernay.free.fr/hacks/cminpack/cminpack.html'
-  url 'http://devernay.free.fr/hacks/cminpack/cminpack-1.3.4.tar.gz'
-  sha1 '58519c93e9d4e28a17eabb551e4c23b3d39e064d'
+  homepage "http://devernay.free.fr/hacks/cminpack/cminpack.html"
+  url "http://devernay.free.fr/hacks/cminpack/cminpack-1.3.4.tar.gz"
+  sha256 "3b517bf7dca68cc9a882883db96dac0a0d37d72aba6dfb0c9c7e78e67af503ca"
+  head "https://github.com/devernay/cminpack.git"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
+    # https://github.com/devernay/cminpack/pull/4
+    # https://github.com/Homebrew/homebrew-science/issues/2696
+    inreplace "cmake/CMakeLists.txt", "${CMAKE_ROOT}", "share/cmake"
+
     system "cmake", ".", *std_cmake_args
-    system "make install"
+    system "make", "install"
   end
 end
