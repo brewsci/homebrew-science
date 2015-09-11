e5d02b569de99865cd9d93ea3a0273c7cca8d294
Tim D. Smith <git@tim-smith.us>
2015-09-10 23:36:58 -0700

    opencv: test Python module
    Closes #2725. Closes #2724.

--- opencv.rb
+++ opencv.rb
@@ -152,5 +152,7 @@ class Opencv < Formula
     EOS
     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
     assert_equal `./test`.strip, version.to_s
+
+    assert_match version.to_s, shell_output("python -c 'import cv2; print(cv2.__version__)'")
   end
 end
