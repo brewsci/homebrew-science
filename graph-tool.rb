84c8b8d4ccc3837e276abdedce11139c3572856e
Tiago de Paula Peixoto <tiago@skewed.de>
2015-09-17 23:45:13 +0200

    Bump graph-tool version to 2.7
    Closes #2748.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- graph-tool.rb
+++ graph-tool.rb
@@ -1,7 +1,7 @@
 class GraphTool < Formula
   homepage "http://graph-tool.skewed.de/"
-  url "http://downloads.skewed.de/graph-tool/graph-tool-2.2.44.tar.bz2"
-  sha256 "42b97c24c155ddd95c79a16b4e3cb034cb3139e912c37aed66e6493e4ff20566"
+  url "http://downloads.skewed.de/graph-tool/graph-tool-2.7.tar.bz2"
+  sha256 "c71ab0056c27d0d5b4f92d58d8d3ad019ef535da822b54898459a09b119449e1"
 
   bottle do
     sha256 "8d9ec4a8eff050f3eb65c66318e1a74d9ba2e68185f393f59a5577bb6e2325b0" => :yosemite
