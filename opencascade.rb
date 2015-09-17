d72d28d61c12246cc4694fa95a6fa4a3cdfa2f35
Denis Davydov <davydden@gmail.com>
2015-09-17 19:59:29 +0200

    Opencascade 6.8.0 (fix)
    fix installation on linux.
    
    Closes #2747.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- opencascade.rb
+++ opencascade.rb
@@ -1,7 +1,8 @@
 class Opencascade < Formula
+  desc "Environment for 3D modeling and development of numerical simulation software including CAD/CAM/CAE"
   homepage "http://www.opencascade.org/"
   url "http://files.opencascade.com/OCCT/OCC_6.8.0_release/opencascade-6.8.0.tgz"
-  sha1 "fe359a12e110e9136adac2db1539026be6cc579e"
+  sha256 "50b145655fd3a4448e291388650cd0dcab8f9d99201aba32e8d26aeff03e8e1c"
 
   bottle do
     cellar :any
@@ -13,7 +14,7 @@ class Opencascade < Formula
 
   conflicts_with "oce", :because => "OCE is a fork for patches/improvements/experiments over OpenCascade"
 
-  option "without-opencl", "Build without OpenCL support"
+  option "without-opencl", "Build without OpenCL support" if OS.mac?
   option "without-extras", "Don't install documentation (~725MB) or samples (~40MB)"
   option "with-tests", "Install tests (~55MB)"
 
@@ -22,26 +23,27 @@ class Opencascade < Formula
   depends_on "qt"
   depends_on "freeimage" => :recommended
   depends_on "gl2ps" => :recommended
-  depends_on "tbb" => :recommended
+  depends_on "tbb" => :recommended if OS.mac? # Couldn't make it find TBB...
   depends_on :macos => :snow_leopard
 
   def install
     # setting DYLD causes many issues; all tests work fine without; suppress
-    inreplace "env.sh", "export DYLD_LIBRARY_PATH", "export OCCT_DYLD_LIBRARY_PATH"
+    inreplace "env.sh", "export DYLD_LIBRARY_PATH", "export OCCT_DYLD_LIBRARY_PATH" if OS.mac?
 
     cmake_args = std_cmake_args
     cmake_args << "-DCMAKE_PREFIX_PATH:PATH=#{HOMEBREW_PREFIX}"
     cmake_args << "-DCMAKE_INCLUDE_PATH:PATH=#{HOMEBREW_PREFIX}/lib"
-    cmake_args << "-DCMAKE_FRAMEWORK_PATH:PATH=#{HOMEBREW_PREFIX}/Frameworks"
+    cmake_args << "-DCMAKE_FRAMEWORK_PATH:PATH=#{HOMEBREW_PREFIX}/Frameworks" if OS.mac?
     cmake_args << "-DINSTALL_DIR:PATH=#{prefix}"
     cmake_args << "-D3RDPARTY_DIR:PATH=#{HOMEBREW_PREFIX}"
     cmake_args << "-D3RDPARTY_TCL_DIR:PATH=/usr"
+    cmake_args << "-D3RDPARTY_TCL_INCLUDE_DIR:PATH=/usr/include/tcl8.6" unless OS.mac?
     cmake_args << "-D3RDPARTY_TK_INCLUDE_DIR:PATH=/usr/include"
     cmake_args << "-DINSTALL_TESTS:BOOL=ON" if build.with? "tests"
     cmake_args << "-D3RDPARTY_TBB_DIR:PATH=#{HOMEBREW_PREFIX}" if build.with? "tbb"
 
     # must specify, otherwise finds old ft2config.h in /usr/X11R6
-    cmake_args << "-D3RDPARTY_FREETYPE_INCLUDE_DIR:PATH=#{HOMEBREW_PREFIX}/include/freetype2"
+    cmake_args << "-D3RDPARTY_FREETYPE_INCLUDE_DIR:PATH=#{HOMEBREW_PREFIX}/include/freetype2" if OS.mac?
 
     %w[freeimage gl2ps opencl tbb].each do |feature|
       cmake_args << "-DUSE_#{feature.upcase}:BOOL=ON" if build.with? feature
@@ -70,7 +72,7 @@ class Opencascade < Formula
     end
 
     # add symlinks to be able to compile against OpenCascade
-    loc = "#{prefix}/mac64/clang"
+    loc = OS.mac? ? "#{prefix}/mac64/clang" : "#{prefix}/lin64/gcc"
     include.install_symlink Dir["#{prefix}/inc/*"]
     bin.install_symlink Dir["#{loc}/bin/*"]
     lib.install_symlink Dir["#{loc}/lib/*"]
@@ -79,6 +81,9 @@ class Opencascade < Formula
   def caveats; <<-EOF.undent
     Some apps will require this enviroment variable:
       CASROOT=#{opt_prefix}
+
+    On Linux make sure the following libraries are installed:
+      sudo apt-get install libgl2ps-dev tcl8.6-dev tk8.6-dev libgl1-mesa-dev libglu1-mesa-dev libxmu-dev libxext-dev
     EOF
   end
 
