7ede36ca9f264116ad8e4cdcdba00a9f09c2af35
Denis Davydov <davydden@gmail.com>
2015-09-17 10:06:00 +0200

    Scotch 6.0.4 (fix)
    fix for linuxbrew.
    
    Closes #2745.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- scotch.rb
+++ scotch.rb
@@ -1,8 +1,9 @@
 class Scotch < Formula
-  desc "A software package for graph and mesh/hypergraph partitioning, graph clustering, and sparse matrix ordering"
+  desc "Software package for graph and mesh/hypergraph partitioning, graph clustering, and sparse matrix ordering"
   homepage "https://gforge.inria.fr/projects/scotch"
   url "https://gforge.inria.fr/frs/download.php/file/34618/scotch_6.0.4.tar.gz"
   sha256 "f53f4d71a8345ba15e2dd4e102a35fd83915abf50ea73e1bf6efe1bc2b4220c7"
+  revision 1
 
   bottle do
     cellar :any
@@ -36,12 +37,19 @@ class Scotch < Formula
       make_args = ["CCS=#{ENV["CC"]}",
                    "CCP=#{ENV["MPICC"]}",
                    "CCD=#{ENV["MPICC"]}",
-                   "LIB=.dylib",
-                   "AR=libtool",
-                   "ARFLAGS=-dynamic -install_name #{lib}/$(notdir $@) -undefined dynamic_lookup -o ",
                    "RANLIB=echo",
                    "CFLAGS=#{cflags.join(" ")}",
                    "LDFLAGS=#{ldflags.join(" ")}"]
+
+      if OS.mac?
+        make_args << "LIB=.dylib"
+        make_args << "AR=libtool"
+        make_args << "ARFLAGS=-dynamic -install_name #{lib}/$(notdir $@) -undefined dynamic_lookup -o "
+      else
+       make_args << "LIB=.so"
+       make_args << "ARCH=ar"
+       make_args << "ARCHFLAGS=-ruv"
+      end
 
       system "make", "scotch", "VERBOSE=ON", *make_args
       system "make", "ptscotch", "VERBOSE=ON", *make_args
