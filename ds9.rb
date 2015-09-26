0699e915f2e50d30098a3b7d17ee346c884662fd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-26 03:52:26 +0100

    ds9: add 7.3.2_1 bottle.

--- ds9.rb
+++ ds9.rb
@@ -6,6 +6,13 @@ class Ds9 < Formula
   sha256 "05d581780f41d02799777c5a2095ea6e74dc70bd80175c96babe241c23d0145f"
   revision 1
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "40e75509fb3fb53f2d3a391f6b1df3971ce0878d2625a2680a053ec1e7ca3edd" => :el_capitan
+    sha256 "a35573fb5f27b6e914711f1d98e7d616ebc2e27242beece1341c2d9c2f0ebca8" => :yosemite
+    sha256 "211226f72b21eef4059b09fba5072adca9fe1a3cc62e5f38730134a707edfd92" => :mavericks
+  end
+
   depends_on :macos => :lion
   depends_on :x11
 
