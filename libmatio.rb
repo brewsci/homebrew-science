e9b07c6768eeda23c27b339295ddd39803e6abde
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-02 20:49:02 +0100

    libmatio: add 1.5.2_1 bottle.

--- libmatio.rb
+++ libmatio.rb
@@ -6,6 +6,13 @@ class Libmatio < Formula
   sha1 'd5a83a51eb2550d75811d2dde967ef3e167d4f52'
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "fc4f887587a4faf0efa6093e0c38cd172b8a9f3bb44e062d166ebdb34c44335c" => :el_capitan
+    sha256 "92c4859f40adb08e7f98766a55c25daf5b4bffc000c9aa51ede93c2b46ac39c2" => :yosemite
+    sha256 "ed278812215d9647a7451de2694995f6f028f27197223d195a677f1136c6d4f4" => :mavericks
+  end
+
   option :universal
   option 'with-hdf5', 'Enable support for newer MAT files that use the HDF5-format'
 
