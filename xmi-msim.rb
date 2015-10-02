2dd424e222e98c79d794d4f6b188bd9b3c938349
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-02 14:48:42 +0100

    xmi-msim: update 5.0_2 bottle.

--- xmi-msim.rb
+++ xmi-msim.rb
@@ -6,9 +6,8 @@ class XmiMsim < Formula
   revision 2
 
   bottle do
-    sha256 "20d513767c84fdce79035995a1c944baa502c8902d9ada3e5936b21b7634b2c2" => :yosemite
-    sha256 "256f4aacfda4a9ce1e86a6c568c9f8fee0fe6e88ed93b18ae1ae86356fe5a83d" => :mavericks
-    sha256 "1761c9e032b189f30309a6afdb46f4883a3e06e49818a5a8164a7b098d6babf9" => :mountain_lion
+    sha256 "2802b089114e7c2b83385e927cee7d078b5cbac8792a91dd0ae27533b85a2fd2" => :yosemite
+    sha256 "8c04f8cb8bc1d70c2b1dd780fff8c551d9f75bb21cada545bc6f710242fd5278" => :mavericks
   end
 
   depends_on :fortran
