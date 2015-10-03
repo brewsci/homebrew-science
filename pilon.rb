5f06138a6e0e884c3703dc33cf7dcc15be33df45
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-03 02:13:55 +0100

    pilon: add 1.13 bottle.

--- pilon.rb
+++ pilon.rb
@@ -1,6 +1,13 @@
 class Pilon < Formula
   desc "Improve draft assemblies and find variation"
   homepage "https://github.com/broadinstitute/pilon/wiki"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ce33601bd3a361ecba9df239a5488fd8b173d6a2411ee04ff5f18ae14db28ad0" => :el_capitan
+    sha256 "68467efec02028a8dfdbf47c5d4fa69fd27303c97cd87f104f7028f16b68206b" => :yosemite
+    sha256 "29d41de34aaede7601b9238deb672417e26733e3810cd042fe7b4fa6bcd8a964" => :mavericks
+  end
+
   # tag "bioinformatics"
   # doi "10.1371/journal.pone.0112963"
 
