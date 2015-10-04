eb65f514c71cc9808b3131c422b509b6514f146f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-04 02:09:25 +0100

    spici: add 20130427 bottle.

--- spici.rb
+++ spici.rb
@@ -1,6 +1,13 @@
 class Spici < Formula
   desc "Fast local network clustering algorithm"
   homepage "http://compbio.cs.princeton.edu/spici/"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6adede55492eaf5a1f1dc54c9ac2834c0ded0dfd7112df19105101e1009de01f" => :el_capitan
+    sha256 "8bd7d83bfa2281feb1015797a6e6d09ee8aa48e0881d03d7ab72da5ad2302ce6" => :yosemite
+    sha256 "d32af840e1c5103c15e4e2659e3a0ac12c3ce4b57331fcb70f3499e72321d371" => :mavericks
+  end
+
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btq078"
 
