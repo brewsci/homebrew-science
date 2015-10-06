ad167e3d347ba5599beb138eddccbb2c75e684e8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-06 16:16:51 +0100

    sailfish: add 0.7.6 bottle.

--- sailfish.rb
+++ sailfish.rb
@@ -2,6 +2,12 @@ class Sailfish < Formula
   desc "Rapid Mapping-based Isoform Quantification from RNA-Seq Reads"
   homepage "http://www.cs.cmu.edu/~ckingsf/software/sailfish"
 
+  bottle do
+    sha256 "1aa9d7ffa390bdb706a23564d39e288fde5adaf2120505553422719ac22cced9" => :el_capitan
+    sha256 "3beba76b6e28b9a21a2e0a6f690891111bb3ae88585cac037883c25ef90fea38" => :yosemite
+    sha256 "8a9fcdcbd46bf0e6d5d6bf9224f142b1d21712fc1fba32b85e0473d139e5a850" => :mavericks
+  end
+
   # doi "10.1038/nbt.2862"
   # tag "bioinformatics"
   url "https://github.com/kingsfordgroup/sailfish/archive/v0.7.6.tar.gz"
