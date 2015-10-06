c83bbcfbbf896a3704ffcd980dd6ead5a31359a3
Kyungdahm Yun <tomyun@gmail.com>
2015-10-05 21:16:21 -0700

    libsigrokdecode 0.3.1
    Closes #2833.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- libsigrokdecode.rb
+++ libsigrokdecode.rb
@@ -1,30 +1,36 @@
 class Libsigrokdecode < Formula
-  url "http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.3.0.tar.gz"
+  desc "Library for (streaming) protocol decoding"
   homepage "http://sigrok.org/"
-  head "git://sigrok.org/libsigrokdecode", :shallow => false
-  sha1 "a75f2839cf62d965281bac22919e761c5210e32e"
-  revision 1
 
-  depends_on "pkg-config" => :build
-  depends_on "glib"
-  depends_on "libzip"
-  depends_on :python3
+  stable do
+    url "http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.3.1.tar.gz"
+    sha256 "2c50efe16c2424b77ab0d9ae6b5d98d7c9894407ddb43dfb43846b3bdef5b5d1"
 
-  depends_on "check"    => :optional
-  depends_on "libftdi0" => :optional
-  depends_on "libusb"   => :recommended
+    resource "librevisa" do
+      url "http://www.librevisa.org/git/librevisa.git", :tag => "alpha-2013-08-12",
+                                                        :revision => "3e3e027ac7bcf2679089adc8886bb3c92574a042"
+    end
 
-  option "with-libserialport", "Build with libserialport"
-  option "with-librevisa", "Build with librevisa"
+    resource "libserialport" do
+      url "http://sigrok.org/download/source/libserialport/libserialport-0.1.0.tar.gz"
+      sha256 "ec905bd64bd8b82234b68a5eded5fd79b67704fe0cd73bf092666b9679a319af"
+    end
 
-  # currently needed for librevisa due to errors in released alpha build
-  if build.with?("librevisa")
-    depends_on "libtool" => :build
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+    resource "libsigrok" do
+      url "http://sigrok.org/download/source/libsigrok/libsigrok-0.3.0.tar.gz"
+      sha256 "43926907a06f1d7aa73c68ae379d66412ac2728483eed7d20a8cf061f73f7050"
+    end
+  end
+
+  bottle do
+    sha256 "d1b4a60ea7394ef0a4122e284f801bf2d39bbd0106518e39b7d9683e073a48f2" => :yosemite
+    sha256 "56cb21acc02072ba1995f96a90dee82af86458cd53b7d0bb04f4747ccffc1892" => :mavericks
+    sha256 "832c21361874d493b5a1ebed85b13b931b027f9b4e7b78414f94e9eefa9e4b98" => :mountain_lion
   end
 
   head do
+    url "git://sigrok.org/libsigrokdecode", :shallow => false
+
     depends_on "libtool" => :build
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -43,34 +49,30 @@ class Libsigrokdecode < Formula
     end
   end
 
-  stable do
-    resource "librevisa" do
-      url "http://www.librevisa.org/git/librevisa.git", :tag => "alpha-2013-08-12"
-      sha1 "1bb24a6721cc994494f294fd86c78594ba4f8b40"
-    end
+  option "with-libserialport", "Build with libserialport"
+  option "with-librevisa", "Build with librevisa"
 
-    resource "libserialport" do
-      url "http://sigrok.org/download/source/libserialport/libserialport-0.1.0.tar.gz"
-      sha1 "f8677c9e63caf1f6e6cb6aa39a8ae3a256516d78"
-    end
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libzip"
+  depends_on :python3
 
-    resource "libsigrok" do
-      url "http://sigrok.org/download/source/libsigrok/libsigrok-0.3.0.tar.gz"
-      sha1 "140db550820f98042d95432a87a9d7e3078b1490"
-    end
+  depends_on "check"    => :optional
+  depends_on "libftdi0" => :optional
+  depends_on "libusb"   => :recommended
+
+  # currently needed for librevisa due to errors in released alpha build
+  if build.with?("librevisa")
+    depends_on "libtool" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
   end
-  bottle do
-    sha256 "d1b4a60ea7394ef0a4122e284f801bf2d39bbd0106518e39b7d9683e073a48f2" => :yosemite
-    sha256 "56cb21acc02072ba1995f96a90dee82af86458cd53b7d0bb04f4747ccffc1892" => :mavericks
-    sha256 "832c21361874d493b5a1ebed85b13b931b027f9b4e7b78414f94e9eefa9e4b98" => :mountain_lion
-  end
-
 
   def install
-    common_args = %W(
+    common_args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-    )
+    ]
 
     ENV.delete "PYTHONPATH"
     ENV.append_path "PKG_CONFIG_PATH", lib / "pkgconfig"
