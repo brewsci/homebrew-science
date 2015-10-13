f4b888a0538f2decd30e2483d685502d71ab1a79
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-13 22:46:05 +0100

    sortmerna: add 2.0 bottle.

--- sortmerna.rb
+++ sortmerna.rb
@@ -9,6 +9,13 @@ class Sortmerna < Formula
 
   head "https://github.com/biocore/sortmerna.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ee75300a501ab2c6f376697830940b68ef6ee22fd86f5483b8b49331f30bd842" => :el_capitan
+    sha256 "b41384be8f1b5dc2c15f3f656cd1ccbe30d743e0ca5a7ca05e9a1964eeddcc31" => :yosemite
+    sha256 "561d4e98f7f1804fd18940c50490c46a6b70b61e10208778b6b4cbd9aa7767c2" => :mavericks
+  end
+
   def install
     system "./configure",
       "--disable-debug",
