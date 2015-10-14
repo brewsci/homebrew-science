0c8a16f106de3836633d0c4d2c98a4753572d00e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 23:11:29 +0100

    astral: update 4.7.8 bottle.

--- astral.rb
+++ astral.rb
@@ -1,13 +1,6 @@
 class Astral < Formula
   desc "Estimate species tree given set of unrooted gene trees"
   homepage "https://github.com/smirarab/ASTRAL"
-  bottle do
-    cellar :any
-    sha256 "fd942a7f95f5af65cf37b664711b0c163709b344c600e9b54549fe3ed34aab8d" => :yosemite
-    sha256 "fd942a7f95f5af65cf37b664711b0c163709b344c600e9b54549fe3ed34aab8d" => :mavericks
-    sha256 "9abe785b08c07bfbb746cd5f3a78c2f3f4b6071fb0698f4e80cc4ae30e94a879" => :mountain_lion
-  end
-
   # doi "10.1093/bioinformatics/btu462"
   # tag "bioinformatics"
 
@@ -15,6 +8,14 @@ class Astral < Formula
   sha256 "5fb6eb0f899d29f2c23aefdd529a2fd2c3d069bb13fed87e381aeb48650cd4c9"
 
   # head "https://github.com/smirarab/ASTRAL.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "3df52b0460d1f9ae3a0a00007a89615e4f540e94b60a6d27aa6fea9b00104ca3" => :el_capitan
+    sha256 "947ddf4a3754661849cf712b18e547cde215198d81c92137927cdbaa8c6e1de0" => :yosemite
+    sha256 "18611c4aeb688d563222163ea42b4e59b4ed31c763c0dfdaa54b1b22bb824aef" => :mavericks
+  end
 
   depends_on :java
 
