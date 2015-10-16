9597fb26c2f22e751522a58903f42d8513de1b60
Alex Stewart <alexs.mac@gmail.com>
2015-10-16 14:46:28 +0100

    Update ceres-solver to 1.11 release.
    Closes #2903.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- ceres-solver.rb
+++ ceres-solver.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class CeresSolver < Formula
-  homepage 'http://ceres-solver.org/'
-  url 'http://ceres-solver.org/ceres-solver-1.11.0.tar.gz'
-  sha1 '5e8683bfb410b1ba8b8204eeb0ec1fba009fb2d0'
-  head 'https://ceres-solver.googlesource.com/ceres-solver.git'
+  desc "C++ library for modeling and solving large, complicated optimization problems."
+  homepage "http://ceres-solver.org/"
+  url "http://ceres-solver.org/ceres-solver-1.11.0.tar.gz"
+  sha256 "4d666cc33296b4c5cd77bad18ffc487b3223d4bbb7d1dfb342ed9a87dc9af844"
+  head "https://ceres-solver.googlesource.com/ceres-solver.git"
 
   bottle do
     sha1 "f63f940e4d950fde0c231e0ea431bd2a0834d1d8" => :yosemite
@@ -12,25 +11,25 @@ class CeresSolver < Formula
     sha1 "04b4f8954e4dcc4284138cdaf5837cf4575a4c20" => :mountain_lion
   end
 
-  option 'without-tests', 'Do not build and run the tests (not recommended).'
+  option "without-tests", "Do not build and run the tests (not recommended)."
 
-  depends_on 'cmake' => :build
-  depends_on 'glog'
-  depends_on 'gflags'
-  depends_on 'eigen'
-  depends_on 'suite-sparse' => :recommended
+  depends_on "cmake" => :build
+  depends_on "glog"
+  depends_on "gflags"
+  depends_on "eigen"
+  depends_on "suite-sparse" => :recommended
 
   def suite_sparse_options
     Tab.for_formula(Formula["suite-sparse"])
   end
 
   def install
-    cmake_args = std_cmake_args + ['-DBUILD_SHARED_LIBS=ON']
+    cmake_args = std_cmake_args + ["-DBUILD_SHARED_LIBS=ON"]
     cmake_args << "-DMETIS_LIBRARY=#{Formula["metis4"].opt_lib}/libmetis.dylib" if suite_sparse_options.with? "metis4"
     cmake_args << "-DEIGEN_INCLUDE_DIR=#{Formula["eigen"].opt_include}/eigen3"
     system "cmake", ".", *cmake_args
     system "make"
-    system "make test" if build.with? 'tests'
+    system "make test" if build.with? "tests"
     system "make", "install"
   end
 end
