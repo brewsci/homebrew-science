a83f471af7cdaf19e20500c0a9bc4dc20e472412
Dominique Orban <dominique.orban@gmail.com>
2015-10-18 20:23:27 -0400

    qr_mumps: cleaner shared lib and test.
    Closes #2910.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- qr_mumps.rb
+++ qr_mumps.rb
@@ -3,7 +3,7 @@ class QrMumps < Formula
   homepage "http://buttari.perso.enseeiht.fr/qr_mumps"
   url "http://buttari.perso.enseeiht.fr/qr_mumps/releases/1.0/qr_mumps-1.0.tgz"
   sha256 "69bfcb2f5718480c5dec88cc4241c57fec15b44eac53c2e14542f4838f375049"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
@@ -19,28 +19,22 @@ class QrMumps < Formula
 
   depends_on "metis4" => :recommended
   depends_on "scotch5" => :optional
-  depends_on "openblas" => :optional
-  depends_on "veclibfort" if build.without? "openblas"
+  depends_on "openblas" => ((OS.mac?) ? :optional : :recommended)
+  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
 
   needs :openmp
 
   def make_shared(l, extra)
-    so = (OS.mac?) ? "dylib" : "so"
-    mkdir "#{l}_shared" do
-      system "ar", "-x", "../#{l}.a"
-      ofiles = Dir["*.o"]
-      if OS.mac?
-        system "#{ENV["FC"]}", "-dynamiclib",
-                               "-undefined", "dynamic_lookup",
-                               "-lgomp",
-                               "-o", "../#{l}.#{so}", *extra, *ofiles
-      else
-        system "#{ENV["FC"]}", "-shared",
-                               "-lgomp",
-                                "-Wl,-soname", "-Wl,#{lib}/#{l}",
-                                "-o", "../#{l}.#{so}", *extra, *ofiles
-      end
+    if OS.mac?
+      so = "dylib"
+      all_load = "-Wl,-all_load"
+      noall_load = "-Wl,-noall_load" # gives a warning but gfortran doesn't want this empty.
+    else
+      so = "so"
+      all_load = "-Wl,-whole-archive"
+      noall_load = "-Wl,-no-whole-archive"
     end
+    system ENV["FC"], "-fPIC", "-shared", all_load, "#{l}.a", noall_load, "-o", "#{l}.#{so}", "-lgomp", *extra
   end
 
   def install
@@ -48,18 +42,23 @@ class QrMumps < Formula
     cp "makeincs/Make.inc.gnu", "Make.inc"
     topdir = ["TOPDIR=#{pwd}"]
     make_args = ["CC=#{ENV["CC"]} -fPIC", "FC=#{ENV["FC"]} -fPIC"]
+    libs = []
     if build.with? "metis4"
+      libs << "-L#{Formula["metis4"].opt_lib}" << "-lmetis"
       make_args << "LMETIS=-L#{Formula["metis4"].opt_lib} -lmetis"
       make_args << "IMETIS=-I#{Formula["metis4"].opt_include}"
     end
     if build.with? "scotch5"
+      libs << "-L#{Formula["scotch5"].opt_lib}" << "-lscotch" << "-lscotcherr"
       make_args << "LSCOTCH=-L#{Formula["scotch5"].opt_lib} -lscotch -lscotcherr"
       make_args << "ISCOTCH=-I#{Formula["scotch5"].opt_include}"
     end
     if build.with? "openblas"
+      libs << "-L#{Formula["openblas"].opt_lib}" << "-lopenblas"
       make_args << "LBLAS=-L#{Formula["openblas"].opt_lib} -lopenblas"
       make_args << "LLAPACK=-L#{Formula["openblas"].opt_lib} -lopenblas"
     else
+      libs << "-lveclibfort"
       make_args << "LBLAS=-lveclibfort" << "LLAPACK=-lveclibfort"
     end
 
@@ -75,9 +74,9 @@ class QrMumps < Formula
 
     # Build shared libraries.
     cd "lib" do
-      make_shared "libqrm_common", []
+      make_shared "libqrm_common", libs
       %w[libsqrm libdqrm libcqrm libzqrm].each do |l|
-        make_shared l, ["-L..", "-lqrm_common"]
+        make_shared l, (["-L.", "-lqrm_common"] + libs)
       end
     end
 
@@ -99,5 +98,67 @@ class QrMumps < Formula
     Fortran modules were installed to
       "#{libexec}/modules"
     EOS
+  end
+
+  test do
+    ENV.fortran
+    (testpath/"test.f90").write <<-EOS
+      program _qrm_test
+        use _qrm_mod
+        implicit none
+
+        type(_qrm_spmat_type)          :: qrm_mat
+        _qrm_data, allocatable, target :: b(:), x(:), r(:)
+        _qrm_real                      :: rnrm, onrm
+
+        call qrm_spmat_init(qrm_mat)
+        call qrm_set('qrm_eunit', 6)
+
+        call qrm_palloc(qrm_mat%irn, 13)
+        call qrm_palloc(qrm_mat%jcn, 13)
+        call qrm_palloc(qrm_mat%val, 13)
+
+        qrm_mat%jcn = (/1,1,1,2,2,3,3,3,3,4,4,5,5/)
+        qrm_mat%irn = (/2,3,6,1,6,2,4,5,7,2,3,2,4/)
+        qrm_mat%val = (/0.7,0.6,0.4,0.1,0.1,0.3,0.6,0.7,0.2,0.5,0.2,0.1,0.6/)
+        qrm_mat%m   = 7
+        qrm_mat%n   = 5
+        qrm_mat%nz  = 13
+
+        call qrm_set(qrm_mat, 'qrm_ib', 2)
+        call qrm_set(qrm_mat, 'qrm_nb', 2)
+        call qrm_set(qrm_mat, 'qrm_nthreads', 1)
+        call qrm_set(qrm_mat, 'qrm_ordering', qrm_natural_)
+
+        call qrm_analyse(qrm_mat, 'n')
+        call qrm_factorize(qrm_mat, 'n')
+
+        call qrm_aalloc(b, qrm_mat%m)
+        call qrm_aalloc(r, qrm_mat%m)
+        call qrm_aalloc(x, qrm_mat%n)
+
+        b = _qrm_one
+        r = b
+
+        call qrm_apply(qrm_mat, 't', b)
+        call qrm_solve(qrm_mat, 'n', b, x)
+
+        call qrm_residual_norm(qrm_mat, r, x, rnrm)
+        call qrm_residual_orth(qrm_mat, r, onrm)
+        write(*, '("||r||/||A||    = ", e10.2)') rnrm
+        write(*, '("||A^Tr||/||r|| = ", e10.2)') onrm
+
+        call qrm_adealloc(b)
+        call qrm_adealloc(r)
+        call qrm_adealloc(x)
+        call qrm_spmat_destroy(qrm_mat, all=.true.)
+      end program _qrm_test
+    EOS
+
+    %w[s d c z].each do |p|
+      system "perl #{pkgshare}/examples/#{p}.pl < test.f90 > #{p}test.f90"
+      system ENV["FC"], "#{p}test.f90", "-o", "#{p}test", "-I#{opt_libexec}/modules", "-L#{opt_lib}", "-lqrm_common", "-l#{p}qrm", "-lgomp"
+      system "./#{p}test"
+    end
   end
 end
