387ecb20ff754f8911959aa0620a4f482a67bdb4
Thierry Moisan <thierry.moisan@gmail.com>
2015-10-25 15:03:41 -0400

    New formula for libminc
    Closes #2932.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

---
+++ libminc.rb
@@ -0,0 +1,34 @@
+class Libminc < Formula
+  desc "Medical Imaging file format and Toolbox"
+  homepage "https://en.wikibooks.org/wiki/MINC"
+  url "https://github.com/BIC-MNI/libminc/archive/libminc-2-3-00.tar.gz"
+  version "2.3.00"
+  sha256 "8c00e0383575ace1f941a5b35b99678db2a0dd5e023f8671d25debc949c7cf12"
+
+  depends_on "hdf5"
+  depends_on "netcdf"
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", "-DCMAKE_INSTALL_PREFIX:PATH=#{libexec}"
+    system "make", "install"
+
+    lib.mkdir
+    ln_sf Dir["#{libexec}/lib/*.a"], lib
+    ln_sf Dir["#{libexec}/lib/*.dylib"], lib
+
+    include.mkdir
+    ln_sf Dir["#{libexec}/includebrew "], include
+
+    cd "testdir" do
+      system "cmake", "."
+    end
+
+    pkgshare.install "testdir"
+  end
+
+  test do
+    cp_r "#{pkgshare}/testdir/.", testpath
+    system "./minc2-datatype-test"
+  end
+end
