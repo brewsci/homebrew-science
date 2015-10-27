aea8c8b046884097170ec6c6227b0d5d2fee2ec4
Denis Davydov <davydden@gmail.com>
2015-10-27 12:38:43 +0100

    slepc: test block, 3.6.2, blopex, fix install name
    Closes #2939.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- slepc.rb
+++ slepc.rb
@@ -1,8 +1,8 @@
 class Slepc < Formula
   desc "Scalable Library for Eigenvalue Computations"
   homepage "http://www.grycap.upv.es/slepc"
-  url "http://slepc.upv.es/download/download.php?filename=slepc-3.6.1.tar.gz"
-  sha256 "c0a3997347144b145ec6f783feadc5021428f05a965c3f285b2dafa9f84eb8a6"
+  url "http://slepc.upv.es/download/download.php?filename=slepc-3.6.2.tar.gz"
+  sha256 "2ab4311bed26ccf7771818665991b2ea3a9b15f97e29fd13911ab1293e8e65df"
 
   bottle do
     sha256 "707c11d73e213a83c5033159eb767b819eee9056f965a99c0d87fca0a549bf8f" => :yosemite
@@ -15,10 +15,11 @@ class Slepc < Formula
   option "with-complex", "Use complex version by default. Otherwise, real-valued version will be symlinked"
   option "without-check", "Skip run-time tests (not recommended)"
   option "with-openblas", "Install dependencies with openblas"
+  option "with-blopex", "Download blopex library"
 
   openblasdep = (build.with? "openblas") ? ["with-openblas"] : []
 
-  depends_on "petsc" => [:build] + openblasdep
+  depends_on "petsc" => openblasdep
   depends_on :mpi => [:cc, :f90]
   depends_on :fortran
   depends_on :x11 => :optional
@@ -33,12 +34,20 @@ class Slepc < Formula
 
     ENV["SLEPC_DIR"] = Dir.getwd
     args = ["--with-clean=true"]
-    args << "--with-arpack-dir=#{Formula["arpack"].lib}" << "--with-arpack-flags=-lparpack,-larpack" if build.with? "arpack"
+    args << "--with-arpack-dir=#{Formula["arpack"].opt_lib}" << "--with-arpack-flags=-lparpack,-larpack" if build.with? "arpack"
+    args << "--download-blopex" if build.with? "blopex"
 
     # real
     ENV["PETSC_DIR"] = "#{Formula["petsc"].opt_prefix}/#{petsc_arch_real}"
     system "./configure", "--prefix=#{prefix}/#{petsc_arch_real}", *args
     system "make"
+    if build.with? "check"
+      log_name = "make-test-real.log"
+      system "make test 2>&1 | tee #{log_name}"
+      ohai `grep "Completed test" "#{log_name}"`.chomp
+      prefix.install "#{log_name}"
+    end
+
     system "make", "test" if build.with? "check"
     system "make", "install"
 
@@ -47,10 +56,13 @@ class Slepc < Formula
     system "./configure", "--prefix=#{prefix}/#{petsc_arch_complex}", *args
     system "make"
     # TODO: investigate why complex tests fail to run on Linuxbrew
-    system "make", "test" if build.with? "check"
+    if build.with? "check"
+      log_name = "make-test-complex.log"
+      system "make test 2>&1 | tee #{log_name}"
+      ohai `grep "Completed test" "#{log_name}"`.chomp
+      prefix.install "#{log_name}"
+    end
     system "make", "install"
-
-    ohai "Test results are in ~/Library/Logs/Homebrew/slepc. Please check."
 
     # Link what we need.
     petsc_arch = ((build.include? "complex") ? petsc_arch_complex : petsc_arch_real)
@@ -61,11 +73,26 @@ class Slepc < Formula
     prefix.install_symlink "#{prefix}/#{petsc_arch}/conf"
     doc.install "docs/slepc.pdf", Dir["docs/*.htm"], "docs/manualpages" # They're not really man pages.
     pkgshare.install "share/slepc/datafiles"
+
+    # install some tutorials for use in test block
+    pkgshare.install "src/eps/examples/tutorials"
+
+    # fix install name on OS-X
+    system "install_name_tool", "-id", "#{opt_prefix}/lib/libslepc.3.6.dylib", "#{prefix}/#{petsc_arch}/lib/libslepc.3.6.2.dylib" if OS.mac?
   end
 
   def caveats; <<-EOS.undent
-    Set your SLEPC_DIR to #{prefix}/real or #{prefix}/complex.
-    Fortran modules are in #{prefix}/real/include and #{prefix}/complex/include.
+    Set your SLEPC_DIR to #{opt_prefix}/real or #{opt_prefix}/complex.
+    Fortran modules are in #{opt_prefix}/real/include and #{opt_prefix}/complex/include.
     EOS
+  end
+
+  test do
+    cp_r prefix/"share/slepc/tutorials", testpath
+    Dir.chdir("tutorials") do
+      system "mpicc", "ex1.c", "-I#{opt_include}", "-I#{Formula["petsc"].opt_include}", "-L#{Formula["petsc"].opt_lib}", "-lpetsc", "-L#{opt_lib}", "-lslepc", "-o", "ex1"
+      system "mpirun -np 3 ex1 2>&1 | tee ex1.out"
+      assert (identical?("output/ex1_1.out", "ex1.out"))
+    end
   end
 end
