cdffe01d62fe882a3669a7ce4b8c9d6b1353e916
a7744hsc <s20121033@xs.ustb.edu.cn>
2015-10-31 11:27:15 +0800

    openblas: version 0.2.15
    Fixes #2941.
    Closes #2957.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- openblas.rb
+++ openblas.rb
@@ -1,12 +1,10 @@
 class Openblas < Formula
   homepage "http://www.openblas.net/"
-  url "https://github.com/xianyi/OpenBLAS/archive/v0.2.14.tar.gz"
+  url "https://github.com/xianyi/OpenBLAS/archive/v0.2.15.tar.gz"
   head "https://github.com/xianyi/OpenBLAS.git", :branch => "develop"
-  sha256 "2411c4f56f477b42dff54db2b7ffc0b7cf53bb9778d54982595c64cc69c40fc1"
-  revision 1
+  sha256 "73c40ace5978282224e5e122a41c8388c5a19e65a6f2329c2b7c0b61bacc9044"
 
   bottle do
-    revision 1
     sha256 "c49c869f45c87cc375c573a160dd7ee219f2c9d16ef2acdbd9d385cc0561bf5a" => :yosemite
     sha256 "668ccea63190d3a0f0cca606bc92424200076cc488c1f398149a74340dd4b82a" => :mavericks
     sha256 "a6a592b7b7378665b606e414658847c5260eeebbc6c5ae32ba699a79e7547c3d" => :mountain_lion
