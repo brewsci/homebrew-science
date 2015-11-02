dda8e32d219485f8f81113686014d7c14b9fc864
Logan Rosen <loganrosen@gmail.com>
2015-11-02 00:18:56 -0500

    mallet: change to sha256, add head and devel
    style fixes, add desc and tag, add basic test
    
    Closes #2962.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- mallet.rb
+++ mallet.rb
@@ -1,9 +1,16 @@
-require 'formula'
-
 class Mallet < Formula
-  homepage 'http://mallet.cs.umass.edu/'
-  url 'http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz'
-  sha1 '45f6ad87ad7605d9f009be5f47b0bbf2ca47d89e'
+  desc "MAchine Learning for LanguagE Toolkit"
+  homepage "http://mallet.cs.umass.edu/"
+  # tag "machine learning"
+
+  url "http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz"
+  sha256 "bf34241b0589be2d28d57a4b369b4d722f273591cb28bf428d0167f58f91f161"
+  head "https://github.com/mimno/Mallet.git"
+
+  devel do
+    url "http://mallet.cs.umass.edu/dist/mallet-2.0.8RC2.tar.gz"
+    sha256 "61f327f3237d814addfae484be146476d84bcbf829d9a669a4e08c4f21a6187c"
+  end
 
   # Creates a wrapper to set the classpath before executing
   # the utility.
@@ -15,14 +22,18 @@ class Mallet < Formula
   end
 
   def install
-    rm Dir['bin/*.{bat,dll,exe}'] # Remove all windows files
+    rm Dir["bin/*.{bat,dll,exe}"] # Remove all windows files
     prefix.install_metafiles
-    libexec.install Dir['*']
-    cd libexec+'bin' do
-      Dir['*'].each do |file|
+    libexec.install Dir["*"]
+    cd libexec+"bin" do
+      Dir["*"].each do |file|
         fn = File.basename(file)
         (bin+fn).write startup_script(fn)
       end
     end
+  end
+
+  test do
+    system "#{bin}/mallet | grep Mallet"
   end
 end
