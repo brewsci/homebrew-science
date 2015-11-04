e0c94eb113e2932ea5ac08c8272add12efbb68cf
Kozo Nishida <knishida@riken.jp>
2015-11-04 14:23:49 +0900

    pathvisio 3.2.1
    Closes #2968.

--- pathvisio.rb
+++ pathvisio.rb
@@ -1,10 +1,11 @@
 class Pathvisio < Formula
+  desc "Extendable Pathway Analysis Toolbox"
   homepage "http://www.pathvisio.org/"
   # tag "bioinformatics"
-  # doi "10.1186/1471-2105-9-399"
-  url "http://pathvisio.org/data/releases/current/pathvisio_bin-3.2.0-r3999.tar.gz"
-  sha256 "77464484215f954c75d506df5d92361568e36afa2caa7d12d123a460f32766d8"
-  version "3.2.0"
+  # doi "10.1371/journal.pcbi.1004085"
+  url "http://www.pathvisio.org/data/releases/current/pathvisio_bin-3.2.1-r4025.zip"
+  version "3.2.1"
+  sha256 "e0891d309ce4cd73ca5e22da2a041a921fed86eaa16bdb9318b52a363daee1d2"
 
   depends_on :java => "1.7"
 
