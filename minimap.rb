68ff4395c912351863b05f1f80a5914378a568f5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-07 02:18:38 +0000

    minimap: add 0.1 bottle.

--- minimap.rb
+++ minimap.rb
@@ -1,6 +1,13 @@
 class Minimap < Formula
   desc "Find approx mapping positions between long sequences"
   homepage "https://github.com/lh3/minimap"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3905460562a5bd6a56e1c17f2fb8985ea51c1b3f51d7bb87d2edff65bd2570a9" => :el_capitan
+    sha256 "f79e81d855a599d1539904a12fdba4d2ad5123b9f034109ea4f04b19619070fc" => :yosemite
+    sha256 "81d18e1798db3e75ae756f87cccbd0cf4109ee461ae25e0129668f1dd10ff435" => :mavericks
+  end
+
   # tag "bioinformatics"
 
   url "https://github.com/lh3/minimap/archive/v0.1.tar.gz"
