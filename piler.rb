4f1015cc0e822508f137a7a1d2d5f45de58b75bf
Torsten Seemann <torsten.seemann@gmail.com>
2015-11-07 12:05:12 +1100

    Piler 1.0 revision (fix Linux -static build failure + modernize)
    Closes #2985.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- piler.rb
+++ piler.rb
@@ -1,23 +1,26 @@
-require "formula"
-
 class Piler < Formula
+  desc "Analyze repetitive DNA found in genome sequences"
   homepage "http://drive5.com/piler/"
+  # doi "10.1093/bioinformatics/bti1003"
+  # tag "bioinformatics"
+
   version "1.0"
+  revision 1
   if OS.mac?
     url "http://drive5.com/piler/piler_pals_osx_src.tar.gz"
-    sha1 "c9a614130b9e06ce12fee03464300fdfbc13d0bf"
+    sha256 "68c20b68a6bc73224e15bbaddfb982a7f0b38e04324e25f98d387b7186f981a4"
   elsif OS.linux?
     url "http://drive5.com/piler/piler_source.tar.gz"
-    sha1 "22d2efa728cbf1df306e723fcdc6ae68b8c05d38"
+    sha256 "3b1be91c497bdb22eac8032a60c9f815bdcd03edc5b25d925314a02f54bec44f"
   else
-    raise 'Unknown operating system'
+    raise "Unknown operating system"
   end
 
-  depends_on 'muscle'
+  depends_on "muscle"
 
   def install
-    cd (if OS.mac? then 'piler' else '.' end) do
-      system "make", "CC=#{ENV.cc} -c", "GPP=#{ENV.cxx}"
+    cd (if OS.mac? then "piler" else "." end) do
+      system "make", "CC=#{ENV.cc} -c", "GPP=#{ENV.cxx}", "LDLIBS=-lm" # remove -static
       if OS.mac?
         bin.install "piler"
       else
@@ -28,6 +31,6 @@ class Piler < Formula
   end
 
   test do
-    system "#{bin}/piler -version"
+    system "#{bin}/piler", "-version"
   end
 end
