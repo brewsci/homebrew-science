4a54cd1e3c0f58d6972980c5e1948561cc7a39b8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-12 21:47:29 +0000

    healpix: add 3.30 bottle.

--- healpix.rb
+++ healpix.rb
@@ -5,6 +5,13 @@ class Healpix < Formula
   version "3.30"
   sha256 "efcc8ff9775f393bd3e7e9d36202126e34e5c762ee568495a728329fa6650bfb"
 
+  bottle do
+    cellar :any
+    sha256 "2e1fd73dea22bec133fdae9611b87620dd4620998e798578e385400ef76c5d2b" => :el_capitan
+    sha256 "a19acb47d7c6f83d19828b99fe190da5309cf148113f6ccea351f7fcb5a627b5" => :yosemite
+    sha256 "ba491c231f7e086b2d3a59d391687abbe2052f6f87b788ae3447b74dfa01ba6d" => :mavericks
+  end
+
   depends_on "cfitsio"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
