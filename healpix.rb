9db79c4e4ca27e9117a17574bf21afe28f3e932b
kkrings <kai.krings88@gmx.net>
2015-11-12 22:27:38 +0100

    healpix 3.30
    Closes #3008.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- healpix.rb
+++ healpix.rb
@@ -1,19 +1,21 @@
-require 'formula'
-
 class Healpix < Formula
-  homepage 'http://healpix.jpl.nasa.gov/'
-  url 'https://downloads.sourceforge.net/project/healpix/Healpix_3.11/Healpix_3.11_2013Apr24.tar.gz'
-  sha1 'f7d6a18ca6aad9fe85a66eca36d7a1f0ef783e95'
-  version '3.11'
+  desc "Hierarchical Equal Area isoLatitude Pixelization of a sphere"
+  homepage "http://healpix.jpl.nasa.gov"
+  url "https://downloads.sourceforge.net/project/healpix/Healpix_3.30/Healpix_3.30_2015Oct08.tar.gz"
+  version "3.30"
+  sha256 "efcc8ff9775f393bd3e7e9d36202126e34e5c762ee568495a728329fa6650bfb"
 
-  depends_on 'cfitsio'
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
+  depends_on "cfitsio"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
-    configure_args = %W[--disable-dependency-tracking --prefix=#{prefix}]
+    configure_args = %W[  --disable-dependency-tracking
+                          --disable-silent-rules
+                          --prefix=#{prefix}  ]
+
     cd "src/C/autotools" do
       system "autoreconf", "--install"
       system "./configure", *configure_args
@@ -22,7 +24,7 @@ class Healpix < Formula
 
     ENV.append_to_cflags "-DENABLE_FITSIO"
     cd "src/cxx/autotools" do
-      system "echo \"AUTOMAKE_OPTIONS = subdir-objects\" >> Makefile.am"
+      system "echo", "\"AUTOMAKE_OPTIONS = subdir-objects\" >> Makefile.am"
       system "autoreconf", "--install"
       system "./configure", *configure_args
       system "make", "install"
