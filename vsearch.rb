6ca13b3b23f897e9231143261564ffd5a144dfbe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-13 22:48:22 +0000

    vsearch: add 1.9.1 bottle.

--- vsearch.rb
+++ vsearch.rb
@@ -1,6 +1,13 @@
 class Vsearch < Formula
   desc "USEARCH-compatible metagenomic sequence tool"
   homepage "https://github.com/torognes/vsearch"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c2e3b55f92f61cf9f9adc4daa1c2aa718b37b77d66d098b1a888705049664b16" => :el_capitan
+    sha256 "cbd33e469676ce6bd1a82252423421ab8369fbc3024804c739390b908957fae9" => :yosemite
+    sha256 "edadfd2741c8f3c4add51c4104052fa425754292d90d66db3a824f21c1610d37" => :mavericks
+  end
+
   # tag "bioinformatics"
   # doi "10.5281/zenodo.31443"
 
