41d65cb645f22af457d90912cae4d9ad5ec98f03
Torsten Seemann <torsten.seemann@gmail.com>
2015-11-17 17:22:15 +1100

    Prokka 1.11 (revision)
    Closes #3030.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- prokka.rb
+++ prokka.rb
@@ -1,4 +1,5 @@
 class Prokka < Formula
+  desc "Rapid annotation of prokaryotic genomes"
   homepage "http://www.vicbioinformatics.com/software.prokka.shtml"
   desc "Prokka: rapid annotation of prokaryotic genomes"
   # doi "10.1093/bioinformatics/btu153"
@@ -6,12 +7,14 @@ class Prokka < Formula
 
   url "http://www.vicbioinformatics.com/prokka-1.11.tar.gz"
   sha256 "ee18146c768fe6ac8e6c9e28bb35f686a5b79d5d5362c4b7665f6a33978101ae"
+  revision 1
 
   head "https://github.com/tseemann/prokka.git"
 
   depends_on "Bio::Perl" => :perl
   depends_on "XML::Simple" => :perl
   depends_on "Time::Piece" => :perl if OS.linux?
+
   depends_on "blast"
   depends_on "infernal"
   depends_on "hmmer"
@@ -19,13 +22,10 @@ class Prokka < Formula
   depends_on "prodigal"
   depends_on "tbl2asn"
   depends_on "parallel"
+  depends_on "minced"
 
-  depends_on "barrnap" => :recommended # fast rRNA searching using NHMMER
+  depends_on "barrnap" => :recommended
   depends_on "rnammer" => :optional
-
-  # These optional dependencies have no formulae.
-  # depends_on "minced" => :recommended # find CRISPRs
-  # depends_on "signalp" => :optional # for --gram / sig_peptide predictions
 
   def install
     prefix.install Dir["*"]
@@ -36,6 +36,6 @@ class Prokka < Formula
   end
 
   test do
-    system "#{bin}/prokka --version"
+    system "#{bin}/prokka", "--version"
   end
 end
