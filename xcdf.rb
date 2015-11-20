df0952d1aa91f9640b755f719894d1165b7e8b4b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-20 02:53:02 +0000

    xcdf: add 2.09.00 bottle.

--- xcdf.rb
+++ xcdf.rb
@@ -5,6 +5,13 @@ class Xcdf < Formula
   sha256 "49a2357392008cf12dc956a2d43e4b0948f1d8c42e014fa04db7e8ac4d267567"
   head "https://github.com/jimbraun/XCDF.git"
 
+  bottle do
+    cellar :any
+    sha256 "ad83615a6d90b1c6d8e9bf53ec615fad0e5803f055bf234e103356f6adc2f50a" => :el_capitan
+    sha256 "84d224c1bc39bb28549ad8e4c08f0d457fbd3fce472132be95fe71e9fabc05ad" => :yosemite
+    sha256 "223d55367d2891499e9f5b0deeb0b63f6ef9ec61d0a953912b8fc4388c205104" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on :python
 
