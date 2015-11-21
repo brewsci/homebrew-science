77279a91c57d1e43ce3a4a88a75fb53ed1f10cca
ackinetics <neils@ackinetics.com>
2015-11-21 16:09:10 -0500

    glpk 4.57
    Closes #3043.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- glpk.rb
+++ glpk.rb
@@ -1,8 +1,9 @@
 class Glpk < Formula
+  desc "Library for Linear (LP) and Mixed-Integer Programming (MIP)"
   homepage "https://www.gnu.org/software/glpk/"
-  url "http://ftpmirror.gnu.org/glpk/glpk-4.52.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/glpk/glpk-4.52.tar.gz"
-  sha1 "44b30b0de777a0a07e00615ac6791af180ff4d2c"
+  url "http://ftpmirror.gnu.org/glpk/glpk-4.57.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/glpk/glpk-4.57.tar.gz"
+  sha256 "7323b2a7cc1f13e45fc845f0fdca74f4daea2af716f5ad2d4d55b41e8394275c"
 
   bottle do
     cellar :any
@@ -34,7 +35,7 @@ class Glpk < Formula
         return 0;
     }
     EOF
-    system ENV.cc, "test.c", "-lglpk", "-o", "test"
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-lglpk", "-o", "test"
     assert_equal `./test`, version.to_s
   end
 end
