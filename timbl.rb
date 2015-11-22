4cd70e34b4336e81422e62a27dbd2b1d5167115d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-22 00:03:32 +0000

    timbl: update 6.4.6_1 bottle.

--- timbl.rb
+++ timbl.rb
@@ -7,9 +7,9 @@ class Timbl < Formula
 
   bottle do
     cellar :any
-    sha1 "7188843d1ad3fee331f6508bfae96ecc68e563ec" => :yosemite
-    sha1 "2def0ce56128f15548948e8487f184377d7d7492" => :mavericks
-    sha1 "ae935f458395343eaeab276825ca9a5dfdf5e95e" => :mountain_lion
+    sha256 "f96e343829eaa220579f120bbde601c6340400ffede6147774fc3eb10220eb14" => :el_capitan
+    sha256 "bd9de88b4a642f4a31b5536b49f77a5dfa4401f7ad90a5a5fd83d90c42541930" => :yosemite
+    sha256 "46c6b0f12e0d97088884abf54007b7ea70adff1a87038756a2285bc1a55e061e" => :mavericks
   end
 
   depends_on "pkg-config" => :build
