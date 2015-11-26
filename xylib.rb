84fd19baa91515d80215a03940568da1a65e106e
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-26 22:24:11 +0100

    xylib: desc added

--- xylib.rb
+++ xylib.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Xylib < Formula
+  desc "Library for reading x-y data files"
   homepage "http://xylib.sourceforge.net"
   url "https://github.com/wojdyr/xylib/releases/download/v1.3/xylib-1.3.tar.bz2"
-  sha1 "61d95a9d3c98239fd4fb335fadadd85be3001a89"
+  sha256 "7047bc6730bc61f7f0be94e8872414768a94a409dcc10059556dbc25b1114426"
 
   depends_on "boost" => :build
   depends_on "zlib" => :recommended
@@ -18,7 +17,7 @@ class Xylib < Formula
 
   test do
     system bin/"xyconv", "--version"
-    (testpath/'with_sigma.txt').write('20.0 490.5')
+    (testpath/"with_sigma.txt").write "20.0 490.5"
     system "#{bin}/xyconv", "-g", "#{testpath}/with_sigma.txt"
   end
 end
