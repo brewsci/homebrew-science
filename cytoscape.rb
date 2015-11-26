396d9f61277073daace9e03cb656dffb91f6611f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-26 13:36:24 +0000

    cytoscape: add 3.3.0 bottle.

--- cytoscape.rb
+++ cytoscape.rb
@@ -6,6 +6,13 @@ class Cytoscape < Formula
   url "http://chianti.ucsd.edu/cytoscape-3.3.0/cytoscape-3.3.0.tar.gz"
   sha256 "e5270c50d34b344c25f470a0064ab823bf0648c0609e2fb36b8d8fd7e44d95dd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7fdcaec1aaee9caf8c0b4104f2c2379b94abc2f3efe888678de68c8f78cad16e" => :el_capitan
+    sha256 "ff2d644cb0de7032fb164c4bd657137f92a0c6d6dfdf7755b61c551a7751aed5" => :yosemite
+    sha256 "aadafb17f1aef90e532d288294558d9c1ba2ea3194ea49776f45128550849213" => :mavericks
+  end
+
   depends_on :java => "1.8"
 
   def install
