65b58318ec8ba63b40735dfcc42d99dafdf35e0b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-30 14:45:35 +0000

    shark: add 3.0.0 bottle.

--- shark.rb
+++ shark.rb
@@ -4,6 +4,13 @@ class Shark < Formula
   url "https://github.com/Shark-ML/Shark/archive/v3.0.0.tar.gz"
   sha256 "b8b3bc9dca52369d4cc432b99ebdd37450056eb1b6ce86070996f16ea44d09ea"
 
+  bottle do
+    cellar :any
+    sha256 "6cb63ae691b9b0876ae7343ee0145c3be23354a2fcdb40a524415367e65cecaf" => :el_capitan
+    sha256 "b2a62a4cadb5a226aac9742947d8a739596ea2cc3ca8945795d781743d3be7ba" => :yosemite
+    sha256 "e6b8a30aa855933b744a2939eb5826d5156e4a134aaa93d2bab3ab99f4f9f681" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
 
