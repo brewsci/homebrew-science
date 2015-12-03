f6be4d0b6b82c1ca6f3fb2e94aac9cc32e4d757b
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-03 11:49:19 +0100

    trf: audit fixes

--- trf.rb
+++ trf.rb
@@ -8,7 +8,7 @@ class Trf < Formula
     sha256 "385ef13d0fcf53532fc0bdfe386c6789323a144b95c0e4b42c54b382bc5a7672"
   elsif OS.linux?
     url "http://tandem.bu.edu/cgi-bin/trf/trf.download.pl?fileselect=26"
-    sha1 "b4f24766f27f3b5f02957fa565da3bd114c37f9a"
+    sha256 "a3a760c7b74c9603fbc08d95e8fa696c00f35a2f179b0bd63b2b13757ad3b471"
   else
     raise "Unknown operating system"
   end
@@ -18,6 +18,6 @@ class Trf < Formula
   end
 
   test do
-    system "trf 2>&1 |grep -q trf"
+    assert_match "trf", shell_output("#{bin}/trf 2>&1")
   end
 end
