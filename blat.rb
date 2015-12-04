5f5adad99e6c7e243a2c8e9d8b82265f5c114b45
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-04 01:40:46 +0000

    blat: update 36 bottle.

--- blat.rb
+++ blat.rb
@@ -8,10 +8,9 @@ class Blat < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "7e1ae73487db28bca75585482ccf50f206468f714365730af8dae7cc1c892ef2" => :yosemite
-    sha256 "fc541ee6ebb3f895b2e9700431325687c9a95bf2ab55a0d6fcafa95110d30c29" => :mavericks
-    sha256 "c21f79a061ee6d0594f1f35b743e99e84c6d1a604f34c738ea32370a1234864b" => :mountain_lion
+    sha256 "af1ecf625bd23b8be03fff89e5f8056fd5bb33be39965b1f956848d6b90dc1d1" => :el_capitan
+    sha256 "c4b95395dd2e649f34c82abe014464bfb17901e5e7abecb99dc46486dc852a52" => :yosemite
+    sha256 "9580697a3ccc4ad36e83df0c24a89517769009d2904c5cc80b4b0f30a1646e6b" => :mavericks
   end
 
   depends_on "libpng" => :build
