259f274122b4aa94c0ba2e33ed417bc85544d7a3
Dominique Orban <dominique.orban@gmail.com>
2015-12-06 16:02:44 -0500

    hdf5: use url for releases.
    Fixes #3059.

--- hdf5.rb
+++ hdf5.rb
@@ -1,7 +1,7 @@
 class Hdf5 < Formula
   desc "File format designed to store large amounts of data"
   homepage "http://www.hdfgroup.org/HDF5"
-  url "http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2"
+  url "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.16/src/hdf5-1.8.16.tar.bz2"
   sha256 "13aaae5ba10b70749ee1718816a4b4bfead897c2fcb72c24176e759aec4598c6"
 
   bottle do
