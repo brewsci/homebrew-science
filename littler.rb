29fdb4cd4bf7cddd70b8fc8699bc67335885db1d
Tim D. Smith <git@tim-smith.us>
2015-12-14 22:14:17 -0800

    littler: bump for r 3.2.3
    Closes #3124. Closes #3111.

--- littler.rb
+++ littler.rb
@@ -5,7 +5,7 @@ class Littler < Formula
   head "https://github.com/eddelbuettel/littler.git"
   url "http://dirk.eddelbuettel.com/code/littler/littler-0.2.3.tar.gz"
   sha256 "98cd741c68a5c8f65b06c96d2f56d3b44979b3990335e7869b002c005ef80ba7"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "1e5af583921af3996ad8af8605306a1dcd1f0e6d34d4e3b1b24e034e24bb12e1" => :yosemite
