f00dbcf8d72c8eb7d9038cbe6b253ea9ed01a777
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-22 00:33:16 +0000

    ess: add 15.09-2 bottle.

--- ess.rb
+++ ess.rb
@@ -5,6 +5,13 @@ class Ess < Formula
   version "15.09-2"
   sha256 "706c41237e1edf33a369902f503bb25254b2bbb750b9ed1adee244e875264afb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "33487613d5e6495e90bb0ef9c67769643e45ad01df1783bcf7b238517f687fa6" => :el_capitan
+    sha256 "faafee99a5a6b3715bce1dcb310313e0396cf29669178545086a7898f6935728" => :yosemite
+    sha256 "ae72a98f8f4131a2cde8219f53df51d6470839eaa1299331fb13d3a8fd7c4f5e" => :mavericks
+  end
+
   depends_on :emacs => "23"
 
   def install
