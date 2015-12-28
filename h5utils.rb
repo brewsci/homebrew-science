c0b1da5187ee2488b56b12cba99c3171e8be91e1
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 10:49:57 +0100

    h5utils: audit fixes

--- h5utils.rb
+++ h5utils.rb
@@ -1,6 +1,5 @@
-require "formula"
-
 class H5utils < Formula
+  desc "Utilities to work with scientific data in HDF5"
   homepage "http://ab-initio.mit.edu/wiki/index.php/H5utils"
   url "http://ab-initio.mit.edu/h5utils/h5utils-1.12.1.tar.gz"
   sha256 "7290290ca5d5d4451d757a70c86baaa70d23a28edb09c951b6b77c22b924a38d"
@@ -18,13 +17,13 @@ class H5utils < Formula
   # A patch is required in order to build h5utils with libpng 1.5
   patch :p0 do
     url "https://trac.macports.org/export/102291/trunk/dports/science/h5utils/files/patch-writepng.c"
-    sha1 "026aa59f2e13388d0b7834de6dcbd48da2858cbe"
+    sha256 "b8737b5e4cd6597570b39ce911ffea5bd0173e0e7a6b32620df188b2d260280f"
   end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-octave"
-    system "make install"
+    system "make", "install"
   end
 end
