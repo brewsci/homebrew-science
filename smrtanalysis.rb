1c55bc4a2662aad433c6a9557ffbdac06de91011
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 12:33:19 +0100

    smrtanalysis: use a SHA256

--- smrtanalysis.rb
+++ smrtanalysis.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Smrtanalysis < Formula
   homepage "http://www.pacb.com/devnet/"
   # doi "10.1038/nmeth.2474"
   url "https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377.run"
-  sha1 "b64cdeb68f84f021ccaeffaa62ec5977029451af"
+  sha256 "17ee0ebce5450a8dcd3d0cfc3cd0676de53c55f7894f642f136da645485a66be"
 
   # Patch
   # https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377-patch-1.run
