e249f38025937368231fdbe9aac60cd00c2e270d
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 12:36:19 +0100

    rmblast: use a SHA256

--- rmblast.rb
+++ rmblast.rb
@@ -15,7 +15,7 @@ class Rmblast < Formula
     sha256 "f94e91487b752eb24386c3571250a3394ec7a00e7a5370dd103f574c721b9c81"
   elsif OS.linux?
     url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-#{version}-x64-linux.tar.gz"
-    sha1 "02abee397973ccdf7d68e161203bd8dbe9322a6c"
+    sha256 "e6503ad25a6760d2d2931f17efec80ba879877b4042a1d10a60820ec21a61cfe"
   else
     raise "Unknown operating system"
   end
