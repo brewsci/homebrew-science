35a96562ef564e9e8f6c1dfa4cee0d2b32b273ba
Dominique Orban <dominique.orban@gmail.com>
2015-12-30 10:11:09 -0500

    superlu: sha256, audits, openmp, veclibfort, tests
    - Fix sha256
    - Fix audit warnings
    - Use vecLibFort by default to avoid single precision bug
    - Add option to build with OpenMP support
    - Compile test drivers
    
    Fixes #3156.
    
    Closes #3157.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- superlu.rb
+++ superlu.rb
@@ -2,7 +2,8 @@ class Superlu < Formula
   desc "Solve large, sparse nonsymmetric systems of equations"
   homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/"
   url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_5.1.tar.gz"
-  sha256 "63df543e5b014b7f314039f5d6a72454853b3e4f209888edda0394179c902b08"
+  sha256 "307ef10edef4cebc6c7f672cd931ae6682db4c4f5f93a44c78e9544a520e2db1"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -11,24 +12,45 @@ class Superlu < Formula
     sha256 "30f20a55944c3712ddf7522fe9c40f42024849630324c52bfc5d1a1184d862cb" => :mavericks
   end
 
-  option "without-check", "skip build-time tests (not recommended)"
+  deprecated_option "without-check" => "without-test"
+
+  option "without-test", "skip build-time tests (not recommended)"
+  option "with-openmp", "Enable OpenMP multithreading"
 
   depends_on :fortran
-  depends_on "openblas" => :optional
+
+  # Accelerate single precision is buggy and causes certain single precision
+  # tests to fail.
+  depends_on "openblas" => ((OS.mac?) ? :optional : :recommended)
+  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
+
+  needs :openmp if build.with? "openmp"
+
+  patch :DATA
 
   def install
     ENV.deparallelize
     cp "MAKE_INC/make.mac-x", "./make.inc"
-    make_args = ["RANLIB=true", "CC=#{ENV.cc}", "CFLAGS=-fPIC #{ENV.cflags}",
-                 "FORTRAN=#{ENV.fc}", "FFLAGS=#{ENV.fcflags}",
+    make_args = ["RANLIB=true",
+                 "CC=#{ENV.cc}",
+                 "CFLAGS=-fPIC #{ENV.cflags}",
+                 "FORTRAN=#{ENV.fc}",
+                 "FFLAGS=#{ENV.fcflags}",
                  "SuperLUroot=#{buildpath}",
                  "SUPERLULIB=$(SuperLUroot)/lib/libsuperlu.a",
-                 "NOOPTS=-fPIC"]
+                 "NOOPTS=-fPIC",
+                ]
 
-    make_args << ((build.with? "openblas") ? "BLASLIB=-L#{Formula["openblas"].opt_lib} -lopenblas" : "BLASLIB=-framework Accelerate")
+    if build.with? "openblas"
+      blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
+    else
+      blas = (OS.mac?) ? "-L#{Formula["veclibfort"].opt_lib} -lvecLibFort" : "-lblas"
+    end
+    make_args << "BLASLIB=#{blas}"
+    make_args << ("LOADOPTS=" + ((build.with? "openmp") ? "-fopenmp" : ""))
 
     system "make", "lib", *make_args
-    if build.with? "check"
+    if build.with? "test"
       system "make", "testing", *make_args
       cd "TESTING" do
         system "make", *make_args
@@ -53,7 +75,30 @@ class Superlu < Formula
   end
 
   test do
-    cd share / "superlu" do
+    ENV.fortran
+    cp_r pkgshare, testpath
+    cp prefix/"make.inc", testpath
+    make_args = ["CC=#{ENV.cc}",
+                 "CFLAGS=-fPIC #{ENV.cflags}",
+                 "FORTRAN=#{ENV.fc}",
+                 "FFLAGS=#{ENV.fcflags}",
+                 "SuperLUroot=#{opt_prefix}",
+                 "SUPERLULIB=#{opt_lib}/libsuperlu.a",
+                 "NOOPTS=-fPIC",
+                 "HEADER=#{opt_include}/superlu",
+                ]
+
+    if build.with? "openblas"
+      blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
+    else
+      blas = (OS.mac?) ? "-L#{Formula["veclibfort"].opt_lib} -lvecLibFort" : "-lblas"
+    end
+    make_args << "BLASLIB=#{blas}"
+    make_args << ("LOADOPTS=" + ((build.with? "openmp") ? "-fopenmp" : ""))
+
+    cd "superlu" do
+      system "make", *make_args
+
       system "./superlu"
       system "./slinsol < g20.rua"
       system "./slinsolx  < g20.rua"
@@ -75,7 +120,7 @@ class Superlu < Formula
       system "./zlinsolx1 < cg20.cua"
       system "./zlinsolx2 < cg20.cua"
 
-      system "./sitersol -h < g20.rua"
+      system "./sitersol -h < g20.rua" # broken with Accelerate
       system "./sitersol1 -h < g20.rua"
       system "./ditersol -h < g20.rua"
       system "./ditersol1 -h < g20.rua"
@@ -86,3 +131,93 @@ class Superlu < Formula
     end
   end
 end
+
+__END__
+diff --git a/EXAMPLE/citersol.c b/EXAMPLE/citersol.c
+index 1bcd6a2..6ced186 100644
+--- a/EXAMPLE/citersol.c
++++ b/EXAMPLE/citersol.c
+@@ -292,7 +292,7 @@ int main(int argc, char *argv[])
+     restrt = SUPERLU_MIN(n / 3 + 1, 50);
+     maxit = 1000;
+     iter = maxit;
+-    resid = 1e-8;
++    resid = 1e-4;
+     if (!(x = complexMalloc(n))) ABORT("Malloc fails for x[].");
+
+     if (info <= n + 1)
+@@ -326,7 +326,7 @@ int main(int argc, char *argv[])
+	if (iter >= maxit)
+	{
+	    if (resid >= 1.0) iter = -180;
+-	    else if (resid > 1e-8) iter = -111;
++	    else if (resid > 1e-4) iter = -111;
+	}
+	printf("iteration: %d\nresidual: %.1e\nGMRES time: %.2f seconds.\n",
+		iter, resid, t);
+diff --git a/EXAMPLE/citersol1.c b/EXAMPLE/citersol1.c
+index 09036d0..836c9ac 100644
+--- a/EXAMPLE/citersol1.c
++++ b/EXAMPLE/citersol1.c
+@@ -304,7 +304,7 @@ int main(int argc, char *argv[])
+     restrt = SUPERLU_MIN(n / 3 + 1, 50);
+     maxit = 1000;
+     iter = maxit;
+-    resid = 1e-8;
++    resid = 1e-4;
+     if (!(x = complexMalloc(n))) ABORT("Malloc fails for x[].");
+
+     if (info <= n + 1)
+@@ -338,7 +338,7 @@ int main(int argc, char *argv[])
+	if (iter >= maxit)
+	{
+	    if (resid >= 1.0) iter = -180;
+-	    else if (resid > 1e-8) iter = -111;
++	    else if (resid > 1e-4) iter = -111;
+	}
+	printf("iteration: %d\nresidual: %.1e\nGMRES time: %.2f seconds.\n",
+		iter, resid, t);
+diff --git a/EXAMPLE/sitersol.c b/EXAMPLE/sitersol.c
+index fc6045c..8f0b6f7 100644
+--- a/EXAMPLE/sitersol.c
++++ b/EXAMPLE/sitersol.c
+@@ -291,7 +291,7 @@ int main(int argc, char *argv[])
+     restrt = SUPERLU_MIN(n / 3 + 1, 50);
+     maxit = 1000;
+     iter = maxit;
+-    resid = 1e-8;
++    resid = 1e-4;
+     if (!(x = floatMalloc(n))) ABORT("Malloc fails for x[].");
+
+     if (info <= n + 1)
+@@ -325,7 +325,7 @@ int main(int argc, char *argv[])
+	if (iter >= maxit)
+	{
+	    if (resid >= 1.0) iter = -180;
+-	    else if (resid > 1e-8) iter = -111;
++	    else if (resid > 1e-4) iter = -111;
+	}
+	printf("iteration: %d\nresidual: %.1e\nGMRES time: %.2f seconds.\n",
+		iter, resid, t);
+diff --git a/EXAMPLE/sitersol1.c b/EXAMPLE/sitersol1.c
+index 7d098fb..2ee355c 100644
+--- a/EXAMPLE/sitersol1.c
++++ b/EXAMPLE/sitersol1.c
+@@ -303,7 +303,7 @@ int main(int argc, char *argv[])
+     restrt = SUPERLU_MIN(n / 3 + 1, 50);
+     maxit = 1000;
+     iter = maxit;
+-    resid = 1e-8;
++    resid = 1e-4;
+     if (!(x = floatMalloc(n))) ABORT("Malloc fails for x[].");
+
+     if (info <= n + 1)
+@@ -337,7 +337,7 @@ int main(int argc, char *argv[])
+	if (iter >= maxit)
+	{
+	    if (resid >= 1.0) iter = -180;
+-	    else if (resid > 1e-8) iter = -111;
++	    else if (resid > 1e-4) iter = -111;
+	}
+	printf("iteration: %d\nresidual: %.1e\nGMRES time: %.2f seconds.\n",
+		iter, resid, t);
