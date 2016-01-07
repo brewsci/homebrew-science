332972f6091cdada9443a93732c014b57d1e1daf
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-07 14:07:47 +0100

    numdiff: url fixed
    Closes #3185.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- numdiff.rb
+++ numdiff.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Numdiff < Formula
+  desc "Putative files comparison tool"
   homepage "http://www.nongnu.org/numdiff"
-  url "http://mirror2.klaus-uwe.me/nongnu/numdiff/numdiff-5.8.1.tar.gz"
-  sha1 "dae1fac5d2ba2e40b7713d6ef9647c94b572b4e1"
+  url "http://mirror6.layerjet.com/nongnu//numdiff/numdiff-5.8.1.tar.gz"
+  sha256 "99aebaadf63325f5658411c09c6dde60d2990c5f9a24a51a6851cb574a4af503"
 
   # we need libintl.h which is normally a part of libc6-dev
   # but within Homebrew can be found in gettext
