fa7cecada850d634c63801da500ccd3539f0be96
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-07 12:08:15 +0100

    methpipe 3.4.2
    Closes #3182.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- methpipe.rb
+++ methpipe.rb
@@ -1,12 +1,10 @@
-require "formula"
-
 class Methpipe < Formula
   homepage "http://smithlabresearch.org/software/methpipe/"
-  url "http://smithlabresearch.org/downloads/methpipe-3.3.1.tar.gz"
-  sha1 "00dea4a51e7b8ed5e7020bba89db63a91d86a70f"
+  url "http://smithlabresearch.org/downloads/methpipe-3.4.2.tar.bz2"
+  sha256 "9dab70723f71af815a058d38abc963dcb43b2e25968e12dbcf17413512ededf7"
   head "https://github.com/smithlabcode/methpipe.git"
 
-  depends_on "gsl" => :build
+  depends_on "gsl"
 
   def install
     system "make", "all"
@@ -15,6 +13,6 @@ class Methpipe < Formula
   end
 
   test do
-    system "symmetric-cpgs -about"
+    system "#{bin}/symmetric-cpgs", "-about"
   end
 end
