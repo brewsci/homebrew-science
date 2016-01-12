51a69ff4decac3058d0c26568d23c899604380e2
Martin Afanasjew <martin@afanasjew.de>
2016-01-12 06:30:22 +0100

    pastix 5.2.2.22
    Closes #3205.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- pastix.rb
+++ pastix.rb
@@ -1,9 +1,8 @@
 class Pastix < Formula
   homepage "http://pastix.gforge.inria.fr"
-  url "https://gforge.inria.fr/frs/download.php/file/34392/pastix_5.2.2.20.tar.bz2"
-  sha256 "758de4f109c852a6f13f0aea248838a3a8fefdc0f9c1da887bc37061fbe2e00a"
+  url "https://gforge.inria.fr/frs/download.php/file/35070/pastix_5.2.2.22.tar.bz2"
+  sha256 "30f771a666719e6b116f549a6e4da451beabab99c2ecabc0745247c3654acbed"
   head "git://scm.gforge.inria.fr/ricar/ricar.git"
-  revision 3
 
   bottle do
     cellar :any
