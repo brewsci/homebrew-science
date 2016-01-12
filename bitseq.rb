d2a1b3803fb5935be6bb8833c5481b062d6e2972
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-12 21:17:12 +0000

    bitseq: add 0.7.5 bottle.

--- bitseq.rb
+++ bitseq.rb
@@ -6,6 +6,13 @@ class Bitseq < Formula
 
   head "https://github.com/BitSeq/BitSeq.git"
 
+  bottle do
+    cellar :any
+    sha256 "53a4eb9db5d4368738234abe42096d0658fd932b87d0b48841b638a4f8446654" => :el_capitan
+    sha256 "17c3a8bc0ab1552eb0a00e9f51739dadbcfc0d647413dd542d498608a7a6b13b" => :yosemite
+    sha256 "c779feb24ca6061132f4d68af03b5301daa5a2c9eb1c85ffe21a0eda838d4fef" => :mavericks
+  end
+
   needs :openmp
   needs :cxx11
 
