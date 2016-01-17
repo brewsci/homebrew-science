39d182b64ef27cb6aba0cc9d149ad88ba2611486
Christian Bundy <christianbundy@fraction.io>
2016-01-16 16:37:00 -0800

    Update PhyML to v3.2.0
    Closes #3230.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- phyml.rb
+++ phyml.rb
@@ -1,5 +1,11 @@
 class Phyml < Formula
   desc "Fast maximum likelihood-based phylogenetic inference"
+  homepage "http://www.atgc-montpellier.fr/phyml/"
+  url "https://github.com/stephaneguindon/phyml/archive/v3.2.0.tar.gz"
+  sha256 "9fec8fc26e69cad8d58bf2c9433b531754e4f026dc3464d07958b6c824783fde"
+  # tag "bioinformatics"
+  # doi "10.1093/sysbio/syq010"
+
   bottle do
     cellar :any
     sha256 "ccae08800290289e8989d6da5aab65adb6cded93e6a32b284f184a7c372f947b" => :yosemite
@@ -7,21 +13,20 @@ class Phyml < Formula
     sha256 "723aa3d01acf9ec3b4fc4aa8ed48abd0db5208abb9c7f8e23c5549e6668a29b5" => :mountain_lion
   end
 
-  # tag "bioinformatics"
-  # doi "10.1093/sysbio/syq010"
-  homepage "http://www.atgc-montpellier.fr/phyml/"
-  url "https://phyml.googlecode.com/files/phyml-20120412.tar.gz"
-  sha256 "a43e51534a1ae2d1ee4c94ced3a1855813ff4872a6c9c2b83b369ddb71198417"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
+    chmod 0755, 'autogen.sh'
+    system "./autogen.sh"
+
     # separate steps required
     system "./configure", "--prefix=#{prefix}"
     system "make"
 
-    system "./configure", "--prefix=#{prefix}", "--enable-phytime"
-    system "make"
-
-    bin.install "src/phyml", "src/phytime"
+    bin.install "src/phyml"
     doc.install "doc/phyml-manual.pdf"
     pkgshare.install Dir["examples/*"]
   end
