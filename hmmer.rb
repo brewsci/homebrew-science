24870de76673befc5b4361e852eabf93d7d22b3d
Austin Davis-Richardson <harekrishna@gmail.com>
2016-02-01 11:37:38 -0800

    hmmer: update URL
    Closes #3276. Closes #3277.
    
    Signed-off-by: Ian Lancaster <ilancaster@gmail.com>

--- hmmer.rb
+++ hmmer.rb
@@ -4,7 +4,7 @@ class Hmmer < Formula
   # doi "10.1371/journal.pcbi.1002195"
   # tag "bioinformatics"
 
-  url "http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz"
+  url "http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz"
   sha256 "dd16edf4385c1df072c9e2f58c16ee1872d855a018a2ee6894205277017b5536"
 
   bottle do
