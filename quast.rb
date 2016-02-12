15ff6cda82041e98554b806a7e41f65bcea6849a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-12 19:42:35 +0000

    quast: update 3.2 bottle.

--- quast.rb
+++ quast.rb
@@ -8,10 +8,10 @@ class Quast < Formula
   sha256 "e1534824fc185679f0fa9c03846fbcf34b6a20466a6ec0110cf74f14c6380c02"
 
   bottle do
-    cellar :any
-    sha256 "31424b63c4575c86f8a8738e344f5de2db54dc825c48768ca05eba203f739cb0" => :yosemite
-    sha256 "e7059ac774256316780e30786c01f62848b47f084c235414d22efbdc3c917598" => :mavericks
-    sha256 "01b2f6d165f44f5883c6a149ae5761902b22e7fc5e1c9ad95928899a142968e4" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "7f4cedcf48ba8849a765d9d199b5f2e68b4dc480438b728a7b3e61798dca7a53" => :el_capitan
+    sha256 "ecbea59dde891b455f1a73c1be3cc3a4190c4a2c89667a2e03f398a223689a6e" => :yosemite
+    sha256 "10af4a838eff2dbc7b706b078d97e8292d2a1449fdea5687e97cf045fa1573d0" => :mavericks
   end
 
   if OS.mac? && MacOS.version <= :mountain_lion
