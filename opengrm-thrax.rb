d4473eccd3ca20ab72281ecc28179acbcfc3863d
Dominique Orban <dominique.orban@gmail.com>
2016-02-17 13:55:31 -0500

    opengrm-thrax: bump revision
    Closes #3320.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -2,6 +2,7 @@ class OpengrmThrax < Formula
   homepage "http://www.openfst.org/twiki/bin/view/GRM/Thrax"
   url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.2.0.tar.gz"
   sha256 "23112837b64634685e34681758b42b55d09b97b999c2f8a43c0002b870f98fc9"
+  revision 1
 
   bottle do
     cellar :any
