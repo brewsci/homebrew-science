8e11eb4889db7cc7b6b5a927be4db6de0068a23d
Johan Pauwels <johan.pauwels@gmail.com>
2016-02-18 20:24:55 +0100

    blis 0.1.8
    Closes #3326.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- blis.rb
+++ blis.rb
@@ -1,20 +1,23 @@
 class Blis < Formula
-  homepage "https://code.google.com/p/blis/"
-  url "https://github.com/flame/blis/archive/0.1.6.tar.gz"
-  sha256 "04171ffd39ece22976f97b6080efc27d69f8fd27a0cfd077fe85f8393cc489ff"
+  desc "BLAS-like Library Instantiation Software Framework"
+  homepage "https://github.com/flame/blis"
+  url "https://github.com/flame/blis/archive/0.1.8.tar.gz"
+  sha256 "b649a13ccdf0040e44bdbd5cf39e7c9a24cc0ac41ded1ad10681fe9bcce4cc89"
   head "https://github.com/flame/blis.git"
+
+  deprecated_option "without-check" => "without-test"
 
   option "with-configuration=",
       "BLIS framework configuration name (default: reference)\n" \
-      "\tSee https://code.google.com/p/blis/wiki/BuildSystem" \
-      "#Step_1:_Choose_a_framework_configuration"
-  option "without-check", "Skip build-time tests (not recommended)"
+      "\thttps://github.com/flame/blis/wiki/BuildSystem" \
+      "#step-1-choose-a-framework-configuration"
+  option "without-test", "Skip build-time tests (not recommended)"
   option "without-shared", "Do not build as a shared library"
   option "without-static", "Do not build as a static library"
 
   def install
-    if build.without?("dynamic") && build.without?("static")
-      raise "Must build either a static or dynamic library"
+    if build.without?("shared") && build.without?("static")
+      raise "Must build either a static or a shared library"
     end
 
     system "./configure",
@@ -25,7 +28,7 @@ class Blis < Formula
       prefix.install "output.testsuite"
     end
     system "make", "install",
-        "BLIS_ENABLE_DYNAMIC_BUILD=" + (build.with?("dynamic") ? "yes" : "no"),
+        "BLIS_ENABLE_DYNAMIC_BUILD=" + (build.with?("shared") ? "yes" : "no"),
         "BLIS_ENABLE_STATIC_BUILD=" + (build.with?("static") ? "yes" : "no")
   end
 
