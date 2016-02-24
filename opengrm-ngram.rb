5d0f33ff597aebb24d6bb3fe6ad39f3271737308
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-24 22:28:01 +0000

    opengrm-ngram: add 1.2.2 bottle.

--- opengrm-ngram.rb
+++ opengrm-ngram.rb
@@ -3,6 +3,13 @@ class OpengrmNgram < Formula
   url "http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramDownload/opengrm-ngram-1.2.2.tar.gz"
   sha256 "12bba4c1345f3933e161859cc9cb5d21b772d2b46173b4511fc778c67ada233b"
 
+  bottle do
+    cellar :any
+    sha256 "58c0aacb0f298b5e4777864487172ef78886e2affffc6c13c5876c529d309529" => :el_capitan
+    sha256 "8a906fb5089a8db967529bc4edf8a38ebb462c3e41c1ce4c849459efca5ed111" => :yosemite
+    sha256 "7b6c06fa7c008e8f0799ef3f0071f9e621cdda196ef9b2ff37282309a7460756" => :mavericks
+  end
+
   depends_on "openfst"
 
   resource "earnest" do
