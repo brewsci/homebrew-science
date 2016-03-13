f430b07d300eb2d435f2332c9ea7e6870286f24e
Tim D. Smith <git@tim-smith.us>
2016-03-12 17:43:27 -0800

    littler: bump for R 3.2.4

--- littler.rb
+++ littler.rb
@@ -5,7 +5,7 @@ class Littler < Formula
   head "https://github.com/eddelbuettel/littler.git"
   url "http://dirk.eddelbuettel.com/code/littler/littler-0.2.3.tar.gz"
   sha256 "98cd741c68a5c8f65b06c96d2f56d3b44979b3990335e7869b002c005ef80ba7"
-  revision 3
+  revision 4
 
   bottle do
     sha256 "6d6cde64cfab4577caee9af690c6203fc43d385e123bbf0669b747810756dd7b" => :el_capitan
