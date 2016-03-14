f12e65eb652def19b785e81e817dbca1099c6daf
Thierry Moisan <thierry.moisan@gmail.com>
2016-03-14 18:10:04 -0400

    clfft 2.10.0
    Closes #3443.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- clfft.rb
+++ clfft.rb
@@ -1,8 +1,8 @@
 class Clfft < Formula
   desc "FFT functions written in OpenCL"
   homepage "https://github.com/clMathLibraries/clFFT/"
-  url "https://github.com/clMathLibraries/clFFT/archive/v2.8.tar.gz"
-  sha256 "9964b537f0af121560e64cb5a2513b153511ca85e011f23a332c45cc66f3d60a"
+  url "https://github.com/clMathLibraries/clFFT/archive/v2.10.0.tar.gz"
+  sha256 "f70d8ae7b8c38f27b4f28e5e7abe55378e18c4f1c5cf8922b561dc908d36ffdc"
 
   bottle do
     cellar :any
@@ -13,9 +13,6 @@ class Clfft < Formula
 
   depends_on "cmake" => :build
   depends_on "boost" => :build
-
-  # https://github.com/clMathLibraries/clFFT/pull/127
-  patch :DATA
 
   def install
     mkdir "build"
@@ -31,39 +28,3 @@ class Clfft < Formula
     assert_match "(120.000000, 360.000000)", `./fft1d`
   end
 end
-
-__END__
-diff --git a/src/callback-client/client.h b/src/callback-client/client.h
-index 1b75e29..31c0264 100644
---- a/src/callback-client/client.h
-+++ b/src/callback-client/client.h
-@@ -93,6 +93,29 @@ public:
-     }
- };
-
-+#elif defined(__APPLE__) || defined(__MACOSX)
-+
-+#include <mach/clock.h>
-+#include <mach/mach.h>
-+
-+struct Timer
-+{
-+    clock_serv_t clock;
-+    mach_timespec_t start, end;
-+
-+public:
-+    Timer() { host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &clock); }
-+    ~Timer() { mach_port_deallocate(mach_task_self(), clock); }
-+
-+    void Start() { clock_get_time(clock, &start); }
-+    double Sample()
-+    {
-+        clock_get_time(clock, &end);
-+        double time = 1000000000L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-+        return time * 1E-9;
-+    }
-+};
-+
- #else
-
- #include <time.h>
