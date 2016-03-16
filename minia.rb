7cefaf067b278894acc3c30296b72df37c527abe
Shaun Jackman <sjackman@gmail.com>
2016-03-16 11:30:54 -0700

    minia 2.0.1
    Requires LaTeX to build the documentation.

--- minia.rb
+++ minia.rb
@@ -1,10 +1,11 @@
 class Minia < Formula
+  desc "Short-read assembler of a Bloom filter de Bruijn graph"
   homepage "http://minia.genouest.org/"
   # doi "10.1186/1748-7188-8-22"
   # tag "bioinformatics"
 
-  url "http://minia.genouest.org/files/minia-1.6906.tar.gz"
-  sha256 "74715bf28d41ca7a25b9c76fd568603a6d4814a193d44654582ca407e8b7f4b3"
+  url "http://gatb-tools.gforge.inria.fr/versions/src/minia-2.0.1-Source.tar.gz"
+  sha256 "528bd08891b0f0261b5ff7744ca7dd63704eb3426ee65f699086acd41306bcc3"
 
   bottle do
     cellar :any
@@ -13,13 +14,24 @@ class Minia < Formula
     sha256 "5858e2410ed92dd870e30e2d1ac8f373812ca472a7e3bd5d84ff5a05fd7249aa" => :mountain_lion
   end
 
+  option "with-docs", "Install documentation. Requires LaTeX"
+
+  depends_on "cmake" => :build
+  depends_on "imagemagic" => :build if build.with? "docs"
+
   def install
-    system "make"
-    bin.install "minia"
-    doc.install "README", "manual/manual.pdf"
+    mkdir "build" do
+      args = std_cmake_args
+      # Fix error: 'hdf5/hdf5.h' file not found
+      args.delete "-DCMAKE_BUILD_TYPE=Release"
+      args << "-DSKIP_DOC=1" if build.without? "docs"
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
+    end
   end
 
   test do
-    system "#{bin}/minia 2>&1 |grep -q minia"
+    assert_match "options", shell_output("#{bin}/minia")
   end
 end
