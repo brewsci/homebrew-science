2406fe96eab2b5fbae1029dedab9aeeee527c0fa
Shaun Jackman <sjackman@gmail.com>
2016-03-17 10:39:49 -0700

    smrtanalysis 2.3.0.140936 p5

--- smrtanalysis.rb
+++ smrtanalysis.rb
@@ -3,25 +3,25 @@ class Smrtanalysis < Formula
   homepage "http://www.pacb.com/products-and-services/analytical-software/smrt-analysis/"
   # doi "10.1038/nmeth.2474"
   # tag "bioinformatics"
-  url "https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377.run"
-  sha256 "17ee0ebce5450a8dcd3d0cfc3cd0676de53c55f7894f642f136da645485a66be"
+  url "http://files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis_2.3.0.140936.run"
+  sha256 "242cd175816949f2cd649244fa38b49045c86026244481d34a6f6d254c49f5cd"
 
   bottle :unneeded
 
   # Patch
   resource "patch" do
-    url "https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.2.0/smrtanalysis-2.2.0.133377-patch-3.run"
-    sha256 "4c661f790103668eef073f40b73e589e0f455683b12ec91901e81d4c6adbc49d"
+    url "https://s3.amazonaws.com/files.pacb.com/software/smrtanalysis/2.3.0/smrtanalysis-patch_2.3.0.140936.p5.run"
+    sha256 "171fcd51462da16c7cc30bf5ea665b511f68394c951d37510ac64af63aaf4d73"
   end
 
   def install
     raise "SMRT Analysis cannot be installed on Mac OS" if OS.mac?
-    system "sh", "./smrtanalysis-#{version}.run", "--extract-only",
+    system "sh", "./smrtanalysis_#{version}.run", "--extract-only",
       "--patchfile", resource("patch").cached_download,
       "--rootdir", prefix
   end
 
   test do
-    system "#{prefix}/install/smrtanalysis-#{version}/analysis/bin/blasr"
+    system "#{prefix}/install/smrtanalysis_#{version}/analysis/bin/blasr"
   end
 end
