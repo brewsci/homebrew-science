a10ffca04fdf51349275ee46b110e889beb2c11c
ilovezfs <ilovezfs@icloud.com>
2016-03-24 08:02:59 -0700

    ckon: bump revision for boost and fix test
    - attempting to run the test would throw
    
      dyld: Symbol not found: __ZN5boost9re_detail12perl_matcherIN9__gnu_cxx17__normal_iteratorIPKcSsEESaINS_9sub_matchIS6_EEENS_12regex_traitsIcNS_16cpp_regex_traitsIcEEEEE14construct_initERKNS_11basic_regexIcSD_EENS_15regex_constants12_match_flagsE
        Referenced from: /usr/local/Cellar/ckon/0.7.1/bin/ckon
          Expected in: /usr/local/lib/libboost_regex-mt.dylib
           in /usr/local/Cellar/ckon/0.7.1/bin/ckon
    
      so ckon needs a new revision built against the latest boost.
    
    - once rebuilt, the test was still slightly broken due to changes in the
      tested repository, so fix that, and peg the test to a specific commit
      revision of tschaume/BesCocktail
    
    Closes #3467.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- ckon.rb
+++ ckon.rb
@@ -1,7 +1,9 @@
 class Ckon < Formula
+  desc "C++ tool for data analyses in the CERN ROOT framework"
   homepage "https://tschaume.github.io/ckon/"
   url "https://github.com/tschaume/ckon/archive/v0.7.1.tar.gz"
   sha256 "4cc8bde10430e21520aed4b7ac5f6d96a80b8a91014760997f9a7039103a7e0d"
+  revision 1
   head "https://github.com/tschaume/ckon.git"
 
   bottle do
@@ -61,7 +63,7 @@ class Ckon < Formula
     Processing file "StRoot/BesCocktail/CmdLine.h"
        class "CmdLine" found at: 255
     Processing file "StRoot/BesCocktail/Database.h"
-       class "DatabaseManager" found at: 1728
+       class "DatabaseManager" found at: 1864
     Processing file "StRoot/BesCocktail/Functions.h"
        class "Functions" found at: 310
     Processing file "StRoot/BesCocktail/Simulation.h"
@@ -71,7 +73,7 @@ class Ckon < Formula
     Processing file "StRoot/BesCocktail/Analysis.h"
     Processing file "StRoot/BesCocktail/CmdLine.h"
     Processing file "StRoot/BesCocktail/Database.h"
-       namespace "YAML" found at: 737
+       namespace "YAML" found at: 756
     Processing file "StRoot/BesCocktail/Functions.h"
     Processing file "StRoot/BesCocktail/Simulation.h"
     Processing file "StRoot/BesCocktail/Utils.h"
@@ -82,6 +84,7 @@ class Ckon < Formula
     cd testpath
     mkdir "StRoot"
     system "git", "clone", "https://github.com/tschaume/BesCocktail.git", "StRoot/BesCocktail"
+    cd("StRoot/BesCocktail") { system "git", "checkout", "-q", "28446981a89cb851c43536200cc21310628aa555" }
     result = File.open(testpath/"ckon.out").read
     require "open3"
     Open3.popen3("#{bin}/ckon", "-v", "dry") do |_, stdout, _|
