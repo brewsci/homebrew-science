199763d7168b689db196c17ac99140048876e1c7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-28 03:03:51 +0100

    picard-tools: update 2.1.1 bottle.

--- picard-tools.rb
+++ picard-tools.rb
@@ -8,9 +8,9 @@ class PicardTools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "900f7c9862a456de3b34ade51f4f657d8a209b9e9b15cb2db72458f4916c5be3" => :el_capitan
-    sha256 "1d3e17a8122d0369882e2b660fe8b3066b2084358423bca34c4a1e5b1731fdd7" => :yosemite
-    sha256 "6a8c1e8e09b4a5881cfad8bd996286c6fc2b3d087163124bd4b5e7f2df48bb3e" => :mavericks
+    sha256 "ba34b255599802feca4996b7cfe7c619b4c5a0b99368f974dd6d2aaa3db8630e" => :el_capitan
+    sha256 "b4b3412c243acae498394393c0a61897d0440dc32b4b1240c784dfc68427d897" => :yosemite
+    sha256 "0065ca96c0051d196a5313d891ed2b2ffce6fe618c881a1c732d4a5b4790b0ac" => :mavericks
   end
 
   # head "https://github.com/broadinstitute/picard.git"
