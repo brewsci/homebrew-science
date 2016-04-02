d39693e7c3c41eb8b0d98b77d4361f757405cb9e
Rob Chekaluk <rchekaluk@ppolitics.org>
2016-04-02 17:10:47 -0400

    qualimap 28-03-16
    Closes #3496.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- qualimap.rb
+++ qualimap.rb
@@ -1,8 +1,9 @@
 class Qualimap < Formula
+  desc "Facilitates the quality control of alignment sequencing data"
   homepage "http://qualimap.bioinfo.cipf.es/"
-  url "https://bitbucket.org/kokonech/qualimap/downloads/qualimap-build-31-08-15.tar.gz"
-  sha256 "30a721b931b8e5b9304089edadbb15718bb9f2bfae70568836595196b4dd523e"
-  version "20150831"
+  url "https://bitbucket.org/kokonech/qualimap/downloads/qualimap-build-28-03-16.tar.gz"
+  version "20160328"
+  sha256 "12fd342a2ccc76ea09bbd5e9ae38c1ae9ae44aad5e6f0296298e584e55b0d8ae"
   bottle do
     cellar :any
     sha256 "312dbd9f9a0537ef4ab379d5d65d905455e7f01af827de75643d89beb0e305b0" => :yosemite
