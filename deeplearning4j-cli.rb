5416245d5c77641ba1014abc7d2067b7e9335550
Mikhail Zyatin <mikhail.zyatin@gmail.com>
2016-04-03 06:24:30 +0300

    deeplearning4j-cli 0.4-rc3.8 (new formula)
    Deeplearning4j is the distributed deep-learning library written for Java and Scala. This formula provides command-line API for it.
    
    Closes #3497.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

---
+++ deeplearning4j-cli.rb
@@ -0,0 +1,21 @@
+class Deeplearning4jCli < Formula
+  desc "Command-line interface for Deeplearning4j"
+  homepage "http://deeplearning4j.org/"
+  url "https://s3-us-west-2.amazonaws.com/skymind/bin/deeplearning4j-cli.tar.gz"
+  version "0.4-rc3.8"
+  sha256 "aaa664958f5d53570d02d75e7f61a491f5f75372d5721bf4f6c1efe31e971fcb"
+
+  bottle :unneeded
+
+  depends_on :java => "1.7+"
+
+  def install
+    inreplace "bin/dl4j", "$DIR/../lib", libexec
+    bin.install "bin/dl4j"
+    libexec.install Dir["lib/*"]
+  end
+
+  test do
+    system "#{bin}/dl4j"
+  end
+end
