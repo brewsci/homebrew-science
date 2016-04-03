5867ec9bc6ed043da57c3ba07141f7cd024437da
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-03 19:51:31 +0100

    qualimap: update 20160328 bottle.

--- qualimap.rb
+++ qualimap.rb
@@ -4,12 +4,14 @@ class Qualimap < Formula
   url "https://bitbucket.org/kokonech/qualimap/downloads/qualimap-build-28-03-16.tar.gz"
   version "20160328"
   sha256 "12fd342a2ccc76ea09bbd5e9ae38c1ae9ae44aad5e6f0296298e584e55b0d8ae"
+
   bottle do
-    cellar :any
-    sha256 "312dbd9f9a0537ef4ab379d5d65d905455e7f01af827de75643d89beb0e305b0" => :yosemite
-    sha256 "8ff12829b1a9ce2cf92a123dfe3e1eaa18b0c7f3929261ae93f6740878e75842" => :mavericks
-    sha256 "52cffc28c7504e6c6cb3757533b2f06ea9af32f965818273f93be9a2a5f5f957" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "87452d8d5cc0dc5a4a4a52952ac94c2804fa3935c2a66685026a595bc9d39c63" => :el_capitan
+    sha256 "91e74025f9f2a738abecbdb3529f0e70bf427402a5ab06f18f41e793c1a46d6f" => :yosemite
+    sha256 "5659d4fef01656d4e8e70394e125e579e1f67090c692434fad1f6244a578a234" => :mavericks
   end
+
   depends_on "r" => :optional
 
   def install
