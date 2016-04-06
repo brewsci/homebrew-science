f338684566ab2b36582b7db8a23b822fe4a3fd36
Sebastian Schöps <schoeps@temf.tu-darmstadt.de>
2016-04-06 23:12:07 +0200

    suite-sparse 4.5.1
    audits + ld path + manual install
    
    Thanks to @syarra.
    
    Closes #3515.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- suite-sparse.rb
+++ suite-sparse.rb
@@ -1,9 +1,8 @@
 class SuiteSparse < Formula
   desc "Suite of Sparse Matrix Software"
   homepage "http://faculty.cse.tamu.edu/davis/suitesparse.html"
-  url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.4.tar.gz"
-  sha256 "f2ae47e96f3f37b313c3dfafca59f13e6dbc1e9e54b35af591551919810fb6fd"
-  revision 2
+  url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.5.1.tar.gz"
+  sha256 "ac4524b9f69c4f8c2652d720b146c92a414c1943f86d46df49b4ff8377ae8752"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,32 +13,27 @@ class SuiteSparse < Formula
 
   option "with-matlab", "Install Matlab interfaces and tools"
   option "with-matlab-path=", "Path to Matlab executable (default: matlab)"
+  option "with-openmp", "Build with OpenMP support"
+
+  option "without-test", "Do not perform build-time tests (not recommended)"
 
   depends_on "tbb" => :recommended
   depends_on "openblas" => :optional
-  depends_on "metis4" => :optional # metis 5.x is not yet supported by suite-sparse
+
+  # SuiteSparse must be compiled with metis 5 and ships with metis-5.1.0.
+  # We prefer to use Homebrew metis.
+  depends_on "metis"
 
   depends_on :fortran if build.with? "matlab"
+  needs :openmp if build.with? "openmp"
 
   def install
-    # SuiteSparse doesn't like to build in parallel
-    ENV.deparallelize
+    cflags = [ENV.cflags.to_s]
+    cflags << "-fopenmp" if build.with? "openmp"
+    cflags << "-I#{Formula["tbb"].opt_include}" if build.with? "tbb"
 
-    # Switch to the Mac base config, per SuiteSparse README.txt
-    mv "SuiteSparse_config/SuiteSparse_config.mk",
-       "SuiteSparse_config/SuiteSparse_config_orig.mk"
-    mv "SuiteSparse_config/SuiteSparse_config_Mac.mk",
-       "SuiteSparse_config/SuiteSparse_config.mk"
+    make_args = ["CFLAGS=#{cflags.join " "}"]
 
-    cflags = "#{ENV.cflags}"
-    cflags += (ENV.compiler == :clang) ? "" : " -fopenmp"
-    cflags += " -I#{Formula["tbb"].opt_include}" if build.with? "tbb"
-
-    make_args = ["CFLAGS=#{cflags}",
-                 "INSTALL_LIB=#{lib}",
-                 "INSTALL_INCLUDE=#{include}",
-                 "RANLIB=echo",
-                ]
     if build.with? "openblas"
       make_args << "BLAS=-L#{Formula["openblas"].opt_lib} -lopenblas"
     elsif OS.mac?
@@ -52,44 +46,40 @@ class SuiteSparse < Formula
     make_args += ["SPQR_CONFIG=-DHAVE_TBB",
                   "TBB=-L#{Formula["tbb"].opt_lib} -ltbb"] if build.with? "tbb"
 
-    # SuiteSparse wants the user to download the source of METIS so it can
-    # compile it. Its way of deciding that METIS has indeed been downloaded is
-    # by checking that METIS_PATH is a valid path. So we specify some valid
-    # path.
-    make_args += ["METIS_PATH=#{Formula["metis4"].opt_prefix}",
-                  "METIS=-L#{Formula["metis4"].opt_lib} -lmetis"] if build.with? "metis4"
-    make_args << "CHOLMOD_CONFIG=-DNPARTITION" if build.without? "metis4"
+    # SuiteSparse is shipped with metis-5.1.0 but it can use Homebrew's version by
+    # setting MY_METIS_LIB and MY_METIS_INC variables.
+    make_args += ["MY_METIS_LIB=-L#{Formula["metis"].opt_lib} -lmetis",
+                  "MY_METIS_INC=#{Formula["metis"].opt_include}"]
 
-    # Add some flags for linux
-    # -DNTIMER is needed to avoid undefined reference to SuiteSparse_time
-    make_args << "CF=-fPIC -O3 -fno-common -fexceptions -DNTIMER $(CFLAGS)" unless OS.mac?
-
-    # make library doesn't build the demos because the latter expect the source
-    # of METIS to have been downloaded and will attempt to build it.
+    # Only building libraries
     system "make", "library", *make_args
-    lib.mkpath
-    include.mkpath
-    system "make", "install", *make_args
-    ["AMD", "CAMD", "CHOLMOD", "KLU", "LDL", "SPQR", "UMFPACK"].each do |pkg|
-      (doc/pkg).install Dir["#{pkg}/Doc/*"]
-    end
 
     if build.with? "matlab"
       matlab = ARGV.value("with-matlab-path") || "matlab"
       system matlab,
-             "-nodesktop", "-nosplash",
+             "-nojvm", "-nodisplay", "-nosplash",
              "-r", "run('SuiteSparse_install(false)'); exit;"
 
       # Install Matlab scripts and Mex files.
       %w[AMD BTF CAMD CCOLAMD CHOLMOD COLAMD CSparse CXSparse KLU LDL SPQR UMFPACK].each do |m|
-        (share / "suite-sparse/matlab/#{m}").install Dir["#{m}/MATLAB/*"]
+        (pkgshare/"matlab/#{m}").install Dir["#{m}/MATLAB/*"]
       end
 
-      mdest = share / "suite-sparse/matlab"
-      mdest.install "MATLAB_Tools"
-      mdest.install "RBio/RBio"
+      (pkgshare/"matlab").install "MATLAB_Tools"
+      (pkgshare/"matlab").install "RBio/RBio"
       (doc/"matlab").install Dir["MATLAB_Tools/Factorize/Doc/*"]
     end
+
+    prefix.install "include"
+    so = OS.mac? ? "dylib" : "so"
+    lib.install Dir["lib/*.#{so}"]
+
+    # Install docs and demos
+    %w[AMD CAMD CCOLAMD CHOLMOD COLAMD CXSparse KLU LDL SPQR UMFPACK].each do |m|
+      (pkgshare/"demo/#{m}").install Dir["#{m}/Demo/*"]
+    end
+    (pkgshare/"demo/CXSparse").install "CXSparse/Matrix"
+    doc.install Dir["share/doc/suitesparse-*/*"]
   end
 
   def caveats
@@ -98,13 +88,46 @@ class SuiteSparse < Formula
       s += <<-EOS.undent
         Matlab interfaces and tools have been installed to
 
-          #{share}/suite-sparse/matlab
-
-        It is possible that the SPQR interface fail to compile
-        if you use the defaults mexopts.sh or if your mexopts.sh
-        does not use gcc-4.9.
+          #{pkgshare}/matlab
       EOS
     end
     s
+  end
+
+  test do
+    cd testpath do
+      system ENV["CC"], "-o", "amd_demo", "-O",
+             pkgshare/"demo/AMD/amd_demo.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lamd"
+      system "./amd_demo"
+      system ENV["CC"], "-o", "camd_demo", "-O",
+             pkgshare/"demo/CAMD/camd_demo.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lcamd"
+      system "./camd_demo"
+      system ENV["CC"], "-o", "ccolamd_example", "-O",
+             pkgshare/"demo/CCOLAMD/ccolamd_example.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lccolamd"
+      system "./ccolamd_example"
+      system ENV["CC"], "-o", "cholmod_simple", "-O",
+             pkgshare/"demo/CHOLMOD/cholmod_simple.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lcholmod"
+      system "./cholmod_simple < #{pkgshare}/demo/CHOLMOD/Matrix/bcsstk01.tri"
+      system ENV["CC"], "-o", "colamd_example", "-O",
+             pkgshare/"demo/COLAMD/colamd_example.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lcolamd"
+      system "./colamd_example"
+      system ENV["CC"], "-o", "cs_demo1", "-O",
+             pkgshare/"demo/CXSparse/cs_demo1.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lcxsparse"
+      system "./cs_demo1 < #{pkgshare}/demo/CXSparse/Matrix/t1"
+      system ENV["CC"], "-o", "klu_simple", "-O",
+             pkgshare/"demo/KLU/klu_simple.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lklu"
+      system "./klu_simple"
+      system ENV["CC"], "-o", "umfpack_simple", "-O",
+             pkgshare/"demo/UMFPACK/umfpack_simple.c", "-L#{lib}", "-I#{include}",
+             "-lsuitesparseconfig", "-lumfpack"
+      system "./umfpack_simple"
+    end
   end
 end
