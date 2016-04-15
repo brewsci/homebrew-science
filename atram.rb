8b8030d98595ec8edddf568e8781952cbd0df77d
ilovezfs <ilovezfs@icloud.com>
2016-04-15 11:44:49 -0700

    atram: sandbox-compliant test
    Also,
    - fix audit errors.
    - temporarily disable bottles until trinity can build with GCC 5
    
    Closes #3548.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- atram.rb
+++ atram.rb
@@ -1,18 +1,15 @@
 class Atram < Formula
+  desc "Automated target restricted assembly method"
   homepage "https://github.com/juliema/aTRAM"
   # doi "10.5281/zenodo.10431"
   # tag "bioinformatics"
 
-  head "https://github.com/juliema/aTRAM.git"
-
   url "https://github.com/juliema/aTRAM/archive/v1.04.tar.gz"
   sha256 "ee05363885d1096e628582eebda4958c6316e7668da448af7ffe1b3f67286bea"
+  revision 1
+  head "https://github.com/juliema/aTRAM.git"
 
-  bottle do
-    sha256 "85c61052c018447043fb8ef1782a6350fbb753661b158095a6ade0bfcfdcab47" => :yosemite
-    sha256 "83f6244d599a0f89837709a154a186698a3020f78bcfd3a6980f196d647b9a23" => :mavericks
-    sha256 "6179c7a1ebe71914b577ebb9088e789f65a9190c0e384097d53b5d800d198e40" => :mountain_lion
-  end
+  bottle :disable, "Cannot currently build dependency Trinity with GCC-5 or Clang"
 
   depends_on "blast"
   depends_on "mafft"
@@ -23,11 +20,11 @@ class Atram < Formula
   def install
     prefix.install Dir["*"]
     cd prefix do
-      system "perl #{prefix}/configure.pl -no"
+      system "perl", prefix/"configure.pl", "-no"
     end
   end
 
   test do
-    system "perl #{prefix}/test/test_all.pl"
+    assert_match /No shards to be made/, shell_output("perl #{prefix}/test/test_format_sra.pl -input #{prefix}/test/test_sra.fasta")
   end
 end
