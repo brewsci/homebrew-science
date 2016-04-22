49e914e0c11257907f0a65b1ca83224ee853b6ec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-22 20:04:41 +0100

    nxtrim: update 0.4.0 bottle.

--- nxtrim.rb
+++ nxtrim.rb
@@ -9,9 +9,10 @@ class Nxtrim < Formula
   head "https://github.com/sequencing/NxTrim.git"
 
   bottle do
-    sha256 "0d38dfae7d61a540813ecf3c689e34ba33cee3e02b330f36b4d8886e69e7d14a" => :yosemite
-    sha256 "ce8c5c57dd26b897c0d436ec145614e58f2941345226e55215920581e3c961a7" => :mavericks
-    sha256 "7b9be71a0acea2373f8664f0a08ae672228afde4b90812a54016b61df76655d4" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "4ff85893ca6b912579bd0ca1cec049590ced304b4d64bac45c2fe16213f8e52d" => :el_capitan
+    sha256 "ba16dd96bc704c65fa99bc19a00f124aca51f9cee8fb8ba3aff049c6fb70d265" => :yosemite
+    sha256 "957d5b0449da272e80c329171f426ff3836dfae55144d76f6f3cf843b3174e72" => :mavericks
   end
 
   depends_on "boost"
