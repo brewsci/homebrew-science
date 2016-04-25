f93536457a60f7b61da3971a043a2bafc395cbbf
ilovezfs <ilovezfs@icloud.com>
2016-04-25 15:24:36 -0700

    express: use boost dynamic libs
    Fixes "libboost_thread-mt.a: error adding symbols: Bad value"
    
    Also,
    - bump revision
    - audit fixes
    - options for protobuf and gperftools
    
    Closes #3556.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- express.rb
+++ express.rb
@@ -1,5 +1,9 @@
 class Express < Formula
+  desc "Streaming quantification for sequencing"
   homepage "http://bio.math.berkeley.edu/eXpress/"
+  url "http://bio.math.berkeley.edu/eXpress/downloads/express-1.5.1/express-1.5.1-src.tgz"
+  sha256 "0c5840a42da830fd8701dda8eef13f4792248bab4e56d665a0e2ca075aff2c0f"
+  revision 1
   head "https://github.com/adarob/eXpress.git"
   bottle do
     cellar :any
@@ -11,14 +15,14 @@ class Express < Formula
   # doi "10.1038/nmeth.2251"
   # tag "bioinformatics"
 
-  url "http://bio.math.berkeley.edu/eXpress/downloads/express-1.5.1/express-1.5.1-src.tgz"
-  sha256 "0c5840a42da830fd8701dda8eef13f4792248bab4e56d665a0e2ca075aff2c0f"
-
   depends_on "bamtools"
   depends_on "boost"
   depends_on "cmake" => :build
+  depends_on "protobuf" => :recommended
+  depends_on "gperftools" => :optional
 
   def install
+    inreplace "CMakeLists.txt", "set(Boost_USE_STATIC_LIBS ON)", ""
     mkdir "bamtools"
     ln_s Formula["bamtools"].include/"bamtools", "bamtools/include"
     ln_s Formula["bamtools"].lib, "bamtools/"
@@ -27,6 +31,6 @@ class Express < Formula
   end
 
   test do
-    system "#{bin}/express 2>&1 |grep express"
+    assert_match version.to_s, shell_output("#{bin}/express 2>&1", 1)
   end
 end
