d24804102787d4146c191f977cf7eb15629f8ffd
Shaun Jackman <sjackman@gmail.com>
2016-04-28 20:05:52 -0700

    tabix: Add desc and tag bioinformatics
    Closes #3607.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- tabix.rb
+++ tabix.rb
@@ -1,7 +1,9 @@
 class Tabix < Formula
+  desc "Generic indexer for TAB-delimited genome position files"
   homepage "http://samtools.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/samtools/tabix/tabix-0.2.6.tar.bz2"
   sha256 "e4066be7101bae83bec62bc2bc6917013f6c2875b66eb5055fbb013488d68b73"
+  # tag "bioinformatics"
 
   head "https://samtools.svn.sourceforge.net/svnroot/samtools/trunk/tabix"
 
