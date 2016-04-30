2689f8ea13589d97275bad56e03f02351d4086a3
Dustin Lang <dstndstn@gmail.com>
2016-04-29 20:23:39 -0400

    Astrometry.net 0.67
    fix complaints from brew audit
    
    Closes #3613.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- astrometry-net.rb
+++ astrometry-net.rb
@@ -1,10 +1,12 @@
 class AstrometryNet < Formula
   desc "Automatic identification of astronomical images"
   homepage "http://astrometry.net"
-  url "http://astrometry.net/downloads/astrometry.net-0.64.tar.gz"
-  sha256 "4b9b8e9d7133a7bcc1f85298cf5762699dce70548be0cb91165286fc6677136d"
+  url "http://astrometry.net/downloads/astrometry.net-0.67.tar.gz"
+  sha256 "e351c81f7787550d42d45855db394a1702fd17c249ba934bdf4b6abf56281446"
 
   head "https://github.com/dstndstn/astrometry.net.git"
+
+  option "without-extras", "Don't try to build plotting code (actually it will still try, but homebrew won't halt the install if it fails)"
 
   depends_on "swig" => :build
   depends_on "pkg-config" => :build
@@ -24,18 +26,16 @@ class AstrometryNet < Formula
   # this formula includes python bindings
   depends_on :python => :recommended
 
-  option "without-extras", "Don't try to build plotting code (actually it will still try, but homebrew won't halt the install if it fails)"
-
   def install
     ENV.j1
-    ENV["INSTALL_DIR"] = "#{prefix}"
+    ENV["INSTALL_DIR"] = prefix.to_s
     ENV["NETPBM_INC"] = "-I#{Formula["netpbm"].opt_include}/netpbm"
     ENV["NETPBM_LIB"] = "-L#{Formula["netpbm"].opt_lib} -lnetpbm"
     ENV["SYSTEM_GSL"] = "yes"
 
     system "make"
-    system "make extra" if build.with? "extras"
-    system "make py"    if build.with? "python"
+    system "make", "extra" if build.with? "extras"
+    system "make", "py"    if build.with? "python"
     system "make", "install"
   end
 
