563e8dbe84b916af35720bf8d76775ff77763d75
Eric Hare <ericrhare@gmail.com>
2016-05-05 10:48:38 -0500

    nonpareil: revision bump

--- nonpareil.rb
+++ nonpareil.rb
@@ -14,6 +14,7 @@ class Nonpareil < Formula
   url "https://github.com/lmrodriguezr/nonpareil/archive/v2.4.01.tar.gz"
   sha256 "ca5955e877098ed4a679404ac87635e28a855d15d6970ca51a6be422266c0999"
   head "https://github.com/lmrodriguezr/nonpareil.git"
+  revision 1
 
   depends_on "r"
   depends_on :mpi => [:cxx, :optional]
