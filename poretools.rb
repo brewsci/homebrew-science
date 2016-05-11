b59ee06c2b3fb200548ea212f7017b3fa2e5de71
Tim D. Smith <git@tim-smith.us>
2016-05-10 20:42:21 -0700

    poretools: adjust dependencies
    Closes #3631. Closes #3485. Closes #2392. Closes #3642.

--- poretools.rb
+++ poretools.rb
@@ -2,9 +2,9 @@ class Poretools < Formula
   desc "Tools for working with nanopore sequencing data"
   homepage "http://poretools.readthedocs.org/"
   url "https://github.com/arq5x/poretools/archive/v0.5.1.tar.gz"
-  sha256 "7cd55a8f30290992dcf8d7792401e7f21efadfde02b3f4604d6680d2f8300479"
+  sha256 "5f547b014c6208ca14a2f95cc10eecc34f9a69edf44e693eade31f083da36b18"
   head "https://github.com/arq5x/poretools.git"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -13,69 +13,72 @@ class Poretools < Formula
     sha256 "791c1099beb63cec062399e8fb8f384688c018a3a7224d33d302220568b26d18" => :mavericks
   end
 
+  depends_on "pkg-config" => :build  # for h5py
+  depends_on "freetype"  # for matplotlib
   depends_on "hdf5"
-  depends_on "r"
+  depends_on :fortran  # for scipy
   depends_on :python if MacOS.version <= :snow_leopard
-  depends_on "openssl"
 
-  resource "rpy2" do
-    url "https://pypi.python.org/packages/source/r/rpy2/rpy2-2.5.6.tar.gz"
-    sha256 "d0d584c435b5ed376925a95a4525dbe87de7fa9260117e9f208029e0c919ad06"
+  cxxstdlib_check :skip
+
+  resource "Cycler" do
+    url "https://pypi.python.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
   end
 
   resource "Cython" do
-    url "http://cython.org/release/Cython-0.22.tar.gz"
-    sha256 "14307e7a69af9a0d0e0024d446af7e51cc0e3e4d0dfb10d36ba837e5e5844015"
+    url "https://pypi.python.org/packages/b1/51/bd5ef7dff3ae02a2c6047aa18d3d06df2fb8a40b00e938e7ea2f75544cac/Cython-0.24.tar.gz"
+    sha256 "6de44d8c482128efc12334641347a9c3e5098d807dd3c69e867fa8f84ec2a3f1"
   end
 
   resource "h5py" do
-    url "https://pypi.python.org/packages/source/h/h5py/h5py-2.5.0.tar.gz#md5=6e4301b5ad5da0d51b0a1e5ac19e3b74"
-    sha256 "9833df8a679e108b561670b245bcf9f3a827b10ccb3a5fa1341523852cfac2f6"
+    url "https://pypi.python.org/packages/22/82/64dada5382a60471f85f16eb7d01cc1a9620aea855cd665609adf6fdbb0d/h5py-2.6.0.tar.gz"
+    sha256 "b2afc35430d5e4c3435c996e4f4ea2aba1ea5610e2d2f46c9cae9f785e33c435"
+  end
+
+  resource "matplotlib" do
+    url "https://pypi.python.org/packages/8f/f4/c0c7e81f64d5f4d36e52e393af687f28882c53dcd924419d684dc9859f40/matplotlib-1.5.1.tar.gz"
+    sha256 "3ab8d968eac602145642d0db63dd8d67c85e9a5444ce0e2ecb2a8fedc7224d40"
+  end
+
+  resource "numpy" do
+    url "https://pypi.python.org/packages/1a/5c/57c6920bf4a1b1c11645b625e5483d778cedb3823ba21a017112730f0a12/numpy-1.11.0.tar.gz"
+    sha256 "a1d1268d200816bfb9727a7a27b78d8e37ecec2e4d5ebd33eb64e2789e0db43e"
+  end
+
+  resource "pandas" do
+    url "https://pypi.python.org/packages/11/09/e66eb844daba8680ddff26335d5b4fead77f60f957678243549a8dd4830d/pandas-0.18.1.tar.gz"
+    sha256 "d2e483692c7915916dffd1b83256ea9761b4224c8d45646ceddf48b977ee77b2"
+  end
+
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/41/45/1f1b5e0f58d9f5c4e60ca062704c033700d866958f5dc02531996239f5da/pyparsing-2.1.2.tar.gz"
+    sha256 "57754e38d618fb47fdd17d1ce7a2dc8cbb7986ab07363ce8dcfc57270e6c9a2a"
+  end
+
+  resource "python-dateutil" do
+    url "https://pypi.python.org/packages/3e/f5/aad82824b369332a676a90a8c0d1e608b17e740bbb6aeeebca726f17b902/python-dateutil-2.5.3.tar.gz"
+    sha256 "1408fdb07c6a1fa9997567ce3fcee6a337b39a503d80699e0f213de4aa4b32ed"
+  end
+
+  resource "pytz" do
+    url "https://pypi.python.org/packages/f4/7d/7c0c85e9c64a75dde11bc9d3e1adc4e09a42ce7cdb873baffa1598118709/pytz-2016.4.tar.bz2"
+    sha256 "ee7c751544e35a7b7fb5e3fb25a49dade37d51e70a93e5107f10575d7102c311"
+  end
+
+  resource "scipy" do
+    url "https://pypi.python.org/packages/16/87/fdd4d069b1e784d4598605c20d8a7c535883b298aef960dc286b395359d7/scipy-0.17.0.tar.gz"
+    sha256 "f600b755fb69437d0f70361f9e560ab4d304b1b66987ed5a28bdd9dd7793e089"
+  end
+
+  resource "seaborn" do
+    url "https://pypi.python.org/packages/05/70/727aea83084506060c5fe2b35b6a640ddf66e926a4b54d752542e031b3c3/seaborn-0.7.0.tar.gz"
+    sha256 "15a8b2747becfdb86cfa60b5fcfa9bb934e42ef0ced660e0d57e8aea741f7145"
   end
 
   resource "six" do
-    url "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz#md5=476881ef4012262dfc8adc645ee786c4"
-    sha256 "e24052411fc4fbd1f672635537c3fc2330d9481b18c0317695b46259512c91d5"
-  end
-
-  resource "watchdog" do
-    url "https://pypi.python.org/packages/source/w/watchdog/watchdog-0.8.3.tar.gz"
-    sha256 "7e65882adb7746039b6f3876ee174952f8eaaa34491ba34333ddf1fe35de4162"
-  end
-
-  resource "pathtools" do
-    url "https://pypi.python.org/packages/source/p/pathtools/pathtools-0.1.2.tar.gz"
-    sha256 "7c35c5421a39bb82e58018febd90e3b6e5db34c5443aaaf742b3f33d4655f1c0"
-  end
-
-  resource "argh" do
-    url "https://pypi.python.org/packages/source/a/argh/argh-0.26.1.tar.gz"
-    sha256 "06a7442cb9130fb8806fe336000fcf20edf1f2f8ad205e7b62cec118505510db"
-  end
-
-  resource "PyYAML" do
-    url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz#md5=f50e08ef0fe55178479d3a618efe21db"
-    sha256 "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"
-  end
-
-  resource "singledispatch" do
-    url "https://pypi.python.org/packages/source/s/singledispatch/singledispatch-3.4.0.3.tar.gz#md5=af2fc6a3d6cc5a02d0bf54d909785fcb"
-    sha256 "5b06af87df13818d14f08a028e42f566640aef80805c3b50c5056b086e3c2b9c"
-  end
-
-  resource "codetools" do
-    url "https://cran.r-project.org/src/contrib/Archive/codetools/codetools_0.2-11.tar.gz"
-    sha256 "b02e8b17ea9173b73c20e84fbd36c420d5a79bb56a6b9d0d45c22a7d540f54d5"
-  end
-
-  resource "MASS" do
-    url "https://cran.r-project.org/src/contrib/Archive/MASS/MASS_7.3-40.tar.gz"
-    sha256 "9e0c937162cb485511ce77c8519a21558370c20f3c6dc34493b895cb355cb516"
-  end
-
-  resource "ggplot2" do
-    url "https://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_1.0.1.tar.gz"
-    sha256 "40248e6b31307787e44e45d806e7a33095844a9bbe864cc7583dd311b19c241d"
+    url "https://pypi.python.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
   end
 
   resource "test" do
@@ -85,28 +88,21 @@ class Poretools < Formula
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    ENV.prepend_create_path "R_LIBS", libexec/"vendor/R/library"
+    ENV.prepend_create_path "PYTHONPATH", buildpath/"cython/lib/python2.7/site-packages"
     ENV.prepend_create_path "PATH", libexec/"vendor/bin"
+    ENV.prepend_create_path "PATH", buildpath/"cython/bin"
+    ENV["HDF5_DIR"] = Formula["hdf5"].opt_prefix
 
-    respy = %w[rpy2 Cython h5py six watchdog pathtools argh PyYAML singledispatch]
-    resr = %w[codetools MASS ggplot2]
+    res = resources.map(&:name).to_set - ["numpy", "Cython", "test"]
+    res = ["numpy"] + res.to_a
 
-    # install python dependencies
-    respy.each do |rp|
-      resource(rp).stage do
-        system "python", *Language::Python.setup_install_args(libexec/"vendor")
-      end
+    resource("Cython").stage do
+      system "python", *Language::Python.setup_install_args(buildpath/"cython")
     end
 
-    # install devtools
-    system "R", "-q", "-e", "install.packages('devtools', lib='" + libexec/"vendor/R/library" + "', repos='http://cran.r-project.org')"
-
-    # install r dependencies
-    ENV.prepend "LDFLAGS", "-L" + Formula["openssl"].opt_lib
-    ENV.prepend "CPPFLAGS", "-I" + Formula["openssl"].opt_include
-    resr.each do |rr|
-      resource(rr).stage do
-        system "R", "-q", "-e", "library(devtools); install(pkg='.', lib='" + libexec/"vendor/R/library" + "')"
+    res.each do |rp|
+      resource(rp).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
@@ -114,9 +110,8 @@ class Poretools < Formula
     ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
     system "python", *Language::Python.setup_install_args(libexec)
 
-    libexec.install Dir[libexec/"bin/*"]
-    libexec.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-    (bin/"poretools").write_env_script(libexec+"bin/poretools", :R_LIBS => ENV["R_LIBS"], :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 
     resource("test").stage { (pkgshare/"test_data").install Dir["*"] }
   end
