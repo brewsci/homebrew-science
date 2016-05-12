79056d2521ca9d6ab30bb21b4134828642ac33d2
Dominique Orban <dominique.orban@gmail.com>
2016-05-12 14:25:10 -0400

    glpk448: patch to expose all symbols
    Closes #3648.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- glpk448.rb
+++ glpk448.rb
@@ -4,6 +4,7 @@ class Glpk448 < Formula
   url "http://ftpmirror.gnu.org/glpk/glpk-4.48.tar.gz"
   mirror "https://ftp.gnu.org/gnu/glpk/glpk-4.48.tar.gz"
   sha256 "abc2c8f895b20a91cdfcfc04367a0bc8677daf8b4ec3f3e86c5b71c79ac6adb1"
+  revision 1
 
   bottle do
     cellar :any
@@ -16,6 +17,8 @@ class Glpk448 < Formula
 
   depends_on "gmp" => :recommended
 
+  patch DATA
+
   def install
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
     args << "--with-gmp" if build.with? "gmp"
@@ -23,6 +26,8 @@ class Glpk448 < Formula
     system "make"
     system "make", "check"
     system "make", "install"
+    include.install "src/amd/amd.h"
+    include.install "src/colamd/colamd.h"
   end
 
   test do
@@ -40,3 +45,33 @@ class Glpk448 < Formula
     assert_equal `./test`, version.to_s
   end
 end
+
+__END__
+diff --git a/src/Makefile.am b/src/Makefile.am
+index cec1f74..9e20042 100644
+--- a/src/Makefile.am
++++ b/src/Makefile.am
+@@ -4,8 +4,7 @@ include_HEADERS = glpk.h
+
+ lib_LTLIBRARIES = libglpk.la
+
+-libglpk_la_LDFLAGS = -version-info 33:0:0 \
+--export-symbols-regex '^(glp_|_glp_lpx_).*'
++libglpk_la_LDFLAGS = -version-info 33:0:0
+
+ libglpk_la_SOURCES = \
+ glpapi01.c \
+diff --git a/src/Makefile.in b/src/Makefile.in
+index 6e61555..4d112dc 100644
+--- a/src/Makefile.in
++++ b/src/Makefile.in
+@@ -264,8 +264,7 @@ top_builddir = @top_builddir@
+ top_srcdir = @top_srcdir@
+ include_HEADERS = glpk.h
+ lib_LTLIBRARIES = libglpk.la
+-libglpk_la_LDFLAGS = -version-info 33:0:0 \
+--export-symbols-regex '^(glp_|_glp_lpx_).*'
++libglpk_la_LDFLAGS = -version-info 33:0:0
+
+ libglpk_la_SOURCES = \
+ glpapi01.c \
