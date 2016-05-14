ecbfe4fff999a1a586b36b287c88e46958134a8c
Kozo Nishida <knishida@riken.jp>
2016-05-15 02:33:49 +0900

    cytoscape 3.4.0
    Closes #3652.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- cytoscape.rb
+++ cytoscape.rb
@@ -3,8 +3,8 @@ class Cytoscape < Formula
   homepage "http://www.cytoscape.org/"
   # tag "bioinformatics"
   # doi "10.1038/nmeth.2212"
-  url "http://chianti.ucsd.edu/cytoscape-3.3.0/cytoscape-3.3.0.tar.gz"
-  sha256 "e5270c50d34b344c25f470a0064ab823bf0648c0609e2fb36b8d8fd7e44d95dd"
+  url "http://chianti.ucsd.edu/cytoscape-3.4.0/cytoscape-3.4.0.tar.gz"
+  sha256 "e20a04b031818005090bd65b78bb08813b7a8e018c73496d41f2f00014d6ae18"
 
   bottle do
     cellar :any_skip_relocation
@@ -17,7 +17,7 @@ class Cytoscape < Formula
 
   def install
     inreplace "cytoscape.sh", "$script_path", prefix
-    prefix.install %W[cytoscape.sh gen_vmoptions.sh framework sampleData]
+    prefix.install %W[cytoscape.sh apps gen_vmoptions.sh framework sampleData]
     bin.install_symlink prefix/"cytoscape.sh" => "cytoscape"
   end
 
