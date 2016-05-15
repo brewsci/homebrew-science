c4f648f3e62d71a4e8f060c3e6c2220e9b035d6a
ilovezfs <ilovezfs@icloud.com>
2016-05-15 02:05:06 -0700

    express: disable bottles
    Attempt to prevent "Illegal instruction: 4" during CI
    
    Closes #3655.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- express.rb
+++ express.rb
@@ -3,18 +3,13 @@ class Express < Formula
   homepage "http://bio.math.berkeley.edu/eXpress/"
   url "http://bio.math.berkeley.edu/eXpress/downloads/express-1.5.1/express-1.5.1-src.tgz"
   sha256 "0c5840a42da830fd8701dda8eef13f4792248bab4e56d665a0e2ca075aff2c0f"
-  revision 2
+  revision 3
   head "https://github.com/adarob/eXpress.git"
-
-  bottle do
-    cellar :any
-    sha256 "ecba8b9a720149f90e8113871f82f2d99ac1688d5a28c9e97b4c2b79df0dd5f6" => :el_capitan
-    sha256 "eebcf8cdd0b5253aab314be0373728d1946b8436cf4760c1188d6abf95233ca5" => :yosemite
-    sha256 "35d07bd4df9794a852220592c16a795aa667ab5a9c5f822d52a0e338d1125a87" => :mavericks
-  end
 
   # doi "10.1038/nmeth.2251"
   # tag "bioinformatics"
+
+  bottle :disable, "Work around 'Illegal instruction: 4' during CI"
 
   depends_on "bamtools"
   depends_on "boost"
