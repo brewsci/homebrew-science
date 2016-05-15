382bf7bca7846cdc2707d8f2a1aadde62f8c01a7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-15 21:44:29 +0100

    corset: update 1.05 bottle.

--- corset.rb
+++ corset.rb
@@ -8,10 +8,10 @@ class Corset < Formula
   sha256 "a4902035ad58e9a5896fe1951de83c3d6b64759919589ecc27cf714c6acfc487"
 
   bottle do
-    cellar :any
-    sha256 "977bc8dd872fedd86bc869f09d1d7262db98a424a239678e54f0744d058dfe0b" => :yosemite
-    sha256 "70ee1716feec3c4510959d64c3a57f7a1b66c520cebeaa9b1af3c6ee91d94ae2" => :mavericks
-    sha256 "0d9c81f718dc245992994db69d1584cac1f75827ee1c48b96fb06b79660e58e5" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "67000ee4879523d782b8a57ec94772a863dae25fd6c29fe573cfc9c1be5c8be1" => :el_capitan
+    sha256 "ee530d4840361f257f7206097d995c04fa8745348e1999628582b9d54b86c80f" => :yosemite
+    sha256 "f6a43255fa86b7cb67ddd6abd614f05c349a3043225f81cfed1edfeae99ae818" => :mavericks
   end
 
   depends_on "samtools"
