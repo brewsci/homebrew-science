7f366cd850bbaf2532a49b7b483f9b3ac0b629a0
ilovezfs <ilovezfs@icloud.com>
2016-05-15 06:13:17 -0700

    smalt: install the manual
    - download the manual as a resource and install it in `doc`
    - add description to fix audit failure
    - update the homepage
    - match against the version not just the author's name
    
    Closes #3658.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- smalt.rb
+++ smalt.rb
@@ -1,9 +1,11 @@
 class Smalt < Formula
-  homepage "https://www.sanger.ac.uk/resources/software/smalt/"
+  desc "Aligns DNA sequencing reads with a reference genome"
+  homepage "http://www.sanger.ac.uk/science/tools/smalt-0"
   # tag "bioinformatics"
 
   url "https://downloads.sourceforge.net/project/smalt/smalt-0.7.6.tar.gz"
   sha256 "89ccdfe471edba3577b43de9ebfdaedb5cd6e26b02bf4000c554253433796b31"
+  revision 1
 
   bottle do
     cellar :any
@@ -12,13 +14,19 @@ class Smalt < Formula
     sha256 "7d2a0d443996d67ef94bbb1a318f108f2ea8a714a9942892c40b94392bd0a8e4" => :mountain_lion
   end
 
+  resource "manual" do
+    url "https://downloads.sourceforge.net/project/smalt/smalt_manual.pdf"
+    sha256 "efd52e8429237b27797027477c33e1071f6247616d7705106af256e48307480a"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+    doc.install resource("manual")
   end
 
   test do
-    assert_match "Hannes", shell_output("smalt version")
+    assert_match version.to_s, shell_output("smalt version")
   end
 end
