64c046e4a1807c5b94804625d15e631600948a34
ilovezfs <ilovezfs@icloud.com>
2016-05-18 10:13:57 -0700

    blast 2.3.0
    - version bump
    - fix the `without-static` build
    - change the option `without-static` to `with-static`
    - remove `without-check` since the tests won't build with >= Boost 1.59
    - remove the patches that upstream has merged
    - modify the test-do to match against the version
    
    Closes #3537.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- blast.rb
+++ blast.rb
@@ -4,11 +4,10 @@ class Blast < Formula
   # doi "10.1016/S0022-2836(05)80360-2"
   # tag "bioinformatics"
 
-  url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-src.tar.gz"
-  mirror "http://mirrors.vbi.vt.edu/mirrors/ftp.ncbi.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-src.tar.gz"
-  version "2.2.31"
-  sha256 "f0960e8af2a6021fde6f2513381493641f687453a804239a7e598649b432f8a5"
-  revision 1
+  url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.3.0/ncbi-blast-2.3.0+-src.tar.gz"
+  mirror "http://mirrors.vbi.vt.edu/mirrors/ftp.ncbi.nih.gov/blast/executables/blast+/2.3.0/ncbi-blast-2.3.0%2b-src.tar.gz"
+  version "2.3.0"
+  sha256 "7ce8dc62f58141b6cdcd56b55ea3c17bea7a672e6256dfd725e6ef94825e94e9"
 
   bottle do
     sha256 "b0e39942bafa2b3305043d5dd43ddc0e63636bfe98342a69502dfa2ac9919eea" => :el_capitan
@@ -19,33 +18,34 @@ class Blast < Formula
   # Fix configure: error: Do not know how to build MT-safe with compiler g++-5 5.1.0
   fails_with :gcc => "5"
 
-  # Build failure reported to toolbox@ncbi.nlm.nih.gov on 11 May 2015,
-  # patch provided by developers; should be included in next release
-  patch :p0, :DATA
+  # Due to boost 1.58
+  fails_with :llvm do
+    build 2335
+    cause "Dropped arguments to functions when linking with boost"
+  end
 
-  option "without-static", "Build without static libraries & binaries"
+  option "with-static", "Build without static libraries and binaries"
   option "with-dll", "Build dynamic libraries"
-  option "without-check", "Skip the self tests (Boost not needed)"
 
-  depends_on "boost" if build.with? "check"
   depends_on "freetype" => :optional
-  depends_on "gnutls"   => :optional
-  depends_on "hdf5"     => :optional
-  depends_on "jpeg"     => :recommended
-  depends_on "libpng"   => :recommended
-  depends_on "lzo"      => :optional
-  depends_on "pcre"     => :recommended
-  depends_on :mysql     => :optional
+  depends_on "gnutls" => :optional
+  depends_on "hdf5" => :optional
+  depends_on "jpeg" => :recommended
+  depends_on "libpng" => :recommended
+  depends_on "lzo" => :optional
+  depends_on :mysql => :optional
+  depends_on "pcre" => :recommended
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    # Fix error:
-    # /bin/sh: line 2: /usr/bin/basename: No such file or directory
-    # See http://www.ncbi.nlm.nih.gov/viewvc/v1?view=revision&revision=65204
-    inreplace "c++/src/build-system/Makefile.in.top", "/usr/bin/basename", "basename"
-
-    # Move libraries to libexec. Libraries and headers conflict with ncbi-c++-toolkit.
-    args = %W[--prefix=#{prefix} --libdir=#{libexec} --without-debug --with-mt]
+    # The libraries and headers conflict with ncbi-c++-toolkit so use libexec.
+    args = %W[
+      --prefix=#{prefix}
+      --libdir=#{libexec}
+      --without-debug
+      --with-mt
+      --without-boost
+    ]
 
     args << (build.with?("mysql") ? "--with-mysql" : "--without-mysql")
     args << (build.with?("freetype") ? "--with-freetype=#{Formula["freetype"].opt_prefix}" : "--without-freetype")
@@ -63,53 +63,34 @@ class Blast < Formula
       args << "--with-dll" if build.with? "dll"
     end
 
-    # Boost is used only for unit tests.
-    args << (build.with?("check") ? "--with-check" : "--without-boost")
+    cd "c++"
 
-    cd "c++" do
-      system "./configure", *args
-      system "make"
-      system "make", "install"
+    # The build invokes datatool but its linked libraries aren't installed yet.
+    ln_s buildpath/"c++/ReleaseMT/lib", prefix/"libexec" if build.without? "static"
 
-      # Remove headers. Libraries and headers conflict with ncbi-c++-toolkit.
-      rm_r include
-    end
+    system "./configure", *args
+    system "make"
+
+    rm prefix/"libexec" if build.without? "static"
+
+    system "make", "install"
+
+    # The libraries and headers conflict with ncbi-c++-toolkit.
+    libexec.install include
   end
 
   def caveats; <<-EOS.undent
-    Using the option '--without-static' will create dynamic binaries instead of
-    static. The NCBI Blast static installation is approximately 7 times larger
+    Using the option "--with-static" will create static binaries instead of
+    dynamic. The NCBI Blast static installation is approximately 7 times larger
     than the dynamic.
 
-    Static binaries should be used for speed if the executable requires
-    fast startup time, such as if another program is frequently restarting
-    the blast executables.
+    Static binaries should be used for speed if the executable requires fast
+    startup time, such as if another program is frequently restarting the blast
+    executables.
     EOS
   end
 
   test do
-    system bin/"blastn", "-version"
+    assert_match version.to_s, shell_output("#{bin}/blastn -version")
   end
 end
-
-__END__
---- c++/include/corelib/ncbimtx.inl (revision 467211)
-+++ c++/include/corelib/ncbimtx.inl (working copy)
-@@ -388,7 +388,17 @@
-     _ASSERT(m_Lock);
-
-     m_ObjLock.Lock();
--    m_Listeners.remove(TRWLockHolder_ListenerWeakRef(listener));
-+    // m_Listeners.remove(TRWLockHolder_ListenerWeakRef(listener));
-+    // The above gives strange errors about invalid operands to operator==
-+    // with the Apple Developer Tools release containing Xcode 6.3.1 and
-+    // "Apple LLVM version 6.1.0 (clang-602.0.49) (based on LLVM 3.6.0svn)".
-+    // The below workaround should be equivalent.
-+    TRWLockHolder_ListenerWeakRef ref(listener);
-+    TListenersList::iterator it;
-+    while ((it = find(m_Listeners.begin(), m_Listeners.end(), ref))
-+           != m_Listeners.end()) {
-+        m_Listeners.erase(it);
-+    }
-     m_ObjLock.Unlock();
- }
