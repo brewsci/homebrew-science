e0d6bf6e3864f0820c239bce38d0b6b6adce1db7
ilovezfs <ilovezfs@icloud.com>
2016-05-18 09:36:29 -0700

    hmmer: trigger CI
    Closes #3684.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- hmmer.rb
+++ hmmer.rb
@@ -6,6 +6,7 @@ class Hmmer < Formula
 
   url "http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz"
   sha256 "dd16edf4385c1df072c9e2f58c16ee1872d855a018a2ee6894205277017b5536"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,18 +16,19 @@ class Hmmer < Formula
     sha256 "96ddaa4a6cedbb776bf94aeff38c102d5c5fd619e89056ba6d6fa645c946259f" => :mountain_lion
   end
 
-  option "without-check", "Skip build-time tests (not recommended)"
-
   head do
     url "https://svn.janelia.org/eddylab/eddys/src/hmmer/trunk"
     depends_on "autoconf" => :build
   end
 
+  option "without-test", "Skip build-time tests (not recommended)"
+  deprecated_option "without-check" => "without-test"
+
   def install
     system "autoconf" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make", "check" if build.with? "check"
+    system "make", "check" if build.with? "test"
     system "make", "install"
 
     # Installing libhmmer.a causes trouble for infernal.
