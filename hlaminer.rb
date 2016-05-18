3662d40fafb9acebc720e15d02c3e468ac746485
ilovezfs <ilovezfs@icloud.com>
2016-05-18 03:06:50 -0700

    hlaminer: trigger CI
    Also,
    
    - add description to prevent audit failure
    - have test match against the version
    - don't install non-executables in `bin`; use `pkgshare/"scripts"`
    - install other items in more standard locations
    
    Closes #3679.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- hlaminer.rb
+++ hlaminer.rb
@@ -1,4 +1,5 @@
 class Hlaminer < Formula
+  desc "HLA predictions from next-generation shotgun (NGS) sequence read data"
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer"
   # doi "10.1186/gm396"
   # tag "bioinformatics"
@@ -6,6 +7,7 @@ class Hlaminer < Formula
   url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.1/HLAminer.tar.gz"
   version "1.1.0"
   sha256 "2c3e4458215f06764f58e8f3d95fb8e3a47d2a19152478d047453944c8cb84c3"
+  revision 1
 
   bottle do
     cellar :any
@@ -18,13 +20,14 @@ class Hlaminer < Formula
   depends_on "tasr"
 
   def install
-    # Conflicts with tasr
-    rm "bin/TASR"
-
-    prefix.install Dir["*"]
+    bin.install Dir["bin/*.pl"]
+    (pkgshare/"scripts").install Dir["bin/*"]
+    pkgshare.install "database"
+    pkgshare.install "test-demo"
+    doc.install Dir["docs/*"]
   end
 
   test do
-    system "#{bin}/HLAminer.pl |grep hlaminer"
+    assert_match "HLAminer.pl [v1.1]", shell_output("#{bin}/HLAminer.pl", 255)
   end
 end
