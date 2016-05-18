92b8f186b17a11ac26bf88c18e8715e0029a1ebc
ilovezfs <ilovezfs@icloud.com>
2016-05-18 09:15:02 -0700

    rmblast: trigger CI
    Also,
    
    - add a description to prevent audit failure
    - put bottle block below url and revision
    - match against version in the test
    
    Closes #3681.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- rmblast.rb
+++ rmblast.rb
@@ -1,15 +1,9 @@
 class Rmblast < Formula
+  desc "RepeatMasker compatible version of the standard NCBI BLAST suite"
   homepage "http://www.repeatmasker.org/RMBlast.html"
   # tag "bioinformatics"
 
   version "2.2.28"
-  bottle do
-    cellar :any
-    sha256 "5d9cf1a5413fd7c28c32652b846eb8b1ddf8bf6014dcd10ccf8c29df85b36231" => :yosemite
-    sha256 "9b98e8e75a24c36c9dc2ecbd5db33953295b1b42a081faa5ae092e8b8b735356" => :mavericks
-    sha256 "02dd45f7669741ef273f314eccc57ddd17a222beee934aea6cf3778dc0101f70" => :mountain_lion
-  end
-
   if OS.mac?
     url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-#{version}-universal-macosx.tar.gz"
     sha256 "f94e91487b752eb24386c3571250a3394ec7a00e7a5370dd103f574c721b9c81"
@@ -19,6 +13,14 @@ class Rmblast < Formula
   else
     raise "Unknown operating system"
   end
+  revision 1
+
+  bottle do
+    cellar :any
+    sha256 "5d9cf1a5413fd7c28c32652b846eb8b1ddf8bf6014dcd10ccf8c29df85b36231" => :yosemite
+    sha256 "9b98e8e75a24c36c9dc2ecbd5db33953295b1b42a081faa5ae092e8b8b735356" => :mavericks
+    sha256 "02dd45f7669741ef273f314eccc57ddd17a222beee934aea6cf3778dc0101f70" => :mountain_lion
+  end
 
   depends_on "blast" => :recommended
 
@@ -27,6 +29,6 @@ class Rmblast < Formula
   end
 
   test do
-    system "#{bin}/rmblastn", "-help"
+    assert_match version.to_s, shell_output("#{bin}/rmblastn -version")
   end
 end
