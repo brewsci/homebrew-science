89a3cbe4eb0c7ad9e7c9325b13e770ea5ae8b0b8
Shaun Jackman <sjackman@gmail.com>
2016-06-02 08:31:34 -0700

    seqtk: update 1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#4.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- seqtk.rb
+++ seqtk.rb
@@ -13,6 +13,7 @@ class Seqtk < Formula
     sha256 "3f9f294d0dc6e875928753bda692c5f0a13290bd2f16f06642921640d9539600" => :el_capitan
     sha256 "3ba2a3a19f2b128a9b41130d023254bb16688cd2c1945b237ca20aba89023968" => :yosemite
     sha256 "5e32f0a3a7dfdf836c1ed54377136459772011bb1829fc7a8695064b8c4badb3" => :mavericks
+    sha256 "e4c54fa49b50a2e9a69bd60aa31b707cb053e728f7f285f224e1f20ad40dc398" => :x86_64_linux
   end
 
   def install
