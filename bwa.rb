928f4752ef49197a71672709460ff67bf81c0fb5
Shaun Jackman <sjackman@gmail.com>
2016-06-02 09:56:01 -0700

    bwa: update 0.7.13 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#7.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bwa.rb
+++ bwa.rb
@@ -14,6 +14,7 @@ class Bwa < Formula
     sha256 "4d285efa2136c1345ff08dc8b2dbc83e8703fe8f3ee7b163d2b10744012ab9cf" => :el_capitan
     sha256 "97358a24e7bdc93005ecff8a54145553592ddded04f354ed2451b1d82e59122f" => :yosemite
     sha256 "ce2ae47bdf1d5995bb6365e4caed2a8b98b5ffa816b7aeff232ed58b86a25fc8" => :mavericks
+    sha256 "b54a5bbfdb239106ff18b1b0ac73f9f183c0fbc907c5d6134c489628830dc3dd" => :x86_64_linux
   end
 
   def install
