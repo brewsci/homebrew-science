df8a1386c9b82fc53cf1d771c42855a6b14e6423
Shaun Jackman <sjackman@gmail.com>
2016-06-03 15:26:14 -0700

    fermi2: update 0.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#74.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fermi2.rb
+++ fermi2.rb
@@ -9,6 +9,7 @@ class Fermi2 < Formula
     sha256 "ca79efffdf75b8943314d713357fccef22c2d66da853cf91457fff66430e78ac" => :yosemite
     sha256 "e3c3944c79d7f857ac5a8a67ec778d4e199ef85b4d18b56314d84eecb0404645" => :mavericks
     sha256 "5037d2613a91c6662e9b5d405a012fb898da42edc857c38f2661d4b3b9c334de" => :mountain_lion
+    sha256 "68b30acd0bf8f8d7009bf46faab53f9832570db92ff830eb0d6178a624599b79" => :x86_64_linux
   end
 
   depends_on "ropebwt2"
