06c9a8e3b8a9bd7e99fdd8ea70c262b14d1018e3
Shaun Jackman <sjackman@gmail.com>
2016-06-03 14:13:19 -0700

    bioawk: update 1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#39.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bioawk.rb
+++ bioawk.rb
@@ -13,6 +13,7 @@ class Bioawk < Formula
     sha256 "ebc6d6a5e8558719af2550297f1efd99e9e9da1e9f93ea9e908df1886fd99cb1" => :el_capitan
     sha256 "eec121fdc26539ba0b1f2700d8202cc6fb9b235d109af9827b22318337f32e28" => :yosemite
     sha256 "f4ce3593f5495e47a07c77070c29b0184f2f22d641a90d2f5602ffb4d1f3f78c" => :mavericks
+    sha256 "6e03d465a7db4cb150038aedb0bec84027e3756243bb6ad021ee314c40f1570a" => :x86_64_linux
   end
 
   depends_on "bison" => :build
