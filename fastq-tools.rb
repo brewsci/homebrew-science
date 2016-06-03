43615b27daa61bf91fe73eb8731dc7a139c1cb3a
Shaun Jackman <sjackman@gmail.com>
2016-06-03 15:33:15 -0700

    fastq-tools: update 0.7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#78.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fastq-tools.rb
+++ fastq-tools.rb
@@ -10,6 +10,7 @@ class FastqTools < Formula
     sha256 "e86ffa61e35cbd3b14228a6183b3c472323c6752d47c2586cd41516a6f5c9d6e" => :yosemite
     sha256 "ee21d34a811b294d11b809bdd22abbdf5cb5b0c7ed855d74592565f9c9c40c04" => :mavericks
     sha256 "b9f6d5213d969eafadfd1d82171a296c145a67afc70f758445e919a2ea31e56a" => :mountain_lion
+    sha256 "1674cbecb09313439c129349cbd0920a77c12aa36934b47a5709c91e3f457041" => :x86_64_linux
   end
 
   depends_on "pcre" => :build
