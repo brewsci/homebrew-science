cc7d96b3c96c272accce78c7d901b658b8f5b7eb
Shaun Jackman <sjackman@gmail.com>
2016-06-03 15:40:27 -0700

    fasttree: update 2.1.8 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#80.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- fasttree.rb
+++ fasttree.rb
@@ -12,6 +12,7 @@ class Fasttree < Formula
     sha256 "aa51e0bac1e69fdcca6b7bc3ce2f991cd8ebddccda9bd017dd9d7c97c37d3b9f" => :yosemite
     sha256 "8fc2e85716afb2b6230faa5ddf48398cc3e8905106a0d429193add343e24dbc4" => :mavericks
     sha256 "4118a55ddf727c3069c9380d164d716e403554b2dff6b5bb833686cb21a3ec19" => :mountain_lion
+    sha256 "642dfb0168fb2ebfcb8314206da71aa9d4aa70c7d4ee7d77fe1a882e2f4e0c3b" => :x86_64_linux
   end
 
   option "with-double", "Use double precision maths for accurate branch lengths (disables SSE)"
