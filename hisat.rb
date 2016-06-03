c86c38456bd6e5a7185d146f8ba86d0e6fdb82d4
Shaun Jackman <sjackman@gmail.com>
2016-06-03 16:17:32 -0700

    hisat: update 0.1.6b bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#101.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- hisat.rb
+++ hisat.rb
@@ -11,6 +11,7 @@ class Hisat < Formula
     sha256 "e34b8f4f30502f56398a2d261bb7da2ff3a597c3ca52a4feb4e460bb99006e8f" => :yosemite
     sha256 "8d6569d480eaddde24e587df969e177a8fef78430aece34fb20e25429878c5ac" => :mavericks
     sha256 "27f1b26e3d83bc9ac3275b2b5d80c69205043813efd8677755ec671791d6cb89" => :mountain_lion
+    sha256 "4b4aaf7061565de3833c7d075b973d1420c5d7d859f0c8291a46a6ee317d49ae" => :x86_64_linux
   end
 
   def install
