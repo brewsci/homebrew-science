64fefa2b2de05d36ca490d00bb10c46e05da1c37
Shaun Jackman <sjackman@gmail.com>
2016-06-04 13:46:35 -0700

    phylip: update 3.696 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#165.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- phylip.rb
+++ phylip.rb
@@ -11,6 +11,7 @@ class Phylip < Formula
     sha256 "de6dce855888ca1ea007f6492a104d80bd261579f9d2bb0320f98bedfa50cfc8" => :yosemite
     sha256 "c13101048ff00f36319cbe601669275f2e79810fa67826f7850b71f05b714b3c" => :mavericks
     sha256 "72d274eb537a6949a4832809b0912050ca25853f439c127f4ac0c7fe059e1768" => :mountain_lion
+    sha256 "059d29ea0a4d231c17be873e8864ae1e7d6568b5b1a858e4f96ce37fd9614e5a" => :x86_64_linux
   end
 
   def install
