26fb31707b1008dbae01e6176f0fdb860ac49e16
Shaun Jackman <sjackman@gmail.com>
2016-06-04 13:44:12 -0700

    pear: update 0.9.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#164.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- pear.rb
+++ pear.rb
@@ -11,6 +11,7 @@ class Pear < Formula
     sha256 "a41b6229ea725bf74c1c6cf2383f55d21b91cd98197f441ca9c7ec6f0fa1416f" => :yosemite
     sha256 "d23c4c17880e9eef24ce46c2ab55662e59ec0bf69da43be96fd19158c7001efc" => :mavericks
     sha256 "426c5620df6007bfe8ea65181ebcfecf4b0f63cb657e346296af21a994e88d7e" => :mountain_lion
+    sha256 "377d1990fe1f56ad5dcb2ea9c4729d9140fbb18ff46590874b1685ce005a4c62" => :x86_64_linux
   end
 
   def install
