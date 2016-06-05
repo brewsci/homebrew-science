62d696d1ee6fafb685ae6ae1d6d3d7002f1fc144
Shaun Jackman <sjackman@gmail.com>
2016-06-05 12:19:36 -0700

    repeatscout: update 1.0.5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#181.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- repeatscout.rb
+++ repeatscout.rb
@@ -11,6 +11,7 @@ class Repeatscout < Formula
     sha256 "3736ad2e736c526c1b485d84fd59aaf644d371d9718fdb7ccafe59efcdbd11f4" => :yosemite
     sha256 "f77ed5e45329577b5042585d0811f6bfc42771c07c14606b5f93502967463719" => :mavericks
     sha256 "3d2fbc43ac051ac3ffe8c699b4cb702fc8841440985bb720925595c24d99b378" => :mountain_lion
+    sha256 "3e23d52321796318510d560e0efb1467340c1e7221503981ddf004d10f329661" => :x86_64_linux
   end
 
   depends_on "trf" => :optional
