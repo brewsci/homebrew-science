bb4881aa04c6c4e2c0159b2c72bc30cd6049a196
Shaun Jackman <sjackman@gmail.com>
2016-06-07 15:43:52 -0700

    skewer: update 0.1.126 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#203.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- skewer.rb
+++ skewer.rb
@@ -13,6 +13,7 @@ class Skewer < Formula
     sha256 "8d7c6e923b77aa9afb8c1334fa531dc34ecb77a00ebd5a0b7c0a5f499c1ddaf2" => :el_capitan
     sha256 "a1396d4d7a617f8abd23b08e1d8ac0d9a995fa1872a556a6dff184513020a4dc" => :yosemite
     sha256 "4f7f544bb0ec00a4ab303a4022cfa1c454034bc5b2be7f5613e3cc553e69d596" => :mavericks
+    sha256 "3d7443bd5e0356d3d1ec6e3185f818e5cedd3cf0d2e3e2444e16e1c22326f269" => :x86_64_linux
   end
 
   def install
