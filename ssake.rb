943dcd928edaa97cbf8a71e19e45cbd5e378a397
Shaun Jackman <sjackman@gmail.com>
2016-06-07 15:52:15 -0700

    ssake: update 3.8.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#213.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- ssake.rb
+++ ssake.rb
@@ -12,6 +12,7 @@ class Ssake < Formula
     sha256 "c51b0294aace8714001b1d3d08af9239262b751636bf1eb61a8c6dfb65e6bd92" => :el_capitan
     sha256 "8332def65e6f706db2371ef9fd7025ebb48ffcb2f3d5520ca4a8c784f8098f34" => :yosemite
     sha256 "f6744c3239aea0f6b4ef1cd3ded7d1669b387281b9343752a5684533a6cc4da4" => :mavericks
+    sha256 "9abcae280ed5a6bb4d72202e70217444a91ce7056817d8477915a2aaf5b651ce" => :x86_64_linux
   end
 
   def install
