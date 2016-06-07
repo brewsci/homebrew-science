5114aadd7200126d5b90135933c45e498461519a
Shaun Jackman <sjackman@gmail.com>
2016-06-07 16:19:48 -0700

    velvet: update 1.2.10 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#153.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- velvet.rb
+++ velvet.rb
@@ -7,6 +7,7 @@ class Velvet < Formula
     sha256 "23c3dbe996eead11507bad1f51fd499c2165d21927fb71812d21da1e4f38669d" => :yosemite
     sha256 "f37e358add021cf9b8b6f9d6eb3f59b41f95c851caa57412d2314347ae8ba7e7" => :mavericks
     sha256 "f0b135f0ef7da58a0b0e396d90e3b96049ebd250ca76e5d081dceddcee1685c9" => :mountain_lion
+    sha256 "2477e5faff7282470c8d05616c366f6c942069def5a5db15f92fdca0e4eaa2d3" => :x86_64_linux
   end
 
   # doi "10.1101/gr.074492.107"
