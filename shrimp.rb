19a53be63acb5be3e40a5733c101d32a71a485f7
Shaun Jackman <sjackman@gmail.com>
2016-06-07 15:40:22 -0700

    shrimp: update 2.2.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#200.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- shrimp.rb
+++ shrimp.rb
@@ -12,6 +12,7 @@ class Shrimp < Formula
     sha256 "2ab2012cdc0dec1e27d8c02a7b2c04b2f24f7d3cb078bc2599e527aa23ffcc4d" => :yosemite
     sha256 "3fae6267589367040d39b817ff6278585116d0c12e63dc7efdd6210b1d75e136" => :mavericks
     sha256 "60fe3a8309be67eac59e464822f024449623a5ded9e1533332d7837fe58e80ba" => :mountain_lion
+    sha256 "c7872a361b1e0799e86b807a8d1e07c9f1e4e2a6bcb3f97cb7437d8c76342f0c" => :x86_64_linux
   end
 
   needs :openmp
