af2de344aeb3b02a95cbbfd22e703dbb93a838a1
Shaun Jackman <sjackman@gmail.com>
2016-06-07 15:54:13 -0700

    stringtie: update 1.0.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#216.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- stringtie.rb
+++ stringtie.rb
@@ -6,6 +6,7 @@ class Stringtie < Formula
     sha256 "de3a457c45fc023c2b9c4e2d8530ec77ade761269897af535fb82a5a904bb354" => :yosemite
     sha256 "157abc752800f726bce574ac5dc1322e2ed112b953fdd06dcdf819c1d7abf258" => :mavericks
     sha256 "03e7791e8119e5ab2e38eb417bd289f6a266bede9d52bbd98a33e79797c98785" => :mountain_lion
+    sha256 "d3274cea211bb0ff362eda641097ca9abbd7ab02fc4cbe37d45579140cbfd495" => :x86_64_linux
   end
 
   # doi "10.1038/nbt.3122"
