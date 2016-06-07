a452eb239c4a22efbc63d242c92d09c2932b1960
Shaun Jackman <sjackman@gmail.com>
2016-06-07 15:37:02 -0700

    sailfish: update 0.9.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#195.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sailfish.rb
+++ sailfish.rb
@@ -10,6 +10,7 @@ class Sailfish < Formula
     sha256 "cc870770c98a55fafb75ceb80ec4eec18be196c165060eabf1366a87359b8fa9" => :el_capitan
     sha256 "4b2e5b0a77b261622374cb2a82b484e79a6a706705e83b89f4094938127ff9a0" => :yosemite
     sha256 "988822bf7f6af7a72320a1f7c4d875e39f0013742667cd92a4d779f1d9a4cbc9" => :mavericks
+    sha256 "43d5d1d3d8fb79811fd45add3e865b612bb8e58cb77be04687d40aaf1ffd0564" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
