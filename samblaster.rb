5af6dd56f8b3248f849f5fac5936260d1d80ca44
Shaun Jackman <sjackman@gmail.com>
2016-06-07 00:01:48 -0700

    samblaster: update 0.1.22 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#197.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- samblaster.rb
+++ samblaster.rb
@@ -13,6 +13,7 @@ class Samblaster < Formula
     sha256 "52e903cdfb8d83b45ed5196cf0677691ce2e82a1de3ae55fe0d0f6aeff7188f7" => :yosemite
     sha256 "8fa4a1f0d5e951e52a3549ba510e4d2c1d259c74af398315a72f93eb927f1c09" => :mavericks
     sha256 "ad7a536d87ec4f4d36307590037ff597898cef59d1ab81b4ef1aeac5e2c38a71" => :mountain_lion
+    sha256 "7194f37e01f0ac74b738cb36c30c29e0fb8822068b13ba20d3db84cea3bb7cbb" => :x86_64_linux
   end
 
   def install
