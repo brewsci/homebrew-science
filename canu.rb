c53e06fd727b18784d84c579958231b940116da2
Shaun Jackman <sjackman@gmail.com>
2016-06-08 16:58:18 -0700

    canu: update 1.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#243.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- canu.rb
+++ canu.rb
@@ -11,6 +11,7 @@ class Canu < Formula
     sha256 "f37d258084205d89ec0aecaa3c04500a394d10b6f357f790ca75bbc40b6f2af3" => :el_capitan
     sha256 "564612c7a5b5de07e7dbd46586df47380f0149c78a21c25abcd215a41727bd98" => :yosemite
     sha256 "496e8b42bdcca4612cede42d3676345a14bfb234937cfb314d4d65a9cea7ae26" => :mavericks
+    sha256 "cf424d48cdd5a6a427b77de3dc4ea55fa6a9b299e6c2d0e84baf8e231ee3de94" => :x86_64_linux
   end
 
   # Fix fatal error: 'omp.h' file not found
