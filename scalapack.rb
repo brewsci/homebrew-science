e283b9e0d577923cb88bce01dc08d06782844cc6
Dominique Orban <dominique.orban@gmail.com>
2016-06-09 22:22:27 -0400

    scalapack: bump for gcc6
    - fix audits
    - add tests

--- scalapack.rb
+++ scalapack.rb
@@ -1,10 +1,10 @@
 class Scalapack < Formula
-  desc "library of high-performance linear algebra routines for parallel distributed memory machines"
+  desc "high-performance linear algebra for distributed memory machines"
   homepage "http://www.netlib.org/scalapack/"
   url "http://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
   sha256 "0c74aeae690fe5ee4db7926f49c5d0bb69ce09eea75beb915e00bba07530395c"
+  revision 6
   head "https://icl.cs.utk.edu/svn/scalapack-dev/scalapack/trunk", :using => :svn
-  revision 4
 
   bottle do
     cellar :any
@@ -13,7 +13,8 @@ class Scalapack < Formula
     sha256 "8fb0b1f4136c13af897d35c2e1d810a57581a12f6e77863033c1ec21e342ccf0" => :mavericks
   end
 
-  option "without-check", "Skip build-time tests (not recommended)"
+  option "without-test", "Skip build-time tests (not recommended)"
+  deprecated_option "without-check" => "without-test"
 
   depends_on :mpi => [:cc, :f90]
   depends_on "cmake" => :build
@@ -29,8 +30,8 @@ class Scalapack < Formula
       blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
       lapack = blas
     else
-      blas = (OS.mac?) ? "-L#{Formula["veclibfort"].opt_lib} -lvecLibFort" : "-lblas"
-      lapack = (OS.mac?) ? blas : "-llapack"
+      blas = OS.mac? ? "-L#{Formula["veclibfort"].opt_lib} -lvecLibFort" : "-lblas"
+      lapack = OS.mac? ? blas : "-llapack"
     end
     args += ["-DBLAS_LIBRARIES=#{blas}", "-DLAPACK_LIBRARIES=#{lapack}"]
 
@@ -38,7 +39,24 @@ class Scalapack < Formula
       system "cmake", "..", *args
       system "make", "all"
       system "make", "install"
-      system "make", "test" if build.with? "check"
+      system "make", "test" if build.with? "test"
+    end
+
+    pkgshare.install "EXAMPLE"
+  end
+
+  test do
+    ENV.fortran
+    cp_r pkgshare/"EXAMPLE", testpath
+    cd "EXAMPLE" do
+      system "mpif90", "-o", "xsscaex", "psscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
+      assert (`mpirun -np 4 ./xsscaex | grep 'INFO code' | awk '{print $NF}'`.to_i == 0)
+      system "mpif90", "-o", "xdscaex", "pdscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
+      assert (`mpirun -np 4 ./xdscaex | grep 'INFO code' | awk '{print $NF}'`.to_i == 0)
+      system "mpif90", "-o", "xcscaex", "pcscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
+      assert (`mpirun -np 4 ./xcscaex | grep 'INFO code' | awk '{print $NF}'`.to_i == 0)
+      system "mpif90", "-o", "xzscaex", "pzscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
+      assert (`mpirun -np 4 ./xzscaex | grep 'INFO code' | awk '{print $NF}'`.to_i == 0)
     end
   end
 end
