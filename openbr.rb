71310c43fcc0458c68f9251ec5cd5bebfef16f0a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-11 22:24:59 +0100

    openbr: add 1.1.0 bottle.

--- openbr.rb
+++ openbr.rb
@@ -2,6 +2,12 @@ class Openbr < Formula
   desc "Open Source Biometric Recognition"
   homepage "http://www.openbiometrics.org/"
 
+  bottle do
+    sha256 "0e9cecc03ce39f45e26c94a6854bdd1ab922f49cc307209fc008366f67bec309" => :el_capitan
+    sha256 "a4809628fb738aee27b5a4ff7d5e3cfd86a788871c89601f1126b5eefc555e0b" => :yosemite
+    sha256 "ad3e698e6a00c5e0a785811dd193a52a63f08f9856b13a4a6b216f913ab216d8" => :mavericks
+  end
+
   # Use git to get submodules
   # url "https://github.com/biometrics/openbr/archive/v1.1.0.tar.gz"
   # sha256 "42daf5dc1aa8e61ec8fc3305b0665cde952583ca674826e78e7ca48ae8f78821"
