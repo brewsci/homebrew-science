bf4bc0295cd19852a4c299514604c945110cc2c9
Dominique Orban <dominique.orban@gmail.com>
2016-06-11 17:09:27 -0400

    openbr 1.1.0
    Closes #2093
    
    Closes #3738.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- openbr.rb
+++ openbr.rb
@@ -1,8 +1,15 @@
 class Openbr < Formula
+  desc "Open Source Biometric Recognition"
   homepage "http://www.openbiometrics.org/"
-  url "https://github.com/biometrics/openbr/releases/download/v0.4.1/openbr0.4.1_withModels.tar.gz"
-  version "0.4.1"
-  sha256 "130433a17625b81b83cefef839dc968d204fc44d2854d9c285999cd0b6c86fdc"
+
+  # Use git to get submodules
+  # url "https://github.com/biometrics/openbr/archive/v1.1.0.tar.gz"
+  # sha256 "42daf5dc1aa8e61ec8fc3305b0665cde952583ca674826e78e7ca48ae8f78821"
+  url "https://github.com/biometrics/openbr.git",
+    :revision => "e6fbbaa60f74fb29bdb432f27f1683526cb84069",
+    :tag => "v1.1.0"
+
+  head "https://github.com/biometrics/openbr.git"
 
   depends_on "cmake" => :build
   depends_on "qt5"
@@ -10,8 +17,10 @@ class Openbr < Formula
   depends_on "eigen"
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
