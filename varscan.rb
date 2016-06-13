773e843e73f22765ee242f47ac83435b1c6d8ac4
Shaun Jackman <sjackman@gmail.com>
2016-06-13 16:12:15 -0700

    varscan: Add desc, bottle :unneeded and fix test

--- varscan.rb
+++ varscan.rb
@@ -1,10 +1,12 @@
 class Varscan < Formula
+  desc "Variant detection in massively parallel sequencing data"
   homepage "http://varscan.sourceforge.net/"
-  # tag "bioinformatics"
-  # doi "10.1101/gr.129684.111"
-
   url "https://downloads.sourceforge.net/project/varscan/VarScan.v2.3.7.jar"
   sha256 "1060f823fc82a182fcc4ae5824a3639b35ff6d969657c536f709f24b2324ed12"
+  # doi "10.1101/gr.129684.111"
+  # tag "bioinformatics"
+
+  bottle :unneeded
 
   depends_on :java
 
@@ -16,7 +18,7 @@ class Varscan < Formula
   end
 
   test do
-    assert_match "somatic", shell_output("varscan 2>&1", 0)
-    assert_match "min-coverage", shell_output("varscan filter -h 2>&1", 0)
+    assert_match "somatic", shell_output("#{bin}/varscan 2>&1", 0)
+    assert_match "min-coverage", shell_output("#{bin}/varscan filter -h 2>&1", 0)
   end
 end
