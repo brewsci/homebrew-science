0e56b75b9b58bd7ba44573d9e6061df560838308
Shaun Jackman <sjackman@gmail.com>
2016-06-13 12:41:32 -0700

    igvtools: Add desc and bottle :unneeded

--- igvtools.rb
+++ igvtools.rb
@@ -1,8 +1,11 @@
 class Igvtools < Formula
+  desc "Utilities for preprocessing data files for IGV"
   homepage "http://www.broadinstitute.org/software/igv/download"
-  # tag "bioinformatics"
   url "http://www.broadinstitute.org/igv/projects/downloads/igvtools_2.3.48.zip"
   sha256 "0ae8fc2f6500495ae84aa0232887d9466c73c188f5af91d64a089e5808d3a84d"
+  # tag "bioinformatics"
+
+  bottle :unneeded
 
   def install
     java = share/"java"
