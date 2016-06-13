f1aa17d3029e3dcf3dabe3588bafb669a68d5a73
Shaun Jackman <sjackman@gmail.com>
2016-06-13 16:09:03 -0700

    vague: Add desc, bottle :unneeded and fix test

--- vague.rb
+++ vague.rb
@@ -1,11 +1,13 @@
 class Vague < Formula
+  desc "Velvet assembler graphical front end"
   homepage "http://www.vicbioinformatics.com/software.vague.shtml"
-  # head "https://github.com/Victorian-Bioinformatics-Consortium/vague.git"
-  # tag "bioinformatics"
-  # doi "10.1093/bioinformatics/bts664"
-
   url "http://www.vicbioinformatics.com/vague-1.0.5.tar.gz"
   sha256 "7144bef4a5c8ccb3d0e6b27201a506e1cfa25812fb9bbe54842dc910100c5907"
+  head "https://github.com/Victorian-Bioinformatics-Consortium/vague.git"
+  # doi "10.1093/bioinformatics/bts664"
+  # tag "bioinformatics"
+
+  bottle :unneeded
 
   depends_on "velvet"
 
@@ -24,6 +26,6 @@ class Vague < Formula
 
   test do
     # can't test main app because it is GUI only so we test a helper script
-    assert_match "genome", shell_output("velvetk.pl 2>&1", 1)
+    assert_match "genome", shell_output("#{bin}/velvetk.pl 2>&1", 1)
   end
 end
