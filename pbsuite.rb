7b6e540538cc8eeaab829675de614244935a9a31
Shaun Jackman <sjackman@gmail.com>
2016-06-13 15:59:45 -0700

    pbsuite: Add desc, bottle :unneeded and fix style

--- pbsuite.rb
+++ pbsuite.rb
@@ -1,24 +1,26 @@
 class Pbsuite < Formula
+  desc "PBJelly and PBHoney: Software for PacBio sequencing data"
   homepage "http://sourceforge.net/projects/pb-jelly/"
-  # doi "10.1371/journal.pone.0047768" => "PBJelly", "10.1186/1471-2105-15-180" => "PBHoney"
-  # tag "bioinformatics"
-
   url "https://downloads.sourceforge.net/project/pb-jelly/PBSuite_14.7.14.tgz"
   sha256 "98dcda7598f0ecf1a4223ba249f699add4438c60f01ae67b07fdca01142cf145"
+  # doi "10.1371/journal.pone.0047768", "10.1186/1471-2105-15-180"
+  # tag "bioinformatics"
+
+  bottle :unneeded
 
   conflicts_with "bedtools", :because => "Both install bin/bamToFastq"
 
   depends_on "blasr" => :recommended
   depends_on "python"
 
+  def install
+    prefix.install Dir["*"]
+  end
+
   def caveats; <<-EOS.undent
     Set the PYTHONPATH environment variable:
       export PYTHONPATH=#{opt_prefix}:$PYTHONPATH
     EOS
-  end
-
-  def install
-    prefix.install Dir["*"]
   end
 
   test do
