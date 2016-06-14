7298c81effe172b4b32943f4c681a1b9e81ec78d
Shaun Jackman <sjackman@gmail.com>
2016-06-14 09:39:56 -0700

    squeezambler: add 2.0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#211.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- squeezambler.rb
+++ squeezambler.rb
@@ -7,6 +7,11 @@ class Squeezambler < Formula
   version "2.0.3"
   sha256 "a10893a57a4b651037455b25c2c6856a1dad6122ff7efb223cf556aa97e04e46"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "247b949cefd6a92b4e7b4d952cdc83f1ec44204167896527de253b31f0173da2" => :x86_64_linux
+  end
+
   # Fix error: 'omp.h' file not found
   needs :openmp
 
