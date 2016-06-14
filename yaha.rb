21151fd9001351db79830075e8cc58554ff083fa
Shaun Jackman <sjackman@gmail.com>
2016-06-14 09:42:52 -0700

    yaha: add 0.1.83 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#235.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- yaha.rb
+++ yaha.rb
@@ -15,6 +15,11 @@ class Yaha < Formula
   url "https://github.com/GregoryFaust/yaha/releases/download/v0.1.83/yaha-0.1.83.tar.gz"
   sha256 "76e052cd92630c6e9871412e37e46f18bfbbf7fc5dd2f345b3b9a73eb74939ef"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "910564c95a23c3ad0297e00f56177a5fb2f22244ecc924415e25e38af87c90e5" => :x86_64_linux
+  end
+
   # Uses Linux header file: https://github.com/Homebrew/homebrew-science/pull/2373
   depends_on LinuxRequirement
 
