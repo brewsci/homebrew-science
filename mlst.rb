9c6b51521a5b15ee31abaa4678dadcef97d37dcf
Shaun Jackman <sjackman@gmail.com>
2016-06-15 09:16:07 -0700

    mlst: update 2.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#142.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- mlst.rb
+++ mlst.rb
@@ -11,6 +11,7 @@ class Mlst < Formula
     sha256 "0d74d46e7a467d9aafe94a2ae33c05eee7dec985841b99e101fa0c60bc10788c" => :el_capitan
     sha256 "b36e0c426f6583f5649620a4f0f4d5a6a0dd84f3dd50c3978d09e9e2e33d0207" => :yosemite
     sha256 "ab387db5c198bc9ed6cea2b3bbcccdda40bdf9dbc8d8f2d18f6947bf0ff7ca67" => :mavericks
+    sha256 "50716cb3a2e7e7d6cfaef9148271c06e35c8ce25578fbe6174dccba95edac964" => :x86_64_linux
   end
 
   depends_on "blast"
