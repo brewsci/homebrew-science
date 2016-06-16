e07a1fdf71645e8376e700971b9d7ded3b1a5798
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-06-16 04:37:03 +0000

    sailfish: update 0.10.1 bottle for Linuxbrew.

--- sailfish.rb
+++ sailfish.rb
@@ -10,6 +10,7 @@ class Sailfish < Formula
     sha256 "c6df8bd3a9116d061b67a44ae26d53b10ef4d3d77e01f078821208505ca9d935" => :el_capitan
     sha256 "bf415e7662f81af38304629155376e745aaee9a96cd0fd48216f0936c5ed1fc5" => :yosemite
     sha256 "eac2cb0702725332f551a53023968d396d3325dbff246cdc7f5955b3341f31c3" => :mavericks
+    sha256 "38d78199750c9d1e28ed25fea65c894f1d48841daa1534eb71b92a6ea0ba29d5" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
