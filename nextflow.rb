9e6968bb9e82ee19f567d046158b926dc6158a34
ilovezfs <ilovezfs@icloud.com>
2016-06-18 03:05:33 -0700

    nextflow: fix the test and set bottle :unneeded
    - use `pipe_ouput` in the test
    - set bottle :unneeded
    
    Closes #3763.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nextflow.rb
+++ nextflow.rb
@@ -7,15 +7,9 @@ class Nextflow < Formula
   url "http://www.nextflow.io/releases/v0.20.1/nextflow"
   version "0.20.1"
   sha256 "02635f3371f76a10e12f7366508c90bacf532ab7c23ae03c895317a150a39bd4"
-
   head "https://github.com/nextflow-io/nextflow.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "30edad0357e60283de15968fae40cdef93e35847d3e0bf6130dc737326aa73c8" => :el_capitan
-    sha256 "347ee40489df030eba34574049aa2b52450736d823484add1a1444a53ff4cac6" => :yosemite
-    sha256 "237730223a9cb46a7de5c046e6c7891322eed7185d18d868b0f3de21e656b952" => :mavericks
-  end
+  bottle :unneeded
 
   depends_on :java => "1.7+"
 
@@ -24,7 +18,8 @@ class Nextflow < Formula
   end
 
   test do
-    system "#{bin}/nextflow", "-download"
-    system "echo", "println \'hello\' | #{bin}/nextflow -q run - |grep hello"
+    system bin/"nextflow", "-download"
+    output = pipe_output("#{bin}/nextflow -q run -", "println 'hello'").chomp
+    assert_equal "hello", output
   end
 end
