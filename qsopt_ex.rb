b9811d756c0b65f211cbac672e1b9691c2418f02
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-23 18:16:43 +0100

    qsopt_ex: add 2.5 bottle.

--- qsopt_ex.rb
+++ qsopt_ex.rb
@@ -5,6 +5,13 @@ class QsoptEx < Formula
   version "2.5"
   sha256 "5949c65a8528fc170371d6f99461280f79313da91b1bc6d71d792c51601fa6bb"
 
+  bottle do
+    cellar :any
+    sha256 "4cdde0071a2ae2a737772f90060d9997fed15f869a43b7cab7ebee8da8578d0f" => :el_capitan
+    sha256 "ba7975b0a1ce2b9502e6eac784855c74502aef70fb3f20ce8a6f344d2956e0b2" => :yosemite
+    sha256 "95037868110bcd2a4482534cc1b59529c3c849e45305b3cf2f266f92cc6f97cc" => :mavericks
+  end
+
   # Patch to fix a name change in a global variable of the GMP library
   patch :DATA
 
