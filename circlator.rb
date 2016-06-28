c22e05e958d37fd10bfd6c7b34f1231be2c35aef
ilovezfs <ilovezfs@icloud.com>
2016-06-28 08:48:04 -0700

    circlator: use :fortran req instead of gcc dep
    Also, it needs to be run-time not just build-time or the build fails
    except when bottling.
    
    Closes #3790.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- circlator.rb
+++ circlator.rb
@@ -18,12 +18,12 @@ class Circlator < Formula
   # tag "bioinformatics"
 
   depends_on "zlib" unless OS.mac?
+  depends_on :fortran
   depends_on :python3
   depends_on "bwa"
   depends_on "prodigal"
   depends_on "samtools"
   depends_on "spades"
-  depends_on "gcc" => :build
   depends_on "homebrew/python/numpy" => ["with-python3"]
   depends_on "homebrew/python/pymummer"
 
