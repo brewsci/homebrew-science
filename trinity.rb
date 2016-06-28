f6699b3c1b0fa95379070c38d2bce95322508e80
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-28 13:15:15 +0100

    trinity: update 2.2.0_1 bottle.

--- trinity.rb
+++ trinity.rb
@@ -12,10 +12,9 @@ class Trinity < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "e4aaf5e0b7b4d39f48028bbc79c506fd6a6ade669fbae675c35e8d8ba10ca637" => :el_capitan
-    sha256 "de5074c29c65908e175ff20bce4ea8f09c6f4daa4a611cf05a34c4db48a06c4f" => :yosemite
-    sha256 "fcb67f469057ccdca350a0e41e602391417620af9c237c0cd3e2222d7bf569b7" => :mavericks
+    sha256 "e0ede844281630b088be3dc1b0983e02aa9307a866ebdbc6320e1d596c142a70" => :el_capitan
+    sha256 "9a6653a4b288245afcf0e71c024a51a8a8325e5ac54e9252920ff021efa7db37" => :yosemite
+    sha256 "5902d884766d2bb88b252e64be063374b17a0d62274c7a26229d46a5e4576d65" => :mavericks
   end
 
   depends_on "express" => :recommended
