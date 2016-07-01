58f51142ed9d86cf36c0bb741e566b89f02352dc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-01 07:22:07 +0100

    qsopt: add 1.01 bottle.

--- qsopt.rb
+++ qsopt.rb
@@ -5,6 +5,13 @@ class Qsopt < Formula
   version "1.01"
   sha256 "fa2f135eb6fa14e4c27abc97549ab032f56155170bbe27d2c0094245fe55e60e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6b1a86ff5c3febe9e3914497113c9fc55e6812cbe0949ea816dfde2f67988484" => :el_capitan
+    sha256 "4786b3521799135fa59b7aa15064572b024c04a4162ad1bfdc9bcaa4f499bfe1" => :yosemite
+    sha256 "c8d642603ae5db27641860f8e194b4b2b2f370512322ae895c0b5f41d81bbb23" => :mavericks
+  end
+
   resource "qsopt.h" do
     url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt.h"
     sha256 "647729f1bd77e1263ecf35e1897c705ef1cb45e2d65dbd9cb8fdf5df5ae65624"
