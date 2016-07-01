d80d53c75a19458e5caf7cc498db313f658a141c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-01 00:19:54 +0000

    bcalm: update 2.0.0 bottle for Linuxbrew.

--- bcalm.rb
+++ bcalm.rb
@@ -13,6 +13,7 @@ class Bcalm < Formula
     sha256 "8dd755580072b0300c2c7c5fb1220c87c00f2d6c0267c9f0e435f8529c7c50d1" => :el_capitan
     sha256 "615881b3cf75e5411969cfd0aa04de7fe12c077e5945c3f5eee18e7c8ebe49b4" => :yosemite
     sha256 "f5a5a290258b96961aecbcda585cb6fffbee76791d12a566fd98b9c8d302fa93" => :mavericks
+    sha256 "3959eb47b21409e0e2e9e21115bf0b0bb5552382afbe78a9b7f2791a037fcdbe" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
