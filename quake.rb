1bd3107b4d748c3a9440301fe0a9155ff57beef0
Shaun Jackman <sjackman@gmail.com>
2016-07-10 17:45:45 -0400

    quake: update 0.3.5_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#178.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- quake.rb
+++ quake.rb
@@ -12,6 +12,7 @@ class Quake < Formula
     sha256 "b76c43979ec8370f99ced3d759c6c01e749782a510de78aea1f24312ae58dc9f" => :el_capitan
     sha256 "2c77da904ef8c7409f0ba8fcc38100f3296f6178365de5211f76b1063b7448b8" => :yosemite
     sha256 "23ba2a9b6fdc08bfb446243d24e7762c79319125532e53688e411b389bc811c6" => :mavericks
+    sha256 "b99dd716a06cce5859c63757c635ede84605b044904c57f1e819a71b346c7309" => :x86_64_linux
   end
 
   needs :openmp
