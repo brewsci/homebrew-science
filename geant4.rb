11960400ad59adc16d2c9806d52efcc3363036cd
David Hall <david.hall.physics@gmail.com>
2016-07-10 14:50:41 -0400

    geant4: 10.2.2
    - renamed geant formula to geant4 (this is the actual name of the
    package, i.e. major version number is 10).
    - added description
    - made multi-threading on by default (most users will want this, and it
    is possible to build single-threaded applications with multi-threaded
    Geant4)
    
    Update to use Qt5
    
    Closes #3822.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- geant4.rb
+++ geant4.rb
@@ -1,7 +1,9 @@
-class Geant < Formula
+class Geant4 < Formula
+  desc "Simulation toolkit for particle transport through matter"
   homepage "http://geant4.cern.ch"
-  url "http://geant4.cern.ch/support/source/geant4.10.02.tar.gz"
-  sha256 "633ca2df88b03ba818c7eb09ba21d0667a94e342f7d6d6ff3c695d83583b8aa3"
+  url "http://geant4.cern.ch/support/source/geant4.10.02.p02.tar.gz"
+  version "10.2.2"
+  sha256 "702fb0f7a78d4bdf1e3f14508de26e4db5e2df6a21a8066a92b7e6ce21f4eb2d"
 
   bottle do
     cellar :any
@@ -14,11 +16,11 @@ class Geant < Formula
   option "with-gdml", "Use GDML"
   option "with-notimeout", "Set notimeout in installing data"
   option "with-usolids", "Use USolids (experimental)"
-  option "with-multithreaded", "Build with multithreading enabled"
+  option "without-multithreaded", "Build without multithreading support"
 
-  depends_on "cmake" => :build
+  depends_on "cmake" => :run
   depends_on :x11
-  depends_on "qt" => :optional
+  depends_on "qt5" => :optional
   depends_on "xerces-c" if build.with? "gdml"
 
   def install
@@ -32,7 +34,7 @@ class Geant < Formula
       ]
 
       args << "-DGEANT4_INSTALL_DATA_TIMEOUT=86400" if build.with? "notimeout"
-      args << "-DGEANT4_USE_QT=ON" if build.with? "qt"
+      args << "-DGEANT4_USE_QT=ON" if build.with? "qt5"
       args << "-DGEANT4_USE_G3TOG4=ON" if build.with? "g3tog4"
       args << "-DGEANT4_USE_GDML=ON" if build.with? "gdml"
       args << "-DGEANT4_USE_USOLIDS=ON" if build.with? "usolids"
@@ -41,5 +43,15 @@ class Geant < Formula
       system "cmake", *args
       system "make", "install"
     end
+  end
+
+  test do
+    system "cmake", share/"Geant4-#{version}/examples/basic/B1"
+    system "make"
+    (testpath/"test.sh").write <<-EOS.undent
+      . #{bin}/geant4.sh
+      ./exampleB1 run2.mac
+    EOS
+    system "/bin/bash", "test.sh"
   end
 end
