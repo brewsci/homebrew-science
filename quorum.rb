71e45eca40ffee8945f38695045d0e32e2245019
Shaun Jackman <sjackman@gmail.com>
2016-07-11 17:17:55 -0400

    quorum: add 1.0.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#183.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- quorum.rb
+++ quorum.rb
@@ -6,6 +6,10 @@ class Quorum < Formula
   url "ftp://ftp.genome.umd.edu/pub/QuorUM/quorum-1.0.0.tar.gz"
   sha256 "ec11af84ab3887b1cb0a526d9ab295e8cde6fe2cbba3060147da9fa2f12a518d"
 
+  bottle do
+    sha256 "fa5fc25145e21539ee3b27872c09f43f2072760ece657388ecbdff6a7d87208a" => :x86_64_linux
+  end
+
   depends_on "jellyfish"
   depends_on "pkg-config" => :build
 
