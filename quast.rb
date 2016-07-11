99aa292b9eef0f3c0145f5eeb55d5d0e2d7a0ccd
Shaun Jackman <sjackman@gmail.com>
2016-07-11 14:27:33 -0400

    quast: update 4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#179.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- quast.rb
+++ quast.rb
@@ -12,6 +12,7 @@ class Quast < Formula
     sha256 "b49c53be3c39c9fbc24082ad29402ecb451501f469c533d6b5838e45d73e2340" => :el_capitan
     sha256 "d43ba8fed47a052605a25c52f893401256043b4b38a2ba7fd5f67ef12636ab15" => :yosemite
     sha256 "b49d73e6874c29fd123b8294fe909cb46d062c5d72d7e4bf7f52f57ab534b145" => :mavericks
+    sha256 "82f8226084ea64acdd28bc20857b6363398592a2d5bdfcbc69e2016a95164096" => :x86_64_linux
   end
 
   if OS.mac? && MacOS.version <= :mountain_lion
