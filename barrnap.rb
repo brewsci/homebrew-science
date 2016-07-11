d4eb9732d4aaa7025890ba983bdc27faccb4c1a5
Shaun Jackman <sjackman@gmail.com>
2016-07-11 17:00:18 -0400

    barrnap: update 0.7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#32.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- barrnap.rb
+++ barrnap.rb
@@ -13,6 +13,7 @@ class Barrnap < Formula
     sha256 "fec76972178844abb223274fc4a3c45b987168d2163c4074998100d688a97e6a" => :yosemite
     sha256 "3d84c1dfdac365506270abd6df0e0c451ef43df9039829d52eefe7b59ada2b14" => :mavericks
     sha256 "362da60326bfc61270f00879a30ecfb97e423893c210612c292f6c0e51ec4d81" => :mountain_lion
+    sha256 "a26f2a63f43fecac5a5bdead99174288a207075c60f278b7bcea7cbb3c708dad" => :x86_64_linux
   end
 
   depends_on "hmmer"
