c80f7394b46240bf0f7160eb63a9200d0d0e8b49
Shaun Jackman <sjackman@gmail.com>
2016-07-11 12:57:22 -0400

    mitofy: update 20120322 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#141.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- mitofy.rb
+++ mitofy.rb
@@ -12,6 +12,7 @@ class Mitofy < Formula
     sha256 "3194d6f95b55e17e7e2099583d2590df400531c82d17333ca94b1f892ea0e74b" => :yosemite
     sha256 "5d8abbd913f8881787cba13c0b22f5fe27c6e3104a1432896edfe075f866fa37" => :mavericks
     sha256 "a8641e8824c35651d7062aa3e403aafeaf038ca62011f8fa93d9849446178942" => :mountain_lion
+    sha256 "674f07fce66705f83ce55094257a8a119824a0da6ad3e1bde72b97b7120a28ae" => :x86_64_linux
   end
 
   # Includes vendored dependencies of BLAST 2.2.25 and tRNAscan-SE 1.4 compiled for Mac OS.
