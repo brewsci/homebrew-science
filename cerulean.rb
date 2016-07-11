35dd50f5b8701a06390459e1a65cea2d5d904f27
Shaun Jackman <sjackman@gmail.com>
2016-07-11 17:16:24 -0400

    cerulean: add 0.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#55.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- cerulean.rb
+++ cerulean.rb
@@ -6,6 +6,11 @@ class Cerulean < Formula
   url "https://downloads.sourceforge.net/project/ceruleanassembler/Cerulean_v_0_1.tar.gz"
   sha256 "b6b9046fb1cf9980a169ccfe1a57c1060c6afbbe12e6b201eb8c47be0849b688"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "728bc5f34edfe6119834b3686351632ff011ca0c14582bd119b1712e78677edd" => :x86_64_linux
+  end
+
   depends_on "abyss" => :recommended
   depends_on "numpy" => :python
 
