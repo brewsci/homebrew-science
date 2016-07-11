aca240004b5c19e195e62e75f6ff91a4ba56670f
Shaun Jackman <sjackman@gmail.com>
2016-07-11 14:29:43 -0400

    phipack: update 2013-03-05 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#237.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- phipack.rb
+++ phipack.rb
@@ -6,6 +6,7 @@ class Phipack < Formula
     sha256 "1fabe869b7a87d8fec85ca7cf7ee1899d12a38994a6046f203f83026733c91a3" => :yosemite
     sha256 "c0af865b47d6146ffa65ae63049933c0cebe523e8ea51edd3b43e85af63c43c8" => :mavericks
     sha256 "8c0f3cd292ba7964acbcd388ad04dccee785c69288c9d78169e9bf7fba9e5ef6" => :mountain_lion
+    sha256 "d14f04741addecb41c8c212f67f82915a78993335adf61fef8736c23ad140ed3" => :x86_64_linux
   end
 
   # doi "10.1534/genetics.105.048975"
