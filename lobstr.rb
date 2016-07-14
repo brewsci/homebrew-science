58d7879548859d2764403f3ebc20b14b7d4016d1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-14 07:44:34 +0100

    lobstr: add 4.0.6 bottle.

--- lobstr.rb
+++ lobstr.rb
@@ -4,6 +4,13 @@ class Lobstr < Formula
   url "https://github.com/mgymrek/lobstr-code/releases/download/v4.0.6/lobSTR-4.0.6.tar.gz"
   sha256 "f13bfc17eebd4aadd58fd798941318d8278a2da2e64e596027ebc26e004ce31c"
 
+  bottle do
+    cellar :any
+    sha256 "7026c87999258f045bf2696e11459fea2397c5b2037248b1f62d7e80ced60dd9" => :el_capitan
+    sha256 "7589320083c7fcbf2a05d1ad68247312de85120d62663c6db75755c3cfbfcca1" => :yosemite
+    sha256 "2e0aae1cc93cdf0d332969bfcb055c8e417ebe3f01314f59e338151b2211dc7b" => :mavericks
+  end
+
   head do
     url "https://github.com/mgymrek/lobstr-code.git"
     depends_on "autoconf" => :build
