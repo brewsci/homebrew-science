f60e03a1d243e7d1f4042d9b4050a7d36bf04dcd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-17 07:38:44 +0100

    fwdpp: update 0.5.1 bottle.

--- fwdpp.rb
+++ fwdpp.rb
@@ -9,10 +9,9 @@ class Fwdpp < Formula
 
   bottle do
     cellar :any
-    sha256 "e74eb0c7786ea05c641f94464df02c9e77551956e55ef9f4cc3074d2dd5a3326" => :el_capitan
-    sha256 "4253862f4d9ea7b7005660cf9fd9b746898a29fa6a68ffa092ad7a6f0ad2aea1" => :yosemite
-    sha256 "5ad08aadd33fe1d7c848498569b9dcee120e9f958284848232767ee19129dd0d" => :mavericks
-    sha256 "e2f6b66524ca427accbebc40fdc79a68ce2b3503d430ff3621be5e1cabf35b8a" => :x86_64_linux
+    sha256 "cc8350248223f8f420ad9a5a0180b2602b15f9c8d0f29cf408a7387be7f87c0b" => :el_capitan
+    sha256 "4c32ba7d08abe91b5824dbd310fc2b9688434695aa80a031d756757564f0c16b" => :yosemite
+    sha256 "f03af30bd365da8c3ff398edc262ee9a91fcda8761fa1bef64fef5e3028503e4" => :mavericks
   end
 
   option "without-check", "Disable build-time checking (not recommended)"
