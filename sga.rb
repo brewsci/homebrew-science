902840b096269eea1d1ebc62cadd3599f7a8dfb1
Shaun Jackman <sjackman@gmail.com>
2016-07-19 16:56:49 -0700

    sga: Add desc and tag bioinformatics
    Closes #3842.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- sga.rb
+++ sga.rb
@@ -1,23 +1,25 @@
 class Sga < Formula
+  desc "de novo genome assembler based on the concept of string graphs"
   homepage "https://github.com/jts/sga"
-  # doi '10.1101/gr.126953.111'
   url "https://github.com/jts/sga/archive/v0.10.13.tar.gz"
   sha256 "77859ab233980594941aa4c4cb5c2cbe1f5c43f2519f329c3a88a97865dee599"
   head "https://github.com/jts/sga.git"
+  # doi "10.1101/gr.126953.111"
+  # tag "bioinformatics"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  # Only header files are used, so :build is appropriate
   depends_on "google-sparsehash" => :build
   depends_on "bamtools"
 
   def install
     cd "src" do
       system "./autogen.sh"
-      system "./configure", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}",
-                            "--with-bamtools=#{Formula["bamtools"].opt_prefix}",
-                            "--with-sparsehash=#{Formula["google-sparsehash"].opt_prefix}"
+      system "./configure",
+        "--disable-dependency-tracking",
+        "--prefix=#{prefix}",
+        "--with-bamtools=#{Formula["bamtools"].prefix}",
+        "--with-sparsehash=#{Formula["google-sparsehash"].prefix}"
       system "make", "install"
       bin.install Dir["bin/*"] - Dir["bin/Makefile*"]
     end
