8a036035473ec29a1191eee97339f0373fe4516e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-20 04:20:54 +0100

    opengrm-thrax: update 1.2.2 bottle.

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -5,9 +5,9 @@ class OpengrmThrax < Formula
 
   bottle do
     cellar :any
-    sha256 "041d8eac5624c0f812f701e825ab5c675daa698523b1819b144722ec0689c5bd" => :el_capitan
-    sha256 "52d6ec14148f99a5a110365c232f879550b13b28b0ffca4f37700bde1d0fb5df" => :yosemite
-    sha256 "036dbbc989df705217c2bf820b07650c21e13026bd6aac4f237e982cbc0ecdda" => :mavericks
+    sha256 "9a2c63a57067e38d39be1a4299401ed87a89b899f26e96742fc3ccbc588f084c" => :el_capitan
+    sha256 "893fda3d8f2539e45d66a49eac9dab81069d3bb04cd849a79377b2ef722bab3c" => :yosemite
+    sha256 "385d96d3bfda3b50fe7b512a3ffadbde175a7de6d336a8d34bccd0e532e43169" => :mavericks
   end
 
   depends_on "openfst"
