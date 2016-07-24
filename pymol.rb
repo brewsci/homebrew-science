75717291d53e19bf163b90623abd3652f6e80408
ilovezfs <ilovezfs@icloud.com>
2016-07-24 10:55:38 -0700

    pymol 1.8.2.1
    - in lieu of a revision for glew
    - remove upstreamed patch
    - audit fixes
    
    Closes #3861.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- pymol.rb
+++ pymol.rb
@@ -1,9 +1,9 @@
 class Pymol < Formula
+  desc "OpenGL based molecular visualization system"
   homepage "http://pymol.org"
-  url "https://downloads.sourceforge.net/project/pymol/pymol/1.7/pymol-v1.7.6.0.tar.bz2"
-  sha256 "cf142732b5206ea65ebcc0cc162f9d3c88ecacc701ac41d1a5e9b38972700395"
+  url "https://downloads.sourceforge.net/project/pymol/pymol/1.8/pymol-v1.8.2.1.tar.bz2"
+  sha256 "fc5d33d7e36364567462cee19b9b132530a83cbab3fafcf373f2a17f127b3f4e"
   head "https://svn.code.sf.net/p/pymol/code/trunk/pymol"
-  revision 1
 
   bottle do
     cellar :any
@@ -17,9 +17,6 @@ class Pymol < Formula
   depends_on "homebrew/dupes/tcl-tk" => ["with-threads", "with-x11"]
   depends_on :x11
 
-  # Fix outdated X11 path: https://sourceforge.net/p/pymol/patches/10/
-  patch :DATA
-
   def install
     ENV.append_to_cflags "-Qunused-arguments" if MacOS.version < :mavericks
 
@@ -31,10 +28,6 @@ class Pymol < Formula
     bin.install libexec/"bin/pymol"
   end
 
-  test do
-    system bin/"pymol", libexec/"lib/python2.7/site-packages/pymol/pymol_path/data/demo/pept.pdb"
-  end
-
   def caveats; <<-EOS.undent
     On some Macs, the graphics drivers do not properly support stereo
     graphics. This will cause visual glitches and shaking that stay
@@ -43,18 +36,8 @@ class Pymol < Formula
       pymol -M
     EOS
   end
-end
-__END__
-diff --git a/modules/pymol/__init__.py b/modules/pymol/__init__.py
-index 792d273..3d335fd 100644
---- a/modules/pymol/__init__.py
-+++ b/modules/pymol/__init__.py
-@@ -391,7 +391,7 @@ def prime_pymol():
 
-     # legacy X11 launching on OSX
-     if sys.platform == 'darwin' and invocation.options.external_gui == 1:
--        xdpyinfo = "/usr/X11R6/bin/xdpyinfo"
-+        xdpyinfo = "/opt/X11/bin/xdpyinfo"
-         if not os.path.exists(xdpyinfo) or \
-                 os.system(xdpyinfo + " >/dev/null 2>&1"):
-             # launch X11 (if needed)
+  test do
+    system bin/"pymol", libexec/"lib/python2.7/site-packages/pymol/pymol_path/data/demo/pept.pdb"
+  end
+end
