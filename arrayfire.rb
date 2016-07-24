ddd339df0e1de092bb14ebc419d9542cb2e42872
ilovezfs <ilovezfs@icloud.com>
2016-07-24 09:53:29 -0700

    arrayfire 3.3.2
    vendor glew1 in since glew2 dropped glewmx support
    
    Closes #3857.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- arrayfire.rb
+++ arrayfire.rb
@@ -4,8 +4,8 @@ class Arrayfire < Formula
 
   stable do
     url "https://github.com/arrayfire/arrayfire.git",
-      :tag => "v3.2.2",
-      :revision => "7507b61b3f89c99d23900ce4b66d3f8aeef4e609"
+      :tag => "v3.3.2",
+      :revision => "f65dd9798f8efeea4d55efe34ba62f4fc3ae7ca0"
     mirror "http://arrayfire.com/arrayfire_source/arrayfire-full-3.2.2.tar.bz2"
     sha256 "7bcc13ff29bdfb647813ee0e9830ce8387217953427abe0d9904de671e600831"
   end
@@ -30,9 +30,10 @@ class Arrayfire < Formula
   needs :cxx11
   # forge dependencies - remove once forge moves to its own formula
   depends_on "fontconfig"
-  depends_on "glew"
   depends_on "homebrew/versions/glfw3"
   depends_on :x11
+
+  depends_on "freetype" => :linked
 
   # build forge separately so we can tell it to use the system freetype
   resource "forge" do
@@ -40,15 +41,32 @@ class Arrayfire < Formula
     sha256 "c81210185e9a17e085640169f562dcc3654f9a6d9e8c8389469ce1a4a31cb514"
   end
 
+  # glew2 has discontinued glewmx support
+  resource "glew1" do
+    url "https://downloads.sourceforge.net/project/glew/glew/1.13.0/glew-1.13.0.tgz"
+    mirror "https://mirrors.kernel.org/debian/pool/main/g/glew/glew_1.13.0.orig.tar.gz"
+    sha256 "aa25dc48ed84b0b64b8d41cdd42c8f40f149c37fa2ffa39cd97f42c78d128bc7"
+  end
+
   def install
     ENV.cxx11
+
+    resource("glew1").stage do
+      inreplace "glew.pc.in", "Requires: @requireslib@", ""
+      system "make", "GLEW_PREFIX=#{libexec}/glew1", "GLEW_DEST=#{libexec}/glew1", "all"
+      system "make", "GLEW_PREFIX=#{libexec}/glew1", "GLEW_DEST=#{libexec}/glew1", "install.all"
+    end
+
+    ENV.prepend "CXXFLAGS", "-I#{libexec}/glew1/include"
+
     resource("forge").stage do
-      system "cmake", "-DUSE_LOCAL_FREETYPE:BOOL=OFF", ".", *std_cmake_args
+      system "cmake", "-DGLEW_ROOT_DIR=#{libexec}/glew1", "-DUSE_LOCAL_FREETYPE:BOOL=OFF", ".", *std_cmake_args
       system "make", "install"
     end
 
     mkdir "build" do
       args = std_cmake_args
+      args << "-DGLEW_ROOT_DIR=#{libexec}/glew1"
       args << "-DBUILD_TEST:BOOL=OFF"
       args << "-DBUILD_OPENCL:BOOL=ON"
       args << "-DUSE_SYSTEM_CLBLAS:BOOL=ON"
