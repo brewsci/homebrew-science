d7cf797d7564f2750b186837a75384b845804a0a
Dominique Orban <dominique.orban@gmail.com>
2016-07-26 09:46:38 -0400

    mathomatic: new url, test
    Fixes #3844
    
    Closes #3866.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- mathomatic.rb
+++ mathomatic.rb
@@ -1,20 +1,35 @@
 class Mathomatic < Formula
-  homepage "http://www.mathomatic.org/"
-  url "http://mathomatic.orgserve.de/mathomatic-16.0.5.tar.bz2"
-  sha256 "976e6fed1014586bcd584e417c074fa86e4ca6a0fcc2950254da2efde99084ca"
-
-  head do
-    url "http://mathomatic.orgserve.de/am.tar.bz2"
-    sha256 "82b17e6fa26d8a88f4836c51dfe9438a95fe2c7ff29ded58a27246c72cf1bafe"
-  end
+  desc "portable, command-line, educational computer algebra system"
+  homepage "https://github.com/mfillpot/mathomatic"
+  url "https://github.com/mfillpot/mathomatic/archive/mathomatic-16.0.5.tar.gz"
+  sha256 "d93fe35914dbbafa0e67000480268d7ca3e4de773b70f4130a6b3f4fbb20fae2"
+  head "https://github.com/mfillpot/mathomatic.git"
 
   def install
-    ENV["prefix"] = prefix
-    system "make READLINE=1"
-    system "make m4install"
+    args = ["prefix=#{prefix}", "datadir=#{pkgshare}", "mandir=#{man}", "mathdocdir=#{doc}", "datadocdir=#{doc}", "READLINE=1"]
+    system "make", *args
+    system "make", "m4install", *args
     cd "primes" do
-      system "make"
-      system "make", "install"
+      system "make", *args
+      system "make", "install", *args
     end
+  end
+
+  test do
+    (testpath/"testo.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <math.h>
+
+      extern double factorial(double);
+
+      int main(void) {
+        double x = 7.188076642536564;
+        double y = factorial(3.141592);
+        printf("%lf\\n", fabs(x-y)/x);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-o", "testo", "testo.c", "#{doc}/examples/fact.c", "-lm"
+    assert (`./testo`.to_f < 1.0e-8)
   end
 end
