1e00ff947d20045caf31c0d61291e353775aa8cd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-27 22:04:40 +0000

    bedops: update 2.4.20 bottle for Linuxbrew.

--- bedops.rb
+++ bedops.rb
@@ -14,6 +14,7 @@ class Bedops < Formula
     sha256 "2ce3e942c119118bc12feb0e5f15a60bd2bca9fcc5c3f604bbaa35f7f3755647" => :el_capitan
     sha256 "084b521cd3999f89e586d36493b14e5eb8678a0c9058a98814472becfc7f2064" => :yosemite
     sha256 "f6097d30f3a5e1e4c51928a5406b4838aa48c1e7f172390afa32b2a7daa55777" => :mavericks
+    sha256 "b2ac4d2f11b5bfc4889381022b82af9ae1561740ac1e557670e7217672b4cdee" => :x86_64_linux
   end
 
   env :std
