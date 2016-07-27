8e93288cc876ae1170f8a4ae70ca866f5d214e11
Alex Reynolds <areynolds@stamlab.org>
2016-07-27 14:23:09 -0700

    bedops 2.4.20
    BEDOPS 2.4.20
    
    Closes #3873.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bedops.rb
+++ bedops.rb
@@ -4,8 +4,8 @@ class Bedops < Formula
   # doi "10.1093/bioinformatics/bts277"
   # tag "bioinformatics"
 
-  url "https://github.com/bedops/bedops/archive/v2.4.19.tar.gz"
-  sha256 "f170e2187d4136ae764fedd731d117f9f1ad243ee39452098f221c79217a77e4"
+  url "https://github.com/bedops/bedops/archive/v2.4.20.tar.gz"
+  sha256 "1c604b1a85d9acdf444f497e3a24c16e5b60df57d8699b0a8594ff2d6188a41a"
 
   head "https://github.com/bedops/bedops.git"
 
