d78ee1d096c3a939649ceaa1d7045a5ea37a746d
Shaun Jackman <sjackman@gmail.com>
2016-07-28 14:39:05 -0700

    pilon: Add head

--- pilon.rb
+++ pilon.rb
@@ -1,13 +1,11 @@
 class Pilon < Formula
   desc "Improve draft assemblies and find variation"
   homepage "https://github.com/broadinstitute/pilon/wiki"
-  # tag "bioinformatics"
-  # doi "10.1371/journal.pone.0112963"
-
   url "https://github.com/broadinstitute/pilon/releases/download/v1.16/pilon-1.16.jar"
   sha256 "49d2fef3343c2759a0e6d7b22bdbf9c6acdb0a1f77ebbadba1c9cf94f2e9f4e6"
-
-  bottle :unneeded
+  head "https://github.com/broadinstitute/pilon.git"
+  # doi "10.1371/journal.pone.0112963"
+  # tag "bioinformatics"
 
   depends_on :java
 
@@ -19,6 +17,6 @@ class Pilon < Formula
   end
 
   test do
-    assert_match "Pilon version", shell_output("pilon --help")
+    assert_match "Usage", shell_output("#{bin}/pilon --help")
   end
 end
