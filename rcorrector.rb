c4e80208f75af7188e88c7f0b429cc2dbc84b68b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-28 20:40:07 +0000

    rcorrector: add 1.0.2 bottle for Linuxbrew.

--- rcorrector.rb
+++ rcorrector.rb
@@ -12,6 +12,7 @@ class Rcorrector < Formula
     sha256 "652a86c2cc09aea5dd00038e2ed9ab3776f9cc086e0859274763b510aa78ee1d" => :el_capitan
     sha256 "9d6f6a32d1bf8d19635f46a48e5062f2ca35dd1014a2f9e4e016a16eb7f40a47" => :yosemite
     sha256 "d3cef8dc62cb87cb197b40c3beb00d28a667f3ccdbde24e953f1393bc7c01c57" => :mavericks
+    sha256 "1ceb79a2fb8fc8d148b04ed638d70faf120d88017ebd211e9ec5b4f0a1d5c034" => :x86_64_linux
   end
 
   depends_on "jellyfish"
