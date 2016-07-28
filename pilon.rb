058c8a422cf450d763a709dcb71c56f0d566de27
Shaun Jackman <sjackman@gmail.com>
2016-07-28 14:40:44 -0700

    pilon 1.17

--- pilon.rb
+++ pilon.rb
@@ -1,8 +1,8 @@
 class Pilon < Formula
   desc "Improve draft assemblies and find variation"
   homepage "https://github.com/broadinstitute/pilon/wiki"
-  url "https://github.com/broadinstitute/pilon/releases/download/v1.16/pilon-1.16.jar"
-  sha256 "49d2fef3343c2759a0e6d7b22bdbf9c6acdb0a1f77ebbadba1c9cf94f2e9f4e6"
+  url "https://github.com/broadinstitute/pilon/releases/download/v1.17/pilon-1.17.jar"
+  sha256 "3c68c660363bec6865ded39b4b9008b452d7cfffab1fecaccd92896d9a2fbd14"
   head "https://github.com/broadinstitute/pilon.git"
   # doi "10.1371/journal.pone.0112963"
   # tag "bioinformatics"
