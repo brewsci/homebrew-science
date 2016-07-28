bb5a9fe09f37b0377dd106259b07a19ae293e9e1
Thierry Moisan <thierry.moisan@gmail.com>
2016-07-27 21:26:08 -0400

    Fix nlopt HEAD build
    Add a more complete test that works on HEAD too
    
    Closes #3876.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- nlopt.rb
+++ nlopt.rb
@@ -3,7 +3,7 @@ class Nlopt < Formula
   homepage "http://ab-initio.mit.edu/nlopt"
   url "http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz"
   sha256 "8099633de9d71cbc06cd435da993eb424bbcdbded8f803cdaa9fb8c6e09c8e89"
-  revision 1
+  revision 2
   head "https://github.com/stevengj/nlopt.git"
 
   bottle do
@@ -17,25 +17,38 @@ class Nlopt < Formula
 
   depends_on :python => "numpy" if build.with? "python"
   depends_on "octave" => :optional
+  depends_on "swig" if build.head?
+  depends_on "cmake" => :build if build.head?
+  depends_on "guile" => :optional if build.head?
 
   def install
     ENV.deparallelize
-    args = [
-      "--prefix=#{prefix}",
-      "--with-cxx",
-      "--enable-shared",
-    ]
-    args << "--without-octave" if build.without? "octave"
-    args << "--without-python" if build.without? "python"
 
-    if build.with? "octave"
-      ENV["OCT_INSTALL_DIR"] = pkgshare/"oct"
-      ENV["M_INSTALL_DIR"] = pkgshare/"m"
-      ENV["MKOCTFILE"] = "#{Formula["octave"].opt_bin}/mkoctfile"
+    if build.head?
+      args = [
+        "-DWITH_CXX=ON",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+      ]
+      args << "-DBUILD_MATLAB=OFF" if build.without? "matlab"
+      args << "-DBUILD_PYTHON=OFF" if build.without? "python"
+      args << "-DBUILD_OCTAVE=OFF" if build.without? "octave"
+      system "cmake", ".", *args
+    else
+      if build.with? "octave"
+        ENV["OCT_INSTALL_DIR"] = pkgshare/"oct"
+        ENV["M_INSTALL_DIR"] = pkgshare/"m"
+        ENV["MKOCTFILE"] = "#{Formula["octave"].opt_bin}/mkoctfile"
+      end
+      args = [
+        "--prefix=#{prefix}",
+        "--with-cxx",
+        "--enable-shared",
+      ]
+      args << "--without-octave" if build.without? "octave"
+      args << "--without-python" if build.without? "python"
+      system "./configure", *args
+      system "make"
     end
-
-    system "./configure", *args
-    system "make"
     system "make", "install"
 
     # Create lib links for C programs
@@ -67,16 +80,57 @@ class Nlopt < Formula
   end
 
   test do
-    (testpath/"test.cpp").write <<-EOS.undent
-      #include <stdio.h>
-      #include <nlopt.hpp>
-      int main()
-      {
-        printf(\"%d.%d.%d\",nlopt::version_major(),nlopt::version_minor(),nlopt::version_bugfix());
+    # Based on http://ab-initio.mit.edu/wiki/index.php/NLopt_Tutorial#Example_in_C.2FC.2B.2B
+    (testpath/"test.c").write <<-EOS.undent
+    #include <math.h>
+    #include <nlopt.h>
+    #include <stdio.h>
+    double myfunc(unsigned n, const double *x, double *grad, void *my_func_data)
+    {
+      if (grad) {
+        grad[0] = 0.0;
+        grad[1] = 0.5 / sqrt(x[1]);
+      }
+      return sqrt(x[1]);
+    }
+    typedef struct {
+      double a, b;
+    } my_constraint_data;
+    double myconstraint(unsigned n, const double *x, double *grad, void *data)
+    {
+      my_constraint_data *d = (my_constraint_data *) data;
+      double a = d->a, b = d->b;
+      if (grad) {
+        grad[0] = 3 * a * (a*x[0] + b) * (a*x[0] + b);
+        grad[1] = -1.0;
+      }
+      return ((a*x[0] + b) * (a*x[0] + b) * (a*x[0] + b) - x[1]);
+     }
+     int main() {
+      double lb[2] = { -HUGE_VAL, 0 }; /* lower bounds */
+      nlopt_opt opt;
+      opt = nlopt_create(NLOPT_LD_MMA, 2); /* algorithm and dimensionality */
+      nlopt_set_lower_bounds(opt, lb);
+      nlopt_set_min_objective(opt, myfunc, NULL);
+      my_constraint_data data[2] = { {2,0}, {-1,1} };
+      nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-8);
+      nlopt_add_inequality_constraint(opt, myconstraint, &data[1], 1e-8);
+      nlopt_set_xtol_rel(opt, 1e-4);
+      double x[2] = { 1.234, 5.678 };  /* some initial guess */
+      double minf; /* the minimum objective value, upon return */
+
+      if (nlopt_optimize(opt, x, &minf) < 0) {
+        printf("nlopt failed!");
+        return 1;
+      }
+      else {
+        printf("found minimum at f(%g,%g) = %0.10g", x[0], x[1], minf);
         return 0;
       }
+      nlopt_destroy(opt);
+    }
     EOS
-    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-lnlopt_cxx", "-o", "test"
-    assert_equal `./test`.chomp, version.to_s
+    system ENV.cc, "test.c", "-o", "test", "-lnlopt", "-lm"
+    system "./test"
   end
 end
