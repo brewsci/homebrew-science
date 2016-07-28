9cadc03a568dbe2147a5ba22a9ae60888eb930d8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 22:44:22 +0100

    skewer: update 0.2.2 bottle.

--- skewer.rb
+++ skewer.rb
@@ -9,10 +9,9 @@ class Skewer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8d7c6e923b77aa9afb8c1334fa531dc34ecb77a00ebd5a0b7c0a5f499c1ddaf2" => :el_capitan
-    sha256 "a1396d4d7a617f8abd23b08e1d8ac0d9a995fa1872a556a6dff184513020a4dc" => :yosemite
-    sha256 "4f7f544bb0ec00a4ab303a4022cfa1c454034bc5b2be7f5613e3cc553e69d596" => :mavericks
-    sha256 "3d7443bd5e0356d3d1ec6e3185f818e5cedd3cf0d2e3e2444e16e1c22326f269" => :x86_64_linux
+    sha256 "c1efd9467a923be12559c55c57d8cf91e2d9054d4141356b91a16fcfb6622f33" => :el_capitan
+    sha256 "1a1ed956768778eac2cab76e56c293cbab437822c3e8077e1a5c5c54bbcd4da4" => :yosemite
+    sha256 "381f84679b7e898b7dfb9a0f69ba657f53c9d85fc33ceae0d2b193d5311743c7" => :mavericks
   end
 
   def install
