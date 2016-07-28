9a4db60863f1cd67abb6cea593f892b004726e37
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 20:44:01 +0100

    transdecoder: update 3.0.0 bottle.

--- transdecoder.rb
+++ transdecoder.rb
@@ -9,11 +9,10 @@ class Transdecoder < Formula
   head "https://github.com/TransDecoder/TransDecoder.git"
 
   bottle do
-    cellar :any
-    sha256 "559044bf89f298ede284e3a9f72ef0fc7bd6c912e2b5ee753ceade016370d704" => :yosemite
-    sha256 "fae619c6f41eec60f5bf1bd76af2b110253be8fee275c7610c3bf7ac80873b02" => :mavericks
-    sha256 "549db77a17285d8a616d64e1e6b9baf21e3beb9e424d6427dfce9af43737c30a" => :mountain_lion
-    sha256 "36447454b2c6504477ef1335989cad531efaa4c2032646e6d8356b440625cbaf" => :x86_64_linux
+    cellar :any_skip_relocation
+    sha256 "ce741f3518c5cb58affd3b81448c15e744ebe94aebdd5643b48862cf05834eb2" => :el_capitan
+    sha256 "4a50568376ef0523f282e83df50e022d967c96973cbbefc39facab97c5203f85" => :yosemite
+    sha256 "eff88daefa4b2d0478e396c96f6106d6930cde4a096efad125a2cd544cab1c9c" => :mavericks
   end
 
   depends_on "cd-hit"
