6593cb1d45470b58c9e3e99b7859131b91d5bd21
ilovezfs <ilovezfs@icloud.com>
2016-07-28 20:40:15 -0700

    seqan 2.2.0
    Closes #3897.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- seqan.rb
+++ seqan.rb
@@ -4,10 +4,9 @@ class Seqan < Formula
   # doi "10.1186/1471-2105-9-11"
   # tag "bioinformatics"
 
-  url "https://github.com/seqan/seqan/releases/download/seqan-v2.1.0/seqan-library-2.1.0.tar.xz"
-  mirror "http://packages.seqan.de/seqan-library/seqan-library-2.1.0.tar.xz"
-  sha256 "986a55d028fb08e0befc538af927cbcfb880e7d4402f9b008d09f76abe63f86e"
-
+  url "https://github.com/seqan/seqan/releases/download/seqan-v2.2.0/seqan-library-2.2.0.tar.xz"
+  mirror "http://packages.seqan.de/seqan-library/seqan-library-2.2.0.tar.xz"
+  sha256 "b5c036a3d2fc2fe5f2d57dcd4d7c523a6df146ab7b44bc789f42004b058d72fd"
   head "https://github.com/seqan/seqan.git"
 
   # seqan-library installs only header files.
@@ -15,7 +14,15 @@ class Seqan < Formula
 
   def install
     include.install "include/seqan"
-    doc.install Dir["share/doc/seqan/*"] unless build.head?
+    if build.head?
+      prefix.install_metafiles
+      pkgshare.install Dir["*"]
+    else
+      prefix.install_metafiles "share/doc/seqan"
+      doc.install Dir["share/doc/seqan/*"]
+      pkgshare.install "share/cmake"
+      lib.install "share/pkgconfig"
+    end
   end
 
   test do
