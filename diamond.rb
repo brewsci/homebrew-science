b20705348700e8dcaddf5cf4257193e29536f068
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-29 09:47:44 +0100

    diamond: update 0.8.16 bottle.

--- diamond.rb
+++ diamond.rb
@@ -8,10 +8,10 @@ class Diamond < Formula
   sha256 "369cbacc8169671a299cb87278a868fea3179da7d4c18eabf04f4c8750a5e482"
 
   bottle do
-    sha256 "e0eb3edc6f875a6b8e37b2d369b27510ec714faf2e94ff414edb94fbd34a1141" => :yosemite
-    sha256 "da0750e96465902fbd7827dc2220c2cb298f71ae488d6175885eb2044e2066ad" => :mavericks
-    sha256 "459c5a98274de600b7a270e51a589c21bae1add6384fe76d4a4b5c3d988778cb" => :mountain_lion
-    sha256 "3a2da6cefa347fd6530eb53ced02e3b10212eb9c560566c7801fde3836fca862" => :x86_64_linux
+    cellar :any_skip_relocation
+    sha256 "e433e71d12831d56e68220397e771418f597208141df7190dfd468df20014dcd" => :el_capitan
+    sha256 "dcf60340842ab8b5699f383453d6ad0aa4824fe179f73b570fb0817065b72718" => :yosemite
+    sha256 "923afaff8450e1f11568e6b1b4adce00d1afb7d0ee33f254cef1f68cad171b60" => :mavericks
   end
 
   depends_on "cmake" => :build
