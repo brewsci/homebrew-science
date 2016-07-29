4e7e076a1ef1144aa5b5ae509520950964d36422
Andrew Janke <andrew@apjanke.net>
2016-07-28 21:43:45 -0400

    adam: audit fixes

--- adam.rb
+++ adam.rb
@@ -1,7 +1,7 @@
 class Adam < Formula
-  desc "Genomics analysis platform with specialized file formats built using Apache Avro, Apache Spark and Parquet"
+  desc "Genomics analysis platform using Apache Avro, Spark, and Parquet"
   homepage "https://github.com/bigdatagenomics/adam"
-  url "https://repo1.maven.org/maven2/org/bdgenomics/adam/adam-distribution_2.10/0.19.0/adam-distribution_2.10-0.19.0-bin.tar.gz"
+  url "https://search.maven.org/remotecontent?filepath=org/bdgenomics/adam/adam-distribution_2.10/0.19.0/adam-distribution_2.10-0.19.0-bin.tar.gz"
   sha256 "9010b198e8fc38de24030a2d00ae86351a6a3af2a3f62b3d21251648e3d60524"
 
   bottle do
@@ -11,20 +11,22 @@ class Adam < Formula
     sha256 "a4cc0fae830a47294b6d83741cbbd5248c3707af8f8a56c418b886c66941424e" => :mavericks
   end
 
-  depends_on "apache-spark"
-
   head do
     url "https://github.com/bigdatagenomics/adam.git", :shallow => false
     depends_on "maven" => :build
   end
 
-  option "without-check", "Disable build-time checking (not recommended)"
+  option "without-test", "Disable build-time checking (not recommended)"
+
+  deprecated_option "without-check" => "without-test"
+
+  depends_on "apache-spark"
 
   def install
     if build.head?
       system "mvn", "clean", "install",
                     "-DskipAssembly=True",
-                    "-DskipTests=" + (build.with?("check") ? "False" : "True")
+                    "-DskipTests=" + (build.with?("test") ? "False" : "True")
       libexec.install Dir["adam-cli/target/appassembler/*"]
     else
       libexec.install Dir["*"]
