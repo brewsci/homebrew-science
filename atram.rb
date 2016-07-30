139a4814662fd18f71fa49a168fa9b54558b56da
Shaun Jackman <sjackman@gmail.com>
2016-07-29 17:15:29 -0700

    atram: update 1.04_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#238.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- atram.rb
+++ atram.rb
@@ -15,6 +15,7 @@ class Atram < Formula
     sha256 "1f676dc882d3ca11f24dd1f97968985db717bcdecbcb4b4cb8c10ef2894ea85e" => :el_capitan
     sha256 "d0912c55cf87b536371d178312c17a35904cff15a30f5402086ec8cf3c982c80" => :yosemite
     sha256 "ff50f781b9940d5fca761d2894cc36f11f2a8928a1ae600254e0882fb4de9505" => :mavericks
+    sha256 "46244967080ec4411d85949741d5a74ca3e76ad42c4a7335fff4be1d3e378768" => :x86_64_linux
   end
 
   depends_on "blast"
