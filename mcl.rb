ed3d6b9be7897226c60c0b594fd06544c99417e1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-30 06:51:13 +0000

    mcl: update 14-137 bottle for Linuxbrew.

--- mcl.rb
+++ mcl.rb
@@ -12,6 +12,7 @@ class Mcl < Formula
     sha256 "96193e00265275f392d64742e6298a482832cd4a7848a9e4e4b18d8440d22075" => :el_capitan
     sha256 "7224fd6d488de1671a28b905a39a2a4451c02a53faff329940b7d9793c82d5bb" => :yosemite
     sha256 "dc43d7491e66496ff3358ed9fb5c057a0bd73e6d9e5227221f5b8842b17980fc" => :mavericks
+    sha256 "f48c0b0d8b161c2915ff6be8aa3184890f1651418ced9a17cd8288fa74406b1d" => :x86_64_linux
   end
 
   def install
