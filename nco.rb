d0d2a89d78998fb4c60b8ee2a17a375024f4d848
ilovezfs <ilovezfs@icloud.com>
2016-07-30 21:51:38 -0700

    nco 4.6.0
    Closes #3915.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nco.rb
+++ nco.rb
@@ -1,20 +1,15 @@
 class Nco < Formula
+  desc "Command-line operators for netCDF and HDF files"
   homepage "http://nco.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/nco/nco-4.4.8.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/n/nco/nco_4.4.8.orig.tar.gz"
-  sha256 "974dea6ac11d8b265e8dd5b29376fede5ea76a50dc06afb4d5de49d7e8d48774"
+  url "https://downloads.sourceforge.net/project/nco/nco-4.6.0.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/n/nco/nco_4.6.0.orig.tar.gz"
+  sha256 "2ebeec6456255d363e9f5ef92d45cd809c058495d2c3920de3eacda5098986a9"
 
   bottle do
     cellar :any
     sha256 "149711db327862ee54ac2746f70574ac825dd25a8a0a406d99a1b3ebe1132b01" => :yosemite
     sha256 "c20f5aed3a1975e73fab0fa04c55f7d6b01e31c78df56ffb0da7727317e5ba10" => :mavericks
     sha256 "39507a6ef69f69038a336ddc8422faa92131a7420af4477b715463cbe5c4a67a" => :mountain_lion
-  end
-
-  devel do
-    url "https://github.com/czender/nco/archive/nco-4.4.9-alpha.tar.gz"
-    sha256 "bebd22d810b9a58e3a148fe316929c238403c6352b77f3ea9757662222540204"
-    version "4.4.9-alpha"
   end
 
   head do
