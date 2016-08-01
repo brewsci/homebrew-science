cf50f996fb3772e17b5428b5c39e99b44e120fa3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-01 18:41:12 +0000

    busco: update 1.22_1 bottle for Linuxbrew.

--- busco.rb
+++ busco.rb
@@ -12,6 +12,7 @@ class Busco < Formula
     sha256 "146e3caa137d45be78e03aae3b832e8c6bcbeeeb7919505f7d4f3cc5842c8cf9" => :el_capitan
     sha256 "12f4fe1bf7d9199aa2ff5f9579051cc741fbd451ae801943caa3df3baeafa8dd" => :yosemite
     sha256 "c4136ee88129c555fe0468d8e600e82c25a9b505c6a7199c74987fb8b3f71861" => :mavericks
+    sha256 "1f0f9d7ae6d7650fc307bef6b1520c87e1d7c3d839a508e021bba6e5c75cad3d" => :x86_64_linux
   end
 
   depends_on :python
