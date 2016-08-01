2e43b2c7f21d62843a5265d11b77b80370e5981f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-01 12:52:48 +0100

    clinfo: update 2.1.16.01.12 bottle.

--- clinfo.rb
+++ clinfo.rb
@@ -5,10 +5,10 @@ class Clinfo < Formula
   sha256 "f92fc60f337ad86c8506d7d03358bf47980cb08fca1a0ca496b15282db59dea3"
 
   bottle do
-    cellar :any
-    sha256 "0e61fd78b8fc24277c5363054eae0dd49f5758c5ecb8361505c4ea86f5deb181" => :yosemite
-    sha256 "6471d9d1c00df767e1f170745b11c6abb1ebf8ddb39946c5fd12ae145c2e587a" => :mavericks
-    sha256 "83e58178fc2ac6189ababf8a65e0280e3e84536b8dda7b999e85bd84ffa9ddb3" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "d9d4b007c3d33031ad4337037c7e517c547243cd7f6f486396f575132c6ba6d0" => :el_capitan
+    sha256 "861f4c5da56d208b7a38ebb5dd7c4c21ca3240e16a0175dea3f8aec29150bbd4" => :yosemite
+    sha256 "1c118487256f381b4b51168e062262c7ba31c09ae4fef06f73c0b44a7a3d508d" => :mavericks
   end
 
   def install
