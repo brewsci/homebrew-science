9291a45d047eb180edccefb4f63e9a4b22984fef
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-02 13:39:56 +0000

    fastqc: update 0.11.5 bottle for Linuxbrew.

--- fastqc.rb
+++ fastqc.rb
@@ -11,6 +11,7 @@ class Fastqc < Formula
     sha256 "19e697374ecfde749c3d8ee4c7a8a6cff4b080bd6c5964345922880312f3afa1" => :el_capitan
     sha256 "978abf5981b17859e6370f24dbff84e7572b7e7f6c9ec2942f1c17c742c5ad9f" => :yosemite
     sha256 "274ad6885a1c7d3bdb496c253f03bd29a1ac4341ee766e2180c4ef41db3b4210" => :mavericks
+    sha256 "3d3db346d39306991f675ddf8dbeeb665bc8b59513bbc10fd9882441cce70a3f" => :x86_64_linux
   end
 
   depends_on :java
