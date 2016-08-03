342583fe5bd5026644720c5f82a4e288feb2f002
ilovezfs <ilovezfs@icloud.com>
2016-08-02 21:02:34 -0700

    trans-abyss 1.5.5
    Closes #3997.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- trans-abyss.rb
+++ trans-abyss.rb
@@ -4,12 +4,11 @@ class TransAbyss < Formula
   # doi "10.1038/nmeth.1517"
   # tag "bioinformatics"
 
-  url "https://github.com/bcgsc/transabyss/archive/1.5.3.tar.gz"
-  sha256 "0661f70d9d971edb3b225906082860c5531adcc4ecb4cceb24365b96c529af96"
+  url "https://github.com/bcgsc/transabyss/archive/1.5.5.tar.gz"
+  sha256 "e3e92661a97ac9f7e097182a1040675bc5a334253ed098898411f3952bddebe3"
+  head "https://github.com/bcgsc/transabyss.git"
 
   bottle :unneeded
-
-  head "https://github.com/bcgsc/transabyss.git"
 
   depends_on "abyss"
   depends_on "blat"
@@ -25,14 +24,11 @@ class TransAbyss < Formula
 
   def install
     prefix.install Dir["*"]
-    bin.install_symlink "../transabyss",
-      "../transabyss-analyze",
-      "../transabyss-merge"
+    bin.install_symlink "../transabyss", "../transabyss-merge"
   end
 
   test do
     system "#{bin}/transabyss", "--help"
-    system "#{bin}/transabyss-analyze", "--help"
     system "#{bin}/transabyss-merge", "--help"
   end
 end
