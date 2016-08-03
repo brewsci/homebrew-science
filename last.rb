439bf2b6fbadf6a1c3833ec70a0961482b4d3bc5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-03 00:25:57 +0000

    last: update 752 bottle for Linuxbrew.

--- last.rb
+++ last.rb
@@ -12,6 +12,7 @@ class Last < Formula
     sha256 "42425823c96441555576551dfcde5b343c2c2f5236f59262a5eb8284a890bb2b" => :el_capitan
     sha256 "8b6623e595b0219cf4747766793a97597048e2859224c2cbfc4cc0688f97454a" => :yosemite
     sha256 "c1851700dac785895aa0bbea9d0dc31ba8c33b9913933cab2df9e93ebaaa9085" => :mavericks
+    sha256 "bbe981e9a0b324301e75656d8568db46ef6e2c560fa5f819017aa95a1259b78c" => :x86_64_linux
   end
 
   resource "mito" do
