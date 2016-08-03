158febc5935446cf57faa2032c07361e9ff51388
Shaun Jackman <sjackman@gmail.com>
2016-08-02 17:00:35 -0700

    diamond: update 0.8.17 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#264.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- diamond.rb
+++ diamond.rb
@@ -12,6 +12,7 @@ class Diamond < Formula
     sha256 "c1a0d3b5f4189b1cfe73b43b20e4a630d235810f81f4e76bb60390645e9c4d08" => :el_capitan
     sha256 "8c9f470bfefc001ca8cf580b15426eb913b716018545f326d2f6a44208398e9f" => :yosemite
     sha256 "e5e6bef6520a2b066cf4f1675445c9aad060d6d2f686923cf4f9ce2779797fc3" => :mavericks
+    sha256 "5b211631e200701cfe43ffe82aa7cb778bc0d73b4b73999817c2feabfc3398cf" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
