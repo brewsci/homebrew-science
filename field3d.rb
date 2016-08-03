86143f7188a8599978922bd21dc35b4ea08fbf76
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-03 06:28:12 +0100

    field3d: update 1.6.1_3 bottle.

--- field3d.rb
+++ field3d.rb
@@ -8,9 +8,9 @@ class Field3d < Formula
 
   bottle do
     cellar :any
-    sha256 "b3b659d655a043988258219f64ae83a521a4c65e6a8d851b1f942d9b8827e8e4" => :el_capitan
-    sha256 "3d60c590970faf76a95ac3331e6e2119f48ad0ec678f98f3cfff61b4a13ae0bf" => :yosemite
-    sha256 "e724ee6d0a3dfd66222b69ef9f1ce75636e01a23748a5a97f5883ea5c162aa7d" => :mavericks
+    sha256 "8be7ea3337e7287fa1d65a6097f762bc2849a4b11a2378e8e7138a4df308321a" => :el_capitan
+    sha256 "3624d9842d9e7f933fe1170bd13ed20dc8b96a0615c938fff72c6f80b5dff657" => :yosemite
+    sha256 "a070c4afd8fc757239d0a4513bef41ff3fe83cac1f36e55298b00cc858b8697b" => :mavericks
   end
 
   depends_on "scons" => :build
