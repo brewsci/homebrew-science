4942f7962152142855f16eda797af2114868f5e0
Shaun Jackman <sjackman@gmail.com>
2016-08-04 09:38:22 -0700

    lobstr: Add DOI and tag bioinformatics

--- lobstr.rb
+++ lobstr.rb
@@ -4,6 +4,8 @@ class Lobstr < Formula
   url "https://github.com/mgymrek/lobstr-code/releases/download/v4.0.6/lobSTR-4.0.6.tar.gz"
   sha256 "f13bfc17eebd4aadd58fd798941318d8278a2da2e64e596027ebc26e004ce31c"
   revision 1
+  # doi "10.1101/gr.135780.111"
+  # tag "bioinformatics"
 
   bottle do
     cellar :any
