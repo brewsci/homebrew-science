63a2402ae521a10d171e5c809352b794a519b6b1
Shaun Jackman <sjackman@gmail.com>
2016-08-04 11:57:43 -0700

    libsequence: update 1.8.4_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#268.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- libsequence.rb
+++ libsequence.rb
@@ -13,6 +13,7 @@ class Libsequence < Formula
     sha256 "20d0a5c889514a6aeacee6e81b857bb42f9a03533a5d3294e3ca510df19aa218" => :el_capitan
     sha256 "1cb31354055aedf42c1013880137df170460a5170634ec32b6130f70c84cafff" => :yosemite
     sha256 "590b3e6113be8d67d1c1cfec48d92337f6736f69ae03c3e138cb9ad70bb39980" => :mavericks
+    sha256 "de3d4025d3516cc0efe10bf96fb13813ebccabefe6e7bf52ba07614964b7f178" => :x86_64_linux
   end
 
   cxx11 = OS.linux? || MacOS.version > :mountain_lion ? [] : ["c++11"]
