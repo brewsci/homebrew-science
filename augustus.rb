c81e9df58182eca931e3a8b52d7138d7c94f5102
Shaun Jackman <sjackman@gmail.com>
2016-08-04 10:55:21 -0700

    augustus: update 3.2.2_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#272.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- augustus.rb
+++ augustus.rb
@@ -13,6 +13,7 @@ class Augustus < Formula
     sha256 "40e3004de6efeab4b5c51a8cdcd550da16e67c9e7d88c303bb9ad0c5ea28c858" => :el_capitan
     sha256 "1eed43538484957cbf3a21dec725950adac6620b57f93f0c0686fc1038c931fa" => :yosemite
     sha256 "51383cc511a460b6c9cf301a862d991b25e3f2e8d0e9ed51dfa1a27d03c61749" => :mavericks
+    sha256 "e8bf9a9233f323f203e58c688ded56dcf97f2bab3d13c40947a480b0733f223d" => :x86_64_linux
   end
 
   option "with-cgp",  "Enable comparative gene prediction"
