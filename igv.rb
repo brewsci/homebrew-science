c8c05436e295756070f013a80572d962372e5c21
Shaun Jackman <sjackman@gmail.com>
2016-08-05 08:01:15 -0700

    igv 2.3.80
    Fix test sandbox error.
    
    Closes #4016.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- igv.rb
+++ igv.rb
@@ -3,8 +3,8 @@ class Igv < Formula
   homepage "http://www.broadinstitute.org/software/igv"
   # tag "bioinformatics"
   # doi "10.1093/bib/bbs017"
-  url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.68.zip"
-  sha256 "763781a5c655d22dd2c07096079ba6eadb73ebde80a9f84ae9cdcea2b4921ee0"
+  url "http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.80.zip"
+  sha256 "f95368bb5c359b28786e8cf2d9c44240f44ad5bad7cd41ba8ae22c3608213535"
   head "https://github.com/broadinstitute/IGV.git"
 
   bottle :unneeded
@@ -19,7 +19,9 @@ class Igv < Formula
   end
 
   test do
+    ENV.append "_JAVA_OPTIONS", "-Duser.home=#{testpath}/java_cache"
     (testpath/"script").write "exit"
-    assert_match "IGV", `#{bin}/igv -b script`
+    # This command returns 0 on Circle and Travis but 1 on BrewTestBot.
+    assert_match "Version", `#{bin}/igv -b script`
   end
 end
