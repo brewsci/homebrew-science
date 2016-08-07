29ee0ff1eb632ea91d66b59b0f0513c7ca1c8a31
ilovezfs <ilovezfs@icloud.com>
2016-08-06 22:52:22 -0700

    stringtie 1.2.4
    Closes #4025.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- stringtie.rb
+++ stringtie.rb
@@ -1,6 +1,13 @@
 class Stringtie < Formula
+  desc "Transcript assembly and quantification for RNA-Seq"
   homepage "http://ccb.jhu.edu/software/stringtie"
-  head "https://github.com/gpertea/stringtie"
+  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.2.4.tar.gz"
+  sha256 "7fcd64c3ad73816cdb446c5d703a1c46cf828db88871221267532355168b0dc9"
+  head "https://github.com/gpertea/stringtie.git"
+
+  # doi "10.1038/nbt.3122"
+  # tag "bioinformatics"
+
   bottle do
     cellar :any
     sha256 "de3a457c45fc023c2b9c4e2d8530ec77ade761269897af535fb82a5a904bb354" => :yosemite
@@ -8,12 +15,6 @@ class Stringtie < Formula
     sha256 "03e7791e8119e5ab2e38eb417bd289f6a266bede9d52bbd98a33e79797c98785" => :mountain_lion
     sha256 "d3274cea211bb0ff362eda641097ca9abbd7ab02fc4cbe37d45579140cbfd495" => :x86_64_linux
   end
-
-  # doi "10.1038/nbt.3122"
-  # tag "bioinformatics"
-
-  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.0.4.tar.gz"
-  sha256 "635099d543bfaf0ec1c84020eb4aa3375714c12e2d0d435dae44901d49fe3ef2"
 
   def install
     system "make", "release"
