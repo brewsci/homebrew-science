da42d0499a04957dd471c73e7e93d13baf7a72e4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-07 06:55:32 +0100

    stringtie: update 1.2.4 bottle.

--- stringtie.rb
+++ stringtie.rb
@@ -9,11 +9,10 @@ class Stringtie < Formula
   # tag "bioinformatics"
 
   bottle do
-    cellar :any
-    sha256 "de3a457c45fc023c2b9c4e2d8530ec77ade761269897af535fb82a5a904bb354" => :yosemite
-    sha256 "157abc752800f726bce574ac5dc1322e2ed112b953fdd06dcdf819c1d7abf258" => :mavericks
-    sha256 "03e7791e8119e5ab2e38eb417bd289f6a266bede9d52bbd98a33e79797c98785" => :mountain_lion
-    sha256 "d3274cea211bb0ff362eda641097ca9abbd7ab02fc4cbe37d45579140cbfd495" => :x86_64_linux
+    cellar :any_skip_relocation
+    sha256 "240dca6cb315b04fb50aa51ef80e4f8d55118a89ae35954611c2a82bf17cefbb" => :el_capitan
+    sha256 "8cf1175bb39b744920c841c979a20e9e89627a45f6ef3b59011b5866ac56b906" => :yosemite
+    sha256 "1583cb027d940bd432ca1b87a61d8545fbbdda3760f615af541eb906fca7add7" => :mavericks
   end
 
   def install
