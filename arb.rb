dd633ac3ba2d40148962572e2ff580ce1e0f7824
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-11 13:26:54 -0300

    arb: add desc
    Closes #4038.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- arb.rb
+++ arb.rb
@@ -1,4 +1,5 @@
 class Arb < Formula
+  desc "C library for arbitrary-precision floating-point ball arithmetic"
   homepage "http://fredrikj.net/arb/index.html"
   url "https://github.com/fredrik-johansson/arb/archive/2.5.0.tar.gz"
   sha256 "1c741b3d7c7a350c01572fb9cdd8218049c669409dff21c1a569a163942e4803"
