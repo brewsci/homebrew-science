f7e0cb0c82b20ad66e8d402d0cd07d7da0b19f6b
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-11 13:22:39 -0300

    alpscore: update code position
    Closes #4040.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- alpscore.rb
+++ alpscore.rb
@@ -3,8 +3,8 @@ class Alpscore < Formula
   homepage "http://alpscore.org"
   url "https://github.com/ALPSCore/ALPSCore/archive/v0.5.4.tar.gz"
   sha256 "909e6a06673e7fa0d6c985f46ecf0090ca7a5ff682002fe63b0270c578ca3b82"
-  head "https://github.com/ALPSCore/ALPSCore.git"
   revision 1
+  head "https://github.com/ALPSCore/ALPSCore.git"
 
   bottle do
     cellar :any
