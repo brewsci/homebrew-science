80e4d71794255c7c2630a5c4fe73ade52d65a6bf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-13 12:04:57 +0100

    tbl2asn: update 25.0 bottle.

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -15,10 +15,9 @@ class Tbl2asn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "46ee9b013b621f4853dc3c07f5d5289b083eb500e9609ab87ce0ef204973d8ed" => :el_capitan
-    sha256 "fe9d754a97c7e313fee8e0ac0883dd635645dd9eea44f4da3dac4047669b2b6c" => :yosemite
-    sha256 "4c12f59c562e6153ef2f34cead09a5331b15296be16423347ae9d4a9c1d826f9" => :mavericks
-    sha256 "de337431a34cf93ed60a47b4327022452202c9ba648584b07edfdfaa8786c284" => :x86_64_linux
+    sha256 "272ee59c3e42c288db111238b976eb25a28c14f24a4a3e09f62f7e8614d80c8e" => :el_capitan
+    sha256 "ecc8f607eb078ae3f32a771c28551071c12ab62723321e4502fb47866451275a" => :yosemite
+    sha256 "b6454c75f3b57c670628cfb11992abf3147427f9d9041815ee4810f91a57e84b" => :mavericks
   end
 
   resource "doc" do
