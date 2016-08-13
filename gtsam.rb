21ff403209b94c25faf57442ba784cdafbcf8005
ilovezfs <ilovezfs@icloud.com>
2016-08-13 02:58:21 -0700

    gtsam: depend on boost155
    always run the tests
    audit fixes
    
    Closes #3931.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- gtsam.rb
+++ gtsam.rb
@@ -1,7 +1,9 @@
 class Gtsam < Formula
+  desc "Library of C++ classes that implement SAM in robotics and vision"
   homepage "https://collab.cc.gatech.edu/borg/gtsam/"
   url "https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-3.2.1.tgz"
   sha256 "1e9217c11d92e6838e2d0bec3a7dd0d36d2131acdf2e50264f6dc225d8ce1a97"
+  revision 1
 
   bottle do
     sha256 "5e1040d1a55f395b038e5ee5647377a2b8182f2af0536f8e8c028c81cc7f57d9" => :yosemite
@@ -9,15 +11,17 @@ class Gtsam < Formula
     sha256 "4c9a630f008d37f1ecd19cd1ce87912206f2eed24caf0c7b6404b5c08eba74cf" => :mountain_lion
   end
 
-  depends_on "cmake" => :build
-  depends_on "boost"
+  option "without-test", "Run unit tests at build-time"
 
-  option "with-tests", "Run unit tests at build-time"
+  deprecated_option "with-tests" => "with-test"
+
+  depends_on "cmake" => :build
+  depends_on "homebrew/versions/boost155"
 
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
-      system "make", "check" if build.with? "tests"
+      system "make", "check" if build.with? "test"
       system "make", "install"
     end
   end
