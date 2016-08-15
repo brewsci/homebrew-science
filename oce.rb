f4bd2303b94315ea86ba0c05a9b1b0d6c2dc3a5d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-15 18:15:46 +0100

    oce: update 0.17.2 bottle.

--- oce.rb
+++ oce.rb
@@ -5,9 +5,9 @@ class Oce < Formula
   sha256 "8d9995360cd531cbd4a7aa4ca5ed969f08ec7c7a37755e2f3d4ef832c1b2f56e"
 
   bottle do
-    sha256 "75932d64aab68b7fad6c27a365cee5158924d311f9fbe789c063379b87759a2d" => :yosemite
-    sha256 "b98386e67112e79bfecb597f61fd00e86d8c2ed0b51fa07bdf5d3b105b34d9e0" => :mavericks
-    sha256 "c7e01db93d10a1bcccb7f52419caaec057eeb5cf246f25f7ca2514533b1d83eb" => :mountain_lion
+    sha256 "73427f93d1bb2be3b1c40145e67e74efe0843867340397cf6b3604ab42c6f82c" => :el_capitan
+    sha256 "1c3347d52fa7fe46f2bdf97adc79afcd3b926340abe697242bf271bc1af624b2" => :yosemite
+    sha256 "c1bcb365bacefd1238d0ddc96382f5cae53180d3be698a6a09f5f654ac3e8a45" => :mavericks
   end
 
   option "without-opencl", "Build without OpenCL support"
