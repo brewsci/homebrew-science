d75b2f2a67933bf72e68d514e38e89b778b398a1
Andrew Janke <janke@eilonwy.local>
2016-08-15 16:47:48 -0400

    bpel2owfn: audit fixes

--- bpel2owfn.rb
+++ bpel2owfn.rb
@@ -1,4 +1,5 @@
 class Bpel2owfn < Formula
+  desc "WS-BPEL to oWFN translator"
   homepage "https://www.gnu.org/software/bpel2owfn"
   url "http://download.gna.org/service-tech/bpel2owfn/bpel2owfn-2.4.tar.gz"
   sha256 "1a6e49673bb9c30491d26904a26f8f71fc310676833c4e8c5d89aca8dfa43ad6"
@@ -17,7 +18,7 @@ class Bpel2owfn < Formula
   end
 
   def install
-    system "autoreconf -i" if build.head?
+    system "autoreconf", "-i" if build.head?
     system "./configure", "--disable-assert",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
