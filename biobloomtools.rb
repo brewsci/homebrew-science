cb908b0e404348523d63966681e4782539d8457d
Andrew Janke <janke@eilonwy.local>
2016-08-15 16:09:25 -0400

    biobloomtools: audit fix

--- biobloomtools.rb
+++ biobloomtools.rb
@@ -7,18 +7,18 @@ class Biobloomtools < Formula
   url "https://github.com/bcgsc/biobloom/releases/download/2.0.12/biobloomtools-2.0.12.tar.gz"
   sha256 "13053036ca4a23032a7fb201bf22862187e4d8f584c3b1f6440d829210954a3e"
 
-  head do
-    url "https://github.com/bcgsc/biobloom.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
   bottle do
     cellar :any
     sha256 "68c125cd6e5e02b1eb2aa79e40ca6e82c2bb2ed8764f6d11445463fb460032d4" => :yosemite
     sha256 "fcf60aca3e3326a1a422535190de56d2c8dc1d26af1cd1320f642485db7fe64c" => :mavericks
     sha256 "0730a556dc8b5cbc99f653ab8eeb4de922875ca3b23113b9103cf42406ec6a43" => :mountain_lion
     sha256 "96ecc7bc17de7cb5e33b68ba00eadcc249106714675190cd819fa47173f0557c" => :x86_64_linux
+  end
+
+  head do
+    url "https://github.com/bcgsc/biobloom.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
   end
 
   depends_on "boost" => :build
