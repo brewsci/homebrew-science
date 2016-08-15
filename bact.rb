b15d8b31621ae79b3c4201eb45229a20d97dca66
Andrew Janke <floss@apjanke.net>
2016-08-15 19:26:20 -0400

    bact: audit fix, add test do (#4066)

--- bact.rb
+++ bact.rb
@@ -1,4 +1,5 @@
 class Bact < Formula
+  desc "Machine learning tool for labeled orderd trees"
   homepage "http://chasen.org/~taku/software/bact/"
   url "http://chasen.org/~taku/software/bact/bact-0.13.tar.gz"
   sha256 "4aa55c30621afd3793bd15471b3c0d77d3b489051cbfb30a9f0640de8928ab40"
@@ -16,5 +17,9 @@ class Bact < Formula
     bin.install "bact_learn", "bact_classify", "bact_mkmodel"
     doc.install "README", "AUTHORS", "COPYING", "index.html", "bact.css"
     pkgshare.install Dir["jp*"], Dir["med.*"]
+  end
+
+  test do
+    system "bact_learn", "#{pkgshare}/jp.test", "./jp.model"
   end
 end
