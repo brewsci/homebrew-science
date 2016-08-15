c2c64a5b699295a2a755f6084e8a6a9010838217
Andrew Janke <janke@eilonwy.local>
2016-08-15 16:10:28 -0400

    biocgal: add desc

--- biocgal.rb
+++ biocgal.rb
@@ -1,7 +1,7 @@
 class Biocgal < Formula
   # The name CGAL conflicts with
   # Computational Geometry Algorithms Library
-
+  desc "Tool for computing genome assembly likelihoods"
   homepage "http://bio.math.berkeley.edu/cgal/"
   url "http://bio.math.berkeley.edu/cgal/cgal-0.9.6-beta.tar"
   sha256 "052e2f2c01fc3a5a7e1a0582a17a51668930cde1f73a8a9e5564a65ddb41c47e"
