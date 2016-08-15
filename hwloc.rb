acb4d0691e0344b7693b7d1b44c67b3a1d8eea88
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-15 14:54:58 +0000

    hwloc: update 1.11.3 bottle for Linuxbrew.

--- hwloc.rb
+++ hwloc.rb
@@ -9,6 +9,7 @@ class Hwloc < Formula
     sha256 "685e2e21f9d2986dd89357f2a88dd1c24f6b7e32eda6a6cb3ac3731f54d6c02d" => :el_capitan
     sha256 "3f2d02f88cfb12df97056c0a2ca1567feaecf987bba16b34001c51a50002dc1f" => :yosemite
     sha256 "177c80b79f094ec60b6ea0e0e35f86fbb90b49a32b0772aec92f5e0edce8c8a4" => :mavericks
+    sha256 "91737b14f08b8307397791536f0082fd6960d03637e825bd319c521ceb2504c1" => :x86_64_linux
   end
 
   head do
