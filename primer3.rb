21bd0902533a93b8242aad4a02f08f0c4b814b9a
Gaik Tamazian <mail@gtamazian.com>
2016-08-18 20:01:31 +0300

    primer3 2.3.7
    Closes #4074.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- primer3.rb
+++ primer3.rb
@@ -1,8 +1,8 @@
 class Primer3 < Formula
   desc "Program for designing PCR primers"
   homepage "http://primer3.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/primer3/primer3/2.3.6/primer3-src-2.3.6.tar.gz"
-  sha256 "2ff54faf957f0d7e4c79d9536fa1027b028bc5e4c6005f142df42ef85562ecd4"
+  url "https://downloads.sourceforge.net/project/primer3/primer3/2.3.7/primer3-2.3.7.tar.gz"
+  sha256 "9e37a301d2ca9e5034c9a9d4afcb3e90041f6cc44c658b63598d0ccfcb176e5e"
 
   bottle do
     cellar :any
@@ -11,18 +11,19 @@ class Primer3 < Formula
     sha256 "0eb3406fb87e960f7c46656f746db8849f2daf3ab9aee31154107312ebab0bf2" => :mountain_lion
   end
 
-  option "without-check", "Skip build-time tests"
+  option "without-test", "Skip build-time tests"
+  deprecated_option "without-check" => "without-test"
 
   def install
     cd "src" do
       system "make"
-      system "make", "test" if build.with? "check"
+      system "make", "test" if build.with? "test"
       bin.install %w[primer3_core ntdpal ntthal oligotm long_seq_tm_test]
       pkgshare.install "primer3_config"
     end
   end
 
   test do
-    system "#{bin}/long_seq_tm_test AAAAGGGCCCCCCCCTTTTTTTTTTT 3 20"
+    system "#{bin}/long_seq_tm_test", "AAAAGGGCCCCCCCCTTTTTTTTTTT", "3", "20"
   end
 end
