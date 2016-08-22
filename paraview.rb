fa339715fd1e796ecbfe913520f8a5c4fee13cb3
Dominique Orban <dominique.orban@gmail.com>
2016-08-22 19:49:13 -0400

    paraview: patch for ffmpeg
    Fixes #3880
    Closes #3982
    
    Closes #4090.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- paraview.rb
+++ paraview.rb
@@ -3,6 +3,7 @@ class Paraview < Formula
   url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.2&type=source&os=all&downloadFile=ParaView-v4.2.0-source.tar.gz"
   sha256 "ac26cc5fe5ce82d27531727a01242353d40984826eaa580edea0791887a07b6b"
   head "git://paraview.org/ParaView.git"
+  revision 1
 
   bottle do
     sha256 "f3c77c1007faebe1294e72f6e431b654916ccfaa632411080932e827e5efa2bf" => :yosemite
@@ -28,7 +29,11 @@ class Paraview < Formula
 
   # Temporary fix for a cast issue related to FreeType.
   # See https://bugs.gentoo.org/show_bug.cgi?id=533444
-  patch :DATA
+  # Fix long-standing bug related to a changing FFMPEG API.
+  # See http://www.paraview.org/Bug/view.php?id=14215
+  # https://gitlab.kitware.com/paraview/paraview/issues/14215
+  # and http://www.paraview.org/Bug/view.php?id=16001
+  patch :DATA unless build.head?
 
   def install
     args = std_cmake_args + %W[
@@ -69,7 +74,7 @@ class Paraview < Formula
   end
 
   test do
-    shell_output("#{prefix}/paraview.app/Contents/MacOS/paraview --version", 1)
+    shell_output("#{opt_prefix}/paraview.app/Contents/MacOS/paraview --version", 1)
   end
 end
 
@@ -98,3 +103,58 @@ index fcbb323..7a48f62 100644
        bbox[3] = std::max(bbox[3], pen[1] + bitmapGlyph->top - 1);
        }
      else
+diff --git a/VTK/IO/FFMPEG/vtkFFMPEGWriter.cxx b/VTK/IO/FFMPEG/vtkFFMPEGWriter.cxx
+index 387a993..9e159c8 100644
+--- a/VTK/IO/FFMPEG/vtkFFMPEGWriter.cxx
++++ b/VTK/IO/FFMPEG/vtkFFMPEGWriter.cxx
+@@ -189,11 +189,11 @@ int vtkFFMPEGWriterInternal::Start()
+   c->height = this->Dim[1];
+   if (this->Writer->GetCompression())
+     {
+-    c->pix_fmt = PIX_FMT_YUVJ422P;
++    c->pix_fmt = AV_PIX_FMT_YUVJ422P;
+     }
+   else
+     {
+-    c->pix_fmt = PIX_FMT_BGR24;
++    c->pix_fmt = AV_PIX_FMT_BGR24;
+     }
+
+   //to do playback at actual recorded rate, this will need more work see also below
+@@ -272,13 +272,13 @@ int vtkFFMPEGWriterInternal::Start()
+ #endif
+
+   //for the output of the writer's input...
+-  this->rgbInput = avcodec_alloc_frame();
++  this->rgbInput = av_frame_alloc();
+   if (!this->rgbInput)
+     {
+     vtkGenericWarningMacro (<< "Could not make rgbInput avframe." );
+     return 0;
+     }
+-  int RGBsize = avpicture_get_size(PIX_FMT_RGB24, c->width, c->height);
++  int RGBsize = avpicture_get_size(AV_PIX_FMT_RGB24, c->width, c->height);
+   unsigned char *rgb = new unsigned char[RGBsize];
+   if (!rgb)
+     {
+@@ -286,10 +286,10 @@ int vtkFFMPEGWriterInternal::Start()
+     return 0;
+     }
+   //The rgb buffer should get deleted when this->rgbInput is.
+-  avpicture_fill((AVPicture *)this->rgbInput, rgb, PIX_FMT_RGB24, c->width, c->height);
++  avpicture_fill((AVPicture *)this->rgbInput, rgb, AV_PIX_FMT_RGB24, c->width, c->height);
+
+   //and for the output to the codec's input.
+-  this->yuvOutput = avcodec_alloc_frame();
++  this->yuvOutput = av_frame_alloc();
+   if (!this->yuvOutput)
+     {
+     vtkGenericWarningMacro (<< "Could not make yuvOutput avframe." );
+@@ -352,7 +352,7 @@ int vtkFFMPEGWriterInternal::Write(vtkImageData *id)
+ #else
+   //convert that to YUV for input to the codec
+   SwsContext* convert_ctx = sws_getContext(
+-    cc->width, cc->height, PIX_FMT_RGB24,
++    cc->width, cc->height, AV_PIX_FMT_RGB24,
+     cc->width, cc->height, cc->pix_fmt,
+     SWS_BICUBIC, NULL, NULL, NULL);
