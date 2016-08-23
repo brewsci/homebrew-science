4d8a94f31da394d3924385ac34b80a587b5474b6
Dominique Orban <dominique.orban@gmail.com>
2016-08-23 00:11:57 -0400

    xylib: remove zlib dependency
    Closes #3996
    
    Closes #4093.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- xylib.rb
+++ xylib.rb
@@ -3,9 +3,9 @@ class Xylib < Formula
   homepage "http://xylib.sourceforge.net"
   url "https://github.com/wojdyr/xylib/releases/download/v1.3/xylib-1.3.tar.bz2"
   sha256 "7047bc6730bc61f7f0be94e8872414768a94a409dcc10059556dbc25b1114426"
+  revision 1
 
   depends_on "boost" => :build
-  depends_on "zlib" => :recommended
 
   def install
     system "./configure", "--disable-debug",
