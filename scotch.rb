7a27744e81238458f3a66d8f44c9dc8814b69620
ilovezfs <ilovezfs@icloud.com>
2016-08-25 08:28:16 -0700

    scotch: revision for open-mpi
    Closes #4133.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- scotch.rb
+++ scotch.rb
@@ -3,7 +3,7 @@ class Scotch < Formula
   homepage "https://gforge.inria.fr/projects/scotch"
   url "https://gforge.inria.fr/frs/download.php/file/34618/scotch_6.0.4.tar.gz"
   sha256 "f53f4d71a8345ba15e2dd4e102a35fd83915abf50ea73e1bf6efe1bc2b4220c7"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
