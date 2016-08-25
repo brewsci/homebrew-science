5c6a76f8a1fa52d872c2fd850410d9b23587e8c5
ilovezfs <ilovezfs@icloud.com>
2016-08-25 08:24:13 -0700

    parmetis: revision for open-mpi
    Closes #4129.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- parmetis.rb
+++ parmetis.rb
@@ -3,7 +3,7 @@ class Parmetis < Formula
   homepage "http://glaros.dtc.umn.edu/gkhome/metis/parmetis/overview"
   url "http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz"
   sha256 "f2d9a231b7cf97f1fee6e8c9663113ebf6c240d407d3c118c55b3633d6be6e5f"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
