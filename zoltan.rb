a3f8e41cd2b71b317fc4ffb94bb27b9d6c77e658
Daniel Vollmer <code@maven.de>
2016-08-25 11:51:06 +0200

    zoltan: Update to 3.83
    Also add test
    
    Closes #4109.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- zoltan.rb
+++ zoltan.rb
@@ -1,7 +1,8 @@
 class Zoltan < Formula
+  desc "Zoltan: Parallel Partitioning, Load Balancing and Data-Management"
   homepage "http://www.cs.sandia.gov/Zoltan"
-  url "http://www.cs.sandia.gov/~kddevin/Zoltan_Distributions/zoltan_distrib_v3.81.tar.gz"
-  sha256 "9d6f2f9e2b37456cab7fe6714d51cd6d613374e915e6cc9f7fddcd72e3f38780"
+  url "http://www.cs.sandia.gov/~kddevin/Zoltan_Distributions/zoltan_distrib_v3.83.tar.gz"
+  sha256 "d0d78fdeab7a385c87d3666b8a8dc748994ff04d3fd846872a4845e12d79c1bb"
 
   bottle do
     sha256 "133207543f781aa413062f7ecbe80b89e76b96577b62fe150727fa9f499a6d3c" => :yosemite
@@ -9,7 +10,9 @@ class Zoltan < Formula
     sha256 "d60f28375337061ea183d9e1112018498ad6dc33acbeaf5764a461b30ad797d5" => :mountain_lion
   end
 
-  option "without-check", "Skip build-time tests (not recommended)"
+  option "without-test", "Skip build-time tests (not recommended)"
+
+  deprecated_option "without-check" => "without-test"
 
   depends_on "scotch"   => :optional
   depends_on "parmetis" => :optional
@@ -25,8 +28,8 @@ class Zoltan < Formula
       "CC=#{ENV["MPICC"]}",
       "CXX=#{ENV["MPICXX"]}",
     ]
-    args << "--with-scotch"         if build.with? "scotch"
-    args << "--with-parmetis"       if build.with? "parmetis"
+    args << "--with-scotch" if build.with? "scotch"
+    args << "--with-parmetis" if build.with? "parmetis"
     args += ["--enable-f90interface", "FC=#{ENV["MPIFC"]}"] if build.with? :fortran
 
     mkdir "zoltan-build" do
@@ -35,5 +38,25 @@ class Zoltan < Formula
       system "make", "check" if build.with? "check"
       system "make", "install"
     end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include "zoltan.h"
+      int main(int argc, char *argv[])
+      {
+        MPI_Init(NULL, NULL);
+        struct Zoltan_Struct *z = Zoltan_Create(MPI_COMM_SELF);
+        if (!z)
+          return -1;
+        Zoltan_Destroy(&z);
+        MPI_Finalize();
+        printf(\"%f\\n\", ZOLTAN_VERSION_NUMBER);
+        return 0;
+      }
+    EOS
+    system "mpicc", "test.c", "-o", "test", "-lzoltan"
+    system "./test"
   end
 end
