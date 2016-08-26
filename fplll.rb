dfe0752a4903b2d333b36a501bc0b05ae5225337
Tancrède Lepoint <tancrede.lepoint@sri.com>
2016-08-25 17:48:09 -0700

    fplll 5.0.1 (new formula)
    Add fplll (lattice algorithms using floating-point arithmetic)
    
    Closes #4140.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

---
+++ fplll.rb
@@ -0,0 +1,43 @@
+class Fplll < Formula
+  desc "fplll: Lattice algorithms using floating-point arithmetic"
+  homepage "https://github.com/fplll/fplll"
+  # tag "math"
+  url "https://github.com/fplll/fplll/releases/download/5.0.1/fplll-5.0.1.tar.gz"
+  sha256 "1145622cf46c309153ae26210b26b9fc1475830103be3db5a7cb5eb56b8affd1"
+
+  needs :cxx11
+
+  option "without-test", "Disable build-time checking (not recommended)"
+
+  depends_on "gmp"
+  depends_on "mpfr"
+
+  def install
+    ENV.cxx11
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                          "--with-mpfr=#{Formula["mpfr"].opt_prefix}"
+    system "make"
+    system "make", "check" if build.with? "test"
+    system "make", "install"
+  end
+
+  test do
+    ENV.cxx11
+    (testpath/"test.cpp").write <<-EOS.undent
+      // inspired from ./tests/test_lll.cpp
+      #include <fplll.h>
+
+      int main()
+      {
+        fplll::ZZ_mat<mpz_t> A;
+        A.resize(50, 50 + 1); // construct 50 × (50+1) integer relations matrix
+        A.gen_intrel(1000);   // generate 1000-bit coefficients
+        return fplll::lll_reduction(A, LLL_DEF_DELTA, LLL_DEF_ETA, LM_PROVED, FT_MPFR, 0, LLL_DEFAULT);
+      }
+    EOS
+    cxx_with_flags = ENV.cxx.split + ["test.cpp", "-lgmp", "-lmpfr", "-lfplll", "-o", "test"]
+    system *cxx_with_flags
+    system "./test"
+  end
+end
