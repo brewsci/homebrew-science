44a5a0ed7fc52dc8b06e59eb184c8a594b1c43a5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-27 20:51:56 +0100

    jsbsim: add 1.0.rc2 bottle.

--- jsbsim.rb
+++ jsbsim.rb
@@ -5,6 +5,13 @@ class Jsbsim < Formula
   version "1.0.rc2"
   sha256 "04accd4efc75867edfa6eeb814ceefebf519b2e8d750518b1de0a6aafa9442e1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a88430d291a5ffb8cff4dd62c122acc976cf10e0a7a362e06b1713447ecb742a" => :el_capitan
+    sha256 "a98477ca4a33b32e4bcabce5a0c8a941992afbde2c5056a7a15fc385fdf811a9" => :yosemite
+    sha256 "992d34b5e4c61efdaeda03c3a493db32094e63c38909fb4cdf082fdcd0dfc8c0" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
