f5abaddc2c3bfcdcb2af084ac084ce871b0b6171
Stefan Novak <stefan@novak.io>
2016-08-27 21:55:39 -0700

    jsbsim: add build from head
    Closes #4153.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- jsbsim.rb
+++ jsbsim.rb
@@ -4,6 +4,7 @@ class Jsbsim < Formula
   url "https://downloads.sourceforge.net/project/jsbsim/JSBSim_Source/JSBSim%20v1.0%20Release%20Candidate%202/JSBSim-1.0.rc2.tar.gz"
   version "1.0.rc2"
   sha256 "04accd4efc75867edfa6eeb814ceefebf519b2e8d750518b1de0a6aafa9442e1"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -12,12 +13,26 @@ class Jsbsim < Formula
     sha256 "992d34b5e4c61efdaeda03c3a493db32094e63c38909fb4cdf082fdcd0dfc8c0" => :mavericks
   end
 
+  head do
+    url "git://git.code.sf.net/p/jsbsim/code"
+
+    # Note: we are removing the compilation of Aeromatic, a web-based
+    # configuration file generator for JSBSim. Additionally, we are removing
+    # a call to "feenableexcept" which is not available in Clang. We also
+    # remove tests since there's a test that also relies on "feenableexcept".
+    patch :DATA
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--enable-libraries"
+    system "./autogen.sh", "--disable-debug",
+                           "--disable-dependency-tracking",
+                           "--disable-silent-rules",
+                           "--prefix=#{prefix}",
+                           "--enable-libraries"
 
     system "make", "install"
 
@@ -28,3 +43,37 @@ class Jsbsim < Formula
     system "#{bin}/JSBSim"
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 2f967c8..468bd34 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -37,8 +37,6 @@ if (CYTHON_FOUND)
+   find_package(PythonLibs)
+   if (PYTHONLIBS_FOUND)
+     include_directories(${CMAKE_CURRENT_LIST_DIR}/src)
+-    enable_testing()
+-    add_subdirectory(tests)
+   endif(PYTHONLIBS_FOUND)
+ endif(CYTHON_FOUND)
+
+diff --git a/src/JSBSim.cpp b/src/JSBSim.cpp
+index 65cd3ed..fab3310 100644
+--- a/src/JSBSim.cpp
++++ b/src/JSBSim.cpp
+@@ -288,7 +288,6 @@ int main(int argc, char* argv[])
+   _controlfp(_controlfp(0, 0) & ~(_EM_INVALID | _EM_ZERODIVIDE | _EM_OVERFLOW),
+            _MCW_EM);
+ #elif defined(__GNUC__) && !defined(sgi)
+-  feenableexcept(FE_DIVBYZERO | FE_INVALID);
+ #endif
+
+   try {
+diff --git a/utils/CMakeLists.txt b/utils/CMakeLists.txt
+index bd08efb..8b13789 100644
+--- a/utils/CMakeLists.txt
++++ b/utils/CMakeLists.txt
+@@ -1,2 +1 @@
+
+-add_subdirectory(aeromatic++)
