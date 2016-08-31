4cecd5324ac63bae5b9eb8650e3e52fa37312959
ilovezfs <ilovezfs@icloud.com>
2016-08-31 07:06:05 -0700

    methpipe: revision for gsl
    Closes #4195.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- methpipe.rb
+++ methpipe.rb
@@ -2,6 +2,8 @@ class Methpipe < Formula
   homepage "http://smithlabresearch.org/software/methpipe/"
   url "http://smithlabresearch.org/downloads/methpipe-3.4.2.tar.bz2"
   sha256 "9dab70723f71af815a058d38abc963dcb43b2e25968e12dbcf17413512ededf7"
+  revision 1
+
   head "https://github.com/smithlabcode/methpipe.git"
 
   bottle do
