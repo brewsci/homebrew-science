a300311fc27261222cddaa020c0de46f1f0226e6
ilovezfs <ilovezfs@icloud.com>
2016-08-31 03:45:29 -0700

    astrometry-net: vendor numpy and pyfits
    also run "make test"
    
    Closes #4174.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- astrometry-net.rb
+++ astrometry-net.rb
@@ -3,6 +3,7 @@ class AstrometryNet < Formula
   homepage "http://astrometry.net"
   url "http://astrometry.net/downloads/astrometry.net-0.67.tar.gz"
   sha256 "e351c81f7787550d42d45855db394a1702fd17c249ba934bdf4b6abf56281446"
+  revision 1
 
   head "https://github.com/dstndstn/astrometry.net.git"
 
@@ -19,23 +20,38 @@ class AstrometryNet < Formula
   depends_on "gsl"
   depends_on "cfitsio"
 
-  # from pip
-  depends_on "pyfits" => :python
-  depends_on "numpy" => :python
-
   # this formula includes python bindings
   depends_on :python => :recommended
 
+  resource "numpy" do
+    url "https://files.pythonhosted.org/packages/e0/4c/515d7c4ac424ff38cc919f7099bf293dd064ba9a600e1e3835b3edefdb18/numpy-1.11.1.tar.gz"
+    sha256 "dc4082c43979cc856a2bf352a8297ea109ccb3244d783ae067eb2ee5b0d577cd"
+  end
+
+  resource "pyfits" do
+    url "https://files.pythonhosted.org/packages/45/98/d6d25932e6a82fa8456d38ab307bfb8945a1e1dd4e896730555e3b61cfc5/pyfits-3.4.tar.gz"
+    sha256 "ce0319cf6ef40846c5915202e4c8bd8d293ad85af4b14aa5a60fb285b7538c4b"
+  end
+
   def install
-    ENV.j1
-    ENV["INSTALL_DIR"] = prefix.to_s
+    ENV["INSTALL_DIR"] = prefix
     ENV["NETPBM_INC"] = "-I#{Formula["netpbm"].opt_include}/netpbm"
     ENV["NETPBM_LIB"] = "-L#{Formula["netpbm"].opt_lib} -lnetpbm"
     ENV["SYSTEM_GSL"] = "yes"
 
+    if build.with? "python"
+      ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+      %w[numpy pyfits].each do |r|
+        resource(r).stage do
+          system "python", *Language::Python.setup_install_args(libexec)
+        end
+      end
+    end
+
     system "make"
+    system "make", "py" if build.with? "python"
     system "make", "extra" if build.with? "extras"
-    system "make", "py"    if build.with? "python"
+    system "make", "test"
     system "make", "install"
   end
 
