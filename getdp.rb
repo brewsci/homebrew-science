a2d1ba9f4d5faf0993831a178169c7a88eff8a53
ilovezfs <ilovezfs@icloud.com>
2016-08-31 06:41:33 -0700

    getdp: revision for gsl
    Closes #4188.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- getdp.rb
+++ getdp.rb
@@ -9,7 +9,7 @@ class Getdp < Formula
   homepage "http://www.geuz.org/getdp/"
   url "http://www.geuz.org/getdp/src/getdp-2.9.0-source.tgz"
   sha256 "08487f3f5a41012d06db0ec97206b883961c0e7853f47f8502f6d1ef80ef67c9"
-  revision 3
+  revision 4
 
   head "https://geuz.org/svn/getdp/trunk", :using => GetdpSvnStrategy
 
