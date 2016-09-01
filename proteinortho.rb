9221241dad02fa52eda8f83caabb5a8ac7547c1a
Torsten Seemann <torsten.seemann@gmail.com>
2016-09-01 21:27:16 +1000

    proteinortho 5.15
    Closes #4208.
    
    Signed-off-by: Torsten Seemann <torsten@seemann.id.au>

--- proteinortho.rb
+++ proteinortho.rb
@@ -1,7 +1,13 @@
 class Proteinortho < Formula
+  desc "Detection of orthologs in large-scale analysis"
   homepage "http://www.bioinf.uni-leipzig.de/Software/proteinortho/"
-  url "http://www.bioinf.uni-leipzig.de/Software/proteinortho/proteinortho_v5.11.tar.gz"
-  sha256 "25f3374854dfeefaf29d701e85d60013fc2b4a035257c4b0ef0d8ba304ac80cc"
+  # doi "10.1186/1471-2105-12-124"
+  # tag "bioinformatics"
+
+  url "http://www.bioinf.uni-leipzig.de/Software/proteinortho/proteinortho_v5.15_src.tar.gz"
+  version "5.15"
+  sha256 "718af74289a4fc0075f9dce2b12fe3fa1c7d96718c7ec8d9ddca94beac658a17"
+
   bottle do
     cellar :any
     sha256 "ecfd27b026b742c4b48d64e5b727b8f3dcb6207e905d27406a8d58205bfed3a6" => :yosemite
@@ -9,22 +15,19 @@ class Proteinortho < Formula
     sha256 "c0158b7c561ce1a175443ac084a261c71f5164d775a8eb12e180e140274c0740" => :x86_64_linux
   end
 
-  # tag "bioinformatics"
-  # doi "10.1186/1471-2105-12-124"
-
   depends_on "blast"
   depends_on "Thread::Queue" => :perl
   depends_on "File::Basename" => :perl
 
   def install
-    rm "proteinortho5_clean_edges"
-    rm "proteinortho5_clustering"
     system "make"
-    bin.install %w[proteinortho5.pl proteinortho5_clustering proteinortho5_clean_edges2.pl proteinortho5_singletons.pl ffadj_mcs.py]
+    mkdir bin
+    system "make", "INSTALLDIR=#{bin}", "install"
     doc.install "manual.html"
+    pkgshare.install "tools", "test"
   end
 
   test do
-    assert_match "orthology", shell_output("proteinortho5.pl 2>&1", 0)
+    assert_match "orthology", shell_output("#{bin}/proteinortho5.pl 2>&1", 0)
   end
 end
