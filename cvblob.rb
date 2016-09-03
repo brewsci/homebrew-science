d60b149782a168e881add9a6ea00aee489e9aeff
Nirbhay Kundan <nirbhay@redmart.com>
2016-09-03 13:17:55 +0800

    cvblob download url changed
    Closes #4218.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- cvblob.rb
+++ cvblob.rb
@@ -1,6 +1,6 @@
 class Cvblob < Formula
   homepage "https://code.google.com/p/cvblob/"
-  url "https://cvblob.googlecode.com/files/cvblob-0.10.4-src.tgz"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/cvblob/cvblob-0.10.4-src.tgz"
   sha256 "94eff3eed03370fac98e9d26a43405a3ac8eb3ca1621157c5738967b95c67bc1"
 
   depends_on "cmake" => :build
