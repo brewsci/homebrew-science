e40f4c1ab721bab4ab6959b027f605b89ab3abb0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-05 05:04:54 +0100

    spaced: add 20160804 bottle.

--- spaced.rb
+++ spaced.rb
@@ -1,6 +1,13 @@
 class Spaced < Formula
   desc "Spaced-Words for alignment-free sequence comparison"
   homepage "http://spaced.gobics.de/"
+  bottle do
+    cellar :any
+    sha256 "e788d0a55ed9021c4c64b5775b63bce95b6d2703a2a7491989c804bd86989b99" => :el_capitan
+    sha256 "e2a00ae89014cbbcb3c1e1ef1d7b0320646faa19be2413af70acc77c2f4d9074" => :yosemite
+    sha256 "807752981616621375d085b40475814ef58478127fb5717afe0ea7d99aa36b58" => :mavericks
+  end
+
   # doi "10.1186/s13015-015-0032-x"
   # tag "bioinformatics"
 
