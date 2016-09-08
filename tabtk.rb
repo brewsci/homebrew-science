f5eb0f7c7381e89b73d68d8a1106bc5e3b1270df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-09 00:42:13 +0100

    tabtk: add 0.1 bottle.

--- tabtk.rb
+++ tabtk.rb
@@ -4,6 +4,13 @@ class Tabtk < Formula
   url "https://github.com/lh3/tabtk/archive/v0.1.tar.gz"
   sha256 "311df21ef04b4d396a7552ce1384bf056e1d6f87a5679d55e905ec6c8591b906"
   head "https://github.com/lh3/tabtk.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bf8c44ad2b333a40490e785a0fa9b71fec66c47f104c92791bab96ed4a07b41b" => :el_capitan
+    sha256 "7e78e89f6582dc4ee90d35eacde0ed1e722dba7c01c3f4bca25eb9f4db59d37a" => :yosemite
+    sha256 "fda99507d62f92c5a5e10d1af97850631fc8ebb97ab53d265d7c81fbc535c78e" => :mavericks
+  end
+
   # tab "bioinformatics"
 
   def install
