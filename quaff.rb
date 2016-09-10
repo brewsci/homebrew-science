73f09f877cd6dae49fbf92308c231d7d1fb18669
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-10 01:39:17 +0100

    quaff: add 0.1.1 bottle.

--- quaff.rb
+++ quaff.rb
@@ -7,6 +7,12 @@ class Quaff < Formula
   head "https://github.com/ihh/quaff.git"
   # tag "bioinformatics"
 
+  bottle do
+    sha256 "3f413816fddd91a49b47f9a04e3afdc4556147c07dfcc24f622914b2b1f5b500" => :el_capitan
+    sha256 "98b3ed8fc748ad7ae9dea576831e4c3228635f35929dd9258f8c4c30c5eb9117" => :yosemite
+    sha256 "2a7f6e1dfa6dd01fc96e904a84e285bec9db8338a873e9412a46fe82f6c45bca" => :mavericks
+  end
+
   depends_on "zlib" unless OS.mac?
   depends_on "pkg-config" => :build
   depends_on "boost" => :recommended
