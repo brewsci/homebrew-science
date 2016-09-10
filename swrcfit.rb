aa483a82e805b760de6685a7b7bda515a8d1eb5e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-10 11:54:53 +0100

    swrcfit: update 3.0 bottle.

--- swrcfit.rb
+++ swrcfit.rb
@@ -8,10 +8,9 @@ class Swrcfit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "77219e3af8a12a5a6e8bda365f59149e1c40f1c8fca5fa390d8bf9361a48b580" => :el_capitan
-    sha256 "8876931c4ca1a5b7a372f2570719f9374cc9ac9b0eeac1851ee85a01620846a0" => :yosemite
-    sha256 "d49f6726358d66d28af8a4ac4addb15ae2e5a331e2dfb54112ca7083a4ee994e" => :mavericks
+    sha256 "cb876429cd9d5d10e0bd03b2559f41664410a3ff67fac19185300ea924934d4e" => :el_capitan
+    sha256 "74514de98ab2bb35f82a8c23e70330d4626a1fde469e9677e6b5b98975f2aabc" => :yosemite
+    sha256 "287c5fa1d803ee75401cdd657cc3f80e6c75330c3b656d11755b4bbf6245b9d6" => :mavericks
   end
 
   depends_on "octave"
