f3ceac6de9f618ee9a9fa073c3725204451295a1
ilovezfs <ilovezfs@icloud.com>
2016-09-12 07:34:24 -0700

    nexusformat 4.4.3
    remove upstreamed patch
    re-enable "make test"
    
    Closes #4294.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nexusformat.rb
+++ nexusformat.rb
@@ -1,9 +1,8 @@
 class Nexusformat < Formula
   desc "Common data format for neutron, x-ray, and muon science"
   homepage "http://www.nexusformat.org"
-  url "https://github.com/nexusformat/code/archive/v4.4.2.tar.gz"
-  sha256 "3cb2860c6040415dd0761ff4cfa062915f65df660c95f6f1fee044c86eddd8a2"
-  revision 2
+  url "https://github.com/nexusformat/code/archive/v4.4.3.tar.gz"
+  sha256 "e78a116feb2ebd04de31a8d8707c65e8e15a64aa8999a40fea305e3909bd6533"
 
   bottle do
     cellar :any
@@ -22,13 +21,6 @@ class Nexusformat < Formula
   depends_on "homebrew/versions/hdf4" => :recommended
   depends_on "doxygen" => :optional
 
-  # C++ API fails to build after selecting the C++11 or later standard.
-  # https://github.com/nexusformat/code/pull/435
-  patch do
-    url "https://github.com/nexusformat/code/pull/435.patch"
-    sha256 "bfc20b4d112a4ccb69dd8755dc78fead1e4c12c01d623c6ece5870ad29ab5f17"
-  end
-
   def install
     ENV.cxx11 if build.cxx11?
     cmake_args = std_cmake_args
@@ -38,9 +30,7 @@ class Nexusformat < Formula
     cmake_args << "-DENABLE_HDF4=TRUE" if build.with? "hdf4"
     system "cmake", ".", *cmake_args
     system "make"
-    # test failures have been reported upstream
-    # https://github.com/nexusformat/code/issues/426
-    # system "make", "test"
+    system "make", "test"
     system "make", "install"
   end
 
