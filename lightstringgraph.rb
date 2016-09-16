dc6f65e81efd6949d81c34379c370c0c4c948697
Jonathan Chang <jchang641@gmail.com>
2016-09-15 23:35:00 -0700

    lightstringgraph 0.4.0
    Closes #4311.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- lightstringgraph.rb
+++ lightstringgraph.rb
@@ -1,22 +1,23 @@
 class Lightstringgraph < Formula
+  desc "Lightweight string graph construction"
   homepage "http://lsg.algolab.eu/"
   # http://arxiv.org/pdf/1405.7520.pdf
+  url "https://github.com/AlgoLab/LightStringGraph/archive/v0.4.0.tar.gz"
+  sha256 "7a1530b147269b285875687fecaecbfd4d94c3db04d33ddeb1d0624547317b00"
   head "https://github.com/AlgoLab/LightStringGraph.git"
-  url "https://github.com/AlgoLab/LightStringGraph/archive/v0.1.0.tar.gz"
-  sha256 "79385a648b2fa34759cc031197c41a709539c655a204889d0e9dddacf0058a3d"
 
+  depends_on "boost"
   depends_on "beetl" => :recommended
 
   fails_with :clang do
-    build 503
+    build 703
     cause "error: variable length array of non-POD element type 'string'"
   end
 
   def install
-    system "make"
+    system "make", "all"
     bin.install Dir["bin/*"]
     doc.install %w[COPYING README.md]
-    prefix.install "script"
   end
 
   test do
