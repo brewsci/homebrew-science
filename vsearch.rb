d01b945344c8c3d7dd074e7fb8c2f124d5982a78
ilovezfs <ilovezfs@icloud.com>
2016-09-17 02:01:32 -0700

    vsearch 2.1.0
    Closes #4312.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- vsearch.rb
+++ vsearch.rb
@@ -1,8 +1,8 @@
 class Vsearch < Formula
   desc "USEARCH-compatible metagenomic sequence tool"
   homepage "https://github.com/torognes/vsearch"
-  url "https://github.com/torognes/vsearch/archive/v2.0.5.tar.gz"
-  sha256 "13726a66a907d679e4a2ccbc9e7c9ec143542947bd8de7193cdf9d2ed524572a"
+  url "https://github.com/torognes/vsearch/archive/v2.1.0.tar.gz"
+  sha256 "dff0af33d38cd9d72081b628f0e7bc9cfe1484ba2ef61ea471d6760dd9f767a4"
   head "https://github.com/torognes/vsearch.git"
   # doi "10.5281/zenodo.31443"
   # tag "bioinformatics"
