aba17cb4d289a3fa0bb98054056bd7bf807c4150
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 10:05:12 +0100

    vsearch: update 2.1.0 bottle.

--- vsearch.rb
+++ vsearch.rb
@@ -9,9 +9,9 @@ class Vsearch < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dca5d11787cea6d50852e5d9645f3142bf8dac58e0f2d22e56a592e9428321c8" => :el_capitan
-    sha256 "28d17c749ecdbd6f358fe9a033d620fd17eaece08df36d37d3615a464070b2b2" => :yosemite
-    sha256 "267808a5e1ac1a723c04bb8fa9326b9d15ff4746a9a2aab0f64826eb213f0ca7" => :mavericks
+    sha256 "8d33db0fcedcd4bfb73c9ad03682577f1d813f15184a3ace787590fb110df5bb" => :el_capitan
+    sha256 "c5876af1dc80251f87a9718842194bd9651c6ce77272479a02884ad63fb4134c" => :yosemite
+    sha256 "209be19232e5349378632da37049e2e315212230fc2a2351dab18bc342097903" => :mavericks
   end
 
   depends_on "autoconf" => :build
