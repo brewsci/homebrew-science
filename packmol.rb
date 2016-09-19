aa2a555e4d84433dfcd9d44187f09d178bd256ca
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-19 05:36:44 +0100

    packmol: add 16.261 bottle.

--- packmol.rb
+++ packmol.rb
@@ -4,6 +4,12 @@ class Packmol < Formula
   url "http://leandro.iqm.unicamp.br/packmol/versionhistory/packmol-16.261.tar.gz"
   sha256 "6ef74005da672743eddfb54b7c231da1b2dd0d6757fdb3ac047110bac9d884cf"
 
+  bottle do
+    sha256 "b25ccbf70892ede3b1a42a85c8c3d29eb5a90552a3cfdd1b3e09626f3a85ae72" => :sierra
+    sha256 "d9dd3d8b3774581542c169b387bcef844af1defdd2d072a1f77a2a5d25d23966" => :el_capitan
+    sha256 "3c56e147cd61f1897791b043ef5b090d73db299cc041cd7b4fe19587243ff686" => :yosemite
+  end
+
   depends_on :fortran
 
   resource "examples" do
