26c6c4d2e2013422f6ac1397749728b12ac95a36
ilovezfs <ilovezfs@icloud.com>
2016-09-21 03:03:36 -0700

    pilon 1.20
    Closes #4329.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- pilon.rb
+++ pilon.rb
@@ -1,8 +1,8 @@
 class Pilon < Formula
   desc "Improve draft assemblies and find variation"
   homepage "https://github.com/broadinstitute/pilon/wiki"
-  url "https://github.com/broadinstitute/pilon/releases/download/v1.18/pilon-1.18.jar"
-  sha256 "50589a96576101ccd68ba89e08e08fe7465cec8129757e3213b52b9ce88a0059"
+  url "https://github.com/broadinstitute/pilon/releases/download/v1.20/pilon-1.20.jar"
+  sha256 "9ae5d8a5a1a369e569da02e2f00ee0a6628c0539d7ac7fd376e94707bb2ef680"
   head "https://github.com/broadinstitute/pilon.git"
   # doi "10.1371/journal.pone.0112963"
   # tag "bioinformatics"
