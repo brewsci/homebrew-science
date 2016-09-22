6c99b36c48535bfd0e2550e008621a895d545478
ilovezfs <ilovezfs@icloud.com>
2016-09-22 03:28:32 -0700

    nextflow 0.22.0
    Closes #4332.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nextflow.rb
+++ nextflow.rb
@@ -1,17 +1,17 @@
 class Nextflow < Formula
   desc "Data-driven computational pipelines"
-  homepage "http://www.nextflow.io/"
+  homepage "https://www.nextflow.io/"
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
-  url "http://www.nextflow.io/releases/v0.21.3/nextflow"
-  version "0.21.3"
-  sha256 "f8cc0b166aa79cc01178f123da281fcd36cc8388c1866deec9ac577f1d2aaa9a"
+  url "https://www.nextflow.io/releases/v0.22.0/nextflow"
+  version "0.22.0"
+  sha256 "cd68328a57ca85da9c848142db8b8ebcb2de4082e171d294cc33b1760043c43b"
   head "https://github.com/nextflow-io/nextflow.git"
 
   bottle :unneeded
 
-  depends_on :java => "1.7+"
+  depends_on java: "1.7+"
 
   def install
     bin.install "nextflow"
