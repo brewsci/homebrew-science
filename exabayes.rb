8b76cad1e298c23d581f94d395ab4efa77d851cd
Jonathan Chang <jchang641@gmail.com>
2016-09-25 02:57:38 -0700

    exabayes 1.5
    * Don't build manual since it needs TeX
    * Tests run faster and use MPI
    * Audit fixes
    
    Closes #4348.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- exabayes.rb
+++ exabayes.rb
@@ -1,9 +1,9 @@
 class Exabayes < Formula
   desc "Large-scale Bayesian tree inference"
   homepage "http://sco.h-its.org/exelixis/web/software/exabayes/"
-  url "http://sco.h-its.org/exelixis/material/exabayes/1.4.1/exabayes-1.4.1-src.tar.gz"
-  sha256 "23e00b361a29365757e760b1acbb9d71744d2be5d9c450f8afdfaf5594d8994f"
-  revision 1
+  url "https://github.com/aberer/exabayes/archive/v1.5.tar.gz"
+  sha256 "3e5747d2ffb875ea929da666ee8bc8727aa3963bfa86207efe14218fd952c6b3"
+  head "https://github.com/aberer/exabayes.git", :branch => "devel"
 
   bottle do
     cellar :any
@@ -12,65 +12,59 @@ class Exabayes < Formula
     sha256 "76761cf6439fe52c10eaeaedde419f4a1bcbd887ebdf04aad265a22f5e38af4d" => :mavericks
   end
 
-  head do
-    url "https://github.com/aberer/exabayes.git", :branch => "devel"
-    depends_on "autoconf" => :build
-    depends_on "autoconf-archive" => :build
-    depends_on "automake" => :build
-  end
-
-  # Fix: ./src/comm/PendingSwap.hpp:50:8: error: no type named 'unique_ptr' in namespace 'std'
-  # ExaBayes needs std::unique_ptr, unordered_map, array
-  needs :cxx11
-
+  depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build
+  depends_on "automake" => :build
   depends_on :mpi => [:cc, :cxx, :recommended]
 
   def install
-    # Fix: ./src/comm/PendingSwap.hpp:50:8: error: no type named 'unique_ptr' in namespace 'std'
-    ENV.libcxx
     args = %W[--disable-dependency-tracking --disable-silent-rules --prefix=#{prefix}]
     args << "--enable-mpi" if build.with? "mpi"
-    system "autoreconf", "--install" if build.head?
+    system "autoreconf", "--install"
     system "./configure", *args
-    system "make", "install"
-    pkgshare.install "examples", "manual"
+    # Only build the binaries; `make` by itself will also
+    # build the manual which requires a full TeX installation.
+    progs = %w[yggdrasil parser-exabayes sdsf postProcParam credibleSet extractBips consense exabayes]
+    system "make", "date", *progs
+    bin.install *progs
+    pkgshare.install "examples"
   end
 
   def caveats; <<-EOS.undent
-    The manual and example files are stored in
+    The example files are stored in
       #{opt_prefix}/share/exabayes
     EOS
   end
 
   test do
-    cd testpath do
-      (testpath/"config.nex").write <<-EOS.undent
-        #nexus
-        begin run;
-          numgen 10000
-          numruns 2
-          numcoupledchains 2
-          heatfactor 0.01
-        end;
-      EOS
+    (testpath/"config.nex").write <<-EOS.undent
+      #nexus
+      begin run;
+        numgen 10000
+        numruns 2
+        numcoupledchains 2
+        convergencecriterion none
+      end;
+    EOS
 
-      (testpath/"aln.phy").write <<-EOS.undent
-         10 60
-        Cow       ATGGCATATCCCATACAACTAGGATTCCAAGATGCAACATCACCAATCATAGAAGAACTA
-        Carp      ATGGCACACCCAACGCAACTAGGTTTCAAGGACGCGGCCATACCCGTTATAGAGGAACTT
-        Chicken   ATGGCCAACCACTCCCAACTAGGCTTTCAAGACGCCTCATCCCCCATCATAGAAGAGCTC
-        Human     ATGGCACATGCAGCGCAAGTAGGTCTACAAGACGCTACTTCCCCTATCATAGAAGAGCTT
-        Loach     ATGGCACATCCCACACAATTAGGATTCCAAGACGCGGCCTCACCCGTAATAGAAGAACTT
-        Mouse     ATGGCCTACCCATTCCAACTTGGTCTACAAGACGCCACATCCCCTATTATAGAAGAGCTA
-        Rat       ATGGCTTACCCATTTCAACTTGGCTTACAAGACGCTACATCACCTATCATAGAAGAACTT
-        Seal      ATGGCATACCCCCTACAAATAGGCCTACAAGATGCAACCTCTCCCATTATAGAGGAGTTA
-        Whale     ATGGCATATCCATTCCAACTAGGTTTCCAAGATGCAGCATCACCCATCATAGAAGAGCTC
-        Frog      ATGGCACACCCATCACAATTAGGTTTTCAAGACGCAGCCTCTCCAATTATAGAAGAATTA
-      EOS
+    (testpath/"aln.phy").write <<-EOS.undent
+       10 60
+      Cow       ATGGCATATCCCATACAACTAGGATTCCAAGATGCAACATCACCAATCATAGAAGAACTA
+      Carp      ATGGCACACCCAACGCAACTAGGTTTCAAGGACGCGGCCATACCCGTTATAGAGGAACTT
+      Chicken   ATGGCCAACCACTCCCAACTAGGCTTTCAAGACGCCTCATCCCCCATCATAGAAGAGCTC
+      Human     ATGGCACATGCAGCGCAAGTAGGTCTACAAGACGCTACTTCCCCTATCATAGAAGAGCTT
+      Loach     ATGGCACATCCCACACAATTAGGATTCCAAGACGCGGCCTCACCCGTAATAGAAGAACTT
+      Mouse     ATGGCCTACCCATTCCAACTTGGTCTACAAGACGCCACATCCCCTATTATAGAAGAGCTA
+      Rat       ATGGCTTACCCATTTCAACTTGGCTTACAAGACGCTACATCACCTATCATAGAAGAACTT
+      Seal      ATGGCATACCCCCTACAAATAGGCCTACAAGATGCAACCTCTCCCATTATAGAGGAGTTA
+      Whale     ATGGCATATCCATTCCAACTAGGTTTCCAAGATGCAGCATCACCCATCATAGAAGAGCTC
+      Frog      ATGGCACACCCATCACAATTAGGTTTTCAAGACGCAGCCTCTCCAATTATAGAAGAATTA
+    EOS
 
-      system *%W[#{bin}/yggdrasil -f aln.phy -m DNA -n test -s 100 -c config.nex -T 2]
-      system *%W[#{bin}/sdsf -f ExaBayes_topologies.test.0 ExaBayes_topologies.test.1]
-      system *%W[#{bin}/consense -n cons -f ExaBayes_topologies.test.0 ExaBayes_topologies.test.1]
-    end
+    args = build.with?("mpi") ? %W[mpirun -np 2 #{bin}/exabayes] : %W["#{bin}/yggdrasil -T 2]
+    args += %W[-f aln.phy -m DNA -n test -s 100 -c config.nex]
+    system *args
+    system "#{bin}/sdsf", "-f", "ExaBayes_topologies.run-0.test", "ExaBayes_topologies.run-1.test"
+    system "#{bin}/consense", "-n", "cons", "-f", "ExaBayes_topologies.run-0.test", "ExaBayes_topologies.run-1.test"
   end
 end
