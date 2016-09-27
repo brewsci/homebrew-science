56af93895624deaec92014994d5c0fb9511ac6c5
Jon Palmer <nextgen.usfs@gmail.com>
2016-09-27 14:22:17 -0500

    repeatscout: update url
    Fixes #4356.
    Closes #4357.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- repeatscout.rb
+++ repeatscout.rb
@@ -3,7 +3,7 @@ class Repeatscout < Formula
   # doi "10.1093/bioinformatics/bti1018"
   # tag "bioinformatics"
 
-  url "http://repeatscout.bioprojects.org/RepeatScout-1.0.5.tar.gz"
+  url "http://bix.ucsd.edu/repeatscout/RepeatScout-1.0.5.tar.gz"
   sha256 "bda6f782382f2b7dcb6a004b7da586d5046b3c12429b158e24787be62de6199c"
 
   bottle do
