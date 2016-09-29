552c8f41afff54bb47d303de960ab2b23cab3d11
ilovezfs <ilovezfs@icloud.com>
2016-09-29 05:20:20 -0700

    qcl: revision for readline
    Closes #4373.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- qcl.rb
+++ qcl.rb
@@ -2,6 +2,7 @@ class Qcl < Formula
   homepage "http://tph.tuwien.ac.at/~oemer/qcl.html"
   url "http://tph.tuwien.ac.at/~oemer/tgz/qcl-0.6.4.tgz"
   sha256 "ff4d4899c5995bd75601e8125312ce20059f065326c29467b7f5dfa3a87d45f4"
+  revision 1
 
   depends_on "flex"
   depends_on "readline"
