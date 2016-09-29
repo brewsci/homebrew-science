2abd865e071644afaf66d4b01936cc139a922725
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 20:46:46 +0100

    fermi-lite: add 0.1 bottle.

--- fermi-lite.rb
+++ fermi-lite.rb
@@ -1,6 +1,13 @@
 class FermiLite < Formula
   desc "Assembling Illumina short reads in small regions"
   homepage "https://github.com/lh3/fermi-lite"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f0534f7ce0bb08aecef23880a4a3483a3ec816534ce3366ed979dda8b912a761" => :sierra
+    sha256 "49038f2b53b55d0ca1fe6b6685517f23140e805ab7542be99302d46554bdabe7" => :el_capitan
+    sha256 "1e7b55c67e73f4bea13226b0af2e188cdc87a6b9670e9ec1bc4ffcedba1a6e7b" => :yosemite
+  end
+
   # doi "10.1093/bioinformatics/bts280"
   # tag "bioinformatics"
 
