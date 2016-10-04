1d2a388f4143b73ad274de61ac3f881f243a9e63
ilovezfs <ilovezfs@icloud.com>
2016-10-04 00:59:48 -0700

    nextflow 0.22.2
    Closes #4398.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nextflow.rb
+++ nextflow.rb
@@ -4,9 +4,9 @@ class Nextflow < Formula
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
-  url "https://www.nextflow.io/releases/v0.22.1/nextflow"
-  version "0.22.1"
-  sha256 "78798f05be57d64c84287d8a817fc489b93132535580620e25adc86271bc239e"
+  url "https://www.nextflow.io/releases/v0.22.2/nextflow"
+  version "0.22.2"
+  sha256 "566ff2e19c049e78132a565c7109305ab9061c6605ba3efb98e3cb92f19c905f"
   head "https://github.com/nextflow-io/nextflow.git"
 
   bottle :unneeded
