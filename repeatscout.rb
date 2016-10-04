e355d50f53fae9790ba8eec94eaef64bd6d9ce8a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-04 15:09:18 +0000

    repeatscout: update 1.0.5 bottle for Linuxbrew.

--- repeatscout.rb
+++ repeatscout.rb
@@ -12,6 +12,7 @@ class Repeatscout < Formula
     sha256 "b03af9a3be3be79b556401d5270a8216c06aa54cd5dec861068ab6e2acd06454" => :sierra
     sha256 "46e0c47129bc7449c083fd6588e2b0db6caa4f4e179f33fe42ffb758a9638543" => :el_capitan
     sha256 "bd9704196baabb1e7abee5c227a209e49f5a155fe47b5395a2db77e63f78708c" => :yosemite
+    sha256 "e5818bb9b5762295c60dc5411ff3f97e749d9a0f3229e9f079d7d0b1c2e6c671" => :x86_64_linux
   end
 
   depends_on "trf" => :optional
