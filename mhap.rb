37f15fe09d84efcf802aad5f6433dea13ee9eabc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-05 07:01:51 +0000

    mhap: update 2.1.1 bottle for Linuxbrew.

--- mhap.rb
+++ mhap.rb
@@ -14,6 +14,7 @@ class Mhap < Formula
     sha256 "9adbb7bd7ecb554569963c18b240943d7ac688e33a134abbd04928194f003bd2" => :sierra
     sha256 "a646dc8d072bc89d60771f7a38d55c599aea25f4a45c057cf4774e99f678d365" => :el_capitan
     sha256 "165943433f514997de36880f545b9d3d4b1ed0f196c8f4fd9e5bb10de4b9dc8c" => :yosemite
+    sha256 "c1cdeee8009b8d12e7e841873fb4b433fb45bed634aa3091f7c444d2c8a346cf" => :x86_64_linux
   end
 
   def install
