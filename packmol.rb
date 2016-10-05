25f95c7674ee0fc1f75a99464a1d8ac7031f12b1
Shaun Jackman <sjackman@gmail.com>
2016-10-05 13:41:22 -0700

    packmol: update 16.261 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#284.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- packmol.rb
+++ packmol.rb
@@ -8,6 +8,7 @@ class Packmol < Formula
     sha256 "b25ccbf70892ede3b1a42a85c8c3d29eb5a90552a3cfdd1b3e09626f3a85ae72" => :sierra
     sha256 "d9dd3d8b3774581542c169b387bcef844af1defdd2d072a1f77a2a5d25d23966" => :el_capitan
     sha256 "3c56e147cd61f1897791b043ef5b090d73db299cc041cd7b4fe19587243ff686" => :yosemite
+    sha256 "4f8db81edbf0a52fe134ddf3d5810bf98858a4c061c3430e4b5bf6d51ef2edb2" => :x86_64_linux
   end
 
   depends_on :fortran
