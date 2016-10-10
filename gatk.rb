0e92b9b3df4c8230e1b6ab762114435837613a45
Shaun Jackman <sjackman@gmail.com>
2016-10-10 10:40:15 -0700

    gatk 3.6 (new formula)
    Genome Analysis Toolkit
    Variant Discovery in High-Throughput Sequencing Data
    
    Closes #4426.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ gatk.rb
@@ -0,0 +1,37 @@
+class Gatk < Formula
+  desc "Genome Analysis Toolkit: Variant Discovery in High-Throughput Sequencing"
+  homepage "https://software.broadinstitute.org/gatk/"
+  url "https://github.com/broadgsa/gatk-protected/archive/3.6.tar.gz"
+  sha256 "51ae87abfad304c0f5d9e9dd52fdd41fed29635ad339d11e991d0e2ffeccb549"
+  head "https://github.com/broadgsa/gatk-protected.git"
+  # doi "10.1101/gr.107524.110"
+  # tag "bioinformatics"
+
+  bottle :disable, "See https://github.com/Homebrew/homebrew-science/pull/4428"
+
+  depends_on :java
+  depends_on "maven" => :build
+
+  def install
+    # Fix error on Circle CI.
+    # Error: Could not find or load main class org.codehaus.plexus.classworlds.launcher.Launcher
+    ENV.delete "M2_HOME"
+
+    system "mvn", "package", "-Dmaven.repo.local=${PWD}/repo"
+    java = share/"java"
+    mkdir_p java
+    cp "target/GenomeAnalysisTK.jar", java
+    bin.write_jar_script java/"GenomeAnalysisTK.jar", "gatk"
+    prefix.install_metafiles
+  end
+
+  def caveats; <<-EOS.undent
+    GATK Official Release Repository: contains the core MIT-licensed GATK
+    framework, plus "protected" tools restricted to non-commercial use only
+    EOS
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/gatk --help 2>&1", 0)
+  end
+end
