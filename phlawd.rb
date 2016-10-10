1e174170012ff159c921f4941c9e9ec1331b186b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-10 22:34:06 +0000

    phlawd: update 3.4a_2 bottle for Linuxbrew.

--- phlawd.rb
+++ phlawd.rb
@@ -18,6 +18,7 @@ class Phlawd < Formula
     sha256 "eab194f5f8a40fffc3801a179d8b794c28ff29232cd2a666a05fe34913c2cdbe" => :sierra
     sha256 "98172e8ee0bce6476d8c39e8bda5bfe6bc9ac582a41c5be2507d3f8a527bc5c4" => :el_capitan
     sha256 "147f7deae6e1c6c0f3697ed7f3c893b338bc147fb0fc2a4f28054806982b5fef" => :yosemite
+    sha256 "c51b6f732f0acb671eecdf4604fb4603fa4e5f40dad96e77d6e0f47b5475d872" => :x86_64_linux
   end
 
   fails_with :llvm do
