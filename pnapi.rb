ef8dfa7a6b849a2323ed89529bb3075a7353a2cc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-15 00:38:24 +0100

    pnapi: add 4.03 bottle.

--- pnapi.rb
+++ pnapi.rb
@@ -4,6 +4,12 @@ class Pnapi < Formula
   url "http://download.gna.org/service-tech/pnapi/pnapi-4.03.tar.gz"
   sha256 "2235be39b1c0615ded0197e57661a5127337681f034a8d29bed833f376c8a836"
 
+  bottle do
+    sha256 "96f1232332d350e2a5fd55bcf040ba88b500b6bdb13cfa322ec37d43e4e38732" => :sierra
+    sha256 "6cc3c17d3f75c12c7785b67be26e0ae9f7c3d1a26a17455ee7bc56af0e21c53d" => :el_capitan
+    sha256 "bdf20541df9f3af23277e207715654c1905de28b44e63771de10741e57312511" => :yosemite
+  end
+
   head do
     url "http://svn.gna.org/svn/service-tech/trunk/pnapi"
     depends_on "libtool" => :build
