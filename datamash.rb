b0557702c26561ad54a2c19a9f17abe21ec8a2fa
Shaun Jackman <sjackman@gmail.com>
2016-10-14 15:06:23 -0700

    datamash: update 1.1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#295.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- datamash.rb
+++ datamash.rb
@@ -9,6 +9,7 @@ class Datamash < Formula
     sha256 "f822b6691ac95a86f46a0fcc4c0d283d69b6915cbef26b580df5ef5850642670" => :el_capitan
     sha256 "13ab517271aa7c33e37b70f69121f76d59a5a40cf1e4696d590ef7fd4924869d" => :yosemite
     sha256 "d019e9ca0090d450f98fdc5c4c1c0a114543392dceb155b15620ba903757662e" => :mavericks
+    sha256 "dddd2d241850472545900c869e081c31b7d8a8d6a7e9095035bc4e0f91bc852b" => :x86_64_linux
   end
 
   head do
