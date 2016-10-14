914a456fcf42c2bd41f9fe75484760cd8616a99e
Dominique Orban <dominique.orban@gmail.com>
2016-10-14 19:41:23 -0400

    sara: fix build, audits
    Closes #2553
    
    Closes #4468.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- sara.rb
+++ sara.rb
@@ -1,18 +1,21 @@
 class Sara < Formula
+  desc "Reachability analysis in petri nets"
   homepage "http://service-technology.org/sara"
   url "http://download.gna.org/service-tech/sara/sara-1.13.tar.gz"
   sha256 "2e1a616b6398bfc898b6a53aab16ef556ad59263b3ab71620592f83c43cd28ca"
 
   head do
     url "http://svn.gna.org/svn/service-tech/trunk/sara"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
     depends_on "gengetopt" => :build
     depends_on "help2man" => :build
     depends_on "libtool" => :build
   end
 
-  option "without-check", "skip build-time checks (not recommended)"
+  option "with-test", "perform build-time checks"
+  deprecated_option "without-check" => "without-test"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
     ENV.deparallelize
