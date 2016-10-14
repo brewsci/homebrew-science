8040c47bb0908cdd8c6e70049ec35612b6ec98a9
Dominique Orban <dominique.orban@gmail.com>
2016-10-14 17:22:25 -0400

    elph: still fail with clang
    - audit fixes
    
    Closes #2543
    
    Closes #4464.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- elph.rb
+++ elph.rb
@@ -1,10 +1,10 @@
 class Elph < Formula
+  desc "Gibbs sampler for finding motifs in DNA or protein sequences"
   homepage "http://cbcb.umd.edu/software/ELPH/"
   url "ftp://ftp.cbcb.umd.edu/pub/software/elph/ELPH-1.0.1.tar.gz"
   sha256 "6d944401d2457d75815a34dbb5780f05df569eb1edfd00909b33c4c4c4ff40b9"
 
   fails_with :clang do
-    build 600
     cause "error: '-I-' not supported, please use -iquote instead"
   end
 
