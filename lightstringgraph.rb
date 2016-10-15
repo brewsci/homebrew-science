5de9585f495e7b0d0e68d70b098cc0d535f77b7c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-15 01:58:53 +0100

    lightstringgraph: add 0.4.0_1 bottle.

--- lightstringgraph.rb
+++ lightstringgraph.rb
@@ -1,6 +1,13 @@
 class Lightstringgraph < Formula
   desc "Lightweight string graph construction"
   homepage "http://lsg.algolab.eu/"
+  bottle do
+    cellar :any
+    sha256 "75fc42a4825b1ab2e49cdc7ac2b811ce703b4e563e4d12e2f42492913b22a607" => :sierra
+    sha256 "3a8f5158f3f9e5ff44dddd3a13ac331d0b2c8824a960a6700ba882104963e1e0" => :el_capitan
+    sha256 "1478fddf3f079339169d24759b9331af18837073b5b3dbafd2b86912df69b1ed" => :yosemite
+  end
+
   # http://arxiv.org/pdf/1405.7520.pdf
   url "https://github.com/AlgoLab/LightStringGraph/archive/v0.4.0.tar.gz"
   sha256 "7a1530b147269b285875687fecaecbfd4d94c3db04d33ddeb1d0624547317b00"
