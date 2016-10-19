62d731b47b9677591c2a317471668f115accbab6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-19 14:03:15 +0100

    silo: add 4.10.2 bottle.

--- silo.rb
+++ silo.rb
@@ -4,10 +4,15 @@ class Silo < Formula
   url "https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2-bsd.tar.gz"
   sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
 
+  bottle do
+    sha256 "121f4802d1e18e049f50360b9d6222867028320cea21a33ead806db3d55e1a3d" => :sierra
+    sha256 "cc3fd7ae2ebf5f32a9d0d49f037405dc01e51e094a5c26e20c46a5dc9f811f80" => :el_capitan
+    sha256 "61153eafb69d477e1270b07a951f402192d38c13759cd064801ad0d7ccd60e02" => :yosemite
+  end
+
   depends_on :x11 => :optional
   depends_on :fortran
   depends_on "readline"
-
 
   def install
     args = ["--prefix=#{prefix}"]
