896cdff0c8fec6df34d1eabde9fd99e2f566750c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-19 12:32:05 +0100

    cfitsio: update 3.390 bottle.

--- cfitsio.rb
+++ cfitsio.rb
@@ -8,6 +8,7 @@ class Cfitsio < Formula
 
   bottle do
     cellar :any
+    sha256 "df961ff99786357430032fae6b7e0ae871364dd60edc05ac548f6361ddd60904" => :sierra
     sha256 "29e485f25a51ce580f6547bcd0d05cc517189e8a01881ac21c5e27781ed8230d" => :el_capitan
     sha256 "3d3de057f3f3ae3ff4111a77193871a18ba6b9b1d71a7a68882937f2dccd216a" => :yosemite
     sha256 "b21eee517163d977dca6c7bd1f2de3f4c16d8f14a91b72899df19404887b36e4" => :mavericks
