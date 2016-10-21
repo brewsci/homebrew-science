ac5df3fdb7f74293729240985c7dc0a6622cb5d5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-21 18:03:00 +0100

    sigrok-cli: add 0.6.0 bottle.

--- sigrok-cli.rb
+++ sigrok-cli.rb
@@ -3,6 +3,13 @@ class SigrokCli < Formula
   url "http://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.6.0.tar.gz"
   sha256 "ab2ede4e245f3987e19a89a530bd204e0d792c07474d9fed3345d4af4e84723c"
 
+  bottle do
+    cellar :any
+    sha256 "5d8e6a75e4b59ddbff563cfb679fa086b59285e4dce385c5b71e362b1ce8f8b4" => :sierra
+    sha256 "25dd3a07147e5ad984236cd6823182ee1ad6c32851eed2b08732ed24d042946e" => :el_capitan
+    sha256 "79d672aba4fa67b2ff2c68a1650ff6f258c88c9b738b20417157d96c39f31e99" => :yosemite
+  end
+
   head do
     url "git://sigrok.org/sigrok-cli", :shallow => false
     depends_on "glib"
