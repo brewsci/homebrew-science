62e02610dce074c713c7b5dc073233a76306b70c
Michka Popoff <michkapopoff@gmail.com>
2016-10-22 22:35:54 +0200

    simpleitk 0.10.0
    - Bumped version to 0.10.0
    - Updated description to comply with brew audit
    - Updated position of options to comply with brew audit
    - Deprecated examples option to comply with brew audit
    - Added option for python 3 support (inspired from the gdcm and vtk formulae)
    
    Closes #4511.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- simpleitk.rb
+++ simpleitk.rb
@@ -1,8 +1,8 @@
 class Simpleitk < Formula
-  desc "SimpleITK is a simplified layer built on top of ITK"
+  desc "Simplified layer built on top of ITK"
   homepage "http://www.simpleitk.org"
-  url "https://downloads.sourceforge.net/project/simpleitk/SimpleITK/0.9.0/Source/SimpleITK-0.9.0.tar.gz"
-  sha256 "111454070e62f93f7b241604f8ba41488032a80d09a5a0e8a1803dfdeaa26bc7"
+  url "https://downloads.sourceforge.net/project/simpleitk/SimpleITK/0.10.0/Source/SimpleITK-0.10.0-1.tar.gz"
+  sha256 "e849d693d67622b0dcb1153124e38ece9256b7c4760e35fa3af3cd90b851662f"
   head "https://github.com/SimpleITK/SimpleITK.git"
 
   bottle do
@@ -12,50 +12,66 @@ class Simpleitk < Formula
     sha256 "b09618f1801915dd6cdba0c25af6685c5ddbb7dc21c8ad11e178c4e9600ae5f8" => :mavericks
   end
 
+  option "with-examples", "Compile and install various examples"
+  option "with-csharp", "Compile for C sharp"
+  option "with-ruby", "Compile for Ruby"
+  option "with-tcl", "Compile for Tcl"
+
+  deprecated_option "examples" => "with-examples"
+
   depends_on "cmake" => :build
   depends_on "swig" => :build
-  depends_on :python
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
   depends_on :java => :optional
   depends_on "r" => :optional
   depends_on "lua" => :optional
 
-  option "examples", "Compile and install various examples"
-  option "with-csharp", "Compile for C sharp"
-  option "with-java", "Compile for Java"
-  option "with-ruby", "Compile for Ruby"
-  option "with-tcl", "Compile for Tcl"
-
   def install
-    args = std_cmake_args + %W[
+    ENV.delete("PYTHONPATH")
+
+    args = std_cmake_args + %w[
       -DBUILD_TESTING=OFF
       -DUSE_SYSTEM_SWIG=ON
     ]
     args << "-DBUILD_EXAMPLES=" + ((build.include? "examples") ? "ON" : "OFF")
+    args << "-DWRAP_PYTHON=ON"
     args << "-DWRAP_CSHARP=" + ((build.with? "csharp") ? "ON" : "OFF")
     args << "-DWRAP_JAVA=" + ((build.with? "java") ? "ON" : "OFF")
     args << "-DWRAP_LUA=" + ((build.with? "lua") ? "ON" : "OFF")
-    args << "-DWRAP_PYTHON=ON"
     args << "-DWRAP_R=" + ((build.with? "r") ? "ON" : "OFF")
     args << "-DWRAP_RUBY=" + ((build.with? "ruby") ? "ON" : "OFF")
     args << "-DWRAP_TCL=" + ((build.with? "tcl") ? "ON" : "OFF")
 
-    # CMake picks up the system's python dylib, even if we have a brewed one.
-    args << "-DPYTHON_LIBRARY='#{`python-config --prefix`.chomp}/lib/libpython2.7.dylib'"
-    args << "-DPYTHON_INCLUDE_DIR='#{`python-config --prefix`.chomp}/include/python2.7'"
-
     # Superbuild does only work in an out-of-source build, create a new folder
     mkdir "sitk-build" do
+      python_executable = `which python`.strip
+      python_executable = `which python3`.strip if build.with? "python3"
+
+      python_prefix = `#{python_executable} -c 'import sys;print(sys.prefix)'`.chomp
+      python_include = `#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'`.chomp
+      python_version = "python" + `#{python_executable} -c 'import sys;print(sys.version[:3])'`.chomp
+
+      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      # CMake picks up the system's python dylib, even if we have a brewed one.
+      if File.exist? "#{python_prefix}/Python"
+        args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+        args << "-DPYTHON_LIBRARY='#{python_prefix}/lib/lib#{python_version}.a'"
+      else
+        args << "-DPYTHON_LIBRARY='#{python_prefix}/lib/lib#{python_version}.dylib'"
+      end
+
+      py_site_packages = "#{lib}/#{python_version}/site-packages"
+      ENV.prepend_create_path "PYTHONPATH", py_site_packages
+
       # The cmake Superbuild will take care of downloading and compiling important
       # dependencies to be able to build simpleitk
       system "cmake", "../SuperBuild/", *args
       system "make"
 
-      ENV.prepend_create_path "PYTHONPATH", lib+"python2.7/site-packages"
-      if build.head?
-        system "python", "SimpleITK-build/Wrapping/Python/Packaging/setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
-      else
-        system "python", "SimpleITK-build/Wrapping/PythonPackage/setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
-      end
+      system python_executable, "SimpleITK-build/Wrapping/Python/Packaging/setupegg.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
     end
   end
 
