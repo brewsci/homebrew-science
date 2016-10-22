610f3d1a1a1103f37e8740215b7a45488dc2eaa5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-22 06:09:42 +0000

    bam-readcount: update 0.8.0 bottle for Linuxbrew.

--- bam-readcount.rb
+++ bam-readcount.rb
@@ -12,6 +12,7 @@ class BamReadcount < Formula
     sha256 "17c0f2260c9fe12b1bfc6bf932ed6c103988e547ea68f1df0c972df73dfb0988" => :sierra
     sha256 "62c7ac502fdece44ec104177a97b37aa46e5244a82d3084e1e58f7f3bc528dba" => :el_capitan
     sha256 "6e7c930c6275a04fc69791ae70f3de147face2068cb94a4b18070a727eedc90a" => :yosemite
+    sha256 "ea78015b0c0c0b458b72dcc474e53b18afaca3387e23118f32238d1297f87473" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
