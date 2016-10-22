7bb4203cd55249fe33ef2f9a376db18cf8db6f6b
ilovezfs <ilovezfs@icloud.com>
2016-10-21 21:54:14 -0700

    delly 0.7.6
    Closes #4507.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- delly.rb
+++ delly.rb
@@ -1,9 +1,8 @@
 class Delly < Formula
   desc "Structural variant discovery by paired-end and split-read analysis"
   homepage "https://github.com/tobiasrausch/delly"
-  url "https://github.com/tobiasrausch/delly/archive/v0.7.5.tar.gz"
-  sha256 "14daf744e7179b2781cbb88b26f674d1221c0093fec6d33c13a6d2863577598a"
-  revision 1
+  url "https://github.com/tobiasrausch/delly/archive/v0.7.6.tar.gz"
+  sha256 "fab273f17dacde4e15e18a187bfe47826a1b8396a8aa40b44a68c56f059f79b5"
   head "https://github.com/tobiasrausch/delly.git"
   # doi "10.1093/bioinformatics/bts378"
   # tag "bioinformatics"
