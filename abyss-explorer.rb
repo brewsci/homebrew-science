54c1180e7620e6f5c1debc989b7e59799bf863e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-23 19:40:37 +0100

    abyss-explorer: update 1.3.4_1 bottle.

--- abyss-explorer.rb
+++ abyss-explorer.rb
@@ -10,10 +10,10 @@ class AbyssExplorer < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "fea28ec5a740dbe3e30bef31ef261626ab37b6c62c7417cb84649547c00ef8bd" => :sierra
     sha256 "8399757c2f8ba384cca623a2eb93ccdd727d6fc564c5a44f4a0a1815a9009b66" => :el_capitan
     sha256 "1e9a7715de74ed13051083c072a2fe0f5134c86c2ad2f9446c3bc0b245a305eb" => :yosemite
     sha256 "420ff6af90cb029ec07869c8f3871520de35109bafefead83477595e2073acb4" => :mavericks
-    sha256 "3257b5db71026d508bec3504b61434d92264600cd9cbcb712a4fe5ff8c4f30cb" => :x86_64_linux
   end
 
   def install
