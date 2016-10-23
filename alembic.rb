5e833e109dcfdb7882d10170cd8faf3c61c4a777
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-23 19:48:12 +0100

    alembic: update 1.6.1 bottle.

--- alembic.rb
+++ alembic.rb
@@ -6,6 +6,7 @@ class Alembic < Formula
   head "https://github.com/alembic/alembic.git"
 
   bottle do
+    sha256 "50872f6593ec3c86dd1b254d5350fee29464d214498e3c292a17594ad473e719" => :sierra
     sha256 "0dd08fe3dc38a92833c88ba787dbe862fd76716c0735935cedc10f1276d8a9a2" => :el_capitan
     sha256 "a9fe69855cb72824509ecceb5e38df9e96c5b3a272018766584067c3f64498af" => :yosemite
     sha256 "a6f98148542b541a2de9d9dc9314fec59167af79d1a05d180e7505055d23e7fd" => :mavericks
