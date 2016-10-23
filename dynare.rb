b6c90637497169a9edc329fa85639f85b744f8e7
ilovezfs <ilovezfs@icloud.com>
2016-10-23 00:53:38 -0700

    dynare: revision for gsl
    Closes #4184.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- dynare.rb
+++ dynare.rb
@@ -1,54 +1,75 @@
 class Dynare < Formula
+  desc "Platform for economic models, particularly DSGE and OLG models"
   homepage "http://www.dynare.org"
-  revision 1
-
-  option "with-matlab=", "Path to Matlab root directory (to build mex files)"
-  option "with-matlab-version=", "Matlab version, e.g., 8.2 (to build mex files)"
-  option "with-doc", "Build documentation"
-  option "without-check", "Disable build-time checks (not recommended)"
+  revision 2
 
   stable do
     url "https://www.dynare.org/release/source/dynare-4.4.3.tar.xz"
     sha256 "d00f97f72bcad7486ec1d18640d44b607d91ff9f585c9b4e01d08b6a3deeae21"
+
     depends_on "matlab2tikz"
   end
 
   head do
     url "https://github.com/DynareTeam/dynare.git"
+
     depends_on "automake" => :build
-    depends_on "flex"     => :build
-    depends_on "bison"    => :build
+    depends_on "bison" => :build
+    depends_on "flex" => :build
   end
 
-  depends_on :tex    => :build
+  option "with-matlab=", "Path to Matlab root directory (to build mex files)"
+  option "with-matlab-version=", "Matlab version, e.g., 8.2 (to build mex files)"
+  option "with-tex", "Build documentation"
+
+  deprecated_option "with-doc" => "with-tex"
+
   depends_on "boost" => :build
-  depends_on "xz"    => :build
+  depends_on "cweb" => :build
+  depends_on "xz" => :build
   depends_on "fftw"
-  depends_on "gsl"
-  depends_on "libmatio"
   depends_on :fortran
-
-  depends_on "doxygen" => :build if build.with? "doc"
-
-  depends_on "slicot" => ["with-default-integer-8"] if build.with? "matlab="
+  depends_on "gsl"
+  depends_on "hdf5"
+  depends_on "libmatio"
+  depends_on "metis"
+  depends_on "readline"
+  depends_on "veclibfort"
   depends_on "octave" => :recommended
+  depends_on "slicot" => "with-default-integer-8" if build.with? "matlab="
+  depends_on "suite-sparse"
+  depends_on :tex => [:build, :optional]
+
+  if build.with? "tex"
+    depends_on "doxygen" => :build
+    depends_on "latex2html" => :build
+    depends_on "texi2html" => :build
+    depends_on "texinfo" => :build
+  end
+
+  needs :cxx11
 
   def install
+    ENV.cxx11
+
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
     ]
+
     matlab_path = ARGV.value("with-matlab") || ""
     matlab_version = ARGV.value("with-matlab-version") || ""
-    if (matlab_path.empty? || matlab_version.empty?) && build.without?("octave")
-      raise "You must build Dynare with Matlab and/or Octave support"
-    end
+    no_matlab = matlab_path.empty? || matlab_version.empty?
+    want_matlab = !matlab_path.empty? || !matlab_version.empty?
 
-    if matlab_path.empty? || matlab_version.empty?
-      unless matlab_path.empty? && matlab_version.empty?
+    if no_matlab
+      if want_matlab
         opoo "Matlab support disabled: specify both Matlab path and version"
+      end
+      if build.without? "octave"
+        odie "You must build Dynare with Matlab and/or Octave support"
       end
       args << "--disable-matlab"
     else
@@ -58,47 +79,42 @@ class Dynare < Formula
 
     args << "--disable-octave" if build.without? "octave"
 
-    system "autoreconf", "-si" if build.head?
+    if build.head?
+      # Work around "Input line too long. (l. 104)"
+      inreplace "dynare++/kord/journal.cweb",
+        "#if !defined(__MINGW32__) && !defined(__CYGWIN32__) && !defined(__CYGWIN__) && !defined(__MINGW64__) && !defined(__CYGWIN64__)",
+        "#if 1"
+
+      inreplace "m4/ax_mexopts.m4",
+        /MACOSX_DEPLOYMENT_TARGET='.*'/,
+        "MACOSX_DEPLOYMENT_TARGET='#{MacOS.version}'"
+
+      system "autoreconf", "-fvi"
+    elsif build.stable?
+      inreplace "mex/build/matlab/configure",
+        /MACOSX_DEPLOYMENT_TARGET='.*'/,
+        "MACOSX_DEPLOYMENT_TARGET='#{MacOS.version}'"
+    end
+
     system "./configure", *args
 
-    if build.with?("doc") && OS.mac?
-      inreplace "doc/Makefile", \
-                "$(TEXI2PDF) $(AM_V_texinfo) --build-dir=$(@:.pdf=.t2p) -o $@ $(AM_V_texidevnull)", \
-                "$(TEXI2PDF) $(AM_V_texinfo) --build-dir=$(@:.pdf=.t2p) $(AM_V_texidevnull)"
+    if build.with?("tex") && OS.mac?
+      inreplace "doc/Makefile",
+        "$(TEXI2PDF) $(AM_V_texinfo) --build-dir=$(@:.pdf=.t2p) -o $@ $(AM_V_texidevnull)",
+        "$(TEXI2PDF) $(AM_V_texinfo) --build-dir=$(@:.pdf=.t2p) $(AM_V_texidevnull)"
     end
 
-    system "make", "pdf" if build.with? "doc"
+    system "make"
+    system "make", "pdf" if build.with? "tex"
     system "make", "install"
 
-    # Install documentation by hand
-    if build.with? "doc"
-      doc.install Dir["doc/*.pdf"]
-      doc.install "doc/macroprocessor/macroprocessor.pdf", \
-      "doc/parallel/parallel.pdf", "doc/preprocessor/preprocessor.pdf", \
-      "doc/userguide/UserGuide.pdf", "doc/gsa/gsa.pdf"
-    end
-
-    # Record Matlab info.
-    if build.with? "matlab="
-      File.open(prefix / "matlab.config", "w") do |f|
-        f.puts(matlab_path)
-        f.puts(matlab_version)
-      end
-    end
-  end
-
-  test do
-    copy("#{lib}/dynare/examples/bkk.mod", testpath)
-    if build.with? "octave"
-      system "octave --no-gui -H " \
-        "--path #{opt_prefix}/lib/dynare/matlab " \
-        "--eval 'dynare bkk.mod console'"
-    end
+    doc.install Dir.glob("doc/**/*.pdf")
 
     if build.with? "matlab="
-      matlab_path = File.open(prefix / "matlab.config") { |f| f.gets.chomp }
-      system "#{matlab_path}/bin/matlab -nosplash -nodisplay " \
-        "-r 'addpath #{opt_prefix}/lib/dynare/matlab; dynare bkk.mod console; exit'"
+      (prefix/"matlab.config").write <<-EOS.undent
+        #{matlab_path}
+        #{matlab_version}
+      EOS
     end
   end
 
@@ -107,5 +123,20 @@ class Dynare < Formula
 
             addpath #{opt_prefix}/lib/dynare/matlab
     EOS
+  end
+
+  test do
+    cp lib/"dynare/examples/bkk.mod", testpath
+    if build.with? "octave"
+      octave = Formula["octave"].opt_bin/"octave"
+      system octave, "--no-gui", "-H", "--path", "#{lib}/dynare/matlab",
+             "--eval", "dynare bkk.mod console"
+    end
+
+    if build.with? "matlab="
+      matlab_path = File.read(prefix/"matlab.config").lines.first.chomp
+      system "#{matlab_path}/bin/matlab", "-nosplash", "-nodisplay", "-r",
+             "addpath #{lib}/dynare/matlab; dynare bkk.mod console; exit"
+    end
   end
 end
