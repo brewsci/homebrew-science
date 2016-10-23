26fbb7ed765ef65b80327bdc04bce1425fc163c5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-23 19:43:55 +0100

    afra: update 2 bottle.

--- afra.rb
+++ afra.rb
@@ -3,6 +3,7 @@ class Afra < Formula
   homepage "https://github.com/EvolBioInf/afra"
   bottle do
     cellar :any
+    sha256 "ef98735d2aa088b912caee2d31d9096af11eaed2dc1e539f33ba85d13320a65d" => :sierra
     sha256 "a9ba59a547b32352bc65c26276b49d5cdcd2b9ff41a7ea169365d46f992ea0f2" => :el_capitan
     sha256 "4e49727c5c250b6befd15f253829c75e597584121402b6d85e0b1b11fe8d392e" => :yosemite
     sha256 "1f0d6421878a65f07582dd81642f351f4386022bf33ea79a8087178a048fe975" => :mavericks
