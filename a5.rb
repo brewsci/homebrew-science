db6b3ed8b40b39c4dc423f6422150b402d81d917
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-24 09:52:43 +0100

    a5: update 20150522 bottle.

--- a5.rb
+++ a5.rb
@@ -14,10 +14,10 @@ class A5 < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "5694a2ae1e9cf95c9c4fdcc2dcb23069c89fc305a05e18f4c3f759caa56c2124" => :sierra
     sha256 "712a52ae946e36d3410f6ea98425605aceffebaa738a397e6721100de6474e12" => :el_capitan
     sha256 "589bb490b43853bd8bbb0817018141efd3e65794844fbcfaf950d6ea55d4d7bb" => :yosemite
     sha256 "c6ae86aad65c91dbcf2b647acdbd25ac5672f8c04f34e7aa9fb991992d9e8fbd" => :mavericks
-    sha256 "174ba48713d53b612375e2ac4b850455625a276a9078edb53f9e02882b11bfc4" => :x86_64_linux
   end
 
   def install
