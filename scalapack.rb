2af2cd49b42ab45539959dff5e1cc546e001abf7
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-25 16:31:20 +0100

    scalapack: update 2.0.2_7 bottle.

--- scalapack.rb
+++ scalapack.rb
@@ -9,10 +9,10 @@ class Scalapack < Formula
 
   bottle do
     cellar :any
-    sha256 "742bce84d22fe05d80f2f357b0cce08527e34cab5cdff7b8a2b749e64ce2f316" => :sierra
-    sha256 "4acfb5c2d1979bee44ec79371215cdc3173ace8100e87442b989198345a1436b" => :el_capitan
-    sha256 "417487f2452d2e9ad500702b09522e88ba6f39b7ec8d1f8bf25b86c4186ba637" => :yosemite
-    sha256 "a6c491eefc1456a555af7918469836110ee969920280fa41adaf10808b7db9b9" => :mavericks
+    rebuild 1
+    sha256 "dbfbf192acc7257cc71b89a33c7ac12daaa622dd4358a839f331c567af8e205f" => :sierra
+    sha256 "db2903d330a92c6e313842f0d9e5dc9d6a502f669a7ed07cbaa6ae083ddb4475" => :el_capitan
+    sha256 "a8c8b3f78ca5a211038b9a6a1b8aa4c4fd64e3c2b023ba08036b5e1e82625786" => :yosemite
   end
 
   option "without-test", "Skip build-time tests (not recommended)"
