0f3a6814fcd2477c16595beeea093dfbe55260f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-26 09:22:28 +0100

    sambamba: update 0.6.5 bottle.

--- sambamba.rb
+++ sambamba.rb
@@ -9,9 +9,9 @@ class Sambamba < Formula
   # tag "bioinformatics"
 
   bottle do
-    sha256 "7b44b4f06b0cc6cfdbfb0b6cb099ed32780767cc02e4a4ea3b90aa22b9812a28" => :el_capitan
-    sha256 "a1fadb84af7e1a73daede860b45d8adfd2f6785d227e1f41d8960ab7d6d34883" => :yosemite
-    sha256 "57171cb36e80bf605e54bd2142aa5edf0f0fadc7b83c52043ea51731839888c7" => :mavericks
+    sha256 "18d33a5ff6dad35ac79bc360f23623ccc910f28b41073e9e1dcd92538f454615" => :sierra
+    sha256 "93eb5f7159b8c271985705abf7894dd4651bfa13063aca2f593a4518fdcdf101" => :el_capitan
+    sha256 "1d69b9413a45c367c404393ce6ad4881382479c8dd14e3451744dd7fb0a9b666" => :yosemite
   end
 
   depends_on "ldc" => :build
