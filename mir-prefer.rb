68c48ca5fee61a28885abd247fa8e437555584bf
Shaun Jackman <sjackman@gmail.com>
2016-10-27 15:59:16 -0700

    mir-prefer: Add DOI and tag bioinformatics

--- mir-prefer.rb
+++ mir-prefer.rb
@@ -3,6 +3,8 @@ class MirPrefer < Formula
   head "https://github.com/hangelwen/miR-PREFeR.git"
   url "https://github.com/hangelwen/miR-PREFeR/archive/v0.19.tar.gz"
   sha256 "e71baa5b8ed3db89e4bf15ecbe639ee2116dc7328d75b92e49829240f75c9daa"
+  # doi "10.1093/bioinformatics/btu380"
+  # tag "bioinformatics"
 
   depends_on "samtools"
   depends_on "viennarna"
