29c491fd4f3e9d8d88cea712aaa2b858c1bd4214
Shaun Jackman <sjackman@gmail.com>
2016-10-29 11:02:05 -0700

    vt: update 0.5772 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#340.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- vt.rb
+++ vt.rb
@@ -6,6 +6,7 @@ class Vt < Formula
     sha256 "8fdf8b6373ce9a6968b230d11815e30e4355fa81b938d718fec4dec7dbaf21bf" => :el_capitan
     sha256 "c3bcf866e90b6a93f666ceb725ca6891cb23448856c4163e066548f13bf4b50a" => :yosemite
     sha256 "3d2e1bad812a3097a4c515ba7940a7cd09518e0f251e9a806d965e8de759db37" => :mavericks
+    sha256 "59764b0c5aed39ec0189da065005d3ba4e89791ed02a8cedcde2fdca85679a6b" => :x86_64_linux
   end
 
   # doi "10.1093/bioinformatics/btv112"
