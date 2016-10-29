2a5e1c9f7565a772a943fcd38849acf828cdc727
Shaun Jackman <sjackman@gmail.com>
2016-10-29 10:59:08 -0700

    dida: update 1.0.1_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#311.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- dida.rb
+++ dida.rb
@@ -13,6 +13,7 @@ class Dida < Formula
     sha256 "38e35d907cb2841dc82b94de93d5323c3b8136d7e0ff4a0e97e2c96c46529232" => :el_capitan
     sha256 "6d221aa3bbb2d3886906b75d021dae0534f40de5ee6173488c0aa5b70e411396" => :yosemite
     sha256 "09d21a097b6b1caa7fbb33b69c09500115f8d5c49a47da810ce64b972593eebb" => :mavericks
+    sha256 "1da64c7a7cbd22ace38891cf5d896138c126488a48123900fa6e57faf6cc39d3" => :x86_64_linux
   end
 
   # Fix error: DIDA must be compiled with a C++ compiler that supports OpenMP threading.
