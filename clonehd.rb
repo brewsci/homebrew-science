10c0ca29b854bb8a29892c0b004cb8b1e86d0b22
Shaun Jackman <sjackman@gmail.com>
2016-10-29 10:58:58 -0700

    clonehd: update 1.17.9 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#309.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- clonehd.rb
+++ clonehd.rb
@@ -8,6 +8,7 @@ class Clonehd < Formula
     sha256 "8ee4733510ff68c206f5a5ce8ae554bef0cdfd5986b9cbebf9d54e591dc774d2" => :el_capitan
     sha256 "debd6eb1f17dd3dcb613e2ce73e8cd087dbbf5f1163b384d89a3e407eb19e675" => :yosemite
     sha256 "aef26cbb76e8df98d9e876b37815804dc7053db999cf81e427c039e06c0980cd" => :mavericks
+    sha256 "0c23dfd1e0c4c3f030d9efd58749835fd0a24d241dc4f10fee5fe4d21bbf1b4d" => :x86_64_linux
   end
 
   # doi "10.1016/j.celrep.2014.04.055"
