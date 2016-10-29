5c2457b1d4867e645e3bcb559750a21e97800ffc
Shaun Jackman <sjackman@gmail.com>
2016-10-29 10:58:53 -0700

    bpipe: update 0.9.9.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#305.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bpipe.rb
+++ bpipe.rb
@@ -13,6 +13,7 @@ class Bpipe < Formula
     sha256 "5cbbd4b15a4ea1a73266a82ad9e47747ac745600e331c7abbef3033958d2b2a0" => :el_capitan
     sha256 "af2b2242a3a04786cd46acfbf56c21a83330d78bf969c49db6cf7be22f62404d" => :yosemite
     sha256 "670604584ce9f32afcad449840fdc4294b228b60c315d5bdc178e6b8d07fc64e" => :mavericks
+    sha256 "5207240e3fbe3182f447f6a08b5407715047d749fdb03967f39a2b5535334095" => :x86_64_linux
   end
 
   depends_on :java
