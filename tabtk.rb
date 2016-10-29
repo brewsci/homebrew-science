a84f66f98fb9bf2b2af8aaf5e6adc36768f79ac6
Shaun Jackman <sjackman@gmail.com>
2016-10-29 11:02:00 -0700

    tabtk: update 0.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#339.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- tabtk.rb
+++ tabtk.rb
@@ -9,6 +9,7 @@ class Tabtk < Formula
     sha256 "bf8c44ad2b333a40490e785a0fa9b71fec66c47f104c92791bab96ed4a07b41b" => :el_capitan
     sha256 "7e78e89f6582dc4ee90d35eacde0ed1e722dba7c01c3f4bca25eb9f4db59d37a" => :yosemite
     sha256 "fda99507d62f92c5a5e10d1af97850631fc8ebb97ab53d265d7c81fbc535c78e" => :mavericks
+    sha256 "edfe0eb28b8641910e8ad91854521a197c0b41d4d2a14febb10f9fdf200c02ac" => :x86_64_linux
   end
 
   # tab "bioinformatics"
