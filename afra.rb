243bca8fca46fac76b53dec83839b4d6f06684f1
Shaun Jackman <sjackman@gmail.com>
2016-10-30 16:05:06 -0700

    afra: update 2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#299.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- afra.rb
+++ afra.rb
@@ -7,6 +7,7 @@ class Afra < Formula
     sha256 "a9ba59a547b32352bc65c26276b49d5cdcd2b9ff41a7ea169365d46f992ea0f2" => :el_capitan
     sha256 "4e49727c5c250b6befd15f253829c75e597584121402b6d85e0b1b11fe8d392e" => :yosemite
     sha256 "1f0d6421878a65f07582dd81642f351f4386022bf33ea79a8087178a048fe975" => :mavericks
+    sha256 "f7fb0b66080a819db4d9c2ca7f31bff05fb37caa67e07bbeb38b88f3599cc869" => :x86_64_linux
   end
 
   # tag "bioinformatics"
