c592e9c028574ece392f65e5dfdb456716b10e07
Shaun Jackman <sjackman@gmail.com>
2016-10-30 16:11:21 -0700

    lsd: update 0.3b bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#321.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- lsd.rb
+++ lsd.rb
@@ -6,6 +6,7 @@ class Lsd < Formula
     sha256 "79d386d8cc6098d384db82e3f429f47cb72e40d1ed61b96ccc9b594f2ea59fca" => :el_capitan
     sha256 "5e96e65e0ba2d7e186b426d0c07ae23001c900070e59362426166d10924f9825" => :yosemite
     sha256 "fc8f99290db0e29e2686f052a0d345fa4893a172b54a0bf294a10741a4943b79" => :mavericks
+    sha256 "b5d84d48a1f2724edeee5cdea68bb2bedb9a4d7f65399a0b374e0c76e380859e" => :x86_64_linux
   end
 
   # doi "10.1093/sysbio/syv068"
