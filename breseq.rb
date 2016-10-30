44cfdcc40a68c8d890ab1a5e1d94a33aa8f6177b
Shaun Jackman <sjackman@gmail.com>
2016-10-30 16:08:21 -0700

    breseq: update 0.28.1.Source bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#307.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- breseq.rb
+++ breseq.rb
@@ -6,6 +6,7 @@ class Breseq < Formula
     sha256 "780ba849d784e3ff5fd5ae0a0906c5108fd50ad30c2a257f60465633919333ee" => :el_capitan
     sha256 "c4a40aa8d7fb673a65c51627a7e48ccf4f5821e99e01001d9fe98cfd9525f763" => :yosemite
     sha256 "e4d5b3c2dc68a539c00fbae40bf7eab32131d1bf4113f3159b10c97fedd44def" => :mavericks
+    sha256 "6d370e68d3cbddc76d078fb9129ebe4211cbb6b1454cce5759458bdf8e9bb873" => :x86_64_linux
   end
 
   # doi "10.1007/978-1-4939-0554-6_12"
