30cbe4a554570a88308042edf120211a757d46f9
Shaun Jackman <sjackman@gmail.com>
2016-10-30 16:08:46 -0700

    cd-hit: update 4.6.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#308.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- cd-hit.rb
+++ cd-hit.rb
@@ -18,6 +18,7 @@ class CdHit < Formula
     sha256 "3fb261d867d15de894911baa06140b920b34408a5917b92a4aeb780db2402e78" => :el_capitan
     sha256 "44f2addb6cc0678b85a5c760ac9e83f9372c21a496e5ecea9b367e585c28aa5c" => :yosemite
     sha256 "c2750c16922bea6bd9df9120dd3bdb776c88f7f753eb01f938fd9ce8dfc987d3" => :mavericks
+    sha256 "1d9d89d4c12a618f10263df837e3b77653e2684fd45984f3caba1104832d5779" => :x86_64_linux
   end
 
   def install
