e84a29985cfb4c50116bab7128611b9d44589db1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-31 12:27:15 +0000

    megahit: update 1.0.6.1 bottle.

--- megahit.rb
+++ megahit.rb
@@ -9,10 +9,9 @@ class Megahit < Formula
   head "https://github.com/voutcn/megahit.git"
 
   bottle do
-    sha256 "7f4912f5884915f4a9f95c60880e72aaafe16c5e632449777859bb313302e542" => :sierra
-    sha256 "9828d04f35fbbc5106dad0e696d8eb5d4b85a522aaba03061355939d36972244" => :el_capitan
-    sha256 "4628d5e3c19988808e15c631ebc08a20eef96d5a4f7773c67163fb715f721ef9" => :yosemite
-    sha256 "cbe3c26c1b373422fac89b257ff7b805c8ef62b3c74573c492e6b5654ea1e17b" => :x86_64_linux
+    sha256 "0c9ecbdd7936b04b96bb7d3b234a41117f84b6f8c582b4d9286463984ec08057" => :sierra
+    sha256 "e8d64afae8ac19d7ab5042ee3958b1cc900bab4b7121cd74d1519fe5d9dd032c" => :el_capitan
+    sha256 "404a411f1e0d3d083e28657ae25f57730d7b10c0f3d5345dbb2e6f01633c0c2e" => :yosemite
   end
 
   fails_with :llvm do
