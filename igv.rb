e6e2be2f670120ac621fc120296cc4d7ed11309f
ilovezfs <ilovezfs@icloud.com>
2016-10-30 21:47:03 -0700

    igv 2.3.85
    Closes #4546.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- igv.rb
+++ igv.rb
@@ -3,8 +3,8 @@ class Igv < Formula
   homepage "https://www.broadinstitute.org/software/igv"
   # tag "bioinformatics"
   # doi "10.1093/bib/bbs017"
-  url "https://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.83.zip"
-  sha256 "9810afc7769249b7cb4f0d0456f1ce89b61331818c5c6164d32a67e302ae3d77"
+  url "https://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.85.zip"
+  sha256 "b1ccf0c886303b0b88d2d7048da6ed7c293490cb1c344ac818183a39631b1abf"
   head "https://github.com/broadinstitute/IGV.git"
 
   bottle :unneeded
