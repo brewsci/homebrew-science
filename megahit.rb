de25cb484165223298aa8d2c79dffee7da36a48d
ilovezfs <ilovezfs@icloud.com>
2016-10-31 05:16:16 -0700

    megahit 1.0.6.1
    Closes #4489.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- megahit.rb
+++ megahit.rb
@@ -4,9 +4,8 @@ class Megahit < Formula
   # doi "10.1093/bioinformatics/btv033"
   # tag "bioinformatics"
 
-  url "https://github.com/voutcn/megahit/archive/v1.0.6.tar.gz"
-  sha256 "16dde39241e11c69f20f7b55c0d28db40860ace9e624db9e43e06e34cb052c11"
-
+  url "https://github.com/voutcn/megahit/archive/v1.0.6.1.tar.gz"
+  sha256 "f86c9d64b0ea859cda4ddf2531bdc59d66150380d09e5375fdb92758d19f600f"
   head "https://github.com/voutcn/megahit.git"
 
   bottle do
@@ -14,13 +13,6 @@ class Megahit < Formula
     sha256 "9828d04f35fbbc5106dad0e696d8eb5d4b85a522aaba03061355939d36972244" => :el_capitan
     sha256 "4628d5e3c19988808e15c631ebc08a20eef96d5a4f7773c67163fb715f721ef9" => :yosemite
     sha256 "cbe3c26c1b373422fac89b257ff7b805c8ef62b3c74573c492e6b5654ea1e17b" => :x86_64_linux
-  end
-
-  # Fix error: 'HashGraph::HashGraph(const HashGraph&)' is private within this context
-  # Fixed upstream: https://github.com/voutcn/megahit/issues/96
-  patch do
-    url "https://github.com/voutcn/megahit/commit/a9f7e90e537f79684fc2d76da971b60cac48bfa0.patch"
-    sha256 "5ec070ae1611df9b0c12e3566f7a6bf6c9f933c3b3e62f46edf2ca1fd5ab5ed1"
   end
 
   fails_with :llvm do
