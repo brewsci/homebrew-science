e3bef4020fe62ad55b6e0012a3a9d7a1179d8a01
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-31 00:10:30 +0000

    mothur: update 1.38.1.1 bottle for Linuxbrew.

--- mothur.rb
+++ mothur.rb
@@ -11,6 +11,7 @@ class Mothur < Formula
     sha256 "41c366e68b69f005fe514abb4198e9ebcbcd197e91c9aabdb15209b2c1de1674" => :sierra
     sha256 "0e0db35f1786a9ac237028c68aa59c50638af478f1ff12b1e01cb3a2a662b75f" => :el_capitan
     sha256 "1e9cf0e95c35030a84d0792dab3d9525059790e290e0eb244f2fd0c108aaed5a" => :yosemite
+    sha256 "5218eeabcab002f56b6cfc2481be948481f429d3e098747b941cf99d152a2688" => :x86_64_linux
   end
 
   depends_on "boost"
