9a516bf68202f8408729f0bf3537c9812e6d08c7
ilovezfs <ilovezfs@icloud.com>
2016-10-31 23:36:48 -0700

    transdecoder 3.0.1
    Closes #4550.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- transdecoder.rb
+++ transdecoder.rb
@@ -1,12 +1,11 @@
 class Transdecoder < Formula
   desc "Identifies candidate coding regions within transcript sequences"
   homepage "https://transdecoder.github.io/"
-  url "https://github.com/TransDecoder/TransDecoder/archive/v3.0.0.tar.gz"
-  sha256 "b5a1ad48c0df64bb539709e523418bbe197d0342bd2fae40e035234946bcaeb6"
+  url "https://github.com/TransDecoder/TransDecoder.git",
+      :tag => "v3.0.1",
+      :revision => "9b52e6933371c89a62b8dde8310d486d7df73d12"
   head "https://github.com/TransDecoder/TransDecoder.git"
   # tag "bioinformatics"
-
-  head "https://github.com/TransDecoder/TransDecoder.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,16 +17,42 @@ class Transdecoder < Formula
 
   depends_on "cd-hit"
 
-  def install
-    # Remove the cd-hit source code
-    rm_rf "transdecoder_plugins"
+  resource "URI::Escape" do
+    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/URI-1.71.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/URI-1.71.tar.gz"
+    sha256 "9c8eca0d7f39e74bbc14706293e653b699238eeb1a7690cc9c136fb8c2644115"
+  end
 
-    prefix.install Dir["*"]
-    bin.install_symlink "../TransDecoder.LongOrfs", "../TransDecoder.Predict"
+  def install
+    inreplace "TransDecoder.Predict",
+      "= &check_program('cd-hit-est');",
+      "= '#{Formula["cd-hit"].opt_bin}/cd-hit-est';"
+
+    rm "Makefile"
+    rm_rf "transdecoder_plugins" # Remove the cd-hit source code
+    rm_rf Dir["TransDecoder.lrgTests/{.git,README.md}"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+
+    ["sample_data", "TransDecoder.lrgTests", "util"].each do |f|
+      pkgshare.install_symlink libexec/f
+    end
+
+    resource("URI::Escape").stage do
+      system "perl", "Makefile.PL", "LIB=#{libexec}/PerlLib", "PREFIX=#{libexec}/vendor"
+      system "make", "install"
+    end
+
+    bin.write_exec_script Dir[libexec/"*"].select { |f| File.file?(f) && File.executable?(f) }
   end
 
   test do
     assert_match "USAGE", shell_output("#{bin}/TransDecoder.LongOrfs 2>&1", 1)
     assert_match "USAGE", shell_output("#{bin}/TransDecoder.Predict 2>&1", 1)
+    cp_r "#{libexec}/.", testpath
+    cd "sample_data" do
+      system "make", "test"
+    end
   end
 end
