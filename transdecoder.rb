1a361914cb9232d0ef7064b5ee69d71495c056dd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-11-01 20:28:59 +0000

    transdecoder: update 3.0.1 bottle for Linuxbrew.

--- transdecoder.rb
+++ transdecoder.rb
@@ -12,6 +12,7 @@ class Transdecoder < Formula
     sha256 "05955821eb2ace7c8ef8bfd5a36ff35321355e35f3b432235c08ce29b4cd7411" => :sierra
     sha256 "b1c90616d9d140b4e313c1ea831d435965cad5d6a680083a3f00b70f0cb1efe1" => :el_capitan
     sha256 "4a812936c775a6c2edca2c8e1930c143596bfe4c2817e9a71b0bcc7d45af38ec" => :yosemite
+    sha256 "d71073ab9daca3ff95cbba7c241e1066ff7b17a62ce180744e26692e1062caa9" => :x86_64_linux
   end
 
   depends_on "cd-hit"
