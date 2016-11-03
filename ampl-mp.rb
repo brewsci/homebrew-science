70baa49039cacfd057f8069f22fab0daebe05124
Dominique Orban <dominique.orban@gmail.com>
2016-11-02 20:37:47 -0400

    ampl-mp: rename from asl
    updates for linux

--- ampl-mp.rb
+++ ampl-mp.rb
@@ -1,9 +1,8 @@
-class Asl < Formula
+class AmplMp < Formula
   desc "The AMPL modeling language solver library"
   homepage "http://www.ampl.com"
   url "https://github.com/ampl/mp/archive/3.1.0.tar.gz"
   sha256 "587c1a88f4c8f57bef95b58a8586956145417c8039f59b1758365ccc5a309ae9"
-  revision 1
 
   bottle do
     cellar :any
@@ -33,14 +32,14 @@ class Asl < Formula
     system "cmake", ".", *cmake_args
     system "make", "all"
     system "make", "test" if build.with? "test"
-    system "install_name_tool", "-change", "@rpath/libmp.3.dylib", lib/"libmp.dylib", "bin/libasl.dylib"
+    system "install_name_tool", "-change", "@rpath/libmp.3.dylib", lib/"libmp.dylib", "bin/libasl.dylib" if OS.mac?
     system "make", "install"
     mkdir libexec
     mv bin, libexec/"bin"
 
     if build.with? "matlab"
       mkdir_p (pkgshare/"matlab")
-      mv Dir["#{libexec}/bin/*.mexmaci64"], (pkgshare/"matlab")
+      mv Dir["#{libexec}/bin/*.mex*"], pkgshare/"matlab"
     end
 
     resource("miniampl").stage do
