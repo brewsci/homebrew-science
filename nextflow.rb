60142da659a0f78e55d735524a7c7a607e3622c0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2016-11-04 22:46:08 +0100

    nextflow 0.22.4
    Closes #4570.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

--- nextflow.rb
+++ nextflow.rb
@@ -4,9 +4,9 @@ class Nextflow < Formula
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
-  url "https://www.nextflow.io/releases/v0.22.3/nextflow"
-  version "0.22.3"
-  sha256 "906c03172c685af23282583ac976a99f1d194a798b6a2d70331bca5d9c49cad7"
+  url "https://www.nextflow.io/releases/v0.22.4/nextflow"
+  version "0.22.4"
+  sha256 "01f1473b6e05b10fa02cbc39fe38665987d71873cd1c1e1d875ba9066f65e6ef"
   head "https://github.com/nextflow-io/nextflow.git"
 
   bottle :unneeded
