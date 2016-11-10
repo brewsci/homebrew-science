80ae39c961a3247996c9108b1ed5c0bd8f2ad6c2
Thierry Moisan <thierry.moisan@jda.com>
2016-11-10 15:52:17 -0500

    cube 4.3.4
    Closes #4585.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- cube.rb
+++ cube.rb
@@ -1,7 +1,8 @@
 class Cube < Formula
+  desc "Performance report explorer for Scalasca and Score-P"
   homepage "http://apps.fz-juelich.de/scalasca/"
-  url "http://apps.fz-juelich.de/scalasca/releases/cube/4.2/dist/cube-4.2.2.tar.gz"
-  sha256 "67a7e2b3cf4927620fc4e987d6fae76b753a74303b3c9d30ea2e0937e64ae82a"
+  url "http://apps.fz-juelich.de/scalasca/releases/cube/4.3/dist/cube-4.3.4.tar.gz"
+  sha256 "34c55fc5d0c84942c0845a7324d84cde09f3bc1b3fae6a0f9556f7ea0e201065"
 
   bottle do
     cellar :any
@@ -10,25 +11,20 @@ class Cube < Formula
     sha256 "a6adbc6dfc92586c4bc71bc86d12f4d52834b58ae30880c75b055594c348e0f8" => :yosemite
   end
 
-  # qt is currently not available on 10.12
-  depends_on "qt" => :recommended if MacOS.version < :sierra
-
-  fails_with :clang do
-    cause <<-EOS.undent
-      Undefined symbols for architecture x86_64:
-      "cube::Cube::def_mirror(std::string const&)", referenced from:
-      MainWidget::readFile(QString) in cube-MainWidget.o
-    EOS
-  end if build.with? "qt"
+  depends_on "qt5"
+  depends_on "cmake" => :build
 
   def install
     ENV.deparallelize
-
     system "./configure",
       "--disable-debug",
       "--disable-dependency-tracking",
       "--disable-silent-rules",
-      "--prefix=#{prefix}"
+      "--prefix=#{prefix}",
+      "--with-nocross-compiler-suite=clang",
+      "--with-qt-specs=macx-clang",
+      'CXXFLAGS="-stdlib=libc++"',
+      'LDFLAGS="-stdlib=libc++"'
     system "make"
     system "make", "install"
   end
