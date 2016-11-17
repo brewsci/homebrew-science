f4a2c0343ec50ddcf729573da73e499aa6df3bfa
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-17 11:11:09 +0000

    nco: update 4.6.2 bottle.

--- nco.rb
+++ nco.rb
@@ -6,9 +6,9 @@ class Nco < Formula
 
   bottle do
     cellar :any
-    sha256 "2261855bded863b34e7318894319bcc029f2592288b0c2102a8a60873d5ecd06" => :el_capitan
-    sha256 "fff806f79884e2acce0e0e6ce497ac8b16d0296015ae15511951df8f063fb628" => :yosemite
-    sha256 "093e3088df1eff83b22311e96713ac1cbf429a2606067532acd1b7a61e4f0341" => :mavericks
+    sha256 "dacf879492656f1e466114cbe3f2f2518398ef9ef340d5fbd6b7a439d42a6550" => :sierra
+    sha256 "7a50e95caa0cf66ae8982331196125dd965c1d47b00d84f0676a784b7748cd16" => :el_capitan
+    sha256 "3d666c782597bd6c5ba13206622260024c869bf2f32e4427c545877a38032bc0" => :yosemite
   end
 
   head do
