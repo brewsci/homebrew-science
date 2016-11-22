6db3dfc04990fef2cfa43ebbf65b2a1a5d208a16
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-11-22 19:57:11 +0000

    e-mem: add 1.0.0 bottle for Linuxbrew.

--- e-mem.rb
+++ e-mem.rb
@@ -12,6 +12,7 @@ class EMem < Formula
     sha256 "b67ed6d5687530d9e86570e6e65164a3358ffe785771a10caad77627444f1d1f" => :sierra
     sha256 "fbd2e31ea4b9b8679fdde1c9ed5936fa3f17f3840270184db11f49979f4bed0f" => :el_capitan
     sha256 "95f69f5b0fd8b4f0fe0e437d4ee985c8a4bdc1bc8b0ed793cdbe5519db863066" => :yosemite
+    sha256 "531f0d557ac8c858906639812dbbf1c53ba813a78cbc964686f5941d78ca3737" => :x86_64_linux
   end
 
   needs :openmp
