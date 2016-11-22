f84a20dc691f5e9e0c455224c15e8689430009b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-22 19:53:55 +0000

    e-mem: add 1.0.0 bottle.

--- e-mem.rb
+++ e-mem.rb
@@ -7,6 +7,13 @@ class EMem < Formula
   # doi "10.1093/bioinformatics/btu687"
   # tag "bioinformatics"
 
+  bottle do
+    cellar :any
+    sha256 "b67ed6d5687530d9e86570e6e65164a3358ffe785771a10caad77627444f1d1f" => :sierra
+    sha256 "fbd2e31ea4b9b8679fdde1c9ed5936fa3f17f3840270184db11f49979f4bed0f" => :el_capitan
+    sha256 "95f69f5b0fd8b4f0fe0e437d4ee985c8a4bdc1bc8b0ed793cdbe5519db863066" => :yosemite
+  end
+
   needs :openmp
   depends_on "boost" => :build
 
