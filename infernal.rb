b214f00834732db28f265c2184fe87d4e672b2df
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-11-24 16:38:11 +0000

    infernal: update 1.1.2 bottle for Linuxbrew.

--- infernal.rb
+++ infernal.rb
@@ -12,6 +12,7 @@ class Infernal < Formula
     sha256 "230ae947e5fff0b1af7828ab3d26f06d8656ba3a4535710691b721dd6fac51b0" => :sierra
     sha256 "c19dd6cc6bdbb6e62b034c945464d374993981ae58aaa14077fccad935310569" => :el_capitan
     sha256 "4f472911166ddea3eefa6d4bb9c7ff70882673d8fe1063cb0b7e766fa5b92018" => :yosemite
+    sha256 "7651ab8097ca8d88a64c3f85c83e3e6eedb7a99227bf04d6cef71058482d5b4d" => :x86_64_linux
   end
 
   deprecated_option "check" => "with-test"
