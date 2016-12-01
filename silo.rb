db1c2f1ae339e9cba5cedfc4ed44a062f3243330
Gunther H. Weber <GHWeber@lbl.gov>
2016-11-30 18:56:41 -0800

    silo: build as shared library, add HDF5 support
    Closes #4632.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- silo.rb
+++ silo.rb
@@ -1,8 +1,9 @@
 class Silo < Formula
-  desc "LLNL Silo library. Allows to create databases for VisIt."
+  desc "LLNL mesh and field Silo I/O library. Allows creating databases for VisIt."
   homepage "https://wci.llnl.gov/simulation/computer-codes/silo"
   url "https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2-bsd.tar.gz"
   sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
+  revision 1
 
   bottle do
     sha256 "121f4802d1e18e049f50360b9d6222867028320cea21a33ead806db3d55e1a3d" => :sierra
@@ -10,19 +11,31 @@ class Silo < Formula
     sha256 "61153eafb69d477e1270b07a951f402192d38c13759cd064801ad0d7ccd60e02" => :yosemite
   end
 
+  option "with-static", "Build as static instead of dynamic library"
+  option "without-lite-headers", "Do not install PDB lite headers"
+
   depends_on :x11 => :optional
   depends_on :fortran
   depends_on "readline"
+  depends_on "hdf5" => :recommended
 
   def install
     args = ["--prefix=#{prefix}"]
     args << "--enable-optimization"
+    args << "--with-zlib"
+    args << "--enable-install-lite-headers" if build.with? "lite-headers"
+    args << "--enable-shared" if build.without? "static"
     args << "--enable-x" if build.with? "x11"
+    args << "--with-hdf5=#{Formula["hdf5"].opt_prefix}" if build.with? "hdf5"
 
     ENV.append "LDFLAGS", "-L#{Formula["readline"].opt_lib} -lreadline"
     system "./configure", *args
     system "make", "install"
-    rm lib/"libsilo.settings"
+    if build.with? "hdf5"
+      rm lib/"libsiloh5.settings"
+    else
+      rm lib/"libsilo.settings"
+    end
   end
 
   test do
@@ -40,7 +53,16 @@ class Silo < Formula
             return 0;
         }
         EOS
-    system ENV.cc, "test.c", "-I#{opt_include}", "-L#{opt_lib}", "-lsilo", "-o", "test"
+    test_args = ["test.c", "-I#{opt_include}", "-L#{opt_lib}", "-o", "test"]
+    if build.with? "hdf5"
+      test_args << "-lsiloh5"
+      test_args << "-L#{Formula["hdf5"].opt_lib}"
+      test_args << "-lhdf5"
+    else
+      test_args << "-lsilo"
+    end
+    test_args << "-lm"
+    system ENV.cc, *test_args
     system "./test"
   end
 end
