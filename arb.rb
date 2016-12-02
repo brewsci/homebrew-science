ce1ca487653dec40bebc9b65b1934d42489ac55f
ilovezfs <ilovezfs@icloud.com>
2016-12-02 14:23:23 -0800

    arb 2.9.0
    Closes #4667.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- arb.rb
+++ arb.rb
@@ -1,8 +1,8 @@
 class Arb < Formula
   desc "C library for arbitrary-precision floating-point ball arithmetic"
   homepage "http://fredrikj.net/arb/index.html"
-  url "https://github.com/fredrik-johansson/arb/archive/2.8.1.tar.gz"
-  sha256 "f4f4ec2d59b348c5e89eaf0b33734bce83a2ef85ab4748b24b984d07f5651012"
+  url "https://github.com/fredrik-johansson/arb/archive/2.9.0.tar.gz"
+  sha256 "98f6f543e41e5dcb62cc6204d607fefa88317388c54c2a8edb9e601f7d243142"
   head "https://github.com/fredrik-johansson/arb.git"
 
   bottle do
@@ -19,8 +19,20 @@ class Arb < Formula
   depends_on "flint"
 
   def install
+    if OS.mac?
+      inreplace "Makefile.in" do |s|
+        s.gsub! "ln -sf \"$(ARB_LIB)\" \"$(ARB_LIBNAME).$(ARB_MAJOR)\";",
+                "ln -sf \"$(ARB_LIB)\" \"libarb.$(ARB_MAJOR).dylib\";"
+        s.gsub! "cp -a $(shell ls $(ARB_LIBNAME)*) \"$(DESTDIR)$(PREFIX)/$(LIBDIR)\";",
+                "cp -a $(shell ls libarb*) \"$(DESTDIR)$(PREFIX)/$(LIBDIR)\";"
+      end
+    end
+
     ENV.prepend "CFLAGS", "-I#{Formula["flint"].opt_include}/flint"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                          "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+                          "--with-flint=#{Formula["flint"].opt_prefix}"
     system "make"
     system "make", "install"
     system "make", "check" if build.with? "test"
