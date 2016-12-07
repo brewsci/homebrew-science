4b64590f661626949a02ce672704ff5dee6de6da
Shaun Jackman <sjackman@gmail.com>
2016-12-07 15:49:47 -0800

    muscle: update 3.8.1551 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#345.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- muscle.rb
+++ muscle.rb
@@ -11,6 +11,7 @@ class Muscle < Formula
     sha256 "e772cd2b482c3fc892485504ccae0ee5a486cb00a57b8e6e154b9f20d0d201e5" => :sierra
     sha256 "46fb3219f56a068c718f6b7f3a8d13db9a4b7c6c9483a969da16c7588e59a4e1" => :el_capitan
     sha256 "7b9fdbd97e273d81c87881e2b58151311221aeaedca1b875c560232d831c0b30" => :yosemite
+    sha256 "c554628301f042c9803c045da94ea9109c414b277f994cd6fc07763c93c7e510" => :x86_64_linux
   end
 
   def install
