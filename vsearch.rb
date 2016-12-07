92d46d123613cb40f7aaf0a79ac6555f5ddc546c
Shaun Jackman <sjackman@gmail.com>
2016-12-07 14:57:10 -0800

    vsearch: update 2.3.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-science#343.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- vsearch.rb
+++ vsearch.rb
@@ -12,6 +12,7 @@ class Vsearch < Formula
     sha256 "36f333bd68c10a8beec38551d4f979aefd9bdf8132ce3503e50cac948950101d" => :sierra
     sha256 "36f333bd68c10a8beec38551d4f979aefd9bdf8132ce3503e50cac948950101d" => :el_capitan
     sha256 "e957e27351df20bded562671ff728d6e47d3380e258dea6259f23c853c88e23b" => :yosemite
+    sha256 "b52fa8aa745b55e85e0656a2b17e96c65c4ac0bc1f12fd63c2d8d2092416bce8" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
