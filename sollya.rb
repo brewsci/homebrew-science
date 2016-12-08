e66c82fed8b2d357b41a1c6b7bfd296e99ac03f6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-08 03:12:30 +0000

    sollya: add 6.0 bottle.

--- sollya.rb
+++ sollya.rb
@@ -4,6 +4,13 @@ class Sollya < Formula
   url "https://gforge.inria.fr/frs/download.php/file/36271/sollya-6.0.tar.bz2"
   sha256 "05321581ba47f5e0804b712157dfc5343a268ca6c1983dce48fdd1e91d5b0a1f"
   head "https://scm.gforge.inria.fr/anonscm/git/sollya/sollya.git"
+  bottle do
+    cellar :any
+    sha256 "f91eef8bbf199d0d8303b84ad048892ccbf48f20ab3c5bdff345f863d2a56e34" => :sierra
+    sha256 "3b7031eb8f5217cbeddee252819708ad50f81bd15c811726188c2aa91dee254d" => :el_capitan
+    sha256 "62f084ab101dbed39a6b459b26762670cf78393b1bfbde381eb0efb1831d6ee8" => :yosemite
+  end
+
   # doi "10.1007/978-3-642-15582-6_5"
 
   option "with-test", "Run full test suite (time consuming)"
