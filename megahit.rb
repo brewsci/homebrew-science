c63ee5a3b43006ade24de4d77a56957cd23ce374
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-12-09 12:15:15 +0000

    megahit: update 1.1.1 bottle for Linuxbrew.

--- megahit.rb
+++ megahit.rb
@@ -12,6 +12,7 @@ class Megahit < Formula
     sha256 "278e59d8abd9baad784b49570ad37fe2e2863dc5b85fe52c0d75c1e68bb31764" => :sierra
     sha256 "68222fb694024e331a0dd56ca69e82788c20c6d281d0c4f4a697f0c307650ae5" => :el_capitan
     sha256 "dbf4070e1c2756d728cc49a465f63287c6d86c4bd103f5caf895b6a392253fa8" => :yosemite
+    sha256 "40cf2e2c113eb4b1b4cfc7ad4c961bfa66e29eb0325b91bda5fffa507c968825" => :x86_64_linux
   end
 
   fails_with :llvm do
