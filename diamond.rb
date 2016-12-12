2e56a80ab553cbc78f605c5faa6b71b35d2bfbbb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-12-12 23:45:15 +0000

    diamond: update 0.8.29 bottle for Linuxbrew.

--- diamond.rb
+++ diamond.rb
@@ -12,6 +12,7 @@ class Diamond < Formula
     sha256 "f30dee7a6d6418d5f0041ef94d3fbfdf3d27f883a6732c8cdd827bfe52438156" => :sierra
     sha256 "978a6e75f38066f2a1d0aa873aedfb5f4f0dd8e06f57c31121b1472bd1bb3830" => :el_capitan
     sha256 "632c72577efc7dd89261af23ffc9b4559b6aad69fb9b318ebb13c9758679cb2b" => :yosemite
+    sha256 "a14c940f6b8d641954c9bd066231b249d96943642f8608919f6aa8dd17236288" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
