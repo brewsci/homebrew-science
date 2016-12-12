d985cc816b60f9cc1281f20110399568840973f5
Mike McQuaid <mike@mikemcquaid.com>
2016-12-12 09:00:46 +0000

    biopp: remove qt option cruft.

--- biopp.rb
+++ biopp.rb
@@ -44,27 +44,12 @@ class Biopp < Formula
     sha256 "c7ec73a5af84808362f301479c548b6a01c47a66065b28a053ff8043409e861a"
   end
 
-  resource "bppqt" do
-    url "http://biopp.univ-montp2.fr/repos/sources/bpp-qt-2.2.0.tar.gz"
-    sha256 "9662f66bc3491d8e128263f6bd91fcdbecdb375ec9f24519f44855cdcdb9d553"
-  end
-
   def install
     %w[bppcore bppseq bppphyl bpppopgen bppseqomics bppraa].each do |r|
       resource(r).stage do
         mkdir "build" do
           system "cmake", "..", *std_cmake_args
           system "make", "#{r}-shared", "#{r}-static"
-          system "make", "install"
-        end
-      end
-    end
-
-    if build.with? "qt"
-      resource("bppqt").stage do
-        mkdir "build" do
-          system "cmake", "..", *std_cmake_args
-          system "make", "bppqt-shared", "bppqt-static"
           system "make", "install"
         end
       end
@@ -115,7 +100,6 @@ class Biopp < Formula
     EOS
     libs = %W[-lbpp-core -lbpp-seq -lbpp-phyl -lbpp-raa -lbpp-seq-omics
               -lbpp-phyl-omics -lbpp-popgen]
-    libs << "-lbpp-qt" if build.with? "qt"
     system ENV.cxx, "-o", "test", "bpp-phyl-test.cpp",
            "-I#{include}", "-L#{lib}", *libs
     system "./test"
