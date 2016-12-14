0ded387ad155a1f24db4ff3211eafbb58c394c90
Shaun Jackman <sjackman@gmail.com>
2016-12-14 14:55:06 -0800

    links-scaffolder 1.6.1

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -1,9 +1,9 @@
 class LinksScaffolder < Formula
   desc "Long Interval Nucleotide K-mer Scaffolder"
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/links"
-  url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.5.2/links_v1-5-2.tar.gz"
-  version "1.5.2"
-  sha256 "3ac4b6287896955759b03993128618616d0ab2f5535b2de91ef2b799819ad707"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.6.1/links_v1-6-1.tar.gz"
+  version "1.6.1"
+  sha256 "c4fa4f39dbee49c484521a80738758a39dc21b4a594d3e3fcf231d4decfdbe97"
   # doi "10.1186/s13742-015-0076-3"
   # tag "bioinformatics"
 
