ce62c01fe852cd535279d4e337098a4bf8fa72bf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-14 02:32:10 +0000

    nexusformat: update 4.4.3_2 bottle.

--- nexusformat.rb
+++ nexusformat.rb
@@ -7,9 +7,9 @@ class Nexusformat < Formula
 
   bottle do
     cellar :any
-    sha256 "8842f1cdb80a0b9ad2ef328e65c900f4232ed4c3a06f21dd4feeb27b12b5ab18" => :sierra
-    sha256 "a9b03b655466ff438801625d934800000e8421943709b04b5aff86ce835a8818" => :el_capitan
-    sha256 "909d6135a481eb51ed35addb71019917239fd308d889ce16b4d8a17f058bbeda" => :yosemite
+    sha256 "e23c2a47b9cd1ab498f951e660d8f2d4a15da97999a51fe9f96c1fd3309825de" => :sierra
+    sha256 "97bbed92c7aa1d9bb1f5753e4fbf5b9645af370c5837ad6c1f9f6ae2a5d59797" => :el_capitan
+    sha256 "edda0f96ca654f020b7ce068a53cdf7de50edb7cfd0d079218275449ec8fc10d" => :yosemite
   end
 
   option :cxx11
