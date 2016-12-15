681431391a741b40145d94cc7161751bb2cb1b67
Shaun Jackman <sjackman@gmail.com>
2016-12-14 16:07:27 -0800

    links-scaffolder: Install BloomFilter Perl module

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -7,11 +7,29 @@ class LinksScaffolder < Formula
   # doi "10.1186/s13742-015-0076-3"
   # tag "bioinformatics"
 
-  bottle :unneeded
-
-  depends_on "Bloom::Faster" => :perl
+  depends_on "swig" => :build
+  depends_on "perl"
 
   def install
+    if OS.mac?
+      # Fix error: no known conversion from 'size_t' (aka 'unsigned long') to 'uint64_t &' (aka 'unsigned long long &')
+      cd "lib/bloomfilter" do
+        inreplace ["BloomFilter.hpp", "RollingHash.h", "RollingHashIterator.h", "swig/BloomFilter.i"],
+          "size_t", "uint64_t"
+      end
+    end
+    cd "lib/bloomfilter/swig" do
+      so = OS.mac? ? "bundle" : "so"
+      perl = Dir[Formula["perl"].lib/"perl5/*/*/CORE"][0]
+      cxxflags = (ENV.cxxflags || "").split
+      system "swig", "-Wall", "-c++", "-perl5", "BloomFilter.i"
+      system ENV.cxx, *cxxflags, "-c", "-fPIC", "-I#{perl}", "BloomFilter_wrap.cxx"
+      system ENV.cxx, *cxxflags, "-shared", "-o", "BloomFilter.#{so}", "BloomFilter_wrap.o", "-L#{perl}", "-lperl"
+      libexec.install "BloomFilter.pm", "BloomFilter.#{so}"
+    end
+
+    inreplace "LINKS", "/usr/bin/perl", "/usr/bin/env perl"
+    inreplace "LINKS", "$FindBin::Bin/./lib/bloomfilter/swig", "$FindBin::RealBin/../libexec"
     bin.install "LINKS"
     chmod 0644, "LINKS-readme.txt"
     doc.install "LINKS-readme.txt"
