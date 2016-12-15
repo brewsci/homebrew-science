5cb163bca1f8956362f3d88ab6c41bbaf678bde4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-12-15 05:01:25 +0000

    canu: update 1.4 bottle for Linuxbrew.

--- canu.rb
+++ canu.rb
@@ -11,6 +11,7 @@ class Canu < Formula
     sha256 "c6eeb78964accfa52f5df247cc07e1ae724a25388897c4cab1cba3039fbe033d" => :sierra
     sha256 "b0aefbb7564f24f07d85557422b709c19ec29c4e491ce0be3415ef08a7a7e290" => :el_capitan
     sha256 "84f57a36a73a45ad302b324d70ce629da0623c97244d296f7a9936b57f349303" => :yosemite
+    sha256 "e3e2e9dcf6661e53567bdff4c7cd5404652ad5572a47a0edf7d307badab3eca4" => :x86_64_linux
   end
 
   # Fix fatal error: 'omp.h' file not found
