55ccd321ec9ecba5aeb4a95c1272c8d4fb746d16
Shaun Jackman <sjackman@gmail.com>
2016-12-14 16:06:24 -0800

    links-scaffolder: Add desc

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -1,11 +1,11 @@
 class LinksScaffolder < Formula
+  desc "Long Interval Nucleotide K-mer Scaffolder"
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/links"
-  # doi "10.1186/s13742-015-0076-3"
-  # tag "bioinformatics"
-
   url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.5.1/links_v1-5-1.tar.gz"
   version "1.5.1"
   sha256 "3f4dbcdd0fc6735ae316d7c379b038a3fada16233a120b2d6f2220d51a24a908"
+  # doi "10.1186/s13742-015-0076-3"
+  # tag "bioinformatics"
 
   bottle :unneeded
 
@@ -15,7 +15,6 @@ class LinksScaffolder < Formula
     bin.install "LINKS"
     chmod 0644, "LINKS-readme.txt"
     doc.install "LINKS-readme.txt"
-    chmod 0644, "LINKS-readme.pdf"
     doc.install "LINKS-readme.pdf"
     prefix.install "test"
     prefix.install "tools"
