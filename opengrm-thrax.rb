b49931f7cab4da60aec4d92c49a7a7e8252e4c14
Steven Bedrick <steve@bedrick.org>
2016-12-15 17:28:23 -0800

    opengrm-thrax: revision for openfst

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -2,6 +2,7 @@ class OpengrmThrax < Formula
   homepage "http://www.openfst.org/twiki/bin/view/GRM/Thrax"
   url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.2.2.tar.gz"
   sha256 "d1744b15ea142e172713c1645451e743a78e1731bb285ec0f4ea5c118d1dcfe7"
+  revision 1
 
   bottle do
     cellar :any
