0748fc97b86544040d0ddbadbdeca7922b6d71a7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-12-16 19:42:38 +0000

    beast2: add 2.4.4 bottle for Linuxbrew.

--- beast2.rb
+++ beast2.rb
@@ -4,15 +4,16 @@ class Beast2 < Formula
   url "https://github.com/CompEvol/beast2/archive/v2.4.4.tar.gz"
   sha256 "1346d359a141723d57c37333ce9d6cbdf0699609e337dbfdcb969ddbff8713c3"
   head "https://github.com/CompEvol/beast2.git"
+  # tag "bioinformatics"
+  # doi "10.1371/journal.pcbi.1003537"
+
   bottle do
     cellar :any_skip_relocation
     sha256 "3f76a7dd57419c9172ea6e23ce691552c2fcd524343b5f640e7bea9f3236a533" => :sierra
     sha256 "b133d0e631901ed5aa9d5fb78d42ee4fc190d05fe3e503e78fd7b1706b99b3a3" => :el_capitan
     sha256 "42336915df95fbf81b5dd4bd4ec12e2d35607b09f07c4b27e2e71c9136f8eff0" => :yosemite
+    sha256 "1352432cf9ddd0a41c8d5fceca229ea4b6ce9e2f841228d5e14b16f31cee0251" => :x86_64_linux
   end
-
-  # tag "bioinformatics"
-  # doi "10.1371/journal.pcbi.1003537"
 
   depends_on :ant => :build
   depends_on :java => "1.8+"
