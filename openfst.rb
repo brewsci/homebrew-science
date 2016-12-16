b1b016d14d7a260ceff93c03c6d6ee271ad4b7e8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-16 19:57:09 +0000

    openfst: update 1.5.4 bottle.

--- openfst.rb
+++ openfst.rb
@@ -6,9 +6,9 @@ class Openfst < Formula
 
   bottle do
     cellar :any
-    sha256 "849391e18b8bd83f1dac534d2de39695f8e2ac86ec52ffc4ca32bf93fa10b193" => :el_capitan
-    sha256 "3e5fab5f10ca685304e3faefabc00ca9a21dfb5d46db03c78eef7226b53d426b" => :yosemite
-    sha256 "bcb3de924944b61d877321c010688eb3997f0d18503d0dc18a7a8144b015357c" => :mavericks
+    sha256 "cfe30e7883dc714d8d43a8e0617434c78ba3338e3f534e7f8858a6b60ce19066" => :sierra
+    sha256 "aab7e1ae09f71d183e432ea84abf841218d77dc0b791b2600c193059c0cacb10" => :el_capitan
+    sha256 "0b4ebfec53234c67e715fffc77ebdf558e477fd4493aca63ff8b96ddae7dc8d1" => :yosemite
   end
 
   needs :cxx11
