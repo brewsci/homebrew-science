dd8cf2827ee469f7b83365684e02a252becbfb6f
ilovezfs <ilovezfs@icloud.com>
2016-12-20 21:35:09 -0800

    opencv 2.4.13.2
    Closes #4742.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- opencv.rb
+++ opencv.rb
@@ -3,23 +3,9 @@ require File.expand_path("../Requirements/cuda_requirement", __FILE__)
 class Opencv < Formula
   desc "Open source computer vision library"
   homepage "http://opencv.org/"
+  url "https://github.com/opencv/opencv/archive/2.4.13.2.tar.gz"
+  sha256 "4b00c110e6c54943cbbb7cf0d35c5bc148133ab2095ee4aaa0ac0a4f67c58080"
   head "https://github.com/opencv/opencv.git", :branch => "2.4"
-
-  stable do
-    url "https://github.com/opencv/opencv/archive/2.4.13.1.tar.gz"
-    sha256 "0d5ce5e0973e3a745f927d1ee097aaf909aae59f787be6d27a03d639e2d96bd7"
-
-    # patch to fix building stable on macOS Sierra and/or with Xcode 8
-    # adds support for AVFoundation and fixes dependencies on removed headers
-    # https://github.com/opencv/opencv/issues/6913 (applies to 2.4 branch as well)
-    # can be removed with next release
-    if !DevelopmentTools.clang_build_version.nil? && DevelopmentTools.clang_build_version >= 800
-      patch do
-        url "https://github.com/opencv/opencv/commit/9ff63a46fcfe784e6465320af80624a53a98ccaa.diff"
-        sha256 "33dd03572c40d2c0ce41910be6318fab1350f72d958f850941aad019dfb67de5"
-      end
-    end
-  end
 
   bottle do
     rebuild 1
@@ -28,17 +14,16 @@ class Opencv < Formula
     sha256 "7c5a86df6e270135297dd74f4218ca899a80666ed613300916011ac5a7971f7e" => :yosemite
   end
 
-  option "32-bit"
   option "with-java", "Build with Java support"
   option "with-tbb", "Enable parallel code in OpenCV using Intel TBB"
-  option "without-test", "Build without accuracy & performance tests"
+  option "without-test", "Build without accuracy and performance tests"
   option "without-opencl", "Disable GPU code in OpenCV using OpenCL"
   option "with-opengl", "Build with OpenGL support"
   option "with-ximea", "Build with XIMEA support"
-  option "without-numpy", "Use a numpy you've installed yourself instead of a Homebrew-packaged numpy"
-  option "without-python", "Build without Python support"
+  option "without-numpy", "Use your own numpy instead of Homebrew's numpy"
+  option "without-python", "Build without python support"
 
-  if DevelopmentTools.clang_build_version.nil? || DevelopmentTools.clang_build_version < 800
+  if DevelopmentTools.clang_build_version < 800
     option "with-quicktime", "Use QuickTime for Video I/O"
   end
 
@@ -48,26 +33,25 @@ class Opencv < Formula
   option :cxx11
   option :universal
 
-  depends_on :ant if build.with? "java"
-  depends_on "cmake"      => :build
-  depends_on CudaRequirement => :optional
-  depends_on "eigen"      => :recommended
-  depends_on "gstreamer"  => :optional
-  depends_on "gst-plugins-good" if build.with? "gstreamer"
-  depends_on "jasper"     => :optional
-  depends_on :java        => :optional
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on "libdc1394"  => :optional
-  depends_on "openexr"    => :recommended
-  depends_on "openni"     => :optional
-  depends_on "pkg-config" => :build
-  depends_on "tbb"        => :optional
-  depends_on "vtk"        => :optional
-
-  depends_on :python => :recommended unless OS.mac? && MacOS.version > :snow_leopard
+  depends_on "eigen" => :recommended
+  depends_on "openexr" => :recommended
+  depends_on :python => :recommended if MacOS.version <= :snow_leopard || !OS.mac?
   depends_on "homebrew/python/numpy" => :recommended if build.with? "python"
+  depends_on CudaRequirement => :optional
+  depends_on "jasper" => :optional
+  depends_on :java => :optional
+  depends_on :ant if build.with? "java"
+  depends_on "gstreamer" => :optional
+  depends_on "gst-plugins-good" if build.with? "gstreamer"
+  depends_on "libdc1394" => :optional
+  depends_on "openni" => :optional
+  depends_on "tbb" => :optional
+  depends_on "vtk" => :optional
 
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
@@ -97,23 +81,24 @@ class Opencv < Formula
     args << "-DBUILD_TESTS=OFF" << "-DBUILD_PERF_TESTS=OFF" if build.without? "test"
     args << "-DBUILD_opencv_python=" + arg_switch("python")
     args << "-DBUILD_opencv_java=" + arg_switch("java")
-    args << "-DWITH_OPENEXR="   + arg_switch("openexr")
-    args << "-DWITH_EIGEN="     + arg_switch("eigen")
-    args << "-DWITH_TBB="       + arg_switch("tbb")
-    args << "-DWITH_FFMPEG="    + arg_switch("ffmpeg")
+    args << "-DWITH_OPENEXR=" + arg_switch("openexr")
+    args << "-DWITH_EIGEN=" + arg_switch("eigen")
+    args << "-DWITH_TBB=" + arg_switch("tbb")
+    args << "-DWITH_FFMPEG=" + arg_switch("ffmpeg")
     args << "-DWITH_QUICKTIME=" + arg_switch("quicktime")
-    args << "-DWITH_1394="      + arg_switch("libdc1394")
-    args << "-DWITH_OPENGL="    + arg_switch("opengl")
-    args << "-DWITH_JASPER="    + arg_switch("jasper")
+    args << "-DWITH_1394=" + arg_switch("libdc1394")
+    args << "-DWITH_OPENGL=" + arg_switch("opengl")
+    args << "-DWITH_JASPER=" + arg_switch("jasper")
     args << "-DWITH_GSTREAMER=" + arg_switch("gstreamer")
-    args << "-DWITH_XIMEA="     + arg_switch("ximea")
-    args << "-DWITH_VTK="       + arg_switch("vtk")
+    args << "-DWITH_XIMEA=" + arg_switch("ximea")
+    args << "-DWITH_VTK=" + arg_switch("vtk")
 
     if build.with? "python"
       py_prefix = `python-config --prefix`.chomp
       py_lib = OS.linux? ? `python-config --configdir`.chomp : "#{py_prefix}/lib"
       args << "-DPYTHON_LIBRARY=#{py_lib}/libpython2.7.#{dylib}"
       args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python2.7"
+
       # Make sure find_program locates system Python
       # https://github.com/Homebrew/homebrew-science/issues/2302
       args << "-DCMAKE_PREFIX_PATH=#{py_prefix}" if OS.mac?
@@ -133,6 +118,7 @@ class Opencv < Formula
 
     if build.with? "openni"
       args << "-DWITH_OPENNI=ON"
+
       # Set proper path for Homebrew's openni
       inreplace "cmake/OpenCVFindOpenNI.cmake" do |s|
         s.gsub! "/usr/include/ni", Formula["openni"].opt_include/"ni"
@@ -140,7 +126,7 @@ class Opencv < Formula
       end
     end
 
-    if build.include? "32-bit"
+    unless MacOS.prefer_64_bit?
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
       args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
       args << "-DOPENCV_EXTRA_CXX_FLAGS='-arch i386 -m32'"
@@ -176,8 +162,7 @@ class Opencv < Formula
       }
     EOS
     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
-    assert_equal `./test`.strip, version.to_s
-
+    assert_equal version.to_s, shell_output("./test").strip
     assert_match version.to_s, shell_output("python -c 'import cv2; print(cv2.__version__)'")
   end
 end
