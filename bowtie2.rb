aa68a8bca2096fd07ca6d6cb5455ecd1cc45cf2d
ilovezfs <ilovezfs@icloud.com>
2016-12-23 01:17:18 -0800

    bowtie2 2.3.0
    Closes #4753.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- bowtie2.rb
+++ bowtie2.rb
@@ -1,13 +1,23 @@
 class Bowtie2 < Formula
   desc "Fast and sensitive gapped read aligner"
-  homepage "http://bowtie-bio.sf.net/bowtie2"
+  homepage "https://bowtie-bio.sourceforge.io/"
   # doi "10.1038/nmeth.1923"
   # tag "bioinformatics"
-
-  url "https://github.com/BenLangmead/bowtie2/archive/v2.2.9.tar.gz"
-  sha256 "af940f77fc36aabea90d3d865724fd7ec7e250788d2d2f793c45c713d16ae5ee"
-
   head "https://github.com/BenLangmead/bowtie2.git"
+
+  stable do
+    url "https://github.com/BenLangmead/bowtie2/archive/v2.3.0.tar.gz"
+    sha256 "9804fddf36233f3f92c11e2250224de3395790cf35c8280c66387075df078221"
+
+    # Remove for > 2.3.0
+    # Prevents tophat test from failing
+    # Upstream commit from 7 Feb 2017 "Prevent aligner from failing when given
+    # empty FASTQ read files"
+    patch do
+      url "https://github.com/BenLangmead/bowtie2/commit/2e9b960.patch"
+      sha256 "074b07ac7b1d1a79469df30e557ea8d8739463298a66b8cee888e26dfde7e4fc"
+    end
+  end
 
   bottle do
     cellar :any
@@ -33,7 +43,7 @@ class Bowtie2 < Formula
   end
 
   test do
-    system "bowtie2-build", "#{pkgshare}/example/reference/lambda_virus.fa", "lambda_virus"
+    system "#{bin}/bowtie2-build", "#{pkgshare}/example/reference/lambda_virus.fa", "lambda_virus"
     assert File.exist?("lambda_virus.1.bt2")
   end
 end
