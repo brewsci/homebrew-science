28c0a8d16bc23f2200f94df646bc3c0fbf7ea959
ilovezfs <ilovezfs@icloud.com>
2016-12-24 12:21:31 -0800

    pykep 1.3.9
    Closes #4762.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- pykep.rb
+++ pykep.rb
@@ -1,9 +1,21 @@
 class Pykep < Formula
+  desc "Library providing basic tools for astrodynamics research"
   homepage "https://esa.github.io/pykep/"
-  url "https://github.com/esa/pykep/archive/1.2.1.tar.gz"
-  sha256 "e297ea58079fe9d766215f20749d32ec57b4edc91ec1e83cd19ca5c212301608"
   head "https://github.com/esa/pykep.git"
-  revision 1
+
+  stable do
+    url "https://github.com/esa/pykep/archive/v1.3.9.tar.gz"
+    sha256 "1bb0a71a1d19908e3703ca6e3cc87d75f2f8b91c65c3a778e1acd1962c1e87f1"
+
+    # Remove for > 1.3.9
+    # Upstream commit from 8 Feb 2017 "fix for #57 - linking to python libraries
+    # only for windows now"
+    # https://github.com/esa/pykep/commit/351b628bf404ed53b3ae3e06eb8bdc56dc1deb11
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/d42dc14/pykep/nopythonlinking.diff"
+      sha256 "6b2372c77b0e4062145d4bed6f372d3f04283c2ab58014f8994950832e56099e"
+    end
+  end
 
   bottle do
     cellar :any
@@ -21,6 +33,7 @@ class Pykep < Formula
     args = std_cmake_args + [
       "-DBUILD_TESTS=ON",
       "-DBUILD_PYKEP=ON",
+      "-DBUILD_SPICE=ON",
       "-DPYTHON_MODULES_DIR=#{lib}/python2.7/site-packages",
     ]
 
