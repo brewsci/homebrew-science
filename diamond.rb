8a782d52d6dd7830ff3f2598ec2b16a9784f945b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-26 18:17:08 +0000

    diamond: update 0.8.30 bottle.

--- diamond.rb
+++ diamond.rb
@@ -9,10 +9,9 @@ class Diamond < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f30dee7a6d6418d5f0041ef94d3fbfdf3d27f883a6732c8cdd827bfe52438156" => :sierra
-    sha256 "978a6e75f38066f2a1d0aa873aedfb5f4f0dd8e06f57c31121b1472bd1bb3830" => :el_capitan
-    sha256 "632c72577efc7dd89261af23ffc9b4559b6aad69fb9b318ebb13c9758679cb2b" => :yosemite
-    sha256 "a14c940f6b8d641954c9bd066231b249d96943642f8608919f6aa8dd17236288" => :x86_64_linux
+    sha256 "ef0b3e494a54ebe0babc20306337d243056b92ceef342dfd4c90aaaf874d232d" => :sierra
+    sha256 "ec6c1681277de4e1ee422a7a86c44480b3a5d95339f021b017ca73bc27b65518" => :el_capitan
+    sha256 "a30619046985c5d61cd2813efec10a881cd29c397d29f161a1cea7b16767be75" => :yosemite
   end
 
   depends_on "cmake" => :build
