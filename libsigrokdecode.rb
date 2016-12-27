71ae86e0909b6d9bb527e09dcc5d448af7836923
ilovezfs <ilovezfs@icloud.com>
2016-12-26 23:10:13 -0800

    libsigrokdecode 0.4.1
    Closes #4770.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- libsigrokdecode.rb
+++ libsigrokdecode.rb
@@ -3,8 +3,8 @@ class Libsigrokdecode < Formula
   homepage "http://sigrok.org/"
 
   stable do
-    url "http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.4.0.tar.gz"
-    sha256 "fd7e9d1b73245e844ead97a16d5321c766196f946c9b28a8646cab2e98ec3537"
+    url "http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.4.1.tar.gz"
+    sha256 "065f70c10971173d86e03a8cf5534e600c7a622775113997d8061572135d4a95"
 
     resource "librevisa" do
       url "http://www.librevisa.org/git/librevisa.git", :tag => "alpha-2013-08-12",
@@ -61,7 +61,7 @@ class Libsigrokdecode < Formula
   depends_on :python3
   depends_on "check"    => :optional
   depends_on "libftdi0" => :optional
-  depends_on "libusb"   => :recommended
+  depends_on "libusb"   => :optional
 
   # currently needed for librevisa due to errors in released alpha build
   if build.with?("librevisa")
@@ -80,20 +80,24 @@ class Libsigrokdecode < Formula
     ENV.append_path "PKG_CONFIG_PATH", lib / "pkgconfig"
     ENV.append_path "PKG_CONFIG_PATH", HOMEBREW_PREFIX / "Frameworks/Python.framework/Versions/3.4/lib/pkgconfig"
 
-    resource("libserialport").stage do
-      system "./autogen.sh" if build.head?
-      system "./configure", *common_args
-      system "make", "install"
-    end if build.with? "libserialport"
+    if build.with? "libserialport"
+      resource("libserialport").stage do
+        system "./autogen.sh" if build.head?
+        system "./configure", *common_args
+        system "make", "install"
+      end
+    end
 
-    resource("librevisa").stage do
-      # needed for 2013-08-12. fixed on HEAD
-      # patch version https://gist.github.com/tduehr/3b4257251c7ed720225c
-      inreplace "autogen.sh", "libtoolize", "glibtoolize" unless build.head?
-      system "./autogen.sh"
-      system "./configure", *common_args
-      system "make", "install"
-    end if build.with? "librevisa"
+    if build.with? "librevisa"
+      resource("librevisa").stage do
+        # needed for 2013-08-12. fixed on HEAD
+        # patch version https://gist.github.com/tduehr/3b4257251c7ed720225c
+        inreplace "autogen.sh", "libtoolize", "glibtoolize" unless build.head?
+        system "./autogen.sh"
+        system "./configure", *common_args
+        system "make", "install"
+      end
+    end
 
     # not strictly needed for libsigrokdecode but both sigrok-cli and pulseview
     # need it so we install it here as a resource to avoid collisions
