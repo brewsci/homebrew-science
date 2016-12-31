c2f81159494109f1ad5d4c89b8b261b514b90c78
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-12-31 00:43:10 +0000

    biobloomtools: update 2.0.13_1 bottle for Linuxbrew.

--- biobloomtools.rb
+++ biobloomtools.rb
@@ -13,6 +13,7 @@ class Biobloomtools < Formula
     sha256 "ed8ee8bd6bb5248e5b22fda73cfcc938c65a30e4a99dc61b2a04427497506565" => :sierra
     sha256 "d7d05e645f14ab8ec5ea2027258be5fe2efb5396acca583c7a2616a6b8aa131f" => :el_capitan
     sha256 "27200effe2f895332b0fb0e35681652364df2999393d3fc427517fc21bd429e0" => :yosemite
+    sha256 "fa9992d8cb709770d8684a2b0bc8b35cb208a3f9a2e8d128f3d078ec929fec80" => :x86_64_linux
   end
 
   head do
