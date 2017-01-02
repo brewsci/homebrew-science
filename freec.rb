8d33cc65679196aa448be7e7b8c525bc86b6c10e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-02 22:04:22 +0000

    freec: update 10.3 bottle for Linuxbrew.

--- freec.rb
+++ freec.rb
@@ -12,6 +12,7 @@ class Freec < Formula
     sha256 "176ac03b194be0da3b1403ffa9b1c877344fc7f48586296cb0f4799de2b25bb1" => :sierra
     sha256 "40afc40855875d9f134ba5bff417ea26a245f2c6c9c402dac2909192c6ef7f09" => :el_capitan
     sha256 "76959d16cea8df6eaf0f2c31cb46197e6f168c03a9d09535bceacea23f9b8211" => :yosemite
+    sha256 "661683a53de278c90947606c7ebbc4382e3a75a4f19ff4d3cd1fde2a771bfdd1" => :x86_64_linux
   end
 
   # Patch to fix builds on macOS. Will be present in next release (>10.3)
