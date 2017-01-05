48307cac69cb9d744f5a46a7709b26c942586c8b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-05 02:26:33 +0000

    ncview: update 2.1.7_2 bottle for Linuxbrew.

--- ncview.rb
+++ ncview.rb
@@ -10,6 +10,7 @@ class Ncview < Formula
     sha256 "699f3ffdf7ffab5ce8f8f6eb47728af1a4584600b4b1641b975592b4364430ae" => :sierra
     sha256 "b51050ba2fda573b144999fc7eed547debaad78a73f867c951beb51abff59e17" => :el_capitan
     sha256 "623fb9f94303ec78c952461dd1498a1c43155c38f09cb5d486a7ee6ff2d2d11c" => :yosemite
+    sha256 "de8b243eb5287debcc7d377b2a4218ef690d673c1632ad60b8bface73f0a8998" => :x86_64_linux
   end
 
   depends_on :x11
