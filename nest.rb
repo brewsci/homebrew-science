d8bd6b77c1c6e888e3f2c74e5a0a84708ca83672
Tammo Ippen <tammo.ippen@posteo.de>
2017-01-09 10:34:47 +0100

    nest 2.12.0
    Update to latest version nest 2.12.0
    
    - Remove (unnecessary) patching.
    - Add python 3 option.
    - Link to correct python 2 libraries.
    - Remove autotools build chain, as we moved to cmake.
    - Some audit cleanups.
    
    Closes #5209.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- nest.rb
+++ nest.rb
@@ -1,9 +1,8 @@
 class Nest < Formula
   desc "The Neural Simulation Tool"
   homepage "http://www.nest-simulator.org/"
-  url "https://github.com/nest/nest-simulator/releases/download/v2.10.0/nest-2.10.0.tar.gz"
-  sha256 "2b6fc562cd6362e812d94bb742562a5a685fb1c7e08403765dbe123d59b0996c"
-  revision 3
+  url "https://github.com/nest/nest-simulator/releases/download/v2.12.0/nest-2.12.0.tar.gz"
+  sha256 "6b5ca7680f758bdd44047e23faea5708b2de2e253574807b1ba98dbcdf1ea813"
 
   head "https://github.com/nest/nest-simulator.git"
 
@@ -13,29 +12,36 @@ class Nest < Formula
     sha256 "b477d20586f143fd4cc7967c1006670b9aac9edbc96c13b9653fc4342e76535f" => :yosemite
   end
 
-  stable do
-    # Fix stable build with GCC 6
-    # https://github.com/nest/nest-simulator/pull/389
-    patch :DATA
-  end
-
-  option "with-python", "Build Python bindings (PyNEST)."
+  option "with-python", "Build Python2 bindings (PyNEST)."
+  option "with-python3", "Build Python3 bindings (PyNEST, precedence over --with-python)."
   option "without-openmp", "Build without OpenMP support."
   needs :openmp if build.with? "openmp"
 
   depends_on "gsl" => :recommended
   depends_on :mpi => [:optional, :cc, :cxx]
-  depends_on :python => :optional if MacOS.version <= :snow_leopard
-  depends_on "numpy" => :python if build.with? "python"
-  depends_on "scipy" => :python if build.with? "python"
-  depends_on "matplotlib" => :python if build.with? "python"
-  depends_on "cython" => :python if build.with? "python"
-  depends_on "nose" => :python if build.with? "python"
+
+  # Any Python >= 2.7 < 3.x is okay (either from macOS or brewed)
+  depends_on :python => :optional
+  if build.with? "python"
+    depends_on "numpy"
+    depends_on "scipy"
+    depends_on "matplotlib"
+    depends_on "cython" => [:python, :build]
+    depends_on "nose" => :python
+  end
+
+  depends_on :python3 => :optional
+  if build.with? "python3"
+    depends_on "numpy"
+    depends_on "scipy"
+    depends_on "matplotlib"
+    depends_on "cython" => [:python3, :build]
+    depends_on "nose" => :python3
+  end
+
   depends_on "libtool" => :run
   depends_on "readline" => :run
-  depends_on "autoconf" => :build unless build.head?
-  depends_on "automake" => :build unless build.head?
-  depends_on "cmake" => :build if build.head?
+  depends_on "cmake" => :build
 
   fails_with :clang do
     cause <<-EOS.undent
@@ -43,68 +49,36 @@ class Nest < Formula
     EOS
   end
 
-  env :std
-
   def install
     ENV.delete("CFLAGS")
     ENV.delete("CXXFLAGS")
 
-    if build.head?
-      args = ["-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}"]
+    args = ["-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}"]
 
-      args << "-Dwith-mpi=ON" if build.with? "mpi"
-      args << "-Dwith-openmp=OFF" if build.without? "openmp"
-      args << "-Dwith-gsl=OFF" if build.without? "gsl"
-      args << "-Dwith-python=OFF" if build.without? "python"
+    args << "-Dwith-mpi=ON" if build.with? "mpi"
+    args << "-Dwith-openmp=OFF" if build.without? "openmp"
+    args << "-Dwith-gsl=OFF" if build.without? "gsl"
 
-      # "out of source" build
-      mkdir "build" do
-        system "cmake", "..", *args
-        system "make"
-        system "make", "install"
-      end
-      return
+    if build.with? "python3"
+      args << "-Dwith-python=3"
+    elsif build.with? "python"
+      dylib = OS.mac? ? "dylib" : "so"
+      py_prefix = `python-config --prefix`.chomp
+      py_lib = "#{py_prefix}/lib"
+
+      args << "-Dwith-python=2"
+      args << "-DPYTHON_LIBRARY=#{py_lib}/libpython2.7.#{dylib}"
+      args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python2.7"
+    else
+      args << "-Dwith-python=OFF"
     end
-
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}",
-           ]
-
-    if build.with? "mpi"
-      # change CC / CXX in open-mpi
-      ENV["OMPI_CC"] = ENV["CC"]
-      ENV["OMPI_CXX"] = ENV["CXX"]
-
-      # change CC / CXX in mpich
-      ENV["MPICH_CC"] = ENV["CC"]
-      ENV["MPICH_CXX"] = ENV["CXX"]
-
-      args << "CC=#{ENV["MPICC"]}"
-      args << "CXX=#{ENV["MPICXX"]}"
-      args << "--with-mpi"
-    end
-
-    args << "--without-openmp" if build.without? "openmp"
-    args << "--without-gsl" if build.without? "gsl"
-    args << "--without-python" if build.without? "python"
 
     # "out of source" build
     mkdir "build" do
-      system "../configure", *args
-      # adjust src and bld path
-      inreplace "../sli/slistartup.cc", /PKGSOURCEDIR/, "\"#{pkgshare}/sources\""
-      inreplace "libnestutil/sliconfig.h", /#define SLI_BUILDDIR .*/, "#define SLI_BUILDDIR \"#{pkgshare}/sources/build\""
-      # do not re-generate .hlp during /validate (tries to regenerate from
-      # not existing source file)
-      inreplace "../lib/sli/helpinit.sli", /^ makehelp$/, "% makehelp"
+      system "cmake", "..", *args
       system "make"
       system "make", "install"
     end
-
-    # install sources for later testing
-    mkdir pkgshare/"sources"
-    (pkgshare/"sources").install Dir["./*"]
   end
 
   test do
@@ -125,46 +99,25 @@ class Nest < Formula
     if build.with? "mpi"
       # we need the command /mpirun defined for the mpi tests
       # and since we are in the sandbox, we create it again
-      nestrc = %{
+      nestrc = <<-EOS
         /mpirun
-        [/integertype /stringtype]
-        [/numproc     /slifile]
+        [/integertype /stringtype /stringtype]
+        [/numproc     /executable /scriptfile]
         {
          () [
-          (mpirun -np ) numproc cvs ( ) statusdict/prefix :: (/bin/nest )  slifile
+          (mpirun -np ) numproc cvs ( ) executable ( ) scriptfile
          ] {join} Fold
         } Function def
-      }
+      EOS
       File.open(ENV["HOME"]+"/.nestrc", "w") { |file| file.write(nestrc) }
     end
 
     # run all tests
     args = []
-    args << "--test-pynest" if build.with? "python"
+    args << "--test-pynest" if build.with?("python") || build.with?("python3")
+    if build.with? "python3"
+      ENV["PYTHON"] = "python3"
+    end
     system pkgshare/"extras/do_tests.sh", *args
   end
 end
-__END__
-diff --git a/sli/slistartup.cc b/sli/slistartup.cc
-index f485c5c..28174ef 100644
---- a/sli/slistartup.cc
-+++ b/sli/slistartup.cc
-@@ -65,14 +65,17 @@ SLIStartup::checkpath( std::string const& path, std::string& result ) const
-   const std::string fullname = fullpath + "/" + startupfilename;
-
-   std::ifstream in( fullname.c_str() );
--  if ( in )
-+
-+  if ( in.good() )
-   {
-     result = fullname;
-+    return true;
-   }
-   else
-+  {
-     result.erase();
--
--  return ( in );
-+    return false;
-+  }
- }
