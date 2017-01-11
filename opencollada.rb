656cfdfc3f4f20c8d11b58e21cfc623edf52be3f
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2017-01-11 14:07:42 -0600

    opencollada 1.6.36 (new formula)
    Closes #4844.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

---
+++ opencollada.rb
@@ -0,0 +1,21 @@
+class Opencollada < Formula
+  desc "Stream based reader and writer library for COLLADA files"
+  homepage "http://www.opencollada.org"
+  url "https://github.com/KhronosGroup/OpenCOLLADA/archive/v1.6.36.tar.gz"
+  sha256 "774b41611ab3ebb3a6008d32bcfd326d4ac1545d5316ba383b4ae90f16335701"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+      prefix.install "bin"
+      Dir.glob("#{bin}/*.xsd") { |p| rm p }
+    end
+  end
+
+  test do
+    system "#{bin}/OpenCOLLADAValidator"
+  end
+end
