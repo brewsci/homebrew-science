c42fb24515d795f0821dcc461e8f9150db51c488
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-11 22:16:45 +0000

    ntcard: add 1.0.0 bottle for Linuxbrew.

--- ntcard.rb
+++ ntcard.rb
@@ -12,6 +12,7 @@ class Ntcard < Formula
     sha256 "2e2ed0de2a798d41993829e8cc0a128ba33a39898971f0366e0e50f39139fb82" => :sierra
     sha256 "4c6e1e6b55c0a7bd1b478c694fe17c3e46fe1fe3e396776f9123dd59651978e5" => :el_capitan
     sha256 "56987a2d89be2345a3f474208e4ddaba96d28f8a09a598076b386f8e7bf7d907" => :yosemite
+    sha256 "a5f230239b82190af955086c0bb1577c1d3fed240f5f0987f9f1b0bfe74a654a" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
