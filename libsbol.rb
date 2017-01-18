05a111cc1317ab1540be0dc3e17d7bfb47e3592b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-18 22:36:59 +0000

    libsbol: add 2.1.1 bottle.

--- libsbol.rb
+++ libsbol.rb
@@ -4,6 +4,13 @@ class Libsbol < Formula
   url "https://github.com/SynBioDex/libSBOL/archive/v2.1.1.tar.gz"
   sha256 "66bb18d752f98f4ea53f774de7d28dfea41b8c8896bd01bce1ee1231f9cc7249"
 
+  bottle do
+    cellar :any
+    sha256 "cf84415dc145a1a160ff8b5ee22a54115837e6d5670b8adb15f011bf64a1180c" => :sierra
+    sha256 "b3a43c8f06bbc09916e9c270068a9aae2d93f443f39d3f2299fb675dd335047d" => :el_capitan
+    sha256 "830fd43ff3c55cb6de8629b9fad79ffc1cf3a783a8c2c41026e4f9fa0dd42279" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "jsoncpp"
