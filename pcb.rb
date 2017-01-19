ab94e09784525770c3d25a857a20b781f5dfabf7
ilovezfs <ilovezfs@icloud.com>
2017-01-19 06:13:01 -0800

    pcb 4.0.0
    Closes #4876.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- pcb.rb
+++ pcb.rb
@@ -1,10 +1,9 @@
 class Pcb < Formula
   desc "Interactive printed circuit board editor"
   homepage "http://pcb.geda-project.org/"
-  url "https://downloads.sourceforge.net/project/pcb/pcb/pcb-20140316/pcb-20140316.tar.gz"
-  sha256 "82c4f39438ee4e278196a3b67ef021145dcfbb00519508ccf51aa7832121c950"
-  revision 3
-
+  url "https://downloads.sourceforge.net/project/pcb/pcb/pcb-4.0.0/pcb-4.0.0.tar.gz"
+  sha256 "8ede13acd0bdc1bc3c347b603718a92c5420b63f80724182ae5b25881e9adac4"
+  version_scheme 1
   head "git://git.geda-project.org/pcb.git"
 
   bottle do
@@ -14,7 +13,7 @@ class Pcb < Formula
   end
 
   option "with-doc", "Build the documentation (requires LaTeX)."
-  option "without-opengl", "Configure pcb without OpenGL, may fix garbled screen."
+  option "with-opengl", "Configure pcb with OpenGL, may cause garbled screen."
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -27,12 +26,21 @@ class Pcb < Formula
   depends_on "glib"
   depends_on "gtkglext"
   depends_on :tex if build.with? "doc"
+  depends_on "perl" if OS.linux?
 
   conflicts_with "gts", :because => "both install `include/gts.h`"
 
+  # There's a missing define from GLU. Defining this fixes everything up.
   patch :DATA
 
   def install
+    if OS.mac? && build.with?("opengl")
+      inreplace ["src/hid/common/hidgl.c", "src/hid/gtk/gtkhid-gl.c"] do |s|
+        s.gsub! "#  include <GL/gl.h>", "#  include <gl.h>"
+        s.gsub! "#  include <GL/glu.h>", "#  include <glu.h>", false
+      end
+    end
+
     system "./autogen.sh" if build.head?
 
     args = %W[
@@ -44,6 +52,7 @@ class Pcb < Formula
     ]
     args << "--disable-doc" if build.without? "doc"
     args << "--disable-gl" if build.without? "opengl"
+    args << "--without-x" if OS.mac?
 
     system "./configure", *args
     system "make"
@@ -55,13 +64,12 @@ class Pcb < Formula
   end
 end
 
-# There's a missing define from GLU. Defining this fixes everything up.
 __END__
 diff --git a/src/hid/common/hidgl.c b/src/hid/common/hidgl.c
-index 15273a6..ff73ca7 100644
+index 93c095d..8f58742 100644
 --- a/src/hid/common/hidgl.c
 +++ b/src/hid/common/hidgl.c
-@@ -66,6 +66,7 @@
+@@ -78,6 +78,7 @@
  #include <dmalloc.h>
  #endif
 
