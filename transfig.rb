82dab77696b8b39369b0ce49f8c1ac9ff299d994
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-19 15:24:10 +0000

    transfig: update 3.2.6a bottle for Linuxbrew.

--- transfig.rb
+++ transfig.rb
@@ -8,6 +8,7 @@ class Transfig < Formula
     sha256 "d316d4d0adf33708b08bec8e9e1087d3043b08a43c6e3a7d9729b4d62c6b3e78" => :sierra
     sha256 "70dd98beb1910d774b6a1734f8bdcbdc07a9d6e2ddf8c0534a5d1e8680472442" => :el_capitan
     sha256 "0bf504206ba10a08d2841e176488b3f0981ee03f621ffd67e19fe5d78b072dca" => :yosemite
+    sha256 "78b384f95a560aab4113e4d1bc4f5a3a9802f4ef200873b5048dfa0df79cc741" => :x86_64_linux
   end
 
   depends_on :x11
