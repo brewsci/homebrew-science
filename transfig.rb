e37013734d3ce9a1de2750bb3f49992bc62eff52
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-19 17:17:25 +0000

    transfig: update 3.2.6a bottle.

--- transfig.rb
+++ transfig.rb
@@ -5,10 +5,9 @@ class Transfig < Formula
   sha256 "5e61a3d9a4f83db4b3199ee82dd54bb65b544369f1e8e38a2606c44cf71667a7"
 
   bottle do
-    rebuild 1
-    sha256 "2944eaf1866357692c24860bab0f9402735b5b4a4909b261823d7fee0af13fdf" => :sierra
-    sha256 "d4a41e9264a7510c87de63da318274dae9962f1c2c511d12a47b71a1f39a522d" => :el_capitan
-    sha256 "3cfb26aec7f9020c90a81e2ac93d54228c5cf3fe117a4c51169289ba04da7404" => :yosemite
+    sha256 "d316d4d0adf33708b08bec8e9e1087d3043b08a43c6e3a7d9729b4d62c6b3e78" => :sierra
+    sha256 "70dd98beb1910d774b6a1734f8bdcbdc07a9d6e2ddf8c0534a5d1e8680472442" => :el_capitan
+    sha256 "0bf504206ba10a08d2841e176488b3f0981ee03f621ffd67e19fe5d78b072dca" => :yosemite
   end
 
   depends_on :x11
