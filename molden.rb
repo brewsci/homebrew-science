4e815a0cba30bf97648c21cc8d5fdadb058e7bd5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-23 10:29:05 +0000

    molden: add 5.7 bottle.

--- molden.rb
+++ molden.rb
@@ -4,6 +4,13 @@ class Molden < Formula
   url "ftp://ftp.cmbi.ru.nl/pub/molgraph/molden/molden5.7.tar.gz"
   sha256 "8c52229f1762e987995c53936f0dc2cfd086277ec777bf9083d76c0bf0483887"
 
+  bottle do
+    cellar :any
+    sha256 "52ef22a806997d35e9d981c1b779ed9c0834eba9e3b9d9c32741b990cab59959" => :sierra
+    sha256 "c4a03bcfac4651420a5a626b95b962333045904c91379fe41f3acd88a8660299" => :el_capitan
+    sha256 "792ce5702cf158143210a46cde9d15e8301cc8005f27daf4010efc774497b85f" => :yosemite
+  end
+
   depends_on :x11
   depends_on :fortran
 
