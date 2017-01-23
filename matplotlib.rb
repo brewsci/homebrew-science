8ed9ebe4bcca04a37c09bbcee0d0081e15d5f642
Mike McQuaid <mike@mikemcquaid.com>
2017-01-23 20:55:24 +0000

    matplotlib: import from homebrew/python.

---
+++ matplotlib.rb
@@ -0,0 +1,192 @@
+class DvipngRequirement < Requirement
+  fatal false
+  cask "mactex"
+
+  satisfy { which("dvipng") }
+
+  def message
+    s = <<-EOS.undent
+      `dvipng` not found. This is optional for Matplotlib.
+    EOS
+    s += super
+    s
+  end
+end
+
+class NoExternalPyCXXPackage < Requirement
+  fatal false
+
+  satisfy do
+    !quiet_system "python", "-c", "import CXX"
+  end
+
+  def message; <<-EOS.undent
+    *** Warning, PyCXX detected! ***
+    On your system, there is already a PyCXX version installed, that will
+    probably make the build of Matplotlib fail. In python you can test if that
+    package is available with `import CXX`. To get a hint where that package
+    is installed, you can:
+        python -c "import os; import CXX; print(os.path.dirname(CXX.__file__))"
+    See also: https://github.com/Homebrew/homebrew-python/issues/56
+    EOS
+  end
+end
+
+class Matplotlib < Formula
+  desc "Python 2D plotting library"
+  homepage "http://matplotlib.org"
+  url "https://files.pythonhosted.org/packages/79/a9/db688816150a6ef91fd9ce284c828467f7271c7dd5982753a73a8e1aaafa/matplotlib-2.0.0.tar.gz"
+  sha256 "36cf0985829c1ab2b8b1dae5e2272e53ae681bf33ab8bedceed4f0565af5f813"
+  head "https://github.com/matplotlib/matplotlib.git"
+
+  option "without-python", "Build without python2 support"
+
+  depends_on :python => :recommended if MacOS.version <= :snow_leopard
+  depends_on :python3 => :optional
+
+  requires_py2 = []
+  requires_py2 << "with-python" if build.with? "python"
+  requires_py3 = []
+  requires_py3 << "with-python3" if build.with? "python3"
+
+  option "with-cairo", "Build with cairo backend support"
+  option "with-pygtk", "Build with pygtk backend support (python2 only)"
+  deprecated_option "with-gtk3" => "with-gtk+3"
+
+  depends_on NoExternalPyCXXPackage => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "numpy" => requires_py3
+  depends_on "ghostscript" => :optional
+  depends_on "homebrew/dupes/tcl-tk" => :optional
+
+  if build.with? "cairo"
+    depends_on "py2cairo" if build.with? "python"
+    depends_on "py3cairo" if build.with? "python3"
+  end
+
+  depends_on "gtk+3" => :optional
+  depends_on "pygobject3" => requires_py3 if build.with? "gtk+3"
+
+  depends_on "pygtk" => :optional
+  depends_on "pygobject" if build.with? "pygtk"
+
+  depends_on "pyqt5" => [:optional] + requires_py2
+
+  depends_on :tex => :optional
+  depends_on DvipngRequirement if build.with? "tex"
+
+  cxxstdlib_check :skip
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/dc/8c/7c9869454bdc53e72fb87ace63eac39336879eef6f2bf96e946edbf03e90/setuptools-33.1.1.zip"
+    sha256 "6b20352ed60ba08c43b3611bdb502286f7a869fbfcf472f40d7279f1e77de145"
+  end
+
+  resource "Cycler" do
+    url "https://files.pythonhosted.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
+  end
+
+  resource "functools32" do
+    url "https://files.pythonhosted.org/packages/c5/60/6ac26ad05857c601308d8fb9e87fa36d0ebf889423f47c3502ef034365db/functools32-3.2.3-2.tar.gz"
+    sha256 "f6253dfbe0538ad2e387bd8fdfd9293c925d63553f5813c4e587745416501e6d"
+  end
+
+  resource "numpy" do
+    url "https://files.pythonhosted.org/packages/b7/9d/8209e555ea5eb8209855b6c9e60ea80119dab5eff5564330b35aa5dc4b2c/numpy-1.12.0.zip"
+    sha256 "ff320ecfe41c6581c8981dce892fe6d7e69806459a899e294e4bf8229737b154"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/38/bb/bf325351dd8ab6eb3c3b7c07c3978f38b2103e2ab48d59726916907cd6fb/pyparsing-2.1.10.tar.gz"
+    sha256 "811c3e7b0031021137fc83e051795025fcb98674d07eb8fe922ba4de53d39188"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/51/fc/39a3fbde6864942e8bb24c93663734b74e281b984d1b8c4f95d64b0c21f6/python-dateutil-2.6.0.tar.gz"
+    sha256 "62a2f8df3d66f878373fd0072eacf4ee52194ba302e00082828e0d263b0418d2"
+  end
+
+  resource "pytz" do
+    url "https://files.pythonhosted.org/packages/d0/e1/aca6ef73a7bd322a7fc73fd99631ee3454d4fc67dc2bee463e2adf6bb3d3/pytz-2016.10.tar.bz2"
+    sha256 "7016b2c4fa075c564b81c37a252a5fccf60d8964aa31b7f5eae59aeb594ae02b"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/b3/b2/238e2590826bfdd113244a40d9d3eb26918bd798fc187e2360a8367068db/six-1.10.0.tar.gz"
+    sha256 "105f8d68616f8248e24bf0e9372ef04d3cc10104f1980f54d57b2ce73a5ad56a"
+  end
+
+  resource "subprocess32" do
+    url "https://files.pythonhosted.org/packages/b8/2f/49e53b0d0e94611a2dc624a1ad24d41b6d94d0f1b0a078443407ea2214c2/subprocess32-3.2.7.tar.gz"
+    sha256 "1e450a4a4c53bf197ad6402c564b9f7a53539385918ef8f12bdf430a61036590"
+  end
+
+  def install
+    if MacOS.version == :el_capitan && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0" \
+      || MacOS.version == :yosemite && MacOS::Xcode.installed? && MacOS::Xcode.version >= "7.0"
+      ENV.delete "SDKROOT"
+    end
+
+    inreplace "setupext.py",
+              "'darwin': ['/usr/local/'",
+              "'darwin': ['#{HOMEBREW_PREFIX}'"
+
+    # Apple has the Frameworks (esp. Tk.Framework) in a different place
+    unless MacOS::CLT.installed?
+      inreplace "setupext.py",
+                "'/System/Library/Frameworks/',",
+                "'#{MacOS.sdk_path}/System/Library/Frameworks',"
+    end
+
+    Language::Python.each_python(build) do |python, version|
+      bundle_path = libexec/"lib/python#{version}/site-packages"
+      bundle_path.mkpath
+      ENV.prepend_path "PYTHONPATH", bundle_path
+
+      res = if version.to_s.start_with? "2"
+        resources.map(&:name).to_set
+      else
+        resources.map(&:name).to_set - ["functools32", "subprocess32"]
+      end
+      res.each do |r|
+        resource(r).stage do
+          system python, *Language::Python.setup_install_args(libexec)
+        end
+      end
+      (lib/"python#{version}/site-packages/homebrew-matplotlib-bundle.pth").write "#{bundle_path}\n"
+
+      system python, *Language::Python.setup_install_args(prefix)
+    end
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      If you want to use the `wxagg` backend, do `brew install wxpython`.
+      This can be done even after the matplotlib install.
+    EOS
+    if build.with?("python") && !Formula["python"].installed?
+      homebrew_site_packages = Language::Python.homebrew_site_packages
+      user_site_packages = Language::Python.user_site_packages "python"
+      s += <<-EOS.undent
+        If you use system python (that comes - depending on the OS X version -
+        with older versions of numpy, scipy and matplotlib), you may need to
+        ensure that the brewed packages come earlier in Python's sys.path with:
+          mkdir -p #{user_site_packages}
+          echo 'import sys; sys.path.insert(1, "#{homebrew_site_packages}")' >> #{user_site_packages}/homebrew.pth
+      EOS
+    end
+    s
+  end
+
+  test do
+    ENV["PYTHONDONTWRITEBYTECODE"] = "1"
+
+    Language::Python.each_python(build) do |python, _|
+      system python, "-c", "import matplotlib"
+    end
+  end
+end
