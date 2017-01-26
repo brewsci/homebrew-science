cf57f463276854226a839689064aab17d933f085
Michka Popoff <michkapopoff@gmail.com>
2017-01-26 20:52:30 +0100

    insighttoolkit: add test
    Closes #4916
    
    The simplest thing to do is to generate an Image object
    and call Update() on it, to trigger the image pipeline.

--- insighttoolkit.rb
+++ insighttoolkit.rb
@@ -98,4 +98,30 @@ class Insighttoolkit < Formula
       system "make", "install"
     end
   end
+
+  test do
+    (testpath/"test.cxx").write <<-EOS
+      #include "itkImage.h"
+
+      int main(int argc, char* argv[])
+      {
+        typedef itk::Image< unsigned short, 3 > ImageType;
+        ImageType::Pointer image = ImageType::New();
+        image->Update();
+
+        return EXIT_SUCCESS;
+      }
+    EOS
+
+    v=version.to_s.split(".")[0..1].join(".")
+    # Build step
+    system ENV.cxx, "-isystem", "#{include}/ITK-#{v}", "-o", "test.cxx.o", "-c", "test.cxx"
+    # Linking step
+    system ENV.cxx, "test.cxx.o", "-o", "test",
+                    "#{lib}/libITKCommon-#{v}.1.dylib",
+                    "#{lib}/libITKVNLInstantiation-#{v}.1.dylib",
+                    "#{lib}/libitkvnl_algo-#{v}.1.dylib",
+                    "#{lib}/libitkvnl-#{v}.1.dylib"
+    system "./test"
+  end
 end
