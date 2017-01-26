2ec967a41c82941a7df6cbb1d4312565f6a5768d
FX Coudert <fxcoudert@gmail.com>
2017-01-26 14:33:51 +0100

    voro++: add desc, tag, DOI, and clean up
    Closes #4915.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- voro++.rb
+++ voro++.rb
@@ -1,17 +1,16 @@
 class Voroxx < Formula
+  desc "3D Voronoi cell software library"
   homepage "http://math.lbl.gov/voro++"
   url "http://math.lbl.gov/voro++/download/dir/voro++-0.4.6.tar.gz"
   sha256 "ef7970071ee2ce3800daa8723649ca069dc4c71cc25f0f7d22552387f3ea437e"
   head "https://codeforge.lbl.gov/anonscm/voro/trunk", :using => :svn
+  # tag "math"
+  # doi "10.1063/1.3215722"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    (share / "voro++").install("examples")
+    pkgshare.install("examples")
     mv prefix / "man", share / "man"
-  end
-
-  test do
-    system "voro++", "-h"
   end
 
   def caveats
@@ -19,5 +18,9 @@ class Voroxx < Formula
     Example scripts are installed here:
       #{HOMEBREW_PREFIX}/share/voro++/examples
     EOS
+  end
+
+  test do
+    system "#{bin}/voro++", "-h"
   end
 end
