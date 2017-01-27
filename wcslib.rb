d8793744626609e67ad6b54040324c8ee4bb59fc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-27 20:07:35 +0000

    wcslib: update 5.16_1 bottle.

--- wcslib.rb
+++ wcslib.rb
@@ -7,10 +7,9 @@ class Wcslib < Formula
 
   bottle do
     cellar :any
-    sha256 "81076955ebb0536666456bbb73bd22fde9a6a11f4016bd4ac35173fc6f8b924e" => :sierra
-    sha256 "b1377f996a7f0c9c3e52d8eca95d44309dda75704297b4e3c3378be1e7d55653" => :el_capitan
-    sha256 "c0ee66a5bf1d1411a5bbad81f3a4b7e3391b7bb339cf22a9cddfcd637fa17cb4" => :yosemite
-    sha256 "47c69b7144501b5ac2005bcc2fc0957f8eafe744e9e5c4c608144f8504cfd838" => :x86_64_linux
+    sha256 "54bbcd8e7f8208f42901a13a637fa2994f0fa9d33e4b3326aba4fc8f171504d4" => :sierra
+    sha256 "209c69286e567c4cc34b6b70a0cf9f3beeb1182bb5a63e4ede05f684a0341771" => :el_capitan
+    sha256 "04388c291155822d31637875b883a42d723a445aff90d6e25c9a267091cb312a" => :yosemite
   end
 
   option "with-pgplot", "Build PGSBOX, a general curvilinear axis drawing routine for PGPLOT"
