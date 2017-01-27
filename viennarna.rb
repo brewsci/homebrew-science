010b049dba11a6d310ba0647cb5257db21ad2604
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-27 21:28:08 +0000

    viennarna: update 2.3.3 bottle for Linuxbrew.

--- viennarna.rb
+++ viennarna.rb
@@ -12,6 +12,7 @@ class Viennarna < Formula
     sha256 "2828a32c707dc311947d32755a609a4f2bc7c6de0254d43b94c7538944e1ce69" => :sierra
     sha256 "b95b4f52ce14c9c4437b8305d3c8b08e2def4c822c33381953c8e79b81cdf67c" => :el_capitan
     sha256 "71e2108defc57d61435957b55ef0c4a3a533c39b86fdc7f17c81ba013567090f" => :yosemite
+    sha256 "2b44a6a50b8223645bb64920ae47ee3c60b26b9b46a1ac08e69cfc7c1d2b291b" => :x86_64_linux
   end
 
   option "with-openmp", "Enable OpenMP multithreading"
