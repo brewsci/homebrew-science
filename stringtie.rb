bd07d2f72dc59f5b75eed927f5eb3a849e3f4f0a
ilovezfs <ilovezfs@icloud.com>
2017-01-27 23:40:21 -0800

    stringtie 1.3.2c
    Closes #4929.
    
    Signed-off-by: Tomasz Pajor <tomek@polishgeeks.com>

--- stringtie.rb
+++ stringtie.rb
@@ -1,8 +1,8 @@
 class Stringtie < Formula
   desc "Transcript assembly and quantification for RNA-Seq"
   homepage "http://ccb.jhu.edu/software/stringtie"
-  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.2b.tar.gz"
-  sha256 "12202e91bf8e314a59bca4b353eeb212e7e6926ea12a9ffdaedb8ee4818b5465"
+  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.2c.tar.gz"
+  sha256 "4d5efd0f2a5d0000e256866ecab61d7b47ecdad370262202c472eb37b66454c9"
   head "https://github.com/gpertea/stringtie.git"
 
   # doi "10.1038/nbt.3122"
