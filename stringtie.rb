f04b10ea3aacb02f18bb7c5c235161001fec5630
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-28 01:06:07 +0000

    stringtie: update 1.3.2b bottle for Linuxbrew.

--- stringtie.rb
+++ stringtie.rb
@@ -13,6 +13,7 @@ class Stringtie < Formula
     sha256 "984b5e6cf01b0a7dc02fd517ce6e4eefecc0422e4109ce13fd206db6adbb1ca7" => :sierra
     sha256 "626e41b97813e1fb2501175c3766c2d51a09008171a7b992a7711c160f634228" => :el_capitan
     sha256 "61f05ee8246d49d45e9f8a95f92aa3904d6ce01d2de5c7c0f0817920426b3340" => :yosemite
+    sha256 "a3aa8520e257a27c70b9a8ef0c0260c9c7c97a0aa8ec4b97beee74249d885b5d" => :x86_64_linux
   end
 
   def install
