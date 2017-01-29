cad8bdc91d185d79fc12203163bf949a0ed64f0b
ilovezfs <ilovezfs@icloud.com>
2017-01-29 07:29:16 -0800

    etsf_io: revision for netcdf
    Closes #4938.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- etsf_io.rb
+++ etsf_io.rb
@@ -1,8 +1,10 @@
 class EtsfIo < Formula
+  desc "Library of F90 routines to read and write the ETSF file format"
   homepage "http://www.etsf.eu/resources/software/libraries_and_tools"
   url "http://www.etsf.eu/system/files/etsf_io-1.0.4.tar.gz"
   sha256 "3140c2cde17f578a0e6b63acb27a5f6e9352257a1371a17b9c15c3d0ef078fa4"
-  revision 3
+  revision 4
+  # doi "10.1016/j.cpc.2008.05.007"
 
   bottle do
     sha256 "8c1b67b178286f5e780c07f170593a6036dddf1733771a4ce72d94c437c123ef" => :el_capitan
@@ -10,18 +12,25 @@ class EtsfIo < Formula
     sha256 "3e35f91bf47ee55f0b9a7b5e394942a264d5dd3e5e8edf03aaa8ccc8dba66434" => :mavericks
   end
 
-  option "without-check", "Skip build-time tests (not recommended)"
+  option "without-test", "Skip build-time tests (not recommended)"
+  deprecated_option "without-check" => "without-test"
 
-  depends_on :fortran
-  depends_on "netcdf" => ["with-fortran"]
+  if OS.mac?
+    depends_on "gcc@5"
+  else
+    depends_on :fortran
+  end
+  depends_on "netcdf" => "with-fortran"
 
   def install
+    ENV["FC"] = Formula["gcc@5"].opt_bin/"gfortran-5" if OS.mac?
+
     system "./configure", "--prefix=#{prefix}", "--with-moduledir=#{include}",
            "--with-netcdf-incs=-I#{Formula["netcdf"].opt_include}",
            "--with-netcdf-libs=-L#{Formula["netcdf"].opt_lib} -lnetcdff -lnetcdf"
 
     system "make"
-    system "make", "check" if build.with? "check"
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 
