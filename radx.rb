432bf82c8860d4c5c3cbc21b689ce8b39278002a
ilovezfs <ilovezfs@icloud.com>
2017-01-29 07:42:21 -0800

    radx: revision for netcdf
    Closes #4950.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- radx.rb
+++ radx.rb
@@ -4,7 +4,7 @@ class Radx < Formula
   url "https://www.eol.ucar.edu/system/files/software/radx/all-oss/radx-20160809.src_.tgz"
   version "20160809"
   sha256 "a071146df16b8abf926d35be4bc7d06b9204feeba8bbc8772858a7805bc5b92a"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
