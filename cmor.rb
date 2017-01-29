828d067555a971b74aedecd045fbfda94e73e788
ilovezfs <ilovezfs@icloud.com>
2017-01-29 07:24:02 -0800

    cmor: revision for netcdf
    Closes #4936.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- cmor.rb
+++ cmor.rb
@@ -1,8 +1,9 @@
 class Cmor < Formula
   desc "Climate Model Output Rewriter for producing CF-compliant netCDF files"
-  homepage "http://www2-pcmdi.llnl.gov/cmor"
+  homepage "http://cmor.llnl.gov/"
   url "https://github.com/PCMDI/cmor/archive/3.2.1.tar.gz"
   sha256 "4838a695be1830a10f7e01bb1b4142fd151f28e0e417d4470aa49b821e3b31a8"
+  revision 1
   # doi "10.5281/zenodo.61943"
 
   bottle do
