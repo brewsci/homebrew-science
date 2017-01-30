76673f6ac38b896f6e970d0284175ef753620b6e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-30 19:30:59 +0000

    adapterremoval: add 2.2.0 bottle.

--- adapterremoval.rb
+++ adapterremoval.rb
@@ -6,6 +6,13 @@ class Adapterremoval < Formula
   # doi "10.1186/s13104-016-1900-2"
   # tag "bioinformatics"
 
+  bottle do
+    cellar :any
+    sha256 "df8e67f8518c8bf15ffe93b644b5fd77dfadfd686d7cdc9eb096ca88176cbc2f" => :sierra
+    sha256 "710e9ecdabfff6400c2059415505a77aea67eb15076ddadf63f76b54e7505a26" => :el_capitan
+    sha256 "71c311b9d04c31e82a71a4fc4b4b748afd3ddb06881cbef4ad3e5cd3bd19410d" => :yosemite
+  end
+
   depends_on "homebrew/dupes/bzip2" => :recommended
   depends_on "homebrew/dupes/zlib" => :recommended
 
