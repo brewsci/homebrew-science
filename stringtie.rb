f3bf4c887ea96572d2333932e62d8535e7277616
ilovezfs <ilovezfs@icloud.com>
2017-01-30 23:38:33 -0800

    stringtie 1.3.2d
    Closes #4964.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- stringtie.rb
+++ stringtie.rb
@@ -1,8 +1,8 @@
 class Stringtie < Formula
   desc "Transcript assembly and quantification for RNA-Seq"
   homepage "http://ccb.jhu.edu/software/stringtie"
-  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.2c.tar.gz"
-  sha256 "4d5efd0f2a5d0000e256866ecab61d7b47ecdad370262202c472eb37b66454c9"
+  url "https://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.2d.tar.gz"
+  sha256 "6e952b7bab6c5da79c751e79541819245ece2bdad9d5d707551119c30f0ccd66"
   head "https://github.com/gpertea/stringtie.git"
 
   # doi "10.1038/nbt.3122"
@@ -18,7 +18,6 @@ class Stringtie < Formula
   def install
     system "make", "release"
     bin.install "stringtie"
-    doc.install "README", "LICENSE"
   end
 
   test do
