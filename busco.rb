16d7cac3a758768f0db070d1674dcb1e6596124a
ilovezfs <ilovezfs@icloud.com>
2017-01-30 23:35:22 -0800

    busco: update URL (#4963)

--- busco.rb
+++ busco.rb
@@ -1,7 +1,7 @@
 class Busco < Formula
   desc "Assess genome assembly completeness with single-copy orthologs"
   homepage "http://busco.ezlab.org"
-  url "http://busco.ezlab.org/files/BUSCO_v1.22.tar.gz"
+  url "http://busco.ezlab.org/v1/files/BUSCO_v1.22.tar.gz"
   sha256 "86088bbd2128ea04ad9e1b2ebd18201f4c79a48a161ba2593feb12abb8a2d0e2"
   revision 1
   # doi "10.1093/bioinformatics/btv351"
