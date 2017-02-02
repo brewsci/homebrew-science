1b69bb9ddf6a7c3a58f89b654e70258efcddd53f
ilovezfs <ilovezfs@icloud.com>
2017-02-02 13:56:14 -0800

    nextflow 0.23.2 (#4979)

--- nextflow.rb
+++ nextflow.rb
@@ -4,14 +4,14 @@ class Nextflow < Formula
   # doi "10.6084/m9.figshare.1254958"
   # tag "bioinformatics"
 
-  url "https://www.nextflow.io/releases/v0.23.1/nextflow"
-  version "0.23.1"
-  sha256 "f3ac9d8d4e835051969c804095266043c88a1a69a6c959de13735a822b772842"
+  url "https://www.nextflow.io/releases/v0.23.2/nextflow"
+  version "0.23.2"
+  sha256 "e8f49ce73e9ad430879ee1dbd1655497a38dc7fdb41be4bc197b15202a6ebb94"
   head "https://github.com/nextflow-io/nextflow.git"
 
   bottle :unneeded
 
-  depends_on java: "1.7+"
+  depends_on :java => "1.7+"
 
   def install
     bin.install "nextflow"
