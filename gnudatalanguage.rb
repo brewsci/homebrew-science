6e00cd449ca24d2a163c0593049f0bca8d02304a
ilovezfs <ilovezfs@icloud.com>
2017-02-03 08:06:16 -0800

    gnudatalanguage 0.9.7
    Closes #4984.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- gnudatalanguage.rb
+++ gnudatalanguage.rb
@@ -1,9 +1,8 @@
 class Gnudatalanguage < Formula
   desc "Free and open-source IDL/PV-WAVE compiler"
   homepage "http://gnudatalanguage.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/gnudatalanguage/gdl/0.9.5/gdl-0.9.5.tar.gz"
-  sha256 "cc9635e836b5ea456cad93f8a07d589aed8649668fbd14c4aad22091991137e2"
-  revision 8
+  url "https://downloads.sourceforge.net/project/gnudatalanguage/gdl/0.9.7/gdl-0.9.7.tgz"
+  sha256 "2b5945d06e4d95f01cb70a3c432ac2fa4c81e1b3ac7c02687a6704ab042a7e21"
 
   bottle do
     sha256 "a7c70e56aabf01d9c9c9ea702698c156f8733383e921784bc095f8115243660e" => :sierra
@@ -28,31 +27,27 @@ class Gnudatalanguage < Formula
   depends_on :x11
   depends_on :python => :optional
 
-  # part 1 taken from macports https://trac.macports.org/browser/trunk/dports/math/gnudatalanguage/files/patch-CMakeLists.txt.diff
-  # other parts taken from https://github.com/freebsd/freebsd-ports/tree/master/science/gnudatalanguage/files
-  patch :p0 do
-    url "https://gist.githubusercontent.com/iml/ee107290ee5cd2850190/raw/83c046bce9facda8c560a8d38ea02913892c4b88/gnudatalanguage.diff"
-    sha256 "b2e9b2c1ed51676e048441b7bfc160488fdf5898b1d1c9396bc58eb85996981e"
-  end
-
-  # restores compatibility with latest plplot. Patch found upstream at http://sourceforge.net/p/gnudatalanguage/bugs/643/
-  patch do
-    url "https://gist.github.com/tschoonj/e01c72165fd2cb9a68c9/raw/5143168a04de0d4f8fc6c3621733ce68fe7c6268/gdl-plplot.patch"
-    sha256 "937e82f6052aa72576df49046aa57d03593c7ba21d074f2455cd614318b881dd"
-  end
-
   patch :DATA if build.with? "hdf4"
 
   def install
-    args = std_cmake_args
-    args << "-DHDF=OFF" if build.without?("hdf4")
-    args << "-DPYTHON=OFF" if build.without?("python")
-    args << "-DWXWIDGETS=OFF" << "-DPSLIB=OFF"
-    system "cmake", ".", *args
-    system "make"
-    # several tests fail
-    # system "make", "check"
-    system "make", "install"
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DHDF=OFF" if build.without?("hdf4")
+      args << "-DPYTHON=OFF" if build.without?("python")
+      args << "-DWXWIDGETS=OFF" << "-DPSLIB=OFF"
+      system "cmake", "..", *args
+      system "make"
+
+      # The following tests FAILED:
+      #    80 - test_execute.pro (Failed)
+      #    84 - test_fft_leak.pro (Failed)
+      #   108 - test_image_statistics.pro (Failed)
+      #   128 - test_obj_isa.pro (Failed)
+      # Reported 3 Feb 2017 https://sourceforge.net/p/gnudatalanguage/bugs/716/
+      # system "make", "check"
+
+      system "make", "install"
+    end
   end
 
   test do
