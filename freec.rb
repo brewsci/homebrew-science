1942899e6e1b32455e439036acd2a06ee129d066
ilovezfs <ilovezfs@icloud.com>
2017-02-04 02:09:11 -0800

    freec 10.4
    Closes #4995.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- freec.rb
+++ freec.rb
@@ -1,8 +1,8 @@
 class Freec < Formula
   desc "Copy number and genotype annotation in whole genome/exome sequencing data"
   homepage "http://bioinfo.curie.fr/projects/freec/"
-  url "https://github.com/BoevaLab/FREEC/archive/v10.3.tar.gz"
-  sha256 "f15b90dc0fc6629ee32f1a38d1e39f062f20089689b02429f78b585f99504e95"
+  url "https://github.com/BoevaLab/FREEC/archive/v10.4.tar.gz"
+  sha256 "d01f44c42318f251c24717b864ef624427e1361069f8fb694f5c52884276fca8"
   head "https://github.com/BoevaLab/FREEC.git"
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btr670"
@@ -13,12 +13,6 @@ class Freec < Formula
     sha256 "40afc40855875d9f134ba5bff417ea26a245f2c6c9c402dac2909192c6ef7f09" => :el_capitan
     sha256 "76959d16cea8df6eaf0f2c31cb46197e6f168c03a9d09535bceacea23f9b8211" => :yosemite
     sha256 "661683a53de278c90947606c7ebbc4382e3a75a4f19ff4d3cd1fde2a771bfdd1" => :x86_64_linux
-  end
-
-  # Patch to fix builds on macOS. Will be present in next release (>10.3)
-  patch do
-    url "https://github.com/BoevaLab/FREEC/commit/b39300fda339bc4fd9b68727e04f99cdbcbe79b2.diff"
-    sha256 "f1ad24896f7d8b60863e7450179334287f12f9fc8ca28e474c01716925f898ca"
   end
 
   def install
