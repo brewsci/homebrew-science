eab7721d6377f9dfc014393a983de9fd497fba71
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-05 19:15:48 +0000

    nccmp: update 1.8.2.0_3 bottle for Linuxbrew.

--- nccmp.rb
+++ nccmp.rb
@@ -9,6 +9,7 @@ class Nccmp < Formula
     sha256 "26b99d3f807f4c86d7d29847d2e654d460243b80cf044f92e66128c29c7522ca" => :sierra
     sha256 "c9f673373e8886dbc99f0ae932b4f4e7ce2ebb1a1ef72364a46cd1a245208389" => :el_capitan
     sha256 "2f2eb6ec6298d0df12a4d3ca5338a6cf677644e6e1cceb084beafd865ef267b0" => :yosemite
+    sha256 "54a4db6629830761389a30492628d5390acd90e0e5c2dba0318d4fb59d8241f7" => :x86_64_linux
   end
 
   depends_on "netcdf"
