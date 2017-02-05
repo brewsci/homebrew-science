943ca7e6988938422c70a7b8c2dbf9329e637db6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-05 17:43:26 +0000

    seqdb: update 0.2.0_4 bottle for Linuxbrew.

--- seqdb.rb
+++ seqdb.rb
@@ -10,6 +10,7 @@ class Seqdb < Formula
     sha256 "3fd434ab0761d5510d6dd9b7f8cc306e05aad2f64fcd7bf7fd44add7a33f42e7" => :sierra
     sha256 "700eaf5a407cb37307a94332e8efdaf74f045360b10007e44f4e4435f767a7f7" => :el_capitan
     sha256 "9a9ff3e6fa1266609cfacd0e2108202fc3284bcf58763efdae5eb4661eb71909" => :yosemite
+    sha256 "0bc7d8e9655c6230563f7e55660e5e84ec40983a82713cb264a6c71a011efff0" => :x86_64_linux
   end
 
   needs :openmp
