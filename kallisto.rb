0012d8bbdd9b00ae743c52804fd7ab5f75680c2b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-05 16:41:30 +0000

    kallisto: update 0.43.0_3 bottle for Linuxbrew.

--- kallisto.rb
+++ kallisto.rb
@@ -12,6 +12,7 @@ class Kallisto < Formula
     sha256 "16c0ef84758ac8c992fec4994702e4affced6b34065b7895be5f91c5e994983c" => :sierra
     sha256 "00248afaaf30532aa50f17941608310d950ec84dd064d2a4678d68b6d6440555" => :el_capitan
     sha256 "c1adba93bbeff588780c8eee54fafb74703ca47360ba8efede25619f46773cc3" => :yosemite
+    sha256 "371cc68776d6a728cf6f1032255a721d54f54c17b2a24107f0839ca35ff8ebdc" => :x86_64_linux
   end
 
   needs :cxx11
