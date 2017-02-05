86470df85f4573753e9956b3ab5d251f025da164
Michka Popoff <michkapopoff@gmail.com>
2017-02-05 19:08:42 +0100

    swrcfit: revision for hdf5
    Closes #5051.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- swrcfit.rb
+++ swrcfit.rb
@@ -4,6 +4,7 @@ class Swrcfit < Formula
   # doi "10.5194/hessd-4-407-2007"
   url "https://github.com/sekika/swrcfit/archive/v3.0.tar.gz"
   sha256 "bee47347bad5db0ac72597b82d5fff20278e57d5792e431f6d987c52360d7021"
+  revision 1
   head "https://github.com/sekika/swrcfit.git"
 
   bottle do
