2dc9379fc6ca3df456f8d72bb4998f8a468dfc38
Michka Popoff <michkapopoff@gmail.com>
2017-02-05 17:12:35 +0100

    cdo: revision for hdf5
    Closes #5013.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- cdo.rb
+++ cdo.rb
@@ -3,7 +3,7 @@ class Cdo < Formula
   homepage "https://code.zmaw.de/projects/cdo"
   url "https://code.zmaw.de/attachments/download/12070/cdo-1.7.1.tar.gz"
   sha256 "5c24a5cb74dcf6e8b5140c67033868a5a0b641341e3adad3cb4035d5ad6e70a6"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
