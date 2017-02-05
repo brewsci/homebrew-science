f1381b0d2cd8714492b3569357d3fa66a0a12f93
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-05 17:28:37 +0000

    radx: update 20160809_5 bottle for Linuxbrew.

--- radx.rb
+++ radx.rb
@@ -11,6 +11,7 @@ class Radx < Formula
     sha256 "ce5104158b0bfd5feb81b6ff57ca9b45e43f6e70634837d821979cb74311df21" => :sierra
     sha256 "bbd4d6270ef58c00aa66ac4567a621419db98bc24345d8e34eba27edb5255837" => :el_capitan
     sha256 "2853715e744da2b615636fdb485fc6d2eacaaabd93cbb991e0f6b8ac700d03d6" => :yosemite
+    sha256 "c3d6699f94686d562fb1cc01bfec85bcbc09b0e8c2f8e9910e91351c247d7eed" => :x86_64_linux
   end
 
   depends_on "hdf5"
