bf0976f117b69050d3b1d2de2bf65774318f4ab9
Michka Popoff <michkapopoff@gmail.com>
2017-02-06 23:32:49 +0100

    littler: fix formula
    The inreplace is no more needed (at least, is
    is failing now, and removing it fixes the build).
    The audit failures were fixed too.
    
    Closes #5074.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- littler.rb
+++ littler.rb
@@ -1,11 +1,12 @@
 class Littler < Formula
+  desc "Scripting and command-line front-end for GNU R"
   homepage "http://dirk.eddelbuettel.com/code/littler.html"
   # tag "math"
 
-  head "https://github.com/eddelbuettel/littler.git"
   url "http://dirk.eddelbuettel.com/code/littler/littler-0.2.3.tar.gz"
   sha256 "98cd741c68a5c8f65b06c96d2f56d3b44979b3990335e7869b002c005ef80ba7"
-  revision 6
+  revision 7
+  head "https://github.com/eddelbuettel/littler.git"
 
   bottle do
     sha256 "df1a3168bdc396b4836021ba476be411accc38e7cb325e0c67ffe7591dadf253" => :el_capitan
@@ -23,7 +24,6 @@ class Littler < Formula
       "--prefix=#{prefix}"
     inreplace "Makefile", Formula["r"].prefix, Formula["r"].opt_prefix
     system "make", "littler.h"
-    inreplace "littler.h", Formula["r"].prefix, Formula["r"].opt_prefix
     system "make"
 
     bin.install "r" => "littler"
