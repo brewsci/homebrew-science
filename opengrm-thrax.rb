bdd0fb21fd9c6be6d38cc2762fbcc0a10d94f126
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-07 16:51:07 +0000

    opengrm-thrax: update 1.2.3_1 bottle for Linuxbrew.

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -10,6 +10,7 @@ class OpengrmThrax < Formula
     sha256 "77c822ef900109767aa57a361150b15ba4d135462b8f5796a5844d795f03f8ba" => :sierra
     sha256 "03d906c22157ecf2c31d4d7f865be93ba099a013d8e6bbaa8c1b8cd207b5969f" => :el_capitan
     sha256 "51fb9e88d762a04e37cd285fdb9d040746e8abea6a774e5b8e44724790d9d7aa" => :yosemite
+    sha256 "adf3cdac6f9a79573a3421e9ae9b9d07726ece0b2aca2e944e906570538996ff" => :x86_64_linux
   end
 
   depends_on "openfst"
