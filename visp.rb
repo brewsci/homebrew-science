1bd1e78d1c11e3046b9aa43d4eedd376449faaeb
Fabien Spindler <Fabien.Spindler@inria.fr>
2017-02-07 08:38:02 +0100

    visp 3.0.1
    Closes #5080.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- visp.rb
+++ visp.rb
@@ -1,9 +1,8 @@
 class Visp < Formula
-  desc "Visual Servoing Platform library, version 3"
+  desc "Visual Servoing Platform library"
   homepage "https://visp.inria.fr"
-  url "http://gforge.inria.fr/frs/download.php/latestfile/475/visp-3.0.0.tar.gz"
-  sha256 "f9fa6f16f5c06d4eaa91ed374ecd7416ad49639d9f3a1865b21933af368e720f"
-  revision 2
+  url "http://gforge.inria.fr/frs/download.php/latestfile/475/visp-3.0.1.tar.gz"
+  sha256 "8aefd21f30dd4f6d210c59c28704f9e3adf874e3337571a3ae65a65946c94326"
 
   bottle do
     sha256 "6101dda8cfd44e9a05e47e8c5dc0865f98bfd37ed6a657be761d779c44c6209b" => :sierra
@@ -14,10 +13,12 @@ class Visp < Formula
   option :cxx11
 
   depends_on "cmake"     => :build
-  depends_on "opencv3"   => :recommended
-  depends_on "libdc1394" => :recommended
-  depends_on "libxml2"   => :recommended
   depends_on "gsl"       => :recommended
+  depends_on "jpeg"      => :recommended
+  depends_on "libdc1394" => :recommended
+  depends_on "libpng"    => :recommended
+  depends_on "libxml2"   => :recommended
+  depends_on "opencv3"   => :recommended
   depends_on "zbar"      => :recommended
   depends_on :x11        => :recommended
 
@@ -38,11 +39,13 @@ class Visp < Formula
 
     args << "-DUSE_CPP11=ON" if build.cxx11?
 
-    args << "-DUSE_X11="          + arg_switch("x11")
-    args << "-DUSE_OPENCV="       + arg_switch("opencv3")
     args << "-DUSE_DC1394="       + arg_switch("libdc1394")
-    args << "-DUSE_XML2="         + arg_switch("libxml2")
     args << "-DUSE_GSL="          + arg_switch("gsl")
+    args << "-DUSE_JPEG="         + arg_switch("jpeg")
+    args << "-DUSE_OPENCV="       + arg_switch("opencv3")
+    args << "-DUSE_PNG="          + arg_switch("libpng")
+    args << "-DUSE_X11="          + arg_switch("x11")
+    args << "-DUSE_XML2="         + arg_switch("libxml2")
     args << "-DUSE_ZBAR="         + arg_switch("zbar")
 
     mkdir "macbuild" do
