47df447ef3cb51b7a19e4f44cd8bb4e8558f43b7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-08 00:37:54 +0000

    libsequence: update 1.9.0 bottle for Linuxbrew.

--- libsequence.rb
+++ libsequence.rb
@@ -12,6 +12,7 @@ class Libsequence < Formula
     sha256 "36ba91dc8ab3b26cf47ec93146a8cfbe9c76a01e209c534c50d5f7be2808da75" => :sierra
     sha256 "3822cf2d95528eed3082c578954c221fc62460ed656ef4b0e2a1eb4bca1088bf" => :el_capitan
     sha256 "e8dafaaf0a02f854cb369b67cda8a8e9149424a3e61bd482bc285d8c33566b1a" => :yosemite
+    sha256 "6b0e9bce99425c68c2026da05734d9e00c404209405538c2f5d382f5680b7101" => :x86_64_linux
   end
 
   cxx11 = OS.linux? || MacOS.version > :mountain_lion ? [] : ["c++11"]
