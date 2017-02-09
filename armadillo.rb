c68e0e5f5263ff1dec0e96e87e7a73f89a7e3154
ilovezfs <ilovezfs@icloud.com>
2017-02-09 05:18:57 -0800

    armadillo 7.700.0
    Closes #5096.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- armadillo.rb
+++ armadillo.rb
@@ -1,8 +1,8 @@
 class Armadillo < Formula
   desc "C++ linear algebra library"
   homepage "http://arma.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/arma/armadillo-7.600.2.tar.xz"
-  sha256 "6790d5e6b41fcac6733632a9c3775239806d00178886226dec3f986a884f4c2d"
+  url "https://downloads.sourceforge.net/project/arma/armadillo-7.700.0.tar.xz"
+  sha256 "f9029490f7edfb7029e117961db6307c2c3ee368691ed15e4fc58d06c9115d94"
 
   bottle do
     cellar :any
@@ -10,6 +10,8 @@ class Armadillo < Formula
     sha256 "4ef4b851eb2b2eccb783972f8388bb4f107a4ece77f7c3d1460b75dad7a476c4" => :el_capitan
     sha256 "7330c877d626a03fc2a3d8ca407846eea5f724f3fa0122b60eaf7a659afbff3f" => :yosemite
   end
+
+  option :cxx11
 
   option "with-hdf5", "Enable the ability to save and load matrices stored in the HDF5 format"
 
@@ -19,15 +21,8 @@ class Armadillo < Formula
   depends_on "hdf5" => :optional
   depends_on "openblas" if OS.linux?
 
-  option :cxx11
-
   def install
     ENV.cxx11 if build.cxx11?
-
-    if build.with? "hdf5"
-      inreplace "CMakeLists.txt", "project(armadillo CXX)",
-                                  "project(armadillo CXX)\nENABLE_LANGUAGE(C)"
-    end
 
     args = std_cmake_args
     args << "-DDETECT_HDF5=ON" if build.with? "hdf5"
