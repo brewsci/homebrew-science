128aded7987ac2503a6bfa6a13afc738489747ea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-14 21:56:30 +0000

    bamm: add 2.5.0 bottle.

--- bamm.rb
+++ bamm.rb
@@ -4,6 +4,13 @@ class Bamm < Formula
   url "https://github.com/macroevolution/bamm/archive/v2.5.0.tar.gz"
   sha256 "526eef85ef011780ee21fe65cbc10ecc62efe54044102ae40bdef49c2985b4f4"
   head "https://github.com/macroevolution/bamm.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4d69a44a7bf8aaccffdce9ac637c4a2135b5b9a07109ee47bc63275db8a9b363" => :sierra
+    sha256 "0895f0659fbe00742ca8644d5a9c93dae9b89afeb10a4c61dbc3b0450bc46b97" => :el_capitan
+    sha256 "6c80230ab8ff97e1295391e79ce3975296d3dcd94aecc1fe9e155e719d6029da" => :yosemite
+  end
+
   # tag "bioinformatics"
   # doi "10.1371/journal.pone.0089543"
 
