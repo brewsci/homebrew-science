de72cc06df7d2b40b8b4ca36465bc9c9e5ecbc95
gcoco <gcoco@users.noreply.github.com>
2017-02-13 21:59:49 -0500

    lmfit 6.1
    Closes #5137.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- lmfit.rb
+++ lmfit.rb
@@ -1,7 +1,8 @@
 class Lmfit < Formula
+  desc "Levenberg-Marquardt least-squares minimization and curve fitting."
   homepage "http://apps.jcns.fz-juelich.de/doku/sc/lmfit"
-  url "http://apps.jcns.fz-juelich.de/src/lmfit/old/lmfit-5.1.tgz"
-  sha256 "4e35bdec551a4985cf6d96f26a808b56c171433edf4a413c2ed50ab3d85a3965"
+  url "http://apps.jcns.fz-juelich.de/src/lmfit/lmfit-6.1.tgz"
+  sha256 "54366788400e3b1eb47cff44c9dae9906da079400cec2df2fb0b865c9e04c6a0"
 
   bottle do
     cellar :any
@@ -18,7 +19,7 @@ class Lmfit < Formula
   end
 
   def caveats
-    "Demo sources have been placed in " + (share/"lmfit/demos")
+    "Demo sources have been placed in " + (pkgshare/"lmfit/demos")
   end
 
   test do
