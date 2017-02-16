7ab22cb4317cc093ad3c176563add59ff1196a78
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-16 05:39:57 +0000

    hisat2: update 2.0.5 bottle for Linuxbrew.

--- hisat2.rb
+++ hisat2.rb
@@ -11,6 +11,7 @@ class Hisat2 < Formula
     sha256 "dd3713455f92812d02e6285cb0fc2bf23faaea1834cb8f67678603a6e4d800ea" => :sierra
     sha256 "39313553a9dc3313a5115193d85ad177feb1d4a9540b4021ccbf47db5d159565" => :el_capitan
     sha256 "a89563bef8fc8d785b8d93b409c3b5ed44c4e6010f35868bdb4d8c893002c2b2" => :yosemite
+    sha256 "002d05148a3df74e97cbbe762d5f27607cb1e9ebbb7932fcc9cd1932574d6d13" => :x86_64_linux
   end
 
   def install
