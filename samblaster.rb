5e395e743a0b2f7236e7373b0150d698aeb73f9f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-18 12:12:54 +0000

    samblaster: update 0.1.24 bottle for Linuxbrew.

--- samblaster.rb
+++ samblaster.rb
@@ -13,6 +13,7 @@ class Samblaster < Formula
     sha256 "74c3db0157feee98c2b8eba2b641e14aa9e6ced1f580e26f347176bfd28f2c64" => :sierra
     sha256 "e9eb9875554987fc79f630f473bea38fe291c09e7dcd4ad19de80a220e0ebf45" => :el_capitan
     sha256 "9d84519d0bdf7ad62d1b8177446067ea00fb991e30602ed2c865ea80b8310330" => :yosemite
+    sha256 "5b57cad3a0a95239eb69585163e9705b8e7a135af2c7c9724bac74d3038ef6a3" => :x86_64_linux
   end
 
   def install
