b959f92339cd9b6bb6e1f0953d8183a45354b65c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-02-18 11:48:23 +0000

    bowtie2: update 2.3.0 bottle for Linuxbrew.

--- bowtie2.rb
+++ bowtie2.rb
@@ -24,6 +24,7 @@ class Bowtie2 < Formula
     sha256 "15264f6251bc52b95ba3ab3dae015fa7afeb7a64a1be2638d17e67915b184587" => :sierra
     sha256 "505e058d3ed28ad98dc848314919bad50c84144c9da45e8e3e2a29c2b6cc52cd" => :el_capitan
     sha256 "c558d87cf2bd95b3e47740403f48cc38d48abb5c80f677806c2b36082dc2a8a4" => :yosemite
+    sha256 "fdc9bc2fdd71b0ed446c955e93bf8c3812462a25775e2eafe9cc76eab86a94f0" => :x86_64_linux
   end
 
   option "without-tbb", "Build without using Intel Thread Building Blocks (TBB)"
