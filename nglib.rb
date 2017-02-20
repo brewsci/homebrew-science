7e8d4191d9e187cd25e4426fa905641fbf32759f
Bruce B. Lacey <bruce@blacey.com>
2017-02-20 15:01:23 -0800

    nglib 5.3.1: opencascade 7.x compatibility
    Closes #5171.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- nglib.rb
+++ nglib.rb
@@ -3,6 +3,7 @@ class Nglib < Formula
   homepage "https://sourceforge.net/projects/netgen-mesher/"
   url "https://downloads.sourceforge.net/project/netgen-mesher/netgen-mesher/5.3/netgen-5.3.1.tar.gz"
   sha256 "cb97f79d8f4d55c00506ab334867285cde10873c8a8dc783522b47d2bc128bf9"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,16 +14,37 @@ class Nglib < Formula
   end
 
   # These two conflict with each other, so we'll have at most one.
-  depends_on "opencascade" => :optional
+  depends_on "opencascade" => :recommended
   depends_on "oce" => :optional
 
-  # Patch three issues:
+  # Patch two main issues:
   #   Makefile - remove TCL scripts that aren't reuquired without NETGEN.
-  #   configure - remove TCL libs that caused issues with ld (#3624).
   #   Partition_Loop2d.cxx - Fix PI that was used rather than M_PI
-  patch :DATA
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/20850ac/nglib/define-PI-and-avoid-tcl-install.diff"
+    sha256 "1f97e60328f6ab59e41d0fa096acbe07efd4c0a600d8965cc7dc5706aec25da4"
+  end
+
+  # OpenCascase 7.x compatibility patches
+  if build.with? "opencascade"
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/20850ac/nglib/occt7.x-compatibility-patches.diff"
+      sha256 "c3f222b47c5da2cf8f278718dbc599fab682546380210a86a6538c3f2d9f1b27"
+    end
+  end
 
   def install
+    ENV.cxx11 if build.with? "opencascade"
+
+    cad_kernel = Formula[build.with?("opencascade") ? "opencascade" : "oce"]
+
+    # Set OCC search path to Homebrew prefix
+    inreplace "configure" do |s|
+      s.gsub!(%r{(OCCFLAGS="-DOCCGEOMETRY -I\$occdir/inc )(.*$)}, "\\1-I#{cad_kernel.opt_include}/#{cad_kernel}\"")
+      s.gsub!(/(^.*OCCLIBS="-L.*)( -lFWOSPlugin")/, "\\1\"") if build.with? "opencascade"
+      s.gsub!(%r{(OCCLIBS="-L\$occdir/lib)(.*$)}, "\\1\"") if OS.mac?
+    end
+
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
@@ -33,18 +55,9 @@ class Nglib < Formula
     if build.with?("opencascade") || build.with?("oce")
       args << "--enable-occ"
 
-      cad_kernel = Formula[build.with?("opencascade") ? "opencascade" : "oce"]
-
       if build.with? "opencascade"
-        args << "--with-occ=#{cad_kernel.opt_prefix}/include"
+        args << "--with-occ=#{cad_kernel.opt_prefix}"
 
-        # A couple mesh output functions were dropped in OpenCASCADE 6.8.1
-        # Would fix via patch, but the relevant files has windows line endings,
-        # which seem to cause problems when embedded in DATA section patches.
-        system "sed", "-i", "-e", "s/\\(.*RelativeMode.*\\)/\\/\\/ \\1/",
-          "#{buildpath}/libsrc/occ/occgeom.cpp"
-        system "sed", "-i", "-e", "s/\\(.*SetDeflection.*\\)/\\/\\/ \\1/",
-          "#{buildpath}/libsrc/occ/occgeom.cpp"
       else
         args << "--with-occ=#{cad_kernel.opt_prefix}/include/oce"
 
@@ -84,50 +97,7 @@ class Nglib < Formula
       }
     EOS
     system ENV.cxx, "-Wall", "-o", "test", "test.cpp",
-           "-I#{include}", "-L#{lib}", "-lnglib"
+           "-I#{include}", "-L#{lib}", "-lnglib", "-lTKIGES"
     system "./test"
   end
 end
-
-__END__
-diff -ur a/configure b/configure
---- a/configure	2014-10-07 00:04:36.000000000 +1300
-+++ b/configure	2016-11-12 21:43:00.000000000 +1300
-@@ -15354,7 +15354,7 @@
-
-	OCCFLAGS="-DOCCGEOMETRY -I$occdir/inc -I/usr/include/opencascade"
-
--	OCCLIBS="-L$occdir/lib -lTKernel -lTKGeomBase -lTKMath -lTKG2d -lTKG3d -lTKXSBase -lTKOffset -lTKFillet -lTKShHealing -lTKMesh -lTKMeshVS -lTKTopAlgo -lTKGeomAlgo -lTKBool -lTKPrim -lTKBO -lTKIGES -lTKBRep -lTKSTEPBase -lTKSTEP -lTKSTL -lTKSTEPAttr -lTKSTEP209 -lTKXDESTEP -lTKXDEIGES -lTKXCAF -lTKLCAF -lFWOSPlugin"
-+	OCCLIBS="-L$occdir/lib -lFWOSPlugin"
-
-
- #  -lTKDCAF
-diff -ur a/libsrc/occ/Partition_Loop2d.cxx b/libsrc/occ/Partition_Loop2d.cxx
---- a/libsrc/occ/Partition_Loop2d.cxx	2016-03-16 07:44:06.000000000 -0700
-+++ b/libsrc/occ/Partition_Loop2d.cxx	2016-03-16 07:45:40.000000000 -0700
-@@ -52,6 +52,10 @@
- #include <gp_Pnt.hxx>
- #include <gp_Pnt2d.hxx>
-
-+#ifndef PI
-+    #define PI M_PI
-+#endif
-+
- //=======================================================================
- //function : Partition_Loop2d
- //purpose  :
-diff -ur a/ng/Makefile.in b/ng/Makefile.in
---- a/ng/Makefile.in	2014-10-06 04:04:37.000000000 -0700
-+++ b/ng/Makefile.in	2016-03-19 14:43:51.000000000 -0700
-@@ -327,10 +327,7 @@
- #   /opt/netgen/lib/libngsolve.a /opt/netgen/lib/libngcomp.a /opt/netgen/lib/libngcomp.a  /opt/netgen/lib/libngfemng.a   /opt/netgen/lib/libngmg.a  /opt/netgen/lib/libngla.a  /opt/netgen/lib/libngbla.a  /opt/netgen/lib/libngstd.a -L/opt/intel/mkl/10.2.1.017/lib/em64t /opt/intel/mkl/10.2.1.017/lib/em64t/libmkl_solver_lp64.a  -lmkl_intel_lp64  -lmkl_gnu_thread -lmkl_core
- #
- #
--dist_bin_SCRIPTS = dialog.tcl menustat.tcl ngicon.tcl ng.tcl	  \
--ngvisual.tcl sockets.tcl drawing.tcl nghelp.tcl ngshell.tcl	  \
--ngtesting.tcl parameters.tcl variables.tcl csgeom.tcl stlgeom.tcl \
--occgeom.tcl acisgeom.tcl netgen.ocf
-+dist_bin_SCRIPTS =
-
- netgen_LDFLAGS = -export-dynamic
- all: all-am
