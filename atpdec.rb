3a11fb8c6d60cc81d85396c3004bfb853f9b5ce5
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-28 20:59:43 -0300

    atpdec: fix audit

--- atpdec.rb
+++ atpdec.rb
@@ -1,5 +1,5 @@
 class Atpdec < Formula
-  homepage "http://atpdec.sourceforge.net"
+  homepage "https://atpdec.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/atpdec/atpdec%20sources/1.7/atpdec-1.7.tar.gz"
   sha256 "6f9d80ddd0438aaa96e003ca46d1ff6f117d2d70ea2f34c28af57e968e9f98f7"
 
