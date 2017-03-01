b8edce6b278d6646f6a1347f81ebfebfc6a3c566
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-01 21:03:05 +0000

    acado: add 1.2.2beta bottle.

--- acado.rb
+++ acado.rb
@@ -4,6 +4,12 @@ class Acado < Formula
   url "https://github.com/acado/acado/archive/v1.2.2beta.tar.gz"
   version "1.2.2beta"
   sha256 "cf0779e64dd5d20989e97340c04ecccf542fe8d993c96b53e5c465693cb354b7"
+  bottle do
+    sha256 "33b701b48e25c3ac74e6dacda3b4146d4d5c9b1316d6cdbea3a943c118f22ea6" => :sierra
+    sha256 "526d28da3f7f3504009cd517d50bec752151ab4810749838698c4c7f82d6a1ba" => :el_capitan
+    sha256 "e5ab45e223ef1e21440826b91cd72ccca0fa305a5fa0df00600be3a9e3ca04bd" => :yosemite
+  end
+
   # doi "10.1002/oca.939"
 
   depends_on "cmake" => :build
