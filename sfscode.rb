75a832576a549dec6d8101c2d3b5a181ae3e68fb
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-28 21:52:02 -0300

    sfscode: use rebuild instead of revision

--- sfscode.rb
+++ sfscode.rb
@@ -6,7 +6,7 @@ class Sfscode < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
+    rebuild 1
     sha256 "65355ebf143d5ae2b9c7ca01bf830b451ce2348ecfb18af8d1f5beed96148f83" => :el_capitan
     sha256 "20c6de798168eb20dbadd36c133cb2c7d1f9f0834e577db37404d4924856dd42" => :yosemite
     sha256 "8bb471709ea95a2285c985a975e6500f56700b28e8e4dd3376098be12bc4cd5f" => :mavericks
