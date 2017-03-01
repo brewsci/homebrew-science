85f7453b8c23b88ca256daf98e74c708d531446e
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-28 21:18:25 -0300

    swrcfit: fix audit

--- swrcfit.rb
+++ swrcfit.rb
@@ -1,6 +1,6 @@
 class Swrcfit < Formula
   desc "Fitting of soil water retention curve"
-  homepage "http://swrcfit.sourceforge.net/"
+  homepage "https://swrcfit.sourceforge.io/"
   # doi "10.5194/hessd-4-407-2007"
   url "https://github.com/sekika/swrcfit/archive/v3.0.tar.gz"
   sha256 "bee47347bad5db0ac72597b82d5fff20278e57d5792e431f6d987c52360d7021"
