84ea42f267999743c6ccf24b03779d8f01e84045
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-28 21:17:22 -0300

    sfscode: fix audit

--- sfscode.rb
+++ sfscode.rb
@@ -1,6 +1,6 @@
 class Sfscode < Formula
   desc "Population genetics simulator"
-  homepage "http://sfscode.sourceforge.net/SFS_CODE/index/index.html"
+  homepage "https://sfscode.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/sfscode/sfscode/sfscode_20140719/sfscode_20140719.tgz"
   sha256 "1f69ed96e20f1453df2882ce78171b49cb3378b25538a3cacd18db00e07c979a"
 
