851a15ecc6cc1568bf43fd867ee3aeb16aba18e8
Miguel Araújo <miguelarauj1o@gmail.com>
2017-02-28 21:13:58 -0300

    niftilib: fix audit

--- niftilib.rb
+++ niftilib.rb
@@ -1,5 +1,5 @@
 class Niftilib < Formula
-  homepage "http://niftilib.sourceforge.net"
+  homepage "https://niftilib.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/niftilib/nifticlib/nifticlib_2_0_0/nifticlib-2.0.0.tar.gz"
   sha256 "a3e988e6a32ec57833056f6b09f940c69e79829028da121ff2c5c6f7f94a7f88"
 
