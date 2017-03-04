85e7845941daa16d5893aa351f643090d77e6685
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-04 17:24:51 +0000

    t-coffee: add 10.00.r1613 bottle for Linuxbrew.

--- t-coffee.rb
+++ t-coffee.rb
@@ -10,6 +10,7 @@ class TCoffee < Formula
     sha256 "bd4da1f25260d3f4457dc06042e2cb7eb5648e99c702e525c49952e1d2d820bb" => :sierra
     sha256 "d90578cbff89b51d6a492e952fe1974e7736afe97b7c6daaa3a517c416e0c462" => :el_capitan
     sha256 "428579ef4a9a9a12813151cadbf6142675c1a2c0b0aafd42ab829693f292f4e6" => :yosemite
+    sha256 "e52169d49c7810dbe0a8a0978cdafcc2bfafd22125ef36a9f419eae63a288331" => :x86_64_linux
   end
 
   # doi "10.1006/jmbi.2000.4042"
