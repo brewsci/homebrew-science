e61047bc86274cfc70916e2f4e0f1235ce78c4f3
Harsh Narang <harshnarang8@gmail.com>
2017-03-04 22:29:41 +0530

    t-coffee: fix audit
    Closes #5226.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- t-coffee.rb
+++ t-coffee.rb
@@ -1,19 +1,20 @@
 class TCoffee < Formula
+  desc "Compute, Manipulate Alignment of DNA-RNA-Protein Sequences, Structures"
   homepage "http://www.tcoffee.org/"
+  url "https://github.com/cbcrg/tcoffee/archive/v10.00.r1613.tar.gz"
   version "10.00.r1613"
-  url "https://github.com/cbcrg/tcoffee/archive/v#{version}.tar.gz"
   sha256 "8280e6002167a6adad7a238181657b1febae27c2b2edf4245027daaf55c8c763"
   head "https://github.com/cbcrg/tcoffee.git"
   # doi "10.1006/jmbi.2000.4042"
 
   def install
-    system *%w[make -C compile t_coffee]
+    system "make", "-C", "compile", "t_coffee"
     prefix.install "lib" => "libexec"
     prefix.install Dir["*"]
     bin.install_symlink "../compile/t_coffee"
   end
 
   test do
-    system "#{bin}/t_coffee -version"
+    system "#{bin}/t_coffee", "-version"
   end
 end
