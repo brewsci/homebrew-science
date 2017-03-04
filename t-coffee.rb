a3412c53243fd8ef31650fafe698c97e065afed1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-04 17:16:52 +0000

    t-coffee: add 10.00.r1613 bottle.

--- t-coffee.rb
+++ t-coffee.rb
@@ -5,6 +5,13 @@ class TCoffee < Formula
   version "10.00.r1613"
   sha256 "8280e6002167a6adad7a238181657b1febae27c2b2edf4245027daaf55c8c763"
   head "https://github.com/cbcrg/tcoffee.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bd4da1f25260d3f4457dc06042e2cb7eb5648e99c702e525c49952e1d2d820bb" => :sierra
+    sha256 "d90578cbff89b51d6a492e952fe1974e7736afe97b7c6daaa3a517c416e0c462" => :el_capitan
+    sha256 "428579ef4a9a9a12813151cadbf6142675c1a2c0b0aafd42ab829693f292f4e6" => :yosemite
+  end
+
   # doi "10.1006/jmbi.2000.4042"
 
   def install
