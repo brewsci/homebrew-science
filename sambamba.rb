d2b7b46b46c87e40285e83c2ce11f90a84100846
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-05 15:13:09 +0000

    sambamba: update 0.6.6 bottle for Linuxbrew.

--- sambamba.rb
+++ sambamba.rb
@@ -19,6 +19,7 @@ class Sambamba < Formula
     sha256 "a318c66f32dc429455d9ff113d17a23bcb3d631460f8e843395b01bd02bb9b7d" => :sierra
     sha256 "5fc0ff69ace3b2e0eab82440f1ba7bcfc669b0a06ceda4c34674bb2a41204773" => :el_capitan
     sha256 "d6a0041d900da0d3c27ad2ee9807c6ab909d541b0d833948b538d5d064ca0459" => :yosemite
+    sha256 "073869bb50a8d5fba8faea2cc46f64b63315077b2fff6099b9fa963636c2936f" => :x86_64_linux
   end
 
   head do
