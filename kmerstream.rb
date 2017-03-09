5b6aa456a08e317904e43591fc3ea47bc39fc0b1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-09 18:35:49 +0000

    kmerstream: update 1.1_1 bottle for Linuxbrew.

--- kmerstream.rb
+++ kmerstream.rb
@@ -13,6 +13,7 @@ class Kmerstream < Formula
     sha256 "ae57c28ea2005478bb32f78448d239e53ab0b4be50635086ee3753ee52256f69" => :sierra
     sha256 "800c38f604b71b2048ff252168305be3855f9767b7dd183a487701707a7e2226" => :el_capitan
     sha256 "67af773688af575858ceee55a5d6b008b347f9c3101e63cb2e406bfb023b924b" => :yosemite
+    sha256 "bbcbe67ac973a7414b26322a81dc9c9e7ebddf8ed3b8be7fadb15994f2dc214b" => :x86_64_linux
   end
 
   needs :openmp
