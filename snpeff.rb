f17e89bbe2a497b97594c20943a55d78c827b7ad
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-15 11:12:23 +0000

    snpeff: update 4.3i bottle for Linuxbrew.

--- snpeff.rb
+++ snpeff.rb
@@ -13,6 +13,7 @@ class Snpeff < Formula
     sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :sierra
     sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :el_capitan
     sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :yosemite
+    sha256 "ae791f614cde0602d42da9c83cc352d143982bef4e5119ec497f58831a9a17ef" => :x86_64_linux
   end
 
   depends_on :java => "1.7+"
