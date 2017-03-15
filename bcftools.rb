fb2ac5919bf2f17bc617b747576f6f4d0e8900c7
ilovezfs <ilovezfs@icloud.com>
2017-03-15 04:49:53 -0700

    bcftools 1.4
    Closes #5265.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- bcftools.rb
+++ bcftools.rb
@@ -3,9 +3,8 @@ class Bcftools < Formula
   homepage "http://www.htslib.org/"
   # tag "bioinformatics"
 
-  url "https://github.com/samtools/bcftools/releases/download/1.3.1/bcftools-1.3.1.tar.bz2"
-  sha256 "12c37a4054cbf1980223e2b3a80a7fdb3fd850324a4ba6832e38fdba91f1b924"
-  head "https://github.com/samtools/bcftools.git"
+  url "https://github.com/samtools/bcftools/releases/download/1.4/bcftools-1.4.tar.bz2"
+  sha256 "8fb1b0a47ed4e1f9d7c70129d7993aa650da1688fd931b10646d1c4707ae234d"
 
   bottle do
     sha256 "aa497ce3f323c1d84ab6f9fd52bad51764ac647503c31c2cd031aef3b3d6c98e" => :el_capitan
@@ -14,30 +13,22 @@ class Bcftools < Formula
     sha256 "538d88065ed818fb4e9d997e7e7bb41b4bd6a5559600fbffe4585dd6bcf5879c" => :x86_64_linux
   end
 
-  option "with-polysomy", "Enable polysomy command. Makes licence GPL3 not MIT/Expat."
+  option "with-gsl", "Enable polysomy command. Makes licence GPL3 not MIT/Expat."
 
-  depends_on "gsl" if build.with? "polysomy"
-  depends_on "htslib"
+  deprecated_option "with-polysomy" => "with-gsl"
+
+  depends_on "xz"
   depends_on "samtools" => :recommended
+  depends_on "gsl" => :optional
 
   def install
-    inreplace "Makefile", "include $(HTSDIR)/htslib.mk", ""
-    htslib = Formula["htslib"].opt_prefix
-    args = %W[make all install prefix=#{prefix} HTSDIR=#{htslib}/include HTSLIB=#{htslib}/lib/libhts.a]
-
-    if build.with? "polysomy"
-      args << "USE_GPL=1"
-      gsl = Formula["gsl"].opt_prefix
-      inreplace "Makefile", "-DUSE_GPL", "-DUSE_GPL -I#{gsl}/include -L#{gsl}/lib"
-      inreplace "Makefile", "-lcblas", "-lgslcblas"
-    end
-
-    system *args
-
+    args = build.with?("gsl") ? "USE_GPL=1" : []
+    system "make", "all", "install", "prefix=#{prefix}", *args
     pkgshare.install "test"
   end
 
   test do
-    assert_match "number of SNPs:\t3", shell_output("bcftools stats #{pkgshare}/test/query.vcf")
+    output = shell_output("#{bin}/bcftools stats #{pkgshare}/test/query.vcf")
+    assert_match "number of SNPs:\t3", output
   end
 end
