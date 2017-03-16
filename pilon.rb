9f8229cc6449372368e0a377d5312aea6ba74c70
ilovezfs <ilovezfs@icloud.com>
2017-03-16 04:15:29 -0700

    pilon 1.22
    Closes #5279.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- pilon.rb
+++ pilon.rb
@@ -1,8 +1,8 @@
 class Pilon < Formula
   desc "Improve draft assemblies and find variation"
   homepage "https://github.com/broadinstitute/pilon/wiki"
-  url "https://github.com/broadinstitute/pilon/releases/download/v1.21/pilon-1.21.jar"
-  sha256 "a57a834b8ecb468399d0c9d55b8d8aee5ecbd8310b8e245d421b6c25d6534910"
+  url "https://github.com/broadinstitute/pilon/releases/download/v1.22/pilon-1.22.jar"
+  sha256 "ff738f3bbb964237f6b2cf69243ebf9a21cb7f4edf10bbdcc66fa4ebaad5d13d"
   head "https://github.com/broadinstitute/pilon.git"
   # doi "10.1371/journal.pone.0112963"
   # tag "bioinformatics"
