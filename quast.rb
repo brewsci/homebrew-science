2c67cf050f47c7c5feb3c4ffa2baa1be363960f6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-17 13:47:59 +0000

    quast: update 4.5 bottle for Linuxbrew.

--- quast.rb
+++ quast.rb
@@ -11,6 +11,7 @@ class Quast < Formula
     sha256 "b0e39c8e7a45032b00c87110e618def5ab305cebe3b3fbf9bc44b080fb09e972" => :sierra
     sha256 "33c358b0970717d2410dc41b64fb5dd3e141e7f3f87893ceda36c750cba78bf0" => :el_capitan
     sha256 "38a0923ae4806858a7099ba64e1f8d2d7100ec13ee703c0f34addf2328db93b6" => :yosemite
+    sha256 "b7d7548f11945ad2ca7484b9a31552c319cf6c027d7dc825f351a7f3960e725d" => :x86_64_linux
   end
 
   if OS.mac? && MacOS.version <= :mountain_lion
