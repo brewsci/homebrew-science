02204f3c737aea9d88b22dd53bcfd88d4da3e568
ilovezfs <ilovezfs@icloud.com>
2017-03-25 09:26:22 -0700

    breseq 0.30.0
    Closes #5317.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- breseq.rb
+++ breseq.rb
@@ -1,6 +1,12 @@
 class Breseq < Formula
   desc "Find mutations in microbes from short reads"
   homepage "http://barricklab.org/twiki/bin/view/Lab/ToolsBacterialGenomeResequencing"
+  url "https://github.com/barricklab/breseq/releases/download/v0.30.0/breseq-0.30.0-Source.tar.gz"
+  sha256 "a5504a9bf7967a773ce28519ff4619c33f6b7f7efaf45ad8ed58273c5dea290d"
+  head "https://github.com/barricklab/breseq.git"
+  # doi "10.1007/978-1-4939-0554-6_12"
+  # tag "bioinformatics"
+
   bottle do
     cellar :any_skip_relocation
     sha256 "780ba849d784e3ff5fd5ae0a0906c5108fd50ad30c2a257f60465633919333ee" => :el_capitan
@@ -8,14 +14,6 @@ class Breseq < Formula
     sha256 "e4d5b3c2dc68a539c00fbae40bf7eab32131d1bf4113f3159b10c97fedd44def" => :mavericks
     sha256 "6d370e68d3cbddc76d078fb9129ebe4211cbb6b1454cce5759458bdf8e9bb873" => :x86_64_linux
   end
-
-  # doi "10.1007/978-1-4939-0554-6_12"
-  # tag "bioinformatics"
-
-  url "https://github.com/barricklab/breseq/releases/download/v0.28.1/breseq-0.28.1.Source.tar.gz"
-  sha256 "234e9c7fc45a88b9a005160a5b1247486eb00fcea2846d9091008208ea219aa0"
-
-  head "https://github.com/barricklab/breseq.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
