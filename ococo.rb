6f338b919f086488a3a9f020d3286fafef3a8387
ilovezfs <ilovezfs@icloud.com>
2017-03-25 10:08:05 -0700

    ococo 0.1.2.4
    Closes #5324.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- ococo.rb
+++ ococo.rb
@@ -1,8 +1,8 @@
 class Ococo < Formula
   desc "Ococo, the first online consensus caller"
   homepage "https://github.com/karel-brinda/ococo"
-  url "https://github.com/karel-brinda/ococo/archive/0.1.2.1.tar.gz"
-  sha256 "9fb55b1fad3647bc651a638851fe39c5c7151a0718034f22bf3129e29a1d965e"
+  url "https://github.com/karel-brinda/ococo/archive/0.1.2.4.tar.gz"
+  sha256 "1716b012acd7e08949ccfbc491d8b018c69b10128bff558f43624a176c871109"
 
   head "https://github.com/karel-brinda/ococo.git"
 
@@ -14,15 +14,17 @@ class Ococo < Formula
   end
 
   depends_on "htslib"
+  depends_on "xz"
 
   def install
+    inreplace "Makefile", /^(LIBS +)( =.*)/, "\\1\\2\n\\1+= -llzma -lbz2"
     system "make", "HTSLIBINCLUDE=#{Formula["htslib"].opt_include}", "HTSLIB=#{Formula["htslib"].opt_lib}/libhts.a"
     bin.install "ococo"
     man1.install "ococo.1"
   end
 
   test do
-    system "#{bin}/ococo -v"
+    system "#{bin}/ococo", "-v"
     (testpath/"test.sam").write "@SQ\tSN:chrom1\tLN:42424242\nread\t0\tchrom1\t2798553\t60\t5M\t*\t0\t0\tAAAGG\t*"
     assert_match "5", shell_output("#{bin}/ococo -i test.sam -P - 2>/dev/null | wc -l")
   end
