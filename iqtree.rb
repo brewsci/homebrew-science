04a632074e87ec0be8df3fc21a844e92950af97a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-25 17:09:52 +0000

    iqtree: add 1.5.3 bottle for Linuxbrew.

--- iqtree.rb
+++ iqtree.rb
@@ -6,6 +6,7 @@ class Iqtree < Formula
     sha256 "12b58df62c8739f9d8e1d344b212d3950b33747b0cc2b790a8f01ac8f087ba7e" => :sierra
     sha256 "d26c7c16dd99407f6e2f5f9b5e6f218b6a4cc02baf14d585c08cf2e20e815d16" => :el_capitan
     sha256 "9624ac5393fac9a4aa3c6eb398cf5ef9aa73c27b9142dcec3bd4e4b68eeab44d" => :yosemite
+    sha256 "4aeacfec24bc050d41ae6c4455fb2d497ad62633657e045cec61da0ffea59e36" => :x86_64_linux
   end
 
   # doi "10.1093/molbev/mst024", "10.1093/molbev/msu300", "10.1093/sysbio/syw037"
