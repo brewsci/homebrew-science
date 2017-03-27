b2cba518bb625a4597b2b3c48f1f6a784e4b1ff0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-27 20:56:41 +0100

    oma: add 2.1.1 bottle.

--- oma.rb
+++ oma.rb
@@ -4,6 +4,13 @@ class Oma < Formula
   url "http://omabrowser.org/standalone/OMA.2.1.1.tgz"
   sha256 "c21fde703a1d13bd759ebc724e09b6d83b9e88abacc7cc1a78fee76972f93ec1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "82cd1c3c7e0f6acfa5cf5e74e13f133a98c712d0d9e36785eb1c8718ebd69dba" => :sierra
+    sha256 "3a5199bf93d7e95af13be8d7b76d7cbb12362683ede8d63d946b8526737ab8d8" => :el_capitan
+    sha256 "d4be2c5916d41dccab7b233714abb5f94dca78acff5cddb72207aed3f351923c" => :yosemite
+  end
+
   # tag "bioinformatics"
   # doi "10.1093/nar/gku1158"
 
