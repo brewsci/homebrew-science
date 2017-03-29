01222dba2fd13de4593c2c0447176fff1729e411
Jorden Whitefield <j.whitefield@surrey.ac.uk>
2017-03-29 12:35:43 +0100

    tamarin-prover: Build Tamarin with SAPIC as default
    The Stateful Applied Pi Calculus plugin now built.
    Added OCaml as build dependency.
    
    Closes #5346.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- tamarin-prover.rb
+++ tamarin-prover.rb
@@ -3,6 +3,7 @@ class TamarinProver < Formula
   homepage "https://tamarin-prover.github.io/"
   url "https://github.com/tamarin-prover/tamarin-prover/archive/1.2.1.tar.gz"
   sha256 "3c8a91c23896f61f1c04b8121125b3fd603191522512ef5a459260d89fdd911a"
+  revision 1
   head "https://github.com/tamarin-prover/tamarin-prover.git", :branch => "develop"
 
   bottle do
@@ -13,6 +14,7 @@ class TamarinProver < Formula
   end
 
   depends_on "haskell-stack" => :build
+  depends_on "ocaml" => :build
   depends_on "maude"
   depends_on "graphviz"
   depends_on :macos => :mountain_lion
@@ -22,12 +24,11 @@ class TamarinProver < Formula
 
   def install
     # Let `stack` handle its own parallelization
-    # This deparallelization prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
-    ENV.deparallelize do
-      system "make", "tamarin"
-    end
+    # Deparallelization prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
+    ENV.deparallelize
+    system "make", "default"
 
-    bin.install ".brew_home/.local/bin/tamarin-prover"
+    bin.install Dir[".brew_home/.local/bin/*"]
   end
 
   test do
