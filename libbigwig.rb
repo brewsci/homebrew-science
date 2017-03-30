472aaada48c1fd6822de3924d4def473c5ee63fa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-30 11:06:51 +0000

    libbigwig: update 0.3.3 bottle for Linuxbrew.

--- libbigwig.rb
+++ libbigwig.rb
@@ -10,6 +10,7 @@ class Libbigwig < Formula
     sha256 "adf5d1e84f5fc696194caaa3a4f44902c35c69cb8a63f0c049e1bae786671bab" => :sierra
     sha256 "b1efca94cc405e840a50ef67f8737a0b4e4d195133b8cff57cf6a69ecf660b87" => :el_capitan
     sha256 "f5b1be46498fd1d89995d36e72a62ade524b00adab054ddbe4a440738f26033e" => :yosemite
+    sha256 "0888f2796d367efa0dc83fd6f8303ffb4ca803657891a6b32fee0968f1327b46" => :x86_64_linux
   end
 
   depends_on "curl"
