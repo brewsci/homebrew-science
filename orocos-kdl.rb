01deeac50bdf32a753c7f9e53c3ce34cc2231ffe
ilovezfs <ilovezfs@icloud.com>
2017-04-03 05:41:32 -0700

    orocos-kdl 1.3.1
    Closes #5372.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- orocos-kdl.rb
+++ orocos-kdl.rb
@@ -1,8 +1,8 @@
 class OrocosKdl < Formula
+  desc "Orocos Kinematics and Dynamics C++ library"
   homepage "http://www.orocos.org/kdl"
-  url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.3.0.tar.gz"
-  sha256 "7be2dd5e4f4c1ceac2cdf1f4fae3d94d4ffd9fc1af8d483c05f04e80ef84b3f9"
-  revision 2
+  url "https://github.com/orocos/orocos_kinematics_dynamics/archive/v1.3.1.tar.gz"
+  sha256 "aff361d2b4e2c6d30ae959308a124022eeef5dc5bea2ce779900f9b36b0537bd"
   head "https://github.com/orocos/orocos_kinematics_dynamics.git"
 
   bottle do
@@ -22,10 +22,6 @@ class OrocosKdl < Formula
 
   def install
     cd "orocos_kdl" do
-      # Removes solvertest from orocos-kdl, as per
-      # http://www.orocos.org/forum/rtt/rtt-dev/bug-1043-new-errors-underlying-ik-solver-are-not-correctly-processed
-      inreplace "tests/CMakeLists.txt", "ADD_TEST(solvertest solvertest)", "" if build.head?
-
       mkdir "build" do
         eigen_include_dir = Formula["eigen@3.2"].opt_include/"eigen3"
         args = std_cmake_args << "-DEIGEN3_INCLUDE_DIR=#{eigen_include_dir}"
