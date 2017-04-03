68678bd10427522eecf921a4d954deaaadde7e84
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-03 13:54:55 +0100

    mir-prefer: add 0.24 bottle.

--- mir-prefer.rb
+++ mir-prefer.rb
@@ -7,6 +7,13 @@ class MirPrefer < Formula
   sha256 "457545478e2d3bc7497d350f3972cf0855b82fa7cb0263a6d91756732f487faf"
   head "https://github.com/hangelwen/miR-PREFeR.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8a5e4e1bc3293170a2b76b7edb706e4330aee2af5f35344d6ea0e77adb7ebce9" => :sierra
+    sha256 "adad9852553b173f5ac03de1741ecf2338d4233f800acf8056b6a3d94c199497" => :el_capitan
+    sha256 "adad9852553b173f5ac03de1741ecf2338d4233f800acf8056b6a3d94c199497" => :yosemite
+  end
+
   depends_on "samtools"
   depends_on "viennarna"
 
