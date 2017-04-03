76e6a0a86e318b5c4bac24b172843793331e0e47
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-03 13:21:54 +0100

    afra: update 2.1 bottle.

--- afra.rb
+++ afra.rb
@@ -9,11 +9,9 @@ class Afra < Formula
 
   bottle do
     cellar :any
-    sha256 "ef98735d2aa088b912caee2d31d9096af11eaed2dc1e539f33ba85d13320a65d" => :sierra
-    sha256 "a9ba59a547b32352bc65c26276b49d5cdcd2b9ff41a7ea169365d46f992ea0f2" => :el_capitan
-    sha256 "4e49727c5c250b6befd15f253829c75e597584121402b6d85e0b1b11fe8d392e" => :yosemite
-    sha256 "1f0d6421878a65f07582dd81642f351f4386022bf33ea79a8087178a048fe975" => :mavericks
-    sha256 "f7fb0b66080a819db4d9c2ca7f31bff05fb37caa67e07bbeb38b88f3599cc869" => :x86_64_linux
+    sha256 "fcbfe8aca9af7fd9409aa13aa8409ca9eb060fbb205ad7bedb82e5741f07a321" => :sierra
+    sha256 "94d67e2cf81bc3ee51b44a7e63c0579f4a5210be959e2c67663a929da437fb5e" => :el_capitan
+    sha256 "31d1a446faaddb4617a84700be5fc3d298eba84ac26f743987df0be818ce118b" => :yosemite
   end
 
   needs :openmp
