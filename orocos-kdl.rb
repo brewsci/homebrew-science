9afc37e36a26b997ab7c81613c21e87fe22a5b6a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-04 06:58:00 +0000

    orocos-kdl: update 1.3.1 bottle for Linuxbrew.

--- orocos-kdl.rb
+++ orocos-kdl.rb
@@ -10,6 +10,7 @@ class OrocosKdl < Formula
     sha256 "9974d7baa598a88040a1e810e0b2034aae8322b2c28a08747e1ac559155b32ec" => :sierra
     sha256 "c3c31cbc991a22aebb18dc1bd5ff67c8584d2f6341cd340cf4cedbe826b2b1f1" => :el_capitan
     sha256 "a61a02662fa0108135864ac311141abffd216d14d3a124a5bb1ca88a347e0079" => :yosemite
+    sha256 "ff623f0c81b0bcb21d4842ea5d802c08d897a8d9d8af3e7eebdc69e24620199b" => :x86_64_linux
   end
 
   option "without-test", "Disable build-time checking"
