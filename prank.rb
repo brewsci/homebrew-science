39c0d3628983e0264e69767dee3158a9185de13c
Shaun Jackman <sjackman@gmail.com>
2017-04-04 11:20:14 -0700

    prank: update 140603 bottle for Linuxbrew.
    Closes #5391.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- prank.rb
+++ prank.rb
@@ -11,6 +11,7 @@ class Prank < Formula
     sha256 "645b55928f814076559c0515ba5760006efbd6352d9599c09f769e5b05d9441a" => :yosemite
     sha256 "c935677f529955560e10caedadaa2294e7df2a29559691c8bd5c4f4406b782d7" => :mavericks
     sha256 "126c9194604ebf8af867ad12e7c621ff271ae7d17fa47dc509941ceb681eb263" => :mountain_lion
+    sha256 "3f9b5ba80cf44800d5bfec891de6ee5e37b00d5ec8dae8bcaa5f2ce7539e38d8" => :x86_64_linux
   end
 
   depends_on "mafft"
