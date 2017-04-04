c91b424d9da1157bfbca723a39fdb0b291a8f7fb
Shaun Jackman <sjackman@gmail.com>
2017-04-04 10:53:14 -0700

    afra: update 2.1 bottle for Linuxbrew.
    Closes #5388.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- afra.rb
+++ afra.rb
@@ -12,6 +12,7 @@ class Afra < Formula
     sha256 "fcbfe8aca9af7fd9409aa13aa8409ca9eb060fbb205ad7bedb82e5741f07a321" => :sierra
     sha256 "94d67e2cf81bc3ee51b44a7e63c0579f4a5210be959e2c67663a929da437fb5e" => :el_capitan
     sha256 "31d1a446faaddb4617a84700be5fc3d298eba84ac26f743987df0be818ce118b" => :yosemite
+    sha256 "1d4dc43e19ab9565736c41791fa9b653317f9e13e794eb88d233f28401a59d15" => :x86_64_linux
   end
 
   needs :openmp
