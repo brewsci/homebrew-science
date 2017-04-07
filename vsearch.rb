9f672889ebaabc3d1fe2e7ae662ea1171d2c7809
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-07 05:58:48 +0000

    vsearch: update 2.4.3 bottle for Linuxbrew.

--- vsearch.rb
+++ vsearch.rb
@@ -12,6 +12,7 @@ class Vsearch < Formula
     sha256 "73683bf339c36c4b845265c9a7f6daa9b8d5c3e7889038fdf8273d66fe1962ac" => :sierra
     sha256 "a045d20921761ca0204bc937b0ecea319d2bba361e0e3f6a356cf9a352c4ea42" => :el_capitan
     sha256 "cb91518460473f6092772b55084662edd88e0e87385e1eea817cd12782e0ce77" => :yosemite
+    sha256 "58038df3f6450cec2c22034f42ce982ba53c0a7289f5b2164b9bba44b84dace2" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
