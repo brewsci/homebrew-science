facd607d79b5b46cea25afa390860cd44ba16763
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-11 06:27:14 +0000

    quicktree: add 2.0 bottle for Linuxbrew.

--- quicktree.rb
+++ quicktree.rb
@@ -12,6 +12,7 @@ class Quicktree < Formula
     sha256 "8e5fc6dfd9189adec6fd97204dc4e5ef2d00ef05f4eb3e7819bb918855c0a9c6" => :sierra
     sha256 "22d08b0f4bbbcc1f92b2b8eb358155b2551ad4f3d344f3557b9b1ecd8eda0501" => :el_capitan
     sha256 "be3f595c268ff391ccd1351f8d91b5f09eb45e819630533f107c52d6d5b32846" => :yosemite
+    sha256 "7654424e618179f3f4a0efd269edc37c582f64523c7103645e5ab2eaf65a51fe" => :x86_64_linux
   end
 
   def install
