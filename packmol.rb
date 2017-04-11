a82f9f9975dc815e535f517adce21b55d074bff9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-11 09:35:25 +0100

    packmol: update 17.099 bottle.

--- packmol.rb
+++ packmol.rb
@@ -8,10 +8,9 @@ class Packmol < Formula
   # doi "10.1002/jcc.21224"
 
   bottle do
-    sha256 "0e943d006e8b48dc59fa8ffc7c93a616b037f9785460b4ac81dde07eddedcd72" => :sierra
-    sha256 "d4ce3e8b0eb0efaf4064c0969a55ecb86176adc0c56ed214dc023bc0f3e25fa6" => :el_capitan
-    sha256 "579e1d1dc835d6f5519d4439ef35700d9db674bc945dc37549e4dd64e2dae577" => :yosemite
-    sha256 "9a9cc6383b100b5e4112974916e72e8e6c2a47bb57103cfc8ec9f87f433b039b" => :x86_64_linux
+    sha256 "d8ac60326c0a8ccde27e1705af846491aa7fb3de2cee87732c3b66b3a18c67ac" => :sierra
+    sha256 "fbb2be0fafc1509369a3fcd146eda943c88f5710cf57dfa9f49dd0ba9ff298a4" => :el_capitan
+    sha256 "e3a0c637a2d5d7331440cef080595137682f3d85a80fac0d978d22eb9f385e51" => :yosemite
   end
 
   depends_on :fortran
