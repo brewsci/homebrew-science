628b0430fd2c627a5d77460e0dc2a532e7777ad6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-04-11 13:07:17 +0100

    xmi-msim 6.0
    Closes #5418.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- xmi-msim.rb
+++ xmi-msim.rb
@@ -1,9 +1,8 @@
 class XmiMsim < Formula
   desc "Monte Carlo simulation of X-ray fluorescence spectrometers"
   homepage "https://github.com/tschoonj/xmimsim"
-  url "https://xmi-msim.tomschoonjans.eu/xmimsim-5.0.tar.gz"
-  sha256 "3503b56bb36ec555dc941b958308fde9f4e550ba3de4af3b6913bc29c2c0c9f1"
-  revision 8
+  url "https://xmi-msim.tomschoonjans.eu/xmimsim-6.0.tar.gz"
+  sha256 "26520645d9e524436183090c2b8d3ea67cf1480e3b695b6feedf5790c436ac5c"
 
   bottle do
     sha256 "68903bf3ed8dcc8ab417856f8290055ce5f553f974599391a8be5ed50cc15947" => :sierra
@@ -20,32 +19,17 @@ class XmiMsim < Formula
   depends_on "hdf5"
   depends_on "pkg-config" => :build
   depends_on "xraylib" => "with-fortran"
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-
-  patch do
-    url "https://github.com/tschoonj/xmimsim/commit/682ff5b413fc326c1cc8e9931d34bce3e920c798.diff"
-    sha256 "bf59bcf0091a8f5b4680ab99e3bd0609bcd6558d79c1b71e0473ecb1ec4d0123"
-  end
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/a810aca/xmi-msim/gsl-2.x.patch"
-    sha256 "2e4a5789248b672ba73a0b8a2ba0f75773cf5c3905b7416e5e38864c67303813"
-  end
 
   def install
     ENV.deparallelize # fortran modules don't like parallel builds
 
-    system "autoreconf", "-i"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
                           "--disable-gui",
                           "--disable-updater",
                           "--disable-mac-integration",
-                          "--disable-libnotify",
-                          "--enable-opencl"
+                          "--disable-libnotify"
     system "make", "install"
   end
 
