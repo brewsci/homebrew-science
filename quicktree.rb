a5913567cc7c9682d30252c45111087ce35a6400
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-12 23:42:33 +0100

    quicktree: add 2.0 bottle.

--- quicktree.rb
+++ quicktree.rb
@@ -7,6 +7,13 @@ class Quicktree < Formula
 
   head "https://github.com/khowe/quicktree.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8e5fc6dfd9189adec6fd97204dc4e5ef2d00ef05f4eb3e7819bb918855c0a9c6" => :sierra
+    sha256 "22d08b0f4bbbcc1f92b2b8eb358155b2551ad4f3d344f3557b9b1ecd8eda0501" => :el_capitan
+    sha256 "be3f595c268ff391ccd1351f8d91b5f09eb45e819630533f107c52d6d5b32846" => :yosemite
+  end
+
   def install
     system "make"
     bin.install "quicktree"
