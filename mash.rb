6c970f9bab1cc718b2a3bd74a736e72b877959a1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-12 21:40:29 +0000

    mash: update 1.1.1_1 bottle for Linuxbrew.

--- mash.rb
+++ mash.rb
@@ -15,6 +15,7 @@ class Mash < Formula
     sha256 "f047fcf2ee8b811c2c23a2716796781799a3e5ed4512669662d886197eb14a73" => :el_capitan
     sha256 "b992bc061d2a5aa75e96421933495bf34ef567056fa67df6b3ca92781a95399e" => :yosemite
     sha256 "db8deedd40e695d6b90b609d4a208b1bc1233e01d5e24e0703ac8d68ce91082b" => :mavericks
+    sha256 "33a8f8bb68461efa0c6eea4efa247ea1974590e59c79e8d4bdcb65efb58d3427" => :x86_64_linux
   end
 
   needs :cxx11
