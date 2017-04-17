d55bcb0ce5efff4916c84e355d0002e824e576d7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-17 13:19:42 +0100

    vips: update 8.5.3 bottle.

--- vips.rb
+++ vips.rb
@@ -5,10 +5,9 @@ class Vips < Formula
   sha256 "606ca1b33bdda57bea76b6f62f5c92d0d0f9b5904da3835d7ec6ed5b10c59fbc"
 
   bottle do
-    sha256 "efb352552111d240990d7863242cb9999ef215452690e5047dd36f6fc915461f" => :sierra
-    sha256 "a975eaec8b5af0d0a52f8814df443405ffa930bb0254d0b297110c0f78b7e633" => :el_capitan
-    sha256 "c352ab87d0c35da9aba77d0a8a40dc15197c48992cebf8dc16fea2946978d8e9" => :yosemite
-    sha256 "ab70e9f07c3bcf62693fc9ccb8fe5d49ea023e4021526e21c4e9a7fa4d593307" => :x86_64_linux
+    sha256 "b9058ec7763e29e39ffb12eb9540215bcd496764c6c24703007400c8dcbf2441" => :sierra
+    sha256 "632bff910d0b1942a8174e19da9b699ce2676b61a68f74ac52e9e37de43b7255" => :el_capitan
+    sha256 "6d66a4270089a7df5ed5b38e18cf8a44fe4e772295e3940dea5b401219bdcc1a" => :yosemite
   end
 
   option "without-test", "Disable build time checks (not recommended)"
