93c1a21fa260d130449176cdaf7681014412f9b8
Shaun Jackman <sjackman@gmail.com>
2017-04-18 16:27:44 -0700

    kraken: Add desc, doi and tag bioinformatics

--- kraken.rb
+++ kraken.rb
@@ -1,7 +1,10 @@
 class Kraken < Formula
+  desc "Assign taxonomic labels to short DNA sequences"
   homepage "http://ccb.jhu.edu/software/kraken/"
   url "https://ccb.jhu.edu/software/kraken/dl/kraken-0.10.5-beta.tgz"
   sha256 "7c0ac64ee0acdcce18e16b51b636b7cdc6d07ea7ab465bb64df078c5a710346b"
+  # doi "10.1186/gb-2014-15-3-r46"
+  # tag "bioinformatics"
 
   bottle do
     cellar :any
