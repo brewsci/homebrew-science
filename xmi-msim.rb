35558d010757ac589753569acd28b219424b7c5c
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-04-19 08:40:12 +0100

    xmi-msim: add support for HDF5 1.10.0
    remove xraylib with-fortran dependency as the fortran option in the
    xraylib formula is now :recommended

--- xmi-msim.rb
+++ xmi-msim.rb
@@ -3,6 +3,7 @@ class XmiMsim < Formula
   homepage "https://github.com/tschoonj/xmimsim"
   url "https://xmi-msim.tomschoonjans.eu/xmimsim-6.0.tar.gz"
   sha256 "26520645d9e524436183090c2b8d3ea67cf1480e3b695b6feedf5790c436ac5c"
+  revision 1
 
   bottle do
     sha256 "de36351cb8eca8086c1d8a56ed2233260adc937eb0b2cf8297ede219685f7483" => :sierra
@@ -11,6 +12,7 @@ class XmiMsim < Formula
     sha256 "bf694dd48ee2fc03f5fd9ef2e6f68d819744642ba2454273e5014fb449369260" => :x86_64_linux
   end
 
+  depends_on "pkg-config" => :build
   depends_on :fortran
   depends_on "gsl"
   depends_on "fgsl"
@@ -18,8 +20,13 @@ class XmiMsim < Formula
   depends_on "libxslt"
   depends_on "glib"
   depends_on "hdf5"
-  depends_on "pkg-config" => :build
-  depends_on "xraylib" => "with-fortran"
+  depends_on "xraylib"
+
+  # add support for HDF5 1.10.0
+  patch do
+    url "https://github.com/tschoonj/xmimsim/commit/1459971313ea4a3cbbfdc87332b91dfcdfc0f3d7.diff"
+    sha256 "d5d435a420b0b089f103173a143ad6e94718967257fe835f8cecd32ff19c2bb4"
+  end
 
   def install
     ENV.deparallelize # fortran modules don't like parallel builds
