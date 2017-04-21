0d24a5e5f9117e2a3c9948e930bc2cb2c5739be3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-21 02:52:44 +0000

    analysis: update 0.8.8_2 bottle for Linuxbrew.

--- analysis.rb
+++ analysis.rb
@@ -10,6 +10,7 @@ class Analysis < Formula
     sha256 "2dcdbb025d1a1c68d6ec551bf1a6a6af663c0e94e19ff1faacea2f106bc7a790" => :sierra
     sha256 "825c7efb29eb13e9d85a4841a7f16788e1aa7d6eec92c43c47f943216ec181c3" => :el_capitan
     sha256 "a8fb8144516347f9722b79044108e41a668152fc6649b01dcb02efff2fc1a2e0" => :yosemite
+    sha256 "fa8c4a067de7239f06910d6af45daa7decffceb56475b300f5c8947f78d27190" => :x86_64_linux
   end
 
   depends_on "gsl"
