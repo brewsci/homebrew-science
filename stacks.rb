65030ce8ce2738b1db68c813e1163d153a763b81
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-21 02:02:51 +0000

    stacks: update 1.46 bottle for Linuxbrew.

--- stacks.rb
+++ stacks.rb
@@ -11,6 +11,7 @@ class Stacks < Formula
     sha256 "0cf5db2f8aa026d172d3b15dc219febab62361b5257890be02be6b5d216545a8" => :sierra
     sha256 "f333436bf678e1122276b4d3de7c47c399cf26c4ae120f3cfbc1be550a5db06b" => :el_capitan
     sha256 "99474e4f01966e2faf92c43cb20db1fe451fbe5d57a0c9d2b6c4a77dfd85e673" => :yosemite
+    sha256 "954189eeaf39bd6a2ed66f1f3b842762bf112aa4f93b28f0ed42aacf9eef4da1" => :x86_64_linux
   end
 
   depends_on "htslib"
