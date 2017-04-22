d149f66c5d5c52e1a2db2f4a04178439e33a1397
Michka Popoff <michkapopoff@gmail.com>
2017-04-22 17:12:16 +0200

    therion: migrate tcl-tk from dupes to core
    Also fixed:
    * The URL http://therion.speleo.sk could use HTTPS rather than HTTP
    * Stable: The URL http://therion.speleo.sk/downloads/therion-5.3.16.tar.gz could use HTTPS rather than HTTP
    * :tex is deprecated
    
    Closes #5484.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- therion.rb
+++ therion.rb
@@ -1,27 +1,20 @@
 class Therion < Formula
   desc "Processes survey data and generates maps or 3D models of caves"
-  homepage "http://therion.speleo.sk"
-  url "http://therion.speleo.sk/downloads/therion-5.3.16.tar.gz"
+  homepage "https://therion.speleo.sk"
+  url "https://therion.speleo.sk/downloads/therion-5.3.16.tar.gz"
   sha256 "73cda5225725d3e8cadd6fada9e506ab94b093d4e7a9fc90eaf23f8c7be6eb85"
-  revision 1
-
-  bottle :disable, "homebrew/dupes/tcl-tk dependency causes CI to fail for VTK"
-
-  option "with-tex", "Build documentation"
+  revision 2
 
   depends_on "freetype"
   depends_on "imagemagick"
   depends_on "lcdf-typetools"
-  depends_on :tex => :optional
   depends_on "vtk"
   depends_on "wxmac"
-  depends_on "homebrew/dupes/tcl-tk" if MacOS.version >= :sierra
+  depends_on "tcl-tk" if MacOS.version >= :sierra
 
   def install
-    if build.without? "tex"
-      inreplace "Makefile", "all: outdirs $(OUTDIR)/therion doc ",
-                            "all: outdirs $(OUTDIR)/therion "
-    end
+    inreplace "Makefile", "all: outdirs $(OUTDIR)/therion doc ",
+                          "all: outdirs $(OUTDIR)/therion "
 
     inreplace "makeinstall.tcl" do |s|
       s.gsub! "/usr/bin", bin
