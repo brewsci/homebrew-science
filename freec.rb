92e4d5642297e13ecc20691741e2c72e7272ebc2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-23 19:00:58 +0000

    freec: update 10.6 bottle for Linuxbrew.

--- freec.rb
+++ freec.rb
@@ -12,6 +12,7 @@ class Freec < Formula
     sha256 "36fbc93543b4f5e4703461b25ed14e099983d4dde3f1e329b0cb911bbbe350a3" => :sierra
     sha256 "3283bcbec7ff79337ea130585a615462714a27aba9bc259167735b2dbd6c0613" => :el_capitan
     sha256 "1a57935e21a4404f234da9b4c8fbd27ed4451f193947c78e0e59764a92066bf0" => :yosemite
+    sha256 "206a459a742525ab32a845c395327bbadab9a14140054eb5e4980bea3dcf5f7d" => :x86_64_linux
   end
 
   def install
