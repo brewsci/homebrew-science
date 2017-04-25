eb355a89289fd1270dd4414be3120c33c635bc0f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-25 14:56:20 +0000

    therion: add 5.3.16_2 bottle.

--- therion.rb
+++ therion.rb
@@ -5,6 +5,12 @@ class Therion < Formula
   sha256 "73cda5225725d3e8cadd6fada9e506ab94b093d4e7a9fc90eaf23f8c7be6eb85"
   revision 2
 
+  bottle do
+    sha256 "216b16038889a32efa33f2907ad8192d57be7edcac41f5f904447233c749df57" => :sierra
+    sha256 "4d7569ce586aaccd35a9a4f2e0037c7dd8c8c7acb020cf3fe211a4589adc7536" => :el_capitan
+    sha256 "bdff18440f953e05e71fff399c40d24b5e3ed05e1a3fc733a5471f1b6f3214eb" => :yosemite
+  end
+
   depends_on "freetype"
   depends_on "imagemagick"
   depends_on "lcdf-typetools"
