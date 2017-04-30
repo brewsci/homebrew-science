9ddcd3b9861161e04814af084be59a510b4ee3c2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-30 07:58:23 +0000

    kissplice: add 2.4.0-p1 bottle.

--- kissplice.rb
+++ kissplice.rb
@@ -12,6 +12,13 @@ class Kissplice < Formula
     :using => KisspliceDownloadStrategy
   sha256 "f4569b444cd0b10eba2a5c3f883d3fcbaf092201a9ebdc4d4c0030b4641676f7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "11d2fc4398c44899d258555b391ccd75895b1c84e0f3204c8e07627b4e166f10" => :sierra
+    sha256 "bad8602b1046b940e9a5fec2ceec34c4bab7c26bb7f17a157c6b1c040066dbdc" => :el_capitan
+    sha256 "6ff193c28f3548b46aaf7a43b8ef91550e6ba74d4dd32052ac03175c0aea7f66" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "zlib" unless OS.mac?
 
