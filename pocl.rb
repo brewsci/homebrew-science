0b37da7f365eb8ba391feaf33e1d5537297c27f4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-01 09:11:52 +0000

    pocl: add 0.14 bottle for Linuxbrew.

--- pocl.rb
+++ pocl.rb
@@ -8,6 +8,7 @@ class Pocl < Formula
     sha256 "e840d7622cc3a4e2991cee46438f4338d3d792b880df87fd997f495c977c4fde" => :sierra
     sha256 "750fd4166e8cddc574d57e283eec0d7a2e8fa50831c42d6e24a5f989fbe2e9b8" => :el_capitan
     sha256 "0c57dd7d46167d461626abf00d3635cdd39086bda540c84bffd85c99fad7d2de" => :yosemite
+    sha256 "6a531d396319978d4c69ba6315832b3c23ee27aedb2b4296836c7c239d319a8f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
