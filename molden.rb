e99e92fe21675f5362f91bd3805d1120e5fb050d
FX Coudert <fxcoudert@gmail.com>
2017-05-02 21:57:25 +0200

    molden: revision for gcc
    Closes #5556.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- molden.rb
+++ molden.rb
@@ -2,7 +2,8 @@ class Molden < Formula
   desc "Pre- and post-processing of molecular and electronic structure"
   homepage "http://www.cmbi.ru.nl/molden/"
   url "ftp://ftp.cmbi.ru.nl/pub/molgraph/molden/molden5.7.tar.gz"
-  sha256 "8c52229f1762e987995c53936f0dc2cfd086277ec777bf9083d76c0bf0483887"
+  sha256 "5e7b3a8bf9251626d362b1d1dd8e4362054e6bce505c43e10254265857bb6e7d"
+  revision 1
   # tag "chemistry"
   # doi "10.1023/A:1008193805436"
 
