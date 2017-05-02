38b2f9a96d61029dde176c954a3e2913564eff67
FX Coudert <fxcoudert@gmail.com>
2017-05-02 22:03:14 +0200

    tisean: revision for gcc
    Closes #5527.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- tisean.rb
+++ tisean.rb
@@ -3,7 +3,7 @@ class Tisean < Formula
   homepage "http://www.mpipks-dresden.mpg.de/~tisean/"
   url "http://www.mpipks-dresden.mpg.de/~tisean/TISEAN_3.0.1.tar.gz"
   sha256 "cd6662505a2e411218f5d34ccb8bf206a6148b6c79b1cc8e4fa4dc11dfd00534"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
