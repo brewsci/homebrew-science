1861d56a9701e71329418d20b6f52c13096b3cc9
FX Coudert <fxcoudert@gmail.com>
2017-05-02 21:57:46 +0200

    octave: revision for gcc
    Closes #5554.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- octave.rb
+++ octave.rb
@@ -1,7 +1,7 @@
 class Octave < Formula
   desc "High-level interpreted language for numerical computing"
   homepage "https://www.gnu.org/software/octave/index.html"
-  revision 1
+  revision 2
 
   stable do
     url "https://ftpmirror.gnu.org/octave/octave-4.2.1.tar.gz"
