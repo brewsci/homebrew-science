79ddc2da78d81c377c33d3a3d99cc7ba4b3b0044
FX Coudert <fxcoudert@gmail.com>
2017-05-02 21:51:20 +0200

    dotwrp: revision for gcc
    Closes #5572.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- dotwrp.rb
+++ dotwrp.rb
@@ -2,7 +2,7 @@ class Dotwrp < Formula
   homepage "https://github.com/tenomoto/dotwrp"
   url "https://github.com/tenomoto/dotwrp/archive/v1.1.tar.gz"
   sha256 "369101851ecdffe89895ff91d21e535ef2df9c00c186f9e4f9443c709dfd825d"
-  revision 1
+  revision 2
 
   head "https://github.com/tenomoto/dotwrp.git"
 
