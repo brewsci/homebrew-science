d52ca2c9e27e51008afdf188997b1050b27c3bb6
FX Coudert <fxcoudert@gmail.com>
2017-05-02 21:56:38 +0200

    lis: revision for gcc, add test, fix audit
    Closes #5560.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- lis.rb
+++ lis.rb
@@ -1,8 +1,9 @@
 class Lis < Formula
+  desc "Library of Iterative Solvers for Linear Systems"
   homepage "http://www.ssisc.org/lis"
   url "http://www.ssisc.org/lis/dl/lis-1.4.34.tar.gz"
   sha256 "e25fb5ef0c52fa0c66efab626da7d1a4b4082776173ffce632a034ab73a4d292"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -11,9 +12,11 @@ class Lis < Formula
     sha256 "3b5cf44e575fd9a4cba5184597444c3b0d8f75839b9ee91753840e8a36a1dc44" => :mavericks
   end
 
-  option "without-check", "Skip build-time checks (not recommended)"
+  option "without-test", "Skip build-time tests (not recommended)"
   option "with-saamg",    "build SA-AMG preconditioner"
   option "with-quad",     "enable quadruple precision operations"
+
+  deprecated_option "without-check" => "without-test"
 
   depends_on :fortran => :recommended
   depends_on :mpi => [:cc, :cxx, :f77, :f90, :recommended]
@@ -21,16 +24,23 @@ class Lis < Formula
   def install
     ENV.deparallelize
 
-    args = %W[ --prefix=#{prefix} --disable-dependency-tracking --enable-shared]
+    args = %W[--prefix=#{prefix} --disable-dependency-tracking --enable-shared]
     args << "--enable-fortran" if build.with? :fortran
     args << "--enable-mpi"     if build.with? :mpi
     args << "--enable-omp" unless ENV.compiler == :clang
     args << "--enable-saamg"   if build.with? "saamg"
     args << "--enable-quad"    if build.with? "quad"
 
+    # Put examples in pkgshare
+    inreplace "test/Makefile.in", "share/examples/lis", "share/lis/examples"
+
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? "check"
     system "make", "install"
+  end
+
+  test do
+    system "#{pkgshare}/examples/test.sh"
   end
 end
