0fd8e2e2dc881007595f1a207381721b617ac0d2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-04 20:54:31 +0000

    raxml: update 8.2.10_1 bottle for Linuxbrew.

--- raxml.rb
+++ raxml.rb
@@ -13,6 +13,7 @@ class Raxml < Formula
     sha256 "358239c9ca84dd007d7052c8d9a1de9be51b8f878831c59d9aedd02252c263ea" => :sierra
     sha256 "1b11094806698b3e330d4880b36e46157af341e07ef3498b36eb5dacfe69d8c2" => :el_capitan
     sha256 "52381f2e1f0c7dbd234863272738ef7ac81761f19cfe0f780496f4689c11fa01" => :yosemite
+    sha256 "8a55594b0e3fe6b7933280e0b925ffad15cff1f5c24d8d9e284fba0d88d059ba" => :x86_64_linux
   end
 
   depends_on :fortran
