744a1db0d53946ead7402546b3a2fc9a9af1431c
Jonathan Chang <jchang641@gmail.com>
2017-05-04 16:21:47 -0700

    hypre: fix audits and update URLs

--- hypre.rb
+++ hypre.rb
@@ -1,10 +1,10 @@
 class Hypre < Formula
   desc "Library featuring parallel multigrid methods for grid problems"
   homepage "http://computation.llnl.gov/casc/hypre/software.html"
-  url "http://ftp.mcs.anl.gov/pub/petsc/externalpackages/hypre-2.11.1.tar.gz"
-  mirror "http://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.11.1.tar.gz"
+  url "https://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.11.1.tar.gz"
   sha256 "6bb2ff565ff694596d0e94d0a75f0c3a2cd6715b8b7652bc71feb8698554db93"
   revision 1
+  head "https://github.com/LLNL/hypre.git"
 
   bottle do
     cellar :any_skip_relocation
