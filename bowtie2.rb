911d656ac6b5445b4911ade7b8f7d37f1f7d3e2c
ilovezfs <ilovezfs@icloud.com>
2017-05-05 21:24:25 -0700

    bowtie2 2.3.2
    Closes #5602.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- bowtie2.rb
+++ bowtie2.rb
@@ -3,21 +3,9 @@ class Bowtie2 < Formula
   homepage "https://bowtie-bio.sourceforge.io/"
   # doi "10.1038/nmeth.1923"
   # tag "bioinformatics"
+  url "https://github.com/BenLangmead/bowtie2/archive/v2.3.2.tar.gz"
+  sha256 "20653fec933c23c8c792c8101b8a1db9b55a694ee797afcfba97be85532e8a43"
   head "https://github.com/BenLangmead/bowtie2.git"
-
-  stable do
-    url "https://github.com/BenLangmead/bowtie2/archive/v2.3.0.tar.gz"
-    sha256 "9804fddf36233f3f92c11e2250224de3395790cf35c8280c66387075df078221"
-
-    # Remove for > 2.3.0
-    # Prevents tophat test from failing
-    # Upstream commit from 7 Feb 2017 "Prevent aligner from failing when given
-    # empty FASTQ read files"
-    patch do
-      url "https://github.com/BenLangmead/bowtie2/commit/2e9b960.patch"
-      sha256 "074b07ac7b1d1a79469df30e557ea8d8739463298a66b8cee888e26dfde7e4fc"
-    end
-  end
 
   bottle do
     cellar :any
@@ -32,6 +20,8 @@ class Bowtie2 < Formula
   depends_on "tbb" => :recommended
 
   def install
+    ENV.delete("SDKROOT") if MacOS.version == :el_capitan
+
     if build.with? "tbb"
       system "make", "install", "WITH_TBB=1",
              "EXTRA_FLAGS=-L #{HOMEBREW_PREFIX}/lib",
