ae5293d1f2811147fd7e9ba76797e3b1f08c2cd0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-06 20:07:22 +0000

    gnuastro: add 0.2 bottle.

--- gnuastro.rb
+++ gnuastro.rb
@@ -4,6 +4,12 @@ class Gnuastro < Formula
   url "https://ftp.gnu.org/gnu/gnuastro/gnuastro-0.2.tar.gz"
   sha256 "3b26190d47dc2d3f1d00ab0799c9fec9ce9bfa931e2e4f924ff98587ff792901"
 
+  bottle do
+    sha256 "ede3ca24ee5c9c4edac3a1b9c714a50adbdc4741154a60d33bfa658ec85f91de" => :sierra
+    sha256 "07ce8883c40f64db183a41b5559d38abce69b8ecf59127e80aefb13f644d1267" => :el_capitan
+    sha256 "2b36e100d8e5fdaa20897ca91855f1f463f6db06df395df346bce3b610d509e9" => :yosemite
+  end
+
   depends_on "cfitsio"
   depends_on "gsl"
   depends_on "wcslib"
