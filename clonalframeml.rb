71e311fa9330257883d2a6b08e326f0a85f8eb2e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-07 00:44:36 +0000

    clonalframeml: update 1.11 bottle.

--- clonalframeml.rb
+++ clonalframeml.rb
@@ -10,11 +10,10 @@ class Clonalframeml < Formula
   head "https://github.com/xavierdidelot/ClonalFrameML.git"
 
   bottle do
-    cellar :any
-    sha256 "09ccaab46eea44a62ffc6e2bda598099a6f83ba36c10855be99796529976a344" => :yosemite
-    sha256 "d927b125595ec16af0e5e4b7f483dbd538737cf8b932c441baadcfeef235c9c0" => :mavericks
-    sha256 "9215a9f0a8bcb333f6be6c9986c8a5d62dcf9160b690d5ad7937005e8cfc0daa" => :mountain_lion
-    sha256 "bbd38c6efff78aff6e692d996aff80f4b978f9fabd0519986d529622fdbf2b96" => :x86_64_linux
+    cellar :any_skip_relocation
+    sha256 "757cfd2bb691170354a55247a3ae4aef4cc970de45b1e983f14da48968fbb40f" => :sierra
+    sha256 "8cf24a620451d7aae8313dceab7165c3471cb6a5675a98d7f0aa8c48d443826c" => :el_capitan
+    sha256 "f253671f7c06e01516b0217bccbaf65000de54b7f1c37f6afbef1201b2fa9ac8" => :yosemite
   end
 
   def install
