f1fb056db9f3043fdaed67fe90f0afd85e2a5ef6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-07 09:22:43 +0000

    xrmc: update 6.6.0 bottle.

--- xrmc.rb
+++ xrmc.rb
@@ -5,9 +5,9 @@ class Xrmc < Formula
   sha256 "89c2ca22c44ddb3bb15e1ce7a497146722e3f5a0c294618cae930a254cbbbb65"
 
   bottle do
-    sha256 "8f0b3aa1e710873206b538625507233be81295c853d8d0aeb936ffc2491eea3b" => :el_capitan
-    sha256 "1cb15f8682775c46458b0a456aceb92af4ca2db551447ca238cab479c5b96c8f" => :yosemite
-    sha256 "5b528bf3ccbeeccf5891d9b7374933e6ec79385ba5c3fa72f7b761178c2d3a9f" => :mavericks
+    sha256 "3c98fe0a3b3e5ee6214c8dd7f2f5198db6b3551bab582869767824842fd385d9" => :sierra
+    sha256 "b4c39cabc73c192a5d602147309a973ed4cd2d92fb6a22ec0f4fc4665d18f7e1" => :el_capitan
+    sha256 "ae287f540f1925817c8b2be79b0ac1fa27853a58fef3393640494471ae711f1a" => :yosemite
   end
 
   option "without-test", "Don't run build-time tests (may take a long time)"
