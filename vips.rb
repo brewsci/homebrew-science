996c131b1b1755a015d9b46e8b0a56424d494941
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-09 07:49:10 +0000

    vips: update 8.5.4 bottle for Linuxbrew.

--- vips.rb
+++ vips.rb
@@ -8,6 +8,7 @@ class Vips < Formula
     sha256 "9dbd5f1936ae43c487564ba275af996f56f0778245062a93e93c3a05cbc060ba" => :sierra
     sha256 "67f9e20785029b87356a830867ef79ab4bedccb89e1bab70f63dc53a53840c44" => :el_capitan
     sha256 "659279ce7a150f1135468d50e8cb0d131bf7cbb92592d80ea3d84c24e5f19c0f" => :yosemite
+    sha256 "700df92f25710571bfe63fbcba55582dc81b59abd6c1a25c7b001ae248586450" => :x86_64_linux
   end
 
   option "without-test", "Disable build time checks (not recommended)"
