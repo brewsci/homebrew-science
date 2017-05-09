675dbefafbae2f08eb9b8464eeeb3d9b5b62b4c8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-09 14:53:30 +0000

    nxtrim: update 0.4.2 bottle for Linuxbrew.

--- nxtrim.rb
+++ nxtrim.rb
@@ -13,6 +13,7 @@ class Nxtrim < Formula
     sha256 "8becf69c161ebb7dd7352b2e7ba4fbde3d288c0be3267ad14fa128c20188e074" => :sierra
     sha256 "3bbcf6b39105f21d1a7e59be6022272912bf8999ef24f6d0bfbb9cd48a3c1735" => :el_capitan
     sha256 "e40e333b5b4a9e940c47dce69bbb47bd49fec79a84ba20d4bdf67d7788edb09c" => :yosemite
+    sha256 "c350dfb9ffdbec63e8c367c84093a9bae671f981f8068f344395baf9dd0c13b1" => :x86_64_linux
   end
 
   depends_on "boost"
