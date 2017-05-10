a15a77208b3a784248bd0e33d9a542d2c4c3ed45
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-10 22:59:07 +0000

    igvtools: add 2.3.92 bottle.

--- igvtools.rb
+++ igvtools.rb
@@ -4,6 +4,13 @@ class Igvtools < Formula
   url "https://github.com/igvteam/igv/archive/v2.3.92.tar.gz"
   sha256 "b1e3202df340d7bff25f044c97063b7a33a81f5ca7be0bd4306f80c93349a7ce"
   head "https://github.com/igvteam/igv.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "77dd70196ad319ff2599ee29525d71c447cdaa3debc20be6ed3c6057b3bff439" => :sierra
+    sha256 "95bc2e4d1fbb9adaa65944e6a272420b5ce06ea2fa66b9b0ef5d2a30caefb7ad" => :el_capitan
+    sha256 "83c1783b599c6b3c55f0b54bfe1fabe68b9f3dfa8f06965d69681a9bef9338ea" => :yosemite
+  end
+
   # tag "bioinformatics"
 
   depends_on "ant" => :build
