461ea5333b5d98d16621065727d42e8bcbcb072d
Michka Popoff <michkapopoff@gmail.com>
2017-05-11 20:21:58 +0200

    openmeeg 2.4.prerelease
    Serves also as revision for hdf5
    Multiple audits were fixed
    A minimal test was added
    
    Closes #5687.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- openmeeg.rb
+++ openmeeg.rb
@@ -1,15 +1,34 @@
 class Openmeeg < Formula
-  homepage "http://www-sop.inria.fr/athena/software/OpenMEEG/"
-  url "https://github.com/openmeeg/openmeeg/archive/release-2.1.tar.gz"
-  sha256 "fa424c59a4366d6331c76e539cf5df21bf5e0b065972b2bdda20cdb9f29aba6a"
+  desc "Package for low-frequency bio-electromagnetism forward models"
+  homepage "https://openmeeg.github.io"
+  url "https://github.com/openmeeg/openmeeg/archive/2.4.prerelease.tar.gz"
+  version "2.4.prerelease"
+  sha256 "fabf9addfcb99d3b3161acde98104a321ec926a1968e444edfce5334adb1ae87"
 
   head "https://github.com/openmeeg/openmeeg.git"
 
   depends_on "cmake" => :build
   depends_on "hdf5"
+  depends_on "libmatio"
+  depends_on "zlib" unless OS.mac?
+  depends_on "openblas" unless OS.mac?
 
   def install
-    system "cmake", ".", "-DUSE_PROGRESSBAR=ON", *std_cmake_args
-    system "make", "install"
+    args = std_cmake_args + %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DENABLE_PYTHON=OFF
+      -DBUILD_DOCUMENTATION=OFF
+      -DUSE_PROGRESSBAR=ON
+    ]
+
+    mkdir "build" do
+      args << "../OpenMEEG"
+      system "cmake", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/om_assemble"
   end
 end
