5918749ccc5da7b1895e6f04eb9afb5fbdacee89
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-11 16:31:39 +0000

    alpscore: update 1.0.0_1 bottle for Linuxbrew.

--- alpscore.rb
+++ alpscore.rb
@@ -11,6 +11,7 @@ class Alpscore < Formula
     sha256 "372982d70c8888e5b9ca08a0604a161afee562b14e39cf3885bdba79eebb8106" => :sierra
     sha256 "888dd120ce82409038b4fb2583cdbdf2d9795766d9f2cf171406fb69c74c0263" => :el_capitan
     sha256 "be350fadc9fe9dc9372bde185d5243757068ed4a0d62abb51d4dceb4a1bb0866" => :yosemite
+    sha256 "8590b46617ee93e25c8750ffe3278a5b3daaa14f94785409efff9132bcf2f05a" => :x86_64_linux
   end
 
   option :cxx11
