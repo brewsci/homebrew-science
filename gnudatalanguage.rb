06c11e994b2b584606d27a56e7ed392624476416
Michka Popoff <michkapopoff@gmail.com>
2017-05-11 18:38:05 +0200

    gnudatalanguage: revision for hdf5
    Closes #5639.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- gnudatalanguage.rb
+++ gnudatalanguage.rb
@@ -3,7 +3,7 @@ class Gnudatalanguage < Formula
   homepage "https://gnudatalanguage.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/gnudatalanguage/gdl/0.9.7/gdl-0.9.7.tgz"
   sha256 "2b5945d06e4d95f01cb70a3c432ac2fa4c81e1b3ac7c02687a6704ab042a7e21"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 1
