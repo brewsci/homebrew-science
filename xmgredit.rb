131b335d27cf1be758518fe1014c3181cb7ca04b
Michka Popoff <michkapopoff@gmail.com>
2017-05-11 19:16:32 +0200

    xmgredit 5.1
    This is needed as there is a version bump of hdf5.
    The link to the dropbox download is broken.
    
    The package can now be found on the official website
    
    The version number was picked randomly from one of the possible
    versions I could find in the different files:
    
    "Version A.901" in vers.c
    "V ersion A .1093" in vers.h
    "4.0" in Makefile (obviously wrong)
    "version 5.1 (Wed Sep 14 10:20:23 PDT 2011)" provided by executing
    the xmgredit binary.
    
    The patch needed to be adapted; this probably means
    that the version we are now delivering is different
    than the one we provided before.
    Looking at the previous binaries, they were reporting
    as xm5.00r (February 22, 2007).
    
    Closes #5657.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- xmgredit.rb
+++ xmgredit.rb
@@ -1,9 +1,9 @@
 class Xmgredit < Formula
   desc "Motif-based editor for computation grids of models such as ADCIRC"
-  homepage "http://www.stccmop.org/~pturner/"
-  url "http://dl.dropbox.com/u/72178/dist/xmgredit-5.tar.gz"
-  sha256 "04de5d65f8332c320f346ad27f0343c1a8501838841efc0b566c168ee4558f47"
-  revision 4
+  homepage "http://www.stccmop.org/knowledge_transfer/software/selfe/ace_tools"
+  url "http://www.stccmop.org/CORIE/modeling/selfe/ace.tgz"
+  version "5.1"
+  sha256 "442abbe1e81e3ea33f310a0a43020ef5165768c4b57cf5a97757bd87a758e9f1"
 
   bottle do
     cellar :any
@@ -13,7 +13,6 @@ class Xmgredit < Formula
   end
 
   depends_on :x11
-
   depends_on "netcdf"
   depends_on "openmotif"
   depends_on "triangle"
@@ -21,6 +20,7 @@ class Xmgredit < Formula
   patch :DATA
 
   def install
+    Dir.chdir "xmgredit5"
     bin.mkpath
     system "make", "install", "INSTALLDIR=#{prefix}"
   end
@@ -29,8 +29,8 @@ __END__
 Patch to remove Triangle dependencies (as those are handled by the Triangle
 formula), to enable NetCDF support and to sort out compiler flags.
 diff -u b/Makefile b/Makefile
---- b/Makefile
-+++ b/Makefile
+--- b/xmgredit5/Makefile
++++ b/xmgredit5/Makefile
 @@ -10,20 +10,20 @@
  # Uncomment the following 3 defines for netCDF support
  # adjust paths to suit local conditions
@@ -43,20 +43,20 @@ diff -u b/Makefile b/Makefile
 +
 +TRIANGLEFLAGS=-DDO_TRIANGLE
 +TRIAGLELIBS=-ltriangle
- 
+
  ############################################
  # LINUX
 -LIBS = -L/usr/X11R6/lib64 -lXm -lXp -lXt -lXext -lXpm -lX11 -lICE -lSM -lm
 -INCLUDES = -I/usr/X11R6/include
--CC = gcc -g -O
+-CC = gcc -g
 +LIBS=$(LDFLAGS) -lXm -lXp -lXt -lXext -lXpm -lX11 -lICE -lSM -lm $(NETCDFLIBS) $(TRIAGLELIBS)
 +CFLAGS+=$(NETCDFFLAGS) $(TRIANGLEFLAGS)
- 
+
  #################
  ####### End of configuration, no changes should be required below #########
  #################
 -CFLAGS = -DDO_TRIANGLE $(INCLUDES)
- 
+
  OBJS = main.o\
  	vers.o\
 @@ -75,7 +75,6 @@
@@ -75,44 +75,46 @@ diff -u b/Makefile b/Makefile
  	tritest.c\
  	vedglist.c\
  	vgeometry.c\
-@@ -173,21 +171,17 @@
+@@ -173,14 +172,13 @@
  	patterns.h\
  	special.h\
  	graphics.h\
 -	triangle.h\
  	vdefines.h\
  	vdefs.h
- 
+
 -all: xmgredit5
 +all: xmgredit
- 
+
 -xmgredit5: $(OBJS) $(PARSOBJS)
 -	$(CC) $(OBJS) $(PARSOBJS) -o xmgredit5 $(LIBS)
 +xmgredit: $(OBJS) $(PARSOBJS)
-+	$(CC) $(CFLAGS) $(CPPFLAGS) $(OBJS) $(PARSOBJS) -o xmgredit $(LIBS)
- 
++	$(CC) $(OBJS) $(PARSOBJS) -o xmgredit $(LIBS)
+
+ tx: tx.o
+	$(CC) tx.o -o tx -L/usr/local/lib -lgd $(LIBS)
+@@ -188,9 +185,6 @@
  $(OBJS): defines.h globals.h
  eventproc.c: defines.h globals.h
- 
+
 -triangle.o:
 -	$(CC) -c -DTRILIBRARY triangle.c
 -
- pars.o: pars.y
- 
  vers.o: $(SRCS) pars.y
+	sh newvers.sh
+	$(CC) -c vers.c
 @@ -218,8 +212,8 @@
  	touch rcs
  	grep Id:  Makefile $(SRCS) $(INCS) $(PARSSRCS) > release.tmp
- 
+
 -install: xmgredit5
 -	cp -p xmgredit5 $(INSTALLDIR)/bin/xmgredit5
 +install: xmgredit
 +	cp -p xmgredit $(INSTALLDIR)/bin/xmgredit
- 
+
  lint:
  	lint  -axv -wk -Nn10000 -Nd10000 $(SRCS) $(PARSSRCS)
 @@ -230,3 +224,3 @@
  clean:
 -	/bin/rm *.o xmgredit5
 +	/bin/rm *.o xmgredit
- 
