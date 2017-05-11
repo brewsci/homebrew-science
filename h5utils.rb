dfe0c7c990e019e04346441f8b1f87a39cfac728
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-11 16:37:11 +0000

    h5utils: update 1.12.1_6 bottle for Linuxbrew.

--- h5utils.rb
+++ h5utils.rb
@@ -9,6 +9,7 @@ class H5utils < Formula
     sha256 "2bffedf786d2c3212d002528fe56f00bc055222c5128db5078c742e9881417d0" => :sierra
     sha256 "b7ddef2444b998a9a7a0245cc6998299142f844b72df77b8d0954592231d698b" => :el_capitan
     sha256 "e7c3dc64b3b0c91b62937b0105d52c2bffd7905ea7545492b1da8622f56668e0" => :yosemite
+    sha256 "c6b9472da5336e8a609aef98f3414e220ea79bb844f6d61ce67bce8b9ff38f86" => :x86_64_linux
   end
 
   depends_on "libpng"
