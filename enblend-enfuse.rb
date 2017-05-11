088beb629eb58e92639d4035cf6fc23e6cc54594
Michka Popoff <michkapopoff@gmail.com>
2017-05-11 20:17:58 +0200

    enblend-enfuse 4.2
    Serves also as revision for hdf5
    
    The build system has migrated to cmake.
    Build only works with c++11.
    
    Fixes audits and adds description.
    
    The test was adapted to work on Linux too.
    
    Closes #5683.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- enblend-enfuse.rb
+++ enblend-enfuse.rb
@@ -1,8 +1,8 @@
 class EnblendEnfuse < Formula
+  desc "Combines images that overlap"
   homepage "https://enblend.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/enblend/enblend-enfuse/enblend-enfuse-4.1/enblend-enfuse-4.1.1.tar.gz"
-  sha256 "9d0947633ebaa0dc1211331b31009408de6fe2108751ad4190760e3a9f181bc9"
-  revision 4
+  url "https://downloads.sourceforge.net/project/enblend/enblend-enfuse/enblend-enfuse-4.2/enblend-enfuse-4.2.tar.gz"
+  sha256 "8703e324939ebd70d76afd350e56800f5ea2c053a040a5f5218b2a1a4300bd48"
 
   bottle do
     cellar :any
@@ -11,10 +11,8 @@ class EnblendEnfuse < Formula
     sha256 "123ba3ca070d1f37cccb382c5cf58b7ba9d25fd0a08aa49ab60df63e2bbcb298" => :yosemite
   end
 
-  option "with-gpu", "Build with GPU support"
-
+  depends_on "cmake" => :build
   depends_on "libpng"
-  depends_on :x11 => :optional
   depends_on "boost"
   depends_on "gsl"
   depends_on "jpeg"
@@ -23,43 +21,26 @@ class EnblendEnfuse < Formula
   depends_on "vigra"
   depends_on "openexr" => :optional
 
-  # builds against the multithreaded boost system library
-  patch :DATA
+  resource "test_image" do
+    url "http://www.schaik.com/pngsuite/basn4a08.png"
+    sha256 "7be24b618bfd437b921ae0caf9341d112338c6290cc62466deb19d2c7a512968"
+  end
+
+  # Fix missing files in the release tar file
+  # https://bugs.launchpad.net/enblend/+bug/1568917
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/6df9856e0327804ec10de38d5d4a436fd3008640/enblend-enfuse/enblend-enfuse-4.2.patch"
+    sha256 "5a2c8f70b3f29364c9082624da22ffa69b2754abd247eeed6f2e50dfe76c1c87"
+  end
 
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}"]
-
-    args << "--without-x" if build.without? "x11"
-    args << "--enable-gpu-support=" + ((build.with? "gpu") ? "yes" : "no")
-
-    system "./configure", *args
+    system "cmake", *std_cmake_args
     system "make", "install"
   end
 
   test do
-    system "#{bin}/enblend", \
-      "/System/Library/Frameworks/SecurityInterface.framework/Versions/A/Resources/Key_Large.png", \
-      "/System/Library/Frameworks/SecurityInterface.framework/Versions/A/Resources/Key_Large.png"
-
-    system "#{bin}/enfuse", \
-      "/System/Library/Frameworks/SecurityInterface.framework/Versions/A/Resources/Key_Large.png", \
-      "/System/Library/Frameworks/SecurityInterface.framework/Versions/A/Resources/Key_Large.png"
+    testpath.install resource("test_image")
+    system "#{bin}/enblend", "basn4a08.png", "basn4a08.png"
+    system "#{bin}/enfuse", "basn4a08.png", "basn4a08.png"
   end
 end
-
-__END__
-diff --git a/configure.orig b/configure
-index 98699f5..70e286b 100755
---- a/configure.orig
-+++ b/configure
-@@ -8279,7 +8279,7 @@ fi
- 
- 
- 
--LIBS="-lboost_system $LIBS"
-+LIBS="-lboost_system-mt $LIBS"
- { $as_echo "$as_me:${as_lineno-$LINENO}: checking for new Boost system library" >&5
- $as_echo_n "checking for new Boost system library... " >&6; }
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
