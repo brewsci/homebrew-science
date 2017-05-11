a2c330f9d7653282990cffd7528ade4085869a4c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-11 22:56:03 +0000

    gmap-gsnap: update 2017-01-14 bottle for Linuxbrew.

--- gmap-gsnap.rb
+++ gmap-gsnap.rb
@@ -11,6 +11,7 @@ class GmapGsnap < Formula
     sha256 "8f3a579e7c037de3423b298be933ddcc33a88cfc2d81279b5e8eb942243cb2e6" => :sierra
     sha256 "6e4004bd20a29eb92ef7ca79a61de90a1a828a94390d0186d9f824c147066b66" => :el_capitan
     sha256 "45a1783741299e4ef20326f4d92e1847d036418316616e9f0401712a4d707dcf" => :yosemite
+    sha256 "88934574226f97eb899872d7c7c766c1a42bfb5ede4f2432a40f0ea7cdfdbda2" => :x86_64_linux
   end
 
   depends_on "samtools"
