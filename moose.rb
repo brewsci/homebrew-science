2b4dd828b1d8942f835e6c416694276700d623d9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-11 17:50:09 +0000

    moose: update 3.1.1_1 bottle for Linuxbrew.

--- moose.rb
+++ moose.rb
@@ -11,6 +11,7 @@ class Moose < Formula
     sha256 "6077f886560480c956270f855cf9576a3e8261c5f2ea064117c3483f74a84462" => :sierra
     sha256 "a637de34ce0b92f16afc120ecb2e0e4aff8f8a2e6a2ada5521ee01cf7ccdca9e" => :el_capitan
     sha256 "1bb0712ef178577a3c44190be8f21f894cddc66ce03f742d768e44371425dce7" => :yosemite
+    sha256 "a62366e1e1de37c13dec6d2b7f91dc63f8b40ab460e35b31a4d94507a0df6219" => :x86_64_linux
   end
 
   option "with-sbml", "Enable sbml support"
