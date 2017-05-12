c0f428b912e4e7d35fcd3aaf713b996f31dadac3
rakhimov <ol.rakhimov@gmail.com>
2017-05-11 22:12:52 -0700

    scram 0.13.0
    Closes #5694.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- scram.rb
+++ scram.rb
@@ -1,16 +1,8 @@
-class MacRequirement < Requirement
-  fatal true
-  satisfy OS.mac?
-  def message
-    "This particular packaging is for Mac only."
-  end
-end
-
 class Scram < Formula
   desc "Probabilistic Risk Analysis Tool"
   homepage "https://scram-pra.org/"
-  url "https://downloads.sourceforge.net/project/iscram/scram-0.11.6.tar.gz"
-  sha256 "19f43d15f12d6879a3eaf88b0862746fcd7ed2f9df49b6f51de16f155d258dd2"
+  url "https://github.com/rakhimov/scram/archive/0.13.0.tar.gz"
+  sha256 "01c4c3cd98e4a197e2b71b85f3953896e75030f5b70970b65fa5a67dbbdc532a"
 
   bottle do
     sha256 "42623f102a91c5253080664d53f3b54cde1f92a56e524b85a0bb5ef1f8f564b9" => :sierra
@@ -18,14 +10,15 @@ class Scram < Formula
     sha256 "308d9f66e32bf77b36afbc7e77893a05f2ae5b4e67b46867a3582015055e5947" => :yosemite
   end
 
-  depends_on MacRequirement
+  needs :cxx14
+
+  # C++14 uses GCC 5.3, which is not ABI compatible with GCC 4.8
+  depends_on :macos unless OS.mac?
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "libxml++"
   depends_on "gperftools" => :recommended
-
-  needs :cxx11
 
   def install
     mkdir "build" do
@@ -40,5 +33,6 @@ class Scram < Formula
 
   test do
     system "#{bin}/scram", "--help"
+    system "#{bin}/scram", "--version"
   end
 end
