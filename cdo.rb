2c3c7cef0fa6164a31b6797674cab990fc46ef00
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-13 09:22:20 +0000

    cdo: update 1.8.1_1 bottle.

--- cdo.rb
+++ cdo.rb
@@ -7,10 +7,9 @@ class Cdo < Formula
 
   bottle do
     cellar :any
-    sha256 "cb16db1b887d6629c1ca40f75d32147b09670d02ad75a0e1413fdd9033d48032" => :sierra
-    sha256 "3f7e2191c319d88347652a61d438fb7712a09f9e2f987f770684e4ff2fcbf5eb" => :el_capitan
-    sha256 "69fbb8fb2e6901945b3276c331d032d2587f2d8dac7a0eb9076074606017486e" => :yosemite
-    sha256 "f777192396dde0db8cd7513719a429e1ecc1bc8ee5c56605df1012d12e63e019" => :x86_64_linux
+    sha256 "a4da8a68c183682a708742e27c17293cdd27e952c4806d8bfaad0617845a413c" => :sierra
+    sha256 "30a7b3e7dead3b924bd0fddd9760bbe8806843d01c7bc21b5e3d91f767da8fd1" => :el_capitan
+    sha256 "64ea8eddfded59fea673626bbf1207ae0690866e9dd7bffe0f17e0dbe2209804" => :yosemite
   end
 
   option "with-grib2", "Compile Fortran bindings"
