26d26777843f6f3a475de6c7c1cfc5826fd4850a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-13 17:18:36 +0000

    fgsl: update 1.2.0_2 bottle for Linuxbrew.

--- fgsl.rb
+++ fgsl.rb
@@ -9,6 +9,7 @@ class Fgsl < Formula
     sha256 "16c3313848afc30d4926abdc248d46eb2a2d12ae4363b88b595f4f1780d50591" => :sierra
     sha256 "c69725dca9c2803c57b095ac779bf160d4f0069e4c8368503b31444a5c116a27" => :el_capitan
     sha256 "d1b375f6c8e045bc33205647fd4c26d2511ec961295ab2ab5dac5ba5c6f676ff" => :yosemite
+    sha256 "ed82f035a924f89b178c5877a1dbd572b51f27e58aa58ee6cd96bc0f6eb5db59" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
