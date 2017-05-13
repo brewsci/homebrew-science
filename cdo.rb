02da239f7168b54ba6511c9f9bc2ca14e22d6458
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-13 09:31:47 +0000

    cdo: update 1.8.1_1 bottle for Linuxbrew.

--- cdo.rb
+++ cdo.rb
@@ -10,6 +10,7 @@ class Cdo < Formula
     sha256 "a4da8a68c183682a708742e27c17293cdd27e952c4806d8bfaad0617845a413c" => :sierra
     sha256 "30a7b3e7dead3b924bd0fddd9760bbe8806843d01c7bc21b5e3d91f767da8fd1" => :el_capitan
     sha256 "64ea8eddfded59fea673626bbf1207ae0690866e9dd7bffe0f17e0dbe2209804" => :yosemite
+    sha256 "bcb956938cb251aa130684e4e956f5fe758dcd03c0d87e5efec24acb67949cc4" => :x86_64_linux
   end
 
   option "with-grib2", "Compile Fortran bindings"
