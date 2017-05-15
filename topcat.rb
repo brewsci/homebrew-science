dc8f48099ed7889a77a6c87e80c0461aee51ca99
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-15 21:35:41 +0000

    topcat: add 4.4 bottle for Linuxbrew.

--- topcat.rb
+++ topcat.rb
@@ -10,6 +10,7 @@ class Topcat < Formula
     sha256 "f93af9fc7e095325b815be10eda702dfddc9e1f263ce6074b8c82c388bbcd2e8" => :sierra
     sha256 "9523448a70b5b0ce08d679a067cfc4ea57ff1001a4c60b7fb2bb018a12d2c99e" => :el_capitan
     sha256 "9523448a70b5b0ce08d679a067cfc4ea57ff1001a4c60b7fb2bb018a12d2c99e" => :yosemite
+    sha256 "ae3ed505432809d5b274f5bdf7d2498dd33e164b101412ea8a9480a288144b5a" => :x86_64_linux
   end
 
   def install
