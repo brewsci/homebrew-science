263acaf06f1e59e85502ce8db229f8e0ce0b2a51
ilovezfs <ilovezfs@icloud.com>
2017-05-25 06:45:27 -0700

    bpipe 0.9.9.3 (#5744)

--- bpipe.rb
+++ bpipe.rb
@@ -4,17 +4,11 @@ class Bpipe < Formula
   # doi "10.1093/bioinformatics/bts167"
   # tag "bioinformatics"
 
-  url "https://github.com/ssadedin/bpipe/releases/download/0.9.9.2/bpipe-0.9.9.2.tar.gz"
-  sha256 "29986da8dfb89d9789c25d3bf7e3486acfd99eb1fbac887e7f3aebc8e07ee471"
+  url "http://download.bpipe.org/versions/bpipe-0.9.9.3.tar.gz"
+  sha256 "414d781c14d770cca0ddd4077fe82f30d691f19c7980aa8f1773f77c1cfddf08"
   head "https://github.com/ssadedin/bpipe.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "5cbbd4b15a4ea1a73266a82ad9e47747ac745600e331c7abbef3033958d2b2a0" => :el_capitan
-    sha256 "af2b2242a3a04786cd46acfbf56c21a83330d78bf969c49db6cf7be22f62404d" => :yosemite
-    sha256 "670604584ce9f32afcad449840fdc4294b228b60c315d5bdc178e6b8d07fc64e" => :mavericks
-    sha256 "5207240e3fbe3182f447f6a08b5407715047d749fdb03967f39a2b5535334095" => :x86_64_linux
-  end
+  bottle :unneeded
 
   depends_on :java
 
@@ -24,6 +18,6 @@ class Bpipe < Formula
   end
 
   test do
-    assert_match "Found 0 currently executing commands", shell_output("#{bin}/bpipe status")
+    assert_match version.to_s, shell_output("#{bin}/bpipe -h")
   end
 end
