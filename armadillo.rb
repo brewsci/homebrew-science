624b7b31d82ec72418bf451b5698f4c662798dcc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-27 21:42:05 +0000

    armadillo: update 7.900.1 bottle for Linuxbrew.

--- armadillo.rb
+++ armadillo.rb
@@ -9,6 +9,7 @@ class Armadillo < Formula
     sha256 "1d2abd10d3081ad594ac6fbe869c577c8302605b6c760a5709519ec6c6f1f484" => :sierra
     sha256 "1e67c6e7e2e19ca7d95eb4cc9dd3ab4a9c26412d71544f6d6eaf1d672d553512" => :el_capitan
     sha256 "e3d72320fa068c7ddb8e0d6beafdde2b751c3267c6619cc122115e9888fe27f1" => :yosemite
+    sha256 "33699debc81c55d2d8e734c5663f89bdd497415d55c6a2e84178b14f14b673ca" => :x86_64_linux
   end
 
   option :cxx11
