022f97be6433cee2622c2beca4a7657c94f566d4
Jorden Whitefield <j.whitefield@surrey.ac.uk>
2017-05-30 07:41:28 +0100

    tamarin-prover 1.2.2
    Zlib now required for linuxbrew build.
    
    Closes #5753.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- tamarin-prover.rb
+++ tamarin-prover.rb
@@ -1,9 +1,8 @@
 class TamarinProver < Formula
   desc "Automated security protocol verification tool"
   homepage "https://tamarin-prover.github.io/"
-  url "https://github.com/tamarin-prover/tamarin-prover/archive/1.2.1.tar.gz"
-  sha256 "3c8a91c23896f61f1c04b8121125b3fd603191522512ef5a459260d89fdd911a"
-  revision 1
+  url "https://github.com/tamarin-prover/tamarin-prover/archive/1.2.2.tar.gz"
+  sha256 "f9b2d3acc01b89f71d2b246a6b3010ebab71e4fe309b3be8a8eac213422b43de"
   head "https://github.com/tamarin-prover/tamarin-prover.git", :branch => "develop"
 
   bottle do
@@ -16,6 +15,7 @@ class TamarinProver < Formula
 
   depends_on "haskell-stack" => :build
   depends_on "ocaml" => :build
+  depends_on "zlib" => :build unless OS.mac?
   depends_on "maude"
   depends_on "graphviz"
   depends_on :macos => :mountain_lion
@@ -27,7 +27,12 @@ class TamarinProver < Formula
     # Let `stack` handle its own parallelization
     # Deparallelization prevents "install: mkdir ... ghc-7.10.3/lib: File exists"
     ENV.deparallelize
-    system "make", "default"
+    jobs = ENV.make_jobs
+    system "stack", "-j#{jobs}", "setup"
+    args = []
+    args << "--extra-include-dirs=#{Formula["zlib"].include}" << "--extra-lib-dirs=#{Formula["zlib"].lib}" unless OS.mac?
+    system "stack", "-j#{jobs}", *args, "install", "--flag", "tamarin-prover:threaded"
+    system "make", "sapic"
 
     bin.install Dir[".brew_home/.local/bin/*"]
   end
