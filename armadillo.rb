722be4106fea542726891e0037a2ab11ad60c694
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-31 06:33:09 +0000

    armadillo: update 7.950.0 bottle.

--- armadillo.rb
+++ armadillo.rb
@@ -6,10 +6,9 @@ class Armadillo < Formula
 
   bottle do
     cellar :any
-    sha256 "1d2abd10d3081ad594ac6fbe869c577c8302605b6c760a5709519ec6c6f1f484" => :sierra
-    sha256 "1e67c6e7e2e19ca7d95eb4cc9dd3ab4a9c26412d71544f6d6eaf1d672d553512" => :el_capitan
-    sha256 "e3d72320fa068c7ddb8e0d6beafdde2b751c3267c6619cc122115e9888fe27f1" => :yosemite
-    sha256 "33699debc81c55d2d8e734c5663f89bdd497415d55c6a2e84178b14f14b673ca" => :x86_64_linux
+    sha256 "0dc626e12308d1cc3aefff4078cfc03d6cf750adcd5636c10664994bec215f61" => :sierra
+    sha256 "30218dcb0043e75d0b603887826cfed44b3ba10cdc45054f8d1611fd13ee6a6f" => :el_capitan
+    sha256 "e0f61540787645ce3fd7abd2fc993d417091b95495267627c7aa2d97f39e790e" => :yosemite
   end
 
   option :cxx11
