771f32116337d7ec0c06296ff24a64cf62feb329
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-31 22:02:04 +0000

    platypusvar: add 0.8.1 bottle.

--- platypusvar.rb
+++ platypusvar.rb
@@ -5,6 +5,13 @@ class Platypusvar < Formula
   version "0.8.1"
   sha256 "a0f39e800ebdc5590e9b568a791bc6746df0fde4d1c3622140db64dea175622b"
 
+  bottle do
+    cellar :any
+    sha256 "941e81e14e2237a2fea587f4843d9a4247dda9144f15d39556039072668f28cb" => :sierra
+    sha256 "5b59b3e929a2b81030e76aecc1976674f50d10c5dd252586f8373f91a9ea0503" => :el_capitan
+    sha256 "576272fc462c6fa7898b797831e43e26ef02e3046e5401b8e11ee36d03777beb" => :yosemite
+  end
+
   depends_on "htslib"
   depends_on :python unless OS.mac?
 
