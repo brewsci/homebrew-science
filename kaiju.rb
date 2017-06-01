161d2befb3d7003030c38ceb10c30c7ad7d9ef9c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-01 06:41:41 +0000

    kaiju: update 1.5.0 bottle for Linuxbrew.

--- kaiju.rb
+++ kaiju.rb
@@ -12,6 +12,7 @@ class Kaiju < Formula
     sha256 "83d0ca10f86875d29fc6312b17b012d12c10f3af6dab1b61b2e832f87651aed7" => :sierra
     sha256 "5143dbaaea8fe258c67cf19a17cbe958c3eef2fbd38bb224036acba9bfb3c089" => :el_capitan
     sha256 "11ccc63d76f9e131aa600a291eff87e4ded5a9d6bf0812081843fed161c1b977" => :yosemite
+    sha256 "deea5e8ee85e4094ab0ede7950d5657dde8e2206f488511cd639f663279882cd" => :x86_64_linux
   end
 
   def install
