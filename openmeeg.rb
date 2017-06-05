3cccf4d8a5c858dad110a553922e13579847a01b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-05 21:47:32 +0000

    openmeeg: add 2.4.prerelease bottle for Linuxbrew.

--- openmeeg.rb
+++ openmeeg.rb
@@ -12,6 +12,7 @@ class Openmeeg < Formula
     sha256 "f1dabe4afb7c10db77e35f1f6545ca92c00f02fda62727e076cd3340154a5fe0" => :sierra
     sha256 "d15f2a93dfd57aa840377e4406e733c9b845af509b1ec0b5f877b375aebf6f80" => :el_capitan
     sha256 "8cd1e3c19bcb20cb728d20e69450b9309e4f26dd8655f1ac161f0a8a55b0cfa8" => :yosemite
+    sha256 "08133e00055d0ae8672a96222de77919561237132a53ca668ef6492ecc7e69c2" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
