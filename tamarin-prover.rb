c2042d3f910bffe1311fe7ce23992cd91a13eed7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-05 09:51:02 +0000

    tamarin-prover: update 1.2.2 bottle for Linuxbrew.

--- tamarin-prover.rb
+++ tamarin-prover.rb
@@ -10,6 +10,7 @@ class TamarinProver < Formula
     sha256 "73d09465fd0af798b11f94d9a845342b4c97b1a4345d6a9425bef89242b34924" => :sierra
     sha256 "5f9265c596f44a2b508ef7cbe007e05fd9b63fd8a1cbaf9fbc3b88b609dcecbd" => :el_capitan
     sha256 "b2e7524e1e6705a9527eeaa3e307b47ea87d669166fef3f2044390e69c912e26" => :yosemite
+    sha256 "80902c79777872f6fca7965f82b6d901a0e3e5ae79aab1b6f49df52ab708bffb" => :x86_64_linux
   end
 
   depends_on "haskell-stack" => :build
