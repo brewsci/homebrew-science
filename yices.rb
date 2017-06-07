d3aab1576a29590f18b33a79ac5d6faed185c033
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-07 19:26:54 +0000

    yices: add 2.5.2 bottle.

--- yices.rb
+++ yices.rb
@@ -4,6 +4,13 @@ class Yices < Formula
   url "https://github.com/SRI-CSL/yices2/archive/Yices-2.5.2.tar.gz"
   sha256 "80a2a9f258a561b068557bdbc7a7ab630bf4acf6ff9675cbffe4423a4ac04a2b"
 
+  bottle do
+    cellar :any
+    sha256 "21c4912d4b5a632d252a90352a91463f7d9bb96add62b82d8bcbf43256b27cbb" => :sierra
+    sha256 "614796d70880e4a7b6ce3ac78ba98697bd9dd6d0084d8a6c1f6f61c943e0fc5f" => :el_capitan
+    sha256 "1a47b5478c6645432615c70a0742f9a2508121e40f7db8a01c85aee4a90d507b" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "autoconf" => :build
   depends_on "gperf"
