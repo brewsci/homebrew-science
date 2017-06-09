5d56c1608ae124e7cb18cea0a0a1d0e4af8328a7
Tatyana Mozgacheva <tmozgach@sfu.ca>
2017-06-09 14:41:13 -0700

    rapsearch2: rebuild bottle
    Fix Error: rapsearch: error while loading shared libraries:
    libboost_serialization.so.1.63.0
    
    Closes #5792.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- rapsearch2.rb
+++ rapsearch2.rb
@@ -4,8 +4,9 @@ class Rapsearch2 < Formula
   url "https://downloads.sourceforge.net/project/rapsearch2/RAPSearch2.24_64bits.tar.gz"
   version "2.24"
   sha256 "85db4573f4c768b6c3c73bb44ff2611ba48dc6c8d188feb40f44bf7c55de36ce"
+  revision 1
   # tag "bioinformatics"
-  # doi "10.1093/bioinformatics/btr595:"
+  # doi "10.1093/bioinformatics/btr595"
 
   bottle do
     sha256 "28f6de891fc6aff968933ca60b388388f21784c2b2d02a65e922a10043fa1ed4" => :sierra
