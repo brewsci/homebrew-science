b883860744ee215c545d3805cd16039c305094a2
Simon Walker <s.r.walker101@googlemail.com>
2017-06-12 21:08:39 +0100

    gnuastro 0.3
    Closes #5806.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- gnuastro.rb
+++ gnuastro.rb
@@ -1,8 +1,8 @@
 class Gnuastro < Formula
   desc "Programs and libs for manipulation and analysis of astronomical data"
   homepage "https://www.gnu.org/software/gnuastro/index.html"
-  url "https://ftp.gnu.org/gnu/gnuastro/gnuastro-0.2.tar.gz"
-  sha256 "3b26190d47dc2d3f1d00ab0799c9fec9ce9bfa931e2e4f924ff98587ff792901"
+  url "https://ftp.gnu.org/gnu/gnuastro/gnuastro-0.3.tar.gz"
+  sha256 "8a5bc2f977d4839f820d7423d10a6b94391a9571df97a64b9feb99a117973d81"
 
   bottle do
     sha256 "ede3ca24ee5c9c4edac3a1b9c714a50adbdc4741154a60d33bfa658ec85f91de" => :sierra
@@ -19,7 +19,10 @@ class Gnuastro < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--enable-bin-op-alltypes",
                           "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
     system "make", "install"
   end
 
