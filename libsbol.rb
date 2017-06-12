282629906bfa8949df3d3ceab08d616cd139a8ae
ilovezfs <ilovezfs@icloud.com>
2017-06-12 01:41:53 -0700

    libsbol 2.2.0
    Closes #5803.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- libsbol.rb
+++ libsbol.rb
@@ -1,8 +1,8 @@
 class Libsbol < Formula
   desc "Read and write files in the Synthetic Biology Open Language (SBOL)"
   homepage "https://synbiodex.github.io/libSBOL"
-  url "https://github.com/SynBioDex/libSBOL/archive/v2.1.1.1.tar.gz"
-  sha256 "76a9895e779bca91a617af885de7b411d4203f571306c3a69942913e1bdaf3ac"
+  url "https://github.com/SynBioDex/libSBOL/archive/v2.2.0.tar.gz"
+  sha256 "f171a1dadcb1414b763a626a83ca1874230b6e4dd7483a8c9526fcf0137d44a9"
 
   bottle do
     cellar :any
@@ -19,7 +19,8 @@ class Libsbol < Formula
   depends_on "raptor"
 
   def install
-    system "cmake", ".", "-DSBOL_BUILD_SHARED=TRUE", "-DSBOL_BUILD_STATIC=FALSE", *std_cmake_args
+    system "cmake", ".", "-DCMAKE_CXX_FLAGS=-I/System/Library/Frameworks/Python.framework/Headers",
+                         "-DSBOL_BUILD_SHARED=TRUE", "-DSBOL_BUILD_STATIC=FALSE", *std_cmake_args
     system "make", "install"
   end
 
@@ -36,9 +37,9 @@ class Libsbol < Formula
       }
     EOS
     if ENV.compiler == :gcc
-      system ENV.cxx, "test.cpp", "-o", "test", "-std=c++11", "-I#{HOMEBREW_PREFIX}/include/curl", "-I#{HOMEBREW_PREFIX}/include", "-I#{HOMEBREW_PREFIX}/include/raptor2", "-I#{include}/sbol", "-L#{HOMEBREW_PREFIX}/lib", "-L#{lib}", "-Wl,-rpath=#{HOMEBREW_PREFIX}/lib,-rpath=#{lib}", "-ljsoncpp", "-lcurl", "-lraptor2", "-lsbol"
+      system ENV.cxx, "test.cpp", "-o", "test", "-std=c++11", "-I/System/Library/Frameworks/Python.framework/Headers", "-I#{HOMEBREW_PREFIX}/include/curl", "-I#{HOMEBREW_PREFIX}/include", "-I#{HOMEBREW_PREFIX}/include/raptor2", "-I#{include}/sbol", "-L#{HOMEBREW_PREFIX}/lib", "-L#{lib}", "-Wl,-rpath=#{HOMEBREW_PREFIX}/lib,-rpath=#{lib}", "-ljsoncpp", "-lcurl", "-lraptor2", "-lsbol"
     else
-      system ENV.cc, "test.cpp", "-o", "test", "-std=c++11", "-I#{HOMEBREW_PREFIX}/include/curl", "-I#{HOMEBREW_PREFIX}/include", "-I#{HOMEBREW_PREFIX}/include/raptor2", "-I#{include}/sbol", "-L#{HOMEBREW_PREFIX}/lib", "-L#{lib}", "-ljsoncpp", "-lcurl", "-lraptor2", "-lsbol", "-lc++"
+      system ENV.cc, "test.cpp", "-o", "test", "-std=c++11", "-I/System/Library/Frameworks/Python.framework/Headers", "-I#{HOMEBREW_PREFIX}/include/curl", "-I#{HOMEBREW_PREFIX}/include", "-I#{HOMEBREW_PREFIX}/include/raptor2", "-I#{include}/sbol", "-L#{HOMEBREW_PREFIX}/lib", "-L#{lib}", "-ljsoncpp", "-lcurl", "-lraptor2", "-lsbol", "-lc++"
     end
     system "./test"
   end
