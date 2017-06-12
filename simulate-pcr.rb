2946446c11955e880b6d15683a2ab85141314a15
Tatyana Mozgacheva <tmozgach@sfu.ca>
2017-06-12 15:56:41 -0700

    simulate-pcr: replace /usr/bin/perl with /usr/bin/env perl
    Add bottle :unneeded.
    
    Closes #5807.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- simulate-pcr.rb
+++ simulate-pcr.rb
@@ -1,12 +1,12 @@
 class SimulatePcr < Formula
   desc "Predicts amplicon products from single or multiplex primers"
   homepage "https://sourceforge.net/projects/simulatepcr/"
-
+  url "https://downloads.sourceforge.net/project/simulatepcr/simulate_PCR-v1.2.tar.gz"
+  sha256 "022d1cc595d78a03b6a8a982865650f99d9fa067997bfea574c2416cc462e982"
   # doi "10.1186/1471-2105-15-237"
   # tag "bioinformatics"
 
-  url "https://downloads.sourceforge.net/project/simulatepcr/simulate_PCR-v1.2.tar.gz"
-  sha256 "022d1cc595d78a03b6a8a982865650f99d9fa067997bfea574c2416cc462e982"
+  bottle :unneeded
 
   depends_on "blast"
   depends_on "Bio::Perl" => :perl
@@ -14,6 +14,7 @@ class SimulatePcr < Formula
 
   def install
     bin.install "simulate_PCR"
+    inreplace bin/"simulate_PCR", "#!/usr/bin/perl", "#!/usr/bin/env perl"
   end
 
   test do
