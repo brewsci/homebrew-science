4efc7a338d7f3d71ab5242b1027ce45661072faa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-13 05:02:44 +0000

    packmol: update 17.163 bottle for Linuxbrew.

--- packmol.rb
+++ packmol.rb
@@ -11,6 +11,7 @@ class Packmol < Formula
     sha256 "9985a6c737f3d52d74728e57fc5867890c948f10e20dc8b024c0537626454fcb" => :sierra
     sha256 "61cc06222a06af226b64ddeb1d28087b193f5bc54be7d1038ddd7f0ef92ddf6b" => :el_capitan
     sha256 "ac938f42a232278e4dd2e93362ba1a052dc67fc4989bfa60883b428448e6f42d" => :yosemite
+    sha256 "01894d7fd69d51151795fcd51814d488ac0d2e086b052159a2de3b90b2e8adc2" => :x86_64_linux
   end
 
   depends_on :fortran
