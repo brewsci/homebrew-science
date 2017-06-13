e7a38365b7a6b28e07cd4a7503be653ad650d4b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-13 09:23:23 +0000

    mad-x: update 5.03.06 bottle.

--- mad-x.rb
+++ mad-x.rb
@@ -7,10 +7,9 @@ class MadX < Formula
 
   bottle do
     cellar :any
-    sha256 "40ccbc6290f8760a7c30abfea2a5de8abbd56b337d4854dfac599b7f4930d82a" => :sierra
-    sha256 "13800e6b51ee23d53d08c43cda553744fb0472ab93a42fa1367956658592bec9" => :el_capitan
-    sha256 "b15aae28e6069fa7694bdb4f4bed3832c5c5fc51a07304d650d3fdcdfd941e74" => :yosemite
-    sha256 "77844850e32a6d1b720fbe611d0f158257036156f4419f07374c4e8d2a4b436c" => :x86_64_linux
+    sha256 "54d31a73ae0744b8093fff79a26bf797012086f00fcf65fdc9dae7251136c991" => :sierra
+    sha256 "3104a72c54c91f2899641da479137bfe837d4e3970922a9791e19ea6b8c0f5a6" => :el_capitan
+    sha256 "1afe12d2cc762817778b54e944b7f40a917d6b605e8ad44bf041bf37a7991c2e" => :yosemite
   end
 
   depends_on "cmake" => :build
