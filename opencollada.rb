898f000ef5914a77a67a9fd519903e808a24bd4f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-15 05:05:28 +0000

    opencollada: update 1.6.54 bottle for Linuxbrew.

--- opencollada.rb
+++ opencollada.rb
@@ -9,6 +9,7 @@ class Opencollada < Formula
     sha256 "87bab39a924f6b713b4fe112ca648cb93b3fc35d03b66937808fb2e12beadb32" => :sierra
     sha256 "28ee2f691892b00442673bf40d21ada9f0f55655c989ea8a86f090f6cb71965f" => :el_capitan
     sha256 "73b29c0f7d79943bb2776464644ce738273e2aa99929a424eff72b44a8eea4cd" => :yosemite
+    sha256 "9d7c3f1a305160d5945eb636138c5564cf15a4e4a50f01e3d3fbfdd49d69e6ce" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
