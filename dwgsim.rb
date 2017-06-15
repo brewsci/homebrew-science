64f25597adb41779f0bd35ed556ecc49d1b50e3f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-15 04:43:49 +0000

    dwgsim: update 0.1.12 bottle for Linuxbrew.

--- dwgsim.rb
+++ dwgsim.rb
@@ -11,6 +11,7 @@ class Dwgsim < Formula
     sha256 "b192be1a5f9e084d1611578edf3e2e9f12e41b4c95135b43ca5d95243e811363" => :sierra
     sha256 "efa87bd65372f93ce3ee7141e60b559dae71a1db9a6f4e72648f04e05e6236d0" => :el_capitan
     sha256 "1ca0062c7ae8e5185604bd34d3e6d60d63e99adfffa3659cac10fdfd5d5f4788" => :yosemite
+    sha256 "43b7ffae90cbd82cc456e786f83efdfecbf222c1a8c337a1a30fbe01b9dca2d6" => :x86_64_linux
   end
 
   unless OS.mac?
