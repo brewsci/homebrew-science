2919b491cf2abf892e114bc7faeae5710c15e1ac
Tatyana Mozgacheva <tmozgach@sfu.ca>
2017-06-16 16:13:33 -0700

    bamm: fix error - Enable multithreading to use std::thread
    Closes #5832.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- bamm.rb
+++ bamm.rb
@@ -3,6 +3,7 @@ class Bamm < Formula
   homepage "http://bamm-project.org"
   url "https://github.com/macroevolution/bamm/archive/v2.5.0.tar.gz"
   sha256 "526eef85ef011780ee21fe65cbc10ecc62efe54044102ae40bdef49c2985b4f4"
+  revision 1
   head "https://github.com/macroevolution/bamm.git"
   # tag "bioinformatics"
   # doi "10.1371/journal.pone.0089543"
@@ -16,6 +17,10 @@ class Bamm < Formula
   end
 
   depends_on "cmake" => :build
+
+  fails_with :gcc => "4.8" do
+    cause "Error: Enable multithreading to use std::thread"
+  end
 
   def install
     mkdir "build" do
