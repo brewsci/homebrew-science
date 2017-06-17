e524ecc2e1d309d996405f1469eebe5a4bb0a065
ilovezfs <ilovezfs@icloud.com>
2017-06-16 22:17:03 -0700

    cmor 3.2.4
    also serves as revision for gcc
    
    Closes #5833.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- cmor.rb
+++ cmor.rb
@@ -1,9 +1,8 @@
 class Cmor < Formula
   desc "Climate Model Output Rewriter for producing CF-compliant netCDF files"
   homepage "https://cmor.llnl.gov/"
-  url "https://github.com/PCMDI/cmor/archive/CMOR-3.2.3.tar.gz"
-  sha256 "e4dcd7e16f6bce5817470e628ae9ba7cb51d7f5cd9e2e3ba86b26ee6a9a916bf"
-  revision 1
+  url "https://github.com/PCMDI/cmor/archive/CMOR-3.2.4.tar.gz"
+  sha256 "55e3dc817b0ecfd0286a187321fc748cd0e0e88e8ebdc01d9667c46df081395f"
   # doi "10.5281/zenodo.61943"
 
   bottle do
@@ -12,7 +11,7 @@ class Cmor < Formula
     sha256 "8d61a3b4b129495941c7f915397263e3c6cc7e8614f48b9abe3a489b81553ca4" => :yosemite
   end
 
-  keg_only "Conflicts with json-c in main repository."
+  keg_only "conflicts with json-c in main repository"
 
   depends_on "ossp-uuid"
   depends_on "udunits"
