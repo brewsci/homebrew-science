1c788f6c9e14c4754a3a82001cbb4ff0cfb0e0dc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-17 05:30:31 +0000

    cmor: update 3.2.4 bottle.

--- cmor.rb
+++ cmor.rb
@@ -6,9 +6,9 @@ class Cmor < Formula
   # doi "10.5281/zenodo.61943"
 
   bottle do
-    sha256 "1848e05c97d342bb198aa39815d4940259787744fb53a85aaa64d5934d4297ab" => :sierra
-    sha256 "7245825b27f0a5611f60f73af3573215b761aceadca3c3690078d0861068528c" => :el_capitan
-    sha256 "8d61a3b4b129495941c7f915397263e3c6cc7e8614f48b9abe3a489b81553ca4" => :yosemite
+    sha256 "77fa48901bada3472fd88eb2c7bf08c9ddca57bcaa8cc9d91590f744a2ee42cf" => :sierra
+    sha256 "83efc240aacb9fd4c0faeb227a60f7e15249f1bd3265ed7f32bd6d4d0e198ad4" => :el_capitan
+    sha256 "b7124ebf9f04f6e84400357ee78c9b2b55f81d3b562956040513d767d1904988" => :yosemite
   end
 
   keg_only "conflicts with json-c in main repository"
