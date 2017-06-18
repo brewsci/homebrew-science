9cc1df0b252bc3fda489346f76d5b9c7efefc942
Michka Popoff <michkapopoff@gmail.com>
2017-06-18 12:13:23 +0200

    coinmp: revision for gcc
    Closes #5836.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- coinmp.rb
+++ coinmp.rb
@@ -1,13 +1,12 @@
 class Coinmp < Formula
-  homepage "http://www.coin-or.org/projects/CoinMP.xml"
-  url "http://www.coin-or.org/download/source/CoinMP/CoinMP-1.7.3.tgz"
+  desc "C-API library for the CLP, CBC and CGL projects"
+  homepage "https://projects.coin-or.org/CoinMP"
+  url "https://www.coin-or.org/download/source/CoinMP/CoinMP-1.7.3.tgz"
   sha256 "45c349e297edfa951458f37800ba16aa451aff86fea2de2923e15723b3909f90"
+  revision 2
   head "https://projects.coin-or.org/svn/CoinMP/trunk", :using => :svn
-  revision 1
 
   depends_on :fortran
-
-  conflicts_with "coinutils", :because => "CoinMP includes CoinUtils."
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
