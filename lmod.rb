a8cd33b66b380f775da217a0c5c35b64ea21f778
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-18 11:56:24 +0000

    lmod: update 7.5 bottle for Linuxbrew.

--- lmod.rb
+++ lmod.rb
@@ -9,6 +9,7 @@ class Lmod < Formula
     sha256 "d99d45f025566dcdc29f9bab078c1b6fcd5bc912f20d190425c65a67f1a07059" => :sierra
     sha256 "8256c67d07825fc6655af7e950dd098748dea11a456d0cdfe6d2e6caa9634477" => :el_capitan
     sha256 "0d520b5b2186336baee0cdc6678d0ef9c26f07ee63de8fd84210cef73ed64b7a" => :yosemite
+    sha256 "68ee9b60c4c1ecd2a3493a6a70d8ef16b898f292f5d5e9f8d204a14684033fe4" => :x86_64_linux
   end
 
   depends_on "lua"
