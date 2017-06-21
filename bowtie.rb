9825ee0bdbee78e2d37f4d906399e874841bec7a
Michael L Heuer <heuermh@acm.org>
2017-06-21 10:47:25 -0500

    bowtie 1.2.1.1
    Closes #5851.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- bowtie.rb
+++ bowtie.rb
@@ -4,8 +4,8 @@ class Bowtie < Formula
   # doi "10.1186/gb-2009-10-3-r25"
   # tag "bioinformatics"
 
-  url "https://github.com/BenLangmead/bowtie/archive/v1.2.0.tar.gz"
-  sha256 "dc4e7951b8eca56ce7714c47fd4e84f72badd5312ee9546c912af1963570f894"
+  url "https://github.com/BenLangmead/bowtie/archive/v1.2.1.1.tar.gz"
+  sha256 "1b38408b88f61d18d7ff28b2470a8cfeefccb3fc59fd46e4cc62e23874e52c20"
   head "https://github.com/BenLangmead/bowtie.git"
 
   bottle do
@@ -18,28 +18,61 @@ class Bowtie < Formula
 
   depends_on "tbb"
 
+  # Needed for the test
+  resource "Sys::Info" do
+    url "https://cpan.metacpan.org/authors/id/B/BU/BURAK/Sys-Info-0.78.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/B/BU/BURAK/Sys-Info-0.78.tar.gz"
+    sha256 "93b2d7d2a670ed0dfb2d524a3cb7f446aeaced5cd3aaa91fc18ac7ba016707e0"
+  end
+  resource "Sys::Info::Base" do
+    url "https://cpan.metacpan.org/authors/id/B/BU/BURAK/Sys-Info-Base-0.7804.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/B/BU/BURAK/Sys-Info-Base-0.7804.tar.gz"
+    sha256 "96ca63d624aaf658aa6869df61cac11df93353041958a3821ed0ca34b6d4611c"
+  end
   if OS.linux?
-    # Needed for the test
     resource "Perl::Clone" do
       url "https://cpan.metacpan.org/authors/id/G/GA/GARU/Clone-0.38.tar.gz"
       mirror "http://search.cpan.org/CPAN/authors/id/G/GA/GARU/Clone-0.38.tar.gz"
       sha256 "9fb0534bb7ef6ca1f6cc1dc3f29750d6d424394d14c40efdc77832fad3cebde8"
     end
+    resource "Sys::Info::Driver::Linux" do
+      url "https://cpan.metacpan.org/authors/id/B/BU/BURAK/Sys-Info-Driver-Linux-0.7903.tar.gz"
+      mirror "http://search.cpan.org/CPAN/authors/id/B/BU/BURAK/Sys-Info-Driver-Linux-0.7903.tar.gz"
+      sha256 "76507907902ab6ef68651629731da3e2100d10ec2c09d5fcdef0346ad747739c"
+    end
     resource "Test::Deep" do
-      url "https://cpan.metacpna.org/authors/id/R/RJ/RJBS/Test-Deep-1.126.tar.gz"
+      url "https://cpan.metacpan.org/authors/id/R/RJ/RJBS/Test-Deep-1.126.tar.gz"
       mirror "http://search.cpan.org/CPAN/authors/id/R/RJ/RJBS/Test-Deep-1.126.tar.gz"
       sha256 "159b42451e4018d9da97994f4ac46d5166abf9b6f343db30071c8fd1cfe0c7c2"
+    end
+    resource "Unix::Processors" do
+      url "https://cpan.metacpan.org/authors/id/W/WS/WSNYDER/Unix-Processors-2.045.tar.gz"
+      mirror "http://search.cpan.org/CPAN/authors/id/W/WS/WSNYDER/Unix-Processors-2.045.tar.gz"
+      sha256 "babd860eda5180939265b3f5241f41e677d5da558fac9420a9ed198ddd12a01e"
+    end
+  end
+  if OS.mac?
+    resource "Mac::PropertyList" do
+      url "https://cpan.metacpan.org/authors/id/B/BD/BDFOY/Mac-PropertyList-1.41.tar.gz"
+      mirror "http://search.cpan.org/CPAN/authors/id/B/BD/BDFOY/Mac-PropertyList-1.41.tar.gz"
+      sha256 "db7f1b3a42a615e7b51df6b3a7b1b51b1ba980e22944c3722cb035362ad95757"
+    end
+    resource "Sys::Info::Driver::OSX" do
+      url "https://cpan.metacpan.org/authors/id/B/BU/BURAK/Sys-Info-Driver-OSX-0.7958.tar.gz"
+      mirror "http://search.cpan.org/CPAN/authors/id/B/BU/BURAK/Sys-Info-Driver-OSX-0.7958.tar.gz"
+      sha256 "875c34cd2f596a83c01ba7572ab98cbe6dd0ccff4a3b9e4192e1c35a2c0f3989"
+    end
+    resource "XML::Entities" do
+      url "https://cpan.metacpan.org/authors/id/S/SI/SIXTEASE/XML-Entities-1.0002.tar.gz"
+      mirror "http://search.cpan.org/CPAN/authors/id/S/SI/SIXTEASE/XML-Entities-1.0002.tar.gz"
+      sha256 "c32aa4f309573d7648ab2e416f62b6b20652f2ad9cfd3eec82fd51101fe7310d"
     end
   end
 
   def install
-    if OS.linux?
-      # Needed for the test
-      resource("Perl::Clone").stage do
-        system "perl", "Makefile.PL", "LIB=#{libexec}/PerlLib", "PREFIX=#{libexec}/vendor"
-        system "make", "install"
-      end
-      resource("Test::Deep").stage do
+    # Needed for the test
+    resources.each do |r|
+      r.stage do
         system "perl", "Makefile.PL", "LIB=#{libexec}/PerlLib", "PREFIX=#{libexec}/vendor"
         system "make", "install"
       end
@@ -58,7 +91,7 @@ class Bowtie < Formula
   end
 
   test do
-    ENV.prepend_path "PERL5LIB", "#{libexec}/PerlLib" unless OS.mac?
+    ENV.prepend_path "PERL5LIB", "#{libexec}/PerlLib"
     system "perl", pkgshare/"scripts/test/simple_tests.pl"
   end
 end
