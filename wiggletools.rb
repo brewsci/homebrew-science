c643ed410345d4d352e47f6f357371ca6c7c665b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-22 01:37:20 +0000

    wiggletools: update 1.2.2_1 bottle for Linuxbrew.

--- wiggletools.rb
+++ wiggletools.rb
@@ -13,6 +13,7 @@ class Wiggletools < Formula
     sha256 "461d183d596237fc2e68094eeed0adfe48d75e8c7c08f1e11e5376c0ac556c22" => :sierra
     sha256 "8020b504935d5eb7145cc47608d6331b6d95411176c8a2b22ff2a011fe1cfd55" => :el_capitan
     sha256 "becc1a3137457844798f374372d24e8c95b50d052f1a46d925094b9a79a9c250" => :yosemite
+    sha256 "b0d40d3b27977a053a2967446c2247d5d18bd939371ba919d8434781b6c0cc3b" => :x86_64_linux
   end
 
   depends_on "curl" unless OS.mac?
