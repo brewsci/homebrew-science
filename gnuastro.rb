51c8031dcefa43a259c5f824df7421ffdca3702f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-22 20:02:47 +0000

    gnuastro: update 0.3_1 bottle.

--- gnuastro.rb
+++ gnuastro.rb
@@ -6,10 +6,9 @@ class Gnuastro < Formula
   revision 1
 
   bottle do
-    sha256 "c3e6ffca6d2b06384b42ddcee53c23fe0d6823765bf4c5b6fd411b10ba19cfa7" => :sierra
-    sha256 "279a30626c3c236f0ce1f14fb2f77dd3338cfcd5dc05e67858b52c06749deab6" => :el_capitan
-    sha256 "62279cca6247ff8c6609b769e88c2b1e776116caa760097993daff43d609115c" => :yosemite
-    sha256 "66293178d82284d4e37039e0a20175442b054541d1e3687b12334231cb6e7f51" => :x86_64_linux
+    sha256 "c5d68842386eafb97a282d409052683bf1a2b86ddabcbb0cfdd6467850f2248b" => :sierra
+    sha256 "9496f71903f53f64d1633709d64e0231f7772ded8ba2e75a629d3f3a3c5ea120" => :el_capitan
+    sha256 "3e236f37762dd8311abc07353325c861316d6778d1cf33e1b5822a11e35feb9e" => :yosemite
   end
 
   depends_on "cfitsio"
