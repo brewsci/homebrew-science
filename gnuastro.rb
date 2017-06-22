3167a9d6984e86b36f5715f839828ec33fae262b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-22 19:36:57 +0000

    gnuastro: update 0.3_1 bottle for Linuxbrew.

--- gnuastro.rb
+++ gnuastro.rb
@@ -9,6 +9,7 @@ class Gnuastro < Formula
     sha256 "c5d68842386eafb97a282d409052683bf1a2b86ddabcbb0cfdd6467850f2248b" => :sierra
     sha256 "9496f71903f53f64d1633709d64e0231f7772ded8ba2e75a629d3f3a3c5ea120" => :el_capitan
     sha256 "3e236f37762dd8311abc07353325c861316d6778d1cf33e1b5822a11e35feb9e" => :yosemite
+    sha256 "f8983b5207a75cb27cbf676937ebb5f03dc7821229c7c072d1a27df092808481" => :x86_64_linux
   end
 
   depends_on "cfitsio"
