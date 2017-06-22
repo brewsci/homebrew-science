f7772a2ff807c28e95bb2faaeb7f551b803c87a2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-22 19:34:41 +0000

    hotspot: update 4.1.0_2 bottle for Linuxbrew.

--- hotspot.rb
+++ hotspot.rb
@@ -12,6 +12,7 @@ class Hotspot < Formula
     sha256 "e10854edf239f5e85f11b77ab48235aa9a68bfc2184e622a03e26d17be45f3df" => :sierra
     sha256 "c32b4b12e774d784ce592bb565c1905eec6b007d0a587bb806b46c2d0a0be06f" => :el_capitan
     sha256 "35da611217337015692fadf3c41e7e0c24d59d419f20847ec3fd732bb6bf6637" => :yosemite
+    sha256 "cd399ba58e6fa37937630411aa6814349c049e2ec96d57615c26a07967f65492" => :x86_64_linux
   end
 
   depends_on "gsl"
