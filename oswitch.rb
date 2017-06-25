9650a8f7b567ca7e494687750a7899d0a9bd4186
ilovezfs <ilovezfs@icloud.com>
2017-06-25 09:56:50 -0700

    oswitch 0.2.7
    Closes #5899.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- oswitch.rb
+++ oswitch.rb
@@ -1,8 +1,8 @@
 class Oswitch < Formula
   desc "Docker environments to work with Bioinformatics tools"
   homepage "https://github.com/yeban/oswitch"
-  url "https://github.com/yeban/oswitch/archive/v0.2.6.tar.gz"
-  sha256 "af3ce13305be8747242dc11bdd35360b568ea738be8c490d448915d4c675871f"
+  url "https://github.com/yeban/oswitch/archive/v0.2.7.tar.gz"
+  sha256 "69090f628213fb93dccedbd8772d462fb7100c20c4495d4079d5f4d9bb84524c"
 
   bottle do
     cellar :any_skip_relocation
@@ -10,6 +10,8 @@ class Oswitch < Formula
     sha256 "33c34d78279af4034cae565ea10cb3bbffe34c32929255e34f6ce063ce8d5ee0" => :yosemite
     sha256 "b515c7a7ca13560f075bb04559b7f8758f446de59627d3ae8f928e450e888d1d" => :mavericks
   end
+
+  depends_on :ruby => "2.0" if OS.linux?
 
   def install
     # Build gem and install to prefix.
