5f0c8cacf432898149b6ab9491857a8b327fa64d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-26 14:43:36 +0000

    samtools: update 1.5 bottle for Linuxbrew.

--- samtools.rb
+++ samtools.rb
@@ -12,6 +12,7 @@ class Samtools < Formula
     sha256 "40fe6e6f0c980d868b2ec75230a1b47deae2d2601b17d5a0d020aac2c0896936" => :sierra
     sha256 "89bf64b374fd0aa557613eba9975d4b613439f781dc2e34823c7d6b0c0963df3" => :el_capitan
     sha256 "5d206a13380f813d6f5a9f6fa01a3904e3d36e9fd5a182e69ea6a69210a2da91" => :yosemite
+    sha256 "8df101a333881891edbd8fab8acec2a0fbd089de4c8f3f14bc6b957d9a9fc9e7" => :x86_64_linux
   end
 
   depends_on "htslib"
