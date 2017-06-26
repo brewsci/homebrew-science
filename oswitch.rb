85d7466f913d2cd1996142d4f48f7cd077444a51
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-26 09:09:39 +0000

    oswitch: update 0.2.7 bottle for Linuxbrew.

--- oswitch.rb
+++ oswitch.rb
@@ -9,6 +9,7 @@ class Oswitch < Formula
     sha256 "9116ee150225d41e743fd5c8bc6bbdee6d321bceef3965c98b4accd295f0274d" => :sierra
     sha256 "1b6a33ff6685f43895d6f83d1b5cdb0d7a92ffa75b99a11c056373f258c9fdf4" => :el_capitan
     sha256 "f9302eed7c95b20561f3d4156bedd954e0c91683606748aa91640f10f01a04eb" => :yosemite
+    sha256 "a98bb8326752204b0e3117c37c8457a15026a9b4b83980d3affe210290b3efdd" => :x86_64_linux
   end
 
   depends_on :ruby => "2.0" if OS.linux?
