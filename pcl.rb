ac4d082d525ba01fa3bc1051d4fe0bba38b84d35
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-30 20:31:48 +0000

    pcl: update 1.8.0_7 bottle for Linuxbrew.

--- pcl.rb
+++ pcl.rb
@@ -14,6 +14,7 @@ class Pcl < Formula
     sha256 "9b3570db0bc4624a00f793c4ada2409036e1d11626a3d434b576623b02065b00" => :sierra
     sha256 "cb7f4b68a7209677821115abfa1048744a7d526a2687580e99044498d075f4d1" => :el_capitan
     sha256 "222272c78558c7516d3dae002f510c9c225e8a4a50374a66d81643cc08672c19" => :yosemite
+    sha256 "558a411b1b361e4336bab9c2ee98880675c401be33c7e6328a29c52ab35396ad" => :x86_64_linux
   end
 
   deprecated_option "with-qt5" => "with-qt"
