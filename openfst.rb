a89982f93485877e6da21e4618b07e53deb52323
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-06 19:55:10 +0000

    openfst: update 1.6.3 bottle for Linuxbrew.

--- openfst.rb
+++ openfst.rb
@@ -9,6 +9,7 @@ class Openfst < Formula
     sha256 "62f64bd93abe2834f07e1ee79a363d1cfa106f4e9faaad8e1f3537f772e88b1e" => :sierra
     sha256 "695595240d43745060ecafa520178c8890dea8b34f47fb5f174f85bcddad9f63" => :el_capitan
     sha256 "97b714421950df1d9693c294e5d8f69ca167474737e43fe6b1305f5269a98b54" => :yosemite
+    sha256 "b20ee657fe8fdd2c8bfde124e1aff050b0724eb061b2bb820a2d928299f24e6e" => :x86_64_linux
   end
 
   needs :cxx11
