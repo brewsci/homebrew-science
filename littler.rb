e46c537bf566b28e1f05ee4f0a1bc644fa8413c7
Eric Hare <ericrhare@gmail.com>
2017-07-06 12:08:15 -0500

    littler: revision for r 3.4.1
    Closes #5933.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- littler.rb
+++ littler.rb
@@ -5,7 +5,7 @@ class Littler < Formula
 
   url "http://dirk.eddelbuettel.com/code/littler/littler-0.2.3.tar.gz"
   sha256 "98cd741c68a5c8f65b06c96d2f56d3b44979b3990335e7869b002c005ef80ba7"
-  revision 11
+  revision 12
   head "https://github.com/eddelbuettel/littler.git"
 
   bottle do
