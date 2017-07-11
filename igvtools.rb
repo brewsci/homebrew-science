1fdc1abed27d0342da5dfba086f4f206bd722c6a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-11 04:50:12 +0000

    igvtools: update 2.3.97 bottle for Linuxbrew.

--- igvtools.rb
+++ igvtools.rb
@@ -10,6 +10,7 @@ class Igvtools < Formula
     sha256 "a395a25dd80489484271d89ca33e33813ee4530bbc30ba555c10ef1d4750fae3" => :sierra
     sha256 "e060e87ce4ebc61a9a4da50097b21dada210d317dc047c9ccd77899610647047" => :el_capitan
     sha256 "78788edbc76b5ac11aa4b42202a4e68e287df575b1664e9e71349b4fe558a228" => :yosemite
+    sha256 "04b0f47824ce14364fd15614a8221b13a6b4145aefea2de95158f94c95e7ed71" => :x86_64_linux
   end
 
   # tag "bioinformatics"
