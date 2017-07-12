c847aeab87e0cb7a39b652140dd35e047e6009df
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-12 11:29:30 +0000

    andi: update 0.11 bottle for Linuxbrew.

--- andi.rb
+++ andi.rb
@@ -12,6 +12,7 @@ class Andi < Formula
     sha256 "c425a345402fa748a7b0db68ee7e64b2d29a5ab3eb9d255efeefb0bc19a4ece3" => :sierra
     sha256 "1948482416f7f91ece5e1e67a99ae2a278340a659d7068f7985a0817b5372634" => :el_capitan
     sha256 "146f7bd5d895fced30bf03c0ea3bd7674d2fcb5ac9aff11e0b0a2bd55965f65d" => :yosemite
+    sha256 "a0a7c09fc0d62dbf6959806a08b244d7fde9ed4853bc0e61c4b336fdee9774c8" => :x86_64_linux
   end
 
   depends_on "gsl"
