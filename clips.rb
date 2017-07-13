13945d3bed3f28f477e43708753cbbf47531ced1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-13 19:11:08 +0000

    clips: add 6.30 bottle for Linuxbrew.

--- clips.rb
+++ clips.rb
@@ -6,10 +6,11 @@ class Clips < Formula
   sha256 "01555b257efae281199b82621ad5cc1106a395acc095b9ba66f40fe50fe3ef1c"
 
   bottle do
-    cellar :any_skip_relocation
+    cellar :any
     sha256 "ec94762baa2bfef3fa4db448ea4ada3d7f876733a8edc68a9128f878d0d9ed04" => :sierra
     sha256 "92882b712f7175ab121a87f5c6f12a2ecab6aa3fbfd67d3b59ca2703d48bbaca" => :el_capitan
     sha256 "82188bf3964afb67b0840ac3d41744be8d85c2a7056e8455e4f2ca2ea0d740df" => :yosemite
+    sha256 "28af635f6aefb1a2f23dd668096c0f0b4c5b405a5cb767d13b35603a0fb8916c" => :x86_64_linux
   end
 
   def install
