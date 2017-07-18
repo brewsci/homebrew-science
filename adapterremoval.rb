72a9626ca9a62ca6e1870fbf62918e4e0c678fb1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-18 04:58:09 +0000

    adapterremoval: update 2.2.2 bottle for Linuxbrew.

--- adapterremoval.rb
+++ adapterremoval.rb
@@ -11,6 +11,7 @@ class Adapterremoval < Formula
     sha256 "b1aae1b89e128b507e119d4ab94844a64e2e817c9b8dc62a50a6b89466d9fd33" => :sierra
     sha256 "8f26751f1ac7176b73270270bfcf6ca917591c64e6bfd722017ed89664165b08" => :el_capitan
     sha256 "939e933e8d3cfd8b733eef9b0baf515813c6065d7b994613270895503a66a385" => :yosemite
+    sha256 "098194dfec4b0dd2436186b2405e08d43c55c8ca56bdee22a53341e359eb9fa7" => :x86_64_linux
   end
 
   depends_on "bzip2" => :recommended
