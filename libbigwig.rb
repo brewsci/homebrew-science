13f3dcd21d9c489aa573a6a16ffa5db32eb39683
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-24 11:41:19 +0000

    libbigwig: update 0.4.0 bottle.

--- libbigwig.rb
+++ libbigwig.rb
@@ -7,10 +7,9 @@ class Libbigwig < Formula
 
   bottle do
     cellar :any
-    sha256 "adf5d1e84f5fc696194caaa3a4f44902c35c69cb8a63f0c049e1bae786671bab" => :sierra
-    sha256 "b1efca94cc405e840a50ef67f8737a0b4e4d195133b8cff57cf6a69ecf660b87" => :el_capitan
-    sha256 "f5b1be46498fd1d89995d36e72a62ade524b00adab054ddbe4a440738f26033e" => :yosemite
-    sha256 "0888f2796d367efa0dc83fd6f8303ffb4ca803657891a6b32fee0968f1327b46" => :x86_64_linux
+    sha256 "9ee6c71ba0dd5695c06ad1fb14eb45fe9b3af606c6c9be2ead46f697ebf9cc18" => :sierra
+    sha256 "39e696991d1f165cb8245387a6879bab3f230792dcb8b16d0ded1a259bc6f6b4" => :el_capitan
+    sha256 "d21ae1e55c07709232942e7f113a2afd0169fa109f594092217f759c267a1682" => :yosemite
   end
 
   depends_on "curl"
