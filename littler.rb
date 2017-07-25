a38e931161f09cf5e06693eeb994eb70a3c14b58
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-25 14:26:45 +0000

    littler: update 0.2.3_14 bottle for Linuxbrew.

--- littler.rb
+++ littler.rb
@@ -12,6 +12,7 @@ class Littler < Formula
     sha256 "e03f8e6b2601bf5f783637880c22e3c218e4111aeeb5c20377288f3e391794ea" => :sierra
     sha256 "1c5ace2ab2899f1d2e0aaa86081bbf291f25fa0f9da82c6a5aa536b366d54aa4" => :el_capitan
     sha256 "fb6b0ba9342eb301ff0ae34259d936bd2f06dad73abc929e48ac8d8ec8114deb" => :yosemite
+    sha256 "20aa581201bdf6a21bc974a1dc3432860011d1b4eea7c7b226bac123a283f5f7" => :x86_64_linux
   end
 
   depends_on "r"
