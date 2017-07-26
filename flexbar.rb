536ec66f8332bd566b87bdabc758958c760106e6
Jonathan Chang <jchang641@gmail.com>
2017-07-25 18:14:45 -0700

    flexbar: fix spacing issues

--- flexbar.rb
+++ flexbar.rb
@@ -4,6 +4,9 @@ class Flexbar < Formula
   url "https://github.com/seqan/flexbar/archive/v3.0.3.tar.gz"
   sha256 "f606f9af540fcaee757bb3b48f228535e4e405ae8affbf411f6a3b22cd7f6967"
   head "https://github.com/seqan/flexbar.git"
+  # tag "bioinformatics"
+  # doi "10.3390/biology1030895"
+
   bottle do
     cellar :any
     sha256 "cd4855a994ec369d2a2be4037436770a18f1c0018a631b2904e77b802003fe36" => :sierra
@@ -11,9 +14,6 @@ class Flexbar < Formula
     sha256 "baba15a73024448b052ed340f337ac7d2e4171692218a44b531085fe6e222bd3" => :yosemite
     sha256 "6cfd83496a680e6e30d27a223a396c4ce44a871fe004ade4e0927cf3125ee651" => :x86_64_linux
   end
-
-  # tag "bioinformatics"
-  # doi "10.3390/biology1030895"
 
   depends_on "cmake" => :build
   depends_on "seqan" => :build
