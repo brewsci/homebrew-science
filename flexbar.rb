53b766b077306d7c8256ad6075c80b4c64f4496e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-26 00:58:22 +0000

    flexbar: add 3.0.3 bottle.

--- flexbar.rb
+++ flexbar.rb
@@ -4,6 +4,13 @@ class Flexbar < Formula
   url "https://github.com/seqan/flexbar/archive/v3.0.3.tar.gz"
   sha256 "f606f9af540fcaee757bb3b48f228535e4e405ae8affbf411f6a3b22cd7f6967"
   head "https://github.com/seqan/flexbar.git"
+  bottle do
+    cellar :any
+    sha256 "cd4855a994ec369d2a2be4037436770a18f1c0018a631b2904e77b802003fe36" => :sierra
+    sha256 "053892e4788d33c20a4f2bdf91d4083baeb87b37241fbdf5bbb4df261a31def9" => :el_capitan
+    sha256 "baba15a73024448b052ed340f337ac7d2e4171692218a44b531085fe6e222bd3" => :yosemite
+  end
+
   # tag "bioinformatics"
   # doi "10.3390/biology1030895"
 
