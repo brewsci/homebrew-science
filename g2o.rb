a7308d97b2c7b6b51c9dbdb9313904102c798d8e
ilovezfs <ilovezfs@icloud.com>
2017-07-30 08:09:52 -0700

    g2o 2017-07-30
    Closes #6027.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- g2o.rb
+++ g2o.rb
@@ -1,10 +1,9 @@
 class G2o < Formula
   desc "General Framework for Graph Optimization"
   homepage "http://openslam.org/g2o.html"
-  url "https://github.com/RainerKuemmerle/g2o/archive/20160424_git.tar.gz"
-  version "2016-04-24"
-  sha256 "31abd5f4acf8407c18318b04f3ffc867c8ea7de89f18d51f9e92eb734d523b24"
-  revision 3
+  url "https://github.com/RainerKuemmerle/g2o/archive/20170730_git.tar.gz"
+  version "2017-07-30"
+  sha256 "799a7a943de423f7514c6cfdc7ed1c7393a15a73ff88aa07ad3fdd571d777d22"
 
   bottle do
     sha256 "a5aaa1339bd2d6cd057cce4ca7a23afe1040a3c6a7cce4d812a00106e515ecbe" => :sierra
@@ -29,7 +28,7 @@ class G2o < Formula
     cp_r pkgshare/"examples/simple_optimize", testpath
     libs = %w[-lg2o_core -lg2o_csparse_extension -lg2o_solver_csparse -lg2o_stuff -lg2o_types_slam2d -lg2o_types_slam3d]
     cd "simple_optimize" do
-      system ENV.cxx, "simple_optimize.cpp", "-I#{opt_include}", "-I#{Formula["eigen"].opt_include}/eigen3", "-o", "simple_optimize", "-L#{Formula["suite-sparse"].opt_lib}", "-lcxsparse", "-L#{opt_lib}", *libs
+      system ENV.cxx, "-std=c++11", "simple_optimize.cpp", "-I#{opt_include}", "-I#{Formula["eigen"].opt_include}/eigen3", "-o", "simple_optimize", "-L#{Formula["suite-sparse"].opt_lib}", "-lcxsparse", "-L#{opt_lib}", *libs
       system "curl", "-k", "-O", "https://svn.openslam.org/data/svn/g2o/trunk/data/2d/intel/intel.g2o"
       system "./simple_optimize", "intel.g2o"
     end
