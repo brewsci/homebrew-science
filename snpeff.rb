e61c777b03b2a4187a95901f713ef5ccc9b8e620
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-01 08:35:40 +0000

    snpeff: update 4.3p bottle.

--- snpeff.rb
+++ snpeff.rb
@@ -10,10 +10,9 @@ class Snpeff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :sierra
-    sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :el_capitan
-    sha256 "2e9ebb430ee2bcc71831ce17889447b097c5f501a0d3b240d59fb3fed255d962" => :yosemite
-    sha256 "ae791f614cde0602d42da9c83cc352d143982bef4e5119ec497f58831a9a17ef" => :x86_64_linux
+    sha256 "563fa3a5f05e4c5ecc2881286eb0cb1b40a5f624b06cee21453fa9fb33a7562c" => :sierra
+    sha256 "563fa3a5f05e4c5ecc2881286eb0cb1b40a5f624b06cee21453fa9fb33a7562c" => :el_capitan
+    sha256 "563fa3a5f05e4c5ecc2881286eb0cb1b40a5f624b06cee21453fa9fb33a7562c" => :yosemite
   end
 
   depends_on :java => "1.8+"
