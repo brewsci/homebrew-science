82eed7d8265f09c51de666771e603eb87f40752d
Jonathan Chang <jchang641@gmail.com>
2017-08-01 17:39:10 -0700

    phlawd 3.4b
    Closes #6048.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- phlawd.rb
+++ phlawd.rb
@@ -1,14 +1,10 @@
 class Phlawd < Formula
   desc "Phylogenetic dataset construction"
   homepage "http://www.phlawd.net/"
-  # The most up to date version of phlawd is the chinchliff fork, which contains
-  # a variety of bug fixes and new features. This fork and the (original)
-  # blackrim fork will eventually be merged.
-  url "https://github.com/chinchliff/phlawd/releases/download/3.4a/phlawd_3.4a_src_with_sqlitewrapped_1.3.1.tar.gz"
-  version "3.4a"
-  sha256 "0ec8e45359af6e932ea4a042fe4f42ddf05b04689a25df937b2d85db41038253"
-  revision 2
-  head "https://github.com/chinchliff/phlawd.git"
+  url "https://github.com/jonchang/phlawd/archive/3.4b.tar.gz"
+  version "3.4b"
+  sha256 "a0fea43866e425f7fed5f74bcb8c391484a10b486f3f03d5b7bbc4df84dd84b8"
+  head "https://github.com/jonchang/phlawd.git"
   # doi "10.1186/1471-2148-9-37"
   # tag "bioinformatics"
 
@@ -21,9 +17,6 @@ class Phlawd < Formula
     sha256 "c51b6f732f0acb671eecdf4604fb4603fa4e5f40dad96e77d6e0f47b5475d872" => :x86_64_linux
   end
 
-  # correct the makefile to look for dependencies where brew installs them
-  patch :DATA
-
   needs :openmp
 
   depends_on "gcc" if OS.mac?
@@ -34,37 +27,14 @@ class Phlawd < Formula
   depends_on "sqlite"
 
   def install
-    # Linux doesn't support -arch
-    if OS.linux?
-      inreplace "sqlitewrapped-1.3.1/Makefile", "-arch x86_64", ""
-      inreplace "src/Makefile.MAC", "-arch x86_64", ""
+    cd "src" do
+      system "./configure"
+      system "make"
+      bin.install "PHLAWD"
     end
-    # compile sqlitewrapped: a dependency included here since it's uncommon and unmaintained
-    system "make", "-C", "sqlitewrapped-1.3.1"
-
-    # compile and install phlawd
-    system "make", "-C", "src", "-f", "Makefile.MAC"
-    bin.install "src/PHLAWD"
   end
 
   test do
-    # currently developing better tests for the next release
     assert_match version.to_s, shell_output("#{bin}/PHLAWD")
   end
 end
-
-__END__
-diff --git a/src/Makefile.MAC b/src/Makefile.MAC
-index a48def0..4b683dd 100644
---- a/src/Makefile.MAC
-+++ b/src/Makefile.MAC
-@@ -91,8 +91,7 @@ all: PHLAWD
- # Tool invocations
- PHLAWD: $(OBJS) $(USER_OBJS)
- 	@echo 'Building target: $@'
--#	$(CC) $(CFLAGS) -L../deps/mac -L/usr/local/lib -L/usr/lib -o "PHLAWD" $(OBJS) $(USER_OBJS) $(LIBS)
--	$(CC) $(CFLAGS) -L../deps/mac -L/usr/local/lib -o "PHLAWD" $(OBJS) $(USER_OBJS) $(LIBS)
-+	$(CC) $(CFLAGS) -L$HOMEBREW_PREFIX/lib -I$HOMEBREW_PREFIX/include -L../sqlitewrapped-1.3.1 -I../sqlitewrapped-1.3.1 -o "PHLAWD" $(OBJS) $(USER_OBJS) $(LIBS)
- 	@echo 'Finished building target: $@'
- 	@echo ' '
- 
