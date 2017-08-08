7f511ac81a6ebadc65aa3516f1663e180c509116
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-08 20:59:15 +0000

    opencv: update 2.4.13.2_1 bottle.

--- opencv.rb
+++ opencv.rb
@@ -9,11 +9,9 @@ class Opencv < Formula
   head "https://github.com/opencv/opencv.git", :branch => "2.4"
 
   bottle do
-    rebuild 1
-    sha256 "181dd2ba326ab518e5d9d07c82304333f86707f50cefd58067ce33df55ef0e4a" => :sierra
-    sha256 "5599a999b8d7baa1982380b784b59bb52fd6c28bd940dee3479bf7d8a521eca8" => :el_capitan
-    sha256 "0bd4816434ac062ec47b19356fa437fe9442710c1cbaede4c65e99603412ac38" => :yosemite
-    sha256 "360c9e975461c600aebe40fba65f888f83effd32a5bf2f210948148b37acae88" => :x86_64_linux
+    sha256 "4ae0b1764da9a01b67fb0d9c8164e759d6e8f0151d4dceae7433fcb609ae80b0" => :sierra
+    sha256 "7e9fe89318390c9861600e6411d5d0213ace469ee8b6dfa0af8971047a941d17" => :el_capitan
+    sha256 "10db2209f60d64d0dccb567d315c217a4ef1429ed7355c4b2728c4d3a316261d" => :yosemite
   end
 
   option "with-java", "Build with Java support"
