59e26584979f668291c509ad100a6fe15f786bc9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-08 19:34:28 +0000

    hdf4: update 4.2.11_3 bottle.

--- hdf4.rb
+++ hdf4.rb
@@ -7,10 +7,9 @@ class Hdf4 < Formula
 
   bottle do
     cellar :any
-    sha256 "62b3ff43680a0e8c9a0a4ea6882b71b061f7f0a6e7cc2fb1be855fa366217973" => :sierra
-    sha256 "260fb928fdf309391a79755d3fe70d510c712193ea8e9aa6a585ffe8745eec6b" => :el_capitan
-    sha256 "fccc9f325d2e532efcc512c5c425efaa21803dedb57ebc95c5069dd7bdb6019f" => :yosemite
-    sha256 "556d2115f449ddda4c48c67149597d14abe3fd3319c791fb08d7877eea595557" => :x86_64_linux
+    sha256 "870c4bd97456d019e7c0bf95d54cefb03cd64e997453c550ca340cc1fb37c0ff" => :sierra
+    sha256 "44bbc921816b8c2002be1e814817199e59410dad2971172e62fce11cfd84d364" => :el_capitan
+    sha256 "534c9fd10793bc1f21517092f23edf9087c8afcce59f722ca908d39dcf051db8" => :yosemite
   end
 
   depends_on "cmake" => :build
