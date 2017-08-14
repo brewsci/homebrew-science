ccb25c0f51acda9466466c03eabc3e719510a335
tmozgach <tmozgach@sfu.ca>
2017-08-14 14:41:39 -0700

    humann2: rebuild the bottle because of new python
    Closes #6134.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- humann2.rb
+++ humann2.rb
@@ -3,6 +3,7 @@ class Humann2 < Formula
   homepage "https://huttenhower.sph.harvard.edu/humann"
   # doi "10.1371/journal.pcbi.1002358"
   # tag "bioinformatics"
+  revision 1
 
   url "https://bitbucket.org/biobakery/humann2/downloads/humann2_v0.4.0.tar.gz"
   sha256 "a9f5272627de6f6bfd42f18e2d6c918982fe0a9f0e4848e7e8d8ed79c91ca77b"
