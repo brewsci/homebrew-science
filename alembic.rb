c5837462fc73a3ce99a4c89544d404f588004090
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-14 12:05:19 +0000

    alembic: update 1.7.3 bottle for Linuxbrew.

--- alembic.rb
+++ alembic.rb
@@ -9,6 +9,7 @@ class Alembic < Formula
     sha256 "6a39b72e4ab69817751669e4e58b43958d7e27102940b5071cab7839f38f5e15" => :sierra
     sha256 "85b2de07c14618a60af0a331e83ea4d7e5c0f84f67084539ff801b6d2108825a" => :el_capitan
     sha256 "1d6af097b50ab39834efb89bd19e9cd44c8c91f78af15e6c1a9d9adc8370347b" => :yosemite
+    sha256 "230a189fb3eda10ae7b6beb127ef046e568ec9e364313c1d976f676c224273bc" => :x86_64_linux
   end
 
   needs :cxx11
