3439636736aa62e567c0989a93b81d738662e4bf
ilovezfs <ilovezfs@icloud.com>
2017-08-13 18:23:27 -0700

    transdecoder 4.0.1
    Closes #6131.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- transdecoder.rb
+++ transdecoder.rb
@@ -2,8 +2,8 @@ class Transdecoder < Formula
   desc "Identifies candidate coding regions within transcript sequences"
   homepage "https://transdecoder.github.io/"
   url "https://github.com/TransDecoder/TransDecoder.git",
-      :tag => "v3.0.1",
-      :revision => "9b52e6933371c89a62b8dde8310d486d7df73d12"
+      :tag => "v4.0.1",
+      :revision => "ad437aee6e06213fd0c8778a7c7e3fbe7a013b5f"
   head "https://github.com/TransDecoder/TransDecoder.git"
   # tag "bioinformatics"
 
@@ -15,21 +15,9 @@ class Transdecoder < Formula
     sha256 "d71073ab9daca3ff95cbba7c241e1066ff7b17a62ce180744e26692e1062caa9" => :x86_64_linux
   end
 
-  depends_on "cd-hit"
-
-  resource "URI::Escape" do
-    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/URI-1.71.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/URI-1.71.tar.gz"
-    sha256 "9c8eca0d7f39e74bbc14706293e653b699238eeb1a7690cc9c136fb8c2644115"
-  end
-
   def install
-    inreplace "TransDecoder.Predict",
-      "= &check_program('cd-hit-est');",
-      "= '#{Formula["cd-hit"].opt_bin}/cd-hit-est';"
-
+    system "make", "test"
     rm "Makefile"
-    rm_rf "transdecoder_plugins" # Remove the cd-hit source code
     rm_rf Dir["TransDecoder.lrgTests/{.git,README.md}"]
 
     prefix.install_metafiles
@@ -39,20 +27,11 @@ class Transdecoder < Formula
       pkgshare.install_symlink libexec/f
     end
 
-    resource("URI::Escape").stage do
-      system "perl", "Makefile.PL", "LIB=#{libexec}/PerlLib", "PREFIX=#{libexec}/vendor"
-      system "make", "install"
-    end
-
     bin.write_exec_script Dir[libexec/"*"].select { |f| File.file?(f) && File.executable?(f) }
   end
 
   test do
     assert_match "USAGE", shell_output("#{bin}/TransDecoder.LongOrfs 2>&1", 1)
     assert_match "USAGE", shell_output("#{bin}/TransDecoder.Predict 2>&1", 1)
-    cp_r "#{libexec}/.", testpath
-    cd "sample_data" do
-      system "make", "test"
-    end
   end
 end
