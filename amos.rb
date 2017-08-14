939d58adaa6179b0c18b2b71ec24c6aa3ebecd7c
Tatyana Mozgacheva <tmozgach@sfu.ca>
2017-08-14 16:06:57 -0700

    amos: Build a bottle for Linuxbrew

--- amos.rb
+++ amos.rb
@@ -1,6 +1,6 @@
 class Amos < Formula
   desc "Modular Open-Source Assembler"
-  homepage "http://sourceforge.net/apps/mediawiki/amos/index.php?title=AMOS"
+  homepage "https://sourceforge.net/projects/amos/"
   url "https://downloads.sourceforge.net/project/amos/amos/3.1.0/amos-3.1.0.tar.gz"
   sha256 "2d9f50e39186ad3dde3d3b28cc265e8d632430657f40fc3978ce34ab1b3db43b"
   revision 1
@@ -16,7 +16,11 @@ class Amos < Formula
   depends_on "blat" => :optional # for minimus2-blat
   depends_on "boost" => :recommended # for Bambus 2
   depends_on "mummer" => :recommended # for minimus2
-  depends_on "Statistics::Descriptive" => :perl
+
+  resource "Statistics::Descriptive" do
+    url "https://www.cpan.org/authors/id/S/SH/SHLOMIF/Statistics-Descriptive-3.0612.tar.gz"
+    sha256 "772413148e5e00efb32f277c4254aa78b9112490a896208dcd0025813afdbf7a"
+  end
 
   fails_with :clang do
     build 600
@@ -24,6 +28,14 @@ class Amos < Formula
   end
 
   def install
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resource("Statistics::Descriptive").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
     # http://seqanswers.com/forums/showthread.php?t=17802
     inreplace "src/Align/find-tandem.cc", "#include <sys/time.h>", "#include <sys/time.h>\n#include <getopt.h>"
 
@@ -41,6 +53,7 @@ class Amos < Formula
 
     system "./configure", *args
     system "make", "install"
+    (bin/"amos").write_env_script("#{libexec}/bin/amos", :PERL5LIB => ENV["PERL5LIB"])
   end
 
   def caveats; <<-EOS.undent
