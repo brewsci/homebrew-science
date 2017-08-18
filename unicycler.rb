81edf7215fdedfb9b731f019822f3e8dfc27cae1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-18 00:26:53 +0000

    unicycler: add 0.4.0 bottle.

--- unicycler.rb
+++ unicycler.rb
@@ -7,6 +7,13 @@ class Unicycler < Formula
   # doi "10.1371/journal.pcbi.1005595"
   # tag "bioinformatics"
 
+  bottle do
+    cellar :any
+    sha256 "ef696a869d7854fbafff01aef5349c57c13af7e9a894655edc30646aaa4b6eba" => :sierra
+    sha256 "874707af9ea2b7aab0a5744c2a47e30d6fe721833adc1f0054d3b25f345f3ce5" => :el_capitan
+    sha256 "7ef48ceb95c5eeae9b2078b85a7299cddd43977735f5d07b65199615c0b7df6a" => :yosemite
+  end
+
   needs :cxx14
 
   depends_on :python3
