50e58a3c16358a24313b810a201fb556f417b8f3
ilovezfs <ilovezfs@icloud.com>
2017-08-19 07:36:38 -0700

    vislcg3 1.1.0
    also serves as revision for icu4c
    
    Closes #6165.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- vislcg3.rb
+++ vislcg3.rb
@@ -2,11 +2,12 @@ require "open3"
 
 class Vislcg3 < Formula
   desc "Constraint grammar for visual interactive syntax learning"
-  homepage "http://beta.visl.sdu.dk/cg3.html"
-  url "http://beta.visl.sdu.dk/download/vislcg3/cg3-0.9.9~r10800.tar.bz2"
-  sha256 "c85446c671fdb55dc01bf6092dd32ccb05ad4e057563d5c4293ee2409df610ba"
-  head "http://beta.visl.sdu.dk/svn/visl/tools/vislcg3/trunk", :using => :svn
-  revision 2
+  homepage "https://beta.visl.sdu.dk/cg3.html"
+  url "https://visl.sdu.dk/download/vislcg3/cg3-1.1.0~r12312.tar.bz2"
+  version "1.1.0"
+  sha256 "b3702b80ee3adceda482ca759918168e18900c63fe6d1a0b70b3c698f8213399"
+  version_scheme 1
+  head "https://github.com/TinoDidriksen/cg3.git"
 
   bottle do
     cellar :any
@@ -15,11 +16,12 @@ class Vislcg3 < Formula
     sha256 "dd5da600c2221cea9a66f86cda03683f61564ac5679aae4f4dcdfc80fcafe844" => :mavericks
   end
 
+  option "without-test", "Disable build-time checking (not recommended)"
+  deprecated_option "without-check" => "without-test"
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "icu4c"
-
-  option "without-check", "Disable build-time checking (not recommended)"
 
   def install
     # Patch submitted to tino@didriksen.cc on 2015-11-27
@@ -30,7 +32,7 @@ class Vislcg3 < Formula
     system "cmake", ".", *std_cmake_args
     system "make"
 
-    if build.with? "check"
+    if build.with? "test"
       Open3.popen3("./test/runall.pl", :err => [:child, :out]) do |_, output|
         output.read.each_line do |line|
           raise line if line.start_with?("T_") && !line.end_with?("Success Success.\n")
@@ -42,6 +44,6 @@ class Vislcg3 < Formula
   end
 
   test do
-    system "vislcg3", "--help"
+    system bin/"vislcg3", "--help"
   end
 end
