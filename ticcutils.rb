c775e47392cd12601de43ff285f147e96d5dad2e
ilovezfs <ilovezfs@icloud.com>
2017-08-19 02:38:31 -0700

    ticcutils 0.15
    Closes #6159.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- ticcutils.rb
+++ ticcutils.rb
@@ -1,9 +1,8 @@
 class Ticcutils < Formula
   desc "Tools for the TiCC Software Stack"
-  homepage "http://ilk.uvt.nl/ticcutils/"
-  url "http://software.ticc.uvt.nl/ticcutils-0.7.tar.gz"
-  sha256 "8a72b78da8f69fb09b83f032dffffc636a985fe266c9e8aca6d2a69ba49ec4f9"
-  revision 1
+  homepage "https://ilk.uvt.nl/ticcutils/"
+  url "https://github.com/LanguageMachines/ticcutils/releases/download/v0.15/ticcutils-0.15.tar.gz"
+  sha256 "424413e8f49e403edef61d82baf4e885e9d000fab78d774ff5cac6d9831ab630"
 
   bottle do
     cellar :any
@@ -13,8 +12,12 @@ class Ticcutils < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "libxml2" unless OS.mac?
+
+  needs :cxx11
 
   def install
+    ENV.cxx11
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
