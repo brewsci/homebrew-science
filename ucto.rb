b8188d8a228ef568ec422e70d60db20a282cfced
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-19 10:44:24 +0000

    ucto: update 0.9.6 bottle for Linuxbrew.

--- ucto.rb
+++ ucto.rb
@@ -8,6 +8,7 @@ class Ucto < Formula
     sha256 "da154d4cd2648113d06fd505922a159f97f4b866ed34d1f4877ba907a2b22749" => :sierra
     sha256 "d7efe3bb35ac31bfdacd09bd4da7336d16cd970a83a58fa6399f56d28ba37779" => :el_capitan
     sha256 "ab8cbbcaff043acbebe4ca65f1aceab7acff12ecd2883491d2d93f394b6feb10" => :yosemite
+    sha256 "d665537d8665a6b6d7c1e7ef18f2269bc25752988172295d0034b2370fd45af8" => :x86_64_linux
   end
 
   option "without-check", "skip build-time checks (not recommended)"
