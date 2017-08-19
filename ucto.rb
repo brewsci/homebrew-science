33196b79e5efef860e7f8fb48b18ed081291693e
ilovezfs <ilovezfs@icloud.com>
2017-08-19 02:55:51 -0700

    ucto 0.9.6
    Closes #6163.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- ucto.rb
+++ ucto.rb
@@ -1,9 +1,8 @@
 class Ucto < Formula
   desc "Unicode tokenizer"
-  homepage "http://ilk.uvt.nl/ucto/"
-  url "http://software.ticc.uvt.nl/ucto-0.8.0.tar.gz"
-  sha256 "d3de9886aac95b5eaa48c45a6f8e69e3bb0c350f1d827987e802f4a2ccbcd0bc"
-  revision 1
+  homepage "https://ilk.uvt.nl/ucto/"
+  url "https://github.com/LanguageMachines/ucto/releases/download/v0.9.6/ucto-0.9.6.tar.gz"
+  sha256 "c10b19f6f6de36cb22a61e663f6b0eaf1aa6fe6557989edce4a30ed28fc3eb64"
 
   bottle do
     sha256 "d1cdf45fc49ed34d8c5a8e6e860a3248421c760a9c8895645cbbc4b4fbb3a1e7" => :el_capitan
