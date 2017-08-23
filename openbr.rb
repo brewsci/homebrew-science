66af47657d05f8bb6c7ed69ee2958d53c4cac4d7
Michka Popoff <michkapopoff@gmail.com>
2017-08-23 23:29:15 +0200

    openbr: depend on opencv@2
    Closes #6195.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- openbr.rb
+++ openbr.rb
@@ -1,6 +1,14 @@
 class Openbr < Formula
   desc "Open Source Biometric Recognition"
   homepage "http://www.openbiometrics.org/"
+  # Use git to get submodules
+  # url "https://github.com/biometrics/openbr/archive/v1.1.0.tar.gz"
+  # sha256 "42daf5dc1aa8e61ec8fc3305b0665cde952583ca674826e78e7ca48ae8f78821"
+  url "https://github.com/biometrics/openbr.git",
+    :revision => "e6fbbaa60f74fb29bdb432f27f1683526cb84069",
+    :tag => "v1.1.0"
+  revision 1
+  head "https://github.com/biometrics/openbr.git"
 
   bottle do
     sha256 "0e9cecc03ce39f45e26c94a6854bdd1ab922f49cc307209fc008366f67bec309" => :el_capitan
@@ -8,18 +16,9 @@ class Openbr < Formula
     sha256 "ad3e698e6a00c5e0a785811dd193a52a63f08f9856b13a4a6b216f913ab216d8" => :mavericks
   end
 
-  # Use git to get submodules
-  # url "https://github.com/biometrics/openbr/archive/v1.1.0.tar.gz"
-  # sha256 "42daf5dc1aa8e61ec8fc3305b0665cde952583ca674826e78e7ca48ae8f78821"
-  url "https://github.com/biometrics/openbr.git",
-    :revision => "e6fbbaa60f74fb29bdb432f27f1683526cb84069",
-    :tag => "v1.1.0"
-
-  head "https://github.com/biometrics/openbr.git"
-
   depends_on "cmake" => :build
-  depends_on "qt5"
-  depends_on "opencv"
+  depends_on "qt"
+  depends_on "opencv@2"
   depends_on "eigen"
 
   def install
