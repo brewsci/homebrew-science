eea96111cabcb6115779a0a120928ea05630a3eb
Michka Popoff <michkapopoff@gmail.com>
2017-08-23 23:21:28 +0200

    cvblob: depend on opencv@2
    Closes #6194.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- cvblob.rb
+++ cvblob.rb
@@ -2,6 +2,7 @@ class Cvblob < Formula
   homepage "https://code.google.com/p/cvblob/"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/cvblob/cvblob-0.10.4-src.tgz"
   sha256 "94eff3eed03370fac98e9d26a43405a3ac8eb3ca1621157c5738967b95c67bc1"
+  revision 1
 
   bottle do
     cellar :any
@@ -11,7 +12,7 @@ class Cvblob < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "opencv"
+  depends_on "opencv@2"
 
   def install
     system "cmake", ".", *std_cmake_args
