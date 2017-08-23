792a636e9970c4a589e5d3ee47ad8821655e055e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-23 22:01:49 +0000

    cvblob: update 0.10.4_1 bottle for Linuxbrew.

--- cvblob.rb
+++ cvblob.rb
@@ -9,6 +9,7 @@ class Cvblob < Formula
     sha256 "c4a7e3257e7f79040a6ebd787bc1f6b36bd8c4ba9ee46a0f3948c118d0606a7b" => :sierra
     sha256 "63fee00e482dfc23d0093653375c5a31452cbcd7a7695190ec510023c9174490" => :el_capitan
     sha256 "785fb1cbfccf5d2301d9648de027615acf2d9d7d97ab61ff0ed84d951c9cdcea" => :yosemite
+    sha256 "bb5ddd848662b841382a03641cff65fb6d49d213bbfdf737f3da4557931edbfa" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
