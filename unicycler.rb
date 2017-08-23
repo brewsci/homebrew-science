65a2dde1bd72e4d01ebdaad5c193a253c26b76ff
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-23 22:46:08 +0000

    unicycler: update 0.4.1 bottle for Linuxbrew.

--- unicycler.rb
+++ unicycler.rb
@@ -12,6 +12,7 @@ class Unicycler < Formula
     sha256 "84b4dbac901c6a9e1e1fd6153b3f3cbd6530d434b53206c926351223c4431525" => :sierra
     sha256 "5fd4d10e7ba577aec25d0f2984cea31b1f98eed6a49e4b21c77edaaea2a3e952" => :el_capitan
     sha256 "0a7d12716f60d234b8c4352c14bf3cd4895524fa9d6e4565f61e169dd7a3e91f" => :yosemite
+    sha256 "39628dfc7b34305ee8bd69c0e70f264f7fc3b49f9b7efe5a28f919fd884eede9" => :x86_64_linux
   end
 
   needs :cxx14
