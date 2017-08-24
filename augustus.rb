d3db5b0fcecb3d40209aec7a7f5bdf2f0d873300
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-24 23:05:21 +0000

    augustus: update 3.2.2_3 bottle for Linuxbrew.

--- augustus.rb
+++ augustus.rb
@@ -12,6 +12,7 @@ class Augustus < Formula
     sha256 "78d63fb1a78421d41d625938d15be4bbe43f0e1869501b1f600843608f3f4ea6" => :sierra
     sha256 "08b5e43664516c4190695fdde94561b5d08a26df78ff0da4591c6f74b486fbbc" => :el_capitan
     sha256 "a9470544bf17c9f649cf928d18fcc8e653282d9f4af51c5bbce429fe7155b869" => :yosemite
+    sha256 "0e4697dd68ed1e77fbfeb332684c6ab58ed5d7646b6c2df89368ef5d6df0665a" => :x86_64_linux
   end
 
   option "with-cgp",  "Enable comparative gene prediction"
