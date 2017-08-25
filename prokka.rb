1679bd92e567d27fdf17e5c392a9b41265281592
Torsten Seemann <torsten.seemann@gmail.com>
2017-08-25 21:03:46 +1000

    prokka 1.12_1 (remove bundled binaries + rpath-ing)
    Closes #6210.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- prokka.rb
+++ prokka.rb
@@ -1,8 +1,11 @@
 class Prokka < Formula
   desc "Rapid annotation of prokaryotic genomes"
   homepage "http://www.vicbioinformatics.com/software.prokka.shtml"
+
   url "https://github.com/tseemann/prokka/archive/v1.12.tar.gz"
   sha256 "845e46c9db167fb1fc9d16c934b7cfd89ddfeb6cd44bd8f42204ae7b4e87adba"
+  revision 1
+
   head "https://github.com/tseemann/prokka.git"
   # doi "10.1093/bioinformatics/btu153"
   # tag "bioinformatics"
@@ -11,45 +14,22 @@ class Prokka < Formula
   depends_on "XML::Simple" => :perl
   depends_on "Time::Piece" => :perl if OS.linux?
 
-  depends_on "blast"
-  depends_on "infernal"
-  depends_on "hmmer"
   depends_on "aragorn"
+  depends_on "barrnap"
+  depends_on "blast"
+  depends_on "hmmer"
+  depends_on "infernal"
+  depends_on "minced"
+  depends_on "parallel"
   depends_on "prodigal"
   depends_on "tbl2asn"
-  depends_on "parallel"
-  depends_on "minced"
 
-  depends_on "barrnap" => :recommended
   depends_on "rnammer" => :optional
 
-  unless OS.mac?
-    depends_on "patchelf" => :build
-    depends_on "bzip2"
-    depends_on "libidn"
-    depends_on "zlib"
-  end
-
   def install
+    # remove all bundled binaries and use Brew for dependencies
+    rm_r "binaries"
     prefix.install Dir["*"]
-
-    if OS.linux?
-      # Use the brewed libidn, zlib and bzip2 rather than the host's.
-      system "patchelf",
-        "--set-rpath", [HOMEBREW_PREFIX, Formula["libidn"].lib, Formula["zlib"].lib].join(":"),
-        "--set-interpreter", HOMEBREW_PREFIX/"lib/ld.so",
-        prefix/"binaries/linux/tbl2asn"
-
-      system "patchelf",
-        "--set-rpath", [HOMEBREW_PREFIX, Formula["bzip2"].lib, Formula["zlib"].lib].join(":"),
-        "--set-interpreter", HOMEBREW_PREFIX/"lib/ld.so",
-        prefix/"binaries/linux/makeblastdb"
-
-      system "patchelf",
-        "--set-rpath", [HOMEBREW_PREFIX, Formula["bzip2"].lib, Formula["zlib"].lib].join(":"),
-        "--set-interpreter", HOMEBREW_PREFIX/"lib/ld.so",
-        prefix/"binaries/linux/blastp"
-    end
   end
 
   def post_install
@@ -58,5 +38,7 @@ class Prokka < Formula
 
   test do
     assert_match "locustag", shell_output("#{bin}/prokka --help 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/prokka --version 2>&1")
+    assert_match "Kingdoms:", shell_output("#{bin}/prokka --listdb 2>&1")
   end
 end
