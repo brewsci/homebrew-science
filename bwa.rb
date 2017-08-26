6d2eb385cef57cdc576e9233b78dfe0fc2117c86
Shaun Jackman <sjackman@gmail.com>
2017-08-26 13:45:22 -0700

    bwa: Move the comments

--- bwa.rb
+++ bwa.rb
@@ -1,13 +1,11 @@
 class Bwa < Formula
   desc "Burrow-Wheeler Aligner for pairwise alignment of DNA"
   homepage "https://github.com/lh3/bwa"
-  # doi "10.1093/bioinformatics/btp324"
-  # tag "bioinformatics"
-
   url "https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2"
   sha256 "8fecdb5f88871351bbe050c18d6078121456c36ad75c5c78f33a926560ffc170"
-
   head "https://github.com/lh3/bwa.git"
+  # doi "10.1093/bioinformatics/btp324"
+  # tag "bioinformatics"
 
   bottle do
     cellar :any_skip_relocation
