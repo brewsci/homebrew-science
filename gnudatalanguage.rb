8b4f7d483303169df9adc9df53d9b02670bc2194
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-27 23:49:03 +0000

    gnudatalanguage: update 0.9.7_3 bottle for Linuxbrew.

--- gnudatalanguage.rb
+++ gnudatalanguage.rb
@@ -10,6 +10,7 @@ class Gnudatalanguage < Formula
     sha256 "1e4e37991aecc97c7fed700d6103672b7d8379636e471f7ef3bb3220ce52face" => :sierra
     sha256 "2e3f10db29219bb7059ed473cdd4906ec884fd87d50f5fa59b4875be9c388bec" => :el_capitan
     sha256 "f6bcb9cf30ff33debf618b181a10ae0143bf56f49b61f71a7f7c668556e5ce8d" => :yosemite
+    sha256 "4fbc1c1a0eed2caa28183bf37aeb9f50be9a3eabb98f862871242b20f6d0f6dd" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
