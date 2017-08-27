3d8ef768e0eb40e7603f64e1366aa5459ba242cc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-27 23:14:18 +0000

    pspp: update 1.0.1 bottle for Linuxbrew.

--- pspp.rb
+++ pspp.rb
@@ -9,6 +9,7 @@ class Pspp < Formula
     sha256 "16197a2755fcf8c15a57d8caea5994862073437b39de7b921ea695090e661d52" => :sierra
     sha256 "303af89c5baba2a558659ed6b83b8eef8ebdccc68500a7e007e0093ae6f00911" => :el_capitan
     sha256 "199674bd2b1d4a42285edceab3239cdabdede267269fa6750d42dd65d5c2d9a0" => :yosemite
+    sha256 "bf38c1cb76fc83069460792a930335b940564a9e36abca63882b03d54e803c67" => :x86_64_linux
   end
 
   option "without-test", "Skip running the PSPP test suite"
