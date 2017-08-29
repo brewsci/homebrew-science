0111e0e12db2684763c60bf96178c5843d22a9f9
Torsten Seemann <torsten.seemann@gmail.com>
2017-08-29 10:34:31 +1000

    nanopolish 0.7.2
    Closes #6222.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- nanopolish.rb
+++ nanopolish.rb
@@ -2,8 +2,8 @@ class Nanopolish < Formula
   desc "Signal-level algorithms for MinION data"
   homepage "https://github.com/jts/nanopolish"
   url "https://github.com/jts/nanopolish.git",
-        :tag => "v0.6.0",
-        :revision => "bcb46f0ec4d7c60054fad1aeb1028159e0292005"
+        :tag => "v0.7.2",
+        :revision => "55b6768062ac283d0109f63aeab7f63203796505"
   head "https://github.com/jts/nanopolish.git"
 
   # doi "10.1038/nmeth.3444"
@@ -25,43 +25,19 @@ class Nanopolish < Formula
   # Fails to build under yosemite
   depends_on MinimumMacOSRequirement => :el_capitan
 
-  # use Homebrew eigen not the one upstream wants to fetch with wget
-  patch :DATA
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/325c6b2390888d6e98083c3e01e961a135092302/nanopolish/nanopolish.patch"
-    sha256 "10afe036f2f9919508c8c55c27d756cf4771e7c9f8307aa14e4c71ed3aed8645"
-  end
-
   def install
-    ln_s Formula["eigen"].opt_include/"eigen3", "eigen"
-    system "make", "HDF5=#{Formula["hdf5"].opt_prefix}"
+    # Ensure we use brewed 'hdf5' and 'eigen3'
+    # Awaiting patch to allow use of brewed 'htslib' https://github.com/jts/nanopolish/issues/213
+    system "make", "HDF5=", "EIGEN=", "EIGEN_INCLUDE=-I#{Formula["eigen"].opt_include}/eigen3"
     prefix.install "scripts", "nanopolish"
     bin.install_symlink "../nanopolish"
+    pkgshare.install "test"
   end
 
   test do
-    system bin/"nanopolish", "--help"
+    exe = "#{bin}/nanopolish"
+    assert_match "valid commands", shell_output("#{exe} --help")
+    assert_match version.to_s, shell_output("#{exe} --version")
+    assert_match ">channel_8_read_24", shell_output("#{exe} extract #{pkgshare}/test/data/LomanLabz_PC_Ecoli_K12_R7.3_2549_1_ch8_file30_strand.fast5")
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 8072faf..6f38c6f 100644
---- a/Makefile
-+++ b/Makefile
-@@ -69,12 +69,10 @@ lib/libhdf5.a:
-
-
- # Download and install eigen if not already downloaded
--EIGEN=eigen/INSTALL
-+EIGEN=eigen
-
- $(EIGEN):
--	wget http://bitbucket.org/eigen/eigen/get/3.2.5.tar.bz2
--	tar -xjvf 3.2.5.tar.bz2
--	mv eigen-eigen-bdd17ee3b1b3 eigen
-+	echo "using brew eigen"
-
- #
- # Source files
