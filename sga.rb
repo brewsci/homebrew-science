284e56ac1adc60e5635cff41f30f2550a881d9ef
ilovezfs <ilovezfs@icloud.com>
2017-08-30 03:10:47 -0700

    sga 0.10.15
    Closes #6250.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- sga.rb
+++ sga.rb
@@ -1,9 +1,8 @@
 class Sga < Formula
-  desc "de novo genome assembler based on the concept of string graphs"
+  desc "De novo genome assembler based on the concept of string graphs"
   homepage "https://github.com/jts/sga"
-  url "https://github.com/jts/sga/archive/v0.10.14.tar.gz"
-  sha256 "763c011b302e1085048c93d917f081ea9348a8470e222dfd369064548e8b3994"
-  revision 1
+  url "https://github.com/jts/sga/archive/v0.10.15.tar.gz"
+  sha256 "1b18996e6ec47985bc4889a8cbc3cd4dd3a8c7d385ae9f450bd474e36342558b"
   head "https://github.com/jts/sga.git"
   # doi "10.1101/gr.126953.111"
   # tag "bioinformatics"
@@ -20,20 +19,13 @@ class Sga < Formula
   depends_on "google-sparsehash" => :build
   depends_on "bamtools"
 
-  # Fix error: use of undeclared identifier 'omp_set_num_threads'
-  patch do
-    url "https://github.com/jts/sga/commit/e0ffbff4eb8a2c8fa53d55da1043c981d5b3813a.patch"
-    sha256 "8087e4c40d0f57ae10c544de4b738e50f16cc3f8c33aa4ed69034e79e278af69"
-  end
-
   def install
     cd "src" do
       system "./autogen.sh"
-      system "./configure",
-        "--disable-dependency-tracking",
-        "--prefix=#{prefix}",
-        "--with-bamtools=#{Formula["bamtools"].prefix}",
-        "--with-sparsehash=#{Formula["google-sparsehash"].prefix}"
+      system "./configure", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}",
+                            "--with-bamtools=#{Formula["bamtools"].prefix}",
+                            "--with-sparsehash=#{Formula["google-sparsehash"].prefix}"
       system "make", "install"
       bin.install Dir["bin/*"] - Dir["bin/Makefile*"]
     end
