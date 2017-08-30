1ac68c7f28e2a515fd7326b9211de5702c311800
ilovezfs <ilovezfs@icloud.com>
2017-08-30 02:29:08 -0700

    prodigal 2.6.3
    Closes #6249.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- prodigal.rb
+++ prodigal.rb
@@ -1,11 +1,11 @@
 class Prodigal < Formula
+  desc "Microbial gene finding program"
   homepage "http://prodigal.ornl.gov/"
+  url "https://github.com/hyattpd/Prodigal/archive/v2.6.3.tar.gz"
+  sha256 "89094ad4bff5a8a8732d899f31cec350f5a4c27bcbdd12663f87c9d1f0ec599f"
   head "https://github.com/hyattpd/Prodigal.git"
   # doi "10.1186/1471-2105-11-119"
   # tag "bioinformatics"
-
-  url "https://github.com/hyattpd/Prodigal/archive/v2.6.2.tar.gz"
-  sha256 "0409e95dc9fd8df57aff0fe6c9da6895dab5b5a90a28fb2fcdbd52f31c581a55"
 
   bottle do
     cellar :any
@@ -23,6 +23,6 @@ class Prodigal < Formula
   end
 
   test do
-    assert_match "#{version}", shell_output("prodigal -v 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/prodigal -v 2>&1")
   end
 end
