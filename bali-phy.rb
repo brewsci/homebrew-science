80f3dc64c965c6a4ad0012ba3930cb8f7e2212ea
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-30 08:03:36 +0000

    bali-phy: update 2.3.8 bottle for Linuxbrew.

--- bali-phy.rb
+++ bali-phy.rb
@@ -9,6 +9,7 @@ class BaliPhy < Formula
     sha256 "50fa703af91bb78303464e6e02db8603e8722e01a1275c8cc6b44254b9dacfab" => :sierra
     sha256 "62fa80287c762052da28393ff946765c1390c1da0c917cdbac6e98f0600856e9" => :el_capitan
     sha256 "9a243403ab53dc740dbfb5f34eb829fd4aa777bed544ece0d09c040994ad0b14" => :yosemite
+    sha256 "86b5806848c469aaae1df3417ccfcba46468f251765235bb33036678a7176d15" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
