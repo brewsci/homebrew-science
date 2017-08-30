aac6ed2527224d8259f5571231064acb5640bd94
ilovezfs <ilovezfs@icloud.com>
2017-08-30 00:44:10 -0700

    cutadapt 1.14
    Closes #6240.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- cutadapt.rb
+++ cutadapt.rb
@@ -3,8 +3,8 @@ class Cutadapt < Formula
 
   desc "Removes adapter sequences, primers, and poly-A tails"
   homepage "https://github.com/marcelm/cutadapt"
-  url "https://files.pythonhosted.org/packages/47/bf/9045e90dac084a90aa2bb72c7d5aadefaea96a5776f445f5b5d9a7a2c78b/cutadapt-1.11.tar.gz"
-  sha256 "11d0af9b39d6d9de6d186f113c7b361c24e7432425b9fa68fb2857a4656890cf"
+  url "https://files.pythonhosted.org/packages/16/e3/06b45eea35359833e7c6fac824b604f1551c2fc7ba0f2bd318d8dd883eb9/cutadapt-1.14.tar.gz"
+  sha256 "f32990a8b2f8b53f8f4c723ada3d256a8e8476febdd296506764cc8e83397d3d"
   head "https://github.com/marcelm/cutadapt.git"
   # doi "10.14806/ej.17.1.200"
   # tag "bioinformatics"
@@ -18,6 +18,11 @@ class Cutadapt < Formula
   end
 
   depends_on :python3
+
+  resource "xopen" do
+    url "https://files.pythonhosted.org/packages/53/1f/01d32269f01fccf1a6d8d5d795f939cb56274c82f7520cc25152ee8de486/xopen-0.2.1.tar.gz"
+    sha256 "9b054f8c1c906ca416412e8b7430bac4e683a2c5ce1a59e7e62d667418165dfe"
+  end
 
   def install
     virtualenv_create(libexec, "python3")
