12c1da7ea36830375f3cd9b7b60d002b4be625cb
ilovezfs <ilovezfs@icloud.com>
2017-08-30 21:36:37 -0700

    openmeeg 2.4-rc
    Closes #6266.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- openmeeg.rb
+++ openmeeg.rb
@@ -1,11 +1,8 @@
 class Openmeeg < Formula
   desc "Package for low-frequency bio-electromagnetism forward models"
   homepage "https://openmeeg.github.io"
-  url "https://github.com/openmeeg/openmeeg/archive/2.4.prerelease.tar.gz"
-  version "2.4.prerelease"
-  sha256 "fabf9addfcb99d3b3161acde98104a321ec926a1968e444edfce5334adb1ae87"
-  revision 1
-
+  url "https://github.com/openmeeg/openmeeg/archive/2.4-rc.tar.gz"
+  sha256 "431c3598d76b7ef282ca9c7729a0e259d54012246463dea239c7dfc5913cf6a0"
   head "https://github.com/openmeeg/openmeeg.git"
 
   bottle do
