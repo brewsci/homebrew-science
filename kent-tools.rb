a372b8522c2fd40a12e609234ca10bcf8caac27c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-07 22:24:07 +0000

    kent-tools: update 353 bottle.

--- kent-tools.rb
+++ kent-tools.rb
@@ -8,10 +8,9 @@ class KentTools < Formula
 
   bottle do
     cellar :any
-    sha256 "356b9196c81db579621b822cc495c9367de1a553ea8db767f6a2ccc794cff2dc" => :sierra
-    sha256 "2b43e396a7686d7700bc53572e42f0942545e116ed299f8afffe766a31d10adc" => :el_capitan
-    sha256 "bf17f561123f84ebf13bfaee93d9c3f20f26f02dd8823dd330d25b9939f971fe" => :yosemite
-    sha256 "65f6bb521972b42ad08cb9355f68937053688159aa5c50ee649869ee34f8d2d2" => :x86_64_linux
+    sha256 "0182493728c26b8c04e5a1ac784d92fb8550f6f5f88288e1c2436f580f4fe180" => :sierra
+    sha256 "a99b9cd252b872d9ae85652bbb7f9fea0e219982b31b03b8038f1e1fa5403302" => :el_capitan
+    sha256 "6d2f37da7f672f4d8a33bb3d05a1c3080b7133ed104a7143f07a5128fa53241b" => :yosemite
   end
 
   depends_on :mysql
