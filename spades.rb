d0b9b613efba01b7ec22c76a87a3935e5b800235
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-07 06:49:46 +0000

    spades: update 3.11.0 bottle for Linuxbrew.

--- spades.rb
+++ spades.rb
@@ -11,6 +11,7 @@ class Spades < Formula
     sha256 "3bdb26858d30ce01e6a643a8c0660f54097347479583c0f196a2a1a309855d93" => :sierra
     sha256 "768f00bcc6d90f2ce23aba7f886a709825e5232bec8a682db8db7b597fc1ef89" => :el_capitan
     sha256 "04f61cf50f6eca0f9364df55cc69c1199f92dcb8d9a0931fe0de8bb731699cab" => :yosemite
+    sha256 "4c4fb027ab51e5a8c143e2cec064f45e72489fc6bb62b8c7d53fa82493b08b87" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
