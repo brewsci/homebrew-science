67f3f088a95e2c28ca604c14bd4201c1c982069e
Michka Popoff <michkapopoff@gmail.com>
2017-09-12 19:36:24 +0200

    tophat: revision for boost 1.65.1
    Closes #6311.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- tophat.rb
+++ tophat.rb
@@ -3,7 +3,7 @@ class Tophat < Formula
   homepage "https://ccb.jhu.edu/software/tophat"
   url "https://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.1.tar.gz"
   sha256 "37840b96f3219630082b15642c47f5ef95d14f6ee99c06a369b08b3d05684da5"
-  revision 5
+  revision 6
   # doi "10.1093/bioinformatics/btp120"
   # tag "bioinformatics"
 
