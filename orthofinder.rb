3395918acc0f0f35bbed5938ebdbfea02ef6910f
ilovezfs <ilovezfs@icloud.com>
2017-09-22 04:52:15 -0700

    orthofinder 1.1.10 (#6336)

--- orthofinder.rb
+++ orthofinder.rb
@@ -1,8 +1,8 @@
 class Orthofinder < Formula
   desc "Accurate inference of orthologous gene groups made easy"
   homepage "https://github.com/davidemms/OrthoFinder"
-  url "https://github.com/davidemms/OrthoFinder/archive/1.1.8.tar.gz"
-  sha256 "5a34bb4532f4d354d40b8bfa3799defc8bda294ee002255599d55e1e2f41c18d"
+  url "https://github.com/davidemms/OrthoFinder/archive/1.1.10.tar.gz"
+  sha256 "9059efbb980c153237ae227c5c453d17f4ea9ce4c308ab8d4e112cd9da7d3c42"
   head "https://github.com/davidemms/OrthoFinder.git"
   # doi "10.1186/s13059-015-0721-2"
   # tag "bioinformatics"
