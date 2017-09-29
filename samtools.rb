4c1c29791e20613e2b8eb192994e072358db72fa
Torsten Seemann <torsten.seemann@gmail.com>
2017-09-29 12:42:05 +1000

    samtools 1.6

--- samtools.rb
+++ samtools.rb
@@ -4,8 +4,8 @@ class Samtools < Formula
   # doi "10.1093/bioinformatics/btp352"
   # tag "bioinformatics"
 
-  url "https://github.com/samtools/samtools/releases/download/1.5/samtools-1.5.tar.bz2"
-  sha256 "8542da26832ee08c1978713f5f6188ff750635b50d8ab126a0c7bb2ac1ae2df6"
+  url "https://github.com/samtools/samtools/releases/download/1.6/samtools-1.6.tar.bz2"
+  sha256 "ee5cd2c8d158a5969a6db59195ff90923c662000816cc0c41a190b2964dbe49e"
 
   bottle do
     cellar :any
