9683b02ec8d4319598e6ccf7fb045d46e48325ac
Shaun Jackman <sjackman@gmail.com>
2017-10-05 18:29:24 -0700

    methpipe: Add tag bioinformatics

--- methpipe.rb
+++ methpipe.rb
@@ -3,6 +3,7 @@ class Methpipe < Formula
   url "http://smithlabresearch.org/downloads/methpipe-3.4.3.tar.bz2"
   sha256 "56716370211a7b45b0a3a2994afb64d64c15dd362028f9ecd8a0551a6e6d65c3"
   head "https://github.com/smithlabcode/methpipe.git"
+  # tag "bioinformatics"
 
   bottle do
     cellar :any
