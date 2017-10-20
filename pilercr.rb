80bd5301c9fd1edc65f9a3bb1cf68b79385b1eb5
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    pilercr: secure url(s)

--- pilercr.rb
+++ pilercr.rb
@@ -1,6 +1,12 @@
 class Pilercr < Formula
   desc "Finding CRISPR repeats in genome sequences"
-  homepage "http://drive5.com/pilercr/"
+  homepage "https://drive5.com/pilercr/"
+  # doi "doi:10.1186/1471-2105-8-18"
+  # tag "bioinformatics"
+
+  url "https://www.drive5.com/pilercr/pilercr1.06.tar.gz"
+  sha256 "50175f7aa171674cda5ba255631f340f9cc7f80e8cc25135a4cb857147d91068"
+
   bottle do
     cellar :any_skip_relocation
     sha256 "7a16c91e43acf94bc30d43be3fa2b34fff56ebca3e60ddc5b99d7193aa7ebf8f" => :el_capitan
@@ -8,12 +14,6 @@ class Pilercr < Formula
     sha256 "6dda505f38845aa031b274637985c99dbc3060e01f85ee303af2536522e505f3" => :mavericks
     sha256 "cba523fc568944de3a6b7408af159d0b11483a433781b57a26f7e102e484185a" => :x86_64_linux
   end
-
-  # doi "doi:10.1186/1471-2105-8-18"
-  # tag "bioinformatics"
-
-  url "http://www.drive5.com/pilercr/pilercr1.06.tar.gz"
-  sha256 "50175f7aa171674cda5ba255631f340f9cc7f80e8cc25135a4cb857147d91068"
 
   depends_on "muscle"
 
