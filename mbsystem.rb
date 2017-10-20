27dbe4b47ec4f5ecd86cad6f1e8f104ec9955cfd
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    mbsystem: secure url(s)

--- mbsystem.rb
+++ mbsystem.rb
@@ -1,6 +1,6 @@
 class Mbsystem < Formula
   desc "MB-System seafloor mapping software"
-  homepage "http://www.mbari.org/data/mbsystem/mb-cookbook/index.html"
+  homepage "https://www.mbari.org/products/research-software/mb-system/"
   url "ftp://ftp.ldeo.columbia.edu/pub/mbsystem/mbsystem-5.5.2284.tar.gz"
   sha256 "62afc8bf4313720af48caa0c11d7596c4fce263420653fce90b600e99c23e709"
   revision 4
