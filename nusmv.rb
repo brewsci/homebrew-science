c5ca438798623c683fb74b5a453f450795f93f93
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    nusmv: secure url(s)

--- nusmv.rb
+++ nusmv.rb
@@ -1,5 +1,5 @@
 class Nusmv < Formula
-  homepage "http://nusmv.fbk.eu"
+  homepage "http://nusmv.fbk.eu/"
   url "http://nusmv.fbk.eu/distrib/NuSMV-2.5.4.tar.gz"
   sha256 "3c250624cba801b1f62f50733f9507b0f3b3ca557ce1cd65956178eb273f1bdf"
 
@@ -15,7 +15,7 @@ class Nusmv < Formula
     end
 
     cd "nusmv" do
-      system "./configure --enable-zchaff --prefix=#{prefix}"
+      system "./configure", "--enable-zchaff", "--prefix=#{prefix}"
       system "make", "install"
     end
   end
