db387cde9e52b33ff07963d4e45b6499811d3d9e
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    qsopt: secure url(s)

--- qsopt.rb
+++ qsopt.rb
@@ -1,11 +1,11 @@
 class Qsopt < Formula
   desc "Creating, manipulating, and solving LP problems"
-  homepage "http://www.math.uwaterloo.ca/~bico/qsopt/index.html"
+  homepage "https://www.math.uwaterloo.ca/~bico/qsopt/"
   if OS.mac?
-    url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt"
+    url "https://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt"
     sha256 "fa2f135eb6fa14e4c27abc97549ab032f56155170bbe27d2c0094245fe55e60e"
   else
-    url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/linux64/qsopt"
+    url "https://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/linux64/qsopt"
     sha256 "d030c3eea14ea57d96c5b5f85212dde8cf6e3239d7a1522143cc3af960c31b4c"
   end
   version "1.01"
@@ -20,17 +20,17 @@ class Qsopt < Formula
   end
 
   resource "qsopt.h" do
-    url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt.h"
+    url "https://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt.h"
     sha256 "647729f1bd77e1263ecf35e1897c705ef1cb45e2d65dbd9cb8fdf5df5ae65624"
   end
 
   resource "qsopt.a_mac" do
-    url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt.a"
+    url "https://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/mac64/qsopt.a"
     sha256 "95c636054d5b5755c3e98a01d101c6b9292a647b2ac77c6857b0c1f19e43a97e"
   end
 
   resource "qsopt.a_linux" do
-    url "http://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/linux64/qsopt.a"
+    url "https://www.math.uwaterloo.ca/~bico/qsopt/beta/codes/linux64/qsopt.a"
     sha256 "41d7cbd2f9cd6575370120f3f8336593cb461f3d87f6c6934a23171a0cc4c28b"
   end
 
