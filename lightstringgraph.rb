c6cb7bb3655b1f594a05816fe46ec706bf6d69a2
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    lightstringgraph: secure url(s)

--- lightstringgraph.rb
+++ lightstringgraph.rb
@@ -1,18 +1,18 @@
 class Lightstringgraph < Formula
   desc "Lightweight string graph construction"
   homepage "http://lsg.algolab.eu/"
+  # https://arxiv.org/pdf/1405.7520.pdf
+  url "https://github.com/AlgoLab/LightStringGraph/archive/v0.4.0.tar.gz"
+  sha256 "7a1530b147269b285875687fecaecbfd4d94c3db04d33ddeb1d0624547317b00"
+  revision 3
+  head "https://github.com/AlgoLab/LightStringGraph.git"
+
   bottle do
     cellar :any
     sha256 "1d4178768a325048be4bd52b8408adabc01f60f15a1d07e27d98d8d0f132c55c" => :sierra
     sha256 "b4d75609112fcb4f04ea8e5b066e74408fd697224e543b22dac1618eb3e6e380" => :el_capitan
     sha256 "7a2c4769125a547e0a79acda893a80184352d5e0d98ca1c38d45e1f4e67c3e7a" => :x86_64_linux
   end
-
-  # http://arxiv.org/pdf/1405.7520.pdf
-  url "https://github.com/AlgoLab/LightStringGraph/archive/v0.4.0.tar.gz"
-  sha256 "7a1530b147269b285875687fecaecbfd4d94c3db04d33ddeb1d0624547317b00"
-  head "https://github.com/AlgoLab/LightStringGraph.git"
-  revision 3
 
   depends_on "boost"
   depends_on "beetl" => :recommended
