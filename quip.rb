23f76c8dbf43105f22465db95225eb9860213ede
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:56 +0000

    quip: secure url(s)

--- quip.rb
+++ quip.rb
@@ -1,6 +1,6 @@
 class Quip < Formula
-  homepage "http://homes.cs.washington.edu/~dcjones/quip/"
-  url "http://homes.cs.washington.edu/~dcjones/quip/quip-1.1.8.tar.gz"
+  homepage "https://homes.cs.washington.edu/~dcjones/quip/"
+  url "https://homes.cs.washington.edu/~dcjones/quip/quip-1.1.8.tar.gz"
   sha256 "525c697cc239a2f44ea493a3f17dda61ba40f83d7c583003673af9de44775a64"
 
   def install
@@ -10,6 +10,6 @@ class Quip < Formula
   end
 
   test do
-    system "quip --version"
+    system "quip", "--version"
   end
 end
