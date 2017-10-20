12b6928dae814e3ddb24371653a2be53fa7d5aff
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:54 +0000

    ess: secure url(s)

--- ess.rb
+++ ess.rb
@@ -1,7 +1,7 @@
 class Ess < Formula
   desc "Emacs Speaks Statistics"
-  homepage "http://ess.r-project.org/"
-  url "http://ess.r-project.org/downloads/ess/ess-15.09-2.tgz"
+  homepage "https://ess.r-project.org/"
+  url "https://ess.r-project.org/downloads/ess/ess-15.09-2.tgz"
   version "15.09-2"
   sha256 "706c41237e1edf33a369902f503bb25254b2bbb750b9ed1adee244e875264afb"
 
