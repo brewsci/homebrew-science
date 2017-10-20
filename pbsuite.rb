be8dc569f503e69125407b1ba78c7b145e69e982
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:55 +0000

    pbsuite: secure url(s)

--- pbsuite.rb
+++ pbsuite.rb
@@ -1,6 +1,6 @@
 class Pbsuite < Formula
   desc "PBJelly and PBHoney: Software for PacBio sequencing data"
-  homepage "http://sourceforge.net/projects/pb-jelly/"
+  homepage "https://sourceforge.net/projects/pb-jelly/"
   url "https://downloads.sourceforge.net/project/pb-jelly/PBSuite_14.7.14.tgz"
   sha256 "98dcda7598f0ecf1a4223ba249f699add4438c60f01ae67b07fdca01142cf145"
   # doi "10.1371/journal.pone.0047768", "10.1186/1471-2105-15-180"
@@ -8,10 +8,10 @@ class Pbsuite < Formula
 
   bottle :unneeded
 
-  conflicts_with "bedtools", :because => "Both install bin/bamToFastq"
-
   depends_on "blasr" => :recommended
   depends_on "python"
+
+  conflicts_with "bedtools", :because => "Both install bin/bamToFastq"
 
   def install
     prefix.install Dir["*"]
