cf1f5cb578b7b217a453b8889368524910cf14ca
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:56 +0000

    viennarna: secure url(s)

--- viennarna.rb
+++ viennarna.rb
@@ -1,10 +1,10 @@
 class Viennarna < Formula
   desc "Prediction and comparison of RNA secondary structures"
-  homepage "http://www.tbi.univie.ac.at/~ronny/RNA/"
+  homepage "https://www.tbi.univie.ac.at/~ronny/RNA/"
   # tag "bioinformatics"
   # doi "10.1186/1748-7188-6-26"
 
-  url "http://www.tbi.univie.ac.at/RNA/packages/source/ViennaRNA-2.3.3.tar.gz"
+  url "https://www.tbi.univie.ac.at/RNA/packages/source/ViennaRNA-2.3.3.tar.gz"
   sha256 "cf92c05e54dff32c2135433b6ebaa5330c05de02a1ae8b7c3b7a865d42eea514"
 
   bottle do
