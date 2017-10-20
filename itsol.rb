57b64835ecbc34bce76c37281538e5544f528fc6
Viktor Szakats <vszakats@users.noreply.github.com>
2017-10-20 10:56:54 +0000

    itsol: secure url(s)

--- itsol.rb
+++ itsol.rb
@@ -1,8 +1,8 @@
 class Itsol < Formula
-  homepage "http://www-users.cs.umn.edu/~saad/software/ITSOL"
-  url "http://www-users.cs.umn.edu/~saad/software/ITSOL/ITSOL_2.tar.gz"
-  sha256 "de8f2726e2dbc248e8ccebdbc9ce8515ad47a8c8595cca87264c22b44845736a"
+  homepage "https://www-users.cs.umn.edu/~saad/software/ITSOL"
+  url "https://www-users.cs.umn.edu/~saad/software/ITSOL/ITSOL_2.tar.gz"
   version "2.0"
+  sha256 "de8f2726e2dbc248e8ccebdbc9ce8515ad47a8c8595cca87264c22b44845736a"
 
   depends_on :fortran
 
