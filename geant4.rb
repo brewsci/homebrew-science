eaf33bb2c1ae424b0d8dca01410deecec091b248
David Hall <dchall@mgh.harvard.edu>
2017-10-22 12:02:06 -0400

    geant4 10.3.3
    Simplify dataset installation.
    
    Closes #6410.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- geant4.rb
+++ geant4.rb
@@ -1,9 +1,9 @@
 class Geant4 < Formula
   desc "Simulation toolkit for particle transport through matter"
   homepage "http://geant4.cern.ch"
-  url "http://geant4.cern.ch/support/source/geant4.10.03.p02.tar.gz"
-  version "10.3.2"
-  sha256 "9b7b6a0041c45b84aca4c045d1a35d202c407c12bc2c59d202dc349123f7cf8e"
+  url "http://geant4.cern.ch/support/source/geant4.10.03.p03.tar.gz"
+  version "10.3.3"
+  sha256 "a164f49c038859ab675eec474d08c9d02be8c4be9c0c2d3aa8e69adf89e1e138"
 
   bottle do
     cellar :any
@@ -23,68 +23,57 @@ class Geant4 < Formula
   depends_on "xerces-c" if build.with? "gdml"
   depends_on "linuxbrew/xorg/glu" unless OS.mac?
 
-  resource "G4NEUTRONHPDATA" do
+  resource "G4NDL" do
     url "http://geant4.cern.ch/support/source/G4NDL.4.5.tar.gz"
     sha256 "cba928a520a788f2bc8229c7ef57f83d0934bb0c6a18c31ef05ef4865edcdf8e"
   end
 
-  resource "G4LEDATA" do
+  resource "G4EMLOW" do
     url "http://geant4.cern.ch/support/source/G4EMLOW.6.50.tar.gz"
     sha256 "c97be73fece5fb4f73c43e11c146b43f651c6991edd0edf8619c9452f8ab1236"
   end
 
-  resource "G4LEVELGAMMADATA" do
+  resource "G4PhotonEvaporation" do
     url "http://geant4.cern.ch/support/source/G4PhotonEvaporation.4.3.2.tar.gz"
     sha256 "d4641a6fe1c645ab2a7ecee09c34e5ea584fb10d63d2838248bfc487d34207c7"
   end
 
-  resource "G4RADIOACTIVEDATA" do
+  resource "G4RadioactiveDecay" do
     url "http://geant4.cern.ch/support/source/G4RadioactiveDecay.5.1.1.tar.gz"
     sha256 "f7a9a0cc998f0d946359f2cb18d30dff1eabb7f3c578891111fc3641833870ae"
   end
 
-  resource "G4NEUTRONXSDATA" do
+  resource "G4NEUTRONXS" do
     url "http://geant4.cern.ch/support/source/G4NEUTRONXS.1.4.tar.gz"
     sha256 "57b38868d7eb060ddd65b26283402d4f161db76ed2169437c266105cca73a8fd"
   end
 
-  resource "G4PIIDATA" do
+  resource "G4PII" do
     url "http://geant4.cern.ch/support/source/G4PII.1.3.tar.gz"
     sha256 "6225ad902675f4381c98c6ba25fc5a06ce87549aa979634d3d03491d6616e926"
   end
 
-  resource "G4REALSURFACEDATA" do
+  resource "RealSurface" do
     url "http://geant4.cern.ch/support/source/RealSurface.1.0.tar.gz"
     sha256 "3e2d2506600d2780ed903f1f2681962e208039329347c58ba1916740679020b1"
   end
 
-  resource "G4SAIDXSDATA" do
+  resource "G4SAIDDATA" do
     url "http://geant4.cern.ch/support/source/G4SAIDDATA.1.1.tar.gz"
     sha256 "a38cd9a83db62311922850fe609ecd250d36adf264a88e88c82ba82b7da0ed7f"
   end
 
-  resource "G4ABLADATA" do
+  resource "G4ABLA" do
     url "http://geant4.cern.ch/support/source/G4ABLA.3.0.tar.gz"
     sha256 "99fd4dcc9b4949778f14ed8364088e45fa4ff3148b3ea36f9f3103241d277014"
   end
 
-  resource "G4ENSDFSTATEDATA" do
+  resource "G4ENSDFSTATE" do
     url "http://geant4.cern.ch/support/source/G4ENSDFSTATE.2.1.tar.gz"
     sha256 "933e7f99b1c70f24694d12d517dfca36d82f4e95b084c15d86756ace2a2790d9"
   end
 
   def install
-    (share/"Geant4-#{version}/data/G4NDL4.5").install resource("G4NEUTRONHPDATA")
-    (share/"Geant4-#{version}/data/G4EMLOW6.50").install resource("G4LEDATA")
-    (share/"Geant4-#{version}/data/PhotonEvaporation4.3").install resource("G4LEVELGAMMADATA")
-    (share/"Geant4-#{version}/data/RadioactiveDecay5.1").install resource("G4RADIOACTIVEDATA")
-    (share/"Geant4-#{version}/data/G4NEUTRONXS1.4").install resource("G4NEUTRONXSDATA")
-    (share/"Geant4-#{version}/data/G4PII1.3").install resource("G4PIIDATA")
-    (share/"Geant4-#{version}/data/RealSurface1.0").install resource("G4REALSURFACEDATA")
-    (share/"Geant4-#{version}/data/G4SAIDDATA1.1").install resource("G4SAIDXSDATA")
-    (share/"Geant4-#{version}/data/G4ABLA3.0").install resource("G4ABLADATA")
-    (share/"Geant4-#{version}/data/G4ENSDFSTATE2.1").install resource("G4ENSDFSTATEDATA")
-
     mkdir "geant-build" do
       args = %w[
         ../
@@ -101,6 +90,12 @@ class Geant4 < Formula
       args.concat(std_cmake_args)
       system "cmake", *args
       system "make", "install"
+    end
+  end
+
+  def post_install
+    resources.each do |r|
+      (share/"Geant4-#{version}/data/#{r.name}#{r.version}").install r
     end
   end
 
