41a9db6c5b5ea958360333de06ba75cf0a85eba3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-24 12:20:36 +0000

    quast: update 4.6.0 bottle.

--- quast.rb
+++ quast.rb
@@ -8,10 +8,8 @@ class Quast < Formula
   sha256 "fcb5ac34c7fd71ceedd75fa8da6136e7ef66cdcc9a87216b7ea11a2012f61f56"
 
   bottle do
-    sha256 "b0e39c8e7a45032b00c87110e618def5ab305cebe3b3fbf9bc44b080fb09e972" => :sierra
-    sha256 "33c358b0970717d2410dc41b64fb5dd3e141e7f3f87893ceda36c750cba78bf0" => :el_capitan
-    sha256 "38a0923ae4806858a7099ba64e1f8d2d7100ec13ee703c0f34addf2328db93b6" => :yosemite
-    sha256 "b7d7548f11945ad2ca7484b9a31552c319cf6c027d7dc825f351a7f3960e725d" => :x86_64_linux
+    sha256 "67b4a3574978a0ba1436462648ff0897bb833948a4322a542c35b8ccd7747d47" => :sierra
+    sha256 "c05c9eb8135431aed74343b77a121622bf806d017c0a1695a5a2ee73fc0dca4c" => :el_capitan
   end
 
   depends_on "matplotlib"
