a4a16f256f88f4da39817cf6c5d61e3a5d0db50e
Torsten Seemann <torsten.seemann@gmail.com>
2017-11-07 09:22:56 +1100

    quicktree 2.2
    Closes #6432.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- quicktree.rb
+++ quicktree.rb
@@ -2,8 +2,8 @@ class Quicktree < Formula
   desc "Phylogenetic neighbor-joining tree builder"
   homepage "https://www.sanger.ac.uk/resources/software/quicktree/"
 
-  url "https://github.com/khowe/quicktree/archive/v2.1.tar.gz"
-  sha256 "5e2d7687578c9489a8b6ad026444dfb56e322a1fb8177feee9fcdc66328a0684"
+  url "https://github.com/khowe/quicktree/archive/v2.2.tar.gz"
+  sha256 "e44d9147a81888d6bfed5e538367ecd4e5d373ae882d5eb9649e5e33f54f1bd6"
 
   head "https://github.com/khowe/quicktree.git"
 
@@ -18,9 +18,11 @@ class Quicktree < Formula
   def install
     system "make"
     bin.install "quicktree"
+    doc.install "LICENSE", "README.md"
   end
 
   test do
-    assert_match "UPGMA", shell_output("#{bin}/quicktree -h 2>&1", 0)
+    assert_match "UPGMA", shell_output("#{bin}/quicktree -h 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/quicktree -v 2>&1")
   end
 end
