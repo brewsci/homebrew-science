7bc3815e6c80612c77a9d2434397b719c686e126
ilovezfs <ilovezfs@icloud.com>
2017-11-10 07:08:16 -0800

    lis: unbottle

--- lis.rb
+++ lis.rb
@@ -3,15 +3,9 @@ class Lis < Formula
   homepage "http://www.ssisc.org/lis"
   url "http://www.ssisc.org/lis/dl/lis-1.4.34.tar.gz"
   sha256 "e25fb5ef0c52fa0c66efab626da7d1a4b4082776173ffce632a034ab73a4d292"
-  revision 4
+  revision 5
 
-  bottle do
-    cellar :any
-    sha256 "bb451fc407949d90b2bc87bdf23285c9f8137d1a8711d163b7fda0079717c9f3" => :sierra
-    sha256 "f710200b217fb5d19a2070733df3395228c1da4c732cf1327b80b58357cd17df" => :el_capitan
-    sha256 "b188ef17d4204081f7579578a7197c5b138791de355a094e79612dab4bb92adc" => :yosemite
-    sha256 "8b2935abf0c48ae8687f7f64e035d840b2e20d8f5e1c9960c054b35ee4da5bc8" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest open-mpi"
 
   option "without-test", "Skip build-time tests (not recommended)"
   option "with-saamg",    "build SA-AMG preconditioner"
