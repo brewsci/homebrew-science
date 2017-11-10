ff27a0f899a77f6dd774008b1701247a9b260fa0
ilovezfs <ilovezfs@icloud.com>
2017-11-10 07:18:25 -0800

    therion: unbottle

--- therion.rb
+++ therion.rb
@@ -3,13 +3,9 @@ class Therion < Formula
   homepage "https://therion.speleo.sk"
   url "https://therion.speleo.sk/downloads/therion-5.3.16.tar.gz"
   sha256 "73cda5225725d3e8cadd6fada9e506ab94b093d4e7a9fc90eaf23f8c7be6eb85"
-  revision 3
+  revision 4
 
-  bottle do
-    sha256 "0007c14f7f19f1b6397fe58c367548b18ee27519b20820d53076f1453d3b0dee" => :sierra
-    sha256 "452c8adc1c96ca72e660c155ae84817b7f277567b72a9c3df29b4afa73267009" => :el_capitan
-    sha256 "065654456caef31f092baddccad66f9af6c420f3175cc065f97fd6e1d18c18cd" => :yosemite
-  end
+  bottle :disable, "needs to be rebuilt with latest netcdf"
 
   depends_on "freetype"
   depends_on "imagemagick"
