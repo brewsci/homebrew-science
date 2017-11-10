29a7a33a39d4a595b617aa3fb45522aa9f5c42e2
ilovezfs <ilovezfs@icloud.com>
2017-11-10 06:44:50 -0800

    quaff: unbottle

--- quaff.rb
+++ quaff.rb
@@ -4,15 +4,11 @@ class Quaff < Formula
   url "https://github.com/ihh/quaff/archive/v0.1-alpha.1.tar.gz"
   version "0.1.1"
   sha256 "dc46b8c2fafcd6c5d2322006b47fda7d1877f9ea3a105d2ecaf4ab178d8eb8dd"
-  revision 1
+  revision 2
   head "https://github.com/ihh/quaff.git"
   # tag "bioinformatics"
 
-  bottle do
-    sha256 "fecf7c9febbc1e297a34478efffecf14cb7feffea7ede8581e12b81320875128" => :sierra
-    sha256 "862e1835bbb3323c660f967c4eff050218e3af3bc2bf83e68b67fce7a174aecd" => :el_capitan
-    sha256 "d2285c4d9423cc1cf0b4260e1f39dbbbf417622296062af529ceed30d643be7c" => :yosemite
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "zlib" unless OS.mac?
   depends_on "pkg-config" => :build
