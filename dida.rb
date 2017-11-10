e68134fe2d6921b2e20712854eaa33baf2639ca0
ilovezfs <ilovezfs@icloud.com>
2017-11-10 07:08:15 -0800

    dida: unbottle

--- dida.rb
+++ dida.rb
@@ -6,15 +6,9 @@ class Dida < Formula
 
   url "http://www.bcgsc.ca/platform/bioinfo/software/dida/releases/1.0.1/dida-1.0.1.tar.gz"
   sha256 "251d8b6d40d061eb7a7c49737a1ce41592b89a6c8647a791fb9d64ff26afd7bd"
-  revision 1
+  revision 2
 
-  bottle do
-    cellar :any
-    sha256 "38e35d907cb2841dc82b94de93d5323c3b8136d7e0ff4a0e97e2c96c46529232" => :el_capitan
-    sha256 "6d221aa3bbb2d3886906b75d021dae0534f40de5ee6173488c0aa5b70e411396" => :yosemite
-    sha256 "09d21a097b6b1caa7fbb33b69c09500115f8d5c49a47da810ce64b972593eebb" => :mavericks
-    sha256 "1da64c7a7cbd22ace38891cf5d896138c126488a48123900fa6e57faf6cc39d3" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest open-mpi"
 
   # Fix error: DIDA must be compiled with a C++ compiler that supports OpenMP threading.
   needs :openmp
