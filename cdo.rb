b19b3116c62819f06bd9a28eb3656eb357770e67
ilovezfs <ilovezfs@icloud.com>
2017-11-10 07:18:24 -0800

    cdo: unbottle

--- cdo.rb
+++ cdo.rb
@@ -3,14 +3,9 @@ class Cdo < Formula
   homepage "https://code.zmaw.de/projects/cdo"
   url "https://code.mpimet.mpg.de/attachments/download/15187/cdo-1.9.0.tar.gz"
   sha256 "df367f8c3abf4ab085bcfc61e0205b28a5ecc69b7b83ba398b4d3c874dd69008"
+  revision 1
 
-  bottle do
-    cellar :any
-    sha256 "4767dc0897ec7dbc651baded5141d722f29413217ff830f01961c2c75a9e4d8d" => :sierra
-    sha256 "a39ba567ab6c4e0e23145cb9e1d96bf321c5ef5512b39725abd7f1dfe6baeb46" => :el_capitan
-    sha256 "601f3887779ea747abbee94eba006b5586b637b0273fd7ed142fe1ee3f2c2bd0" => :yosemite
-    sha256 "3a7d5012555b91198889789f0acb7aefe9f3348bd4d71a1fad7542ce55f8ad57" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest netcdf"
 
   option "with-grib2", "Compile Fortran bindings"
   deprecated_option "enable-grib2" => "with-grib2"
