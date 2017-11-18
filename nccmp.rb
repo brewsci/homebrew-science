b9fd4d202e6f35cd4897962a05ad90d03d7c91f1
ilovezfs <ilovezfs@icloud.com>
2017-11-18 12:17:46 -0800

    nccmp 1.8.2.1
    re-enable bottle
    
    Closes #6458.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- nccmp.rb
+++ nccmp.rb
@@ -1,15 +1,14 @@
 class Nccmp < Formula
   desc "Compare two NetCDF files bitwise, semantically or with a tolerance."
   homepage "https://nccmp.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/nccmp/nccmp-1.8.2.0.tar.gz"
-  sha256 "7f5dad4e8670568a71f79d2bcebb08d95b875506d3d5faefafe1a8b3afa14f18"
-  revision 5
-
-  bottle :disable, "needs to be rebuilt with latest netcdf"
+  url "https://downloads.sourceforge.net/project/nccmp/nccmp-1.8.2.1.tar.gz"
+  sha256 "5bbde05f402aa909480436247165eb0c3681bc53e931b6342af808fd59636c8b"
 
   depends_on "netcdf"
 
   def install
+    chmod 0555, "install-sh"
+
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
