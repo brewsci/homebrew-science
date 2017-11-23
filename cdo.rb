31b8f53d90f16e03d9f58ca1ec394e01ffb4fbea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-23 16:44:48 +0000

    cdo: add 1.9.2 bottle.

--- cdo.rb
+++ cdo.rb
@@ -4,6 +4,13 @@ class Cdo < Formula
   url "https://code.mpimet.mpg.de/attachments/download/16035/cdo-1.9.2.tar.gz"
   sha256 "d1c5092167034a48e4b8ada24cf78a1d4b84e364ffbb08b9ca70d13f428f300c"
 
+  bottle do
+    cellar :any
+    sha256 "787310ee2158732c70585046aada3e8d79b77ae47c635505e73e19c5e7caefd6" => :high_sierra
+    sha256 "76253eaeb404fd01e949e59d43ec2fba66b2154c7b026ad032ac22f7330513b9" => :sierra
+    sha256 "9cc65d5e6a09abbfc6af4db8537fce8670c020f54feb18cf676c87cf144e49a4" => :el_capitan
+  end
+
   option "with-grib2", "Compile Fortran bindings"
   deprecated_option "enable-grib2" => "with-grib2"
   option "with-openmp", "Compile with OpenMP support"
