61f2ad0e8f3669e2b344f4029ec342248aea10ac
Roger Leigh <r.leigh@dundee.ac.uk>
2017-12-04 12:43:04 +0000

    ome-files 0.5.0
    Closes #6481.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- ome-files.rb
+++ ome-files.rb
@@ -1,12 +1,11 @@
 class OmeFiles < Formula
+  include Language::Python::Virtualenv
+
   desc "Open Microscopy Environment (OME) File scientific image I/O library"
   homepage "https://www.openmicroscopy.org/site/products/ome-files-cpp/"
-  url "https://downloads.openmicroscopy.org/ome-files-cpp/0.4.0/source/ome-files-cpp-0.4.0.tar.xz"
-  sha256 "eca5741be2b70c26f55423362bcc862762244b3facb95e8d49043f3a610ca280"
-  revision 1
+  url "https://downloads.openmicroscopy.org/ome-files-cpp/0.5.0/source/ome-files-cpp-0.5.0.tar.xz"
+  sha256 "5a89daea8be0efe74baa11431afda7e0b6e240b3ea8d2027e39db80ee67866d7"
   head "https://github.com/ome/ome-files-cpp.git", :branch => "develop", :shallow => false
-
-  bottle :disable, "needs to be rebuilt with latest boost"
 
   option "with-api-docs", "Build API reference"
   option "without-docs", "Build man pages and manual using sphinx"
@@ -31,45 +30,7 @@ class OmeFiles < Formula
     sha256 "58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8"
   end
 
-  # Required python modules for docs
-  resource "sphinx" do
-    url "https://files.pythonhosted.org/packages/source/S/Sphinx/Sphinx-1.2.3.tar.gz"
-    sha256 "94933b64e2fe0807da0612c574a021c0dac28c7bd3c4a23723ae5a39ea8f3d04"
-  end
-
-  resource "docutils" do
-    url "https://files.pythonhosted.org/packages/source/d/docutils/docutils-0.12.tar.gz"
-    sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
-  end
-
-  resource "pygments" do
-    url "https://files.pythonhosted.org/packages/source/P/Pygments/Pygments-2.0.2.tar.gz"
-    sha256 "7320919084e6dac8f4540638a46447a3bd730fca172afc17d2c03eed22cf4f51"
-  end
-
-  resource "jinja2" do
-    url "https://files.pythonhosted.org/packages/source/J/Jinja2/Jinja2-2.7.3.tar.gz"
-    sha256 "2e24ac5d004db5714976a04ac0e80c6df6e47e98c354cb2c0d82f8879d4f8fdb"
-  end
-
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/source/M/MarkupSafe/MarkupSafe-0.23.tar.gz"
-    sha256 "a4ec1aff59b95a14b45eb2e23761a0179e98319da5a7eb76b56ea8cdc7b871c3"
-  end
-
   def install
-    # Set up python modules
-    ENV.prepend_create_path "PYTHONPATH", buildpath/"python/lib/python2.7/site-packages"
-    ENV.prepend_path "PATH", buildpath/"python/bin"
-    ENV.prepend_create_path "CMAKE_PROGRAM_PATH", buildpath/"python/bin"
-    resources.each do |r|
-      next if r.name == "gtest"
-      next if build.without?("docs")
-      r.stage do
-        system "python", *Language::Python.setup_install_args(buildpath/"python")
-      end
-    end
-
     if build.with? "test"
       (buildpath/"gtest-source").install resource("gtest")
 
@@ -81,6 +42,32 @@ class OmeFiles < Formula
         system "cmake", ".", *gtest_args
         system "make", "install"
       end
+    end
+
+    if build.with?("docs")
+      pip_packages = [
+        "alabaster==0.7.10",
+        "Babel==2.5.1",
+        "certifi==2017.11.5",
+        "chardet==3.0.4",
+        "docutils==0.14",
+        "idna==2.6",
+        "imagesize==0.7.1",
+        "Jinja2==2.10",
+        "MarkupSafe==1.0",
+        "Pygments==2.2.0",
+        "pytz==2017.3",
+        "requests==2.18.4",
+        "six==1.11.0",
+        "snowballstemmer==1.2.1",
+        "Sphinx==1.6.5",
+        "sphinxcontrib-websupport==1.0.1",
+        "typing==3.6.2",
+        "urllib3==1.22",
+      ]
+      venv = virtualenv_create(buildpath/"python")
+      venv.pip_install pip_packages if build.with?("docs")
+      ENV.prepend_path "CMAKE_PROGRAM_PATH", buildpath/"python/bin"
     end
 
     args = *std_cmake_args + [
