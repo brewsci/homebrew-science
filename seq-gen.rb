7d554441555f8a09af17974687e8b7b93278ace5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-12 01:17:24 +0000

    seq-gen: add 1.3.4 bottle for Linuxbrew.

--- seq-gen.rb
+++ seq-gen.rb
@@ -9,6 +9,7 @@ class SeqGen < Formula
     sha256 "c9ff4a5b0ee94ab990bd9cbeafe60ab652ed5197bf7111f0cfd8ec76426e89c3" => :high_sierra
     sha256 "772c48c053f90b6826910ee9b02d742397b5348eba99eb7fa75f1e1464ddab0a" => :sierra
     sha256 "f0bab4aea0dfe1f025460c43d7c1d3e39c7ddea35bd06cc4b1c0aa56769b6fcc" => :el_capitan
+    sha256 "ab0fa30e722f79c3dc822c182bacfe9346abf005e373d32877ba3415a25ae422" => :x86_64_linux
   end
 
   def install
