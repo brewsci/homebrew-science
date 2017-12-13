3c39f89441a2f3b82d455de056016ff884563673
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-13 01:20:31 +0000

    quicktree: update 2.2 bottle for Linuxbrew.

--- quicktree.rb
+++ quicktree.rb
@@ -12,6 +12,7 @@ class Quicktree < Formula
     sha256 "8222c84b13bf458277491486e8640e730a55d4ec783da259519257f3acb44fad" => :high_sierra
     sha256 "ccf2bef7031e048b6db14e166350c88eee40e0ea0307f99c2aa21e90ce141f85" => :sierra
     sha256 "063f94991fce68c6ffffcfc9ec15c94744039e2ebedd03ad15e9c0c74a5bc192" => :el_capitan
+    sha256 "105ef0941e85b3bf1fdd939da409ed4c62c4a91e7be8762b79e7706ae9a18b1f" => :x86_64_linux
   end
 
   def install
