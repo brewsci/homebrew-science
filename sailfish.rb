596714f34a8943c8630ad47251339dad39edd828
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:25 -0800

    sailfish: revision for boost

--- sailfish.rb
+++ sailfish.rb
@@ -5,14 +5,9 @@ class Sailfish < Formula
   # tag "bioinformatics"
   url "https://github.com/kingsfordgroup/sailfish/archive/v0.10.1.tar.gz"
   sha256 "a0d6d944382f2e07ffbfd0371132588e2f22bb846ecfc3d3435ff3d81b30d6c6"
-  revision 1
+  revision 2
 
-  bottle do
-    sha256 "16e1cce6665ec8c6587f31e7fdf94bd5d0da0f21392efafd6b6d6464f344284d" => :el_capitan
-    sha256 "62c8f97b957c13e3f9d97b58cd8363c651c4b6a43a9c94710b6ca95900a5ae55" => :yosemite
-    sha256 "16906591aea875161ae8dfeaeeb84f71653c422470e2e68f8ccf9d235344d9a2" => :mavericks
-    sha256 "e6b53b6c2f593384c1f0fea66e10dd386d9145ddf3e6ed34e616e03213f8d74a" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
