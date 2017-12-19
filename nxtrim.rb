8c92e7df6a46675c18dd8ed3998cc97fa12a649c
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:24 -0800

    nxtrim: revision for boost

--- nxtrim.rb
+++ nxtrim.rb
@@ -6,15 +6,10 @@ class Nxtrim < Formula
 
   url "https://github.com/sequencing/NxTrim/archive/v0.4.2.tar.gz"
   sha256 "851dc82e1e503485ae70ea0770563b977a15d7b4dd29c4ca318bec4323355c19"
+  revision 1
   head "https://github.com/sequencing/NxTrim.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "8becf69c161ebb7dd7352b2e7ba4fbde3d288c0be3267ad14fa128c20188e074" => :sierra
-    sha256 "3bbcf6b39105f21d1a7e59be6022272912bf8999ef24f6d0bfbb9cd48a3c1735" => :el_capitan
-    sha256 "e40e333b5b4a9e940c47dce69bbb47bd49fec79a84ba20d4bdf67d7788edb09c" => :yosemite
-    sha256 "c350dfb9ffdbec63e8c367c84093a9bae671f981f8068f344395baf9dd0c13b1" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "boost"
 
