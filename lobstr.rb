3e139695bc283a1937354d936c9497edfec139cb
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:23 -0800

    lobstr: revision for boost

--- lobstr.rb
+++ lobstr.rb
@@ -3,17 +3,11 @@ class Lobstr < Formula
   homepage "http://lobstr.teamerlich.org"
   url "https://github.com/mgymrek/lobstr-code/releases/download/v4.0.6/lobSTR-4.0.6.tar.gz"
   sha256 "f13bfc17eebd4aadd58fd798941318d8278a2da2e64e596027ebc26e004ce31c"
-  revision 2
+  revision 3
   # doi "10.1101/gr.135780.111"
   # tag "bioinformatics"
 
-  bottle do
-    cellar :any
-    sha256 "d7358fd7b7bc1753c94b7b7d96c1cdedd80b71d600caed576f8dcd072cc76287" => :el_capitan
-    sha256 "c5c3b7e8f8d4be75fc313335b86bf2bd23ee25348ae0b13c271524a87e33ee69" => :yosemite
-    sha256 "9807d22a6e867e6e5acf465f489bf6b8e4958f3de9c12ebd09a9960bc30dda9d" => :mavericks
-    sha256 "4d4d4fac3e79836e4a89dd9d0269d7cd987332c8613cbc904ab4aa384c42b8a7" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   head do
     url "https://github.com/mgymrek/lobstr-code.git"
