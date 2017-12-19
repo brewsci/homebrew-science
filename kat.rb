c95746e83c67acd2b69143e72eaaa44064bb7c9c
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:23 -0800

    kat: revision for boost

--- kat.rb
+++ kat.rb
@@ -3,14 +3,10 @@ class Kat < Formula
   homepage "https://github.com/TGAC/KAT"
   url "https://github.com/TGAC/KAT/releases/download/Release-2.3.4/kat-2.3.4.tar.gz"
   sha256 "40ac5e1ea310b4dac35620f74e489a749c355b41d850d297a06c3822f58295e1"
-  revision 1
+  revision 2
   # tag "bioinformatics"
 
-  bottle do
-    sha256 "2ee4951cf052bb1b735e69e7c8e8daeb6ca8ec8b28bc1cd6b43f56700ac9f433" => :sierra
-    sha256 "d44878f2e4a4e19c7dbff4af223bed7e74917892a9a8f03ef40540554cbbb2d2" => :el_capitan
-    sha256 "ee74a2829a1702187eac36a74c457076278cbf93e710a06166186403a28dee4c" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   head do
     url "https://github.com/TGAC/KAT.git"
