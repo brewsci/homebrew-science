4cf0e95b64772115d995231636957d43297541ad
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:25 -0800

    rapsearch2: revision for boost

--- rapsearch2.rb
+++ rapsearch2.rb
@@ -4,7 +4,7 @@ class Rapsearch2 < Formula
   url "https://downloads.sourceforge.net/project/rapsearch2/RAPSearch2.24_64bits.tar.gz"
   version "2.24"
   sha256 "85db4573f4c768b6c3c73bb44ff2611ba48dc6c8d188feb40f44bf7c55de36ce"
-  revision 2
+  revision 3
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btr595"
 
