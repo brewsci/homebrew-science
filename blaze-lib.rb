2d926c95f54fb810189c8d18409445ff7427c49d
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:21 -0800

    blaze-lib: revision for boost

--- blaze-lib.rb
+++ blaze-lib.rb
@@ -3,14 +3,9 @@ class BlazeLib < Formula
   homepage "https://bitbucket.org/blaze-lib/blaze/"
   url "https://bitbucket.org/blaze-lib/blaze/downloads/blaze-2.4.tar.gz"
   sha256 "34af70c8bb4da5fd0017b7c47e5efbfef9aadbabc5aae416582901a4059d1fa3"
-  revision 1
+  revision 2
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b0821e7f8723d7e206c22c4c801a3485767b4a2f717c81bb79bb762bd6ce4183" => :el_capitan
-    sha256 "0a203be18d1275ecef21931798fb52f0fca170526361e1cbbd52ee9cc331ca2f" => :yosemite
-    sha256 "0799170a3a4a5eb34cb5560b9e857d2431fc745812d3e4ff0a36e25923501e59" => :mavericks
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "boost"
 
