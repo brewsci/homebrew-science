828b8942506a3db2d8ff029312f863834ff7ea97
ilovezfs <ilovezfs@icloud.com>
2017-12-19 07:51:24 -0800

    mothur: revision for boost

--- mothur.rb
+++ mothur.rb
@@ -3,16 +3,12 @@ class Mothur < Formula
   homepage "https://www.mothur.org/"
   url "https://github.com/mothur/mothur/archive/v1.39.5.tar.gz"
   sha256 "9f1cd691e9631a2ab7647b19eb59cd21ea643f29b22cde73d7f343372dfee342"
-  revision 2
+  revision 3
   head "https://github.com/mothur/mothur.git"
   # tag "bioinformatics"
   # doi "10.1128/AEM.01541-09"
 
-  bottle do
-    sha256 "636aebc396ee5e1fc67c11255a3faad9e168be01a7792c14ffbf10d1d9cd0f93" => :sierra
-    sha256 "9d1c44ba78ffb070127afe193b22ee2acb04e837dce1b60fea18cced369694c9" => :el_capitan
-    sha256 "9ed3d8adfb234c985562f8b06081a1f4e12afe944e7c1626594ddf7d20865ac4" => :x86_64_linux
-  end
+  bottle :disable, "needs to be rebuilt with latest boost"
 
   depends_on "boost"
   depends_on "readline" unless OS.mac?
