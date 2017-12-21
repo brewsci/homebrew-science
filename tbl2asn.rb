42b7ecb0924bc57528b97a39166934e4a2110ff8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-21 20:01:28 +0000

    tbl2asn: update 25.6 bottle for Linuxbrew.

--- tbl2asn.rb
+++ tbl2asn.rb
@@ -24,6 +24,7 @@ class Tbl2asn < Formula
     sha256 "2081bf977df5f20889cb7c5ebde93a697e907b0ce4b2b45b78a2420f6ccb75c5" => :high_sierra
     sha256 "2452a512d57a512eb025d5f3165e5fb6f4dd5306c1dbbcc831c0fe10b06c17d0" => :sierra
     sha256 "19f8ac7b05c8cde4ff77abb66c1a19b76c55ce83e137b960c27e3476cc9a1010" => :el_capitan
+    sha256 "1e8646d9cbd0894c5c6e620b4dfc72ada878e70b9c07f0e42e30772ad298ad28" => :x86_64_linux
   end
 
   resource "doc" do
