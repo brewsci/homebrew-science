b3baeeeba39386748fd5ff4d4a32ed5731661e30
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-24 08:37:37 +0000

    prank: update 170427 bottle for Linuxbrew.

--- prank.rb
+++ prank.rb
@@ -11,6 +11,7 @@ class Prank < Formula
     sha256 "2ab7158ba5b9ee5ab22d14be9b6eb451f475578b7edd8caa96c5c77435e79f26" => :high_sierra
     sha256 "dff62b6c346bc089c2a941d76f98f4796f985518b49f33fb1a0633f9fcf44174" => :sierra
     sha256 "535477c82397b4de4f74732345040448a99fa908a7c45532a3e730e0aff2cde6" => :el_capitan
+    sha256 "7d08424bdb8b26c063d796b31c10dfd8bc841888b4cc80380baa5c7fa8056277" => :x86_64_linux
   end
 
   depends_on "mafft"
