021545b0358f06fb4d1f6e8e70fec266d6bf2725
ilovezfs <ilovezfs@icloud.com>
2017-12-24 00:18:25 -0800

    ticcutils 0.17
    depend on boost and icu4c
    depend on bzip2 and zlib on Linux
    
    Closes #6557.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- ticcutils.rb
+++ ticcutils.rb
@@ -1,8 +1,8 @@
 class Ticcutils < Formula
   desc "Tools for the TiCC Software Stack"
   homepage "https://ilk.uvt.nl/ticcutils/"
-  url "https://github.com/LanguageMachines/ticcutils/releases/download/v0.15/ticcutils-0.15.tar.gz"
-  sha256 "424413e8f49e403edef61d82baf4e885e9d000fab78d774ff5cac6d9831ab630"
+  url "https://github.com/LanguageMachines/ticcutils/releases/download/v0.17/ticcutils-0.17.tar.gz"
+  sha256 "4e5ed6b66a8595f4bdb75c46458723c6a8001a570ff47c068ea4e1ff1517c8a1"
 
   bottle do
     cellar :any
@@ -13,7 +13,14 @@ class Ticcutils < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "libxml2" unless OS.mac?
+  depends_on "boost"
+  depends_on "icu4c"
+
+  unless OS.mac?
+    depends_on "bzip2"
+    depends_on "libxml2"
+    depends_on "zlib"
+  end
 
   needs :cxx11
 
@@ -21,7 +28,8 @@ class Ticcutils < Formula
     ENV.cxx11
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--with-boost=#{Formula["boost"].opt_prefix}"
     system "make", "install"
   end
 end
