6e83fb963e443fd840783c0bbf145ccac59dc927
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-25 09:54:52 +0000

    bwa: update 0.7.17 bottle for Linuxbrew.

--- bwa.rb
+++ bwa.rb
@@ -12,6 +12,7 @@ class Bwa < Formula
     sha256 "e0e7e4026b7866edd1bdeda620411742b95ceda0fc55ceaa477bfd78aa1628a5" => :high_sierra
     sha256 "beadf03b7a3d202e6931755e0e40fdcf218c3a57b26e701772e6e31b117575bc" => :sierra
     sha256 "4fdd0af4751761bc123211a5f67e25b82827eec68713328107ab24cb11cfa763" => :el_capitan
+    sha256 "5b82162cc3e2f2c3c2da9fc3e47e0118e1bf92e7b5397c1373825f50cf4f31ac" => :x86_64_linux
   end
 
   depends_on "zlib" unless OS.mac?
