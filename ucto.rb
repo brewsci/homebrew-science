7689909a905efb27226271cd1a83a3ab8b4fe5b5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-25 08:43:37 +0000

    ucto: update 0.11 bottle.

--- ucto.rb
+++ ucto.rb
@@ -5,10 +5,9 @@ class Ucto < Formula
   sha256 "f61f2465a1d1e79b964df7807a7957eba1cb4acc21c9cd74e91de662a8d192e8"
 
   bottle do
-    sha256 "da154d4cd2648113d06fd505922a159f97f4b866ed34d1f4877ba907a2b22749" => :sierra
-    sha256 "d7efe3bb35ac31bfdacd09bd4da7336d16cd970a83a58fa6399f56d28ba37779" => :el_capitan
-    sha256 "ab8cbbcaff043acbebe4ca65f1aceab7acff12ecd2883491d2d93f394b6feb10" => :yosemite
-    sha256 "d665537d8665a6b6d7c1e7ef18f2269bc25752988172295d0034b2370fd45af8" => :x86_64_linux
+    sha256 "7112464e306403c095c1bad09cb487acd81fc929ff63cbba390242594fbc2050" => :high_sierra
+    sha256 "56a6f9ffdbb9d7959c9e95771205e58e01cbfd367c3ba2ad549d39e30ed44412" => :sierra
+    sha256 "405afb2d6830686e4e5ebe75c19dd02217e50ea863add31e6d3a41cbde1b6a6b" => :el_capitan
   end
 
   option "without-check", "skip build-time checks (not recommended)"
