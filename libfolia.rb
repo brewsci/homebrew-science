f5d1b41fbe70858ef20190cc4b365bdf6343bda5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-25 08:23:08 +0000

    libfolia: update 1.11 bottle for Linuxbrew.

--- libfolia.rb
+++ libfolia.rb
@@ -9,6 +9,7 @@ class Libfolia < Formula
     sha256 "0db59fd9d1f80c6616d5b7935e9d5f3307cbaa2fec366740a768efabd3794e3c" => :high_sierra
     sha256 "c67dbd706bfe1af2ca072e61243f460e89b3b00e80207ef1513bb163269b7a8e" => :sierra
     sha256 "25bb602a0d4d757aa91779948cc25f589a0475b32e14d4c0d1527a15a5d0d0a8" => :el_capitan
+    sha256 "bfd9792fb8767d6f042bb333ef6c4d09d22640c10dc48af71ca77d74954edf1e" => :x86_64_linux
   end
 
   option "without-test", "skip build-time checks (not recommended)"
