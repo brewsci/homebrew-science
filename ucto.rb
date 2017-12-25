b0aa1fc2df0e2298c38f1310df0a44ffffd1867b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-25 08:45:51 +0000

    ucto: update 0.11 bottle for Linuxbrew.

--- ucto.rb
+++ ucto.rb
@@ -8,6 +8,7 @@ class Ucto < Formula
     sha256 "7112464e306403c095c1bad09cb487acd81fc929ff63cbba390242594fbc2050" => :high_sierra
     sha256 "56a6f9ffdbb9d7959c9e95771205e58e01cbfd367c3ba2ad549d39e30ed44412" => :sierra
     sha256 "405afb2d6830686e4e5ebe75c19dd02217e50ea863add31e6d3a41cbde1b6a6b" => :el_capitan
+    sha256 "aa4f105c027da6ae3040bfc0c9894392f08de23d0d8b5dcccced9aa799e256b9" => :x86_64_linux
   end
 
   option "without-check", "skip build-time checks (not recommended)"
