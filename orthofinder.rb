ee95a29f982811178e17853aa8dc3141ea31afa2
ilovezfs <ilovezfs@icloud.com>
2017-12-25 03:48:34 -0800

    orthofinder 2.1.2 (#6551)
    depend on the scipy formula instead of the pip package

--- orthofinder.rb
+++ orthofinder.rb
@@ -1,8 +1,8 @@
 class Orthofinder < Formula
   desc "Accurate inference of orthologous gene groups made easy"
   homepage "https://github.com/davidemms/OrthoFinder"
-  url "https://github.com/davidemms/OrthoFinder/archive/1.1.10.tar.gz"
-  sha256 "9059efbb980c153237ae227c5c453d17f4ea9ce4c308ab8d4e112cd9da7d3c42"
+  url "https://github.com/davidemms/OrthoFinder/archive/2.1.2.tar.gz"
+  sha256 "638f5f5d4983eacf8f0f20887e2ae12ba8e2b7510838edf9a922d8c8f66b4f8e"
   head "https://github.com/davidemms/OrthoFinder.git"
   # doi "10.1186/s13059-015-0721-2"
   # tag "bioinformatics"
@@ -11,9 +11,9 @@ class Orthofinder < Formula
 
   depends_on "blast"
   depends_on "mcl"
+  depends_on "scipy"
   depends_on "fasttree" => :recommended
   depends_on "mafft" => :recommended
-  depends_on "scipy" => :python
 
   def install
     pkgshare.install "orthofinder/ExampleDataset"
