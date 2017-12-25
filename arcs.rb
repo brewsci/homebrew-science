44a5a1dbb1273297cdfcb6e630ac4cca49e64945
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-25 08:37:40 +0000

    arcs: update 1.0.1 bottle.

--- arcs.rb
+++ arcs.rb
@@ -7,11 +7,9 @@ class Arcs < Formula
   # tag "bioinformatics"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "f116675834ecda922c07024db1e56c6e5603204dc21795486dbfaab9e4a81673" => :sierra
-    sha256 "981dec5fe0f72a2893084e234eb786e5b1c38a67a9a14d758cb13003c532221f" => :el_capitan
-    sha256 "898af980b1167279c1780f1baed5af9b567289ac1b3a1ff60440d436a914ccc7" => :yosemite
-    sha256 "8c88b76af9133db18520d6d04b0b32a3cd02081bfdd78e03febcd6351cb29cb7" => :x86_64_linux
+    sha256 "5fc2a163b0cab7ee8b662fc9894320eb1ffe65480375588f9c46f6a6e32469fc" => :high_sierra
+    sha256 "77faa701b7565e526c7f7d7c8cb9e8643433848daecabfe7c70be238c8211a77" => :sierra
+    sha256 "b0b6e643c146d8738db6e01433ccaf9e17c463a68c70ea8867300b4e15f39362" => :el_capitan
   end
 
   depends_on "autoconf" => :build
