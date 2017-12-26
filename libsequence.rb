55b4d303b3d92868b8818ac0f1e5726390e4992b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-26 15:14:42 +0000

    libsequence: add 1.9.2 bottle for Linuxbrew.

--- libsequence.rb
+++ libsequence.rb
@@ -12,6 +12,7 @@ class Libsequence < Formula
     sha256 "68eb801e1ab47f24b76ca666fb97308cbf3a44628e76c7f8efc4da535ffd9bc8" => :high_sierra
     sha256 "a97325b11c57ef0868674e59ce336e7a08ef08a5ea4985bfbb15b4812d12b728" => :sierra
     sha256 "2cf76ed0b54d947fae9ae83e18070ac105a455214e506aff2163cd8a61b51eba" => :el_capitan
+    sha256 "e0ce37efba1d12163f8133296f2c1cb638739a038b386e0911ab5d64ce20b38b" => :x86_64_linux
   end
 
   cxx11 = if OS.mac?
