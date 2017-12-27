3c8504fc757e0803974db153c6cc3dce20fa3510
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-27 06:33:33 +0000

    iqtree: update 1.6.1 bottle for Linuxbrew.

--- iqtree.rb
+++ iqtree.rb
@@ -11,6 +11,7 @@ class Iqtree < Formula
     sha256 "5ebbc2860628d5ef6600f01a331ad208e9c6d9408e9e88bbfa5dffdfd373d4ce" => :high_sierra
     sha256 "9fb5ec543d750421ccc8d8a756df4fcf7248688cd58b27b29e1c3726b2e1ee44" => :sierra
     sha256 "7ced91d8704659c858ffc73d1acf653722432033699b1c1588c69f74feb00096" => :el_capitan
+    sha256 "edf54f7d3032d45c4898b5bacb6f3dd1348de9f80595a8483c79cb55f52c2d07" => :x86_64_linux
   end
 
   needs :openmp
