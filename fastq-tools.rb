6bb7b6a379564438437a51fbaf3893a292efaf51
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-28 18:45:02 +0000

    fastq-tools: update 0.8 bottle for Linuxbrew.

--- fastq-tools.rb
+++ fastq-tools.rb
@@ -10,6 +10,7 @@ class FastqTools < Formula
     sha256 "fc6d0fd33d43cd37122c1a57ff6378da67eeac2dc38939913547dfd510918854" => :high_sierra
     sha256 "ef3f6cfb458a061dafd325b6f16b6ab6e546084ff246088e085cd07600099970" => :sierra
     sha256 "63edd331dc15dd6d907eb629dbc40d020f3bb3e87d9f96dcb723fb403a724cdf" => :el_capitan
+    sha256 "5be8af5fc1d3265c218ab64c04879ebd31de73391b47a1cd8b1799176220d163" => :x86_64_linux
   end
 
   depends_on "pcre"
