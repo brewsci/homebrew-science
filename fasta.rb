4fe9be753645f5446a9b9111783e14fb8790d3a7
ilovezfs <ilovezfs@icloud.com>
2017-12-28 10:39:35 -0800

    fasta 36.3.8
    depend on zlib for Linux
    
    Closes #6582.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- fasta.rb
+++ fasta.rb
@@ -3,8 +3,8 @@ class Fasta < Formula
   # doi "10.1016/0076-6879(90)83007-V"
   # tag "bioinformatics"
 
-  url "http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.7.tar.gz"
-  sha256 "95fe27b6055e6a4a5f16bb4f52b3d8f188c8e3447fc16c58ddab8e7548b1cac2"
+  url "http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.8.tar.gz"
+  sha256 "e235458afd16591bbcd5b89b639497e20346841091440a2445593318510a2262"
 
   bottle do
     cellar :any
@@ -13,6 +13,8 @@ class Fasta < Formula
     sha256 "6b237170e06c877f6ccd486104b7f8505b927b9ab8d62e7193ff778e96ace18e" => :mountain_lion
     sha256 "cbf9e27307f4a0f88f5f7557b326039495c20b3e692c7a3ee4d605ea77f78f52" => :x86_64_linux
   end
+
+  depends_on "zlib" unless OS.mac?
 
   def install
     mkdir "bin"
@@ -26,6 +28,7 @@ class Fasta < Formula
           raise "Unknown operating system"
         end
     end
+    rm "bin/README"
     bin.install Dir["bin/*"]
     doc.install Dir["doc/*"]
   end
