23566a2da60dd36a6d3de2ed909ecb0feced1233
ilovezfs <ilovezfs@icloud.com>
2017-12-28 09:45:49 -0800

    ccfits 2.5
    Closes #6575.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- ccfits.rb
+++ ccfits.rb
@@ -1,15 +1,11 @@
 class Ccfits < Formula
   homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
-  url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.4.tar.gz"
-  sha256 "ba6c5012b260adf7633f92581279ea582e331343d8c973981aa7de07242bd7f8"
+  url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
+  sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
 
   option "without-check", "Disable build-time checking (not recommended)"
 
   depends_on "cfitsio"
-
-  # patch for OS X Mavericks build system
-  # see https://heasarc.gsfc.nasa.gov/fitsio/CCfits/html/
-  patch :DATA
 
   def install
     system "./configure", "--disable-debug",
@@ -21,32 +17,3 @@ class Ccfits < Formula
     system "make", "install"
   end
 end
-
-__END__
---- a/ColumnVectorData.h
-+++ b/ColumnVectorData.h
-@@ -260,13 +260,16 @@
-           if ( that.m_data.size() != n ) return false;
-           for (size_t i = 0; i < n ; i++)
-           {
--                size_t nn = m_data[i].size();
--                // first check size (also, == on 2 valarrays is only defined if they
--                // are equal in size).
--                if (that.m_data[i].size() != nn ) return false;
--
--                std::valarray<bool> test = (m_data[i] == that.m_data[i]);
--                for (size_t j = 0; j < nn ; j++ ) if ( !test[j] ) return false;
-+                const std::valarray<T>& thisValArray=m_data[i];
-+                const std::valarray<T>& thatValArray=that.m_data[i];
-+                size_t nn = thisValArray.size();
-+                if (thatValArray.size() != nn ) return false;
-+     
-+                for (size_t j = 0; j < nn ; j++ ) 
-+                {
-+                   if (thisValArray[j] != thatValArray[j])
-+                      return false;
-+                }
-           }
-           return true;
-   }
-
