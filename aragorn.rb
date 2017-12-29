94f3816162168c49ef101ef4c4609aa1c92c4118
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 13:43:16 +0000

    aragorn: update 1.2.38 bottle.

--- aragorn.rb
+++ aragorn.rb
@@ -9,12 +9,9 @@ class Aragorn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "2f7ed8dc01aae4b9137f5825aace3aa0ee068b0fbec995703f3ff1cd16ab6402" => :sierra
-    sha256 "88d79d7d6175a458e2452da9f4871d426671a5b1934c14ee38f664aa63659dab" => :el_capitan
-    sha256 "5352a50548ef3e7a9818356f74f208d36acc711f106be2c6a203050e793e414f" => :yosemite
-    sha256 "8e9939797bdb6d1846b0ed296a377b047d051e1699cc58f1bdfc50a176789e33" => :mavericks
-    sha256 "e87c85dddd796c958d1f3c0d9dbe477b601608a2c4a709025257ebb31a6355e2" => :x86_64_linux
+    sha256 "45b1f0c63b4ffc0c564ece26c839259bf723b69c7dbe621ce2d4fe1131a1666c" => :high_sierra
+    sha256 "64bf898f9262a25fe4a7b28e5e542b1a2927ec3a884851d2623b61742eaf4a16" => :sierra
+    sha256 "d59bd3eaf61234b4b584a6f53754f8e7f2f704c6a9c6218dcd23034b34105ef6" => :el_capitan
   end
 
   def install
