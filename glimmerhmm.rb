55855930eb325d1b78e6ced98ddc056338a24c06
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-29 17:13:51 +0000

    glimmerhmm: update 3.0.4 bottle for Linuxbrew.

--- glimmerhmm.rb
+++ glimmerhmm.rb
@@ -11,6 +11,7 @@ class Glimmerhmm < Formula
     sha256 "6c129f46df339601105a9bea74cc34ebd44bfefd70af9602db8728d6efca187b" => :high_sierra
     sha256 "bec2c26c9b1b965de11c91126e799151b14c41854deea4bd3f02e80e1170ceab" => :sierra
     sha256 "085de0bdee7ea9d218357d4d82fc2eed1963e1c3a7b1930b7f8da48f734d707a" => :el_capitan
+    sha256 "64ad84363d8dbe8c904d886748a84efabc536b3fab72516fb3c18977aba3d559" => :x86_64_linux
   end
 
   def install
