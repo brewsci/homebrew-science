3e60b446276596cf23b4ece0a83f26b5b42395a8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 20:11:21 +0000

    last: update 916 bottle.

--- last.rb
+++ last.rb
@@ -9,10 +9,9 @@ class Last < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "42425823c96441555576551dfcde5b343c2c2f5236f59262a5eb8284a890bb2b" => :el_capitan
-    sha256 "8b6623e595b0219cf4747766793a97597048e2859224c2cbfc4cc0688f97454a" => :yosemite
-    sha256 "c1851700dac785895aa0bbea9d0dc31ba8c33b9913933cab2df9e93ebaaa9085" => :mavericks
-    sha256 "bbe981e9a0b324301e75656d8568db46ef6e2c560fa5f819017aa95a1259b78c" => :x86_64_linux
+    sha256 "74d5bd73e6caa407045bd965ce269d38af02de252cb734862e5e3175c6223930" => :high_sierra
+    sha256 "473b6c50184bdc10e12d789706dd72081305330075278034524fa96e262531b2" => :sierra
+    sha256 "1599151f147caa774395b2bdb310a5db82b960853a8a90b2427f010f74988f5c" => :el_capitan
   end
 
   depends_on "zlib" unless OS.mac?
