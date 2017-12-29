07bbe52d6ac9a48d1d1a1f8790aef1df7fc87d43
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-29 20:38:33 +0000

    lis: add 2.0.6 bottle for Linuxbrew.

--- lis.rb
+++ lis.rb
@@ -9,6 +9,7 @@ class Lis < Formula
     sha256 "fd673e526a8b0d09aaf1ebaac24225126169fca6b573043743378ecbf4f474c3" => :high_sierra
     sha256 "5270c7b46956bbefcf51b34889f8c5f591a9961fc5d20c54b122ebfdbaec869f" => :sierra
     sha256 "26b812f8b22b005b210322b456a17dcff9ef025d896759fda41d36358ba925b8" => :el_capitan
+    sha256 "a9d50a8973f4f78ad1b7b2fa9ed7f3e078727e4c71ede0333eb134db5e414059" => :x86_64_linux
   end
 
   option "without-test", "Skip build-time tests (not recommended)"
