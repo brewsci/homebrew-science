26784f7caaeec449dd7563047c2d9f26f8f48e20
ilovezfs <ilovezfs@icloud.com>
2017-12-28 22:27:31 -0800

    last 916
    depend on zlib for Linux
    
    Closes #6591.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- last.rb
+++ last.rb
@@ -1,8 +1,8 @@
 class Last < Formula
   desc "Find similar regions between sequences"
   homepage "http://last.cbrc.jp/"
-  url "http://last.cbrc.jp/last-752.zip"
-  sha256 "e2e8efc1ce1ec9b20cb3554126bf7a86a62fff712a07e156b414554283236ed5"
+  url "http://last.cbrc.jp/last-916.zip"
+  sha256 "57f9155f26b4b81d39d4a9d230df8080c8a8d366b5585ab3574340b790cfec88"
   head "http://last.cbrc.jp/last", :using => :hg
   # doi "10.1101/gr.113985.110"
   # tag "bioinformatics"
@@ -14,6 +14,8 @@ class Last < Formula
     sha256 "c1851700dac785895aa0bbea9d0dc31ba8c33b9913933cab2df9e93ebaaa9085" => :mavericks
     sha256 "bbe981e9a0b324301e75656d8568db46ef6e2c560fa5f819017aa95a1259b78c" => :x86_64_linux
   end
+
+  depends_on "zlib" unless OS.mac?
 
   resource "mito" do
     url "http://last.cbrc.jp/tutorial/files/human-mito.fasta"
