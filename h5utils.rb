34e6dce7eda1cd87e60271a5f0b4da21316710e0
ilovezfs <ilovezfs@icloud.com>
2017-12-28 20:09:18 -0800

    h5utils 1.13
    Closes #6588.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- h5utils.rb
+++ h5utils.rb
@@ -1,9 +1,8 @@
 class H5utils < Formula
   desc "Utilities to work with scientific data in HDF5"
   homepage "http://ab-initio.mit.edu/wiki/index.php/H5utils"
-  url "http://ab-initio.mit.edu/h5utils/h5utils-1.12.1.tar.gz"
-  sha256 "7290290ca5d5d4451d757a70c86baaa70d23a28edb09c951b6b77c22b924a38d"
-  revision 6
+  url "https://github.com/stevengj/h5utils/releases/download/1.13/h5utils-1.13.tar.gz"
+  sha256 "7e8b05942908975455e81e12b0dcbc7bc12e9560c29d5203fce3b25d7de6e494"
 
   bottle do
     sha256 "2bffedf786d2c3212d002528fe56f00bc055222c5128db5078c742e9881417d0" => :sierra
@@ -14,12 +13,6 @@ class H5utils < Formula
 
   depends_on "libpng"
   depends_on "hdf5"
-
-  # A patch is required in order to build h5utils with libpng 1.5
-  patch :p0 do
-    url "https://trac.macports.org/export/102291/trunk/dports/science/h5utils/files/patch-writepng.c"
-    sha256 "b8737b5e4cd6597570b39ce911ffea5bd0173e0e7a6b32620df188b2d260280f"
-  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
