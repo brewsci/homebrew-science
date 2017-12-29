d4cc4ab1c39b9dd3ab22668ff58cd9750a45e0aa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-29 14:32:55 +0000

    fasta: update 36.3.8 bottle for Linuxbrew.

--- fasta.rb
+++ fasta.rb
@@ -11,6 +11,7 @@ class Fasta < Formula
     sha256 "6cba2aee49586bdeb6121e2fe933329b938d927b823bbca4dd58ce45e7c2b379" => :high_sierra
     sha256 "a892fdacc1b440df741f6790de590430dac7a2444c04c49e9d9104b29ddf2a40" => :sierra
     sha256 "3e0a3877f9e711fc6ad856df2f94ed209e4d9732dec982ac02a169c6b46bf6d3" => :el_capitan
+    sha256 "7ac967feb0b5cc8b59a2fe3cbc7786e9a2d5925af50f7dd8db36cd16abf08032" => :x86_64_linux
   end
 
   depends_on "zlib" unless OS.mac?
