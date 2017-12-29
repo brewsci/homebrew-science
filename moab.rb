a81f74bb245d6724aae28452e4ed015c17cb75c2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 20:08:33 +0000

    moab: add 5.0.0 bottle.

--- moab.rb
+++ moab.rb
@@ -5,6 +5,13 @@ class Moab < Formula
   sha256 "df5d5eb8c0d0dbb046de2e60aa611f276cbf007c9226c44a24ed19c570244e64"
   head "https://bitbucket.org/fathomteam/moab.git"
 
+  bottle do
+    cellar :any
+    sha256 "072821d3130afad401b2180a6d6bfe76ceb716d0899c2f8f3cad6c37addf67e1" => :high_sierra
+    sha256 "8b887f52a8acf00983caacbad8a6bab7fc93de480c78a0176695a1d6241fca6c" => :sierra
+    sha256 "a6c240f0e59505610982d58f8087529809485d5629be5d371a3560beb5dbc7fe" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
