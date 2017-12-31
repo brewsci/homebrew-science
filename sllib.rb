887b471a060dbf7e71627e29530ee707ee2f9118
ilovezfs <ilovezfs@icloud.com>
2017-12-31 02:43:57 -0800

    sllib 1.4.5
    depend on bzip2, ncurses, readline and zlib for Linux
    deparallelize "make install"
    
    Closes #6608.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- sllib.rb
+++ sllib.rb
@@ -1,11 +1,19 @@
 class Sllib < Formula
   homepage "https://www.ir.isas.jaxa.jp/~cyamauch/sli/index.html"
-  url "https://www.ir.isas.jaxa.jp/~cyamauch/sli/sllib-1.4.2f.tar.gz"
-  sha256 "dfd14039387db26c27c1895c6dd8ad8035f9166fcd005ddcae8632ee211666dc"
+  url "https://www.ir.isas.jaxa.jp/~cyamauch/sli/sllib-1.4.5.tar.gz"
+  sha256 "149e4677b7636b6feaebe9d2b6f72597829209cc4eda1386bdfa1d5dfb9cc6bc"
+
+  unless OS.mac?
+    depends_on "bzip2"
+    depends_on "ncurses"
+    depends_on "readline"
+    depends_on "zlib"
+  end
 
   def install
     system "sh", "configure", "--prefix=#{prefix}"
     system "make"
+    ENV.deparallelize
     system "make", "install"
   end
 end
