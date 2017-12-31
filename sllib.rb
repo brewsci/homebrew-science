502a75d63860d6c5421b166c291e60cfb0addf3b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-31 11:37:47 +0000

    sllib: add 1.4.5 bottle.

--- sllib.rb
+++ sllib.rb
@@ -3,6 +3,13 @@ class Sllib < Formula
   url "https://www.ir.isas.jaxa.jp/~cyamauch/sli/sllib-1.4.5.tar.gz"
   sha256 "149e4677b7636b6feaebe9d2b6f72597829209cc4eda1386bdfa1d5dfb9cc6bc"
 
+  bottle do
+    cellar :any
+    sha256 "e41419c2d18c872a48c2b5165c9d4a048471c3a9a81b8a6362d88f615ca472ec" => :high_sierra
+    sha256 "65c7396911355831597843d36422ca1ca68cd40f5d56038e870a86b6bee3201d" => :sierra
+    sha256 "83ec434d44643b8b87669fff66e250d31ac0c258923ac0c43fa85a8fe6e8856f" => :el_capitan
+  end
+
   unless OS.mac?
     depends_on "bzip2"
     depends_on "ncurses"
