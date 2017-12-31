749a26a9e2a3dfd02d3b108bcb9c487842cf14c1
ilovezfs <ilovezfs@icloud.com>
2017-12-31 04:01:41 -0800

    wcstools 3.9.5
    Closes #6614.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- wcstools.rb
+++ wcstools.rb
@@ -1,8 +1,8 @@
 class Wcstools < Formula
   desc "Tools for using World Coordinate Systems (WCS) in astronomical images"
   homepage "http://tdc-www.harvard.edu/wcstools/"
-  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.2.tar.gz"
-  sha256 "481fe357cf755426fb8e25f4f890e827cce5de657a4e5044d4e31ce27bef1c8b"
+  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.5.tar.gz"
+  sha256 "b2f9be55fdec29f0c640028a9986771bfd6ab3d2f633953e4c7cc3b410e5fe9c"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,6 +18,6 @@ class Wcstools < Formula
   end
 
   test do
-    system "imhead 2>&1 | grep -q 'IMHEAD'"
+    assert_match "IMHEAD", shell_output("#{bin}/imhead 2>&1", 1)
   end
 end
