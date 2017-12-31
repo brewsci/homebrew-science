6a9b0eb2d378e80280b3682da3bb1b545a9d4405
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-31 10:03:28 +0000

    sfscode: update 20150910 bottle for Linuxbrew.

--- sfscode.rb
+++ sfscode.rb
@@ -9,6 +9,7 @@ class Sfscode < Formula
     sha256 "4e44262e9ab34e4dc1b5d1892afee52b372605734c81d44b79c42946fbe6e54a" => :high_sierra
     sha256 "35bfa0e2fe611812febcb8059d6fe0a3dba64e3f28a1340621ea7bebbbea4973" => :sierra
     sha256 "99c13636f500c799eeda98cf7d5fe70a46030d7f211c94da8c9e8f13dbac0a10" => :el_capitan
+    sha256 "b913b821adb0a665abdc68c4c773d215fa5508aaa7632750df19ba5d0d4069f3" => :x86_64_linux
   end
 
   def install
