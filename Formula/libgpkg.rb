d82cc19da2a69cf6b20065afcaccdaa5968df13a
Shaun Jackman <sjackman@gmail.com>
2018-02-05 16:26:05 -0800

    libgpkg: Depends on ruby to run tests

--- Formula/libgpkg.rb
+++ Formula/libgpkg.rb
@@ -1,15 +1,5 @@
-class RubyVersion19 < Requirement
-  fatal true
-  satisfy(:build_env => false) { `ruby -e 'print RUBY_VERSION'`.strip.to_f >= 1.9 }
-
-  def message; <<~EOS
-      Ruby >= 1.9 is required to run tests, which utilize Encoding class.
-      Install without `--with-tests` option.
-  EOS
-  end
-end
-
 class Libgpkg < Formula
+  desc "SQLite 3 extension that provides a minimal OGC GeoPackage implementation"
   homepage "https://bitbucket.org/luciad/libgpkg"
   url "https://bitbucket.org/luciad/libgpkg/get/0.9.16.tar.gz"
   sha256 "be43a2725f5fcecbe3b1baf95e6c45847f7a9a3d8ac20978b9e11aa765e8d980"
@@ -20,10 +10,7 @@ class Libgpkg < Formula
 
   depends_on "cmake" => :build
   depends_on "geos" => :recommended
-  if build.with? "tests"
-    depends_on RubyVersion19
-    depends_on "bundler" => :ruby
-  end
+  depends_on "ruby" if build.with? "tests"
 
   env :std
 
@@ -41,12 +28,11 @@ class Libgpkg < Formula
   end
 
   def caveats; <<~EOS
-      Custom SQLite command-line shell that autoloads static GeoPackage extension:
-      #{opt_prefix}/bin/gpkg
+    Custom SQLite command-line shell that autoloads static GeoPackage extension:
+    #{opt_prefix}/bin/gpkg
 
-      Make sure to review Usage (extension loading) and Function Reference docs:
-      https://bitbucket.org/luciad/libgpkg/wiki/Home
-
+    Make sure to review Usage (extension loading) and Function Reference docs:
+    https://bitbucket.org/luciad/libgpkg/wiki/Home
   EOS
   end
 end
