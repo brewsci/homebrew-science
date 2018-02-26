eaf906cdfa721644c90053072815f550553b1bd1
Shaun Jackman <sjackman@gmail.com>
2018-02-26 10:33:17 -0800

    sga: Disable bottle
    Missing libraries:
      libbamtools.so.2.4.1

--- Formula/sga.rb
+++ Formula/sga.rb
@@ -7,12 +7,7 @@ class Sga < Formula
   # doi "10.1101/gr.126953.111"
   # tag "bioinformatics"
 
-  bottle do
-    sha256 "6d04064faf78bb77d302783b3977e57d28f8d16d524b42660b2b85fe9363d43b" => :sierra
-    sha256 "ff5ee31db095e42df84c8701afaf4adc95adbdf4efb259c8d9207d1518e0f398" => :el_capitan
-    sha256 "eccaea15d67467f0fce69cdbf83771af215be8c2a8903a93f4e43eb7c0bf63d5" => :yosemite
-    sha256 "d816e1738680e02afa84882f4658af82735f79134529690b66cbb9e2eff17c46" => :x86_64_linux
-  end
+  bottle :disable, "Missing library: libbamtools.so.2.4.1"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
