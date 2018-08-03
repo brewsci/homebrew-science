d87844cc2bdc72f0ed9ef82422c02f204949888d
specter119 <specter119@gmail.com>
2018-08-03 15:20:29 +0800

    add xcrysden

---
+++ Formula/xcrysden.rb
@@ -0,0 +1,46 @@
+class Xcrysden < Formula
+  desc "Crystalline and molecular structure visualisation program"
+  homepage "http://www.xcrysden.org/"
+  url "http://www.xcrysden.org/download/xcrysden-1.5.60.tar.gz"
+  sha256 "a695729f1bb3e486b86a74106c06a392c8aca048dc6b0f20785c3c311cfb2ef4"
+
+  depends_on :x11
+  depends_on "gcc"
+  depends_on "tcl-tk" => "with-x11"
+  depends_on "fftw"
+  depends_on "wget" => :build
+
+  # Fix togl -accum false in Tcl and modify Make.sys
+  patch do
+    url "https://gist.githubusercontent.com/specter119/4f630e538d39edcf67ec742f78c23aab/raw/ff73b6838cc2efd8c4fac3608e1a3f63355382a9/xcrysden-homebrew.patch"
+    sha256 "ad55c57702345a0cfd99364bd7e9dc8982f002409537ef173040bc67ef67dda1"
+  end
+
+  def install
+    cp "system/Make.macosx-x11", "Make.sys"
+
+    ENV.deparallelize
+    system "make", "xcrysden"
+
+    args = %W[
+      prefix=#{prefix}
+    ]
+
+    system "make", *args, "install"
+  end
+
+  def caveats
+    <<~EOS
+      XCrySDen can be user-customized. Create $HOME/.xcrysden/ directory
+      and copy the "custom-definitions" and "Xcrysden_resources" files
+      from the Tcl/ subdirectory of the XCrySDen root directory.
+      These can be then modified according to user preference.
+
+      For more info about customization, see: http://www.xcrysden.org/doc/custom.html
+    EOS
+  end
+
+  test do
+    system bin/"xcrysden", "--version"
+  end
+end
